ad1a670befc68d9ef230589dfd42d6cc67844719
Deepak Michael <mikee76@gmail.com>
2011-07-25 12:00:55 +0530

    Lang package created for files to copy to generated folder

---
+++ java/src/lang/java/com/wordnik/annotations/MethodArgumentNames.java
@@ -0,0 +1,13 @@
+package com.wordnik.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+
+@Target({ElementType.FIELD,ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface MethodArgumentNames {
+	String value() default "";
+}
