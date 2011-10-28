4920da870aa0f75944d402757b5e91075096a0ee
Tony Tam <tony@eatbacon.org>
2011-10-28 09:37:08 -0700

    added php client

---
+++ bin/generate-php-lib.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+if [ $# -ne 4 ]
+then
+    echo "Error in $0 - Invalid Argument Count"
+    echo "Syntax: $0 location_of_service api_key package_name library_root"
+    exit
+fi
+
+echo "" > classpath.txt
+for file in `ls lib`;
+        do echo -n 'lib/' >> classpath.txt;
+        echo -n $file >> classpath.txt;
+        echo -n ':' >> classpath.txt;
+done
+for file in `ls build`;
+	do echo -n 'build/' >> classpath.txt;
+	echo -n $file >> classpath.txt;
+	echo -n ':' >> classpath.txt;
+done
+
+export CLASSPATH=$(cat classpath.txt):conf/php/templates
+export JAVA_OPTS="${JAVA_OPTS} -Dproperty=Xmx2g"
+java $WORDNIK_OPTS $JAVA_CONFIG_OPTIONS $JAVA_OPTS -cp $CLASSPATH com.wordnik.swagger.codegen.config.php.PHPLibCodeGen "$@"
