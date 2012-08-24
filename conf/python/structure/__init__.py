9e3fc6603faeffe1b1caf70dbb8754c2cc6179a5
Tony Tam <fehguy@gmail.com>
2012-08-24 15:27:18 -0700

    updated to 2.0

--- conf/python/structure/__init__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-"""Load all of the modules in the models directory."""
-import os
-
-for module in os.listdir(os.path.dirname(__file__)):
-    if module != '__init__.py' and module[-3:] == '.py':
-        __import__(module[:-3], locals(), globals())
