7e823f879e0d05e1de6a3214c800a26d48255383
Tony Tam <fehguy@gmail.com>
2012-08-24 15:27:27 -0700

    updated to 2.0

--- bin/runscala.sh
+++ bin/runscala.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 echo "" > classpath.txt
-for file in `ls lib`;
-        do echo -n 'lib/' >> classpath.txt;
+for file in `ls target/*.jar`;
+        do echo -n '' >> classpath.txt;
         echo -n $file >> classpath.txt;
         echo -n ':' >> classpath.txt;
 done
-for file in `ls build`;
-	do echo -n 'build/' >> classpath.txt;
+for file in `ls target/lib`;
+	do echo -n 'target/lib/' >> classpath.txt;
 	echo -n $file >> classpath.txt;
 	echo -n ':' >> classpath.txt;
 done
 
-export CLASSPATH=$(cat classpath.txt)
-export JAVA_OPTS="${JAVA_OPTS} -DrulePath=data -Dproperty=Xmx2g -DloggerPath=$BUILD_COMMON/test-config/log4j.properties"
-scala $WORDNIK_OPTS $JAVA_CONFIG_OPTIONS $JAVA_OPTS -cp $CLASSPATH "$@"
+WORDNIK_OPTS=${WORDNIK_OPTS:=NOPE}
 
+if [ $WORDNIK_OPTS = NOPE ];
+	then
+    export WORDNIK_OPTS="-DconfigFile=conf/config.xml -DdevMode=true"
+	
+fi
+
+export CLASSPATH=$(cat classpath.txt)
+export JAVA_OPTS="${JAVA_OPTS} -DrulePath=data -Xmx4096M -DloggerPath=conf/log4j.properties"
+scala $WORDNIK_OPTS $JAVA_CONFIG_OPTIONS -cp $CLASSPATH "$@"
