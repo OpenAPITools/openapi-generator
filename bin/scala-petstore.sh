5773626d539e89c9100aba0b0b742fd91ddf123b
Tony Tam <fehguy@gmail.com>
2012-08-24 18:31:09 -0700

    added petstore sample

--- bin/scala-petstore.sh
+++ bin/scala-petstore.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
-echo "" > classpath.txt
-for file in `ls target/*.jar`;
-        do echo -n '' >> classpath.txt;
-        echo -n $file >> classpath.txt;
-        echo -n ':' >> classpath.txt;
-done
-for file in `ls target/lib`;
-	do echo -n 'target/lib/' >> classpath.txt;
-	echo -n $file >> classpath.txt;
-	echo -n ':' >> classpath.txt;
-done
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-export CLASSPATH=$(cat classpath.txt)
+export CLASSPATH="$DIR/../target/lib/*:$DIR/../target/*"
 export JAVA_OPTS="${JAVA_OPTS} -Xmx1024M -DloggerPath=conf/log4j.properties"
-scala $WORDNIK_OPTS $JAVA_CONFIG_OPTIONS -cp $CLASSPATH "$@" src/main/scala/ScalaPetstoreCodegen.scala http://petstore.swagger.wordnik.com/api/resources.json special-key
+scala $WORDNIK_OPTS $JAVA_CONFIG_OPTIONS -cp $CLASSPATH "$@" ScalaPetstoreCodegen http://petstore.swagger.wordnik.com/api/resources.json special-key
