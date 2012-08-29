de1a4059b19c469cc4d01968a2683cc311593590
Tony Tam <fehguy@gmail.com>
2012-08-29 09:32:12 -0700

    updated some formatting

--- samples/server-generator/scalatra/output/src/main/scala/apis/PetApi.scala
+++ samples/server-generator/scalatra/output/src/main/scala/apis/PetApi.scala
@@ -1,6 +1,5 @@
 package apis
 
-import json.JsonUtil
 import com.wordnik.swagger.core.ApiPropertiesReader
 import com.wordnik.client.model.Pet
 import org.scalatra.ScalatraServlet
@@ -8,93 +7,73 @@ import org.scalatra.swagger._
 
 import scala.collection.JavaConverters._
 
-class PetApi (implicit val swagger: Swagger) extends ScalatraServlet with SwaggerBase with SwaggerSupport {
+class PetApi(implicit val swagger: Swagger) extends ScalatraServlet with SwaggerBase with SwaggerSupport {
   protected def buildFullUrl(path: String) = "http://petstore.swagger.wordnik.com/api/%s" format path
-
-//  val data = new PetData
-  val m = JsonUtil.mapper
-
-//  models = Map(swaggerToModel(classOf[Pet]))
 
   get("/:petId",
     summary("Find pet by ID"),
     nickname("getPetById"),
     responseClass("Pet"),
-    endpoint("{NOT SURE}"),
+    endpoint("{TBD}"),
     notes("Returns a pet based on ID"),
     parameters(
-      // it's a path param
       Parameter("petId", "ID of pet that needs to be fetched",
         dataType = DataType.String,
-        paramType = ParamType.Path)
-      
-      )) {
-  }
+        paramType = ParamType.Path))) {
+    }
 
   post("/",
     summary("Add a new pet to the store"),
     nickname("addPet"),
     responseClass("void"),
-    endpoint("{NOT SURE}"),
+    endpoint("{TBD}"),
     notes(""),
     parameters(
-      // it's a body param
       Parameter("body", "Pet object that needs to be added to the store",
         dataType = DataType("Pet"),
-        paramType = ParamType.Body)
-      
-      )) {
-  }
+        paramType = ParamType.Body))) {
+    }
 
   put("/",
     summary("Update an existing pet"),
     nickname("updatePet"),
     responseClass("void"),
-    endpoint("{NOT SURE}"),
+    endpoint("{TBD}"),
     notes(""),
     parameters(
-      // it's a body param
       Parameter("body", "Pet object that needs to be updated in the store",
         dataType = DataType("Pet"),
-        paramType = ParamType.Body)
-      
-      )) {
-  }
+        paramType = ParamType.Body))) {
+    }
 
   get("/findByStatus",
     summary("Finds Pets by status"),
     nickname("findPetsByStatus"),
     responseClass("List[Pet]"),
-    endpoint("{NOT SURE}"),
+    endpoint("{TBD}"),
     notes("Multiple status values can be provided with comma seperated strings"),
     parameters(
-      // it's a query param
       Parameter("status", "Status values that need to be considered for filter",
         paramType = ParamType.Query,
         required = true,
         allowMultiple = true,
-        allowableValues = "LIST[available,pending,sold]",defaultValue = Some("available"),
-        dataType = DataType("String"))
-      
-      )) {
-  }
+        allowableValues = "LIST[available,pending,sold]", defaultValue = Some("available"),
+        dataType = DataType("String")))) {
+    }
 
   get("/findByTags",
     summary("Finds Pets by tags"),
     nickname("findPetsByTags"),
     responseClass("List[Pet]"),
-    endpoint("{NOT SURE}"),
+    endpoint("{TBD}"),
     notes("Muliple tags can be provided with comma seperated strings. Use tag1, tag2, tag3 for testing."),
     parameters(
-      // it's a query param
       Parameter("tags", "Tags to filter by",
         paramType = ParamType.Query,
         required = true,
         allowMultiple = true,
         defaultValue = None,
-        dataType = DataType("String"))
-      
-      )) {
-  }
+        dataType = DataType("String")))) {
+    }
 
-  }
+}
--- samples/server-generator/scalatra/output/src/main/scala/apis/StoreApi.scala
+++ samples/server-generator/scalatra/output/src/main/scala/apis/StoreApi.scala
@@ -1,6 +1,5 @@
 package apis
 
-import json.JsonUtil
 import com.wordnik.swagger.core.ApiPropertiesReader
 import com.wordnik.client.model.Order
 import org.scalatra.ScalatraServlet
@@ -8,57 +7,43 @@ import org.scalatra.swagger._
 
 import scala.collection.JavaConverters._
 
-class StoreApi (implicit val swagger: Swagger) extends ScalatraServlet with SwaggerBase with SwaggerSupport {
+class StoreApi(implicit val swagger: Swagger) extends ScalatraServlet with SwaggerBase with SwaggerSupport {
   protected def buildFullUrl(path: String) = "http://petstore.swagger.wordnik.com/api/%s" format path
-
-//  val data = new PetData
-  val m = JsonUtil.mapper
-
-//  models = Map(swaggerToModel(classOf[Pet]))
 
   get("/order/:orderId",
     summary("Find purchase order by ID"),
     nickname("getOrderById"),
     responseClass("Order"),
-    endpoint("{NOT SURE}"),
+    endpoint("{TBD}"),
     notes("For valid response try integer IDs with value <= 5. Anything above 5 or nonintegers will generate API errors"),
     parameters(
-      // it's a path param
       Parameter("orderId", "ID of pet that needs to be fetched",
         dataType = DataType.String,
-        paramType = ParamType.Path)
-      
-      )) {
-  }
+        paramType = ParamType.Path))) {
+    }
 
   delete("/order/:orderId",
     summary("Delete purchase order by ID"),
     nickname("deleteOrder"),
     responseClass("void"),
-    endpoint("{NOT SURE}"),
+    endpoint("{TBD}"),
     notes("For valid response try integer IDs with value < 1000. Anything above 1000 or nonintegers will generate API errors"),
     parameters(
-      // it's a path param
       Parameter("orderId", "ID of the order that needs to be deleted",
         dataType = DataType.String,
-        paramType = ParamType.Path)
-      
-      )) {
-  }
+        paramType = ParamType.Path))) {
+    }
 
   post("/order",
     summary("Place an order for a pet"),
     nickname("placeOrder"),
     responseClass("void"),
-    endpoint("{NOT SURE}"),
+    endpoint("{TBD}"),
     notes(""),
     parameters(
-      // it's a body param
       Parameter("body", "order placed for purchasing the pet",
         dataType = DataType("Order"),
-        paramType = ParamType.Body)
-      
-      )) {
-  }
+        paramType = ParamType.Body))) {
+    }
 
-  }
+}
