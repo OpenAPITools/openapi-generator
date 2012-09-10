53c612c13b3cb7fe1a8e210465b1dd500cf39a94
Russell Horton <russ@osprey.local>
2012-09-10 01:56:02 -0700

    can't pass JAVA_OPTS directly to Scala compiler

--- bin/runscala.sh
+++ bin/runscala.sh
@@ -1,5 +1,5 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export CLASSPATH="$DIR/../target/lib/*:$DIR/../target/*"
 export JAVA_OPTS="${JAVA_OPTS} -DXmx4096M"
-scala $WORDNIK_OPTS $JAVA_OPTS -cp $CLASSPATH "$@"
+scala $WORDNIK_OPTS -cp $CLASSPATH "$@"
 
