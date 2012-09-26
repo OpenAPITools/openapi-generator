548b86aef1b0488c289c476e6d195902ac921d5b
Tony Tam <fehguy@gmail.com>
2012-09-25 22:30:47 -0700

    fixed package

--- samples/client/petstore/java/src/main/java/com/wordnik/client/ApiInvoker.java
+++ samples/client/petstore/java/src/main/java/com/wordnik/client/ApiInvoker.java
@@ -31,7 +31,7 @@ public class ApiInvoker {
   }
   
   public void addDefaultHeader(String key, String value) {
-	   defaultHeaderMap.put(key, value);
+     defaultHeaderMap.put(key, value);
   }
 
   public String escapeString(String str) {
@@ -77,58 +77,59 @@ public class ApiInvoker {
     StringBuilder b = new StringBuilder();
     
     for(String key : queryParams.keySet()) {
-    	String value = queryParams.get(key);
-    	if (value != null){
-    		if(b.toString().length() == 0) b.append("?");
-    		else b.append("&");
-    		b.append(escapeString(key)).append("=").append(escapeString(value));
-    	}
+      String value = queryParams.get(key);
+      if (value != null){
+        if(b.toString().length() == 0) b.append("?");
+        else b.append("&");
+        b.append(escapeString(key)).append("=").append(escapeString(value));
+      }
     }
     String querystring = b.toString();
 
     Builder builder = client.resource(host + path + querystring).type("application/json");
     for(String key : headerParams.keySet()) {
-    	builder.header(key, headerParams.get(key));
+      builder.header(key, headerParams.get(key));
     }
     
     for(String key : defaultHeaderMap.keySet()) {
-    	if(!headerParams.containsKey(key)) {
-    		builder.header(key, defaultHeaderMap.get(key));
-    	}
+      if(!headerParams.containsKey(key)) {
+        builder.header(key, defaultHeaderMap.get(key));
+      }
     }
     ClientResponse response = null;
 
     if("GET".equals(method)) {
-    	response = (ClientResponse) builder.get(ClientResponse.class);
+      response = (ClientResponse) builder.get(ClientResponse.class);
     }
     else if ("POST".equals(method)) {
         response = builder.post(ClientResponse.class, serialize(body));
     }
     else if ("PUT".equals(method)) {
-    	response = builder.put(ClientResponse.class, serialize(body));
+      response = builder.put(ClientResponse.class, serialize(body));
       }
     else if ("DELETE".equals(method)) {
         response = builder.delete(ClientResponse.class, serialize(body));
     }
     else {
-    	throw new ApiException(500, "unknown method type " + method);
+      throw new ApiException(500, "unknown method type " + method);
     }
     if(response.getClientResponseStatus() == ClientResponse.Status.OK) {
-    	return (String) response.getEntity(String.class);
+      return (String) response.getEntity(String.class);
     }
     else {
-    	throw new ApiException(
-    	          response.getClientResponseStatus().getStatusCode(),
-    	          response.getEntity(String.class));    	
+      throw new ApiException(
+                response.getClientResponseStatus().getStatusCode(),
+                response.getEntity(String.class));      
     }
   }
 
   private Client getClient(String host) {
-	if(!hostMap.containsKey(host)) {
-		Client client = Client.create();
-		client.addFilter(new LoggingFilter());
+  if(!hostMap.containsKey(host)) {
+    Client client = Client.create();
+    client.addFilter(new LoggingFilter());
         hostMap.put(host, client);
-	}
-	return hostMap.get(host);
+  }
+  return hostMap.get(host);
   }
 }
+
