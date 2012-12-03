8b5e63840959f4d959b15062d5a3429fb8a168ba
Tony Tam <fehguy@gmail.com>
2012-12-03 13:48:00 -0800

    detabbed

--- src/main/scala/com/wordnik/swagger/model/SwaggerModelSerializer.scala
+++ src/main/scala/com/wordnik/swagger/model/SwaggerModelSerializer.scala
@@ -6,38 +6,38 @@ import org.json4s.jackson.JsonMethods._
 import org.json4s.native.Serialization.{read, write}
 
 class ModelPropertySerializer extends CustomSerializer[ModelProperty] (formats => ({
-	case json =>
-		implicit val fmts: Formats = formats
-		ModelProperty(
-			`type` = (json \ "type").extractOrElse(""),
-			required = ((json \ "required").extractOrElse("false")).toBoolean,
-			description = (json \ "description").extractOpt[String],
-			allowableValues = (json \ "allowableValues").extract[AllowableValuesFoo],
-			items = (json \ "items").extractOpt[ModelRef]
-		)
+  case json =>
+    implicit val fmts: Formats = formats
+    ModelProperty(
+      `type` = (json \ "type").extractOrElse(""),
+      required = ((json \ "required").extractOrElse("false")).toBoolean,
+      description = (json \ "description").extractOpt[String],
+      allowableValues = (json \ "allowableValues").extract[AllowableValuesFoo],
+      items = (json \ "items").extractOpt[ModelRef]
+    )
 }, {
-	case x: ModelProperty =>
-	implicit val fmts = formats
-	("type" -> x.`type`) ~
-	("required" -> x.required) ~
-	("description" -> x.description) ~
-	("allowableValues" -> {
-		x.allowableValues match {
-			case Any => JNothing // don't serialize when not a concrete type
-			case e:AllowableValuesFoo => Extraction.decompose(x.allowableValues)
-			case _ => JNothing
-		}
-	}) ~
-	("items" -> Extraction.decompose(x.items))
+  case x: ModelProperty =>
+  implicit val fmts = formats
+  ("type" -> x.`type`) ~
+  ("required" -> x.required) ~
+  ("description" -> x.description) ~
+  ("allowableValues" -> {
+    x.allowableValues match {
+      case Any => JNothing // don't serialize when not a concrete type
+      case e:AllowableValuesFoo => Extraction.decompose(x.allowableValues)
+      case _ => JNothing
+    }
+  }) ~
+  ("items" -> Extraction.decompose(x.items))
 }))
 
 class ModelRefSerializer extends CustomSerializer[ModelRef](formats => ({
   case json =>
     implicit val fmts: Formats = formats
     ModelRef(
-    	(json \ "$ref").extract[String],
-    	(json \ "type").extract[String]
-		)
+      (json \ "$ref").extract[String],
+      (json \ "type").extract[String]
+    )
 }, {
   case x: ModelRef =>
   implicit val fmts = formats
@@ -49,16 +49,16 @@ class AllowableValuesSerializer extends CustomSerializer[AllowableValuesFoo](for
   case json =>
     implicit val fmts: Formats = formats
     json \ "valueType" match {
-    	case JString(x) if x.equalsIgnoreCase("list") =>
+      case JString(x) if x.equalsIgnoreCase("list") =>
         AllowableListValues((json \ "values").extract[List[String]])
-    	case JString(x) if x.equalsIgnoreCase("range") =>
+      case JString(x) if x.equalsIgnoreCase("range") =>
         AllowableRangeValues((json \ "min").extract[String], (json \ "max").extract[String])
       case _ => Any
-		}
+    }
 }, {
   case AllowableListValues(values, "LIST") => 
     implicit val fmts = formats
-		("valueType" -> "LIST") ~ ("values" -> Extraction.decompose(values))
+    ("valueType" -> "LIST") ~ ("values" -> Extraction.decompose(values))
   case AllowableRangeValues(min, max)  => 
-  	("valueType" -> "RANGE") ~ ("min" -> min) ~ ("max" -> max)
+    ("valueType" -> "RANGE") ~ ("min" -> min) ~ ("max" -> max)
 }))
