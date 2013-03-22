ffc6a2fc9efbfd92d9027c536a38e4420beab80e
Tony Tam <fehguy@gmail.com>
2013-03-22 11:03:21 -0700

    updated to snapshot

--- samples/server-generator/scalatra/output/src/main/scala/com/wordnik/client/model/Pet.scala
+++ samples/server-generator/scalatra/output/src/main/scala/com/wordnik/client/model/Pet.scala
@@ -4,16 +4,10 @@ import com.wordnik.client.model.Category
 import com.wordnik.client.model.Tag
 case class Pet (
   tags: Option[List[Tag]],
-
   id: Option[Long],
-
   category: Option[Category],
-
   status: Option[String],// pet status in the store
-
   name: Option[String],
-
   photoUrls: Option[List[String]]
-
-  )
+)
 
