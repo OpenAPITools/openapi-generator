0ee6531fcd60f6c0202f6a092f7aaeafa050f032
Robert Blair <robblair@ix.netcom.com>
2013-11-27 01:02:04 -0800

    Version mapping for scala 2.10.* should be consistent.

--- bin/Version.scala
+++ bin/Version.scala
@@ -1,5 +1,7 @@
 val version = scala.util.Properties.scalaPropOrElse("version.number", "unknown").toString match {
   case "2.10.0" => "2.10"
+  case "2.10.2" => "2.10"
+  case "2.10.3" => "2.10"
   case e: String => e
 }
 println(version)
