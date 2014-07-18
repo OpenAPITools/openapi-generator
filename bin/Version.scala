e1c8f58d285024f750fcb0292fc7783047a82433
Tony Tam <fehguy@gmail.com>
2014-07-18 09:52:53 -0600

    added 2.10.4

--- bin/Version.scala
+++ bin/Version.scala
@@ -2,6 +2,7 @@ val version = scala.util.Properties.scalaPropOrElse("version.number", "unknown")
   case "2.10.0" => "2.10"
   case "2.10.2" => "2.10"
   case "2.10.3" => "2.10"
+  case "2.10.4" => "2.10"
   case e: String => e
 }
 println(version)
