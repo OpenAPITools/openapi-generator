7630fd4f854690f9dbf056f1daf689837bbbf9dc
Tony Tam <fehguy@gmail.com>
2014-09-19 16:36:28 -0700

    added scripts

---
+++ bin/objc-petstore.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+SCRIPT="$0"
+
+while [ -h "$SCRIPT" ] ; do
+  ls=`ls -ld "$SCRIPT"`
+  link=`expr "$ls" : '.*-> \(.*\)$'`
+  if expr "$link" : '/.*' > /dev/null; then
+    SCRIPT="$link"
+  else
+    SCRIPT=`dirname "$SCRIPT"`/"$link"
+  fi
+done
+
+if [ ! -d "${APP_DIR}" ]; then
+  APP_DIR=`dirname "$SCRIPT"`/..
+  APP_DIR=`cd "${APP_DIR}"; pwd`
+fi
+
+cd $APP_DIR
+
+# if you've executed sbt assembly previously it will use that instead.
+export JAVA_OPTS="${JAVA_OPTS} -XX:MaxPermSize=256M -Xmx1024M -DloggerPath=conf/log4j.properties"
+ags="$@ com.wordnik.swagger.codegen.Codegen -i src/test/resources/petstore.json -l objc"
+
+java -cp $APP_DIR/target/*:$APP_DIR/target/lib/* $ags
