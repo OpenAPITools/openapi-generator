89a009c41139d8d78a34977e971a2a05a2125d4c
Martin Ehrnhoefer <martin@ehrnhoefer.com>
2014-09-29 16:26:06 +0200

    [2.0 Spec] Implement Schema Object.example field
     see issue https://github.com/wordnik/swagger-core/issues/699,
     pull-request https://github.com/wordnik/swagger-core/pull/700

--- src/main/java/com/wordnik/swagger/codegen/CodegenProperty.java
+++ src/main/java/com/wordnik/swagger/codegen/CodegenProperty.java
@@ -12,6 +12,8 @@ public class CodegenProperty {
   public Integer minLength;
   /** pattern validation for strings, see http://json-schema.org/latest/json-schema-validation.html#rfc.section.5.2.3 */
   public String pattern;
+  /** A free-form property to include an example of an instance for this schema. */
+  public String example;
 
   public Double minimum, maximum, exclusiveMinimum, exclusiveMaximum;
   public Boolean hasMore = null, required = null, secondaryParam = null;
