86f9a0a58a22f647384ef762304b2d9ecb62973c
Tony Tam <fehguy@gmail.com>
2014-10-20 08:42:35 -0700

    fixed build error

--- src/main/java/com/wordnik/swagger/codegen/CodegenParameter.java
+++ src/main/java/com/wordnik/swagger/codegen/CodegenParameter.java
@@ -1,7 +1,7 @@
 package com.wordnik.swagger.codegen;
 
 public class CodegenParameter {
-  public Boolean hasMore = null, isContainer = null, secondaryParam = null, required = null;
+  public Boolean hasMore = null, isContainer = null, secondaryParam = null;
   public String baseName, paramName, dataType, collectionFormat, description, baseType;
   public Boolean isQueryParam, isPathParam, isHeaderParam, isCookieParam, isBodyParam;
   /**
