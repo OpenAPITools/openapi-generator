3b96894e75579234a29640cc253e1948f5110caa
Tony Tam <fehguy@gmail.com>
2015-02-03 21:49:05 -0800

    added file & forms

--- src/main/java/com/wordnik/swagger/codegen/CodegenParameter.java
+++ src/main/java/com/wordnik/swagger/codegen/CodegenParameter.java
@@ -1,9 +1,9 @@
 package com.wordnik.swagger.codegen;
 
 public class CodegenParameter {
-  public Boolean hasMore = null, isContainer = null, secondaryParam = null;
+  public Boolean isFile, notFile, hasMore = null, isContainer = null, secondaryParam = null;
   public String baseName, paramName, dataType, collectionFormat, description, baseType;
-  public Boolean isQueryParam, isPathParam, isHeaderParam, isCookieParam, isBodyParam;
+  public Boolean isFormParam, isQueryParam, isPathParam, isHeaderParam, isCookieParam, isBodyParam;
   /**
    * Determines whether this parameter is mandatory. If the parameter is in "path",
    * this property is required and its value MUST be true. Otherwise, the property
