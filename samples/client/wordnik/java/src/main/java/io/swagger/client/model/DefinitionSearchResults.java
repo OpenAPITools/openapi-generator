e86837dd9a57df56d6e3ded6acc43c33f6f4dc4e
Tony Tam <fehguy@gmail.com>
2015-02-05 21:02:12 -0800

    added samples

---
+++ samples/client/wordnik/java/src/main/java/io/swagger/client/model/DefinitionSearchResults.java
@@ -0,0 +1,54 @@
+package io.swagger.client.model;
+
+import java.util.*;
+import io.swagger.client.model.Definition;
+
+import com.wordnik.swagger.annotations.*;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+
+@ApiModel(description = "")
+public class DefinitionSearchResults  { 
+  private List<Definition> results = new ArrayList<Definition>() ;
+  private Integer totalResults = null;
+  
+  //public enum totalResultsEnum {  }; 
+  
+  
+  
+  /**
+   **/
+  @ApiModelProperty(required = false, value = "")
+  @JsonProperty("results")
+  public List<Definition> getResults() {
+    return results;
+  }
+  public void setResults(List<Definition> results) {
+    this.results = results;
+  }
+
+  
+  /**
+   **/
+  @ApiModelProperty(required = false, value = "")
+  @JsonProperty("totalResults")
+  public Integer getTotalResults() {
+    return totalResults;
+  }
+  public void setTotalResults(Integer totalResults) {
+    this.totalResults = totalResults;
+  }
+
+  
+
+  @Override
+  public String toString()  {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class DefinitionSearchResults {\n");
+    
+    sb.append("  results: ").append(results).append("\n");
+    sb.append("  totalResults: ").append(totalResults).append("\n");
+    sb.append("}\n");
+    return sb.toString();
+  }
+}
