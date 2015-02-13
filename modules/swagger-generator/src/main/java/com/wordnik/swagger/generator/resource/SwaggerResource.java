2bc5e71f9170467dcb6f19bc9d2516bacbdbc539
Tony Tam <fehguy@gmail.com>
2015-02-12 21:53:37 -0800

    updated year in comments

--- modules/swagger-generator/src/main/java/com/wordnik/swagger/generator/resource/SwaggerResource.java
+++ modules/swagger-generator/src/main/java/com/wordnik/swagger/generator/resource/SwaggerResource.java
@@ -1,19 +1,3 @@
-/**
- *  Copyright 2014 Reverb, Inc.
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- */
-
 package com.wordnik.swagger.generator.resource;
 
 import com.wordnik.swagger.codegen.*;
@@ -37,6 +21,7 @@ public class SwaggerResource {
   @GET
   @Path("/download/{fileId}")
   @Produces({"application/zip", "application/json"})
+  @ApiOperation(value = "Downloads a pre-generated file", response = File.class)
   public Response downloadFile(@PathParam("fileId") String fileId) throws Exception {
     Generated g = fileMap.get(fileId);
     System.out.println("looking for fileId " + fileId);
@@ -58,8 +43,7 @@ public class SwaggerResource {
   @POST
   @Path("/clients/{language}")
   @Produces({"application/zip", "application/json"})
-  @ApiOperation(value = "Generates a client library based on the config",
-    notes = "The model representing this is not accurate, it needs to contain a consolidated JSON structure")
+  @ApiOperation(value = "Generates a client library based on the config")
   public Response generateClient(
     @ApiParam(value = "The target language for the client library", allowableValues = "android,java,php,objc,docs", required = true) @PathParam("language") String language,
     @ApiParam(value = "Configuration for building the client library", required = true) GeneratorInput opts) throws Exception {
