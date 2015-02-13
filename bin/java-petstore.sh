9985d9e2d9d0715e015b0f1f8e1f510c677969a5
Tony Tam <fehguy@gmail.com>
2015-02-12 22:26:42 -0800

    removed template path

--- bin/java-petstore.sh
+++ bin/java-petstore.sh
@@ -31,6 +31,6 @@ fi
 
 # if you've executed sbt assembly previously it will use that instead.
 export JAVA_OPTS="${JAVA_OPTS} -XX:MaxPermSize=256M -Xmx1024M -DloggerPath=conf/log4j.properties"
-ags="$@ -i http://petstore.swagger.io/v2/swagger.json -l java  -o samples/client/petstore/java"
+ags="$@ -i http://petstore.swagger.io/v2/swagger.json -l java -o samples/client/petstore/java"
 
 java $JAVA_OPTS -jar $executable $ags
