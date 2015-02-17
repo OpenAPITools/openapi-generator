0563be891c171d5598208c2e368e3bf3dcaf8d45
Tony Tam <fehguy@gmail.com>
2015-02-16 17:36:48 -0800

    made scripts call local file to avoid transient server issues

--- bin/objc-petstore.sh
+++ bin/objc-petstore.sh
@@ -30,7 +30,7 @@ then
 fi
 
 # if you've executed sbt assembly previously it will use that instead.
-export JAVA_OPTS="${JAVA_OPTS} -XX:MaxPermSize=256M -Xmx1024M -DloggerPath=conf/log4j.properties"
+export JAVA_OPTS="${JAVA_OPTS} -DdebugOperations -XX:MaxPermSize=256M -Xmx1024M -DloggerPath=conf/log4j.properties"
 ags="$@ -i modules/swagger-codegen/src/test/resources/2_0/petstore.json -l objc -o samples/client/petstore/objc"
 
 java $JAVA_OPTS -jar $executable $ags
--- bin/scala-petstore.sh
+++ bin/scala-petstore.sh
@@ -31,6 +31,6 @@ fi
 
 # if you've executed sbt assembly previously it will use that instead.
 export JAVA_OPTS="${JAVA_OPTS} -XX:MaxPermSize=256M -Xmx1024M -DloggerPath=conf/log4j.properties"
-ags="$@ -i http://petstore.swagger.io/v2/swagger.json -l scala -o samples/client/petstore/scala"
+ags="$@ -i modules/swagger-codegen/src/test/resources/2_0/petstore.json -l scala -o samples/client/petstore/scala"
 
 java $JAVA_OPTS -jar $executable $ags
