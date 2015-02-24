4bd227271aad52f014fc29f069ab5a207ae3b277
Tony Tam <fehguy@gmail.com>
2015-02-23 22:45:06 -0800

    updated version

---
+++ modules/swagger-generator/src/test/scala/GeneratorInputTest.scala
@@ -0,0 +1,19 @@
+import com.wordnik.swagger.online._
+import com.wordnik.swagger.generator.model._
+import com.wordnik.swagger.util.Json
+
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.FlatSpec
+import org.scalatest.Matchers
+
+import scala.collection.JavaConverters._
+
+@RunWith(classOf[JUnitRunner])
+class GeneratorInputTest extends FlatSpec with Matchers {
+  it should "write an object" in {
+    val obj = new GeneratorInput()
+    obj.setSwaggerUrl("http://petstore.swagger.io/v2/swagger.json")
+    Json.prettyPrint(obj)
+  }
+}
