d0dcb2fb433d65203f7339baccdf6e8d3054b49e
William Cheng <wing328hk@gmail.com>
2015-03-18 16:33:00 +0800

    fix bug with ruby header parameter, remove extra linebreak in generated code

--- samples/client/petstore/ruby/models/Order.rb
+++ samples/client/petstore/ruby/models/Order.rb
@@ -4,18 +4,12 @@ class Order
   # :internal => :external
   def self.attribute_map
     {
-      
-      :id => :id,
-      
-      :petId => :petId,
-      
-      :quantity => :quantity,
-      
-      :shipDate => :shipDate,
-      
-      :status => :status,
-      
-      :complete => :complete
+      :id => :'id',
+      :petId => :'petId',
+      :quantity => :'quantity',
+      :shipDate => :'shipDate',
+      :status => :'status',
+      :complete => :'complete'
       
     }
   end
@@ -25,39 +19,27 @@ class Order
     # Morph attribute keys into undescored rubyish style
     
     if self.class.attribute_map[:"id"]
-      
       @id = attributes["id"]
-      
     end
     
     if self.class.attribute_map[:"petId"]
-      
       @petId = attributes["petId"]
-      
     end
     
     if self.class.attribute_map[:"quantity"]
-      
       @quantity = attributes["quantity"]
-      
     end
     
     if self.class.attribute_map[:"shipDate"]
-      
       @shipDate = attributes["shipDate"]
-      
     end
     
     if self.class.attribute_map[:"status"]
-      
       @status = attributes["status"]
-      
     end
     
     if self.class.attribute_map[:"complete"]
-      
       @complete = attributes["complete"]
-      
     end
     
   end
