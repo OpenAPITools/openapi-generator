de56f76284479b644f4242852d17497072cd8c2e
Tony Tam <fehguy@gmail.com>
2015-03-19 14:52:45 -0700

    rebuilt sample

---
+++ samples/server/petstore/nodejs/controllers/PetService.js
@@ -0,0 +1,112 @@
+'use strict';
+
+exports.updatePet = function(body) {
+
+  var examples = {};
+  
+
+  
+}
+exports.addPet = function(body) {
+
+  var examples = {};
+  
+
+  
+}
+exports.findPetsByStatus = function(status) {
+
+  var examples = {};
+  
+  examples['application/json'] = [ {
+  "tags" : [ {
+    "id" : 123456789,
+    "name" : "aeiou"
+  } ],
+  "id" : 123456789,
+  "category" : {
+    "id" : 123456789,
+    "name" : "aeiou"
+  },
+  "status" : "aeiou",
+  "name" : "doggie",
+  "photoUrls" : [ "aeiou" ]
+} ];
+  
+
+  
+  if(Object.keys(examples).length > 0)
+    return examples[Object.keys(examples)[0]];
+  
+}
+exports.findPetsByTags = function(tags) {
+
+  var examples = {};
+  
+  examples['application/json'] = [ {
+  "tags" : [ {
+    "id" : 123456789,
+    "name" : "aeiou"
+  } ],
+  "id" : 123456789,
+  "category" : {
+    "id" : 123456789,
+    "name" : "aeiou"
+  },
+  "status" : "aeiou",
+  "name" : "doggie",
+  "photoUrls" : [ "aeiou" ]
+} ];
+  
+
+  
+  if(Object.keys(examples).length > 0)
+    return examples[Object.keys(examples)[0]];
+  
+}
+exports.getPetById = function(petId) {
+
+  var examples = {};
+  
+  examples['application/json'] = {
+  "tags" : [ {
+    "id" : 123456789,
+    "name" : "aeiou"
+  } ],
+  "id" : 123456789,
+  "category" : {
+    "id" : 123456789,
+    "name" : "aeiou"
+  },
+  "status" : "aeiou",
+  "name" : "doggie",
+  "photoUrls" : [ "aeiou" ]
+};
+  
+
+  
+  if(Object.keys(examples).length > 0)
+    return examples[Object.keys(examples)[0]];
+  
+}
+exports.updatePetWithForm = function(petId, name, status) {
+
+  var examples = {};
+  
+
+  
+}
+exports.deletePet = function(api_key, petId) {
+
+  var examples = {};
+  
+
+  
+}
+exports.uploadFile = function(petId, additionalMetadata, file) {
+
+  var examples = {};
+  
+
+  
+}
---
+++ samples/server/petstore/nodejs/controllers/UserService.js
@@ -0,0 +1,77 @@
+'use strict';
+
+exports.createUser = function(body) {
+
+  var examples = {};
+  
+
+  
+}
+exports.createUsersWithArrayInput = function(body) {
+
+  var examples = {};
+  
+
+  
+}
+exports.createUsersWithListInput = function(body) {
+
+  var examples = {};
+  
+
+  
+}
+exports.loginUser = function(username, password) {
+
+  var examples = {};
+  
+  examples['application/json'] = "aeiou";
+  
+
+  
+  if(Object.keys(examples).length > 0)
+    return examples[Object.keys(examples)[0]];
+  
+}
+exports.logoutUser = function() {
+
+  var examples = {};
+  
+
+  
+}
+exports.getUserByName = function(username) {
+
+  var examples = {};
+  
+  examples['application/json'] = {
+  "id" : 123456789,
+  "lastName" : "aeiou",
+  "phone" : "aeiou",
+  "username" : "aeiou",
+  "email" : "aeiou",
+  "userStatus" : 123,
+  "firstName" : "aeiou",
+  "password" : "aeiou"
+};
+  
+
+  
+  if(Object.keys(examples).length > 0)
+    return examples[Object.keys(examples)[0]];
+  
+}
+exports.updateUser = function(username, body) {
+
+  var examples = {};
+  
+
+  
+}
+exports.deleteUser = function(username) {
+
+  var examples = {};
+  
+
+  
+}
