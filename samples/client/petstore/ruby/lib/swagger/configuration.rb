a149bb91207ae78955891ca8d8704913d2ce2260
William Cheng <wing328hk@gmail.com>
2015-03-19 17:17:39 +0800

    update ruby petstore example

--- samples/client/petstore/ruby/lib/swagger/configuration.rb
+++ samples/client/petstore/ruby/lib/swagger/configuration.rb
@@ -3,7 +3,7 @@ module Swagger
   class Configuration
     require 'swagger/version'    
     
-    attr_accessor :format, :api_key, :username, :password, :auth_token, :scheme, :host, :base_path, :user_agent, :logger, :inject_format, :force_ending_format, :camelize_params
+    attr_accessor :format, :api_key, :username, :password, :auth_token, :scheme, :host, :base_path, :user_agent, :logger, :inject_format, :force_ending_format, :camelize_params, :user_agent
     
     # Defaults go in here..
     def initialize
@@ -11,7 +11,7 @@ module Swagger
       @scheme = 'http'
       @host = 'api.wordnik.com'
       @base_path = '/v4'
-      @user_agent = "ruby-#{Swagger::VERSION}"
+      @user_agent = "ruby-swagger-#{Swagger::VERSION}"
       @inject_format = true
       @force_ending_format = false
       @camelize_params = true
