b287d71e53be97421b82b1bd16ba5248cb81e39f
Tony Tam <fehguy@gmail.com>
2015-03-24 17:02:12 -0700

    added property for app name

--- bin/objc-petstore.sh
+++ bin/objc-petstore.sh
@@ -33,4 +33,4 @@ fi
 export JAVA_OPTS="${JAVA_OPTS} -XX:MaxPermSize=256M -Xmx1024M -DloggerPath=conf/log4j.properties"
 ags="$@ -i modules/swagger-codegen/src/test/resources/2_0/petstore.json -l objc -o samples/client/petstore/objc"
 
-java $JAVA_OPTS -jar $executable $ags
+java -DappName=PetstoreClient $JAVA_OPTS -jar $executable $ags
