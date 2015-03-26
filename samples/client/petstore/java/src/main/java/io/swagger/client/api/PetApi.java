8ab297d63e914c5bc4a39c05e27e28057b490f1d
William Cheng <wing328hk@gmail.com>
2015-03-26 16:27:00 +0800

    update java doc

--- samples/client/petstore/java/src/main/java/io/swagger/client/api/PetApi.java
+++ samples/client/petstore/java/src/main/java/io/swagger/client/api/PetApi.java
@@ -36,7 +36,12 @@ public class PetApi {
   }
 
   
-    
+  /**
+   * Update an existing pet
+   * 
+   * @param body Pet object that needs to be added to the store
+   * @return void
+   */
   public void updatePet (Pet body) throws ApiException {
     Object postBody = body;
     
@@ -81,7 +86,12 @@ public class PetApi {
     }
   }
   
-    
+  /**
+   * Add a new pet to the store
+   * 
+   * @param body Pet object that needs to be added to the store
+   * @return void
+   */
   public void addPet (Pet body) throws ApiException {
     Object postBody = body;
     
@@ -126,7 +136,12 @@ public class PetApi {
     }
   }
   
-    
+  /**
+   * Finds Pets by status
+   * Multiple status values can be provided with comma seperated strings
+   * @param status Status values that need to be considered for filter
+   * @return List<Pet>
+   */
   public List<Pet> findPetsByStatus (List<String> status) throws ApiException {
     Object postBody = null;
     
@@ -173,7 +188,12 @@ public class PetApi {
     }
   }
   
-    
+  /**
+   * Finds Pets by tags
+   * Muliple tags can be provided with comma seperated strings. Use tag1, tag2, tag3 for testing.
+   * @param tags Tags to filter by
+   * @return List<Pet>
+   */
   public List<Pet> findPetsByTags (List<String> tags) throws ApiException {
     Object postBody = null;
     
@@ -220,7 +240,12 @@ public class PetApi {
     }
   }
   
-    
+  /**
+   * Find pet by ID
+   * Returns a pet when ID &lt; 10.  ID &gt; 10 or nonintegers will simulate API error conditions
+   * @param petId ID of pet that needs to be fetched
+   * @return Pet
+   */
   public Pet getPetById (Long petId) throws ApiException {
     Object postBody = null;
     
@@ -266,7 +291,14 @@ public class PetApi {
     }
   }
   
-    
+  /**
+   * Updates a pet in the store with form data
+   * 
+   * @param petId ID of pet that needs to be updated
+   * @param name Updated name of the pet
+   * @param status Updated status of the pet
+   * @return void
+   */
   public void updatePetWithForm (String petId, String name, String status) throws ApiException {
     Object postBody = null;
     
@@ -320,7 +352,13 @@ public class PetApi {
     }
   }
   
-    
+  /**
+   * Deletes a pet
+   * 
+   * @param apiKey 
+   * @param petId Pet id to delete
+   * @return void
+   */
   public void deletePet (String apiKey, Long petId) throws ApiException {
     Object postBody = null;
     
@@ -367,7 +405,14 @@ public class PetApi {
     }
   }
   
-    
+  /**
+   * uploads an image
+   * 
+   * @param petId ID of pet to update
+   * @param additionalMetadata Additional data to pass to server
+   * @param file file to upload
+   * @return void
+   */
   public void uploadFile (Long petId, String additionalMetadata, File file) throws ApiException {
     Object postBody = null;
     
