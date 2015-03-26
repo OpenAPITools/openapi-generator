ae52a62c0963e2485e74786f38e4d29c51060d0c
William Cheng <wing328hk@gmail.com>
2015-03-26 16:27:00 +0800

    update java doc

--- samples/client/petstore/java/src/main/java/io/swagger/client/api/StoreApi.java
+++ samples/client/petstore/java/src/main/java/io/swagger/client/api/StoreApi.java
@@ -36,7 +36,11 @@ public class StoreApi {
   }
 
   
-    
+  /**
+   * Returns pet inventories by status
+   * Returns a map of status codes to quantities
+   * @return Map<String, Integer>
+   */
   public Map<String, Integer> getInventory () throws ApiException {
     Object postBody = null;
     
@@ -81,7 +85,12 @@ public class StoreApi {
     }
   }
   
-    
+  /**
+   * Place an order for a pet
+   * 
+   * @param body order placed for purchasing the pet
+   * @return Order
+   */
   public Order placeOrder (Order body) throws ApiException {
     Object postBody = body;
     
@@ -126,7 +135,12 @@ public class StoreApi {
     }
   }
   
-    
+  /**
+   * Find purchase order by ID
+   * For valid response try integer IDs with value &lt;= 5 or &gt; 10. Other values will generated exceptions
+   * @param orderId ID of pet that needs to be fetched
+   * @return Order
+   */
   public Order getOrderById (String orderId) throws ApiException {
     Object postBody = null;
     
@@ -172,7 +186,12 @@ public class StoreApi {
     }
   }
   
-    
+  /**
+   * Delete purchase order by ID
+   * For valid response try integer IDs with value &lt; 1000. Anything above 1000 or nonintegers will generate API errors
+   * @param orderId ID of the order that needs to be deleted
+   * @return void
+   */
   public void deleteOrder (String orderId) throws ApiException {
     Object postBody = null;
     
