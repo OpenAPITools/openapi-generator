fea39b433a1400a70e409f81ffd5f228b7c0b037
William Cheng <wing328hk@gmail.com>
2015-04-01 04:26:32 +0800

    Fix content-type and accept in PHP API

---
+++ samples/client/petstore/csharp/src/main/csharp/io/swagger/Model/pet.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace io.swagger.Model {
+  public class pet {
+    
+
+    
+    public long? id { get; set; }
+
+    
+
+    
+    public Category category { get; set; }
+
+    
+
+    
+    public string name { get; set; }
+
+    
+
+    
+    public List<string> photoUrls { get; set; }
+
+    
+
+    
+    public List<Tag> tags { get; set; }
+
+    
+
+    /* pet status in the store */
+    
+    public string status { get; set; }
+
+    
+
+    public override string ToString()  {
+      var sb = new StringBuilder();
+      sb.Append("class pet {\n");
+      
+      sb.Append("  id: ").Append(id).Append("\n");
+      
+      sb.Append("  category: ").Append(category).Append("\n");
+      
+      sb.Append("  name: ").Append(name).Append("\n");
+      
+      sb.Append("  photoUrls: ").Append(photoUrls).Append("\n");
+      
+      sb.Append("  tags: ").Append(tags).Append("\n");
+      
+      sb.Append("  status: ").Append(status).Append("\n");
+      
+      sb.Append("}\n");
+      return sb.ToString();
+    }
+  }
+  
+  
+}
