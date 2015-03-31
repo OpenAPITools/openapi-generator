f022d8f133ce33e5a37b30ee105eb624eafd3f30
Tony Tam <fehguy@gmail.com>
2015-03-31 15:57:50 -0700

    formatting

--- samples/client/petstore/ruby/spec/pet_spec.rb
+++ samples/client/petstore/ruby/spec/pet_spec.rb
@@ -2,11 +2,11 @@ require 'spec_helper'
 
 describe "Pet" do
   before do
-	  Swagger.configure do |config|
-	    config.api_key = 'special-key' 
-	    config.host = 'petstore.swagger.io'
-	    config.base_path = '/v2'
-	  end
+    Swagger.configure do |config|
+      config.api_key = 'special-key' 
+      config.host = 'petstore.swagger.io'
+      config.base_path = '/v2'
+    end
   end
   
   describe "pet methods" do
@@ -26,22 +26,22 @@ describe "Pet" do
       pets.length.should == 0
     end
 
-	  it "should find a pet by status" do
-	    pets = PetApi.findPetsByStatus("available,sold")
-	    pets.map {|pet| 
-	      if(pet.status != 'available' && pet.status != 'sold') 
-	        raise "pet status wasn't right"
-	      end
-	    }
-	  end
-	  
-	  it "should update a pet" do
-	    pet = Pet.new({'id' => 99, 'name' => 'programmer', 'status' => 'coding'})
-	    PetApi.addPet(pet)
-	    
-	    fetched = PetApi.getPetById(99)
-	    fetched.id.should == 99
-	  end
+    it "should find a pet by status" do
+      pets = PetApi.findPetsByStatus("available,sold")
+      pets.map {|pet| 
+        if(pet.status != 'available' && pet.status != 'sold') 
+          raise "pet status wasn't right"
+        end
+      }
+    end
+    
+    it "should update a pet" do
+      pet = Pet.new({'id' => 99, 'name' => 'programmer', 'status' => 'coding'})
+      PetApi.addPet(pet)
+      
+      fetched = PetApi.getPetById(99)
+      fetched.id.should == 99
+    end
 
     it "should create a pet" do 
       pet = Pet.new('id' => 100, 'name' => "Gorilla")
@@ -56,11 +56,11 @@ end
 
 describe "Store" do
   before do
-	  Swagger.configure do |config|
-	    config.api_key = 'special-key' 
-	    config.host = 'petstore.swagger.io'
-	    config.base_path = '/v2'
-	  end
+    Swagger.configure do |config|
+      config.api_key = 'special-key' 
+      config.host = 'petstore.swagger.io'
+      config.base_path = '/v2'
+    end
   end
 
   it "should fetch an order" do
