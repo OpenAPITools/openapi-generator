6ac497d774618509ef362e7466a1929e291b5811
Tony Tam <fehguy@gmail.com>
2015-03-31 16:12:42 -0700

    rebuilt

--- bin/objc-petstore.sh
+++ bin/objc-petstore.sh
@@ -28,4 +28,4 @@ fi
 export JAVA_OPTS="${JAVA_OPTS} -XX:MaxPermSize=256M -Xmx1024M -DloggerPath=conf/log4j.properties"
 ags="$@ generate -t modules/swagger-codegen/src/main/resources/objc -i modules/swagger-codegen/src/test/resources/2_0/petstore.json -l objc -o samples/client/petstore/objc"
 
-java -DdebugModels -DappName=PetstoreClient $JAVA_OPTS -jar $executable $ags
+java -DappName=PetstoreClient $JAVA_OPTS -jar $executable $ags
