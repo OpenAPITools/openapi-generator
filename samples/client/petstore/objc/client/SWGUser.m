be206362f15864c08e7e6e6ad5cf5476a124b2da
geekerzp <geekerzp@gmail.com>
2015-04-15 17:53:45 +0800

    Using `propertyIsOptional` to implement optional property instead of using `Optional` protocol in objc client.

--- samples/client/petstore/objc/client/SWGUser.m
+++ samples/client/petstore/objc/client/SWGUser.m
@@ -7,4 +7,16 @@
   return [[JSONKeyMapper alloc] initWithDictionary:@{ @"id": @"_id", @"username": @"username", @"firstName": @"firstName", @"lastName": @"lastName", @"email": @"email", @"password": @"password", @"phone": @"phone", @"userStatus": @"userStatus" }];
 }
 
++ (BOOL)propertyIsOptional:(NSString *)propertyName
+{
+  NSArray *optionalProperties = @[@"_id", @"username", @"firstName", @"lastName", @"email", @"password", @"phone", @"userStatus"];
+
+  if ([optionalProperties containsObject:propertyName]) {
+    return YES;
+  }
+  else {
+    return NO;
+  }
+}
+
 @end
