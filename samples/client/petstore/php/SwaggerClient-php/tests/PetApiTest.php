c30ed059fe78dbe47ac216a8513ef93b48d44196
wing328 <wing328hk@gmail.com>
2015-04-17 00:16:09 +0800

    update php api test with SwaggerClient.php

--- samples/client/petstore/php/SwaggerClient-php/tests/PetApiTest.php
+++ samples/client/petstore/php/SwaggerClient-php/tests/PetApiTest.php
@@ -1,13 +1,13 @@
 <?php
-require_once('SwaggerPetstore.php');
+require_once('SwaggerClient.php');
 class PetApiTest extends \PHPUnit_Framework_TestCase
 {
   public function testGetPetById()
   {
     // initialize the API client
-    $api_client = new SwaggerPetstore\APIClient('http://petstore.swagger.io/v2');
+    $api_client = new SwaggerClient\APIClient('http://petstore.swagger.io/v2');
     $petId = 10005;  // ID of pet that needs to be fetched
-    $pet_api = new SwaggerPetstore\PetAPI($api_client);
+    $pet_api = new SwaggerClient\PetAPI($api_client);
     // return Pet (model)
     $response = $pet_api->getPetById($petId);
     $this->assertSame($response->id, $petId);
