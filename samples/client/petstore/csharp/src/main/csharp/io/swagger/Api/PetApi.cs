2abc15010fae4a16a579ec289f36b0909e3e6607
wing328 <wing328hk@gmail.com>
2015-04-29 10:43:57 +0800

    udpate csharp template with better comment and ParameterToString

--- samples/client/petstore/csharp/src/main/csharp/io/swagger/Api/PetApi.cs
+++ samples/client/petstore/csharp/src/main/csharp/io/swagger/Api/PetApi.cs
@@ -39,7 +39,6 @@ namespace io.swagger.Api {
     /// Update an existing pet 
     /// </summary>
     /// <param name="Body">Pet object that needs to be added to the store</param>
-    
     /// <returns></returns>
     public void  UpdatePet (Pet Body) {
       // create path and map variables
@@ -49,17 +48,13 @@ namespace io.swagger.Api {
 
       
 
-      // path (url segment) parameters
       _request.AddUrlSegment("format", "json"); // set format to json by default
       
-      // query parameters, if any
-      
-      // header parameters, if any
-      
-      // form parameters, if any
       
       
-      _request.AddParameter("application/json", ApiInvoker.Serialize(Body), ParameterType.RequestBody);
+      
+      
+      _request.AddParameter("application/json", ApiInvoker.Serialize(Body), ParameterType.RequestBody); // HTTP request body (model)
       
 
       try {
@@ -81,7 +76,6 @@ namespace io.swagger.Api {
     /// Add a new pet to the store 
     /// </summary>
     /// <param name="Body">Pet object that needs to be added to the store</param>
-    
     /// <returns></returns>
     public void  AddPet (Pet Body) {
       // create path and map variables
@@ -91,17 +85,13 @@ namespace io.swagger.Api {
 
       
 
-      // path (url segment) parameters
       _request.AddUrlSegment("format", "json"); // set format to json by default
       
-      // query parameters, if any
-      
-      // header parameters, if any
-      
-      // form parameters, if any
       
       
-      _request.AddParameter("application/json", ApiInvoker.Serialize(Body), ParameterType.RequestBody);
+      
+      
+      _request.AddParameter("application/json", ApiInvoker.Serialize(Body), ParameterType.RequestBody); // HTTP request body (model)
       
 
       try {
@@ -123,7 +113,6 @@ namespace io.swagger.Api {
     /// Finds Pets by status Multiple status values can be provided with comma seperated strings
     /// </summary>
     /// <param name="Status">Status values that need to be considered for filter</param>
-    
     /// <returns></returns>
     public List<Pet>  FindPetsByStatus (List<string> Status) {
       // create path and map variables
@@ -133,14 +122,11 @@ namespace io.swagger.Api {
 
       
 
-      // path (url segment) parameters
       _request.AddUrlSegment("format", "json"); // set format to json by default
       
-      // query parameters, if any
-       if (Status != null) _request.AddParameter("status", Status);
-      // header parameters, if any
+       if (Status != null) _request.AddParameter("status", ApiInvoker.ParameterToString(Status)); // query parameter
       
-      // form parameters, if any
+      
       
       
 
@@ -164,7 +150,6 @@ namespace io.swagger.Api {
     /// Finds Pets by tags Muliple tags can be provided with comma seperated strings. Use tag1, tag2, tag3 for testing.
     /// </summary>
     /// <param name="Tags">Tags to filter by</param>
-    
     /// <returns></returns>
     public List<Pet>  FindPetsByTags (List<string> Tags) {
       // create path and map variables
@@ -174,14 +159,11 @@ namespace io.swagger.Api {
 
       
 
-      // path (url segment) parameters
       _request.AddUrlSegment("format", "json"); // set format to json by default
       
-      // query parameters, if any
-       if (Tags != null) _request.AddParameter("tags", Tags);
-      // header parameters, if any
+       if (Tags != null) _request.AddParameter("tags", ApiInvoker.ParameterToString(Tags)); // query parameter
       
-      // form parameters, if any
+      
       
       
 
@@ -205,7 +187,6 @@ namespace io.swagger.Api {
     /// Find pet by ID Returns a pet when ID &lt; 10.  ID &gt; 10 or nonintegers will simulate API error conditions
     /// </summary>
     /// <param name="PetId">ID of pet that needs to be fetched</param>
-    
     /// <returns></returns>
     public Pet  GetPetById (long? PetId) {
       // create path and map variables
@@ -215,14 +196,11 @@ namespace io.swagger.Api {
 
       
 
-      // path (url segment) parameters
       _request.AddUrlSegment("format", "json"); // set format to json by default
-      _request.AddUrlSegment("petId", ApiInvoker.ParameterToString(PetId));
-      // query parameters, if any
+      _request.AddUrlSegment("petId", ApiInvoker.ParameterToString(PetId)); // path (url segment) parameter
       
-      // header parameters, if any
       
-      // form parameters, if any
+      
       
       
 
@@ -248,7 +226,6 @@ namespace io.swagger.Api {
     /// <param name="PetId">ID of pet that needs to be updated</param>
     /// <param name="Name">Updated name of the pet</param>
     /// <param name="Status">Updated status of the pet</param>
-    
     /// <returns></returns>
     public void  UpdatePetWithForm (string PetId, string Name, string Status) {
       // create path and map variables
@@ -258,16 +235,13 @@ namespace io.swagger.Api {
 
       
 
-      // path (url segment) parameters
       _request.AddUrlSegment("format", "json"); // set format to json by default
-      _request.AddUrlSegment("petId", ApiInvoker.ParameterToString(PetId));
-      // query parameters, if any
+      _request.AddUrlSegment("petId", ApiInvoker.ParameterToString(PetId)); // path (url segment) parameter
       
-      // header parameters, if any
       
-      // form parameters, if any
-      if (Name != null) _request.AddParameter("name", Name);
-      if (Status != null) _request.AddParameter("status", Status);
+      
+      if (Name != null) _request.AddParameter("name", ApiInvoker.ParameterToString(Name)); // form parameter
+      if (Status != null) _request.AddParameter("status", ApiInvoker.ParameterToString(Status)); // form parameter
       
       
 
@@ -291,7 +265,6 @@ namespace io.swagger.Api {
     /// </summary>
     /// <param name="ApiKey"></param>
     /// <param name="PetId">Pet id to delete</param>
-    
     /// <returns></returns>
     public void  DeletePet (string ApiKey, long? PetId) {
       // create path and map variables
@@ -301,14 +274,12 @@ namespace io.swagger.Api {
 
       
 
-      // path (url segment) parameters
       _request.AddUrlSegment("format", "json"); // set format to json by default
-      _request.AddUrlSegment("petId", ApiInvoker.ParameterToString(PetId));
-      // query parameters, if any
+      _request.AddUrlSegment("petId", ApiInvoker.ParameterToString(PetId)); // path (url segment) parameter
       
-      // header parameters, if any
-       if (ApiKey != null) _request.AddHeader("api_key", ApiKey);
-      // form parameters, if any
+      
+       if (ApiKey != null) _request.AddHeader("api_key", ApiInvoker.ParameterToString(ApiKey)); // header parameter
+      
       
       
 
@@ -333,7 +304,6 @@ namespace io.swagger.Api {
     /// <param name="PetId">ID of pet to update</param>
     /// <param name="AdditionalMetadata">Additional data to pass to server</param>
     /// <param name="File">file to upload</param>
-    
     /// <returns></returns>
     public void  UploadFile (long? PetId, string AdditionalMetadata, byte[] File) {
       // create path and map variables
@@ -343,16 +313,13 @@ namespace io.swagger.Api {
 
       
 
-      // path (url segment) parameters
       _request.AddUrlSegment("format", "json"); // set format to json by default
-      _request.AddUrlSegment("petId", ApiInvoker.ParameterToString(PetId));
-      // query parameters, if any
+      _request.AddUrlSegment("petId", ApiInvoker.ParameterToString(PetId)); // path (url segment) parameter
       
-      // header parameters, if any
       
-      // form parameters, if any
-      if (AdditionalMetadata != null) _request.AddParameter("additionalMetadata", AdditionalMetadata);
-      if (File != null) _request.AddFile("file", File);
+      
+      if (AdditionalMetadata != null) _request.AddParameter("additionalMetadata", ApiInvoker.ParameterToString(AdditionalMetadata)); // form parameter
+      if (File != null) _request.AddFile("file", File); // form parameter
       
       
 
