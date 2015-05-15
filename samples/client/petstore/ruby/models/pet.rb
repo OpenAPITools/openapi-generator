f8a4bbf87641097941654cccbb56d6ec0e58a0f0
William Cheng <wing328hk@gmail.com>
2015-05-16 03:51:21 +0800

    add base object

--- samples/client/petstore/ruby/models/pet.rb
+++ samples/client/petstore/ruby/models/pet.rb
@@ -1,58 +1,160 @@
+require_relative 'base_object'
 
-class Pet
+#
+class Pet < BaseObject
   attr_accessor :id, :category, :name, :photo_urls, :tags, :status
-  # :internal => :external
+  # attribute mapping from ruby-style variable name to JSON key
   def self.attribute_map
     {
-      :id => :'id',
-      :category => :'category',
-      :name => :'name',
-      :photo_urls => :'photoUrls',
-      :tags => :'tags',
-      :status => :'status'
+      
+      # 
+      :'id' => :'id',
+      
+      # 
+      :'category' => :'category',
+      
+      # 
+      :'name' => :'name',
+      
+      # 
+      :'photo_urls' => :'photoUrls',
+      
+      # 
+      :'tags' => :'tags',
+      
+      # pet status in the store
+      :'status' => :'status'
+      
+    }
+  end
+
+  # attribute type
+  def self.swagger_types
+    {
+      :'id' => :'int',
+      :'category' => :'Category',
+      :'name' => :'string',
+      :'photo_urls' => :'array[string]',
+      :'tags' => :'array[Tag]',
+      :'status' => :'string'
       
     }
   end
 
   def initialize(attributes = {})
-    return if attributes.empty?
-    # Morph attribute keys into undescored rubyish style
+    return if !attributes.is_a?(Hash) || attributes.empty?
+
+    # convert string to symbol for hash key
+    attributes = attributes.inject({}){|memo,(k,v)| memo[k.to_sym] = v; memo}
+
     
-    if self.class.attribute_map[:"id"]
-      @id = attributes["id"]
+    if attributes[:'id']
+      @id = attributes[:'id']
     end
     
-    if self.class.attribute_map[:"category"]
-      @category = attributes["category"]
+    if attributes[:'category']
+      @category = attributes[:'category']
     end
     
-    if self.class.attribute_map[:"name"]
-      @name = attributes["name"]
+    if attributes[:'name']
+      @name = attributes[:'name']
     end
     
-    if self.class.attribute_map[:"photo_urls"]
-      if (value = attributes["photoUrls"]).is_a?(Array)
+    if attributes[:'photoUrls']
+      if (value = attributes[:'photoUrls']).is_a?(Array)
         @photo_urls = value
       end
     end
     
-    if self.class.attribute_map[:"tags"]
-      if (value = attributes["tags"]).is_a?(Array)
-        @tags = value.map{ |v| Tag.new(v) }
+    if attributes[:'tags']
+      if (value = attributes[:'tags']).is_a?(Array)
+        #@tags = value.map{ |v| Tag.new(v) }
+        @tags = value
       end
     end
     
-    if self.class.attribute_map[:"status"]
-      @status = attributes["status"]
+    if attributes[:'status']
+      @status = attributes[:'status']
     end
     
   end
 
-  def to_body
-    body = {}
-    self.class.attribute_map.each_pair do |key, value|
-      body[value] = self.send(key) unless self.send(key).nil?
-    end
-    body
-  end
+#  # return the object in the form of hash
+#  def to_body
+#    body = {}
+#    self.class.attribute_map.each_pair do |key, value|
+#      body[value] = self.send(key) unless self.send(key).nil?
+#    end
+#    body
+#  end
+#
+#  # build the object from hash
+#  def build_from_hash(attributes)
+#    self.class.swagger_types.each_pair do |key, type|
+#      if type =~ /^array\[(.*)\]/i
+#        if attributes[self.class.attribute_map[key]].is_a?(Array)
+#          self.send("#{key}=", attributes[self.class.attribute_map[key]].map{ |v| _deserialize($1, v) } )
+#        else
+#          #TODO show warning in debug mode
+#        end
+#      elsif !attributes[self.class.attribute_map[key]].nil?
+#        self.send("#{key}=", _deserialize(type, attributes[self.class.attribute_map[key]]))
+#      else
+#        # data not found in attributes(hash), not an issue as the data can be optional
+#      end
+#    end
+#
+#    self
+#  end
+#
+#  def _deserialize(type, value)
+#    case type
+#    when :DateTime
+#      DateTime.parse(value)
+#    when :string
+#      value.to_s
+#    when :int
+#      value.to_i
+#    when :double
+#      value.to_f
+#    when :boolean
+#      if value =~ /^(true|t|yes|y|1)$/i
+#        true
+#      else
+#        false
+#      end
+#    else # model
+#      _model = Object.const_get(type).new
+#      _model.build_from_hash(value)
+#    end
+#  end
+#
+#
+#
+#  # to_body is an alias to to_body (backward compatibility)
+#  def to_hash
+#    hash = {}
+#    self.class.attribute_map.each_pair do |key, value|
+#      if self.send(key).is_a?(Array)
+#        next if self.send(key).empty?
+#        hash[value] = self.send(key).select{|v| !v.nil?}.map{ |v| _to_hash v} unless self.send(key).nil?
+#      else
+#        unless (_tmp_value = _to_hash self.send(key)).nil?
+#          hash[value] = _tmp_value
+#        end
+#      end
+#    end
+#    hash
+#  end
+#
+#  # Method to output non-array value in the form of hash
+#  # For object, use to_hash. Otherwise, just return the value
+#  def _to_hash(value)
+#    if value.respond_to? :to_hash
+#      value.to_hash
+#    else
+#      value
+#    end
+#  end
+
 end
--- samples/client/petstore/ruby/models/tag.rb
+++ samples/client/petstore/ruby/models/tag.rb
@@ -1,34 +1,123 @@
+require_relative 'base_object'
 
-class Tag
+#
+class Tag < BaseObject
   attr_accessor :id, :name
-  # :internal => :external
+  # attribute mapping from ruby-style variable name to JSON key
   def self.attribute_map
     {
-      :id => :'id',
-      :name => :'name'
+      
+      # 
+      :'id' => :'id',
+      
+      # 
+      :'name' => :'name'
+      
+    }
+  end
+
+  # attribute type
+  def self.swagger_types
+    {
+      :'id' => :'int',
+      :'name' => :'string'
       
     }
   end
 
   def initialize(attributes = {})
-    return if attributes.empty?
-    # Morph attribute keys into undescored rubyish style
+    return if !attributes.is_a?(Hash) || attributes.empty?
+
+    # convert string to symbol for hash key
+    attributes = attributes.inject({}){|memo,(k,v)| memo[k.to_sym] = v; memo}
+
     
-    if self.class.attribute_map[:"id"]
-      @id = attributes["id"]
+    if attributes[:'id']
+      @id = attributes[:'id']
     end
     
-    if self.class.attribute_map[:"name"]
-      @name = attributes["name"]
+    if attributes[:'name']
+      @name = attributes[:'name']
     end
     
   end
 
-  def to_body
-    body = {}
-    self.class.attribute_map.each_pair do |key, value|
-      body[value] = self.send(key) unless self.send(key).nil?
-    end
-    body
-  end
+#  # return the object in the form of hash
+#  def to_body
+#    body = {}
+#    self.class.attribute_map.each_pair do |key, value|
+#      body[value] = self.send(key) unless self.send(key).nil?
+#    end
+#    body
+#  end
+#
+#  # build the object from hash
+#  def build_from_hash(attributes)
+#    self.class.swagger_types.each_pair do |key, type|
+#      if type =~ /^array\[(.*)\]/i
+#        if attributes[self.class.attribute_map[key]].is_a?(Array)
+#          self.send("#{key}=", attributes[self.class.attribute_map[key]].map{ |v| _deserialize($1, v) } )
+#        else
+#          #TODO show warning in debug mode
+#        end
+#      elsif !attributes[self.class.attribute_map[key]].nil?
+#        self.send("#{key}=", _deserialize(type, attributes[self.class.attribute_map[key]]))
+#      else
+#        # data not found in attributes(hash), not an issue as the data can be optional
+#      end
+#    end
+#
+#    self
+#  end
+#
+#  def _deserialize(type, value)
+#    case type
+#    when :DateTime
+#      DateTime.parse(value)
+#    when :string
+#      value.to_s
+#    when :int
+#      value.to_i
+#    when :double
+#      value.to_f
+#    when :boolean
+#      if value =~ /^(true|t|yes|y|1)$/i
+#        true
+#      else
+#        false
+#      end
+#    else # model
+#      _model = Object.const_get(type).new
+#      _model.build_from_hash(value)
+#    end
+#  end
+#
+#
+#
+#  # to_body is an alias to to_body (backward compatibility)
+#  def to_hash
+#    hash = {}
+#    self.class.attribute_map.each_pair do |key, value|
+#      if self.send(key).is_a?(Array)
+#        next if self.send(key).empty?
+#        hash[value] = self.send(key).select{|v| !v.nil?}.map{ |v| _to_hash v} unless self.send(key).nil?
+#      else
+#        unless (_tmp_value = _to_hash self.send(key)).nil?
+#          hash[value] = _tmp_value
+#        end
+#      end
+#    end
+#    hash
+#  end
+#
+#  # Method to output non-array value in the form of hash
+#  # For object, use to_hash. Otherwise, just return the value
+#  def _to_hash(value)
+#    if value.respond_to? :to_hash
+#      value.to_hash
+#    else
+#      value
+#    end
+#  end
+
 end
