a500e5ccf37aad92c9742dd6a0c20ae0cd6ce4ce
xhh <xhh@xhh.me>
2015-05-18 16:47:35 +0800

    Fix tests

--- samples/client/petstore/ruby/spec/pet_spec.rb
+++ samples/client/petstore/ruby/spec/pet_spec.rb
@@ -6,17 +6,17 @@ describe "Pet" do
     configure_swagger
     prepare_pet
   end
-  
+
   describe "pet methods" do
     it "should construct a new pet object" do
-      tag1 = Tag.new({'id' => 1, 'name'=> 'tag1'})
-      tag2 = Tag.new({'id' => 2, 'name'=> 'tag2'})
-      category1 = Category.new({:id => 1, :name => 'category unknown'})
+      tag1 = SwaggerClient::Tag.new({'id' => 1, 'name'=> 'tag1'})
+      tag2 = SwaggerClient::Tag.new({'id' => 2, 'name'=> 'tag2'})
+      category1 = SwaggerClient::Category.new({:id => 1, :name => 'category unknown'})
       # initalize using both string and symbol key
       pet_hash = {:'id' => 10002, :'name' => "RUBY UNIT TESTING", :'status' => "pending",
               :'photo_urls' => ["url1", "url2"], :'category' => category1,
               :'tags' => [tag1, tag2]}
-      pet = Pet.new(pet_hash)
+      pet = SwaggerClient::Pet.new(pet_hash)
       # test new
       pet.name.should == "RUBY UNIT TESTING"
       pet.status.should == "pending"
@@ -26,7 +26,7 @@ describe "Pet" do
       pet.category.name.should == 'category unknown'
 
       # test build_from_hash
-      pet2 = Pet.new
+      pet2 = SwaggerClient::Pet.new
       pet2.build_from_hash(pet.to_hash)
       pet.to_hash.should == pet2.to_hash
 
@@ -49,7 +49,7 @@ describe "Pet" do
       pets = SwaggerClient::PetApi.find_pets_by_status(:status => 'available')
       pets.length.should >= 3
     end
-    
+
     it "should not find a pet with invalid status" do
       pets = SwaggerClient::PetApi.find_pets_by_status(:status => 'invalid-status')
       pets.length.should == 0
@@ -57,23 +57,23 @@ describe "Pet" do
 
     it "should find a pet by status" do
       pets = SwaggerClient::PetApi.find_pets_by_status(:status => "available,sold")
-      pets.map {|pet| 
-        if(pet.status != 'available' && pet.status != 'sold') 
+      pets.map {|pet|
+        if(pet.status != 'available' && pet.status != 'sold')
           raise "pet status wasn't right"
         end
       }
     end
-    
+
     it "should update a pet" do
       pet = SwaggerClient::Pet.new({'id' => 10002, 'status' => 'sold'})
       SwaggerClient::PetApi.add_pet(:body => pet)
-      
+
       fetched = SwaggerClient::PetApi.get_pet_by_id(10002)
       fetched.id.should == 10002
       fetched.status.should == 'sold'
     end
 
-    it "should create a pet" do 
+    it "should create a pet" do
       pet = SwaggerClient::Pet.new('id' => 10002, 'name' => "RUBY UNIT TESTING")
       SwaggerClient::PetApi.add_pet(:body => pet)
 
