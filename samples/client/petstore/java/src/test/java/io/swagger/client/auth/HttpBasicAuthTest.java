3ad59480d36760cf3918ea6bc19f4f9317deb077
xhh <xhh@xhh.me>
2015-05-22 11:17:44 +0800

    Add test cases for API key and HTTP basic auth

---
+++ samples/client/petstore/java/src/test/java/io/swagger/client/auth/HttpBasicAuthTest.java
@@ -0,0 +1,33 @@
+package io.swagger.client.auth;
+
+import java.util.Map;
+import java.util.HashMap;
+
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class HttpBasicAuthTest {
+  HttpBasicAuth auth = null;
+
+  @Before
+  public void setup() {
+    auth = new HttpBasicAuth();
+  }
+
+  @Test
+  public void testProcessParams() {
+    Map<String, String> queryParams = new HashMap<String, String>();
+    Map<String, String> headerParams = new HashMap<String, String>();
+
+    auth.setUsername("my-username");
+    auth.setPassword("my-password");
+    auth.processParams(queryParams, headerParams);
+
+    // no changes to query parameters
+    assertEquals(0, queryParams.size());
+    assertEquals(1, headerParams.size());
+    // the string below is base64-encoded result of "my-username:my-password" with the "Basic " prefix
+    final String expected = "Basic bXktdXNlcm5hbWU6bXktcGFzc3dvcmQ=";
+    assertEquals(expected, headerParams.get("Authorization"));
+  }
+}
