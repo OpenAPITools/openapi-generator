85d0e08a80c0d155d96c4440ea7f708e12296ce5
xhh <xhh@xhh.me>
2015-05-25 12:31:54 +0800

    Rebuild Java Petstore client

---
+++ samples/client/petstore/java/src/main/java/io/swagger/client/StringUtil.java
@@ -0,0 +1,23 @@
+package io.swagger.client;
+
+public class StringUtil {
+  public static boolean containsIgnoreCase(String[] array, String value) {
+    for (String str : array) {
+      if (value == null && str == null) return true;
+      if (value != null && value.equalsIgnoreCase(str)) return true;
+    }
+    return false;
+  }
+
+  public static String join(String[] array, String separator) {
+    int len = array.length;
+    if (len == 0) return "";
+
+    StringBuilder out = new StringBuilder();
+    out.append(array[0]);
+    for (int i = 1; i < len; i++) {
+      out.append(separator).append(array[i]);
+    }
+    return out.toString();
+  }
+}
