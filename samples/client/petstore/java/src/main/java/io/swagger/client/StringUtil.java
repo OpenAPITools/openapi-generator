1ef821b157c94ee2366ec29d59fca2be2605519c
xhh <xhh@xhh.me>
2015-05-25 16:01:57 +0800

    Add comments to string utility methods

--- samples/client/petstore/java/src/main/java/io/swagger/client/StringUtil.java
+++ samples/client/petstore/java/src/main/java/io/swagger/client/StringUtil.java
@@ -1,6 +1,13 @@
 package io.swagger.client;
 
 public class StringUtil {
+  /**
+   * Check if the given array contains the given value (with case-insensitive comparison).
+   *
+   * @param array The array
+   * @param value The value to search
+   * @return true if the array contains the value
+   */
   public static boolean containsIgnoreCase(String[] array, String value) {
     for (String str : array) {
       if (value == null && str == null) return true;
@@ -9,6 +16,17 @@ public class StringUtil {
     return false;
   }
 
+  /**
+   * Join an array of strings with the given separator.
+   * <p>
+   * Note: This might be replaced by utility method from commons-lang or guava someday
+   * if one of those libraries is added as dependency.
+   * </p>
+   *
+   * @param array     The array of strings
+   * @param separator The separator
+   * @return the resulting string
+   */
   public static String join(String[] array, String separator) {
     int len = array.length;
     if (len == 0) return "";
