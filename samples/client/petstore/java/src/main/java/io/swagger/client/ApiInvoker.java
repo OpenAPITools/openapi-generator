aa03be7f76e88b231a34c7d3d26b5bbb1e7f5354
xhh <xhh@xhh.me>
2015-05-25 17:25:01 +0800

    Clean up old code

--- samples/client/petstore/java/src/main/java/io/swagger/client/ApiInvoker.java
+++ samples/client/petstore/java/src/main/java/io/swagger/client/ApiInvoker.java
@@ -119,18 +119,6 @@ public class ApiInvoker {
     return contentTypes[0];
   }
 
-  public static String joinString(String[] array, String separator) {
-    int len = array.length;
-    if (len == 0) return "";
-
-    StringBuilder out = new StringBuilder();
-    out.append(array[0]);
-    for (int i = 1; i < len; i++) {
-      out.append(separator).append(array[i]);
-    }
-    return out.toString();
-  }
-
   public void enableDebug() {
     isDebug = true;
   }
