d95c7314a4d5d11d5f6735452b17727b7bf047ba
xhh <xhh@xhh.me>
2015-05-29 11:55:48 +0800

    Allow omitting username/password for HTTP basic auth

--- samples/client/petstore/java/src/test/java/io/swagger/client/auth/HttpBasicAuthTest.java
+++ samples/client/petstore/java/src/test/java/io/swagger/client/auth/HttpBasicAuthTest.java
@@ -27,7 +27,22 @@ public class HttpBasicAuthTest {
     assertEquals(0, queryParams.size());
     assertEquals(1, headerParams.size());
     // the string below is base64-encoded result of "my-username:my-password" with the "Basic " prefix
-    final String expected = "Basic bXktdXNlcm5hbWU6bXktcGFzc3dvcmQ=";
+    String expected = "Basic bXktdXNlcm5hbWU6bXktcGFzc3dvcmQ=";
+    assertEquals(expected, headerParams.get("Authorization"));
+
+    // null username should be treated as empty string
+    auth.setUsername(null);
+    auth.applyToParams(queryParams, headerParams);
+    // the string below is base64-encoded result of ":my-password" with the "Basic " prefix
+    expected = "Basic Om15LXBhc3N3b3Jk";
+    assertEquals(expected, headerParams.get("Authorization"));
+
+    // null password should be treated as empty string
+    auth.setUsername("my-username");
+    auth.setPassword(null);
+    auth.applyToParams(queryParams, headerParams);
+    // the string below is base64-encoded result of "my-username:" with the "Basic " prefix
+    expected = "Basic bXktdXNlcm5hbWU6";
     assertEquals(expected, headerParams.get("Authorization"));
   }
 }
