cranberyxl/swagger-codegen

add template for Retrofit and Gson

modules/swagger-codegen/src/main/java/com/wordnik/swagger/codegen/languages/RetrofitClientCodegen.java
@@ -1,0 +1,190 @@
+package com.wordnik.swagger.codegen.languages;
+
+import com.wordnik.swagger.codegen.*;
+import com.wordnik.swagger.models.Operation;
+import com.wordnik.swagger.models.properties.*;
+
+import java.util.*;
+import java.io.File;
+
+public class RetrofitClientCodegen extends DefaultCodegen implements CodegenConfig {
+  protected String invokerPackage = "io.swagger.client";
+  protected String groupId = "io.swagger";
+  protected String artifactId = "swagger-java-client";
+  protected String artifactVersion = "1.0.0";
+  protected String sourceFolder = "src/main/java";
+
+  public CodegenType getTag() {
+    return CodegenType.CLIENT;
+  }
+
+  public String getName() {
+    return "retrofit";
+  }
+
+  public String getHelp() {
+    return "Generates a Retrofit client library.";
+  }
+
+  public RetrofitClientCodegen() {
+    super();
+    outputFolder = "generated-code/java";
+    modelTemplateFiles.put("model.mustache", ".java");
+    apiTemplateFiles.put("api.mustache", ".java");
+    templateDir = "Java";
+    apiPackage = "io.swagger.client.api";
+    modelPackage = "io.swagger.client.model";
+
+    reservedWords = new HashSet<String> (
+      Arrays.asList(
+        "abstract", "continue", "for", "new", "switch", "assert", 
+        "default", "if", "package", "synchronized", "boolean", "do", "goto", "private", 
+        "this", "break", "double", "implements", "protected", "throw", "byte", "else", 
+        "import", "public", "throws", "case", "enum", "instanceof", "return", "transient", 
+        "catch", "extends", "int", "short", "try", "char", "final", "interface", "static", 
+        "void", "class", "finally", "long", "strictfp", "volatile", "const", "float", 
+        "native", "super", "while")
+    );
+
+    additionalProperties.put("invokerPackage", invokerPackage);
+    additionalProperties.put("groupId", groupId);
+    additionalProperties.put("artifactId", artifactId);
+    additionalProperties.put("artifactVersion", artifactVersion);
+
+    supportingFiles.add(new SupportingFile("pom.mustache", "", "pom.xml"));
+    supportingFiles.add(new SupportingFile("service.mustache", 
+      (sourceFolder + File.separator + invokerPackage).replace(".", java.io.File.separator), "ServiceGenerator.java"));
+
+    languageSpecificPrimitives = new HashSet<String>(
+      Arrays.asList(
+        "String",
+        "boolean",
+        "Boolean",
+        "Double",
+        "Integer",
+        "Long",
+        "Float",
+        "Object")
+      );
+    instantiationTypes.put("array", "ArrayList");
+    instantiationTypes.put("map", "HashMap");
+  }
+
+  @Override
+  public String escapeReservedWord(String name) {
+    return "_" + name;
+  }
+
+  @Override
+  public String apiFileFolder() {
+    return outputFolder + "/" + sourceFolder + "/" + apiPackage().replace('.', File.separatorChar);
+  }
+
+  public String modelFileFolder() {
+    return outputFolder + "/" + sourceFolder + "/" + modelPackage().replace('.', File.separatorChar);
+  }
+
+  @Override
+  public String toVarName(String name) {
+    // replace - with _ e.g. created-at => created_at
+    name = name.replaceAll("-", "_");
+
+    // if it's all uppper case, do nothing
+    if (name.matches("^[A-Z_]*$"))
+      return name;
+
+    // camelize (lower first character) the variable name
+    // pet_id => petId
+    name = camelize(name, true);
+
+    // for reserved word or word starting with number, append _
+    if(reservedWords.contains(name) || name.matches("^\\d.*"))
+      name = escapeReservedWord(name);
+
+    return name;
+  }
+
+  @Override
+  public String toParamName(String name) {
+    // should be the same as variable name
+    return toVarName(name);
+  }
+
+  @Override
+  public String toModelName(String name) {
+    // model name cannot use reserved keyword, e.g. return
+    if(reservedWords.contains(name))
+      throw new RuntimeException(name + " (reserved word) cannot be used as a model name");
+
+    // camelize the model name
+    // phone_number => PhoneNumber
+    return camelize(name);
+  }
+
+  @Override
+  public String toModelFilename(String name) {
+    // should be the same as the model name
+    return toModelName(name);
+  }
+
+
+  @Override
+  public String getTypeDeclaration(Property p) {
+    if(p instanceof ArrayProperty) {
+      ArrayProperty ap = (ArrayProperty) p;
+      Property inner = ap.getItems();
+      return getSwaggerType(p) + "<" + getTypeDeclaration(inner) + ">";
+    }
+    else if (p instanceof MapProperty) {
+      MapProperty mp = (MapProperty) p;
+      Property inner = mp.getAdditionalProperties();
+
+      return getSwaggerType(p) + "<String, " + getTypeDeclaration(inner) + ">";
+    }
+    return super.getTypeDeclaration(p);
+  }
+
+  @Override
+  public String getSwaggerType(Property p) {
+    String swaggerType = super.getSwaggerType(p);
+    String type = null;
+    if(typeMapping.containsKey(swaggerType)) {
+      type = typeMapping.get(swaggerType);
+      if(languageSpecificPrimitives.contains(type))
+        return toModelName(type);
+    }
+    else
+      type = swaggerType;
+    return toModelName(type);
+  }
+
+  @Override
+  public String toOperationId(String operationId) {
+    // method name cannot use reserved keyword, e.g. return
+    if(reservedWords.contains(operationId))
+      throw new RuntimeException(operationId + " (reserved word) cannot be used as method name");
+
+    return camelize(operationId, true);
+  }
+
+  public Map<String, Object> postProcessOperations(Map<String, Object> objs) {
+    Map<String, Object> operations = (Map<String, Object>)objs.get("operations");
+    if(operations != null) {
+      List<CodegenOperation> ops = (List<CodegenOperation>) operations.get("operation");
+      for(CodegenOperation operation : ops) {
+        if (operation.hasConsumes == Boolean.TRUE) {
+          Map<String, String> firstType = operation.consumes.get(0);
+          if (firstType != null) {
+            if ("multipart/form-data".equals(firstType.get("mediaType"))) {
+              operation.isMultipart = Boolean.TRUE;
+            }
+          }
+        }
+        if(operation.returnType == null) {
+          operation.returnType = "Void";        
+        }
+      }
+    }
+    return objs;
+  }
+}

OpenAPITools/openapi-generator

modules/swagger-codegen/src/main/java/com/wordnik/swagger/codegen/languages/RetrofitClientCodegen.java
@@ -1,0 +1,190 @@
+package com.wordnik.swagger.codegen.languages;
+
+import com.wordnik.swagger.codegen.*;
+import com.wordnik.swagger.models.Operation;
+import com.wordnik.swagger.models.properties.*;
+
+import java.util.*;
+import java.io.File;
+
+public class RetrofitClientCodegen extends DefaultCodegen implements CodegenConfig {
+  protected String invokerPackage = "io.swagger.client";
+  protected String groupId = "io.swagger";
+  protected String artifactId = "swagger-java-client";
+  protected String artifactVersion = "1.0.0";
+  protected String sourceFolder = "src/main/java";
+
+  public CodegenType getTag() {
+    return CodegenType.CLIENT;
+  }
+
+  public String getName() {
+    return "retrofit";
+  }
+
+  public String getHelp() {
+    return "Generates a Retrofit client library.";
+  }
+
+  public RetrofitClientCodegen() {
+    super();
+    outputFolder = "generated-code/java";
+    modelTemplateFiles.put("model.mustache", ".java");
+    apiTemplateFiles.put("api.mustache", ".java");
+    templateDir = "retrofit";
+    apiPackage = "io.swagger.client.api";
+    modelPackage = "io.swagger.client.model";
+
+    reservedWords = new HashSet<String> (
+      Arrays.asList(
+        "abstract", "continue", "for", "new", "switch", "assert", 
+        "default", "if", "package", "synchronized", "boolean", "do", "goto", "private", 
+        "this", "break", "double", "implements", "protected", "throw", "byte", "else", 
+        "import", "public", "throws", "case", "enum", "instanceof", "return", "transient", 
+        "catch", "extends", "int", "short", "try", "char", "final", "interface", "static", 
+        "void", "class", "finally", "long", "strictfp", "volatile", "const", "float", 
+        "native", "super", "while")
+    );
+
+    additionalProperties.put("invokerPackage", invokerPackage);
+    additionalProperties.put("groupId", groupId);
+    additionalProperties.put("artifactId", artifactId);
+    additionalProperties.put("artifactVersion", artifactVersion);
+
+    supportingFiles.add(new SupportingFile("pom.mustache", "", "pom.xml"));
+    supportingFiles.add(new SupportingFile("service.mustache", 
+      (sourceFolder + File.separator + invokerPackage).replace(".", java.io.File.separator), "ServiceGenerator.java"));
+
+    languageSpecificPrimitives = new HashSet<String>(
+      Arrays.asList(
+        "String",
+        "boolean",
+        "Boolean",
+        "Double",
+        "Integer",
+        "Long",
+        "Float",
+        "Object")
+      );
+    instantiationTypes.put("array", "ArrayList");
+    instantiationTypes.put("map", "HashMap");
+  }
+
+  @Override
+  public String escapeReservedWord(String name) {
+    return "_" + name;
+  }
+
+  @Override
+  public String apiFileFolder() {
+    return outputFolder + "/" + sourceFolder + "/" + apiPackage().replace('.', File.separatorChar);
+  }
+
+  public String modelFileFolder() {
+    return outputFolder + "/" + sourceFolder + "/" + modelPackage().replace('.', File.separatorChar);
+  }
+
+  @Override
+  public String toVarName(String name) {
+    // replace - with _ e.g. created-at => created_at
+    name = name.replaceAll("-", "_");
+
+    // if it's all uppper case, do nothing
+    if (name.matches("^[A-Z_]*$"))
+      return name;
+
+    // camelize (lower first character) the variable name
+    // pet_id => petId
+    name = camelize(name, true);
+
+    // for reserved word or word starting with number, append _
+    if(reservedWords.contains(name) || name.matches("^\\d.*"))
+      name = escapeReservedWord(name);
+
+    return name;
+  }
+
+  @Override
+  public String toParamName(String name) {
+    // should be the same as variable name
+    return toVarName(name);
+  }
+
+  @Override
+  public String toModelName(String name) {
+    // model name cannot use reserved keyword, e.g. return
+    if(reservedWords.contains(name))
+      throw new RuntimeException(name + " (reserved word) cannot be used as a model name");
+
+    // camelize the model name
+    // phone_number => PhoneNumber
+    return camelize(name);
+  }
+
+  @Override
+  public String toModelFilename(String name) {
+    // should be the same as the model name
+    return toModelName(name);
+  }
+
+
+  @Override
+  public String getTypeDeclaration(Property p) {
+    if(p instanceof ArrayProperty) {
+      ArrayProperty ap = (ArrayProperty) p;
+      Property inner = ap.getItems();
+      return getSwaggerType(p) + "<" + getTypeDeclaration(inner) + ">";
+    }
+    else if (p instanceof MapProperty) {
+      MapProperty mp = (MapProperty) p;
+      Property inner = mp.getAdditionalProperties();
+
+      return getSwaggerType(p) + "<String, " + getTypeDeclaration(inner) + ">";
+    }
+    return super.getTypeDeclaration(p);
+  }
+
+  @Override
+  public String getSwaggerType(Property p) {
+    String swaggerType = super.getSwaggerType(p);
+    String type = null;
+    if(typeMapping.containsKey(swaggerType)) {
+      type = typeMapping.get(swaggerType);
+      if(languageSpecificPrimitives.contains(type))
+        return toModelName(type);
+    }
+    else
+      type = swaggerType;
+    return toModelName(type);
+  }
+
+  @Override
+  public String toOperationId(String operationId) {
+    // method name cannot use reserved keyword, e.g. return
+    if(reservedWords.contains(operationId))
+      throw new RuntimeException(operationId + " (reserved word) cannot be used as method name");
+
+    return camelize(operationId, true);
+  }
+
+  public Map<String, Object> postProcessOperations(Map<String, Object> objs) {
+    Map<String, Object> operations = (Map<String, Object>)objs.get("operations");
+    if(operations != null) {
+      List<CodegenOperation> ops = (List<CodegenOperation>) operations.get("operation");
+      for(CodegenOperation operation : ops) {
+        if (operation.hasConsumes == Boolean.TRUE) {
+          Map<String, String> firstType = operation.consumes.get(0);
+          if (firstType != null) {
+            if ("multipart/form-data".equals(firstType.get("mediaType"))) {
+              operation.isMultipart = Boolean.TRUE;
+            }
+          }
+        }
+        if(operation.returnType == null) {
+          operation.returnType = "Void";        
+        }
+      }
+    }
+    return objs;
+  }
+}