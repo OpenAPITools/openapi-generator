e542063f56541c22b1eb099012baa7d4104dfe3f
xhh <xhh@xhh.me>
2015-05-29 11:28:03 +0800

    Add authentication helper methods and test cases

---
+++ samples/client/petstore/java/src/test/java/io/swagger/client/ConfigurationTest.java
@@ -0,0 +1,56 @@
+package io.swagger.client;
+
+import io.swagger.client.auth.*;
+
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class ConfigurationTest {
+  @Test
+  public void testGetAuthentication() {
+    Authentication auth = Configuration.getAuthentication("api_key");
+    assertNotNull(auth);
+    assertTrue(auth instanceof ApiKeyAuth);
+    ApiKeyAuth apiKeyAuth = (ApiKeyAuth) auth;
+    assertEquals("header", apiKeyAuth.getLocation());
+    assertEquals("api_key", apiKeyAuth.getParamName());
+
+    auth = Configuration.getAuthentication("petstore_auth");
+    assertTrue(auth instanceof OAuth);
+
+    assertNull(Configuration.getAuthentication("unknown"));
+  }
+
+  @Test
+  public void testSetUsername() {
+    try {
+      Configuration.setUsername("my-username");
+      fail("should throw RuntimeException");
+    } catch (RuntimeException e) {
+    }
+  }
+
+  @Test
+  public void testSetPassword() {
+    try {
+      Configuration.setPassword("my-password");
+      fail("should throw RuntimeException");
+    } catch (RuntimeException e) {
+    }
+  }
+
+  @Test
+  public void testSetApiKeyAndPrefix() {
+    ApiKeyAuth auth = (ApiKeyAuth) Configuration.getAuthentication("api_key");
+    auth.setApiKey(null);
+    auth.setApiKeyPrefix(null);
+
+    Configuration.setApiKey("my-api-key");
+    Configuration.setApiKeyPrefix("Token");
+    assertEquals("my-api-key", auth.getApiKey());
+    assertEquals("Token", auth.getApiKeyPrefix());
+
+    auth.setApiKey(null);
+    auth.setApiKeyPrefix(null);
+  }
+}
