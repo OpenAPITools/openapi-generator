04acae7fa1d49c38916a0114ad550e39ade20189
hrachya <hrachya@HYEGHSEALNXD1>
2015-06-01 10:43:23 -0700

    removing generic header comments

--- modules/swagger-codegen-cli/src/main/java/com/wordnik/swagger/codegen/cmd/ConfigHelp.java
+++ modules/swagger-codegen-cli/src/main/java/com/wordnik/swagger/codegen/cmd/ConfigHelp.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.wordnik.swagger.codegen.cmd;
 
 import com.wordnik.swagger.codegen.CliOption;
