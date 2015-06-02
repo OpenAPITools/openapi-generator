b64315d967714091c65d4ae7ff1b00712b5dc0b2
geekerzp <geekerzp@gmail.com>
2015-06-02 11:50:47 +0800

    added comments for objc client

--- samples/client/petstore/objc/client/SWGApiClient.h
+++ samples/client/petstore/objc/client/SWGApiClient.h
@@ -3,7 +3,7 @@
 
 /**
  * A key for `NSError` user info dictionaries.
- * 
+ *
  * The corresponding value is the parsed response body for an HTTP error.
  */
 extern NSString *const SWGResponseObjectErrorKey;
@@ -20,44 +20,104 @@ extern NSString *const SWGResponseObjectErrorKey;
 @property(nonatomic, assign) BOOL logHTTP;
 @property(nonatomic, readonly) NSOperationQueue* queue;
 
+/**
+ * Get the Api Client instance from pool
+ */
 +(SWGApiClient *)sharedClientFromPool:(NSString *)baseUrl;
 
+/**
+ * Get the operations queue
+ */
 +(NSOperationQueue*) sharedQueue;
 
+/**
+ * Turn on logging
+ */
 +(void)setLoggingEnabled:(bool) state;
 
+/**
+ * Clear Cache
+ */
 +(void)clearCache;
 
+/**
+ * Turn on cache
+ */
 +(void)setCacheEnabled:(BOOL) enabled;
 
+/**
+ * Get the request queue size
+ */
 +(unsigned long)requestQueueSize;
 
+/**
+ * Set the client unreachable
+ */
 +(void) setOfflineState:(BOOL) state;
 
+/**
+ * Get the client reachability
+ */
 +(AFNetworkReachabilityStatus) getReachabilityStatus;
 
+/**
+ * Get the next request id
+ */
 +(NSNumber*) nextRequestId;
 
+/**
+ * Generate request id and add it to the queue
+ */
 +(NSNumber*) queueRequest;
 
+/**
+ * Remove request id from the queue
+ */
 +(void) cancelRequest:(NSNumber*)requestId;
 
+/**
+ * URL encode NSString
+ */
 +(NSString*) escape:(id)unescaped;
 
+/**
+ * Set the client reachability
+ */
 +(void) setReachabilityChangeBlock:(void(^)(int))changeBlock;
 
+/**
+ * Set the client reachability strategy
+ */
 +(void) configureCacheReachibilityForHost:(NSString*)host;
 
+/**
+ * Detect Accept header from accepts NSArray
+ */
 +(NSString *) selectHeaderAccept:(NSArray *)accepts;
+
+/**
+ * Detect Content-Type header from contentTypes NSArray
+ */
 +(NSString *) selectHeaderContentType:(NSArray *)contentTypes;
 
+/**
+ * Set header for request
+ */
 -(void)setHeaderValue:(NSString*) value
                 forKey:(NSString*) forKey;
 
+/**
+ * Update header parameters and query parameters for authentication
+ */
 - (void) updateHeaderParams:(NSDictionary **)headers
                 queryParams:(NSDictionary **)querys
            WithAuthSettings:(NSArray *)authSettings;
 
+/**
+ * Perform request
+ *
+ * @discussion Request with non-empty response
+ */
 -(NSNumber*)  dictionary:(NSString*) path
                   method:(NSString*) method
              queryParams:(NSDictionary*) queryParams
@@ -68,6 +128,11 @@ extern NSString *const SWGResponseObjectErrorKey;
      responseContentType:(NSString*) responseContentType
          completionBlock:(void (^)(NSDictionary*, NSError *))completionBlock;
 
+/**
+ * Perform request
+ *
+ * @discussion Request with empty response
+ */
 -(NSNumber*)  stringWithCompletionBlock:(NSString*) path
                                  method:(NSString*) method
                             queryParams:(NSDictionary*) queryParams
@@ -78,13 +143,3 @@ extern NSString *const SWGResponseObjectErrorKey;
                     responseContentType:(NSString*) responseContentType
                         completionBlock:(void (^)(NSString*, NSError *))completionBlock;
 @end
-
-
-
-
-
-
-
-
-
-
