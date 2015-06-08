3ee415517a2b79d8e0e069914a88c7cefcab4ddc
Tony Tam <fehguy@gmail.com>
2015-06-08 15:48:49 -0700

    rebuilt

--- samples/client/petstore/async-scala/src/main/scala/io/swagger/client/model/Order.scala
+++ samples/client/petstore/async-scala/src/main/scala/io/swagger/client/model/Order.scala
@@ -1,12 +1,16 @@
 package io.swagger.client.model
 
+import org.joda.time.DateTime
 
-case class Order(
-                  id: Long,
-                  petId: Long,
-                  quantity: Integer,
-                  shipDate: DateTime,
-                  status: String, // Order Status
-                  complete: Boolean
 
-                  )
+    
+        case class Order (
+        id: Long,
+        petId: Long,
+        quantity: Integer,
+        shipDate: DateTime,
+        status: String,  // Order Status
+        complete: Boolean
+        
+        )
+    
