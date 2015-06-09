c85175dd1afa05028726236488f2cbdeb67cbc9c
wing328 <wing328hk@gmail.com>
2015-06-09 13:52:01 +0800

    fix csharp refer to apiinvoker

--- samples/client/petstore/csharp/src/main/csharp/io/swagger/client/Configuration.cs
+++ samples/client/petstore/csharp/src/main/csharp/io/swagger/client/Configuration.cs
@@ -7,62 +7,41 @@ using System.Text;
 using IO.Swagger.Client;
 
 namespace IO.Swagger.Client {
-///
-<summary>
-    /// Represents a set of configuration settings
-    ///
-</summary>
-public class Configuration{
+  /// <summary>
+  /// Represents a set of configuration settings
+  /// </summary>
+  public class Configuration{
 
-///
-<summary>
+    /// <summary>
     /// Gets or sets the API client. This is the default API client for making HTTP calls.
-    ///
-</summary>
-///
-<value>The API client.</value>
-public static ApiClient apiClient = new ApiClient();
+    /// </summary>
+    /// <value>The API client.</value>
+    public static ApiClient apiClient = new ApiClient();
 
-///
-<summary>
+    /// <summary>
     /// Gets or sets the username (HTTP basic authentication)
-    ///
-</summary>
-///
-<value>The username.</value>
-public static String username { get; set; }
+    /// </summary>
+    /// <value>The username.</value>
+    public static String username { get; set; }
 
-///
-<summary>
+    /// <summary>
     /// Gets or sets the password (HTTP basic authentication)
-    ///
-</summary>
-///
-<value>The password.</value>
-public static String password { get; set; }
+    /// </summary>
+    /// <value>The password.</value>
+    public static String password { get; set; }
 
-///
-<summary>
-    /// Gets or sets the API key based on the authentication name
-    ///
-</summary>
-///
-<value>The API key.</value>
-public static Dictionary
-<String, String> apiKey = new Dictionary
-<String, String>();
+    /// <summary>
+    /// Gets or sets the API key based on the authentication name 
+    /// </summary>
+    /// <value>The API key.</value>
+    public static Dictionary<String, String> apiKey = new Dictionary<String, String>();
 
-///
-<summary>
-    /// Gets or sets the prefix (e.g. Token) of the API key based on the authentication name
-    ///
-</summary>
-///
-<value>The prefix of the API key.</value>
-public static Dictionary
-<String, String> apiKeyPrefix = new Dictionary
-<String, String>();
+    /// <summary>
+    /// Gets or sets the prefix (e.g. Token) of the API key based on the authentication name 
+    /// </summary>
+    /// <value>The prefix of the API key.</value>
+    public static Dictionary<String, String> apiKeyPrefix = new Dictionary<String, String>();
 
 
-}
+  }
 }
