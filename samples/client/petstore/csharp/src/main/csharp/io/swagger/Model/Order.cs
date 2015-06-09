680078dc585c0a6a1e667bc8a96c0903a6b48910
wing328 <wing328hk@gmail.com>
2015-06-09 13:52:01 +0800

    fix csharp refer to apiinvoker

--- samples/client/petstore/csharp/src/main/csharp/io/swagger/Model/Order.cs
+++ samples/client/petstore/csharp/src/main/csharp/io/swagger/Model/Order.cs
@@ -5,86 +5,78 @@ using System.Collections.Generic;
 using System.Runtime.Serialization;
 using Newtonsoft.Json;
 
+namespace IO.Swagger.Model {
+
+  /// <summary>
+  /// 
+  /// </summary>
+  [DataContract]
+  public class Order {
     
-        namespace IO.Swagger.Model {
-
-        ///
-        <summary>
-            /// 
-            ///
-        </summary>
-        [DataContract]
-        public class Order {
-        
-            
-            [DataMember(Name="id", EmitDefaultValue=false)]
-            public long? Id { get; set; }
-
-        
-            
-            [DataMember(Name="petId", EmitDefaultValue=false)]
-            public long? PetId { get; set; }
-
-        
-            
-            [DataMember(Name="quantity", EmitDefaultValue=false)]
-            public int? Quantity { get; set; }
-
-        
-            
-            [DataMember(Name="shipDate", EmitDefaultValue=false)]
-            public DateTime? ShipDate { get; set; }
-
-        
-            /* Order Status */
-            [DataMember(Name="status", EmitDefaultValue=false)]
-            public string Status { get; set; }
-
-        
-            
-            [DataMember(Name="complete", EmitDefaultValue=false)]
-            public bool? Complete { get; set; }
-
-        
-
-        ///
-        <summary>
-            /// Get the string presentation of the object
-            ///
-        </summary>
-        ///
-        <returns>String presentation of the object</returns>
-        public override string ToString()  {
-        var sb = new StringBuilder();
-        sb.Append("class Order {\n");
-        
-            sb.Append("  Id: ").Append(Id).Append("\n");
-        
-            sb.Append("  PetId: ").Append(PetId).Append("\n");
-        
-            sb.Append("  Quantity: ").Append(Quantity).Append("\n");
-        
-            sb.Append("  ShipDate: ").Append(ShipDate).Append("\n");
-        
-            sb.Append("  Status: ").Append(Status).Append("\n");
-        
-            sb.Append("  Complete: ").Append(Complete).Append("\n");
-        
-        sb.Append("}\n");
-        return sb.ToString();
-        }
-
-        ///
-        <summary>
-            /// Get the JSON string presentation of the object
-            ///
-        </summary>
-        ///
-        <returns>JSON string presentation of the object</returns>
-        public string ToJson() {
-        return JsonConvert.SerializeObject(this, Formatting.Indented);
-        }
-
-        }
     
+    [DataMember(Name="id", EmitDefaultValue=false)]
+    public long? Id { get; set; }
+
+    
+    
+    [DataMember(Name="petId", EmitDefaultValue=false)]
+    public long? PetId { get; set; }
+
+    
+    
+    [DataMember(Name="quantity", EmitDefaultValue=false)]
+    public int? Quantity { get; set; }
+
+    
+    
+    [DataMember(Name="shipDate", EmitDefaultValue=false)]
+    public DateTime? ShipDate { get; set; }
+
+    
+    /* Order Status */
+    [DataMember(Name="status", EmitDefaultValue=false)]
+    public string Status { get; set; }
+
+    
+    
+    [DataMember(Name="complete", EmitDefaultValue=false)]
+    public bool? Complete { get; set; }
+
+    
+
+    /// <summary>
+    /// Get the string presentation of the object
+    /// </summary>
+    /// <returns>String presentation of the object</returns>
+    public override string ToString()  {
+      var sb = new StringBuilder();
+      sb.Append("class Order {\n");
+      
+      sb.Append("  Id: ").Append(Id).Append("\n");
+      
+      sb.Append("  PetId: ").Append(PetId).Append("\n");
+      
+      sb.Append("  Quantity: ").Append(Quantity).Append("\n");
+      
+      sb.Append("  ShipDate: ").Append(ShipDate).Append("\n");
+      
+      sb.Append("  Status: ").Append(Status).Append("\n");
+      
+      sb.Append("  Complete: ").Append(Complete).Append("\n");
+      
+      sb.Append("}\n");
+      return sb.ToString();
+    }
+
+    /// <summary>
+    /// Get the JSON string presentation of the object
+    /// </summary>
+    /// <returns>JSON string presentation of the object</returns>
+    public string ToJson() {
+      return JsonConvert.SerializeObject(this, Formatting.Indented);
+    }
+
+}
+  
+  
 }
