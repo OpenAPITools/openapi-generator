7fb749ae17c8017131d6717f1cfbb48390ad4789
wing328 <wing328hk@gmail.com>
2015-06-14 00:59:40 +0800

    fix jar path in windows

--- modules/swagger-codegen/src/main/java/io/swagger/codegen/DefaultGenerator.java
+++ modules/swagger-codegen/src/main/java/io/swagger/codegen/DefaultGenerator.java
@@ -268,7 +268,7 @@ public class DefaultGenerator extends AbstractGenerator implements Generator {
                         // continue
                     }
                     if (in == null) {
-                        in = this.getClass().getClassLoader().getResourceAsStream(config.templateDir() + File.separator + support.templateFile);
+                        in = this.getClass().getClassLoader().getResourceAsStream(getCPResourcePath(config.templateDir() + File.separator + support.templateFile));
                     }
                     File outputFile = new File(outputFilename);
                     OutputStream out = new FileOutputStream(outputFile, false);
