bc92465bf7596d488b9e3cb73325b773033a2482
geekerzp <geekerzp@gmail.com>
2015-06-13 10:48:45 +0800

    Update Objective-C method description (doc comments) style

--- samples/client/petstore/objc/client/SWGUserApi.h
+++ samples/client/petstore/objc/client/SWGUserApi.h
@@ -14,61 +14,61 @@
 +(SWGUserApi*) apiWithHeader:(NSString*)headerValue key:(NSString*)key;
 +(void) setBasePath:(NSString*)basePath;
 +(NSString*) getBasePath;
-//
-//
-// Create user
-// This can only be done by the logged in user.
-//
-// @param body Created user object
-// 
-//
-// @return 
+///
+///
+/// Create user
+/// This can only be done by the logged in user.
+///
+/// @param body Created user object
+/// 
+///
+/// @return 
 -(NSNumber*) createUserWithCompletionBlock :(SWGUser*) body 
     
     
     completionHandler: (void (^)(NSError* error))completionBlock;
 
 
-//
-//
-// Creates list of users with given input array
-// 
-//
-// @param body List of user object
-// 
-//
-// @return 
+///
+///
+/// Creates list of users with given input array
+/// 
+///
+/// @param body List of user object
+/// 
+///
+/// @return 
 -(NSNumber*) createUsersWithArrayInputWithCompletionBlock :(NSArray<SWGUser>*) body 
     
     
     completionHandler: (void (^)(NSError* error))completionBlock;
 
 
-//
-//
-// Creates list of users with given input array
-// 
-//
-// @param body List of user object
-// 
-//
-// @return 
+///
+///
+/// Creates list of users with given input array
+/// 
+///
+/// @param body List of user object
+/// 
+///
+/// @return 
 -(NSNumber*) createUsersWithListInputWithCompletionBlock :(NSArray<SWGUser>*) body 
     
     
     completionHandler: (void (^)(NSError* error))completionBlock;
 
 
-//
-//
-// Logs user into the system
-// 
-//
-// @param username The user name for login
-// @param password The password for login in clear text
-// 
-//
-// @return NSString*
+///
+///
+/// Logs user into the system
+/// 
+///
+/// @param username The user name for login
+/// @param password The password for login in clear text
+/// 
+///
+/// @return NSString*
 -(NSNumber*) loginUserWithCompletionBlock :(NSString*) username 
      password:(NSString*) password 
     
@@ -76,44 +76,44 @@
     
 
 
-//
-//
-// Logs out current logged in user session
-// 
-//
-// 
-//
-// @return 
+///
+///
+/// Logs out current logged in user session
+/// 
+///
+/// 
+///
+/// @return 
 -(NSNumber*) logoutUserWithCompletionBlock :
     
     (void (^)(NSError* error))completionBlock;
 
 
-//
-//
-// Get user by user name
-// 
-//
-// @param username The name that needs to be fetched. Use user1 for testing. 
-// 
-//
-// @return SWGUser*
+///
+///
+/// Get user by user name
+/// 
+///
+/// @param username The name that needs to be fetched. Use user1 for testing. 
+/// 
+///
+/// @return SWGUser*
 -(NSNumber*) getUserByNameWithCompletionBlock :(NSString*) username 
     
     completionHandler: (void (^)(SWGUser* output, NSError* error))completionBlock;
     
 
 
-//
-//
-// Updated user
-// This can only be done by the logged in user.
-//
-// @param username name that need to be deleted
-// @param body Updated user object
-// 
-//
-// @return 
+///
+///
+/// Updated user
+/// This can only be done by the logged in user.
+///
+/// @param username name that need to be deleted
+/// @param body Updated user object
+/// 
+///
+/// @return 
 -(NSNumber*) updateUserWithCompletionBlock :(NSString*) username 
      body:(SWGUser*) body 
     
@@ -121,15 +121,15 @@
     completionHandler: (void (^)(NSError* error))completionBlock;
 
 
-//
-//
-// Delete user
-// This can only be done by the logged in user.
-//
-// @param username The name that needs to be deleted
-// 
-//
-// @return 
+///
+///
+/// Delete user
+/// This can only be done by the logged in user.
+///
+/// @param username The name that needs to be deleted
+/// 
+///
+/// @return 
 -(NSNumber*) deleteUserWithCompletionBlock :(NSString*) username 
     
     
