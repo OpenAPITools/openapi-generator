df72188bc0c7e4db21d87b288fb6ae97ab5ccfd5
wing328 <wing328hk@gmail.com>
2015-06-18 22:06:59 +0800

    add cli support to csharp

--- samples/client/petstore/csharp/compile.bat
+++ samples/client/petstore/csharp/compile.bat
@@ -1,3 +1,3 @@
 SET CSCPATH=%SYSTEMROOT%\Microsoft.NET\Framework\v4.0.30319
-%CSCPATH%\csc /reference:bin/Newtonsoft.Json.dll;bin/RestSharp.dll /target:library /out:bin/IO.Swagger.Client.dll /recurse:src\*.cs /doc:bin/IO.Swagger.Client.xml
+%CSCPATH%\csc /reference:bin/Newtonsoft.Json.dll;bin/RestSharp.dll /target:library /out:bin/.dll /recurse:src\*.cs /doc:bin/.xml
 
