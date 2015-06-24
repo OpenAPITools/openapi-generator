621e3e7b1a42ea91037629b41d35f1d8a0b909ad
xhh <xhh@xhh.me>
2015-06-24 15:57:01 +0800

    Add comments for some configuration options

--- samples/client/petstore/ruby/lib/swagger_client/swagger/configuration.rb
+++ samples/client/petstore/ruby/lib/swagger_client/swagger/configuration.rb
@@ -3,17 +3,62 @@ require 'logger'
 module SwaggerClient
   module Swagger
     class Configuration
-      attr_accessor :format, :api_key, :api_key_prefix, :username, :password, :auth_token,
-                    :scheme, :host, :base_path, :user_agent, :debug, :logger, :inject_format,
-                    :force_ending_format, :camelize_params, :user_agent, :verify_ssl
+      attr_accessor :scheme, :host, :base_path, :user_agent, :format, :auth_token, :inject_format, :force_ending_format, :camelize_params
+
+      # Defines the username used with HTTP basic authentication.
+      #
+      # @return [String]
+      attr_accessor :username
+
+      # Defines the password used with HTTP basic authentication.
+      #
+      # @return [String]
+      attr_accessor :password
+
+      # Defines API keys used with API Key authentications.
+      #
+      # @return [Hash] key: parameter name, value: parameter value (API key)
+      #
+      # @example parameter name is "api_key", API key is "xxx" (e.g. "api_key=xxx" in query string)
+      #   config.api_key['api_key'] = 'xxx'
+      attr_accessor :api_key
+
+      # Defines API key prefixes used with API Key authentications.
+      #
+      # @return [Hash] key: parameter name, value: API key prefix
+      #
+      # @example parameter name is "Authorization", API key prefix is "Token" (e.g. "Authorization: Token xxx" in headers)
+      #   config.api_key_prefix['api_key'] = 'Token'
+      attr_accessor :api_key_prefix
+
+      # Set this to false to skip verifying SSL certificate when calling API from https server.
+      # Default to true.
+      #
+      # @note Do NOT set it to false in production code, otherwise you would face multiple types of cryptographic attacks.
+      #
+      # @return [true, false]
+      attr_accessor :verify_ssl
 
       # Set this to customize the certificate file to verify the peer.
       #
       # @return [String] the path to the certificate file
       #
       # @see The `cainfo` option of Typhoeus, `--cert` option of libcurl. Related source code:
-      #   https://github.com/typhoeus/typhoeus/blob/master/lib/typhoeus/easy_factory.rb#L145
+      # https://github.com/typhoeus/typhoeus/blob/master/lib/typhoeus/easy_factory.rb#L145
       attr_accessor :ssl_ca_cert
+
+      # Set this to enable/disable debugging. When enabled (set to true), HTTP request/response
+      # details will be logged with `logger.debug` (see the `logger` attribute).
+      # Default to false.
+      #
+      # @return [true, false]
+      attr_accessor :debug
+
+      # Defines the logger used for debugging.
+      # Default to `Rails.logger` (when in Rails) or logging to STDOUT.
+      #
+      # @return [#debug]
+      attr_accessor :logger
 
       # Defaults go in here..
       def initialize
@@ -26,20 +71,12 @@ module SwaggerClient
         @force_ending_format = false
         @camelize_params = true
 
-        # keys for API key authentication (param-name => api-key)
         @api_key = {}
-        # api-key prefix for API key authentication, e.g. "Bearer" (param-name => api-key-prefix)
         @api_key_prefix = {}
 
-        # whether to verify SSL certificate, default to true
-        # Note: do NOT set it to false in production code, otherwise you would
-        #   face multiple types of cryptographic attacks
         @verify_ssl = true
 
-        # whether to enable debugging
         @debug = false
-
-        # configure logger, default to logger of Rails (if in Rails) or STDOUT
         @logger = defined?(Rails) ? Rails.logger : Logger.new(STDOUT)
       end
     end
