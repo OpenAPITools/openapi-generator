e7640fb40fa004697a4da45c43d6519120fe4eca
xhh <xhh@xhh.me>
2015-06-26 17:16:38 +0800

    Fix naming in specs

--- samples/client/petstore/ruby/spec/pet_spec.rb
+++ samples/client/petstore/ruby/spec/pet_spec.rb
@@ -9,9 +9,9 @@ describe "Pet" do
 
   describe "pet methods" do
     it "should construct a new pet object" do
-      tag1 = SwaggerClient::Tag.new({'id' => 1, 'name'=> 'tag1'})
-      tag2 = SwaggerClient::Tag.new({'id' => 2, 'name'=> 'tag2'})
-      category1 = SwaggerClient::Category.new({:id => 1, :name => 'category unknown'})
+      tag1 = Petstore::Tag.new({'id' => 1, 'name'=> 'tag1'})
+      tag2 = Petstore::Tag.new({'id' => 2, 'name'=> 'tag2'})
+      category1 = Petstore::Category.new({:id => 1, :name => 'category unknown'})
       # initalize using both string and symbol key
       pet_hash = {
         :id => 10002,
@@ -21,7 +21,7 @@ describe "Pet" do
         :category => category1,
         :tags => [tag1, tag2]
       }
-      pet = SwaggerClient::Pet.new(pet_hash)
+      pet = Petstore::Pet.new(pet_hash)
       # test new
       pet.name.should == "RUBY UNIT TESTING"
       pet.status.should == "pending"
@@ -31,7 +31,7 @@ describe "Pet" do
       pet.category.name.should == 'category unknown'
 
       # test build_from_hash
-      pet2 = SwaggerClient::Pet.new
+      pet2 = Petstore::Pet.new
       pet2.build_from_hash(pet.to_hash)
       pet.to_hash.should == pet2.to_hash
 
@@ -42,8 +42,8 @@ describe "Pet" do
     end
 
     it "should fetch a pet object" do
-      pet = SwaggerClient::PetApi.get_pet_by_id(10002)
-      pet.should be_a(SwaggerClient::Pet)
+      pet = Petstore::PetApi.get_pet_by_id(10002)
+      pet.should be_a(Petstore::Pet)
       pet.id.should == 10002
       pet.name.should == "RUBY UNIT TESTING"
       pet.tags[0].name.should == "tag test"
@@ -52,9 +52,9 @@ describe "Pet" do
 
     it "should not find a pet that does not exist" do
       begin
-        SwaggerClient::PetApi.get_pet_by_id(-1)
+        Petstore::PetApi.get_pet_by_id(-1)
         fail 'it should raise error'
-      rescue SwaggerClient::Swagger::ApiError => e
+      rescue Petstore::Swagger::ApiError => e
         e.code.should == 404
         e.message.should == 'Not Found'
         e.response_body.should == '{"code":1,"type":"error","message":"Pet not found"}'
@@ -64,21 +64,21 @@ describe "Pet" do
     end
 
     it "should find pets by status" do
-      pets = SwaggerClient::PetApi.find_pets_by_status(:status => 'available')
+      pets = Petstore::PetApi.find_pets_by_status(:status => 'available')
       pets.length.should >= 3
       pets.each do |pet|
-        pet.should be_a(SwaggerClient::Pet)
+        pet.should be_a(Petstore::Pet)
         pet.status.should == 'available'
       end
     end
 
     it "should not find a pet with invalid status" do
-      pets = SwaggerClient::PetApi.find_pets_by_status(:status => 'invalid-status')
+      pets = Petstore::PetApi.find_pets_by_status(:status => 'invalid-status')
       pets.length.should == 0
     end
 
     it "should find a pet by status" do
-      pets = SwaggerClient::PetApi.find_pets_by_status(:status => "available,sold")
+      pets = Petstore::PetApi.find_pets_by_status(:status => "available,sold")
       pets.each do |pet|
         if pet.status != 'available' && pet.status != 'sold'
           raise "pet status wasn't right"
@@ -87,21 +87,21 @@ describe "Pet" do
     end
 
     it "should update a pet" do
-      pet = SwaggerClient::Pet.new({'id' => 10002, 'status' => 'sold'})
-      SwaggerClient::PetApi.add_pet(:body => pet)
+      pet = Petstore::Pet.new({'id' => 10002, 'status' => 'sold'})
+      Petstore::PetApi.add_pet(:body => pet)
 
-      fetched = SwaggerClient::PetApi.get_pet_by_id(10002)
+      fetched = Petstore::PetApi.get_pet_by_id(10002)
       fetched.id.should == 10002
       fetched.status.should == 'sold'
     end
 
     it "should create a pet" do
-      pet = SwaggerClient::Pet.new('id' => 10002, 'name' => "RUBY UNIT TESTING")
-      result = SwaggerClient::PetApi.add_pet(:body => pet)
+      pet = Petstore::Pet.new('id' => 10002, 'name' => "RUBY UNIT TESTING")
+      result = Petstore::PetApi.add_pet(:body => pet)
       # nothing is returned
       result.should be_nil
 
-      pet = SwaggerClient::PetApi.get_pet_by_id(10002)
+      pet = Petstore::PetApi.get_pet_by_id(10002)
       pet.id.should == 10002
       pet.name.should == "RUBY UNIT TESTING"
     end
