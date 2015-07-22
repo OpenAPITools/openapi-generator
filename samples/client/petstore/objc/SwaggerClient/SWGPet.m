4c42555e5a0600d4b185e18749f6d454d90bdbfa
geekerzp <geekerzp@gmail.com>
2015-07-22 18:31:50 +0800

    Update model body template in objc client.
    * Add some comments.

--- samples/client/petstore/objc/SwaggerClient/SWGPet.m
+++ samples/client/petstore/objc/SwaggerClient/SWGPet.m
@@ -2,11 +2,20 @@
 
 @implementation SWGPet
 
+/**
+ * Maps json key to property name.
+ * This method is used by `JSONModel`.
+ */
 + (JSONKeyMapper *)keyMapper
 {
   return [[JSONKeyMapper alloc] initWithDictionary:@{ @"id": @"_id", @"category": @"category", @"name": @"name", @"photoUrls": @"photoUrls", @"tags": @"tags", @"status": @"status" }];
 }
 
+/**
+ * Indicates whether the property with the given name is optional.
+ * If `propertyName` is optional, then return `YES`, or return `NO`.
+ * This method is used by `JSONModel`.
+ */
 + (BOOL)propertyIsOptional:(NSString *)propertyName
 {
   NSArray *optionalProperties = @[@"_id", @"category", @"tags", @"status"];
@@ -19,6 +28,10 @@
   }
 }
 
+/**
+ * Return a string that represents the properties json of the model.
+ * This method will be called when logging model object using `NSLog`.
+ */
 - (NSString *)description {
     return [[self toDictionary] description];
 }
