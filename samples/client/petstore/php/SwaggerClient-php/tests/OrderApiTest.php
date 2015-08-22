f2df26f6e6504347a9c1270f39befb5ddce7c542
wing328 <wing328hk@gmail.com>
2015-08-22 23:09:12 +0800

    fix deserializing datetime, add test case

--- samples/client/petstore/php/SwaggerClient-php/tests/OrderApiTest.php
+++ samples/client/petstore/php/SwaggerClient-php/tests/OrderApiTest.php
@@ -31,6 +31,31 @@ class OrderApiTest extends \PHPUnit_Framework_TestCase
         $order = new Swagger\Client\Model\Order();
         $order->setStatus("invalid_value");
     }
+
+    // test deseralization of order
+    public function testDeserializationOfOrder()
+    {
+        $order_json = <<<ORDER
+{
+  "id": 10,
+  "petId": 20,
+  "quantity": 30,
+  "shipDate": "2015-08-22T07:13:36.613Z",
+  "status": "placed",
+  "complete": false
+}
+ORDER;
+        $serializer = new Swagger\Client\ObjectSerializer;
+        $order = $serializer->deserialize(json_decode($order_json), 'Swagger\Client\Model\Order');
+        
+        $this->assertInstanceOf('Swagger\Client\Model\Order', $order);
+        $this->assertSame(10, $order->getId());
+        $this->assertSame(20, $order->getPetId());
+        $this->assertSame(30, $order->getQuantity());
+        $this->assertTrue(new DateTime("2015-08-22T07:13:36.613Z") == $order->getShipDate());
+        $this->assertSame("placed", $order->getStatus());
+        $this->assertSame(false, $order->getComplete());
+    }
   
 }
 
