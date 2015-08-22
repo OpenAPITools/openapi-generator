dc423cc11de72e9147752d16386ed384dfecc152
geekerzp <geekerzp@gmail.com>
2015-08-22 17:46:13 +0800

    Fix issue that Mssing query parameters in python client.

--- samples/client/petstore/python/swagger_client/apis/pet_api.py
+++ samples/client/petstore/python/swagger_client/apis/pet_api.py
@@ -408,7 +408,7 @@ class PetApi(object):
             select_header_content_type([])
 
         # Authentication setting
-        auth_settings = ['api_key', 'petstore_auth']
+        auth_settings = ['petstore_auth', 'api_key']
 
         response = self.api_client.call_api(resource_path, method,
                                             path_params,
