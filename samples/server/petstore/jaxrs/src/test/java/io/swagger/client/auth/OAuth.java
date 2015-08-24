6c1f7243bf298cfa5ecf22258e1b35523795d9b7
Tony Tam <fehguy@gmail.com>
2015-08-23 23:29:53 -0700

    rebuilt

---
+++ samples/server/petstore/jaxrs/src/test/java/io/swagger/client/auth/OAuth.java
@@ -0,0 +1,14 @@
+package io.swagger.client.auth;
+
+import io.swagger.client.Pair;
+
+import java.util.Map;
+import java.util.List;
+
+@javax.annotation.Generated(value = "class io.swagger.codegen.languages.JavaClientCodegen", date = "2015-08-23T11:08:48.917-07:00")
+public class OAuth implements Authentication {
+  @Override
+  public void applyToParams(List<Pair> queryParams, Map<String, String> headerParams) {
+    // TODO: support oauth
+  }
+}
