715c0af41f76da3ee63c4b81e7e0b0a439a40caf
Tony Tam <fehguy@gmail.com>
2015-08-24 00:03:20 -0700

    merged & rebuilt

--- modules/swagger-codegen/src/main/java/io/swagger/codegen/languages/JavaClientCodegen.java
+++ modules/swagger-codegen/src/main/java/io/swagger/codegen/languages/JavaClientCodegen.java
@@ -141,9 +141,9 @@ public class JavaClientCodegen extends DefaultCodegen implements CodegenConfig {
         if (additionalProperties.containsKey("serializableModel")) {
             this.setSerializableModel(Boolean.valueOf((String)additionalProperties.get("serializableModel").toString()));
         }
+
         // need to put back serializableModel (boolean) into additionalProperties as value in additionalProperties is string
-          additionalProperties.put("serializableModel", serializableModel);
-        }
+        additionalProperties.put("serializableModel", serializableModel);
 
         this.sanitizeConfig();
 
