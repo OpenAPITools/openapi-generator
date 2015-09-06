58c5ecf13ce93ea706ad0eb5349e0f7aeb08a20b
Yissachar Radcliffe <ydradcliffe@gmail.com>
2015-09-06 19:58:28 -0400

    Generate auth files to correct location

--- samples/client/petstore/dart/auth/authentication.dart
+++
@@ -1,7 +0,0 @@
-part of api;
-
-abstract class Authentication {
-	
-	/// Apply authentication settings to header and query params.
-	void applyToParams(Map<String, String> queryParams, Map<String, String> headerParams);
-}
