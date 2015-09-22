a6df340cdc80f71cc4bdb88aa7174013a5d41c5c
Evan Fossier <efossier@twilio.com>
2015-09-21 17:34:23 -0700

    Expose readOnly attribute on model properties to templates, can now use isReadOnly property in model templates

--- modules/swagger-codegen/src/main/java/io/swagger/codegen/CodegenProperty.java
+++ modules/swagger-codegen/src/main/java/io/swagger/codegen/CodegenProperty.java
@@ -32,6 +32,7 @@ public class CodegenProperty {
     public Boolean hasMore = null, required = null, secondaryParam = null;
     public Boolean isPrimitiveType, isContainer, isNotContainer;
     public boolean isEnum;
+    public Boolean isReadOnly = false;
     public List<String> _enum;
     public Map<String, Object> allowableValues;
     public CodegenProperty items;
