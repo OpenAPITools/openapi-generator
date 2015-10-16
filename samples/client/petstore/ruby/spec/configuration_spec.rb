ccbd444e77ff194278a3ca9074248f0141e50665
xhh <xhh@xhh.me>
2015-10-16 12:02:30 +0800

    Ruby client: remove trailing slashes from base_url
    Closes #1399

---
+++ samples/client/petstore/ruby/spec/configuration_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+describe Petstore::Configuration do
+  let(:config) { Petstore::Configuration.instance }
+
+  before(:each) do
+    Petstore.configure do |c|
+      c.host = 'petstore.swagger.io'
+      c.base_path = 'v2'
+    end
+  end
+
+  describe '#base_url' do
+    it 'should have the default value' do
+      config.base_url.should == 'http://petstore.swagger.io/v2'
+    end
+
+    it 'should remove trailing slashes' do
+      [nil, '', '/', '//'].each do |base_path|
+        config.base_path = base_path
+        config.base_url.should == 'http://petstore.swagger.io'
+      end
+    end
+  end
+end
