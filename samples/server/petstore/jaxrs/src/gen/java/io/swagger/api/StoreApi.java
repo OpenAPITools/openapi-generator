7ba2b55269daea664d4ad0a159397b114c10f97f
Tony Tam <fehguy@gmail.com>
2015-10-20 10:29:25 -0700

    rebuilt

--- samples/server/petstore/jaxrs/src/gen/java/io/swagger/api/StoreApi.java
+++ samples/server/petstore/jaxrs/src/gen/java/io/swagger/api/StoreApi.java
@@ -26,7 +26,7 @@ import javax.ws.rs.*;
 
 
 @io.swagger.annotations.Api(value = "/store", description = "the store API")
-@javax.annotation.Generated(value = "class io.swagger.codegen.languages.JaxRSServerCodegen", date = "2015-08-23T23:29:16.812-07:00")
+@javax.annotation.Generated(value = "class io.swagger.codegen.languages.JaxRSServerCodegen", date = "2015-10-20T08:49:54.299-07:00")
 public class StoreApi  {
 
    private final StoreApiService delegate = StoreApiServiceFactory.getStoreApi();
@@ -41,7 +41,7 @@ public class StoreApi  {
 
     public Response getInventory()
     throws NotFoundException {
-    return delegate.getInventory();
+        return delegate.getInventory();
     }
     @POST
     @Path("/order")
@@ -55,7 +55,7 @@ public class StoreApi  {
 
     public Response placeOrder(@ApiParam(value = "order placed for purchasing the pet"  ) Order body)
     throws NotFoundException {
-    return delegate.placeOrder(body);
+        return delegate.placeOrder(body);
     }
     @GET
     @Path("/order/{orderId}")
@@ -63,15 +63,15 @@ public class StoreApi  {
     @Produces({ "application/json", "application/xml" })
     @io.swagger.annotations.ApiOperation(value = "Find purchase order by ID", notes = "For valid response try integer IDs with value <= 5 or > 10. Other values will generated exceptions", response = Order.class)
     @io.swagger.annotations.ApiResponses(value = { 
-        @io.swagger.annotations.ApiResponse(code = 404, message = "Order not found", response = Order.class),
-        
         @io.swagger.annotations.ApiResponse(code = 200, message = "successful operation", response = Order.class),
         
-        @io.swagger.annotations.ApiResponse(code = 400, message = "Invalid ID supplied", response = Order.class) })
+        @io.swagger.annotations.ApiResponse(code = 400, message = "Invalid ID supplied", response = Order.class),
+        
+        @io.swagger.annotations.ApiResponse(code = 404, message = "Order not found", response = Order.class) })
 
     public Response getOrderById(@ApiParam(value = "ID of pet that needs to be fetched",required=true ) @PathParam("orderId") String orderId)
     throws NotFoundException {
-    return delegate.getOrderById(orderId);
+        return delegate.getOrderById(orderId);
     }
     @DELETE
     @Path("/order/{orderId}")
@@ -79,13 +79,13 @@ public class StoreApi  {
     @Produces({ "application/json", "application/xml" })
     @io.swagger.annotations.ApiOperation(value = "Delete purchase order by ID", notes = "For valid response try integer IDs with value < 1000. Anything above 1000 or nonintegers will generate API errors", response = Void.class)
     @io.swagger.annotations.ApiResponses(value = { 
-        @io.swagger.annotations.ApiResponse(code = 404, message = "Order not found", response = Void.class),
+        @io.swagger.annotations.ApiResponse(code = 400, message = "Invalid ID supplied", response = Void.class),
         
-        @io.swagger.annotations.ApiResponse(code = 400, message = "Invalid ID supplied", response = Void.class) })
+        @io.swagger.annotations.ApiResponse(code = 404, message = "Order not found", response = Void.class) })
 
     public Response deleteOrder(@ApiParam(value = "ID of the order that needs to be deleted",required=true ) @PathParam("orderId") String orderId)
     throws NotFoundException {
-    return delegate.deleteOrder(orderId);
+        return delegate.deleteOrder(orderId);
     }
 }
 
