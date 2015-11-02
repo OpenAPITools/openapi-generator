dadc85b6fd43153bec534093e24f2974f573cad5
Tony Tam <fehguy@gmail.com>
2015-11-02 14:46:31 -0800

    added resource, method stub for generator options

--- modules/swagger-generator/src/main/java/io/swagger/generator/online/Generator.java
+++ modules/swagger-generator/src/main/java/io/swagger/generator/online/Generator.java
@@ -20,9 +20,14 @@ import org.slf4j.LoggerFactory;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class Generator {
     static Logger LOGGER = LoggerFactory.getLogger(Generator.class);
+
+    public static Map<String, String> getOptions(String language) {
+        return null;
+    }
 
     public enum Type {
         CLIENT("client"),
