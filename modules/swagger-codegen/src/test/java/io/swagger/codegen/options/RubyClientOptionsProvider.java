e9ba5ed57c46abe7f4980a98f0d39e27b05dce2c
xhh <xhh@xhh.me>
2015-11-04 21:20:16 +0800

    Fix tests

--- modules/swagger-codegen/src/test/java/io/swagger/codegen/options/RubyClientOptionsProvider.java
+++ modules/swagger-codegen/src/test/java/io/swagger/codegen/options/RubyClientOptionsProvider.java
@@ -1,5 +1,6 @@
 package io.swagger.codegen.options;
 
+import io.swagger.codegen.CodegenConstants;
 import io.swagger.codegen.languages.RubyClientCodegen;
 
 import com.google.common.collect.ImmutableMap;
@@ -10,6 +11,8 @@ public class RubyClientOptionsProvider implements OptionsProvider {
     public static final String GEM_NAME_VALUE = "swagger_client_ruby";
     public static final String MODULE_NAME_VALUE = "SwaggerClientRuby";
     public static final String GEM_VERSION_VALUE = "1.0.0-SNAPSHOT";
+    public static final String SORT_PARAMS_VALUE = "false";
+    public static final String ENSURE_UNIQUE_PARAMS_VALUE = "true";
 
     @Override
     public String getLanguage() {
@@ -22,6 +25,8 @@ public class RubyClientOptionsProvider implements OptionsProvider {
         return builder.put(RubyClientCodegen.GEM_NAME, GEM_NAME_VALUE)
                 .put(RubyClientCodegen.MODULE_NAME, MODULE_NAME_VALUE)
                 .put(RubyClientCodegen.GEM_VERSION, GEM_VERSION_VALUE)
+                .put(CodegenConstants.SORT_PARAMS_BY_REQUIRED_FLAG, SORT_PARAMS_VALUE)
+                .put(CodegenConstants.ENSURE_UNIQUE_PARAMS, ENSURE_UNIQUE_PARAMS_VALUE)
                 .build();
     }
 
