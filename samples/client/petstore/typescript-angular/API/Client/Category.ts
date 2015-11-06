9c15c9639875b88c7c591e75a21f6d1a8b6a8b9c
wing328 <wing328hk@gmail.com>
2015-11-06 21:46:47 +0800

    add reserved word handling for typescript

--- samples/client/petstore/typescript-angular/API/Client/Category.ts
+++ samples/client/petstore/typescript-angular/API/Client/Category.ts
@@ -3,22 +3,11 @@
 namespace API.Client {
     'use strict';
 
-
-
-
     export interface Category {
-
-
 
         id: number;
 
-
-
         name: string;
-
     }
-
-
-
 
 }
--- samples/client/petstore/typescript-angular/API/Client/User.ts
+++ samples/client/petstore/typescript-angular/API/Client/User.ts
@@ -3,50 +3,26 @@
 namespace API.Client {
     'use strict';
 
-
-
-
     export interface User {
-
-
 
         id: number;
 
-
-
         username: string;
-
-
 
         firstName: string;
 
-
-
         lastName: string;
-
-
 
         email: string;
 
-
-
         password: string;
 
-
-
         phone: string;
-
-
 
         /**
          * User Status
          */
-
         userStatus: number;
-
     }
-
-
-
 
 }
