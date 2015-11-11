f8db16c5cd1bf7e37df87278f19d4bd4f85c00b9
Cristian Oneț <onet.cristian@audionowdigital.com>
2015-11-11 14:32:02 +0200

    Fix typo in swift error variable name.

--- samples/client/petstore/swift/PetstoreClient/Classes/Swaggers/APIs/StoreAPI.swift
+++ samples/client/petstore/swift/PetstoreClient/Classes/Swaggers/APIs/StoreAPI.swift
@@ -52,14 +52,14 @@ extension PetstoreClientAPI {
   "petId" : 123456789,
   "quantity" : 123,
   "id" : 123456789,
-  "shipDate" : "2015-11-11T10:03:22.835+0000",
+  "shipDate" : "2015-11-11T12:31:12.079+0000",
   "complete" : true,
   "status" : "aeiou"
 }}, {contentType=application/xml, example=<Order>
   <id>123456</id>
   <petId>123456</petId>
   <quantity>0</quantity>
-  <shipDate>2015-11-11T12:03:22.838Z</shipDate>
+  <shipDate>2015-11-11T14:31:12.082Z</shipDate>
   <status>string</status>
   <complete>true</complete>
 </Order>}]
@@ -67,14 +67,14 @@ extension PetstoreClientAPI {
   "petId" : 123456789,
   "quantity" : 123,
   "id" : 123456789,
-  "shipDate" : "2015-11-11T10:03:22.835+0000",
+  "shipDate" : "2015-11-11T12:31:12.079+0000",
   "complete" : true,
   "status" : "aeiou"
 }}, {contentType=application/xml, example=<Order>
   <id>123456</id>
   <petId>123456</petId>
   <quantity>0</quantity>
-  <shipDate>2015-11-11T12:03:22.838Z</shipDate>
+  <shipDate>2015-11-11T14:31:12.082Z</shipDate>
   <status>string</status>
   <complete>true</complete>
 </Order>}]
@@ -104,14 +104,14 @@ extension PetstoreClientAPI {
   "petId" : 123456789,
   "quantity" : 123,
   "id" : 123456789,
-  "shipDate" : "2015-11-11T10:03:22.839+0000",
+  "shipDate" : "2015-11-11T12:31:12.083+0000",
   "complete" : true,
   "status" : "aeiou"
 }}, {contentType=application/xml, example=<Order>
   <id>123456</id>
   <petId>123456</petId>
   <quantity>0</quantity>
-  <shipDate>2015-11-11T12:03:22.839Z</shipDate>
+  <shipDate>2015-11-11T14:31:12.083Z</shipDate>
   <status>string</status>
   <complete>true</complete>
 </Order>}]
@@ -119,14 +119,14 @@ extension PetstoreClientAPI {
   "petId" : 123456789,
   "quantity" : 123,
   "id" : 123456789,
-  "shipDate" : "2015-11-11T10:03:22.839+0000",
+  "shipDate" : "2015-11-11T12:31:12.083+0000",
   "complete" : true,
   "status" : "aeiou"
 }}, {contentType=application/xml, example=<Order>
   <id>123456</id>
   <petId>123456</petId>
   <quantity>0</quantity>
-  <shipDate>2015-11-11T12:03:22.839Z</shipDate>
+  <shipDate>2015-11-11T14:31:12.083Z</shipDate>
   <status>string</status>
   <complete>true</complete>
 </Order>}]
