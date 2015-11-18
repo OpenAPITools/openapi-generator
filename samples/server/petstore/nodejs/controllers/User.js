c7e5c305a937a7dcd86b79b2b62703f4d66bf015
Tony Tam <fehguy@gmail.com>
2015-11-17 18:46:33 -0800

    fixes #1581, rebuilt server

--- samples/server/petstore/nodejs/controllers/User.js
+++ samples/server/petstore/nodejs/controllers/User.js
@@ -7,114 +7,33 @@ var User = require('./UserService');
 
 
 module.exports.createUser = function createUser (req, res, next) {
-  var body = req.swagger.params['body'].value;
-  
-
-  var result = User.createUser(body);
-
-  if(typeof result !== 'undefined') {
-    res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify(result || {}, null, 2));
-  }
-  else
-    res.end();
+  User.createUser(req.swagger.params, res, next);
 };
 
 module.exports.createUsersWithArrayInput = function createUsersWithArrayInput (req, res, next) {
-  var body = req.swagger.params['body'].value;
-  
-
-  var result = User.createUsersWithArrayInput(body);
-
-  if(typeof result !== 'undefined') {
-    res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify(result || {}, null, 2));
-  }
-  else
-    res.end();
+  User.createUsersWithArrayInput(req.swagger.params, res, next);
 };
 
 module.exports.createUsersWithListInput = function createUsersWithListInput (req, res, next) {
-  var body = req.swagger.params['body'].value;
-  
-
-  var result = User.createUsersWithListInput(body);
-
-  if(typeof result !== 'undefined') {
-    res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify(result || {}, null, 2));
-  }
-  else
-    res.end();
+  User.createUsersWithListInput(req.swagger.params, res, next);
 };
 
 module.exports.loginUser = function loginUser (req, res, next) {
-  var username = req.swagger.params['username'].value;
-  var password = req.swagger.params['password'].value;
-  
-
-  var result = User.loginUser(username, password);
-
-  if(typeof result !== 'undefined') {
-    res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify(result || {}, null, 2));
-  }
-  else
-    res.end();
+  User.loginUser(req.swagger.params, res, next);
 };
 
 module.exports.logoutUser = function logoutUser (req, res, next) {
-  
-
-  var result = User.logoutUser();
-
-  if(typeof result !== 'undefined') {
-    res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify(result || {}, null, 2));
-  }
-  else
-    res.end();
+  User.logoutUser(req.swagger.params, res, next);
 };
 
 module.exports.getUserByName = function getUserByName (req, res, next) {
-  var username = req.swagger.params['username'].value;
-  
-
-  var result = User.getUserByName(username);
-
-  if(typeof result !== 'undefined') {
-    res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify(result || {}, null, 2));
-  }
-  else
-    res.end();
+  User.getUserByName(req.swagger.params, res, next);
 };
 
 module.exports.updateUser = function updateUser (req, res, next) {
-  var username = req.swagger.params['username'].value;
-  var body = req.swagger.params['body'].value;
-  
-
-  var result = User.updateUser(username, body);
-
-  if(typeof result !== 'undefined') {
-    res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify(result || {}, null, 2));
-  }
-  else
-    res.end();
+  User.updateUser(req.swagger.params, res, next);
 };
 
 module.exports.deleteUser = function deleteUser (req, res, next) {
-  var username = req.swagger.params['username'].value;
-  
-
-  var result = User.deleteUser(username);
-
-  if(typeof result !== 'undefined') {
-    res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify(result || {}, null, 2));
-  }
-  else
-    res.end();
+  User.deleteUser(req.swagger.params, res, next);
 };
