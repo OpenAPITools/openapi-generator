d38933f927e8914f60d2f791a4a58a2ec1fe3314
jfastnacht <github@fastnacht.consulting>
2015-11-23 17:49:19 +0100

    Added model.mustache for basic class definitions. Added sample class files.

---
+++ samples/server/petstore/slim/SwaggerServer/lib/models/Category.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * Category
+ */
+namespace SwaggerServer\lib\models;
+
+/*
+ * Category
+ */
+class Category {
+    /* @var int $id  */
+    private $id;
+    /* @var string $name  */
+    private $name;
+    
+}
