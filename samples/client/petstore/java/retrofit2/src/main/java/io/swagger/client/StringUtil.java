359542a954165273817bab12da720963d70fbfa9
wing328 <wing328hk@gmail.com>
2015-11-28 16:01:59 +0800

    update retrofit2 gradle to use double quote

--- samples/client/petstore/java/retrofit2/src/main/java/io/swagger/client/StringUtil.java
+++ samples/client/petstore/java/retrofit2/src/main/java/io/swagger/client/StringUtil.java
@@ -1,6 +1,6 @@
 package io.swagger.client;
 
-@javax.annotation.Generated(value = "class io.swagger.codegen.languages.JavaClientCodegen", date = "2015-11-29T00:18:18.137+08:00")
+@javax.annotation.Generated(value = "class io.swagger.codegen.languages.JavaClientCodegen", date = "2015-11-28T16:01:49.902+08:00")
 public class StringUtil {
   /**
    * Check if the given array contains the given value (with case-insensitive comparison).
