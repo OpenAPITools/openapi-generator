be9227792949b5f4688f1027bb4743bde237c5c9
wing328 <wing328hk@gmail.com>
2015-12-03 16:34:51 +0800

    add WithHttpInfo method

--- samples/client/petstore/csharp/SwaggerClientTest/Lib/SwaggerClient/src/main/csharp/IO/Swagger/Client/ApiResponse.cs
+++ samples/client/petstore/csharp/SwaggerClientTest/Lib/SwaggerClient/src/main/csharp/IO/Swagger/Client/ApiResponse.cs
@@ -12,19 +12,19 @@ namespace IO.Swagger.Client
         /// Gets or sets the status code (HTTP status code)
         /// </summary>
         /// <value>The status code.</value>
-        public int StatusCode { get; set; }
+        public int StatusCode { get; private set; }
   
         /// <summary>
         /// Gets or sets the HTTP headers
         /// </summary>
         /// <value>HTTP headers</value>
-        public IDictionary<string, string> Headers { get; set; }
+        public IDictionary<string, string> Headers { get; private set; }
   
         /// <summary>
         /// Gets or sets the data (parsed HTTP body)
         /// </summary>
         /// <value>The data.</value>
-        public T Data { get; set; }
+        public T Data { get; private set; }
   
         /// <summary>
         /// Initializes a new instance of the <see cref="ApiResponse"/> class.
