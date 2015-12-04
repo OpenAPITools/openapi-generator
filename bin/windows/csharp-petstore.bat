fe96d8769f5f2a868a913355cd91e58a21cf7b67
wing328 <wing328hk@gmail.com>
2015-12-04 18:16:46 +0800

    minor fix to csharp and objc batch file

--- bin/windows/csharp-petstore.bat
+++ bin/windows/csharp-petstore.bat
@@ -5,6 +5,6 @@ If Not Exist %executable% (
 )
 
 set JAVA_OPTS=%JAVA_OPTS% -XX:MaxPermSize=256M -Xmx1024M -DloggerPath=conf/log4j.properties
-set ags=generate -t modules\swagger-codegen\src\main\resources\csharp -i modules\swagger-codegen\src\test\resources\2_0\petstore.json -l csharp -o samples\client\petstore\csharp
+set ags=generate -t modules\swagger-codegen\src\main\resources\csharp -i modules\swagger-codegen\src\test\resources\2_0\petstore.json -l csharp -o samples\client\petstore\csharp\SwaggerClientTest\Lib\SwaggerClient
 
 java %JAVA_OPTS% -jar %executable% %ags%
--- bin/windows/objc-petstore.bat
+++ bin/windows/objc-petstore.bat
@@ -7,4 +7,4 @@ If Not Exist %executable% (
 set JAVA_OPTS=%JAVA_OPTS% -XX:MaxPermSize=256M -Xmx1024M -DloggerPath=conf/log4j.properties
 set ags=generate -t modules\swagger-codegen\src\main\resources\objc -i modules\swagger-codegen\src\test\resources\2_0\petstore.json -l objc -o samples\client\petstore\objc
 
-java %JAVA_OPTS% -jar %executable% %ags%
+java %JAVA_OPTS% -DappName=PetstoreClient -jar %executable% %ags%
