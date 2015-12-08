f9f0992cc8bdf0817f7e1f5b3a7659111e2da034
wing328 <wing328hk@gmail.com>
2015-12-08 15:09:30 +0800

    add default member's value for C# models

--- samples/client/petstore/csharp/SwaggerClientTest/Lib/SwaggerClient/src/main/csharp/IO/Swagger/Model/User.cs
+++ samples/client/petstore/csharp/SwaggerClientTest/Lib/SwaggerClient/src/main/csharp/IO/Swagger/Model/User.cs
@@ -16,6 +16,14 @@ namespace IO.Swagger.Model
     [DataContract]
     public class User : IEquatable<User>
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="User" /> class.
+        /// </summary>
+        public User()
+        {
+            
+        }
+
         
         /// <summary>
         /// Gets or Sets Id
