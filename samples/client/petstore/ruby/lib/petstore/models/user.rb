b896c9169a48f4df946ef8ae425a62ac354b3eb6
xhh <xhh@xhh.me>
2015-12-09 11:16:14 +0800

    Add comments to model classes in Ruby client

--- samples/client/petstore/ruby/lib/petstore/models/user.rb
+++ samples/client/petstore/ruby/lib/petstore/models/user.rb
@@ -1,30 +1,23 @@
 module Petstore
-  # 
   class User < BaseObject
     attr_accessor :id, :username, :first_name, :last_name, :email, :password, :phone, :user_status
-    # attribute mapping from ruby-style variable name to JSON key
+
+    # Attribute mapping from ruby-style variable name to JSON key.
     def self.attribute_map
       {
         
-        # 
         :'id' => :'id',
         
-        # 
         :'username' => :'username',
         
-        # 
         :'first_name' => :'firstName',
         
-        # 
         :'last_name' => :'lastName',
         
-        # 
         :'email' => :'email',
         
-        # 
         :'password' => :'password',
         
-        # 
         :'phone' => :'phone',
         
         # User Status
@@ -33,7 +26,7 @@ module Petstore
       }
     end
 
-    # attribute type
+    # Attribute type mapping.
     def self.swagger_types
       {
         :'id' => :'Integer',
@@ -89,6 +82,7 @@ module Petstore
       
     end
 
+    # Check equality by comparing each attribute.
     def ==(o)
       return true if self.equal?(o)
       self.class == o.class &&
@@ -102,10 +96,12 @@ module Petstore
           user_status == o.user_status
     end
 
+    # @see the `==` method
     def eql?(o)
       self == o
     end
 
+    # Calculate hash code according to all attributes.
     def hash
       [id, username, first_name, last_name, email, password, phone, user_status].hash
     end
