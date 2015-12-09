806b0a6bc64760313a8c13ef9f274b3e1b86d6d3
wing328 <wing328hk@gmail.com>
2015-12-09 16:21:00 +0800

    add default value to objc model properties

--- samples/client/petstore/objc/SwaggerClient/SWGUser.m
+++ samples/client/petstore/objc/SwaggerClient/SWGUser.m
@@ -2,6 +2,16 @@
 
 @implementation SWGUser
 
+- (instancetype)init {
+  self = [super init];
+  if (self) 
+  {
+    // initalise property's default value, if any
+    
+  }
+  return self;
+}
+
 /**
  * Maps json key to property name.
  * This method is used by `JSONModel`.
