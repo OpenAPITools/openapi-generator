9fc0bd07113229c010c5a60b4c34f69f9ed5b8f0
Jeff Kwan <jkwan@vistaprint.com>
2015-12-10 20:42:30 -0500

    Add some tests for time zones

--- samples/client/petstore/csharp/SwaggerClientTest/TestApiClient.cs
+++ samples/client/petstore/csharp/SwaggerClientTest/TestApiClient.cs
@@ -21,8 +21,16 @@ namespace SwaggerClient.TestApiClient
 			Assert.AreEqual("1,37", api.ParameterToString (numList));
 
 			// test datetime
-			DateTime date = DateTime.Parse("2008-04-10T13:30:00.0000000Z", null, System.Globalization.DateTimeStyles.RoundtripKind);
-			Assert.AreEqual("2008-04-10T13:30:00.0000000Z", api.ParameterToString (date));
+			DateTime dateUtc = DateTime.Parse("2008-04-10T13:30:00.0000000z", null, System.Globalization.DateTimeStyles.RoundtripKind);
+			Assert.AreEqual("2008-04-10T13:30:00.0000000Z", api.ParameterToString (dateUtc));
+
+			// test datetime with no timezone
+			DateTime dateWithNoTz = DateTime.Parse("2008-04-10T13:30:00.000", null, System.Globalization.DateTimeStyles.RoundtripKind);
+			Assert.AreEqual("2008-04-10T13:30:00.0000000", api.ParameterToString (dateWithNoTz));
+
+			// test datetime with a time zone
+			DateTime dateWithTz = DateTime.Parse("2008-04-10T13:30:00.0000000-04:00", null, System.Globalization.DateTimeStyles.RoundtripKind);
+			Assert.AreEqual("2008-04-10T13:30:00.0000000-04:00", api.ParameterToString (dateWithTz));
 
 		}
 	}
