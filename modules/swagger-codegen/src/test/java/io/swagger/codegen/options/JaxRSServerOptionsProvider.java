6423e4834e2bd7a4cad7eec976a169b919f2baf0
evigeant <evigeant@gmail.com>
2015-12-16 22:22:01 -0500

    Fixed CodeGen tests following addition of implFolder additional property in jaxrs

--- modules/swagger-codegen/src/test/java/io/swagger/codegen/options/JaxRSServerOptionsProvider.java
+++ modules/swagger-codegen/src/test/java/io/swagger/codegen/options/JaxRSServerOptionsProvider.java
@@ -1,7 +1,23 @@
 package io.swagger.codegen.options;
 
+import java.util.Map;
+
+import com.google.common.collect.ImmutableMap;
+
+import io.swagger.codegen.CodegenConstants;
+
 public class JaxRSServerOptionsProvider extends JavaOptionsProvider {
+    public static final String IMPL_FOLDER_VALUE = "src/main/java/impl";
+	
     @Override
+	public Map<String, String> createOptions() {
+        ImmutableMap.Builder<String, String> builder = new ImmutableMap.Builder<String, String>();
+        return builder.putAll(super.createOptions())
+                .put(CodegenConstants.IMPL_FOLDER, IMPL_FOLDER_VALUE)
+                .build();
+	}
+
+	@Override
     public boolean isServer() {
         return true;
     }
