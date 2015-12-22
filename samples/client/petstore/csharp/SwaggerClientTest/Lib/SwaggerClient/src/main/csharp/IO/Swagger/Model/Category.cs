fe073021ba61d53b92f65da1324224fcc9728658
Justus Thorvaldsson <justus.thorvaldsson@svenskaspel.se>
2015-12-22 13:49:04 +0100

    Fixed space between inline enumclasses and removed extra tostring

--- samples/client/petstore/csharp/SwaggerClientTest/Lib/SwaggerClient/src/main/csharp/IO/Swagger/Model/Category.cs
+++ samples/client/petstore/csharp/SwaggerClientTest/Lib/SwaggerClient/src/main/csharp/IO/Swagger/Model/Category.cs
@@ -54,21 +54,6 @@ namespace IO.Swagger.Model
         }
   
         /// <summary>
-        /// Returns the string presentation of the object
-        /// </summary>
-        /// <returns>String presentation of the object</returns>
-        public override string ToString()
-        {
-            var sb = new StringBuilder();
-            sb.Append("class Category {\n");
-            sb.Append("  Id: ").Append(Id).Append("\n");
-            sb.Append("  Name: ").Append(Name).Append("\n");
-            
-            sb.Append("}\n");
-            return sb.ToString();
-        }
-  
-        /// <summary>
         /// Returns the JSON string presentation of the object
         /// </summary>
         /// <returns>JSON string presentation of the object</returns>
