3d4b5ff1c74c8e5f8fb7515a5002fcc551c14ec1
xhh <xhh@xhh.me>
2015-12-29 12:09:54 +0800

    Java: Move method StringUtil.toIndentedString to model
    Closes #1701

--- samples/client/petstore/java/default/src/main/java/io/swagger/client/model/Category.java
+++ samples/client/petstore/java/default/src/main/java/io/swagger/client/model/Category.java
@@ -11,7 +11,7 @@ import com.fasterxml.jackson.annotation.*;
 
 
 @ApiModel(description = "")
-@javax.annotation.Generated(value = "class io.swagger.codegen.languages.JavaClientCodegen", date = "2015-12-21T14:12:11.520+08:00")
+@javax.annotation.Generated(value = "class io.swagger.codegen.languages.JavaClientCodegen", date = "2015-12-29T11:32:50.163+08:00")
 public class Category   {
   
   private Long id = null;
@@ -62,13 +62,25 @@ public class Category   {
   }
 
   @Override
-  public String toString()  {
+  public String toString() {
     StringBuilder sb = new StringBuilder();
     sb.append("class Category {\n");
     
-    sb.append("    id: ").append(StringUtil.toIndentedString(id)).append("\n");
-    sb.append("    name: ").append(StringUtil.toIndentedString(name)).append("\n");
+    sb.append("    id: ").append(toIndentedString(id)).append("\n");
+    sb.append("    name: ").append(toIndentedString(name)).append("\n");
     sb.append("}");
     return sb.toString();
   }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
 }
+
