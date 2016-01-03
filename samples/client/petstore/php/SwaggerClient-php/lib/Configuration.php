1292fe4dfdad168418d4a4b426549bce21248184
Arne Jørgensen <arne@arnested.dk>
2016-01-03 22:59:15 +0100

    Regenerated PHP petstore sample.

--- samples/client/petstore/php/SwaggerClient-php/lib/Configuration.php
+++ samples/client/petstore/php/SwaggerClient-php/lib/Configuration.php
@@ -516,7 +516,7 @@ class Configuration
         $report  = "PHP SDK (Swagger\Client) Debug Report:\n";
         $report .= "    OS: ".php_uname()."\n";
         $report .= "    PHP Version: ".phpversion()."\n";
-        $report .= "    Swagger Spec Version: 1.0.0\n";
+        $report .= "    OpenAPI Spec Version: 1.0.0\n";
         $report .= "    SDK Package Version: 1.0.0\n";
         $report .= "    Temp Folder Path: ".self::getDefaultConfiguration()->getTempFolderPath()."\n";
 
