0bdff1c50b9836ab9d9695eff4f7ed93be7da326
wing328 <wing328hk@gmail.com>
2016-01-07 11:33:16 +0800

    update reserved word to include local variable names for android, scala

--- modules/swagger-codegen/src/main/java/io/swagger/codegen/languages/AsyncScalaClientCodegen.java
+++ modules/swagger-codegen/src/main/java/io/swagger/codegen/languages/AsyncScalaClientCodegen.java
@@ -47,11 +47,16 @@ public class AsyncScalaClientCodegen extends DefaultCodegen implements CodegenCo
 
         reservedWords = new HashSet<String>(
                 Arrays.asList(
-                        "abstract", "case", "catch", "class", "def", "do", "else", "extends",
-                        "false", "final", "finally", "for", "forSome", "if", "implicit",
-                        "import", "lazy", "match", "new", "null", "object", "override", "package",
-                        "private", "protected", "return", "sealed", "super", "this", "throw",
-                        "trait", "try", "true", "type", "val", "var", "while", "with", "yield")
+                    // local variable names used in API methods (endpoints)
+                    "config", "path", "contentTypes", "contentType", "queryParams", "headerParams",
+                    "formParams", "postBody", "resFuture", "client", "reader",
+
+                    // scala reserved words
+                    "abstract", "case", "catch", "class", "def", "do", "else", "extends",
+                    "false", "final", "finally", "for", "forSome", "if", "implicit",
+                    "import", "lazy", "match", "new", "null", "object", "override", "package",
+                    "private", "protected", "return", "sealed", "super", "this", "throw",
+                    "trait", "try", "true", "type", "val", "var", "while", "with", "yield")
         );
 
         additionalProperties.put(CodegenConstants.INVOKER_PACKAGE, invokerPackage);
