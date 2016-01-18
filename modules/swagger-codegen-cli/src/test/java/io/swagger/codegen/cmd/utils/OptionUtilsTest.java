370a8d136fd0c86eb3b47f0303a793b74b52d162
Maelig Nantel <maelig.nantel@gmail.com>
2016-01-18 11:35:57 +0100

    fixes

--- modules/swagger-codegen-cli/src/test/java/io/swagger/codegen/cmd/utils/OptionUtilsTest.java
+++ modules/swagger-codegen-cli/src/test/java/io/swagger/codegen/cmd/utils/OptionUtilsTest.java
@@ -10,6 +10,7 @@ import java.util.List;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertNotNull;
 
+@SuppressWarnings("static-method")
 public class OptionUtilsTest {
 
     @Test
@@ -30,7 +31,7 @@ public class OptionUtilsTest {
         doTupleListTest(null, new ArrayList<Pair<String, String>>());
     }
 
-    private void doTupleListTest(String input, List<Pair<String, String>> expectedResults) {
+    private static void doTupleListTest(String input, List<Pair<String, String>> expectedResults) {
         final List<Pair<String, String>> result = OptionUtils.parseCommaSeparatedTuples(input);
         assertNotNull(result);
         assertEquals(result.size(), expectedResults.size());
