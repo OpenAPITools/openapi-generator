fa77b711d68bf00851a2783389168046590c880a
xhh <xhh@xhh.me>
2016-01-19 10:46:20 +0800

    Make JavaScript client work in both Node.js and browser
    * Replace jQuery with SuperAgent which works in both Node.js and browser
    * Use UMD pattern (returnExports.js) to make the module exporting compatible
      with all major systems: AMD, Node.js (CommonJS) and browser
    * Implement support of header and form parameters. Closes #1736
    * Move HTTP requesting code to `ApiClient` and allow customizing options
      in it, e.g. "basePath"
    * Update unit tests accordingly and add some tests for `ApiClient`

--- samples/client/petstore/javascript/src/model/Order.js
+++ samples/client/petstore/javascript/src/model/Order.js
@@ -1,11 +1,22 @@
-// require files in Node.js environment
+(function(root, factory) {
+  if (typeof define === 'function' && define.amd) {
+    // AMD. Register as an anonymous module.
+    define([undefined], factory);
+  } else if (typeof module === 'object' && module.exports) {
+    // CommonJS-like environments that support module.exports, like Node.
+    module.exports = factory(undefined);
+  } else {
+    // Browser globals (root is window)
+    if (!root.SwaggerPetstore) {
+      root.SwaggerPetstore = {};
+    }
+    factory(root.SwaggerPetstore);
+  }
+}(this, function(module) {
+  'use strict';
 
-if (typeof module === 'object' && module.exports) {
   
-}
-
-
-
+  
 //export module
 if ( typeof define === "function" && define.amd ) {     
 	define('StatusEnum', ['jquery'], function($) {
@@ -35,162 +46,159 @@ var StatusEnum = function StatusEnum() {
 }
 
 
-//export module
-if ( typeof define === "function" && define.amd ) {
-  define('Order', ['jquery'],
-    function($) {
-      return Order;
-   });
-}
+  
+  var Order = function Order() { 
+    var self = this;
+    
+    /**
+     * datatype: Integer
+     **/
+    self.id = null;
+    
+    /**
+     * datatype: Integer
+     **/
+    self.petId = null;
+    
+    /**
+     * datatype: Integer
+     **/
+    self.quantity = null;
+    
+    /**
+     * datatype: Date
+     **/
+    self.shipDate = null;
+    
+    /**
+     * Order Status
+     * datatype: StatusEnum
+     **/
+    self.status = null;
+    
+    /**
+     * datatype: Boolean
+     **/
+    self.complete = null;
+    
 
+    self.constructFromObject = function(data) {
+      if (!data) {
+        return;
+      }
+      
+      self.id = data.id;
+      
+      self.petId = data.petId;
+      
+      self.quantity = data.quantity;
+      
+      self.shipDate = data.shipDate;
+      
+      self.status = data.status;
+      
+      self.complete = data.complete;
+      
+    }
 
-var Order = function Order() { 
-  var self = this;
-  
-  /**
-   * datatype: Integer
-   **/
-  self.id = null;
-  
-  /**
-   * datatype: Integer
-   **/
-  self.petId = null;
-  
-  /**
-   * datatype: Integer
-   **/
-  self.quantity = null;
-  
-  /**
-   * datatype: Date
-   **/
-  self.shipDate = null;
-  
-  /**
-   * Order Status
-   * datatype: StatusEnum
-   **/
-  self.status = null;
-  
-  /**
-   * datatype: Boolean
-   **/
-  self.complete = null;
-  
-  
-  self.constructFromObject = function(data) {
-    if (!data) {
-      return;
+    
+    /**
+     * @return {Integer}
+     **/
+    self.getId = function() {
+      return self.id;
+    }
+
+    /**
+     * @param {Integer} id
+     **/
+    self.setId = function (id) {
+      self.id = id;
     }
     
-    self.id = data.id;
+    /**
+     * @return {Integer}
+     **/
+    self.getPetId = function() {
+      return self.petId;
+    }
+
+    /**
+     * @param {Integer} petId
+     **/
+    self.setPetId = function (petId) {
+      self.petId = petId;
+    }
     
-    self.petId = data.petId;
+    /**
+     * @return {Integer}
+     **/
+    self.getQuantity = function() {
+      return self.quantity;
+    }
+
+    /**
+     * @param {Integer} quantity
+     **/
+    self.setQuantity = function (quantity) {
+      self.quantity = quantity;
+    }
     
-    self.quantity = data.quantity;
+    /**
+     * @return {Date}
+     **/
+    self.getShipDate = function() {
+      return self.shipDate;
+    }
+
+    /**
+     * @param {Date} shipDate
+     **/
+    self.setShipDate = function (shipDate) {
+      self.shipDate = shipDate;
+    }
     
-    self.shipDate = data.shipDate;
+    /**
+     * get Order Status
+     * @return {StatusEnum}
+     **/
+    self.getStatus = function() {
+      return self.status;
+    }
+
+    /**
+     * set Order Status
+     * @param {StatusEnum} status
+     **/
+    self.setStatus = function (status) {
+      self.status = status;
+    }
     
-    self.status = data.status;
+    /**
+     * @return {Boolean}
+     **/
+    self.getComplete = function() {
+      return self.complete;
+    }
+
+    /**
+     * @param {Boolean} complete
+     **/
+    self.setComplete = function (complete) {
+      self.complete = complete;
+    }
     
-    self.complete = data.complete;
-    
+
+    self.toJson = function () {
+      return JSON.stringify(self);
+    }
+  };
+
+  if (module) {
+    module.Order = Order;
   }
 
+  return Order;
   
-  /**
-   * @return {Integer}
-   **/
-  self.getId = function() {
-    return self.id;
-  }
-
-  /**
-   * @param {Integer} id
-   **/
-  self.setId = function (id) {
-    self.id = id;
-  }
   
-  /**
-   * @return {Integer}
-   **/
-  self.getPetId = function() {
-    return self.petId;
-  }
-
-  /**
-   * @param {Integer} petId
-   **/
-  self.setPetId = function (petId) {
-    self.petId = petId;
-  }
-  
-  /**
-   * @return {Integer}
-   **/
-  self.getQuantity = function() {
-    return self.quantity;
-  }
-
-  /**
-   * @param {Integer} quantity
-   **/
-  self.setQuantity = function (quantity) {
-    self.quantity = quantity;
-  }
-  
-  /**
-   * @return {Date}
-   **/
-  self.getShipDate = function() {
-    return self.shipDate;
-  }
-
-  /**
-   * @param {Date} shipDate
-   **/
-  self.setShipDate = function (shipDate) {
-    self.shipDate = shipDate;
-  }
-  
-  /**
-   * get Order Status
-   * @return {StatusEnum}
-   **/
-  self.getStatus = function() {
-    return self.status;
-  }
-
-  /**
-   * set Order Status
-   * @param {StatusEnum} status
-   **/
-  self.setStatus = function (status) {
-    self.status = status;
-  }
-  
-  /**
-   * @return {Boolean}
-   **/
-  self.getComplete = function() {
-    return self.complete;
-  }
-
-  /**
-   * @param {Boolean} complete
-   **/
-  self.setComplete = function (complete) {
-    self.complete = complete;
-  }
-  
-
-  self.toJson = function () {
-    return JSON.stringify(self);
-  }
-}
-
-if (typeof module === 'object' && module.exports) {
-  module.exports = Order;
-}
+}));
--- samples/client/petstore/javascript/src/model/User.js
+++ samples/client/petstore/javascript/src/model/User.js
@@ -1,210 +1,218 @@
-// require files in Node.js environment
+(function(root, factory) {
+  if (typeof define === 'function' && define.amd) {
+    // AMD. Register as an anonymous module.
+    define([undefined], factory);
+  } else if (typeof module === 'object' && module.exports) {
+    // CommonJS-like environments that support module.exports, like Node.
+    module.exports = factory(undefined);
+  } else {
+    // Browser globals (root is window)
+    if (!root.SwaggerPetstore) {
+      root.SwaggerPetstore = {};
+    }
+    factory(root.SwaggerPetstore);
+  }
+}(this, function(module) {
+  'use strict';
 
-if (typeof module === 'object' && module.exports) {
   
-}
+  
 
+  
+  var User = function User() { 
+    var self = this;
+    
+    /**
+     * datatype: Integer
+     **/
+    self.id = null;
+    
+    /**
+     * datatype: String
+     **/
+    self.username = null;
+    
+    /**
+     * datatype: String
+     **/
+    self.firstName = null;
+    
+    /**
+     * datatype: String
+     **/
+    self.lastName = null;
+    
+    /**
+     * datatype: String
+     **/
+    self.email = null;
+    
+    /**
+     * datatype: String
+     **/
+    self.password = null;
+    
+    /**
+     * datatype: String
+     **/
+    self.phone = null;
+    
+    /**
+     * User Status
+     * datatype: Integer
+     **/
+    self.userStatus = null;
+    
 
+    self.constructFromObject = function(data) {
+      if (!data) {
+        return;
+      }
+      
+      self.id = data.id;
+      
+      self.username = data.username;
+      
+      self.firstName = data.firstName;
+      
+      self.lastName = data.lastName;
+      
+      self.email = data.email;
+      
+      self.password = data.password;
+      
+      self.phone = data.phone;
+      
+      self.userStatus = data.userStatus;
+      
+    }
 
+    
+    /**
+     * @return {Integer}
+     **/
+    self.getId = function() {
+      return self.id;
+    }
 
-//export module
-if ( typeof define === "function" && define.amd ) {
-  define('User', ['jquery'],
-    function($) {
-      return User;
-   });
-}
-
-
-var User = function User() { 
-  var self = this;
-  
-  /**
-   * datatype: Integer
-   **/
-  self.id = null;
-  
-  /**
-   * datatype: String
-   **/
-  self.username = null;
-  
-  /**
-   * datatype: String
-   **/
-  self.firstName = null;
-  
-  /**
-   * datatype: String
-   **/
-  self.lastName = null;
-  
-  /**
-   * datatype: String
-   **/
-  self.email = null;
-  
-  /**
-   * datatype: String
-   **/
-  self.password = null;
-  
-  /**
-   * datatype: String
-   **/
-  self.phone = null;
-  
-  /**
-   * User Status
-   * datatype: Integer
-   **/
-  self.userStatus = null;
-  
-  
-  self.constructFromObject = function(data) {
-    if (!data) {
-      return;
+    /**
+     * @param {Integer} id
+     **/
+    self.setId = function (id) {
+      self.id = id;
     }
     
-    self.id = data.id;
+    /**
+     * @return {String}
+     **/
+    self.getUsername = function() {
+      return self.username;
+    }
+
+    /**
+     * @param {String} username
+     **/
+    self.setUsername = function (username) {
+      self.username = username;
+    }
     
-    self.username = data.username;
+    /**
+     * @return {String}
+     **/
+    self.getFirstName = function() {
+      return self.firstName;
+    }
+
+    /**
+     * @param {String} firstName
+     **/
+    self.setFirstName = function (firstName) {
+      self.firstName = firstName;
+    }
     
-    self.firstName = data.firstName;
+    /**
+     * @return {String}
+     **/
+    self.getLastName = function() {
+      return self.lastName;
+    }
+
+    /**
+     * @param {String} lastName
+     **/
+    self.setLastName = function (lastName) {
+      self.lastName = lastName;
+    }
     
-    self.lastName = data.lastName;
+    /**
+     * @return {String}
+     **/
+    self.getEmail = function() {
+      return self.email;
+    }
+
+    /**
+     * @param {String} email
+     **/
+    self.setEmail = function (email) {
+      self.email = email;
+    }
     
-    self.email = data.email;
+    /**
+     * @return {String}
+     **/
+    self.getPassword = function() {
+      return self.password;
+    }
+
+    /**
+     * @param {String} password
+     **/
+    self.setPassword = function (password) {
+      self.password = password;
+    }
     
-    self.password = data.password;
+    /**
+     * @return {String}
+     **/
+    self.getPhone = function() {
+      return self.phone;
+    }
+
+    /**
+     * @param {String} phone
+     **/
+    self.setPhone = function (phone) {
+      self.phone = phone;
+    }
     
-    self.phone = data.phone;
+    /**
+     * get User Status
+     * @return {Integer}
+     **/
+    self.getUserStatus = function() {
+      return self.userStatus;
+    }
+
+    /**
+     * set User Status
+     * @param {Integer} userStatus
+     **/
+    self.setUserStatus = function (userStatus) {
+      self.userStatus = userStatus;
+    }
     
-    self.userStatus = data.userStatus;
-    
+
+    self.toJson = function () {
+      return JSON.stringify(self);
+    }
+  };
+
+  if (module) {
+    module.User = User;
   }
 
+  return User;
   
-  /**
-   * @return {Integer}
-   **/
-  self.getId = function() {
-    return self.id;
-  }
-
-  /**
-   * @param {Integer} id
-   **/
-  self.setId = function (id) {
-    self.id = id;
-  }
   
-  /**
-   * @return {String}
-   **/
-  self.getUsername = function() {
-    return self.username;
-  }
-
-  /**
-   * @param {String} username
-   **/
-  self.setUsername = function (username) {
-    self.username = username;
-  }
-  
-  /**
-   * @return {String}
-   **/
-  self.getFirstName = function() {
-    return self.firstName;
-  }
-
-  /**
-   * @param {String} firstName
-   **/
-  self.setFirstName = function (firstName) {
-    self.firstName = firstName;
-  }
-  
-  /**
-   * @return {String}
-   **/
-  self.getLastName = function() {
-    return self.lastName;
-  }
-
-  /**
-   * @param {String} lastName
-   **/
-  self.setLastName = function (lastName) {
-    self.lastName = lastName;
-  }
-  
-  /**
-   * @return {String}
-   **/
-  self.getEmail = function() {
-    return self.email;
-  }
-
-  /**
-   * @param {String} email
-   **/
-  self.setEmail = function (email) {
-    self.email = email;
-  }
-  
-  /**
-   * @return {String}
-   **/
-  self.getPassword = function() {
-    return self.password;
-  }
-
-  /**
-   * @param {String} password
-   **/
-  self.setPassword = function (password) {
-    self.password = password;
-  }
-  
-  /**
-   * @return {String}
-   **/
-  self.getPhone = function() {
-    return self.phone;
-  }
-
-  /**
-   * @param {String} phone
-   **/
-  self.setPhone = function (phone) {
-    self.phone = phone;
-  }
-  
-  /**
-   * get User Status
-   * @return {Integer}
-   **/
-  self.getUserStatus = function() {
-    return self.userStatus;
-  }
-
-  /**
-   * set User Status
-   * @param {Integer} userStatus
-   **/
-  self.setUserStatus = function (userStatus) {
-    self.userStatus = userStatus;
-  }
-  
-
-  self.toJson = function () {
-    return JSON.stringify(self);
-  }
-}
-
-if (typeof module === 'object' && module.exports) {
-  module.exports = User;
-}
+}));
