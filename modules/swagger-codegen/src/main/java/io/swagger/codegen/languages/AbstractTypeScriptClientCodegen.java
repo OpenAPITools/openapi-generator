d5d1c7b98f24862978c69b1b63cce3095af37b33
wing328 <wing328hk@gmail.com>
2016-01-25 09:48:32 +0800

    remove unused default value

--- modules/swagger-codegen/src/main/java/io/swagger/codegen/languages/AbstractTypeScriptClientCodegen.java
+++ modules/swagger-codegen/src/main/java/io/swagger/codegen/languages/AbstractTypeScriptClientCodegen.java
@@ -54,7 +54,7 @@ public abstract class AbstractTypeScriptClientCodegen extends DefaultCodegen imp
         // mapped to String as a workaround
         typeMapping.put("binary", "string");
 
-        cliOptions.add(new CliOption(CodegenConstants.MODEL_PROPERTY_NAMING, CodegenConstants.MODEL_PROPERTY_NAMING_DESC).defaultValue("CamelCase"));
+        cliOptions.add(new CliOption(CodegenConstants.MODEL_PROPERTY_NAMING, CodegenConstants.MODEL_PROPERTY_NAMING_DESC));
 
 
 	}
