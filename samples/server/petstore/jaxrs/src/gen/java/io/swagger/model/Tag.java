943c66a97583a1a1bb5493fd91c273d13bf5c00b
Nick Maynard <nick.maynard@alumni.doc.ic.ac.uk>
2016-01-31 21:12:16 +0000

    Update samples for DefaultValue

--- samples/server/petstore/jaxrs/src/gen/java/io/swagger/model/Tag.java
+++ samples/server/petstore/jaxrs/src/gen/java/io/swagger/model/Tag.java
@@ -10,7 +10,7 @@ import io.swagger.annotations.ApiModelProperty;
 
 
 
-@javax.annotation.Generated(value = "class io.swagger.codegen.languages.JaxRSServerCodegen", date = "2016-01-14T21:37:36.074Z")
+@javax.annotation.Generated(value = "class io.swagger.codegen.languages.JavaJerseyServerCodegen", date = "2016-01-31T21:10:14.319Z")
 public class Tag   {
   
   private Long id = null;
