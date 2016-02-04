b3dc9067ac67e1f619abf0280c29cdbeb55a0fa6
Scott Williams <scott.rw93@gmail.com>
2016-02-04 14:16:08 +0000

    Fix for issue #2036, python client wont upload list of files

--- modules/swagger-codegen/src/main/java/io/swagger/codegen/DefaultCodegen.java
+++ modules/swagger-codegen/src/main/java/io/swagger/codegen/DefaultCodegen.java
@@ -1366,6 +1366,8 @@ public class DefaultCodegen {
                 } else if (param instanceof FormParameter) {
                     if ("file".equalsIgnoreCase(((FormParameter) param).getType())) {
                         p.isFile = true;
+                    } else if("file".equals(p.baseType)){
+                    	p.isFile = true;
                     } else {
                         p.notFile = true;
                     }
