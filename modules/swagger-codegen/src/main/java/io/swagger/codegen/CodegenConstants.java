246369ccdd441f1fc1e4207891158a93b1fa8439
wing328 <wing328hk@gmail.com>
2016-02-04 15:35:05 +0800

    update csharp help, fix default header

--- modules/swagger-codegen/src/main/java/io/swagger/codegen/CodegenConstants.java
+++ modules/swagger-codegen/src/main/java/io/swagger/codegen/CodegenConstants.java
@@ -60,7 +60,7 @@ public class CodegenConstants {
     public static final String OPTIONAL_METHOD_ARGUMENT_DESC = "Optional method argument, e.g. void square(int x=10) (.net 4.0+ only).";
 
     public static final String OPTIONAL_ASSEMBLY_INFO = "optionalAssemblyInfo";
-    public static final String OPTIONAL_ASSEMBLY_INFO_DESC = "Generate AssemblyInfo.cs (Default: true).";
+    public static final String OPTIONAL_ASSEMBLY_INFO_DESC = "Generate AssemblyInfo.cs.";
 
     public static final String USE_COLLECTION = "useCollection";
     public static final String USE_COLLECTION_DESC = "Deserialize array types to Collection<T> instead of List<T>.";
@@ -69,7 +69,7 @@ public class CodegenConstants {
     public static final String RETURN_ICOLLECTION_DESC = "Return ICollection<T> instead of the concrete type.";
     
     public static final String OPTIONAL_PROJECT_FILE = "optionalProjectFile";
-    public static final String OPTIONAL_PROJECT_FILE_DESC = "Generate {PackageName}.csproj (Default: false).";
+    public static final String OPTIONAL_PROJECT_FILE_DESC = "Generate {PackageName}.csproj.";
     
     public static final String OPTIONAL_PROJECT_GUID = "packageGuid";
     public static final String OPTIONAL_PROJECT_GUID_DESC = "The GUID that will be associated with the C# project";
