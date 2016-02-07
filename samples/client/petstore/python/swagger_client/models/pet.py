3b64a2145637a60bc1227e77c35876958917f405
Matan Goldman <matan.goldman@pixoneye.com>
2016-02-07 20:42:24 +0200

    change the order of the elif inside to_dict for better performance (dictionary/additionalProperties is less probable)

--- samples/client/petstore/python/swagger_client/models/pet.py
+++ samples/client/petstore/python/swagger_client/models/pet.py
@@ -212,14 +212,14 @@ class Pet(object):
                     lambda x: x.to_dict() if hasattr(x, "to_dict") else x,
                     value
                 ))
+            elif hasattr(value, "to_dict"):
+                result[attr] = value.to_dict()
             elif isinstance(value, dict):
                 result[attr] = dict(map(
                     lambda item: (item[0], item[1].to_dict())
                     if hasattr(item[1], "to_dict") else item,
                     value.items()
                 ))
-            elif hasattr(value, "to_dict"):
-                result[attr] = value.to_dict()
             else:
                 result[attr] = value
 
