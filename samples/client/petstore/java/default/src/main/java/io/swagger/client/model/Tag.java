143e7a7d5b1bf13a7567d961ea06d4315ef7553a
Andrew B <who@users.noreply.github.com>
2016-02-12 18:52:49 -0800

    Adding PATCH header override the default Java client

--- samples/client/petstore/java/default/src/main/java/io/swagger/client/model/Tag.java
+++ samples/client/petstore/java/default/src/main/java/io/swagger/client/model/Tag.java
@@ -10,7 +10,7 @@ import io.swagger.annotations.ApiModelProperty;
 
 
 
-@javax.annotation.Generated(value = "class io.swagger.codegen.languages.JavaClientCodegen", date = "2016-01-15T19:19:23.415+08:00")
+@javax.annotation.Generated(value = "class io.swagger.codegen.languages.JavaClientCodegen", date = "2016-02-12T18:48:10.013-08:00")
 public class Tag   {
   
   private Long id = null;
@@ -19,8 +19,13 @@ public class Tag   {
   
   /**
    **/
+  public Tag id(Long id) {
+    this.id = id;
+    return this;
+  }
+
   
-  @ApiModelProperty(value = "")
+  @ApiModelProperty(example = "null", value = "")
   @JsonProperty("id")
   public Long getId() {
     return id;
@@ -32,8 +37,13 @@ public class Tag   {
   
   /**
    **/
+  public Tag name(String name) {
+    this.name = name;
+    return this;
+  }
+
   
-  @ApiModelProperty(value = "")
+  @ApiModelProperty(example = "null", value = "")
   @JsonProperty("name")
   public String getName() {
     return name;
