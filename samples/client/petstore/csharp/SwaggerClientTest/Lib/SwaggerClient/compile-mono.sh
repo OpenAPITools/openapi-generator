1d57e70e767cab509ba8d61a1015ab7feda6482a
Jim Schubert <james.schubert@gmail.com>
2016-02-17 13:31:50 -0500

    [csharp] Set RestSharp to 105.1.0 due to bug in newer versions

--- samples/client/petstore/csharp/SwaggerClientTest/Lib/SwaggerClient/compile-mono.sh
+++ samples/client/petstore/csharp/SwaggerClientTest/Lib/SwaggerClient/compile-mono.sh
@@ -8,7 +8,7 @@ mono nuget.exe install vendor/packages.config -o vendor;
 mkdir -p bin;
 
 cp vendor/Newtonsoft.Json.8.0.2/lib/net45/Newtonsoft.Json.dll bin/Newtonsoft.Json.dll;
-cp vendor/RestSharp.105.2.3/lib/net45/RestSharp.dll bin/RestSharp.dll;
+cp vendor/RestSharp.105.1.0/lib/net45/RestSharp.dll bin/RestSharp.dll;
 
 mcs -sdk:${netfx} -r:bin/Newtonsoft.Json.dll,\
 bin/RestSharp.dll,\
--- samples/client/petstore/csharp/SwaggerClientTest/Lib/SwaggerClient/compile.bat
+++ samples/client/petstore/csharp/SwaggerClientTest/Lib/SwaggerClient/compile.bat
@@ -1,10 +1,12 @@
 @echo off
+
 SET CSCPATH=%SYSTEMROOT%\Microsoft.NET\Framework\v4.0.30319
+
 
 if not exist ".\nuget.exe" powershell -Command "(new-object System.Net.WebClient).DownloadFile('https://nuget.org/nuget.exe', '.\nuget.exe')"
 .\nuget.exe install vendor/packages.config -o vendor
 
 cp vendor/Newtonsoft.Json.8.0.2/lib/net45/Newtonsoft.Json.dll bin/Newtonsoft.Json.dll
-cp vendor/RestSharp.105.2.3/lib/net45/RestSharp.dll bin/RestSharp.dll
+cp vendor/RestSharp.105.1.0/lib/net45/RestSharp.dll bin/RestSharp.dll
 
 %CSCPATH%\csc /reference:bin/Newtonsoft.Json.dll;bin/RestSharp.dll /target:library /out:bin/IO.Swagger.dll /recurse:src\*.cs /doc:bin/IO.Swagger.xml
