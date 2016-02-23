8d2e1b191bd59c94489badc655b1de6a1e672608
Tony Tam <fehguy@gmail.com>
2016-02-22 16:35:02 -0800

    fixes #2214

--- samples/client/wordnik/scala/src/main/java/io/swagger/client/model/ExampleSearchResults.scala
+++
@@ -1,7 +0,0 @@
-package io.swagger.client.model
-
-
-case class ExampleSearchResults(
-                                 facets: List[Facet],
-                                 examples: List[Example])
-  
