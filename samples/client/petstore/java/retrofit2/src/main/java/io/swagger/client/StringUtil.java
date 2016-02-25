6788c8981663dee42d2850fa90b095a2c25562ea
David Hontecillas <dhontecillas@gmail.com>
2016-02-25 17:00:56 +0100

    fix templates for retrofit2rx and build.gradle

--- samples/client/petstore/java/retrofit2/src/main/java/io/swagger/client/StringUtil.java
+++ samples/client/petstore/java/retrofit2/src/main/java/io/swagger/client/StringUtil.java
@@ -1,6 +1,6 @@
 package io.swagger.client;
 
-@javax.annotation.Generated(value = "class io.swagger.codegen.languages.JavaClientCodegen", date = "2016-02-25T10:57:20.452+01:00")
+@javax.annotation.Generated(value = "class io.swagger.codegen.languages.JavaClientCodegen", date = "2016-02-25T16:20:24.158+01:00")
 public class StringUtil {
   /**
    * Check if the given array contains the given value (with case-insensitive comparison).
