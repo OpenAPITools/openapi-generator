784649f3c3df3f188f96e8eae5858aab31fd631f
xhh <xhh@xhh.me>
2016-03-07 15:57:13 +0800

    JS client: remove "undefined" from model dependency arguments
    Closes #2279

--- samples/client/petstore/javascript-promise/src/model/User.js
+++ samples/client/petstore/javascript-promise/src/model/User.js
@@ -1,18 +1,18 @@
 (function(root, factory) {
   if (typeof define === 'function' && define.amd) {
     // AMD. Register as an anonymous module.
-    define([undefined, '../ApiClient'], factory);
+    define(['../ApiClient'], factory);
   } else if (typeof module === 'object' && module.exports) {
     // CommonJS-like environments that support module.exports, like Node.
-    module.exports = factory(undefined, require('../ApiClient'));
+    module.exports = factory(require('../ApiClient'));
   } else {
     // Browser globals (root is window)
     if (!root.SwaggerPetstore) {
       root.SwaggerPetstore = {};
     }
-    factory(root.SwaggerPetstore, root.SwaggerPetstore.ApiClient);
+    root.SwaggerPetstore.User = factory(root.SwaggerPetstore.ApiClient);
   }
-}(this, function(module, ApiClient) {
+}(this, function(ApiClient) {
   'use strict';
   
   
@@ -180,10 +180,6 @@
   
 
   
-
-  if (module) {
-    module.User = User;
-  }
 
   return User;
   
