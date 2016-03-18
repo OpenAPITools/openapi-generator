e27d788f579f075c82e92eadec658399658c990c
wing328 <wing328hk@gmail.com>
2016-03-18 17:47:59 +0800

    udpate ruby api doc

--- samples/client/petstore/ruby/lib/petstore/configuration.rb
+++ samples/client/petstore/ruby/lib/petstore/configuration.rb
@@ -157,26 +157,12 @@ module Petstore
     # Returns Auth Settings hash for api client.
     def auth_settings
       {
-        'petstore_auth' =>
-          {
-            type: 'oauth2',
-            in: 'header',
-            key: 'Authorization',
-            value: "Bearer #{access_token}"
-          },
-        'test_api_client_id' =>
+        'test_api_key_header' =>
           {
             type: 'api_key',
             in: 'header',
-            key: 'x-test_api_client_id',
-            value: api_key_with_prefix('x-test_api_client_id')
-          },
-        'test_api_client_secret' =>
-          {
-            type: 'api_key',
-            in: 'header',
-            key: 'x-test_api_client_secret',
-            value: api_key_with_prefix('x-test_api_client_secret')
+            key: 'test_api_key_header',
+            value: api_key_with_prefix('test_api_key_header')
           },
         'api_key' =>
           {
@@ -192,6 +178,20 @@ module Petstore
             key: 'Authorization',
             value: basic_auth_token
           },
+        'test_api_client_secret' =>
+          {
+            type: 'api_key',
+            in: 'header',
+            key: 'x-test_api_client_secret',
+            value: api_key_with_prefix('x-test_api_client_secret')
+          },
+        'test_api_client_id' =>
+          {
+            type: 'api_key',
+            in: 'header',
+            key: 'x-test_api_client_id',
+            value: api_key_with_prefix('x-test_api_client_id')
+          },
         'test_api_key_query' =>
           {
             type: 'api_key',
@@ -199,12 +199,12 @@ module Petstore
             key: 'test_api_key_query',
             value: api_key_with_prefix('test_api_key_query')
           },
-        'test_api_key_header' =>
+        'petstore_auth' =>
           {
-            type: 'api_key',
+            type: 'oauth2',
             in: 'header',
-            key: 'test_api_key_header',
-            value: api_key_with_prefix('test_api_key_header')
+            key: 'Authorization',
+            value: "Bearer #{access_token}"
           },
       }
     end
