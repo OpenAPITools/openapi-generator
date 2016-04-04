c2cab8461228da80e3ff02339816f54a30568436
wing328 <wing328hk@gmail.com>
2016-04-04 15:58:50 +0800

    fix typo, update sinatra to use origianl petstore spec (yaml)

--- samples/server/petstore/sinatra/api/store_api.rb
+++ samples/server/petstore/sinatra/api/store_api.rb
@@ -1,44 +1,24 @@
 require 'json'
 
 
-MyApp.add_route('GET', '/v2/store/inventory', {
+MyApp.add_route('DELETE', '/v2/stores/order/{orderId}', {
   "resourcePath" => "/Store",
-  "summary" => "Returns pet inventories by status",
-  "nickname" => "get_inventory", 
-  "responseClass" => "map[string,int]", 
-  "endpoint" => "/store/inventory", 
-  "notes" => "Returns a map of status codes to quantities",
+  "summary" => "Delete purchase order by ID",
+  "nickname" => "delete_order", 
+  "responseClass" => "void", 
+  "endpoint" => "/stores/order/{orderId}", 
+  "notes" => "For valid response try integer IDs with value < 1000. Anything above 1000 or nonintegers will generate API errors",
   "parameters" => [
-    
-    
-    
-    
-    ]}) do
-  cross_origin
-  # the guts live here
-
-  {"message" => "yes, it worked"}.to_json
-end
-
-
-MyApp.add_route('POST', '/v2/store/order', {
-  "resourcePath" => "/Store",
-  "summary" => "Place an order for a pet",
-  "nickname" => "place_order", 
-  "responseClass" => "Order", 
-  "endpoint" => "/store/order", 
-  "notes" => "",
-  "parameters" => [
-    
-    
     
     
     {
-      "name" => "body",
-      "description" => "order placed for purchasing the pet",
-      "dataType" => "Order",
-      "paramType" => "body",
-    }
+      "name" => "order_id",
+      "description" => "ID of the order that needs to be deleted",
+      "dataType" => "string",
+      "paramType" => "path",
+    },
+    
+    
     
     ]}) do
   cross_origin
@@ -48,12 +28,12 @@ MyApp.add_route('POST', '/v2/store/order', {
 end
 
 
-MyApp.add_route('GET', '/v2/store/order/{orderId}', {
+MyApp.add_route('GET', '/v2/stores/order/{orderId}', {
   "resourcePath" => "/Store",
   "summary" => "Find purchase order by ID",
   "nickname" => "get_order_by_id", 
   "responseClass" => "Order", 
-  "endpoint" => "/store/order/{orderId}", 
+  "endpoint" => "/stores/order/{orderId}", 
   "notes" => "For valid response try integer IDs with value <= 5 or > 10. Other values will generated exceptions",
   "parameters" => [
     
@@ -75,24 +55,24 @@ MyApp.add_route('GET', '/v2/store/order/{orderId}', {
 end
 
 
-MyApp.add_route('DELETE', '/v2/store/order/{orderId}', {
+MyApp.add_route('POST', '/v2/stores/order', {
   "resourcePath" => "/Store",
-  "summary" => "Delete purchase order by ID",
-  "nickname" => "delete_order", 
-  "responseClass" => "void", 
-  "endpoint" => "/store/order/{orderId}", 
-  "notes" => "For valid response try integer IDs with value < 1000. Anything above 1000 or nonintegers will generate API errors",
+  "summary" => "Place an order for a pet",
+  "nickname" => "place_order", 
+  "responseClass" => "Order", 
+  "endpoint" => "/stores/order", 
+  "notes" => "",
   "parameters" => [
     
     
+    
+    
     {
-      "name" => "order_id",
-      "description" => "ID of the order that needs to be deleted",
-      "dataType" => "string",
-      "paramType" => "path",
-    },
-    
-    
+      "name" => "body",
+      "description" => "order placed for purchasing the pet",
+      "dataType" => "Order",
+      "paramType" => "body",
+    }
     
     ]}) do
   cross_origin
