68cb0306099b2d3deb96ffebd2736444f16ac122
wing328 <wing328hk@gmail.com>
2016-04-11 22:11:53 +0800

    add new files for format test model

---
+++ samples/client/petstore/java/default/src/main/java/io/swagger/client/model/Cat.java
@@ -0,0 +1,95 @@
+package io.swagger.client.model;
+
+import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import io.swagger.client.model.Animal;
+
+
+
+
+
+
+public class Cat extends Animal  {
+  
+  private String className = null;
+  private Boolean declawed = null;
+
+  
+  /**
+   **/
+  public Cat className(String className) {
+    this.className = className;
+    return this;
+  }
+  
+  @ApiModelProperty(example = "null", required = true, value = "")
+  @JsonProperty("className")
+  public String getClassName() {
+    return className;
+  }
+  public void setClassName(String className) {
+    this.className = className;
+  }
+
+
+  /**
+   **/
+  public Cat declawed(Boolean declawed) {
+    this.declawed = declawed;
+    return this;
+  }
+  
+  @ApiModelProperty(example = "null", value = "")
+  @JsonProperty("declawed")
+  public Boolean getDeclawed() {
+    return declawed;
+  }
+  public void setDeclawed(Boolean declawed) {
+    this.declawed = declawed;
+  }
+
+
+  @Override
+  public boolean equals(java.lang.Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Cat cat = (Cat) o;
+    return Objects.equals(this.className, cat.className) &&
+        Objects.equals(this.declawed, cat.declawed) &&
+        super.equals(o);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(className, declawed, super.hashCode());
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class Cat {\n");
+    sb.append("    ").append(toIndentedString(super.toString())).append("\n");
+    sb.append("    className: ").append(toIndentedString(className)).append("\n");
+    sb.append("    declawed: ").append(toIndentedString(declawed)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(java.lang.Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
+
---
+++ samples/client/petstore/ruby/docs/FormatTest.md
@@ -0,0 +1,18 @@
+# Petstore::FormatTest
+
+## Properties
+Name | Type | Description | Notes
+------------ | ------------- | ------------- | -------------
+**integer** | **Integer** |  | [optional] 
+**int32** | **Integer** |  | [optional] 
+**int64** | **Integer** |  | [optional] 
+**number** | **Float** |  | 
+**float** | **Float** |  | [optional] 
+**double** | **Float** |  | [optional] 
+**string** | **String** |  | [optional] 
+**byte** | **String** |  | [optional] 
+**binary** | **String** |  | [optional] 
+**date** | **Date** |  | [optional] 
+**date_time** | **String** |  | [optional] 
+
+
