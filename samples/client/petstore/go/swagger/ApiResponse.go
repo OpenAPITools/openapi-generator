8539ecacd29dd863bfe8aac9c0e887fb1b1deaa7
wing328 <wing328hk@gmail.com>
2016-04-11 23:44:06 +0800

    udpate type mapping for go, fix indention in go model

--- samples/client/petstore/go/swagger/ApiResponse.go
+++ samples/client/petstore/go/swagger/ApiResponse.go
@@ -5,6 +5,6 @@ import (
 
 type ApiResponse struct {
     Code  int32  `json:"code,omitempty"`
-Type_  string  `json:"type,omitempty"`
-Message  string  `json:"message,omitempty"`
+    Type_  string  `json:"type,omitempty"`
+    Message  string  `json:"message,omitempty"`
 }
