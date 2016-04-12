7afa74637c15340306baebdf4eeb688832f8a118
Guo Huang <guohuang@gmail.com>
2016-04-11 22:25:02 -0700

    added api prefix for authentication

--- samples/client/petstore/go/swagger/Configuration.go
+++ samples/client/petstore/go/swagger/Configuration.go
@@ -7,7 +7,8 @@ import (
 type Configuration struct {
     UserName  string  `json:"userName,omitempty"`
     Password  string  `json:"password,omitempty"`
-    ApiKey  string  `json:"apiKey,omitempty"`
+    ApiKeyPrefix map[string] string `json:"apiKeyPrefix,omitempty"`
+    ApiKey map[string] string  `json:"apiKey,omitempty"`
     Debug  bool  `json:"debug,omitempty"`
     DebugFile  string  `json:"debugFile,omitempty"`
     OAuthToken  string  `json:"oAuthToken,omitempty"`
@@ -21,11 +22,15 @@ type Configuration struct {
 
 func NewConfiguration() *Configuration {
     defaultHeader := make(map[string]string)
+    apiKey := make(map[string]string) 
+    apiKeyPrefix := make(map[string]string) 
     return &Configuration{
         BasePath: "http://petstore.swagger.io/v2",
         UserName: "",
         Debug: false,
         DefaultHeader: defaultHeader,
+        ApiKey: apiKey,
+        ApiKeyPrefix: apiKeyPrefix,
         }
 }
 
@@ -35,4 +40,14 @@ func (c *Configuration) GetBasicAuthEncodedString() string {
 
 func (c *Configuration) AddDefaultHeader(key string, value string) {
     c.DefaultHeader[key] = value
+}
+
+func (c *Configuration) GetApiKeyWithPrefix(apiKeyIdentifier string) string {
+    var returnValue = c.ApiKey[apiKeyIdentifier]
+    var apiKeyPrefix = c.ApiKeyPrefix[apiKeyIdentifier]
+    if apiKeyPrefix != ""{
+        returnValue = apiKeyPrefix + " " + returnValue
+    }
+        
+    return returnValue
 }
