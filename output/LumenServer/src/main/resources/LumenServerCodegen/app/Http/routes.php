a92ac4537ebd377f705bbfdebb17bfd78b85564a
abcsun <chenglongsun_nj@163.com>
2016-04-14 15:21:31 +0800

    init lumen 5.2

---
+++ output/LumenServer/src/main/resources/LumenServerCodegen/app/Http/routes.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+
+$app->get('/', function () use ($app) {
+    return $app->version();
+});
