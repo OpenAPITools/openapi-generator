ed164a77bddd0c1c1872aaf32894c71a10ec95f3
kolyjjj <me.koly.li@gmail.com>
2016-04-14 23:25:43 +0800

    [issue-2374][koly] generated the spring mvc sample

--- samples/server/petstore/spring-mvc/src/main/java/io/swagger/configuration/WebApplication.java
+++ samples/server/petstore/spring-mvc/src/main/java/io/swagger/configuration/WebApplication.java
@@ -2,7 +2,7 @@ package io.swagger.configuration;
 
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
-@javax.annotation.Generated(value = "class io.swagger.codegen.languages.SpringMVCServerCodegen", date = "2016-02-26T13:58:54.483Z")
+@javax.annotation.Generated(value = "class io.swagger.codegen.languages.SpringMVCServerCodegen", date = "2016-04-14T23:14:04.836+08:00")
 public class WebApplication extends AbstractAnnotationConfigDispatcherServletInitializer {
 
     @Override
