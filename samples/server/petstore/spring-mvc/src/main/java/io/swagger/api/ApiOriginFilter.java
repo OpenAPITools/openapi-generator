abbafc43e01895dc2b117435c8d870d72c279652
kolyjjj <me.koly.li@gmail.com>
2016-04-14 23:25:43 +0800

    [issue-2374][koly] generated the spring mvc sample

--- samples/server/petstore/spring-mvc/src/main/java/io/swagger/api/ApiOriginFilter.java
+++ samples/server/petstore/spring-mvc/src/main/java/io/swagger/api/ApiOriginFilter.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import javax.servlet.*;
 import javax.servlet.http.HttpServletResponse;
 
-@javax.annotation.Generated(value = "class io.swagger.codegen.languages.SpringMVCServerCodegen", date = "2016-02-26T13:58:54.483Z")
+@javax.annotation.Generated(value = "class io.swagger.codegen.languages.SpringMVCServerCodegen", date = "2016-04-14T23:14:04.836+08:00")
 public class ApiOriginFilter implements javax.servlet.Filter {
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response,
