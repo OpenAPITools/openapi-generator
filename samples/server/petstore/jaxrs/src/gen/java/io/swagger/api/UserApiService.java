400cc9e6f90bf584995cc2a83dfe0e93f4e730cd
wing328 <wing328hk@gmail.com>
2016-04-15 18:18:18 +0800

    update sample for jaxrs

--- samples/server/petstore/jaxrs/src/gen/java/io/swagger/api/UserApiService.java
+++ samples/server/petstore/jaxrs/src/gen/java/io/swagger/api/UserApiService.java
@@ -21,29 +21,20 @@ import javax.ws.rs.core.SecurityContext;
 
 
 public abstract class UserApiService {
-  
       public abstract Response createUser(User body,SecurityContext securityContext)
       throws NotFoundException;
-  
       public abstract Response createUsersWithArrayInput(List<User> body,SecurityContext securityContext)
       throws NotFoundException;
-  
       public abstract Response createUsersWithListInput(List<User> body,SecurityContext securityContext)
       throws NotFoundException;
-  
       public abstract Response deleteUser(String username,SecurityContext securityContext)
       throws NotFoundException;
-  
       public abstract Response getUserByName(String username,SecurityContext securityContext)
       throws NotFoundException;
-  
       public abstract Response loginUser(String username,String password,SecurityContext securityContext)
       throws NotFoundException;
-  
       public abstract Response logoutUser(SecurityContext securityContext)
       throws NotFoundException;
-  
       public abstract Response updateUser(String username,User body,SecurityContext securityContext)
       throws NotFoundException;
-  
 }
