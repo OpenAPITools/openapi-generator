69150883c18fc94f9ef7bc6a3791a660608eb757
Guo Huang <guohuang@gmail.com>
2016-04-15 14:57:47 -0700

    added pom.mustache to generate pom.xml

--- modules/swagger-codegen/src/main/java/io/swagger/codegen/languages/GoClientCodegen.java
+++ modules/swagger-codegen/src/main/java/io/swagger/codegen/languages/GoClientCodegen.java
@@ -134,6 +134,7 @@ public class GoClientCodegen extends DefaultCodegen implements CodegenConfig {
         supportingFiles.add(new SupportingFile("git_push.sh.mustache", "", "git_push.sh"));
         supportingFiles.add(new SupportingFile("gitignore.mustache", "", ".gitignore"));
         supportingFiles.add(new SupportingFile("configuration.mustache", packageName, "configuration.go"));
+        supportingFiles.add(new SupportingFile("pom.mustache", "", "pom.xml"));
     }
 
     @Override
