c93fcc2643c279e644cffa22bc5a7eaf8454437a
wing328 <wing328hk@gmail.com>
2016-04-15 20:05:45 +0800

    update php, nodejs, scalatra server for petstore

--- samples/server/petstore/scalatra/src/main/scala/ScalatraBootstrap.scala
+++ samples/server/petstore/scalatra/src/main/scala/ScalatraBootstrap.scala
@@ -1,6 +1,6 @@
 import com.wordnik.client.api._
 import akka.actor.ActorSystem
-import io.swagger.app.{ResourcesApp, SwaggerApp}
+import io.swagger.app.{ ResourcesApp, SwaggerApp }
 import javax.servlet.ServletContext
 import org.scalatra.LifeCycle
 
@@ -10,10 +10,10 @@ class ScalatraBootstrap extends LifeCycle {
   override def init(context: ServletContext) {
     implicit val system = ActorSystem("appActorSystem")
     try {
-      context mount (new UserApi, "/User/*")
       context mount (new PetApi, "/Pet/*")
       context mount (new StoreApi, "/Store/*")
-      
+      context mount (new UserApi, "/User/*")
+
       context mount (new ResourcesApp, "/api-docs/*")
     } catch {
       case e: Throwable => e.printStackTrace()
--- samples/server/petstore/scalatra/src/main/scala/com/wordnik/client/model/User.scala
+++ samples/server/petstore/scalatra/src/main/scala/com/wordnik/client/model/User.scala
@@ -1,14 +1,12 @@
 package com.wordnik.client.model
 
-
-
-case class User (
-  id: Long,
-  username: String,
-  firstName: String,
-  lastName: String,
-  email: String,
-  password: String,
-  phone: String,
-  userStatus: Int
-)
+case class User(
+  id: Option[Long],
+  username: Option[String],
+  firstName: Option[String],
+  lastName: Option[String],
+  email: Option[String],
+  password: Option[String],
+  phone: Option[String],
+  userStatus: Option[Int] // User Status
+  )
