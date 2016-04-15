0982afbca0a53fcbe4b951e22346d2a8c322e884
wing328 <wing328hk@gmail.com>
2016-04-15 17:42:18 +0800

    update server generator to use petstore.yaml

--- samples/server/petstore/aspnet5/src/IO.Swagger/Models/Pet.cs
+++ samples/server/petstore/aspnet5/src/IO.Swagger/Models/Pet.cs
@@ -51,44 +51,37 @@ namespace IO.Swagger.Models
             
         }
 
-        
         /// <summary>
         /// Gets or Sets Id
         /// </summary>
         public long? Id { get; set; }
 
-        
         /// <summary>
         /// Gets or Sets Category
         /// </summary>
         public Category Category { get; set; }
 
-        
         /// <summary>
         /// Gets or Sets Name
         /// </summary>
         public string Name { get; set; }
 
-        
         /// <summary>
         /// Gets or Sets PhotoUrls
         /// </summary>
         public List<string> PhotoUrls { get; set; }
 
-        
         /// <summary>
         /// Gets or Sets Tags
         /// </summary>
         public List<Tag> Tags { get; set; }
 
-        
         /// <summary>
         /// pet status in the store
         /// </summary>
         /// <value>pet status in the store</value>
         public string Status { get; set; }
 
-        
 
         /// <summary>
         /// Returns the string presentation of the object
@@ -99,12 +92,11 @@ namespace IO.Swagger.Models
             var sb = new StringBuilder();
             sb.Append("class Pet {\n");
             sb.Append("  Id: ").Append(Id).Append("\n");
-            sb.Append("  Category: ").Append(Category).Append("\n");
-            sb.Append("  Name: ").Append(Name).Append("\n");
-            sb.Append("  PhotoUrls: ").Append(PhotoUrls).Append("\n");
-            sb.Append("  Tags: ").Append(Tags).Append("\n");
-            sb.Append("  Status: ").Append(Status).Append("\n");
-            
+sb.Append("  Category: ").Append(Category).Append("\n");
+sb.Append("  Name: ").Append(Name).Append("\n");
+sb.Append("  PhotoUrls: ").Append(PhotoUrls).Append("\n");
+sb.Append("  Tags: ").Append(Tags).Append("\n");
+sb.Append("  Status: ").Append(Status).Append("\n");
             sb.Append("}\n");
             return sb.ToString();
         }
@@ -186,25 +178,18 @@ namespace IO.Swagger.Models
             {
                 int hash = 41;
                 // Suitable nullity checks etc, of course :)
-                
                     if (this.Id != null)
                     hash = hash * 59 + this.Id.GetHashCode();
-                
                     if (this.Category != null)
                     hash = hash * 59 + this.Category.GetHashCode();
-                
                     if (this.Name != null)
                     hash = hash * 59 + this.Name.GetHashCode();
-                
                     if (this.PhotoUrls != null)
                     hash = hash * 59 + this.PhotoUrls.GetHashCode();
-                
                     if (this.Tags != null)
                     hash = hash * 59 + this.Tags.GetHashCode();
-                
                     if (this.Status != null)
                     hash = hash * 59 + this.Status.GetHashCode();
-                
                 return hash;
             }
         }
