a6453e45c1b4ac6528b9f6c66cdef90d9b4d007e
Guo Huang <guohuang@gmail.com>
2016-04-16 22:22:23 -0700

    fixed go build issue after removing swagger folder

--- samples/client/petstore/go/api_client.go
+++ samples/client/petstore/go/api_client.go
@@ -1,41 +1,40 @@
 package swagger
 
 import (
-    "strings"
+	"strings"
 )
 
 type ApiClient struct {
-
 }
 
 func (c *ApiClient) SelectHeaderContentType(contentTypes []string) string {
-    if (len(contentTypes) == 0){
-        return ""
-    }
-    if contains(contentTypes,"application/json") {
-        return "application/json"
-    }
+	if len(contentTypes) == 0 {
+		return ""
+	}
+	if contains(contentTypes, "application/json") {
+		return "application/json"
+	}
 
-    return contentTypes[0] // use the first content type specified in 'consumes'
+	return contentTypes[0] // use the first content type specified in 'consumes'
 }
 
 func (c *ApiClient) SelectHeaderAccept(accepts []string) string {
-    if (len(accepts) == 0){        
-        return ""
-    }
+	if len(accepts) == 0 {
+		return ""
+	}
 
-    if contains(accepts,"application/json"){        
-        return "application/json"
-    }
+	if contains(accepts, "application/json") {
+		return "application/json"
+	}
 
-    return strings.Join(accepts,",")
+	return strings.Join(accepts, ",")
 }
 
 func contains(source []string, containvalue string) bool {
-    for _, a := range source {
-        if strings.ToLower(a) == strings.ToLower(containvalue) {
-            return true
-        }
-    }
-    return false
+	for _, a := range source {
+		if strings.ToLower(a) == strings.ToLower(containvalue) {
+			return true
+		}
+	}
+	return false
 }
--- samples/client/petstore/go/pet_api_test.go
+++ samples/client/petstore/go/pet_api_test.go
@@ -1,15 +1,13 @@
-package main
+package swagger
 
 import (
-	"testing"
-
-	sw "./swagger"
 	"github.com/stretchr/testify/assert"
+	"testing"
 )
 
 func TestAddPet(t *testing.T) {
-	s := sw.NewPetApi()
-	newPet := (sw.Pet{Id: 12830, Name: "gopher",
+	s := NewPetApi()
+	newPet := (Pet{Id: 12830, Name: "gopher",
 		PhotoUrls: []string{"http://1.com", "http://2.com"}, Status: "pending"})
 
 	err := s.AddPet(newPet)
@@ -23,22 +21,22 @@ func TestAddPet(t *testing.T) {
 func TestGetPetById(t *testing.T) {
 	assert := assert.New(t)
 
-	s := sw.NewPetApi()
+	s := NewPetApi()
 	resp, err := s.GetPetById(12830)
 	if err != nil {
 		t.Errorf("Error while getting pet by id")
 		t.Log(err)
 	} else {
-		assert.Equal(resp.Id, "12830", "Pet id should be equal")
+		assert.Equal(resp.Id, int64(12830), "Pet id should be equal")
 		assert.Equal(resp.Name, "gopher", "Pet name should be gopher")
 		assert.Equal(resp.Status, "pending", "Pet status should be pending")
 
-		t.Log(resp)
+		//t.Log(resp)
 	}
 }
 
 func TestUpdatePetWithForm(t *testing.T) {
-	s := sw.NewPetApi()
+	s := NewPetApi()
 	err := s.UpdatePetWithForm(12830, "golang", "available")
 
 	if err != nil {
