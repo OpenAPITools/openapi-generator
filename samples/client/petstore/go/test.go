85d30191dfae63699ab846445c3c9eb8844dbe24
Guo Huang <guohuang@gmail.com>
2016-04-16 22:22:23 -0700

    fixed go build issue after removing swagger folder

--- samples/client/petstore/go/test.go
+++ samples/client/petstore/go/test.go
@@ -1,17 +1,16 @@
-package main
+package swagger
 
 import (
-	sw "./swagger"
 	"encoding/json"
 	"fmt"
 )
 
 func main() {
 
-	s := sw.NewPetApi()
+	s := NewPetApi()
 
 	// test POST(body)
-	newPet := (sw.Pet{Id: 12830, Name: "gopher",
+	newPet := (Pet{Id: 12830, Name: "gopher",
 		PhotoUrls: []string{"http://1.com", "http://2.com"}, Status: "pending"})
 
 	jsonNewPet, _ := json.Marshal(newPet)
