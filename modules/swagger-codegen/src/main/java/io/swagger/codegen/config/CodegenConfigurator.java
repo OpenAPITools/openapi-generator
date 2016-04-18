89e150cfb295584bf80fc372fd29904e5c87d135
wing328 <wing328hk@gmail.com>
2016-04-18 16:55:34 +0800

    remove unused import

--- modules/swagger-codegen/src/main/java/io/swagger/codegen/config/CodegenConfigurator.java
+++ modules/swagger-codegen/src/main/java/io/swagger/codegen/config/CodegenConfigurator.java
@@ -19,7 +19,6 @@ import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.HashSet;
