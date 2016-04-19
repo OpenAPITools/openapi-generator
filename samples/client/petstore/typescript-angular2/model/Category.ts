16b749717f9c1aa83ab632964b79a5b57d2a6197
Kristof Vrolijkx <kristof.vrolijkx@aca-it.be>
2016-04-19 14:13:16 +0200

    #1809 adding samples with .sh and .bat scripts

---
+++ samples/client/petstore/typescript-angular2/model/Category.ts
@@ -0,0 +1,12 @@
+'use strict';
+import * as model from "./model.d.ts"
+
+export interface Category {
+
+
+    id?: number;
+
+
+    name?: string;
+}
+
---
+++ samples/client/petstore/typescript-angular2/model/model.d.ts
@@ -0,0 +1,13 @@
+export * from './Category';
+export * from './Order';
+export * from './Pet';
+export * from './Tag';
+export * from './User';
+
+
+
+export * from '../api/PetApi';
+export * from '../api/StoreApi';
+export * from '../api/UserApi';
+
+
