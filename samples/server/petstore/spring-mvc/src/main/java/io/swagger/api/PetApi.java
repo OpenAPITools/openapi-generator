3905de4630d7497c4db77dd48f75854c98e922ab
wing328 <wing328hk@gmail.com>
2016-04-22 17:55:02 +0800

    regenerate petstore sample for springmv

--- samples/server/petstore/spring-mvc/src/main/java/io/swagger/api/PetApi.java
+++ samples/server/petstore/spring-mvc/src/main/java/io/swagger/api/PetApi.java
@@ -32,7 +32,7 @@ import static org.springframework.http.MediaType.*;
 @Controller
 @RequestMapping(value = "/pet", produces = {APPLICATION_JSON_VALUE})
 @Api(value = "/pet", description = "the pet API")
-@javax.annotation.Generated(value = "class io.swagger.codegen.languages.SpringMVCServerCodegen", date = "2016-04-22T17:03:40.798+08:00")
+@javax.annotation.Generated(value = "class io.swagger.codegen.languages.SpringMVCServerCodegen", date = "2016-04-22T17:54:58.453+08:00")
 public class PetApi {
 
   @ApiOperation(value = "Add a new pet to the store", notes = "", response = Void.class, authorizations = {
