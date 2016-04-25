28f444e80885f4cf6a5545d2cfe5b5ba19902c0e
Kristof Vrolijkx <kristof.vrolijkx@aca-it.be>
2016-04-25 08:08:19 +0200

    start adding packaging info to nodejs client.

--- modules/swagger-codegen/src/main/java/io/swagger/codegen/languages/TypeScriptNodeClientCodegen.java
+++ modules/swagger-codegen/src/main/java/io/swagger/codegen/languages/TypeScriptNodeClientCodegen.java
@@ -1,8 +1,26 @@
 package io.swagger.codegen.languages;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import io.swagger.codegen.SupportingFile;
 
 public class TypeScriptNodeClientCodegen extends AbstractTypeScriptClientCodegen {
+    private static final Logger LOGGER = LoggerFactory.getLogger(TypeScriptNodeClientCodegen.class);
+    private static final SimpleDateFormat SNAPSHOT_SUFFIX_FORMAT = new SimpleDateFormat("yyyyMMddHHmm");
+
+    public static final String NPM_NAME = "npmName";
+    public static final String NPM_VERSION = "npmVersion";
+    public static final String NPM_REPOSITORY = "npmRepository";
+    public static final String SNAPSHOT = "snapshot";
+
+    protected String npmName = null;
+    protected String npmVersion = "1.0.0";
+    protected String npmRepository = null;
 
     @Override
     public String getName() {
@@ -14,18 +32,71 @@ public class TypeScriptNodeClientCodegen extends AbstractTypeScriptClientCodegen
         return "Generates a TypeScript nodejs client library.";
     }
 
-	@Override
+    @Override
     public void processOpts() {
         super.processOpts();
         supportingFiles.add(new SupportingFile("api.mustache", null, "api.ts"));
         supportingFiles.add(new SupportingFile("git_push.sh.mustache", "", "git_push.sh"));
         //supportingFiles.add(new SupportingFile("gitignore.mustache", "", ".gitignore"));
+
+        LOGGER.warn("check additionals: " + additionalProperties.get(NPM_NAME));
+        if(additionalProperties.containsKey(NPM_NAME)) {
+            addNpmPackageGeneration();
+        }
+    }
+
+    private void addNpmPackageGeneration() {
+        if(additionalProperties.containsKey(NPM_NAME)) {
+            this.setNpmName(additionalProperties.get(NPM_NAME).toString());
+        }
+
+        if (additionalProperties.containsKey(NPM_VERSION)) {
+            this.setNpmVersion(additionalProperties.get(NPM_VERSION).toString());
+        }
+
+        if (additionalProperties.containsKey(SNAPSHOT) && Boolean.valueOf(additionalProperties.get(SNAPSHOT).toString())) {
+            this.setNpmVersion(npmVersion + "-SNAPSHOT." + SNAPSHOT_SUFFIX_FORMAT.format(new Date()));
+        }
+        additionalProperties.put(NPM_VERSION, npmVersion);
+
+        if (additionalProperties.containsKey(NPM_REPOSITORY)) {
+            this.setNpmRepository(additionalProperties.get(NPM_REPOSITORY).toString());
+        }
+
+        //Files for building our lib
+        supportingFiles.add(new SupportingFile("package.mustache", getPackageRootDirectory(), "package.json"));
+        supportingFiles.add(new SupportingFile("typings.mustache", getPackageRootDirectory(), "typings.json"));
+        supportingFiles.add(new SupportingFile("tsconfig.mustache", getPackageRootDirectory(), "tsconfig.json"));
+    }
+
+    private String getPackageRootDirectory() {
+        String indexPackage = modelPackage.substring(0, Math.max(0, modelPackage.lastIndexOf('.')));
+        return indexPackage.replace('.', File.separatorChar);
     }
     
     public TypeScriptNodeClientCodegen() {
         super();
         outputFolder = "generated-code/typescript-node";
-        embeddedTemplateDir = templateDir = "TypeScript-node";
+        embeddedTemplateDir = templateDir = "typescript-node";
     }
 
+    public void setNpmName(String npmName) {
+        this.npmName = npmName;
+    }
+
+    public void setNpmVersion(String npmVersion) {
+        this.npmVersion = npmVersion;
+    }
+
+    public String getNpmVersion() {
+        return npmVersion;
+    }
+
+    public String getNpmRepository() {
+        return npmRepository;
+    }
+
+    public void setNpmRepository(String npmRepository) {
+        this.npmRepository = npmRepository;
+    }
 }
