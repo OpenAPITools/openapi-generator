63701659128e3f1ef6e241e2af7250ce75378a2b
wing328 <wing328hk@gmail.com>
2016-04-28 16:13:44 +0800

    fix retrofit 1, 2 java sample

--- samples/client/petstore/java/retrofit/src/main/java/io/swagger/client/StringUtil.java
+++ samples/client/petstore/java/retrofit/src/main/java/io/swagger/client/StringUtil.java
@@ -1,6 +1,6 @@
 package io.swagger.client;
 
-@javax.annotation.Generated(value = "class io.swagger.codegen.languages.JavaClientCodegen", date = "2016-04-28T15:51:06.629+08:00")
+@javax.annotation.Generated(value = "class io.swagger.codegen.languages.JavaClientCodegen", date = "2016-04-28T16:10:49.444+08:00")
 public class StringUtil {
   /**
    * Check if the given array contains the given value (with case-insensitive comparison).
