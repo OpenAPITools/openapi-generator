bb80188d6e7d1a3ce1ec92b212160408bf127b16
Newell Zhu <zlx.star@gmail.com>
2016-05-15 00:19:51 +0800

    Add Rails5 sample

---
+++ samples/server/petstore/rails5/app/controllers/user_controllers.rb
@@ -0,0 +1,50 @@
+class UserController < ApplicationController
+
+  def create
+    # Your code here
+
+    render json: {"message" => "yes, it worked"}
+  end
+
+  def create_users_with_array_input
+    # Your code here
+
+    render json: {"message" => "yes, it worked"}
+  end
+
+  def create_users_with_list_input
+    # Your code here
+
+    render json: {"message" => "yes, it worked"}
+  end
+
+  def destroy
+    # Your code here
+
+    render json: {"message" => "yes, it worked"}
+  end
+
+  def show
+    # Your code here
+
+    render json: {"message" => "yes, it worked"}
+  end
+
+  def login_user
+    # Your code here
+
+    render json: {"message" => "yes, it worked"}
+  end
+
+  def logout_user
+    # Your code here
+
+    render json: {"message" => "yes, it worked"}
+  end
+
+  def update
+    # Your code here
+
+    render json: {"message" => "yes, it worked"}
+  end
+end
