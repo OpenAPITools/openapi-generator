02ac9e5c706d7b6405b9dc51e15b5ff6469818b6
cbornet <cbornet@hotmail.com>
2016-05-17 10:35:37 +0200

    fix wrong method call

--- samples/client/petstore/java/retrofit2/src/main/java/io/swagger/client/StringUtil.java
+++ samples/client/petstore/java/retrofit2/src/main/java/io/swagger/client/StringUtil.java
@@ -1,6 +1,6 @@
 package io.swagger.client;
 
-@javax.annotation.Generated(value = "class io.swagger.codegen.languages.JavaClientCodegen", date = "2016-05-17T10:01:17.552+02:00")
+@javax.annotation.Generated(value = "class io.swagger.codegen.languages.JavaClientCodegen", date = "2016-05-17T10:34:26.353+02:00")
 public class StringUtil {
   /**
    * Check if the given array contains the given value (with case-insensitive comparison).
