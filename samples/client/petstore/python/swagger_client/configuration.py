e6b224fbc856f4fa6463b6f9e90d94298ff9c8da
Scott Williams <scott.rw93@gmail.com>
2016-05-25 22:35:38 +0100

    Put license header in shared file

--- samples/client/petstore/python/swagger_client/configuration.py
+++ samples/client/petstore/python/swagger_client/configuration.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 
 """
-Copyright 2016 SmartBear Software
+   Copyright 2016 SmartBear Software
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
@@ -14,8 +14,6 @@ Copyright 2016 SmartBear Software
    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
    See the License for the specific language governing permissions and
    limitations under the License.
-
-   ref: https://github.com/swagger-api/swagger-codegen
 """
 
 from __future__ import absolute_import
