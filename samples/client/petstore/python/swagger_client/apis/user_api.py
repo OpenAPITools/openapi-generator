e0dbb44b37a9a2d085bbb25a5e349dca4347c467
Scott Williams <scott.rw93@gmail.com>
2016-05-25 22:35:38 +0100

    Put license header in shared file

--- samples/client/petstore/python/swagger_client/apis/user_api.py
+++ samples/client/petstore/python/swagger_client/apis/user_api.py
@@ -1,8 +1,7 @@
 # coding: utf-8
 
 """
-UserApi.py
-Copyright 2016 SmartBear Software
+   Copyright 2016 SmartBear Software
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
