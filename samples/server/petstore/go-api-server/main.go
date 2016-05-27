58e07203f36a2d97ba3f0ee60bb20400ea03e4d6
Guo Huang <guohuang@gmail.com>
2016-05-26 22:49:13 -0700

    fixed spacing issue

--- samples/server/petstore/go-api-server/main.go
+++ samples/server/petstore/go-api-server/main.go
@@ -2,14 +2,14 @@ package main
 
 import (
 	sw "./go"
-    "log"
-    "net/http"
+	"log"
+	"net/http"
 )
 
 func main() {
 	log.Printf("Server started")
 
-    router := sw.NewRouter()
-    
-    log.Fatal(http.ListenAndServe(":8080", router))
+	router := sw.NewRouter()
+	
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
