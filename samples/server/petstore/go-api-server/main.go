2d1dafb4732164c6de642718c6e68ee5aa9f3707
Guo Huang <guohuang@gmail.com>
2016-05-26 22:34:18 -0700

    issue#2970, [Go] add go server codeine template

---
+++ samples/server/petstore/go-api-server/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	sw "./go"
+    "log"
+    "net/http"
+)
+
+func main() {
+	log.Printf("Server started")
+
+    router := sw.NewRouter()
+    
+    log.Fatal(http.ListenAndServe(":8080", router))
+}
