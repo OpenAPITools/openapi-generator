73ab94dfbff0bb908f95a486346d3abaf234291e
wing328 <wing328hk@gmail.com>
2016-05-31 22:22:54 +0800

    fix sbt build for okhttp-gson

--- samples/client/petstore/java/okhttp-gson/src/main/java/io/swagger/client/ApiException.java
+++ samples/client/petstore/java/okhttp-gson/src/main/java/io/swagger/client/ApiException.java
@@ -3,7 +3,7 @@ package io.swagger.client;
 import java.util.Map;
 import java.util.List;
 
-@javax.annotation.Generated(value = "class io.swagger.codegen.languages.JavaClientCodegen", date = "2016-04-28T16:09:59.809+08:00")
+@javax.annotation.Generated(value = "class io.swagger.codegen.languages.JavaClientCodegen", date = "2016-05-31T22:17:12.179+08:00")
 public class ApiException extends Exception {
   private int code = 0;
   private Map<String, List<String>> responseHeaders = null;
