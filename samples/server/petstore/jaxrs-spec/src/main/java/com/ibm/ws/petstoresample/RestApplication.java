956ea19046c1db80befa3f9c0468555e17b741c8
Artur Dzmitryieu <dzmitry@ca.ibm.com>
2016-06-06 15:03:44 -0400

    Update a Readme and add sample output for petstore json

---
+++ samples/server/petstore/jaxrs-spec/src/main/java/com/ibm/ws/petstoresample/RestApplication.java
@@ -0,0 +1,9 @@
+package com.ibm.ws.petstoresample;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class RestApplication extends Application {
+	
+}
