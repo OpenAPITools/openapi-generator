2d170fd7a6afd36e9b55986e183e3251bc0d7ca8
wing328 <wing328hk@gmail.com>
2016-06-09 16:07:36 +0800

    add license to

--- samples/client/petstore/java/okhttp-gson/src/main/java/io/swagger/client/auth/OAuth.java
+++ samples/client/petstore/java/okhttp-gson/src/main/java/io/swagger/client/auth/OAuth.java
@@ -5,7 +5,7 @@ import io.swagger.client.Pair;
 import java.util.Map;
 import java.util.List;
 
-@javax.annotation.Generated(value = "class io.swagger.codegen.languages.JavaClientCodegen", date = "2016-06-09T00:01:22.559+08:00")
+
 public class OAuth implements Authentication {
   private String accessToken;
 
