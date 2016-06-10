fe757e8371b214cbb749640071706753153f176d
wing328 <wing328hk@gmail.com>
2016-06-10 17:01:27 +0800

    add message about rmeoving files/folders under output folder

--- bin/springboot-petstore-server.sh
+++ bin/springboot-petstore-server.sh
@@ -28,6 +28,7 @@ fi
 export JAVA_OPTS="${JAVA_OPTS} -XX:MaxPermSize=256M -Xmx1024M -DloggerPath=conf/log4j.properties"
 ags="$@ generate -t modules/swagger-codegen/src/main/resources/JavaSpringBoot -i modules/swagger-codegen/src/test/resources/2_0/petstore.yaml -l springboot -o samples/server/petstore/springboot -DdateLibrary=joda,hideGenerationTimestamp=true"
 
+echo "Removing files and folders under samples/server/petstore/springboot/src/main"
 rm -rf samples/server/petstore/springboot/src/main
 find samples/server/petstore/springboot -maxdepth 1 -type f ! -name "README.md" -exec rm {} +
 java $JAVA_OPTS -jar $executable $ags
