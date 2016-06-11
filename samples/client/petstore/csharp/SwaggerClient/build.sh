bc04ebed94ea8036cb3bef86835dc482a57e5778
wing328 <wing328hk@gmail.com>
2016-06-11 23:44:48 +0800

    add mono script to nunit test csharp client

--- samples/client/petstore/csharp/SwaggerClient/build.sh
+++ samples/client/petstore/csharp/SwaggerClient/build.sh
@@ -2,14 +2,19 @@
 frameworkVersion=net45
 netfx=${frameworkVersion#net}
 
+echo "[INFO] Target framework: ${frameworkVersion}"
+
+echo "[INFO] Download nuget and packages"
 wget -nc https://nuget.org/nuget.exe;
 mozroots --import --sync
 mono nuget.exe install src/IO.Swagger/packages.config -o packages;
 mkdir -p bin;
 
+echo "[INFO] Copy DLLs to the 'bin' folder"
 cp packages/Newtonsoft.Json.8.0.2/lib/net45/Newtonsoft.Json.dll bin/Newtonsoft.Json.dll;
 cp packages/RestSharp.105.1.0/lib/net45/RestSharp.dll bin/RestSharp.dll;
 
+echo "[INFO] Run 'mcs' to build bin/IO.Swagger.dll"
 mcs -sdk:${netfx} -r:bin/Newtonsoft.Json.dll,\
 bin/RestSharp.dll,\
 System.Runtime.Serialization.dll \
