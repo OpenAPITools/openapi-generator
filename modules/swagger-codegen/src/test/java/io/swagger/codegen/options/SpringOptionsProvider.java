027ff93ae46415c6d3c478d27a2e64d762a0a4c0
cbornet <cbornet@hotmail.com>
2016-06-14 17:37:33 +0200

    rename and remove dead code

--- modules/swagger-codegen/src/test/java/io/swagger/codegen/options/SpringOptionsProvider.java
+++ modules/swagger-codegen/src/test/java/io/swagger/codegen/options/SpringOptionsProvider.java
@@ -1,15 +1,15 @@
 package io.swagger.codegen.options;
 
 import io.swagger.codegen.CodegenConstants;
-import io.swagger.codegen.languages.SpringBootServerCodegen;
+import io.swagger.codegen.languages.SpringCodegen;
 
 import java.util.HashMap;
 import java.util.Map;
 
-public class SpringBootServerOptionsProvider extends JavaOptionsProvider {
+public class SpringOptionsProvider extends JavaOptionsProvider {
     public static final String CONFIG_PACKAGE_VALUE = "configPackage";
     public static final String BASE_PACKAGE_VALUE = "basePackage";
-    public static final String LIBRARY_VALUE = "j8-async"; //FIXME hidding value from super class
+    public static final String LIBRARY_VALUE = "spring-mvc"; //FIXME hidding value from super class
     public static final String INTERFACE_ONLY = "true";
     public static final String SINGLE_CONTENT_TYPES = "true";
     public static final String JAVA_8 = "true";
@@ -17,19 +17,19 @@ public class SpringBootServerOptionsProvider extends JavaOptionsProvider {
 
     @Override
     public String getLanguage() {
-        return "springboot";
+        return "spring";
     }
 
     @Override
     public Map<String, String> createOptions() {
         Map<String, String> options = new HashMap<String, String>(super.createOptions());
-        options.put(SpringBootServerCodegen.CONFIG_PACKAGE, CONFIG_PACKAGE_VALUE);
-        options.put(SpringBootServerCodegen.BASE_PACKAGE, BASE_PACKAGE_VALUE);
+        options.put(SpringCodegen.CONFIG_PACKAGE, CONFIG_PACKAGE_VALUE);
+        options.put(SpringCodegen.BASE_PACKAGE, BASE_PACKAGE_VALUE);
         options.put(CodegenConstants.LIBRARY, LIBRARY_VALUE);
-        options.put(SpringBootServerCodegen.INTERFACE_ONLY, INTERFACE_ONLY);
-        options.put(SpringBootServerCodegen.SINGLE_CONTENT_TYPES, SINGLE_CONTENT_TYPES);
-        options.put(SpringBootServerCodegen.JAVA_8, JAVA_8);
-        options.put(SpringBootServerCodegen.ASYNC, ASYNC);
+        options.put(SpringCodegen.INTERFACE_ONLY, INTERFACE_ONLY);
+        options.put(SpringCodegen.SINGLE_CONTENT_TYPES, SINGLE_CONTENT_TYPES);
+        options.put(SpringCodegen.JAVA_8, JAVA_8);
+        options.put(SpringCodegen.ASYNC, ASYNC);
 
         return options;
     }
