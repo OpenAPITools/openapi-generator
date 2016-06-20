8916b000218d98d2770c9c26f4974515d9280170
Andreas Reiter <andreas.reiter@iaik.tugraz.at>
2016-06-20 13:25:54 +0200

    Updated dart petstore

--- samples/client/petstore/dart/lib/model/user.dart
+++ samples/client/petstore/dart/lib/model/user.dart
@@ -4,27 +4,35 @@ part of api;
 @Entity()
 class User {
   
+  @Property(name: 'id')
   int id = null;
   
 
+  @Property(name: 'username')
   String username = null;
   
 
+  @Property(name: 'firstName')
   String firstName = null;
   
 
+  @Property(name: 'lastName')
   String lastName = null;
   
 
+  @Property(name: 'email')
   String email = null;
   
 
+  @Property(name: 'password')
   String password = null;
   
 
+  @Property(name: 'phone')
   String phone = null;
   
 /* User Status */
+  @Property(name: 'userStatus')
   int userStatus = null;
   
   User();
