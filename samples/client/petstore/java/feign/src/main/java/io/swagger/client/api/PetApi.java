a6ca021337a1c4e877c3721f6b53f75e56293d76
wing328 <wing328hk@gmail.com>
2016-06-20 14:51:17 +0800

    fix java property with the name "class"

--- samples/client/petstore/java/feign/src/main/java/io/swagger/client/api/PetApi.java
+++ samples/client/petstore/java/feign/src/main/java/io/swagger/client/api/PetApi.java
@@ -3,8 +3,8 @@ package io.swagger.client.api;
 import io.swagger.client.ApiClient;
 
 import io.swagger.client.model.Pet;
-import io.swagger.client.model.ModelApiResponse;
 import java.io.File;
+import io.swagger.client.model.ModelApiResponse;
 
 import java.util.ArrayList;
 import java.util.HashMap;
