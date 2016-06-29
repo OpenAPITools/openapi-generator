9ea2eb4a2fa39f7295285fa1d2388121943a6c85
Chad Rosen <chadrosen@gmail.com>
2016-06-29 13:19:37 -0700

    update ruby samples by running the bin/ruby-petstore.sh

--- samples/client/petstore/ruby/lib/petstore/models/read_only_first.rb
+++ samples/client/petstore/ruby/lib/petstore/models/read_only_first.rb
@@ -30,6 +30,7 @@ module Petstore
 
     attr_accessor :baz
 
+
     # Attribute mapping from ruby-style variable name to JSON key.
     def self.attribute_map
       {
@@ -74,6 +75,7 @@ module Petstore
     # Check to see if the all the properties in the model are valid
     # @return true if the model is valid
     def valid?
+      return true
     end
 
     # Checks equality by comparing each attribute.
