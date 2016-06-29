1916f80c56a1dff9e122ae9458c7153c42930891
wing328 <wing328hk@gmail.com>
2016-06-29 21:47:41 +0800

    remove node_modeuls under petstore security test for js

--- samples/client/petstore-security-test/javascript/node_modules/mocha/lib/browser/debug.js
+++
@@ -1,4 +0,0 @@
-/* eslint-disable no-unused-vars */
-module.exports = function(type) {
-  return function() {};
-};
--- samples/client/petstore-security-test/javascript/node_modules/sinon/pkg/sinon-1.10.0.js
+++
@@ -1,4 +0,0 @@
-this.sinon = (function () {
-var samsam, formatio;
-function define(mod, deps, fn) { if (mod == "samsam") { samsam = deps(); } else if (typeof fn === "function") { formatio = fn(samsam); } }
-define.amd = {};
