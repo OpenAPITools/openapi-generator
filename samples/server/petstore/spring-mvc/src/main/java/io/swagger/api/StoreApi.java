e1fead8ee576cd0ee97debb25f02bcd3f650b3df
wing328 <wing328hk@gmail.com>
2016-07-07 11:32:05 +0800

    update spring petstore sample

--- samples/server/petstore/spring-mvc/src/main/java/io/swagger/api/StoreApi.java
+++ samples/server/petstore/spring-mvc/src/main/java/io/swagger/api/StoreApi.java
@@ -20,7 +20,7 @@ import java.util.List;
 @Api(value = "store", description = "the store API")
 public interface StoreApi {
 
-    @ApiOperation(value = "Delete purchase order by ID", notes = "For valid response try integer IDs with value < 1000. Anything above 1000 or nonintegers will generate API errors", response = Void.class)
+    @ApiOperation(value = "Delete purchase order by ID", notes = "For valid response try integer IDs with value < 1000. Anything above 1000 or nonintegers will generate API errors", response = Void.class, tags={ "store", })
     @ApiResponses(value = { 
         @ApiResponse(code = 400, message = "Invalid ID supplied", response = Void.class),
         @ApiResponse(code = 404, message = "Order not found", response = Void.class) })
@@ -32,7 +32,7 @@ public interface StoreApi {
 
     @ApiOperation(value = "Returns pet inventories by status", notes = "Returns a map of status codes to quantities", response = Integer.class, responseContainer = "Map", authorizations = {
         @Authorization(value = "api_key")
-    })
+    }, tags={ "store", })
     @ApiResponses(value = { 
         @ApiResponse(code = 200, message = "successful operation", response = Integer.class) })
     @RequestMapping(value = "/store/inventory",
@@ -41,7 +41,7 @@ public interface StoreApi {
     ResponseEntity<Map<String, Integer>> getInventory();
 
 
-    @ApiOperation(value = "Find purchase order by ID", notes = "For valid response try integer IDs with value <= 5 or > 10. Other values will generated exceptions", response = Order.class)
+    @ApiOperation(value = "Find purchase order by ID", notes = "For valid response try integer IDs with value <= 5 or > 10. Other values will generated exceptions", response = Order.class, tags={ "store", })
     @ApiResponses(value = { 
         @ApiResponse(code = 200, message = "successful operation", response = Order.class),
         @ApiResponse(code = 400, message = "Invalid ID supplied", response = Order.class),
@@ -52,7 +52,7 @@ public interface StoreApi {
     ResponseEntity<Order> getOrderById(@ApiParam(value = "ID of pet that needs to be fetched",required=true ) @PathVariable("orderId") Long orderId);
 
 
-    @ApiOperation(value = "Place an order for a pet", notes = "", response = Order.class)
+    @ApiOperation(value = "Place an order for a pet", notes = "", response = Order.class, tags={ "store", })
     @ApiResponses(value = { 
         @ApiResponse(code = 200, message = "successful operation", response = Order.class),
         @ApiResponse(code = 400, message = "Invalid Order", response = Order.class) })
