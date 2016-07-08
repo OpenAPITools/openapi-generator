ea038492b0af76efb6fb7a129b3b206bad962da0
Christian Loitsch <christian.loitsch@inetcompany.com>
2016-07-08 15:45:53 +0200

    doc: explain how to run tests

--- bin/dart-petstore.sh
+++ bin/dart-petstore.sh
@@ -28,4 +28,7 @@ fi
 export JAVA_OPTS="${JAVA_OPTS} -XX:MaxPermSize=256M -Xmx1024M -DloggerPath=conf/log4j.properties"
 ags="$@ generate -i modules/swagger-codegen/src/test/resources/2_0/petstore.yaml -l dart -o samples/client/petstore/dart"
 
+# for dart vm lib generation:
+#ags="$@ generate -i modules/swagger-codegen/src/test/resources/2_0/petstore.yaml -l dart -o samples/client/petstore/dart --additional-properties browserClient=false"
+
 java $JAVA_OPTS -jar $executable $ags
