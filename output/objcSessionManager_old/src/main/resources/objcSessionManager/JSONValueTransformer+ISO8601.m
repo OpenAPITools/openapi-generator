d617320324d5d56c2bb812fced4eb6aaddef6a55
Wolfgang Berger <w.berger@diamonddogs.cc>
2016-07-15 15:36:24 +0200

    integrate latest objc changes into objc-sessionmanager

---
+++ output/objcSessionManager_old/src/main/resources/objcSessionManager/JSONValueTransformer+ISO8601.m
@@ -0,0 +1,11 @@
+#import <ISO8601/NSDate+ISO8601.h>
+#import "JSONValueTransformer+ISO8601.h"
+
+@implementation JSONValueTransformer (ISO8601)
+
+- (NSDate *) NSDateFromNSString:(NSString *)string
+{
+    return [NSDate dateWithISO8601String:string];
+}
+
+@end
