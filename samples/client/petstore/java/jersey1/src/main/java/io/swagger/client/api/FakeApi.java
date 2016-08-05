d1f328459a011aaee6f041122d3c108b48412676
wing328 <wing328hk@gmail.com>
2016-08-05 11:49:34 +0800

    add test cases for jersey1 enum

--- samples/client/petstore/java/jersey1/src/main/java/io/swagger/client/api/FakeApi.java
+++ samples/client/petstore/java/jersey1/src/main/java/io/swagger/client/api/FakeApi.java
@@ -34,8 +34,8 @@ import io.swagger.client.Pair;
 
 import io.swagger.client.model.Client;
 import org.joda.time.LocalDate;
-import java.math.BigDecimal;
 import org.joda.time.DateTime;
+import java.math.BigDecimal;
 
 
 import java.util.ArrayList;
