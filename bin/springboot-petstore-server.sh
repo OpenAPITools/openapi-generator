d626200479d112cd605e07478aa70d9866a64554
wing328 <wing328hk@gmail.com>
2016-08-09 19:49:38 +0800

    fix form enum parameters for spring generator, add more test cases

--- bin/springboot-petstore-server.sh
+++ bin/springboot-petstore-server.sh
@@ -26,7 +26,7 @@ fi
 
 # if you've executed sbt assembly previously it will use that instead.
 export JAVA_OPTS="${JAVA_OPTS} -XX:MaxPermSize=256M -Xmx1024M -DloggerPath=conf/log4j.properties"
-ags="$@ generate -t modules/swagger-codegen/src/main/resources/JavaSpring -i modules/swagger-codegen/src/test/resources/2_0/petstore.yaml -l spring -o samples/server/petstore/springboot -DhideGenerationTimestamp=true"
+ags="$@ generate -t modules/swagger-codegen/src/main/resources/JavaSpring -i modules/swagger-codegen/src/test/resources/2_0/petstore-with-fake-endpoints-models-for-testing.yaml -l spring -o samples/server/petstore/springboot -DhideGenerationTimestamp=true"
 
 echo "Removing files and folders under samples/server/petstore/springboot/src/main"
 rm -rf samples/server/petstore/springboot/src/main
