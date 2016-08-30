dd1ed1231884f08804fd5726ab5beabac59d7abd
Daniel Ge <daniel@dge.io>
2016-08-29 19:28:07 -0700

    Make connection failures visible with generated Ruby SDKs (#3640)
    * Make connection failures visible in Ruby SDK
    
    The underlying HTTP library, Typhoeus, requires you to be explicit about
    error handling. Unfortunately, this also means that we can't assume that
    `response.success?` will be false only when the HTTP status code is not
    a 200; it could also be false when the request fails (timeouts, TLS
    verification issues, etc.). This commit adds explicit error handling for
    these cases.
    
    * Update samples

--- samples/client/petstore-security-test/ruby/lib/petstore/api_client.rb
+++ samples/client/petstore-security-test/ruby/lib/petstore/api_client.rb
@@ -66,10 +66,18 @@ module Petstore
       end
 
       unless response.success?
-        fail ApiError.new(:code => response.code,
-                          :response_headers => response.headers,
-                          :response_body => response.body),
-             response.status_message
+        if response.timed_out?
+          fail ApiError.new('Connection timed out')
+        elsif response.code == 0
+          # Errors from libcurl will be made visible here
+          fail ApiError.new(:code => 0,
+                            :message => response.return_message)
+        else
+          fail ApiError.new(:code => response.code,
+                            :response_headers => response.headers,
+                            :response_body => response.body),
+               response.status_message
+        end
       end
 
       if opts[:return_type]
@@ -288,7 +296,7 @@ module Petstore
     # Update hearder and query params based on authentication settings.
     #
     # @param [Hash] header_params Header parameters
-    # @param [Hash] form_params Query parameters
+    # @param [Hash] query_params Query parameters
     # @param [String] auth_names Authentication scheme name
     def update_params_for_auth!(header_params, query_params, auth_names)
       Array(auth_names).each do |auth_name|
