4ba3aa51449360639375ab47cbb58f80c8d60581
Ville Skyttä <ville.skytta@iki.fi>
2016-09-06 14:45:04 +0300

    Spelling fixes

--- samples/client/petstore/php/SwaggerClient-php/test/Model/EnumClassTest.php
+++ samples/client/petstore/php/SwaggerClient-php/test/Model/EnumClassTest.php
@@ -7,7 +7,7 @@
  * @category Class
  * @package  Swagger\Client
  * @author   http://github.com/swagger-api/swagger-codegen
- * @license  http://www.apache.org/licenses/LICENSE-2.0 Apache Licene v2
+ * @license  http://www.apache.org/licenses/LICENSE-2.0 Apache License v2
  * @link     https://github.com/swagger-api/swagger-codegen
  */
 /**
@@ -40,7 +40,7 @@ namespace Swagger\Client\Model;
  * @description EnumClass
  * @package     Swagger\Client
  * @author      http://github.com/swagger-api/swagger-codegen
- * @license     http://www.apache.org/licenses/LICENSE-2.0 Apache Licene v2
+ * @license     http://www.apache.org/licenses/LICENSE-2.0 Apache License v2
  * @link        https://github.com/swagger-api/swagger-codegen
  */
 class EnumClassTest extends \PHPUnit_Framework_TestCase
