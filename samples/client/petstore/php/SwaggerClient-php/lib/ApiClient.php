91b7494e3e7d8b7f2e448d32410c2c85caf00880
Ville Skyttä <ville.skytta@iki.fi>
2016-09-06 14:45:04 +0300

    Spelling fixes

--- samples/client/petstore/php/SwaggerClient-php/lib/ApiClient.php
+++ samples/client/petstore/php/SwaggerClient-php/lib/ApiClient.php
@@ -7,7 +7,7 @@
  * @category Class
  * @package  Swagger\Client
  * @author   http://github.com/swagger-api/swagger-codegen
- * @license  http://www.apache.org/licenses/LICENSE-2.0 Apache Licene v2
+ * @license  http://www.apache.org/licenses/LICENSE-2.0 Apache License v2
  * @link     https://github.com/swagger-api/swagger-codegen
  */
 
@@ -47,7 +47,7 @@ namespace Swagger\Client;
  * @category Class
  * @package  Swagger\Client
  * @author   http://github.com/swagger-api/swagger-codegen
- * @license  http://www.apache.org/licenses/LICENSE-2.0 Apache Licene v2
+ * @license  http://www.apache.org/licenses/LICENSE-2.0 Apache License v2
  * @link     https://github.com/swagger-api/swagger-codegen
  */
 class ApiClient
