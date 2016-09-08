d4d3f51f5df31dc08253c8ee82e6b4329f6c1dd9
Paul Ebermann <paul.ebermann@zalando.de>
2016-09-08 12:53:32 +0200

    Issue #3738: regenerate Scala samples (no code changes).

--- samples/client/petstore/akka-scala/src/main/scala/io/swagger/client/api/PetApi.scala
+++ samples/client/petstore/akka-scala/src/main/scala/io/swagger/client/api/PetApi.scala
@@ -6,8 +6,8 @@
 package io.swagger.client.api
 
 import io.swagger.client.model.Pet
-import io.swagger.client.model.ApiResponse
 import java.io.File
+import io.swagger.client.model.ApiResponse
 import io.swagger.client.core._
 import io.swagger.client.core.CollectionFormats._
 import io.swagger.client.core.ApiKeyLocations._
