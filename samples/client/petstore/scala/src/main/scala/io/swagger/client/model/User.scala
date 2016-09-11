96a65aaa6e8b2aea27c5f16889e2c28daafdbfd6
wing328 <wing328hk@gmail.com>
2016-09-11 18:44:36 +0800

    better format of model properties

--- samples/client/petstore/scala/src/main/scala/io/swagger/client/model/User.scala
+++ samples/client/petstore/scala/src/main/scala/io/swagger/client/model/User.scala
@@ -25,15 +25,14 @@
 package io.swagger.client.model
 
 
-
-
 case class User (
   id: Long,
-username: String,
-firstName: String,
-lastName: String,
-email: String,
-password: String,
-phone: String,
-/* User Status */
-  userStatus: Integer)
+  username: String,
+  firstName: String,
+  lastName: String,
+  email: String,
+  password: String,
+  phone: String,
+  /* User Status */
+  userStatus: Integer
+)
--- samples/server/petstore/scalatra/src/main/scala/com/wordnik/client/model/Order.scala
+++ samples/server/petstore/scalatra/src/main/scala/com/wordnik/client/model/Order.scala
@@ -26,12 +26,12 @@ package com.wordnik.client.model
 
 import java.util.Date
 
-
 case class Order (
   id: Option[Long],
-petId: Option[Long],
-quantity: Option[Int],
-shipDate: Option[Date],
-status: Option[String],  // Order Status
-complete: Option[Boolean]
+  petId: Option[Long],
+  quantity: Option[Int],
+  shipDate: Option[Date],
+  /* Order Status */
+  status: Option[String],
+  complete: Option[Boolean]
 )
