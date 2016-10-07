65f925f45c9a07e99f9b32363b3fd548a11dae88
wing328 <wing328hk@gmail.com>
2016-10-07 15:50:57 +0800

    fix python required property check and validation for optional properties

--- samples/client/petstore/python/petstore_api/models/animal.py
+++ samples/client/petstore/python/petstore_api/models/animal.py
@@ -75,6 +75,8 @@ class Animal(object):
         :param class_name: The class_name of this Animal.
         :type: str
         """
+        if class_name is None:
+            raise ValueError("Invalid value for `class_name`, must not be `None`")
 
         self._class_name = class_name
 
