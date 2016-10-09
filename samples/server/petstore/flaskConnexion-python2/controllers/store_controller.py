fc16e6ec82b32199e956d1dcc164d2731a3f5b34
wing328 <wing328hk@gmail.com>
2016-10-09 14:06:55 +0800

    [Python][Flask] fix python2 support in Flask (#3952)
    * flask python2 petstore sample
    
    * fix python2 support in flask

---
+++ samples/server/petstore/flaskConnexion-python2/controllers/store_controller.py
@@ -0,0 +1,12 @@
+
+def delete_order(orderId):
+    return 'do some magic!'
+
+def get_inventory():
+    return 'do some magic!'
+
+def get_order_by_id(orderId):
+    return 'do some magic!'
+
+def place_order(body):
+    return 'do some magic!'
