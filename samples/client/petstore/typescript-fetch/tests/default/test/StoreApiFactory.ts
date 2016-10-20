c67137879b74bbc385142131e3db3ad09fbd68e1
wing328 <wing328hk@gmail.com>
2016-10-20 18:38:32 +0800

    [Typscript][Fetch] add configuration to support authentication (#4008)
    * add configuration to typescript fetch
    
    * fix issues reported by the CI
    
    * fix header parameters in ts fetch client
    
    * add new configuration file for ts fetch
    
    * skip if check for configuration in ts fetch
    
    * Fix `this.configuration` issue in paramsCreator
    
    * update ts fetch samples
    
    * Fix typescript fetch tests to work with Configuration
    
    * Add dictionary to store apikeys (#8)
    
    * Add dictionary to store apikeys
    
    * Type apikey with expected params rather than Dictionary - typescript-fetch
    
    * Update test samples to new typescript-fetch api
    
    * Fix tests for typesript-fetch api
    
    * update typescript sample using petstore.yaml

--- samples/client/petstore/typescript-fetch/tests/default/test/StoreApiFactory.ts
+++ samples/client/petstore/typescript-fetch/tests/default/test/StoreApiFactory.ts
@@ -1,9 +1,22 @@
 import {expect} from 'chai';
 import {StoreApiFactory} from 'typescript-fetch-api';
+import {Configuration} from 'typescript-fetch-api/dist/configuration';
+
+let config: Configuration;
+
+before(function() {
+  config = new Configuration();
+  config.accessToken = "foobar";
+  config.apiKey = {
+    api_key: "foobar"
+  };
+  config.username = "foo";
+  config.password = "bar";
+});
 
 describe('StoreApiFactory', function() {
   it('should get inventory', function() {
-    return StoreApiFactory().getInventory().then((result) => {
+    return StoreApiFactory().getInventory(config).then((result) => {
       expect(Object.keys(result)).to.not.be.empty;
     });
   });
