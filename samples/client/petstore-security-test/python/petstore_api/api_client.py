fa59424132cef4fd0a787cfcb56c3fea2fa19c91
Cliffano Subagio <cliffano@gmail.com>
2016-11-01 21:21:53 +1100

    [python] Add packageUrl option, replacing infoUrl in setup.py (#4101)
    * [python] Add packageUrl option, replacing infoUrl in setup.py .
    
    * [python] Regenerate Python petstore sample.
    
    * [python] Regenerate Python petstore security test sample.

--- samples/client/petstore-security-test/python/petstore_api/api_client.py
+++ samples/client/petstore-security-test/python/petstore_api/api_client.py
@@ -19,17 +19,15 @@ Copyright 2016 SmartBear Software
 """
 
 from __future__ import absolute_import
+
 from . import models
 from .rest import RESTClientObject
 from .rest import ApiException
 
 import os
 import re
-import sys
-import urllib
 import json
 import mimetypes
-import random
 import tempfile
 import threading
 
@@ -37,14 +35,8 @@ from datetime import datetime
 from datetime import date
 
 # python 2 and python 3 compatibility library
-from six import iteritems
-
-try:
-    # for python3
-    from urllib.parse import quote
-except ImportError:
-    # for python2
-    from urllib import quote
+from six import PY3, integer_types, iteritems, text_type
+from six.moves.urllib.parse import quote
 
 from .configuration import Configuration
 
@@ -103,34 +95,40 @@ class ApiClient(object):
     def __call_api(self, resource_path, method,
                    path_params=None, query_params=None, header_params=None,
                    body=None, post_params=None, files=None,
-                   response_type=None, auth_settings=None, callback=None, _return_http_data_only=None):
+                   response_type=None, auth_settings=None, callback=None,
+                   _return_http_data_only=None, collection_formats=None):
 
-        # headers parameters
+        # header parameters
         header_params = header_params or {}
         header_params.update(self.default_headers)
         if self.cookie:
             header_params['Cookie'] = self.cookie
         if header_params:
             header_params = self.sanitize_for_serialization(header_params)
+            header_params = dict(self.parameters_to_tuples(header_params,
+                                                           collection_formats))
 
         # path parameters
         if path_params:
             path_params = self.sanitize_for_serialization(path_params)
-            for k, v in iteritems(path_params):
-                replacement = quote(str(self.to_path_value(v)))
-                resource_path = resource_path.\
-                    replace('{' + k + '}', replacement)
+            path_params = self.parameters_to_tuples(path_params,
+                                                    collection_formats)
+            for k, v in path_params:
+                resource_path = resource_path.replace(
+                    '{%s}' % k, quote(str(v)))
 
         # query parameters
         if query_params:
             query_params = self.sanitize_for_serialization(query_params)
-            query_params = {k: self.to_path_value(v)
-                            for k, v in iteritems(query_params)}
+            query_params = self.parameters_to_tuples(query_params,
+                                                     collection_formats)
 
         # post parameters
         if post_params or files:
             post_params = self.prepare_post_parameters(post_params, files)
             post_params = self.sanitize_for_serialization(post_params)
+            post_params = self.parameters_to_tuples(post_params,
+                                                    collection_formats)
 
         # auth setting
         self.update_params_for_auth(header_params, query_params, auth_settings)
@@ -159,31 +157,16 @@ class ApiClient(object):
         if callback:
             callback(deserialized_data) if _return_http_data_only else callback((deserialized_data, response_data.status, response_data.getheaders()))
         elif _return_http_data_only:
-            return ( deserialized_data );
+            return (deserialized_data)
         else:
             return (deserialized_data, response_data.status, response_data.getheaders())
-        
-
-    def to_path_value(self, obj):
-        """
-        Takes value and turn it into a string suitable for inclusion in
-        the path, by url-encoding.
-
-        :param obj: object or string value.
-
-        :return string: quoted value.
-        """
-        if type(obj) == list:
-            return ','.join(obj)
-        else:
-            return str(obj)
 
     def sanitize_for_serialization(self, obj):
         """
         Builds a JSON POST object.
 
         If obj is None, return None.
-        If obj is str, int, float, bool, return directly.
+        If obj is str, int, long, float, bool, return directly.
         If obj is datetime.datetime, datetime.date
             convert to string in iso8601 format.
         If obj is list, sanitize each element in the list.
@@ -193,9 +176,7 @@ class ApiClient(object):
         :param obj: The data to serialize.
         :return: The serialized form of data.
         """
-        types = (str, int, float, bool, tuple)
-        if sys.version_info < (3, 0):
-            types = types + (unicode,)
+        types = (str, float, bool, bytes) + tuple(integer_types) + (text_type,)
         if isinstance(obj, type(None)):
             return None
         elif isinstance(obj, types):
@@ -203,6 +184,9 @@ class ApiClient(object):
         elif isinstance(obj, list):
             return [self.sanitize_for_serialization(sub_obj)
                     for sub_obj in obj]
+        elif isinstance(obj, tuple):
+            return tuple(self.sanitize_for_serialization(sub_obj)
+                for sub_obj in obj)
         elif isinstance(obj, (datetime, date)):
             return obj.isoformat()
         else:
@@ -227,7 +211,7 @@ class ApiClient(object):
 
         :param response: RESTResponse object to be deserialized.
         :param response_type: class literal for
-            deserialzied object, or string of class name.
+            deserialized object, or string of class name.
 
         :return: deserialized object.
         """
@@ -272,11 +256,13 @@ class ApiClient(object):
             if klass in ['int', 'float', 'str', 'bool',
                          "date", 'datetime', "object"]:
                 klass = eval(klass)
+            elif klass == 'long':
+                klass = int if PY3 else long
             # for model types
             else:
                 klass = eval('models.' + klass)
 
-        if klass in [int, float, str, bool]:
+        if klass in integer_types or klass in (float, str, bool):
             return self.__deserialize_primitive(data, klass)
         elif klass == object:
             return self.__deserialize_object(data)
@@ -290,7 +276,8 @@ class ApiClient(object):
     def call_api(self, resource_path, method,
                  path_params=None, query_params=None, header_params=None,
                  body=None, post_params=None, files=None,
-                 response_type=None, auth_settings=None, callback=None, _return_http_data_only=None):
+                 response_type=None, auth_settings=None, callback=None,
+                 _return_http_data_only=None, collection_formats=None):
         """
         Makes the HTTP request (synchronous) and return the deserialized data.
         To make an async request, define a function for callback.
@@ -312,6 +299,8 @@ class ApiClient(object):
             If provide this parameter,
             the request will be called asynchronously.
         :param _return_http_data_only: response data without head status code and headers
+        :param collection_formats: dict of collection formats for path, query,
+            header, and post parameters.
         :return:
             If provide parameter callback,
             the request will be called asynchronously.
@@ -323,7 +312,8 @@ class ApiClient(object):
             return self.__call_api(resource_path, method,
                                    path_params, query_params, header_params,
                                    body, post_params, files,
-                                   response_type, auth_settings, callback, _return_http_data_only)
+                                   response_type, auth_settings, callback,
+                                   _return_http_data_only, collection_formats)
         else:
             thread = threading.Thread(target=self.__call_api,
                                       args=(resource_path, method,
@@ -331,7 +321,8 @@ class ApiClient(object):
                                             header_params, body,
                                             post_params, files,
                                             response_type, auth_settings,
-                                            callback,_return_http_data_only))
+                                            callback, _return_http_data_only,
+                                            collection_formats))
         thread.start()
         return thread
 
@@ -379,9 +370,40 @@ class ApiClient(object):
                                            body=body)
         else:
             raise ValueError(
-                "http method must be `GET`, `HEAD`,"
+                "http method must be `GET`, `HEAD`, `OPTIONS`,"
                 " `POST`, `PATCH`, `PUT` or `DELETE`."
             )
+
+    def parameters_to_tuples(self, params, collection_formats):
+        """
+        Get parameters as list of tuples, formatting collections.
+
+        :param params: Parameters as dict or list of two-tuples
+        :param dict collection_formats: Parameter collection formats
+        :return: Parameters as list of tuples, collections formatted
+        """
+        new_params = []
+        if collection_formats is None:
+            collection_formats = {}
+        for k, v in iteritems(params) if isinstance(params, dict) else params:
+            if k in collection_formats:
+                collection_format = collection_formats[k]
+                if collection_format == 'multi':
+                    new_params.extend((k, value) for value in v)
+                else:
+                    if collection_format == 'ssv':
+                        delimiter = ' '
+                    elif collection_format == 'tsv':
+                        delimiter = '\t'
+                    elif collection_format == 'pipes':
+                        delimiter = '|'
+                    else:  # csv is the default
+                        delimiter = ','
+                    new_params.append(
+                        (k, delimiter.join(str(value) for value in v)))
+            else:
+                new_params.append((k, v))
+        return new_params
 
     def prepare_post_parameters(self, post_params=None, files=None):
         """
@@ -450,7 +472,7 @@ class ApiClient(object):
         Updates header and query params based on authentication setting.
 
         :param headers: Header parameters dict to be updated.
-        :param querys: Query parameters dict to be updated.
+        :param querys: Query parameters tuple list to be updated.
         :param auth_settings: Authentication setting identifiers list.
         """
         config = Configuration()
@@ -466,7 +488,7 @@ class ApiClient(object):
                 elif auth_setting['in'] == 'header':
                     headers[auth_setting['key']] = auth_setting['value']
                 elif auth_setting['in'] == 'query':
-                    querys[auth_setting['key']] = auth_setting['value']
+                    querys.append((auth_setting['key'], auth_setting['value']))
                 else:
                     raise ValueError(
                         'Authentication token must be in `query` or `header`'
@@ -505,7 +527,7 @@ class ApiClient(object):
         :param data: str.
         :param klass: class literal.
 
-        :return: int, float, str, bool.
+        :return: int, long, float, str, bool.
         """
         try:
             value = klass(data)
@@ -572,6 +594,9 @@ class ApiClient(object):
         :return: model object.
         """
         instance = klass()
+
+        if not instance.swagger_types:
+            return data
 
         for attr, attr_type in iteritems(instance.swagger_types):
             if data is not None \
