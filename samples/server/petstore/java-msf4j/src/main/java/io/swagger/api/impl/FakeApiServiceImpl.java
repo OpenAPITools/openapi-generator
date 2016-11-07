38c51cd895406193822c629ca80017298ec6fb88
wing328 <wing328hk@gmail.com>
2016-11-07 23:54:47 +0800

    update mxf4j sample

--- samples/server/petstore/java-msf4j/src/main/java/io/swagger/api/impl/FakeApiServiceImpl.java
+++ samples/server/petstore/java-msf4j/src/main/java/io/swagger/api/impl/FakeApiServiceImpl.java
@@ -12,7 +12,8 @@ import io.swagger.api.NotFoundException;
 
 import java.io.InputStream;
 
-import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
+import org.wso2.msf4j.formparam.FormDataParam;
+import org.wso2.msf4j.formparam.FileInfo;
 
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.SecurityContext;
@@ -20,17 +21,40 @@ import javax.ws.rs.core.SecurityContext;
 
 public class FakeApiServiceImpl extends FakeApiService {
     @Override
-    public Response testClientModel(Client body ) throws NotFoundException {
+    public Response testClientModel(Client body
+ ) throws NotFoundException {
         // do some magic!
         return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "magic!")).build();
     }
     @Override
-    public Response testEndpointParameters(BigDecimal number, Double _double, String patternWithoutDelimiter, byte[] _byte, Integer integer, Integer int32, Long int64, Float _float, String string, byte[] binary, Date date, Date dateTime, String password, String paramCallback ) throws NotFoundException {
+    public Response testEndpointParameters(BigDecimal number
+, Double _double
+, String patternWithoutDelimiter
+, byte[] _byte
+, Integer integer
+, Integer int32
+, Long int64
+, Float _float
+, String string
+, byte[] binary
+, Date date
+, Date dateTime
+, String password
+, String paramCallback
+ ) throws NotFoundException {
         // do some magic!
         return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "magic!")).build();
     }
     @Override
-    public Response testEnumParameters(List<String> enumFormStringArray, String enumFormString, List<String> enumHeaderStringArray, String enumHeaderString, List<String> enumQueryStringArray, String enumQueryString, BigDecimal enumQueryInteger, Double enumQueryDouble ) throws NotFoundException {
+    public Response testEnumParameters(List<String> enumFormStringArray
+, String enumFormString
+, List<String> enumHeaderStringArray
+, String enumHeaderString
+, List<String> enumQueryStringArray
+, String enumQueryString
+, BigDecimal enumQueryInteger
+, Double enumQueryDouble
+ ) throws NotFoundException {
         // do some magic!
         return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "magic!")).build();
     }
