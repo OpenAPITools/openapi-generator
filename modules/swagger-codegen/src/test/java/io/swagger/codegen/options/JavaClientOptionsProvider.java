55df652a8f12724255f1d2cbd1559d4a8967044b
jfiala <johannes.fiala@fwd.at>
2016-11-12 23:09:57 +0100

    add tests for java client performBeanValidation #2549

--- modules/swagger-codegen/src/test/java/io/swagger/codegen/options/JavaClientOptionsProvider.java
+++ modules/swagger-codegen/src/test/java/io/swagger/codegen/options/JavaClientOptionsProvider.java
@@ -9,6 +9,8 @@ import java.util.Map;
 
 public class JavaClientOptionsProvider extends JavaOptionsProvider {
 
+    public static final String PERFORM_BEANVALIDATION = "false";
+
     public static final String DEFAULT_LIBRARY_VALUE = "jersey2";
 
     @Override
@@ -19,6 +21,7 @@ public class JavaClientOptionsProvider extends JavaOptionsProvider {
         options.put(JavaClientCodegen.PARCELABLE_MODEL, "false");
         options.put(JavaClientCodegen.SUPPORT_JAVA6, "false");
         options.put(JavaClientCodegen.USE_BEANVALIDATION, "false");
+        options.put(JavaClientCodegen.PERFORM_BEANVALIDATION, PERFORM_BEANVALIDATION);
 
         return options;
     }
