1366d204f900eaa04437b198b826f31ea96bf89d
lukoyanov <v.lukoyanov@gmail.com>
2016-12-14 12:32:49 +0300

    [Java] Play! Framework 2.4 WS client support + retrofit2 (#4270)
    * implemented core integration with play 2.4 ws
    
    * added shell script to test on CI
    
    * added shell script to composite file for all java generators
    
    * added some comments
    changed promise param to Response<T> to allow access to http status code and raw response if needed
    
    * removed unnecessary whitespace changes
    
    * added java7 compatibility, play ws deps to pom.xml
    
    * added generated play24 client
    
    * fixed imports

--- modules/swagger-codegen/src/test/java/io/swagger/codegen/options/JavaClientOptionsProvider.java
+++ modules/swagger-codegen/src/test/java/io/swagger/codegen/options/JavaClientOptionsProvider.java
@@ -1,6 +1,5 @@
 package io.swagger.codegen.options;
 
-import com.google.common.collect.ImmutableMap;
 import io.swagger.codegen.CodegenConstants;
 import io.swagger.codegen.languages.JavaClientCodegen;
 
@@ -18,6 +17,7 @@ public class JavaClientOptionsProvider extends JavaOptionsProvider {
         Map<String, String> options = new HashMap<String, String>(super.createOptions());
         options.put(CodegenConstants.LIBRARY, DEFAULT_LIBRARY_VALUE);
         options.put(JavaClientCodegen.USE_RX_JAVA, "false");
+        options.put(JavaClientCodegen.USE_PLAY24_WS, "false");
         options.put(JavaClientCodegen.PARCELABLE_MODEL, "false");
         options.put(JavaClientCodegen.SUPPORT_JAVA6, "false");
         options.put(JavaClientCodegen.USE_BEANVALIDATION, "false");
