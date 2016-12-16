c6c8ffe4e0bf3ed7e1955167cf6e56465aa93480
Nicholas DiPiazza <nicholas.dipiazza@lucidworks.com>
2016-12-16 05:04:13 -0600

    Do not NPE when array properties items are not specified (#4063)

--- modules/swagger-codegen/src/main/java/io/swagger/codegen/languages/AbstractJavaCodegen.java
+++ modules/swagger-codegen/src/main/java/io/swagger/codegen/languages/AbstractJavaCodegen.java
@@ -461,6 +461,9 @@ public abstract class AbstractJavaCodegen extends DefaultCodegen implements Code
         if (p instanceof ArrayProperty) {
             ArrayProperty ap = (ArrayProperty) p;
             Property inner = ap.getItems();
+            if (inner == null) {
+              return null;
+            }
             return getSwaggerType(p) + "<" + getTypeDeclaration(inner) + ">";
         } else if (p instanceof MapProperty) {
             MapProperty mp = (MapProperty) p;
@@ -479,6 +482,9 @@ public abstract class AbstractJavaCodegen extends DefaultCodegen implements Code
                 pattern = "new java.util.ArrayList<%s>()";
             } else {
                 pattern = "new ArrayList<%s>()";
+            }
+            if (ap.getItems() == null) {
+              return null;
             }
             return String.format(pattern, getTypeDeclaration(ap.getItems()));
         } else if (p instanceof MapProperty) {
