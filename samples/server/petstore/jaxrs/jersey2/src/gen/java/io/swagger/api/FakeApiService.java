87daf2149f93dda249fbeacaf0001623a57dfdfd
J. Fiala <johannes.fiala@fwd.at>
2017-01-06 15:16:39 +0100

    update language jaxrs (Jersey2) and samples

--- samples/server/petstore/jaxrs/jersey2/src/gen/java/io/swagger/api/FakeApiService.java
+++ samples/server/petstore/jaxrs/jersey2/src/gen/java/io/swagger/api/FakeApiService.java
@@ -16,10 +16,10 @@ import java.io.InputStream;
 
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.SecurityContext;
-
+import javax.validation.constraints.*;
 
 public abstract class FakeApiService {
     public abstract Response testClientModel(Client body,SecurityContext securityContext) throws NotFoundException;
     public abstract Response testEndpointParameters(BigDecimal number,Double _double,String patternWithoutDelimiter,byte[] _byte,Integer integer,Integer int32,Long int64,Float _float,String string,byte[] binary,Date date,Date dateTime,String password,String paramCallback,SecurityContext securityContext) throws NotFoundException;
-    public abstract Response testEnumParameters(List<String> enumFormStringArray,String enumFormString,List<String> enumHeaderStringArray,String enumHeaderString,List<String> enumQueryStringArray,String enumQueryString,BigDecimal enumQueryInteger,Double enumQueryDouble,SecurityContext securityContext) throws NotFoundException;
+    public abstract Response testEnumParameters(List<String> enumFormStringArray,String enumFormString,List<String> enumHeaderStringArray,String enumHeaderString, List<String> enumQueryStringArray, String enumQueryString, Integer enumQueryInteger,Double enumQueryDouble,SecurityContext securityContext) throws NotFoundException;
 }
