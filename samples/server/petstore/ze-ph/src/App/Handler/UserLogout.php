44e45ad7cb0b39fe674c8008acc65b4853fde4e9
Articus <articusw@gmail.com>
2017-01-15 21:05:04 +0400

    PHP server generator ze-ph (Zend Expressive + Path Handler) (#4559)
    * Server generator ze-ph (Zend Expressive + Path Handler)
    
    * Command line scripts for new ze-ph generator

---
+++ samples/server/petstore/ze-ph/src/App/Handler/UserLogout.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Handler;
+
+use Articus\PathHandler\Operation;
+use Articus\PathHandler\Annotation as PHA;
+use Articus\PathHandler\Consumer as PHConsumer;
+use Articus\PathHandler\Producer as PHProducer;
+use Articus\PathHandler\Attribute as PHAttribute;
+use Articus\PathHandler\Exception as PHException;
+use Psr\Http\Message\ServerRequestInterface;
+
+
+class UserLogout implements Operation\GetInterface
+{
+    /**
+     * Logs out current logged in user session
+     * TODO check if producer is valid, if it has correct priority and if it can be moved to class annotation
+     * @PHA\Producer(name=PHProducer\Transfer::class, mediaType="application/xml")
+     * TODO check if producer is valid, if it has correct priority and if it can be moved to class annotation
+     * @PHA\Producer(name=PHProducer\Transfer::class, mediaType="application/json")
+     */
+    public function handleGet(ServerRequestInterface $request)
+    {
+        //TODO implement method
+        throw new PHException\HttpCode(500, "Not implemented");
+    }
+}
