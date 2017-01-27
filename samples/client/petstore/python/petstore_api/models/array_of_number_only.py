3f8ad2f065bdf478802a84142badadda4ea2b8b2
Frode Danielsen <frode@e5r.no>
2017-01-27 17:42:53 +0100

    [Python] Improve model equality implementation (#4663)
    * [Python] Improve model equality implementation
    
    The model equality implementation template blindly tried accessing the
    `__dict__` attribute of the variable to compare equality against. This
    attribute does not exist for a lot of Python built-in types (`None`,
    `str` etc.) and the equality check would simply crash.
    
    This adds a simple guard to only continue with equality check if the
    variable to compare is an instance of the model being compared against.
    
    * Remove wrong auto-update of Python requirements

--- samples/client/petstore/python/petstore_api/models/array_of_number_only.py
+++ samples/client/petstore/python/petstore_api/models/array_of_number_only.py
@@ -103,6 +103,9 @@ class ArrayOfNumberOnly(object):
         """
         Returns true if both objects are equal
         """
+        if not isinstance(other, ArrayOfNumberOnly):
+            return False
+
         return self.__dict__ == other.__dict__
 
     def __ne__(self, other):
--- samples/client/petstore/python/petstore_api/models/enum_arrays.py
+++ samples/client/petstore/python/petstore_api/models/enum_arrays.py
@@ -140,6 +140,9 @@ class EnumArrays(object):
         """
         Returns true if both objects are equal
         """
+        if not isinstance(other, EnumArrays):
+            return False
+
         return self.__dict__ == other.__dict__
 
     def __ne__(self, other):
