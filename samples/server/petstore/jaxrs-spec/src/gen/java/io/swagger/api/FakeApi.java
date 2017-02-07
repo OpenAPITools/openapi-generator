aef27d36af0fe35e58c1cfc06ae1fd0956d0217f
jfiala <johannes.fiala@fwd.at>
2017-02-07 17:41:36 +0100

    [Jaxrs-spec] fix usage of Jersey templates in shellscript (#4722)
    * remove jersey templates #4720
    
    * regenerate sample with jaxrs-spec templates #4720

--- samples/server/petstore/jaxrs-spec/src/gen/java/io/swagger/api/FakeApi.java
+++ samples/server/petstore/jaxrs-spec/src/gen/java/io/swagger/api/FakeApi.java
@@ -1,98 +1,61 @@
 package io.swagger.api;
 
-import io.swagger.model.*;
-import io.swagger.api.FakeApiService;
-import io.swagger.api.factories.FakeApiServiceFactory;
-
-import io.swagger.annotations.ApiParam;
-import io.swagger.jaxrs.*;
-
 import java.math.BigDecimal;
 import io.swagger.model.Client;
 import org.joda.time.LocalDate;
 
-import java.util.List;
-import io.swagger.api.NotFoundException;
-
-import java.io.InputStream;
-
-import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
-import org.glassfish.jersey.media.multipart.FormDataParam;
-
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.Response;
-import javax.ws.rs.core.SecurityContext;
 import javax.ws.rs.*;
+import javax.ws.rs.core.Response;
+
+import io.swagger.annotations.*;
+
+import java.util.List;
 import javax.validation.constraints.*;
 
 @Path("/fake")
 
+@Api(description = "the fake API")
 
-@io.swagger.annotations.Api(description = "the fake API")
+
+
 
 public class FakeApi  {
-   private final FakeApiService delegate = FakeApiServiceFactory.getFakeApi();
 
     @PATCH
     
     @Consumes({ "application/json" })
     @Produces({ "application/json" })
-    @io.swagger.annotations.ApiOperation(value = "To test \"client\" model", notes = "To test \"client\" model", response = Client.class, tags={ "fake", })
-    @io.swagger.annotations.ApiResponses(value = { 
-        @io.swagger.annotations.ApiResponse(code = 200, message = "successful operation", response = Client.class) })
-    public Response testClientModel(@ApiParam(value = "client model" ,required=true) Client body
-,@Context SecurityContext securityContext)
-    throws NotFoundException {
-        return delegate.testClientModel(body,securityContext);
+    @ApiOperation(value = "To test \"client\" model", notes = "To test \"client\" model", response = Client.class, tags={ "fake",  })
+    @ApiResponses(value = { 
+        @ApiResponse(code = 200, message = "successful operation", response = Client.class) })
+    public Response testClientModel(Client body) {
+    	return Response.ok().entity("magic!").build();
     }
+
     @POST
     
-    @Consumes({ "application/xml; charset=utf-8", "application/json; charset=utf-8" })
-    @Produces({ "application/xml; charset=utf-8", "application/json; charset=utf-8" })
-    @io.swagger.annotations.ApiOperation(value = "Fake endpoint for testing various parameters 假端點 偽のエンドポイント 가짜 엔드 포인트 ", notes = "Fake endpoint for testing various parameters 假端點 偽のエンドポイント 가짜 엔드 포인트 ", response = void.class, authorizations = {
-        @io.swagger.annotations.Authorization(value = "http_basic_test")
-    }, tags={ "fake", })
-    @io.swagger.annotations.ApiResponses(value = { 
-        @io.swagger.annotations.ApiResponse(code = 400, message = "Invalid username supplied", response = void.class),
-        
-        @io.swagger.annotations.ApiResponse(code = 404, message = "User not found", response = void.class) })
-    public Response testEndpointParameters(@ApiParam(value = "None", required=true)  @FormParam("number")  BigDecimal number
-,@ApiParam(value = "None", required=true)  @FormParam("double")  Double _double
-,@ApiParam(value = "None", required=true)  @FormParam("pattern_without_delimiter")  String patternWithoutDelimiter
-,@ApiParam(value = "None", required=true)  @FormParam("byte")  byte[] _byte
-,@ApiParam(value = "None")  @FormParam("integer")  Integer integer
-,@ApiParam(value = "None")  @FormParam("int32")  Integer int32
-,@ApiParam(value = "None")  @FormParam("int64")  Long int64
-,@ApiParam(value = "None")  @FormParam("float")  Float _float
-,@ApiParam(value = "None")  @FormParam("string")  String string
-,@ApiParam(value = "None")  @FormParam("binary")  byte[] binary
-,@ApiParam(value = "None")  @FormParam("date")  LocalDate date
-,@ApiParam(value = "None")  @FormParam("dateTime")  javax.xml.datatype.XMLGregorianCalendar dateTime
-,@ApiParam(value = "None")  @FormParam("password")  String password
-,@ApiParam(value = "None")  @FormParam("callback")  String paramCallback
-,@Context SecurityContext securityContext)
-    throws NotFoundException {
-        return delegate.testEndpointParameters(number,_double,patternWithoutDelimiter,_byte,integer,int32,int64,_float,string,binary,date,dateTime,password,paramCallback,securityContext);
+    @Consumes({ "application/xml; charset&#x3D;utf-8", "application/json; charset&#x3D;utf-8" })
+    @Produces({ "application/xml; charset&#x3D;utf-8", "application/json; charset&#x3D;utf-8" })
+    @ApiOperation(value = "Fake endpoint for testing various parameters 假端點 偽のエンドポイント 가짜 엔드 포인트 ", notes = "Fake endpoint for testing various parameters 假端點 偽のエンドポイント 가짜 엔드 포인트 ", response = void.class, authorizations = {
+        @Authorization(value = "http_basic_test")
+    }, tags={ "fake",  })
+    @ApiResponses(value = { 
+        @ApiResponse(code = 400, message = "Invalid username supplied", response = void.class),
+        @ApiResponse(code = 404, message = "User not found", response = void.class) })
+    public Response testEndpointParameters(@FormParam(value = "number")  BigDecimal number,@FormParam(value = "_double")  Double _double,@FormParam(value = "patternWithoutDelimiter")  String patternWithoutDelimiter,@FormParam(value = "_byte")  byte[] _byte,@FormParam(value = "integer")  Integer integer,@FormParam(value = "int32")  Integer int32,@FormParam(value = "int64")  Long int64,@FormParam(value = "_float")  Float _float,@FormParam(value = "string")  String string,@FormParam(value = "binary")  byte[] binary,@FormParam(value = "date")  LocalDate date,@FormParam(value = "dateTime")  javax.xml.datatype.XMLGregorianCalendar dateTime,@FormParam(value = "password")  String password,@FormParam(value = "paramCallback")  String paramCallback) {
+    	return Response.ok().entity("magic!").build();
     }
+
     @GET
     
     @Consumes({ "*/*" })
     @Produces({ "*/*" })
-    @io.swagger.annotations.ApiOperation(value = "To test enum parameters", notes = "To test enum parameters", response = void.class, tags={ "fake", })
-    @io.swagger.annotations.ApiResponses(value = { 
-        @io.swagger.annotations.ApiResponse(code = 400, message = "Invalid request", response = void.class),
-        
-        @io.swagger.annotations.ApiResponse(code = 404, message = "Not found", response = void.class) })
-    public Response testEnumParameters(@ApiParam(value = "Form parameter enum test (string array)", allowableValues=">, $")  @FormParam("enum_form_string_array")  List<String> enumFormStringArray
-,@ApiParam(value = "Form parameter enum test (string)", allowableValues="_abc, -efg, (xyz)", defaultValue="-efg")  @DefaultValue("-efg") @FormParam("enum_form_string")  String enumFormString
-,@ApiParam(value = "Header parameter enum test (string array)" , allowableValues=">, $")@HeaderParam("enum_header_string_array") List<String> enumHeaderStringArray
-,@ApiParam(value = "Header parameter enum test (string)" , allowableValues="_abc, -efg, (xyz)", defaultValue="-efg")@HeaderParam("enum_header_string") String enumHeaderString
-,@ApiParam(value = "Query parameter enum test (string array)", allowableValues=">, $") @QueryParam("enum_query_string_array") List<String> enumQueryStringArray
-,@ApiParam(value = "Query parameter enum test (string)", allowableValues="_abc, -efg, (xyz)", defaultValue="-efg") @DefaultValue("-efg") @QueryParam("enum_query_string") String enumQueryString
-,@ApiParam(value = "Query parameter enum test (double)") @QueryParam("enum_query_integer") Integer enumQueryInteger
-,@ApiParam(value = "Query parameter enum test (double)")  @FormParam("enum_query_double")  Double enumQueryDouble
-,@Context SecurityContext securityContext)
-    throws NotFoundException {
-        return delegate.testEnumParameters(enumFormStringArray,enumFormString,enumHeaderStringArray,enumHeaderString,enumQueryStringArray,enumQueryString,enumQueryInteger,enumQueryDouble,securityContext);
+    @ApiOperation(value = "To test enum parameters", notes = "To test enum parameters", response = void.class, tags={ "fake" })
+    @ApiResponses(value = { 
+        @ApiResponse(code = 400, message = "Invalid request", response = void.class),
+        @ApiResponse(code = 404, message = "Not found", response = void.class) })
+    public Response testEnumParameters(@FormParam(value = "enumFormStringArray")  List<String> enumFormStringArray,@FormParam(value = "enumFormString")  String enumFormString,@HeaderParam("enum_header_string_array") List<String> enumHeaderStringArray,@HeaderParam("enum_header_string") String enumHeaderString,@QueryParam("enum_query_string_array")  List<String> enumQueryStringArray,@QueryParam("enum_query_string")  String enumQueryString,@QueryParam("enum_query_integer")  Integer enumQueryInteger,@FormParam(value = "enumQueryDouble")  Double enumQueryDouble) {
+    	return Response.ok().entity("magic!").build();
     }
 }
+
