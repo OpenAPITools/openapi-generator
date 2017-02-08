882c3ecba54906e9020137f0f145a16dc940fcf6
wing328 <wing328hk@gmail.com>
2017-02-08 16:07:09 +0800

    update java petstore with new uuid mapping

--- samples/client/petstore/java/jersey2-java8/src/main/java/io/swagger/client/model/MixedPropertiesAndAdditionalPropertiesClass.java
+++ samples/client/petstore/java/jersey2-java8/src/main/java/io/swagger/client/model/MixedPropertiesAndAdditionalPropertiesClass.java
@@ -23,6 +23,7 @@ import java.time.OffsetDateTime;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 /**
  * MixedPropertiesAndAdditionalPropertiesClass
@@ -30,7 +31,7 @@ import java.util.Map;
 
 public class MixedPropertiesAndAdditionalPropertiesClass {
   @JsonProperty("uuid")
-  private String uuid = null;
+  private UUID uuid = null;
 
   @JsonProperty("dateTime")
   private OffsetDateTime dateTime = null;
@@ -38,7 +39,7 @@ public class MixedPropertiesAndAdditionalPropertiesClass {
   @JsonProperty("map")
   private Map<String, Animal> map = new HashMap<String, Animal>();
 
-  public MixedPropertiesAndAdditionalPropertiesClass uuid(String uuid) {
+  public MixedPropertiesAndAdditionalPropertiesClass uuid(UUID uuid) {
     this.uuid = uuid;
     return this;
   }
@@ -48,11 +49,11 @@ public class MixedPropertiesAndAdditionalPropertiesClass {
    * @return uuid
   **/
   @ApiModelProperty(example = "null", value = "")
-  public String getUuid() {
+  public UUID getUuid() {
     return uuid;
   }
 
-  public void setUuid(String uuid) {
+  public void setUuid(UUID uuid) {
     this.uuid = uuid;
   }
 
