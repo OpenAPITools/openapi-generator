8f2e9bce6deb6ada27e331525d5f50c75c36f80b
wing328 <wing328hk@gmail.com>
2017-02-13 18:53:34 +0800

    remove trailing whitespaces in java api client (#4784)

--- samples/client/petstore/java/retrofit2rx/src/main/java/io/swagger/client/ApiClient.java
+++ samples/client/petstore/java/retrofit2rx/src/main/java/io/swagger/client/ApiClient.java
@@ -51,13 +51,13 @@ public class ApiClient {
 
     public ApiClient(String[] authNames) {
         this();
-        for(String authName : authNames) { 
+        for(String authName : authNames) {
             Interceptor auth;
-            if (authName == "api_key") { 
+            if ("api_key".equals(authName)) {
                 auth = new ApiKeyAuth("header", "api_key");
-            } else if (authName == "http_basic_test") { 
+            } else if ("http_basic_test".equals(authName)) {
                 auth = new HttpBasicAuth();
-            } else if (authName == "petstore_auth") { 
+            } else if ("petstore_auth".equals(authName)) {
                 auth = new OAuth(OAuthFlow.implicit, "http://petstore.swagger.io/api/oauth/dialog", "", "write:pets, read:pets");
             } else {
                 throw new RuntimeException("auth name \"" + authName + "\" not found in available auth names");
