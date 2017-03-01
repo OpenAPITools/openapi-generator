a691e4e4531c6caadcbb0f72ec2aa90ffd9f8568
Tony Tam <fehguy@gmail.com>
2017-03-01 10:02:42 -0500

    rebuilt

--- samples/server/petstore/jaxrs-spec/src/gen/java/io/swagger/model/EnumArrays.java
+++ samples/server/petstore/jaxrs-spec/src/gen/java/io/swagger/model/EnumArrays.java
@@ -110,7 +110,7 @@ public enum ArrayEnumEnum {
 
 
   @Override
-  public boolean equals(Object o) {
+  public boolean equals(java.lang.Object o) {
     if (this == o) {
       return true;
     }
@@ -142,7 +142,7 @@ public enum ArrayEnumEnum {
    * Convert the given object to string with each line indented by 4 spaces
    * (except the first line).
    */
-  private String toIndentedString(Object o) {
+  private String toIndentedString(java.lang.Object o) {
     if (o == null) {
       return "null";
     }
