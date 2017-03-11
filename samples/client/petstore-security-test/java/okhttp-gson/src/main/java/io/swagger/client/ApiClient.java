9e0e7626514d8545930976597aa818f9eb1e8860
XuXinkun <xuxinkun@users.noreply.github.com>
2017-03-11 17:02:49 +0800

    fix handleResponse to not leak okhttp connections (#4997)

--- samples/client/petstore-security-test/java/okhttp-gson/src/main/java/io/swagger/client/ApiClient.java
+++ samples/client/petstore-security-test/java/okhttp-gson/src/main/java/io/swagger/client/ApiClient.java
@@ -1032,6 +1032,13 @@ public class ApiClient {
             if (returnType == null || response.code() == 204) {
                 // returning null if the returnType is not defined,
                 // or the status code is 204 (No Content)
+                if (response.body() != null) {
+                    try {
+                        response.body().close();
+                    } catch (IOException e) {
+                        throw new ApiException(response.message(), e, response.code(), response.headers().toMultimap());
+                    }
+                }
                 return null;
             } else {
                 return deserialize(response, returnType);
