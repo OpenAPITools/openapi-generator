ba380c9ddaf2fb2dc81cd1a465389c9f6976bbfd
Jeff Haynes <jhaynes@gotransverse.com>
2017-03-13 02:35:21 -0500

    Issue 4759: Import complex type for body parameters (#4775)

--- modules/swagger-codegen/src/main/java/io/swagger/codegen/DefaultCodegen.java
+++ modules/swagger-codegen/src/main/java/io/swagger/codegen/DefaultCodegen.java
@@ -2480,6 +2480,9 @@ public class DefaultCodegen {
                         p.isPrimitiveType = cp.isPrimitiveType;
                         p.isBinary = isDataTypeBinary(cp.datatype);
                         p.isFile = isDataTypeFile(cp.datatype);
+                        if (cp.complexType != null) {
+                            imports.add(cp.complexType);
+                        }
                     }
 
                     // set boolean flag (e.g. isString)
