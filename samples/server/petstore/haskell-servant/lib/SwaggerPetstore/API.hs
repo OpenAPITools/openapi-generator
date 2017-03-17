2f0e6937fffce21dd746602fe26e64c13b2b5e74
Sebastian Mandrean <sebastian.mandrean@gmail.com>
2017-03-17 09:55:11 +0100

    [Haskell] Fix broken client/server compilation errors (#5097)
    * Remove dead legacy code
    
    * Update cosmetics according to Haskell standard practices
    
    * Fix failing pattern matching for lookupEither
    
    * Bump to latest dependencies without any breaking changes
    
    * Remove duplicate instance declarations already existing in Servant.API.Verbs
    
    * Fix double Java/Haskell escapement bug
    
    * Re-generate Petstore sample client/server

--- samples/server/petstore/haskell-servant/lib/SwaggerPetstore/API.hs
+++ samples/server/petstore/haskell-servant/lib/SwaggerPetstore/API.hs
@@ -1,75 +1,92 @@
-{-# LANGUAGE DataKinds, TypeFamilies, TypeOperators, FlexibleInstances, OverloadedStrings, ViewPatterns #-}
-{-# LANGUAGE RecordWildCards, GeneralizedNewtypeDeriving, DeriveTraversable, FlexibleContexts, DeriveGeneric #-}
-{-# OPTIONS_GHC -fno-warn-unused-binds -fno-warn-unused-imports -fcontext-stack=328 #-}
-module SwaggerPetstore.API (
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DeriveTraversable #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE ViewPatterns #-}
+{-# OPTIONS_GHC
+-fno-warn-unused-binds -fno-warn-unused-imports -fcontext-stack=328 #-}
+
+module SwaggerPetstore.API
   -- * Client and Server
-  ServerConfig(..),
-  SwaggerPetstoreBackend,
-  createSwaggerPetstoreClient,
-  runSwaggerPetstoreServer,
-  runSwaggerPetstoreClient,
-  runSwaggerPetstoreClientWithManager,
-  SwaggerPetstoreClient,
+  ( ServerConfig(..)
+  , SwaggerPetstoreBackend
+  , createSwaggerPetstoreClient
+  , runSwaggerPetstoreServer
+  , runSwaggerPetstoreClient
+  , runSwaggerPetstoreClientWithManager
+  , SwaggerPetstoreClient
   -- ** Servant
-  SwaggerPetstoreAPI,
+  , SwaggerPetstoreAPI
   ) where
 
 import SwaggerPetstore.Types
 
+import Control.Monad.Except (ExceptT)
+import Control.Monad.IO.Class
 import Data.Aeson (Value)
 import Data.Coerce (coerce)
-import Servant.API
-import Servant (serve, ServantErr)
-import Web.HttpApiData
-import qualified Network.Wai.Handler.Warp as Warp
-import qualified Data.Text as T
-import Data.Text (Text)
-import Servant.Common.BaseUrl(BaseUrl(..))
-import Servant.Client (ServantError, client, Scheme(Http))
-import Data.Proxy (Proxy(..))
-import Control.Monad.IO.Class
 import Data.Function ((&))
-import GHC.Exts (IsString(..))
 import qualified Data.Map as Map
-import GHC.Generics (Generic)
 import Data.Monoid ((<>))
-import Servant.API.Verbs (Verb, StdMethod(..))
-import Control.Monad.Except (ExceptT)
-import Network.HTTP.Client (Manager, newManager, defaultManagerSettings)
+import Data.Proxy (Proxy(..))
+import Data.Text (Text)
+import qualified Data.Text as T
+import GHC.Exts (IsString(..))
+import GHC.Generics (Generic)
+import Network.HTTP.Client (Manager, defaultManagerSettings, newManager)
 import Network.HTTP.Types.Method (methodOptions)
-
-instance ReflectMethod 'OPTIONS where
-  reflectMethod _ = methodOptions
+import qualified Network.Wai.Handler.Warp as Warp
+import Servant (ServantErr, serve)
+import Servant.API
+import Servant.API.Verbs (StdMethod(..), Verb)
+import Servant.Client (Scheme(Http), ServantError, client)
+import Servant.Common.BaseUrl (BaseUrl(..))
+import Web.HttpApiData
 
 
 
 data FormUpdatePetWithForm = FormUpdatePetWithForm
-    { updatePetWithFormName :: Text
-    , updatePetWithFormStatus :: Text
-    } deriving (Show, Eq, Generic)
+  { updatePetWithFormName :: Text
+  , updatePetWithFormStatus :: Text
+  } deriving (Show, Eq, Generic)
 
 instance FromFormUrlEncoded FormUpdatePetWithForm where
-    fromFormUrlEncoded inputs = FormUpdatePetWithForm <$>  lookupEither "name" inputs <*>  lookupEither "status" inputs
-instance ToFormUrlEncoded FormUpdatePetWithForm where
-    toFormUrlEncoded value = [("name", toQueryParam $ updatePetWithFormName value), ("status", toQueryParam $ updatePetWithFormStatus value)]
+  fromFormUrlEncoded inputs = FormUpdatePetWithForm <$> lookupEither "name" inputs <*> lookupEither "status" inputs
 
+instance ToFormUrlEncoded FormUpdatePetWithForm where
+  toFormUrlEncoded value =
+    [ ("name", toQueryParam $ updatePetWithFormName value)
+    , ("status", toQueryParam $ updatePetWithFormStatus value)
+    ]
 data FormUploadFile = FormUploadFile
-    { uploadFileAdditionalMetadata :: Text
-    , uploadFileFile :: FilePath
-    } deriving (Show, Eq, Generic)
+  { uploadFileAdditionalMetadata :: Text
+  , uploadFileFile :: FilePath
+  } deriving (Show, Eq, Generic)
 
 instance FromFormUrlEncoded FormUploadFile where
-    fromFormUrlEncoded inputs = FormUploadFile <$>  lookupEither "additionalMetadata" inputs <*>  lookupEither "file" inputs
-instance ToFormUrlEncoded FormUploadFile where
-    toFormUrlEncoded value = [("additionalMetadata", toQueryParam $ uploadFileAdditionalMetadata value), ("file", toQueryParam $ uploadFileFile value)]
+  fromFormUrlEncoded inputs = FormUploadFile <$> lookupEither "additionalMetadata" inputs <*> lookupEither "file" inputs
 
+instance ToFormUrlEncoded FormUploadFile where
+  toFormUrlEncoded value =
+    [ ("additionalMetadata", toQueryParam $ uploadFileAdditionalMetadata value)
+    , ("file", toQueryParam $ uploadFileFile value)
+    ]
 
 -- For the form data code generation.
-lookupEither :: FromHttpApiData b => Text -> [(Text, Text)] -> Either Text b
+lookupEither :: FromHttpApiData b => Text -> [(Text, Text)] -> Either String b
 lookupEither key assocs =
   case lookup key assocs of
-    Nothing -> Left $ "Could not find parameter " <> key <> " in form data"
-    Just value -> parseQueryParam value
+    Nothing -> Left $ "Could not find parameter " <> (T.unpack key) <> " in form data"
+    Just value ->
+      case parseQueryParam value of
+        Left result -> Left $ T.unpack result
+        Right result -> Right $ result
 
 -- | Servant type-level API, generated from the Swagger spec for SwaggerPetstore.
 type SwaggerPetstoreAPI
@@ -95,54 +112,56 @@ type SwaggerPetstoreAPI
     :<|> "user" :> Capture "username" Text :> ReqBody '[JSON] User :> Verb 'PUT 200 '[JSON] () -- 'updateUser' route
 
 -- | Server or client configuration, specifying the host and port to query or serve on.
-data ServerConfig = ServerConfig {
-    configHost :: String,  -- ^ Hostname to serve on, e.g. "127.0.0.1"
-    configPort :: Int      -- ^ Port to serve on, e.g. 8080
+data ServerConfig = ServerConfig
+  { configHost :: String  -- ^ Hostname to serve on, e.g. "127.0.0.1"
+  , configPort :: Int      -- ^ Port to serve on, e.g. 8080
   } deriving (Eq, Ord, Show, Read)
 
 -- | List of elements parsed from a query.
-newtype QueryList (p :: CollectionFormat) a = QueryList { fromQueryList :: [a] }
-  deriving (Functor, Applicative, Monad, Foldable, Traversable)
+newtype QueryList (p :: CollectionFormat) a = QueryList
+  { fromQueryList :: [a]
+  } deriving (Functor, Applicative, Monad, Foldable, Traversable)
 
 -- | Formats in which a list can be encoded into a HTTP path.
-data CollectionFormat = CommaSeparated -- ^ CSV format for multiple parameters.
-                      | SpaceSeparated -- ^ Also called "SSV"
-                      | TabSeparated -- ^ Also called "TSV"
-                      | PipeSeparated -- ^ `value1|value2|value2`
-                      | MultiParamArray -- ^ Using multiple GET parameters, e.g. `foo=bar&foo=baz`. Only for GET params.
+data CollectionFormat
+  = CommaSeparated -- ^ CSV format for multiple parameters.
+  | SpaceSeparated -- ^ Also called "SSV"
+  | TabSeparated -- ^ Also called "TSV"
+  | PipeSeparated -- ^ `value1|value2|value2`
+  | MultiParamArray -- ^ Using multiple GET parameters, e.g. `foo=bar&foo=baz`. Only for GET params.
 
 instance FromHttpApiData a => FromHttpApiData (QueryList 'CommaSeparated a) where
-    parseQueryParam = parseSeparatedQueryList ','
+  parseQueryParam = parseSeparatedQueryList ','
 
 instance FromHttpApiData a => FromHttpApiData (QueryList 'TabSeparated a) where
-    parseQueryParam = parseSeparatedQueryList '\t'
+  parseQueryParam = parseSeparatedQueryList '\t'
 
 instance FromHttpApiData a => FromHttpApiData (QueryList 'SpaceSeparated a) where
-    parseQueryParam = parseSeparatedQueryList ' '
+  parseQueryParam = parseSeparatedQueryList ' '
 
 instance FromHttpApiData a => FromHttpApiData (QueryList 'PipeSeparated a) where
-    parseQueryParam = parseSeparatedQueryList '|'
+  parseQueryParam = parseSeparatedQueryList '|'
 
 instance FromHttpApiData a => FromHttpApiData (QueryList 'MultiParamArray a) where
-    parseQueryParam = error "unimplemented FromHttpApiData for MultiParamArray collection format"
+  parseQueryParam = error "unimplemented FromHttpApiData for MultiParamArray collection format"
 
 parseSeparatedQueryList :: FromHttpApiData a => Char -> Text -> Either Text (QueryList p a)
 parseSeparatedQueryList char = fmap QueryList . mapM parseQueryParam . T.split (== char)
 
 instance ToHttpApiData a => ToHttpApiData (QueryList 'CommaSeparated a) where
-    toQueryParam = formatSeparatedQueryList ','
+  toQueryParam = formatSeparatedQueryList ','
 
 instance ToHttpApiData a => ToHttpApiData (QueryList 'TabSeparated a) where
-    toQueryParam = formatSeparatedQueryList '\t'
+  toQueryParam = formatSeparatedQueryList '\t'
 
 instance ToHttpApiData a => ToHttpApiData (QueryList 'SpaceSeparated a) where
-    toQueryParam = formatSeparatedQueryList ' '
+  toQueryParam = formatSeparatedQueryList ' '
 
 instance ToHttpApiData a => ToHttpApiData (QueryList 'PipeSeparated a) where
-    toQueryParam = formatSeparatedQueryList '|'
+  toQueryParam = formatSeparatedQueryList '|'
 
 instance ToHttpApiData a => ToHttpApiData (QueryList 'MultiParamArray a) where
-    toQueryParam = error "unimplemented ToHttpApiData for MultiParamArray collection format"
+  toQueryParam = error "unimplemented ToHttpApiData for MultiParamArray collection format"
 
 formatSeparatedQueryList :: ToHttpApiData a => Char ->  QueryList p a -> Text
 formatSeparatedQueryList char = T.intercalate (T.singleton char) . map toQueryParam . fromQueryList
@@ -152,43 +171,46 @@ formatSeparatedQueryList char = T.intercalate (T.singleton char) . map toQueryPa
 -- The backend can be used both for the client and the server. The client generated from the SwaggerPetstore Swagger spec
 -- is a backend that executes actions by sending HTTP requests (see @createSwaggerPetstoreClient@). Alternatively, provided
 -- a backend, the API can be served using @runSwaggerPetstoreServer@.
-data SwaggerPetstoreBackend m = SwaggerPetstoreBackend {
-    addPet :: Pet -> m (){- ^  -},
-    deletePet :: Integer -> Maybe Text -> m (){- ^  -},
-    findPetsByStatus :: Maybe [Text] -> m [Pet]{- ^ Multiple status values can be provided with comma separated strings -},
-    findPetsByTags :: Maybe [Text] -> m [Pet]{- ^ Multiple tags can be provided with comma separated strings. Use tag1, tag2, tag3 for testing. -},
-    getPetById :: Integer -> m Pet{- ^ Returns a single pet -},
-    updatePet :: Pet -> m (){- ^  -},
-    updatePetWithForm :: Integer -> FormUpdatePetWithForm -> m (){- ^  -},
-    uploadFile :: Integer -> FormUploadFile -> m ApiResponse{- ^  -},
-    deleteOrder :: Text -> m (){- ^ For valid response try integer IDs with value < 1000. Anything above 1000 or nonintegers will generate API errors -},
-    getInventory :: m (Map.Map String Int){- ^ Returns a map of status codes to quantities -},
-    getOrderById :: Integer -> m Order{- ^ For valid response try integer IDs with value <= 5 or > 10. Other values will generated exceptions -},
-    placeOrder :: Order -> m Order{- ^  -},
-    createUser :: User -> m (){- ^ This can only be done by the logged in user. -},
-    createUsersWithArrayInput :: [User] -> m (){- ^  -},
-    createUsersWithListInput :: [User] -> m (){- ^  -},
-    deleteUser :: Text -> m (){- ^ This can only be done by the logged in user. -},
-    getUserByName :: Text -> m User{- ^  -},
-    loginUser :: Maybe Text -> Maybe Text -> m Text{- ^  -},
-    logoutUser :: m (){- ^  -},
-    updateUser :: Text -> User -> m (){- ^ This can only be done by the logged in user. -}
+data SwaggerPetstoreBackend m = SwaggerPetstoreBackend
+  { addPet :: Pet -> m (){- ^  -}
+  , deletePet :: Integer -> Maybe Text -> m (){- ^  -}
+  , findPetsByStatus :: Maybe [Text] -> m [Pet]{- ^ Multiple status values can be provided with comma separated strings -}
+  , findPetsByTags :: Maybe [Text] -> m [Pet]{- ^ Multiple tags can be provided with comma separated strings. Use tag1, tag2, tag3 for testing. -}
+  , getPetById :: Integer -> m Pet{- ^ Returns a single pet -}
+  , updatePet :: Pet -> m (){- ^  -}
+  , updatePetWithForm :: Integer -> FormUpdatePetWithForm -> m (){- ^  -}
+  , uploadFile :: Integer -> FormUploadFile -> m ApiResponse{- ^  -}
+  , deleteOrder :: Text -> m (){- ^ For valid response try integer IDs with value < 1000. Anything above 1000 or nonintegers will generate API errors -}
+  , getInventory :: m (Map.Map String Int){- ^ Returns a map of status codes to quantities -}
+  , getOrderById :: Integer -> m Order{- ^ For valid response try integer IDs with value <= 5 or > 10. Other values will generated exceptions -}
+  , placeOrder :: Order -> m Order{- ^  -}
+  , createUser :: User -> m (){- ^ This can only be done by the logged in user. -}
+  , createUsersWithArrayInput :: [User] -> m (){- ^  -}
+  , createUsersWithListInput :: [User] -> m (){- ^  -}
+  , deleteUser :: Text -> m (){- ^ This can only be done by the logged in user. -}
+  , getUserByName :: Text -> m User{- ^  -}
+  , loginUser :: Maybe Text -> Maybe Text -> m Text{- ^  -}
+  , logoutUser :: m (){- ^  -}
+  , updateUser :: Text -> User -> m (){- ^ This can only be done by the logged in user. -}
   }
 
-newtype SwaggerPetstoreClient a = SwaggerPetstoreClient { runClient :: Manager -> BaseUrl -> ExceptT ServantError IO a }
-    deriving Functor
+newtype SwaggerPetstoreClient a = SwaggerPetstoreClient
+  { runClient :: Manager -> BaseUrl -> ExceptT ServantError IO a
+  } deriving Functor
 
 instance Applicative SwaggerPetstoreClient where
-    pure x = SwaggerPetstoreClient (\_ _ -> pure x)
-    (SwaggerPetstoreClient f) <*> (SwaggerPetstoreClient x) = SwaggerPetstoreClient (\manager url -> f manager url <*> x manager url)
+  pure x = SwaggerPetstoreClient (\_ _ -> pure x)
+  (SwaggerPetstoreClient f) <*> (SwaggerPetstoreClient x) =
+    SwaggerPetstoreClient (\manager url -> f manager url <*> x manager url)
 
 instance Monad SwaggerPetstoreClient where
-    (SwaggerPetstoreClient a) >>= f = SwaggerPetstoreClient (\manager url -> do
-        value <- a manager url
-        runClient (f value) manager url)
+  (SwaggerPetstoreClient a) >>= f =
+    SwaggerPetstoreClient (\manager url -> do
+      value <- a manager url
+      runClient (f value) manager url)
 
 instance MonadIO SwaggerPetstoreClient where
-    liftIO io = SwaggerPetstoreClient (\_ _ -> liftIO io)
+  liftIO io = SwaggerPetstoreClient (\_ _ -> liftIO io)
 
 createSwaggerPetstoreClient :: SwaggerPetstoreBackend SwaggerPetstoreClient
 createSwaggerPetstoreClient = SwaggerPetstoreBackend{..}
@@ -229,7 +251,6 @@ runSwaggerPetstoreClientWithManager manager clientConfig cl =
 runSwaggerPetstoreServer :: MonadIO m => ServerConfig -> SwaggerPetstoreBackend (ExceptT ServantErr IO)  -> m ()
 runSwaggerPetstoreServer ServerConfig{..} backend =
   liftIO $ Warp.runSettings warpSettings $ serve (Proxy :: Proxy SwaggerPetstoreAPI) (serverFromBackend backend)
-
   where
     warpSettings = Warp.defaultSettings & Warp.setPort configPort & Warp.setHost (fromString configHost)
     serverFromBackend SwaggerPetstoreBackend{..} =
