3ebee288add0bf344b9a17dd553c65e31185f592
wing328 <wing328hk@gmail.com>
2017-03-21 16:17:28 +0800

    add back missing changes from #4539 (#5109)

--- samples/client/petstore/typescript-angular2/npm/api/api.ts
+++ samples/client/petstore/typescript-angular2/npm/api/api.ts
@@ -1,4 +1,7 @@
 export * from './pet.service';
+import { PetService }  from './PetService';
 export * from './store.service';
+import { StoreService }  from './StoreService';
 export * from './user.service';
+import { UserService }  from './UserService';
 export const APIS = [ PetService, StoreService, UserService, ];
