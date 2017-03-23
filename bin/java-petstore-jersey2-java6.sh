5a3bc7fcf805b2c780bf0a6a0688bf09c3411b35
Paŭlo Ebermann <Paul-Ebermann@gmx.de>
2017-03-23 08:22:14 +0100

    Update samples for java/jersey2-java6 (and fix artifact ID) (#5118)
    * Update samples for java/jersey2-java6.
    
    * Let the sample generation script set the right name for jersey2-java6.
    
    This is the equivalent change to #5095, for jersey2-java6.
    
    * Update samples for Java/Jersey2-java6.

--- bin/java-petstore-jersey2-java6.sh
+++ bin/java-petstore-jersey2-java6.sh
@@ -26,7 +26,7 @@ fi
 
 # if you've executed sbt assembly previously it will use that instead.
 export JAVA_OPTS="${JAVA_OPTS} -XX:MaxPermSize=256M -Xmx1024M -DloggerPath=conf/log4j.properties"
-ags="$@ generate -i modules/swagger-codegen/src/test/resources/2_0/petstore-with-fake-endpoints-models-for-testing.yaml -l java -c bin/java-petstore-jersey2.json -o samples/client/petstore/java/jersey2-java6 -DhideGenerationTimestamp=true,supportJava6=true"
+ags="$@ generate --artifact-id swagger-petstore-jersey2-java6 -i modules/swagger-codegen/src/test/resources/2_0/petstore-with-fake-endpoints-models-for-testing.yaml -l java -c bin/java-petstore-jersey2.json -o samples/client/petstore/java/jersey2-java6 -DhideGenerationTimestamp=true,supportJava6=true"
 
 echo "Removing files and folders under samples/client/petstore/java/jersey2/src/main"
 rm -rf samples/client/petstore/java/jersey2-java6/src/main
