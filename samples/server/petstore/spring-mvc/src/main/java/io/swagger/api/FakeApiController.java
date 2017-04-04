f0b0226e468613415a8f7d08b0fe196eb74ad537
Paŭlo Ebermann <Paul-Ebermann@gmx.de>
2017-04-04 15:41:22 +0200

    [Java][JaxRS][Spring-MVC] Update samples for jaxrs with Jersey 1/2 and for spring-mvc (#5212)
    * Update samples for Spring-MVC.
    
    * Update samples for Spring-MVC-Jav8-async.
    
    * Update samples jaxrs/jersey1.
    
    * Update samples jaxrs/jersey2.
    
    * Update samples spring-mvc
    
    * Update samples spring-mvc-j8-async

--- samples/server/petstore/spring-mvc/src/main/java/io/swagger/api/FakeApiController.java
+++ samples/server/petstore/spring-mvc/src/main/java/io/swagger/api/FakeApiController.java
@@ -55,8 +55,8 @@ public class FakeApiController implements FakeApi {
         @ApiParam(value = "Header parameter enum test (string)"  , allowableValues="_ABC, _EFG, _XYZ_", defaultValue="-efg") @RequestHeader(value="enum_header_string", required=false) String enumHeaderString,
          @ApiParam(value = "Query parameter enum test (string array)", allowableValues = "GREATER_THAN, DOLLAR") @RequestParam(value = "enum_query_string_array", required = false) List<String> enumQueryStringArray,
          @ApiParam(value = "Query parameter enum test (string)", allowableValues = "_ABC, _EFG, _XYZ_", defaultValue = "-efg") @RequestParam(value = "enum_query_string", required = false, defaultValue="-efg") String enumQueryString,
-         @ApiParam(value = "Query parameter enum test (double)") @RequestParam(value = "enum_query_integer", required = false) Integer enumQueryInteger,
-        @ApiParam(value = "Query parameter enum test (double)" ) @RequestPart(value="enum_query_double", required=false)  Double enumQueryDouble) {
+         @ApiParam(value = "Query parameter enum test (double)", allowableValues = "NUMBER_1, NUMBER_MINUS_2") @RequestParam(value = "enum_query_integer", required = false) Integer enumQueryInteger,
+        @ApiParam(value = "Query parameter enum test (double)" , allowableValues="NUMBER_1_DOT_1, NUMBER_MINUS_1_DOT_2") @RequestPart(value="enum_query_double", required=false)  Double enumQueryDouble) {
         // do some magic!
         return new ResponseEntity<Void>(HttpStatus.OK);
     }
