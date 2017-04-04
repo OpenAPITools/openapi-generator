a07c8a1bbaf776afc29182eb022accb19d2c441b
Paŭlo Ebermann <Paul-Ebermann@gmx.de>
2017-04-04 08:42:48 +0200

    Updating security samples for Python client. (#5294)

--- samples/client/petstore-security-test/python/petstore_api/apis/fake_api.py
+++ samples/client/petstore-security-test/python/petstore_api/apis/fake_api.py
@@ -103,7 +103,6 @@ class FakeApi(object):
 
         collection_formats = {}
 
-        resource_path = '/fake'.replace('{format}', 'json')
         path_params = {}
 
         query_params = {}
@@ -127,7 +126,7 @@ class FakeApi(object):
         # Authentication setting
         auth_settings = []
 
-        return self.api_client.call_api(resource_path, 'PUT',
+        return self.api_client.call_api('/fake', 'PUT',
                                         path_params,
                                         query_params,
                                         header_params,
