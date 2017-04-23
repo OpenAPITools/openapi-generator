caec1939fd10b000905abb37bf0a6c0b479a0a6f
wing328 <wing328hk@gmail.com>
2017-04-23 15:42:42 +0800

    add csharp batch files

---
+++ bin/windows/csharp-petstore-all.bat
@@ -0,0 +1,8 @@
+REM C# Petstore API client
+.\bin\windows\csharp-petstore.bat
+
+REM C# Petstore API client with PropertyChanged
+.\bin\windows\csharp-property-changed-petstore.bat
+
+REM C# Petstore API client (v5.0 for .net standarnd 1.3+)
+.\bin\windows\csharp-petstore-netstandard.bat
