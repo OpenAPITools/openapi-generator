26fa547fd2f8396da850003bb44c91e14a9bc975
Ian White <ian@stardog.io>
2017-04-23 05:44:40 -0400

    typescript-angular2: ensuring that dates are strings and datetimes are Dates (#5417)

--- modules/swagger-codegen/src/test/java/io/swagger/codegen/typescript/typescriptangular2/TypeScriptAngular2ModelTest.java
+++ modules/swagger-codegen/src/test/java/io/swagger/codegen/typescript/typescriptangular2/TypeScriptAngular2ModelTest.java
@@ -14,6 +14,7 @@ import io.swagger.models.Model;
 import io.swagger.models.ModelImpl;
 import io.swagger.models.properties.ArrayProperty;
 import io.swagger.models.properties.DateTimeProperty;
+import io.swagger.models.properties.DateProperty;
 import io.swagger.models.properties.LongProperty;
 import io.swagger.models.properties.RefProperty;
 import io.swagger.models.properties.StringProperty;
@@ -28,6 +29,7 @@ public class TypeScriptAngular2ModelTest {
                 .property("id", new LongProperty())
                 .property("name", new StringProperty())
                 .property("createdAt", new DateTimeProperty())
+                .property("birthDate", new DateProperty())
                 .required("id")
                 .required("name");
         final DefaultCodegen codegen = new TypeScriptAngular2ClientCodegen();
@@ -36,7 +38,7 @@ public class TypeScriptAngular2ModelTest {
         Assert.assertEquals(cm.name, "sample");
         Assert.assertEquals(cm.classname, "Sample");
         Assert.assertEquals(cm.description, "a sample model");
-        Assert.assertEquals(cm.vars.size(), 3);
+        Assert.assertEquals(cm.vars.size(), 4);
 
         final CodegenProperty property1 = cm.vars.get(0);
         Assert.assertEquals(property1.baseName, "id");
@@ -63,10 +65,22 @@ public class TypeScriptAngular2ModelTest {
         Assert.assertEquals(property3.complexType, null);
         Assert.assertEquals(property3.datatype, "Date");
         Assert.assertEquals(property3.name, "createdAt");
+        Assert.assertEquals(property3.baseType, "Date");
         Assert.assertEquals(property3.defaultValue, "null");
-        Assert.assertFalse(property3.hasMore);
+        Assert.assertTrue(property3.hasMore);
         Assert.assertFalse(property3.required);
         Assert.assertTrue(property3.isNotContainer);
+
+        final CodegenProperty property4 = cm.vars.get(3);
+        Assert.assertEquals(property4.baseName, "birthDate");
+        Assert.assertEquals(property4.complexType, null);
+        Assert.assertEquals(property4.datatype, "string");
+        Assert.assertEquals(property4.name, "birthDate");
+        Assert.assertEquals(property4.baseType, "string");
+        Assert.assertEquals(property4.defaultValue, "null");
+        Assert.assertFalse(property4.hasMore);
+        Assert.assertFalse(property4.required);
+        Assert.assertTrue(property4.isNotContainer);
     }
 
     @Test(description = "convert a model with list property")
