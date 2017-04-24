36fd78a6ba17763539f270439b7888e1831d3459
Kenny Jones <kenjones-cisco@users.noreply.github.com>
2017-04-24 11:18:04 -0400

    Feature: Enable validation (#5391)
    When useBeanValidation is enabled make sure the annotation @Valid
    is added to the attributes of the model.

--- samples/server/petstore/springboot/src/main/java/io/swagger/model/EnumClass.java
+++ samples/server/petstore/springboot/src/main/java/io/swagger/model/EnumClass.java
@@ -2,7 +2,9 @@ package io.swagger.model;
 
 import java.util.Objects;
 import com.fasterxml.jackson.annotation.JsonValue;
+import javax.validation.Valid;
 import javax.validation.constraints.*;
+
 import com.fasterxml.jackson.annotation.JsonCreator;
 
 /**
