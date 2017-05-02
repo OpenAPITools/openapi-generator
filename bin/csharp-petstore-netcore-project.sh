4512619acfca303c6ebce02ae8177ccd61cb2275
Benjamin Douglas <bbdouglas@gmail.com>
2017-05-02 00:12:45 -0700

    Minor fixes to sample generation scripts (#5523)
    - typo in csharp-petstore-net-standard.sh (Stanard -> Standard)
    - non-existent config file name name in csharp-petstore-netcore-project.sh
    - non-existing config file name in ruby-petstore.sh
    - snapshot versioning in typesript-petstore-npm.json causes
      non-deterministic output

--- bin/csharp-petstore-netcore-project.sh
+++ bin/csharp-petstore-netcore-project.sh
@@ -26,6 +26,6 @@ fi
 
 # if you've executed sbt assembly previously it will use that instead.
 export JAVA_OPTS="${JAVA_OPTS} -XX:MaxPermSize=256M -Xmx1024M -DloggerPath=conf/log4j.properties"
-ags="generate $@ -i modules/swagger-codegen/src/test/resources/2_0/petstore-with-fake-endpoints-models-for-testing.yaml -l csharp -o samples/client/petstore/csharp/SwaggerClientNetCoreProject --additional-properties packageGuid={67035b31-f8e5-41a4-9673-954035084f7d},netCoreProjectFile=true -c ./bin/csharp-petstore-v5.json"
+ags="generate $@ -i modules/swagger-codegen/src/test/resources/2_0/petstore-with-fake-endpoints-models-for-testing.yaml -l csharp -o samples/client/petstore/csharp/SwaggerClientNetCoreProject --additional-properties packageGuid={67035b31-f8e5-41a4-9673-954035084f7d},netCoreProjectFile=true -c ./bin/csharp-petstore-net-standard.json"
 
 java $JAVA_OPTS -jar $executable $ags
