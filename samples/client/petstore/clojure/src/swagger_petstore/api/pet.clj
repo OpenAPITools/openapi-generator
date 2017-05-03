b872209f148c21a594c5f225f05bbb38d054a219
Benjamin Douglas <bdouglas@yelp.com>
2017-05-02 19:55:47 -0700

    Reverting petstore sample gen (fails tests)

--- samples/client/petstore/clojure/src/swagger_petstore/api/pet.clj
+++ samples/client/petstore/clojure/src/swagger_petstore/api/pet.clj
@@ -5,22 +5,24 @@
 (defn add-pet-with-http-info
   "Add a new pet to the store
   "
-  [body ]
-  (call-api "/pet" :post
-            {:path-params   {}
-             :header-params {}
-             :query-params  {}
-             :form-params   {}
-             :body-param    body
-             :content-types ["application/json" "application/xml"]
-             :accepts       ["application/xml" "application/json"]
-             :auth-names    ["petstore_auth"]}))
+  ([] (add-pet-with-http-info nil))
+  ([{:keys [body ]}]
+   (call-api "/pet" :post
+             {:path-params   {}
+              :header-params {}
+              :query-params  {}
+              :form-params   {}
+              :body-param    body
+              :content-types ["application/json" "application/xml"]
+              :accepts       ["application/json" "application/xml"]
+              :auth-names    ["petstore_auth"]})))
 
 (defn add-pet
   "Add a new pet to the store
   "
-  [body ]
-  (:data (add-pet-with-http-info body)))
+  ([] (add-pet nil))
+  ([optional-params]
+   (:data (add-pet-with-http-info optional-params))))
 
 (defn delete-pet-with-http-info
   "Deletes a pet
@@ -33,7 +35,7 @@
               :query-params  {}
               :form-params   {}
               :content-types []
-              :accepts       ["application/xml" "application/json"]
+              :accepts       ["application/json" "application/xml"]
               :auth-names    ["petstore_auth"]})))
 
 (defn delete-pet
@@ -46,44 +48,48 @@
 (defn find-pets-by-status-with-http-info
   "Finds Pets by status
   Multiple status values can be provided with comma separated strings"
-  [status ]
-  (call-api "/pet/findByStatus" :get
-            {:path-params   {}
-             :header-params {}
-             :query-params  {"status" (with-collection-format status :csv) }
-             :form-params   {}
-             :content-types []
-             :accepts       ["application/xml" "application/json"]
-             :auth-names    ["petstore_auth"]}))
+  ([] (find-pets-by-status-with-http-info nil))
+  ([{:keys [status ]}]
+   (call-api "/pet/findByStatus" :get
+             {:path-params   {}
+              :header-params {}
+              :query-params  {"status" (with-collection-format status :multi) }
+              :form-params   {}
+              :content-types []
+              :accepts       ["application/json" "application/xml"]
+              :auth-names    ["petstore_auth"]})))
 
 (defn find-pets-by-status
   "Finds Pets by status
   Multiple status values can be provided with comma separated strings"
-  [status ]
-  (:data (find-pets-by-status-with-http-info status)))
+  ([] (find-pets-by-status nil))
+  ([optional-params]
+   (:data (find-pets-by-status-with-http-info optional-params))))
 
 (defn find-pets-by-tags-with-http-info
   "Finds Pets by tags
   Multiple tags can be provided with comma separated strings. Use tag1, tag2, tag3 for testing."
-  [tags ]
-  (call-api "/pet/findByTags" :get
-            {:path-params   {}
-             :header-params {}
-             :query-params  {"tags" (with-collection-format tags :csv) }
-             :form-params   {}
-             :content-types []
-             :accepts       ["application/xml" "application/json"]
-             :auth-names    ["petstore_auth"]}))
+  ([] (find-pets-by-tags-with-http-info nil))
+  ([{:keys [tags ]}]
+   (call-api "/pet/findByTags" :get
+             {:path-params   {}
+              :header-params {}
+              :query-params  {"tags" (with-collection-format tags :multi) }
+              :form-params   {}
+              :content-types []
+              :accepts       ["application/json" "application/xml"]
+              :auth-names    ["petstore_auth"]})))
 
 (defn find-pets-by-tags
   "Finds Pets by tags
   Multiple tags can be provided with comma separated strings. Use tag1, tag2, tag3 for testing."
-  [tags ]
-  (:data (find-pets-by-tags-with-http-info tags)))
+  ([] (find-pets-by-tags nil))
+  ([optional-params]
+   (:data (find-pets-by-tags-with-http-info optional-params))))
 
 (defn get-pet-by-id-with-http-info
   "Find pet by ID
-  Returns a single pet"
+  Returns a pet when ID < 10.  ID > 10 or nonintegers will simulate API error conditions"
   [pet-id ]
   (call-api "/pet/{petId}" :get
             {:path-params   {"petId" pet-id }
@@ -91,34 +97,36 @@
              :query-params  {}
              :form-params   {}
              :content-types []
-             :accepts       ["application/xml" "application/json"]
-             :auth-names    ["api_key"]}))
+             :accepts       ["application/json" "application/xml"]
+             :auth-names    ["petstore_auth" "api_key"]}))
 
 (defn get-pet-by-id
   "Find pet by ID
-  Returns a single pet"
+  Returns a pet when ID < 10.  ID > 10 or nonintegers will simulate API error conditions"
   [pet-id ]
   (:data (get-pet-by-id-with-http-info pet-id)))
 
 (defn update-pet-with-http-info
   "Update an existing pet
   "
-  [body ]
-  (call-api "/pet" :put
-            {:path-params   {}
-             :header-params {}
-             :query-params  {}
-             :form-params   {}
-             :body-param    body
-             :content-types ["application/json" "application/xml"]
-             :accepts       ["application/xml" "application/json"]
-             :auth-names    ["petstore_auth"]}))
+  ([] (update-pet-with-http-info nil))
+  ([{:keys [body ]}]
+   (call-api "/pet" :put
+             {:path-params   {}
+              :header-params {}
+              :query-params  {}
+              :form-params   {}
+              :body-param    body
+              :content-types ["application/json" "application/xml"]
+              :accepts       ["application/json" "application/xml"]
+              :auth-names    ["petstore_auth"]})))
 
 (defn update-pet
   "Update an existing pet
   "
-  [body ]
-  (:data (update-pet-with-http-info body)))
+  ([] (update-pet nil))
+  ([optional-params]
+   (:data (update-pet-with-http-info optional-params))))
 
 (defn update-pet-with-form-with-http-info
   "Updates a pet in the store with form data
@@ -131,7 +139,7 @@
               :query-params  {}
               :form-params   {"name" name "status" status }
               :content-types ["application/x-www-form-urlencoded"]
-              :accepts       ["application/xml" "application/json"]
+              :accepts       ["application/json" "application/xml"]
               :auth-names    ["petstore_auth"]})))
 
 (defn update-pet-with-form
@@ -152,7 +160,7 @@
               :query-params  {}
               :form-params   {"additionalMetadata" additional-metadata "file" file }
               :content-types ["multipart/form-data"]
-              :accepts       ["application/json"]
+              :accepts       ["application/json" "application/xml"]
               :auth-names    ["petstore_auth"]})))
 
 (defn upload-file
