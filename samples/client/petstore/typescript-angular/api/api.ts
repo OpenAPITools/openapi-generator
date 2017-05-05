47d1a3d5c8bbbe8451b2f3a0a0214a7f900189d2
wing328 <wing328hk@gmail.com>
2017-05-05 17:05:45 +0800

    remove trailing comma in ts angular template (#5563)

--- samples/client/petstore/typescript-angular/api/api.ts
+++ samples/client/petstore/typescript-angular/api/api.ts
@@ -4,4 +4,4 @@ export * from './StoreApi';
 import { StoreApi } from './StoreApi';
 export * from './UserApi';
 import { UserApi } from './UserApi';
-export const APIS = [PetApi, StoreApi, UserApi, ];
+export const APIS = [PetApi, StoreApi, UserApi];
