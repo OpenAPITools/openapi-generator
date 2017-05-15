5b250aa18f3862fcdbdb69b414b84c6012b4c4e6
wing328 <wing328hk@gmail.com>
2017-05-15 16:11:51 +0800

    add warning message to window batch file

--- bin/windows/run-all-petstore.cmd
+++ bin/windows/run-all-petstore.cmd
@@ -1,3 +1,11 @@
+REM this batch file will loop through all the .bat files under bin\windows\
+REM execute the script and check the error level to see if there's any error
+
+echo IMPORTANT: this script should be run by the CI (e.g. appveyor) only. There's no need to run this script to update Petstore samples for all generators.
+echo Please press CTRL+C to stop or the script will continue in 10 seconds.
+
+timeout 10
+
 for /f "delims=" %%i in ('dir /b ".\bin\windows\*.bat"') do (
 
   CALL .\bin\windows\%%i
