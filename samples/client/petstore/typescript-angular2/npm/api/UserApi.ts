bd227df4db32161f0bd1847d8a95944cf31526bc
wing328 <wing328hk@gmail.com>
2017-05-24 15:55:34 +0800

    update ts angualr2 petstore clients

--- samples/client/petstore/typescript-angular2/npm/api/UserApi.ts
+++ samples/client/petstore/typescript-angular2/npm/api/UserApi.ts
@@ -27,6 +27,7 @@ import { Configuration }                                     from '../configurat
 
 @Injectable()
 export class UserApi {
+
     protected basePath = 'http://petstore.swagger.io/v2';
     public defaultHeaders: Headers = new Headers();
     public configuration: Configuration = new Configuration();
