6ee332a5130f471c264c266af29247a8004e9131
wing328 <wing328hk@gmail.com>
2017-06-05 22:22:56 +0800

    [Swift] Fix issue with Swift Petstore integration tests due to pod install (#5774)
    * add pod file for swift3
    
    * skip pod install
    
    * avoid pod install in swift3 petstore (promisekit,rxswift)
    
    * use https instead of http for cocoapod spec
    
    * avoid timeout in swift3 test

---
+++ samples/client/petstore/swift3/rxswift/SwaggerClientTests/Pods/Target Support Files/RxSwift/RxSwift-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double RxSwiftVersionNumber;
+FOUNDATION_EXPORT const unsigned char RxSwiftVersionString[];
+
--- samples/client/petstore/swift3/rxswift/SwaggerClientTests/run_xcodebuild.sh
+++ samples/client/petstore/swift3/rxswift/SwaggerClientTests/run_xcodebuild.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-pod install && xcodebuild -workspace "SwaggerClient.xcworkspace" -scheme "SwaggerClient" test -destination "platform=iOS Simulator,name=iPhone 6,OS=9.3" | xcpretty && exit ${PIPESTATUS[0]}
+xcodebuild -workspace "SwaggerClient.xcworkspace" -scheme "SwaggerClient" test -destination "platform=iOS Simulator,name=iPhone 6,OS=9.3" | xcpretty && exit ${PIPESTATUS[0]}
