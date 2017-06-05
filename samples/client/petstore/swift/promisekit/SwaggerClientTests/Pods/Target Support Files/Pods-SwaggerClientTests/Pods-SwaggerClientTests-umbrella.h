7931a892a3b540d7feaf351cba5e6a1d3adb38ea
wing328 <wing328hk@gmail.com>
2017-06-05 15:43:20 +0800

    Enable Travis CI tests for Swift, ObjC clients and move some tests to CircleCI (#5687)
    * enable travis CI tests for swift, objc, move some tests to circleci
    
    * fix comment in xml file
    
    * use xcode8.3
    
    * use ruby 2.2.5
    
    * fix objc core data pom.xml
    
    * use cocoapods 1.2.1
    
    * use xcpretty for clearer test results
    
    * fix pom with relative path to script
    
    * comment out objc test
    
    * fix xcpretty exit code
    
    * add && exit ${PIPESTATUS[0]} for swift3 xcodebuild script
    
    * use xcode 8.2
    
    * update promisekit version for swift 2x, 3x
    
    * add pod repo update
    
    * remove Pods directory
    
    * update swift dependencies to the latest version
    
    * update podfile.lock
    
    * rollback Alamofire to 4.0 for swift3
    
    * fix swift3 rxswift api cliiet
    
    * fix testDeletePet test in Swift3 rxswift petstore
    
    * update clojure petstore
    
    * comment out clojure test in travis (already covered in circleci)
    
    * run pestore server locally
    
    * use wing328/swagger-samples to run petstore
    
    * run petstore server in the background
    
    * test ruby petstore client
    
    * add /Users/travis/.cocoapods/repos/master to cache
    
    * add back ruby test, use public pestore server
    
    * add back bash client test
    
    * add npm config set registry to avoid time out
    
    * use docker branch in swagger samples
    
    * remove bash test
    
    * show go version, reorder
    
    * debug go petstore client
    
    * reinstall go
    
    * comment out pod repo update
    
    * uncomment pod repo update
    
    * test go in circleci
    
    * remove go from travis test
    
    * brew install sbt

--- samples/client/petstore/swift/promisekit/SwaggerClientTests/Pods/Target Support Files/Pods-SwaggerClientTests/Pods-SwaggerClientTests-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_SwaggerClientTestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_SwaggerClientTestsVersionString[];
-
--- samples/client/petstore/swift/rxswift/SwaggerClientTests/Pods/Target Support Files/Alamofire/Alamofire-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double AlamofireVersionNumber;
-FOUNDATION_EXPORT const unsigned char AlamofireVersionString[];
-
