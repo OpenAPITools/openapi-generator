ef8365ecd22f89074468b150666e74750fb473fd
Jason Gavris <jgavris@butterflynetinc.com>
2017-06-11 05:30:51 +0100

    [Swift3] Fix handling of query item with array value (#5684)

--- samples/client/petstore/swift3/rxswift/PetstoreClient/Classes/Swaggers/APIHelper.swift
+++ samples/client/petstore/swift3/rxswift/PetstoreClient/Classes/Swaggers/APIHelper.swift
@@ -49,16 +49,30 @@ class APIHelper {
         return destination
     }
 
-
     static func mapValuesToQueryItems(values: [String:Any?]) -> [URLQueryItem]? {
         let returnValues = values
             .filter { $0.1 != nil }
-            .map { (item: (_key: String, _value: Any?)) -> URLQueryItem in
-                URLQueryItem(name: item._key, value:"\(item._value!)")
+            .map { (item: (_key: String, _value: Any?)) -> [URLQueryItem] in
+                if let value = item._value as? Array<String> {
+                    return value.map { (v) -> URLQueryItem in
+                        URLQueryItem(
+                            name: item._key,
+                            value: v
+                        )
+                    }
+                } else {
+                    return [URLQueryItem(
+                        name: item._key,
+                        value: "\(item._value!)"
+                    )]
+                }
             }
+            .flatMap { $0 }
+
         if returnValues.count == 0 {
             return nil
         }
+
         return returnValues
     }
 
