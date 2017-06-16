ec448a61674f0cec0f47cbe5365f83cdca25844e
Paŭlo Ebermann <Paul-Ebermann@gmx.de>
2017-06-16 11:29:44 +0200

    [CLI] #5147: make -D work like system properties (#5191)
    * Issue #5147: allow empty values for properties.
    
    * Issue #5147: Allow multiple `-D` options.
    
    * Issue #5147: take advantage (and demonstrate) the new usage of -D.
    
    * Issue #5147: also update windows script and security ones.

--- bin/javascript-promise-petstore.sh
+++ bin/javascript-promise-petstore.sh
@@ -26,9 +26,12 @@ fi
 
 # if you've executed sbt assembly previously it will use that instead.
 export JAVA_OPTS="${JAVA_OPTS} -XX:MaxPermSize=256M -Xmx1024M -DloggerPath=conf/log4j.properties"
-ags="$@ generate -t modules/swagger-codegen/src/main/resources/Javascript \
--i modules/swagger-codegen/src/test/resources/2_0/petstore-with-fake-endpoints-models-for-testing.yaml -l javascript \
+ags="$@ generate \
+-t modules/swagger-codegen/src/main/resources/Javascript \
+-i modules/swagger-codegen/src/test/resources/2_0/petstore-with-fake-endpoints-models-for-testing.yaml \
+-l javascript \
 -o samples/client/petstore/javascript-promise \
---additional-properties usePromises=true"
+--additional-properties usePromises=true \
+-DappName=PetstoreClient"
 
-java -DappName=PetstoreClient $JAVA_OPTS -jar $executable $ags
+java $JAVA_OPTS -jar $executable $ags
