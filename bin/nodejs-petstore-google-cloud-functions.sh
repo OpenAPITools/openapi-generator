01fd1ddff8b545959248e75e957425833d58c51c
Paŭlo Ebermann <Paul-Ebermann@gmx.de>
2017-06-16 11:29:44 +0200

    [CLI] #5147: make -D work like system properties (#5191)
    * Issue #5147: allow empty values for properties.
    
    * Issue #5147: Allow multiple `-D` options.
    
    * Issue #5147: take advantage (and demonstrate) the new usage of -D.
    
    * Issue #5147: also update windows script and security ones.

--- bin/nodejs-petstore-google-cloud-functions.sh
+++ bin/nodejs-petstore-google-cloud-functions.sh
@@ -26,6 +26,6 @@ fi
 
 # if you've executed sbt assembly previously it will use that instead.
 export JAVA_OPTS="${JAVA_OPTS} -XX:MaxPermSize=256M -Xmx1024M -DloggerPath=conf/log4j.properties"
-ags="$@ generate -i modules/swagger-codegen/src/test/resources/2_0/petstore.yaml -l nodejs-server --additional-properties=googleCloudFunctions=true -o samples/server/petstore/nodejs-google-cloud-functions"
+ags="$@ generate -i modules/swagger-codegen/src/test/resources/2_0/petstore.yaml -l nodejs-server --additional-properties=googleCloudFunctions=true -o samples/server/petstore/nodejs-google-cloud-functions -Dservice"
 
-java $JAVA_OPTS -Dservice -jar $executable $ags
+java $JAVA_OPTS -jar $executable $ags
