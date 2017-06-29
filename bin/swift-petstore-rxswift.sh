a568cad42821262c495366cf7c049ec4e8a8fe98
wing328 <wing328hk@gmail.com>
2017-06-29 15:32:10 +0800

    update petstore script usage

--- bin/swift-petstore-rxswift.sh
+++ bin/swift-petstore-rxswift.sh
@@ -26,6 +26,6 @@ fi
 
 # if you've executed sbt assembly previously it will use that instead.
 export JAVA_OPTS="${JAVA_OPTS} -XX:MaxPermSize=256M -Xmx1024M -DloggerPath=conf/log4j.properties"
-ags="$@ generate -t modules/swagger-codegen/src/main/resources/swift -i modules/swagger-codegen/src/test/resources/2_0/petstore.json -l swift -c ./bin/swift-petstore-rxswift.json -o samples/client/petstore/swift/rxswift"
+ags="generate -t modules/swagger-codegen/src/main/resources/swift -i modules/swagger-codegen/src/test/resources/2_0/petstore.json -l swift -c ./bin/swift-petstore-rxswift.json -o samples/client/petstore/swift/rxswift $@"
 
 java $JAVA_OPTS -jar $executable $ags
