928551f7308d30c63884368c0c7e034062607b15
Jean-François Côté <jcote@stingray.com>
2017-07-03 11:08:48 -0400

    Typescript-Jquery refactor/improvement proposition (#5751)
    * First commit of the Java Play Framework server generator. It is highly based on Spring so there might me a couple of things that don't make sense (like options or parameters) for the Play Framework.
    
    * Fix suggestions in the PR discussion + add .bat and .sh file as requested.
    
    * Updated Readme.md file
    
    * Remove unused mustache file + fix baseName vs paramName in all the mustache files.
    
    * Fix the compilation error when we have a body which is a list or map. Doesn't fix the problem with the annotation itself.
    
    * Fix the problem with the Http.MultipartFormData.FilePart
    
    * First iteration of updating the typescript-jquery template/generator
    
    * first RC of the new version of Typescript-JQuery using better models.
    
    * Minor fix to the generation
    
    * first RC of the new version of Typescript-JQuery using better models.
    
    * Add an options for people using this client in a legacy app and that already have jquery loaded with a script tag somewhere
    
    * Generation of the samples based on the latest changes from the typescript-jquery generator
    
    * Fix to the check if the value is null and undefined
    
    * Small fix when using collection

---
+++ samples/client/petstore/typescript-jquery/default/api/api.ts
@@ -0,0 +1,7 @@
+export * from './PetApi';
+import { PetApi } from './PetApi';
+export * from './StoreApi';
+import { StoreApi } from './StoreApi';
+export * from './UserApi';
+import { UserApi } from './UserApi';
+export const APIS = [PetApi, StoreApi, UserApi];
