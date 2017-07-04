7452c6d1240e77684878ae993405b08778f70b60
Julien Fouilhé <julien@fouilhe.fr>
2017-07-04 05:21:25 +0200

    fix(swift3): add en_US_POSIX locale to date formatters (#5602)

--- samples/client/petstore/swift3/default/PetstoreClient/Classes/Swaggers/APIs/PetAPI.swift
+++ samples/client/petstore/swift3/default/PetstoreClient/Classes/Swaggers/APIs/PetAPI.swift
@@ -123,7 +123,7 @@ open class PetAPI: APIBase {
      - OAuth:
        - type: oauth2
        - name: petstore_auth
-     - examples: [{contentType=application/xml, example=<Pet>
+     - examples: [{example=<Pet>
   <id>123456789</id>
   <name>doggie</name>
   <photoUrls>
@@ -132,21 +132,21 @@ open class PetAPI: APIBase {
   <tags>
   </tags>
   <status>aeiou</status>
-</Pet>}, {contentType=application/json, example=[ {
-  "photoUrls" : [ "aeiou" ],
-  "name" : "doggie",
+</Pet>, contentType=application/xml}, {example=[ {
+  "tags" : [ {
+    "id" : 1,
+    "name" : "aeiou"
+  } ],
   "id" : 0,
   "category" : {
-    "name" : "aeiou",
-    "id" : 6
+    "id" : 6,
+    "name" : "aeiou"
   },
-  "tags" : [ {
-    "name" : "aeiou",
-    "id" : 1
-  } ],
-  "status" : "available"
-} ]}]
-     - examples: [{contentType=application/xml, example=<Pet>
+  "status" : "available",
+  "name" : "doggie",
+  "photoUrls" : [ "aeiou" ]
+} ], contentType=application/json}]
+     - examples: [{example=<Pet>
   <id>123456789</id>
   <name>doggie</name>
   <photoUrls>
@@ -155,20 +155,20 @@ open class PetAPI: APIBase {
   <tags>
   </tags>
   <status>aeiou</status>
-</Pet>}, {contentType=application/json, example=[ {
-  "photoUrls" : [ "aeiou" ],
-  "name" : "doggie",
+</Pet>, contentType=application/xml}, {example=[ {
+  "tags" : [ {
+    "id" : 1,
+    "name" : "aeiou"
+  } ],
   "id" : 0,
   "category" : {
-    "name" : "aeiou",
-    "id" : 6
+    "id" : 6,
+    "name" : "aeiou"
   },
-  "tags" : [ {
-    "name" : "aeiou",
-    "id" : 1
-  } ],
-  "status" : "available"
-} ]}]
+  "status" : "available",
+  "name" : "doggie",
+  "photoUrls" : [ "aeiou" ]
+} ], contentType=application/json}]
      
      - parameter status: (query) Status values that need to be considered for filter 
 
@@ -210,7 +210,7 @@ open class PetAPI: APIBase {
      - OAuth:
        - type: oauth2
        - name: petstore_auth
-     - examples: [{contentType=application/xml, example=<Pet>
+     - examples: [{example=<Pet>
   <id>123456789</id>
   <name>doggie</name>
   <photoUrls>
@@ -219,21 +219,21 @@ open class PetAPI: APIBase {
   <tags>
   </tags>
   <status>aeiou</status>
-</Pet>}, {contentType=application/json, example=[ {
-  "photoUrls" : [ "aeiou" ],
-  "name" : "doggie",
+</Pet>, contentType=application/xml}, {example=[ {
+  "tags" : [ {
+    "id" : 1,
+    "name" : "aeiou"
+  } ],
   "id" : 0,
   "category" : {
-    "name" : "aeiou",
-    "id" : 6
+    "id" : 6,
+    "name" : "aeiou"
   },
-  "tags" : [ {
-    "name" : "aeiou",
-    "id" : 1
-  } ],
-  "status" : "available"
-} ]}]
-     - examples: [{contentType=application/xml, example=<Pet>
+  "status" : "available",
+  "name" : "doggie",
+  "photoUrls" : [ "aeiou" ]
+} ], contentType=application/json}]
+     - examples: [{example=<Pet>
   <id>123456789</id>
   <name>doggie</name>
   <photoUrls>
@@ -242,20 +242,20 @@ open class PetAPI: APIBase {
   <tags>
   </tags>
   <status>aeiou</status>
-</Pet>}, {contentType=application/json, example=[ {
-  "photoUrls" : [ "aeiou" ],
-  "name" : "doggie",
+</Pet>, contentType=application/xml}, {example=[ {
+  "tags" : [ {
+    "id" : 1,
+    "name" : "aeiou"
+  } ],
   "id" : 0,
   "category" : {
-    "name" : "aeiou",
-    "id" : 6
+    "id" : 6,
+    "name" : "aeiou"
   },
-  "tags" : [ {
-    "name" : "aeiou",
-    "id" : 1
-  } ],
-  "status" : "available"
-} ]}]
+  "status" : "available",
+  "name" : "doggie",
+  "photoUrls" : [ "aeiou" ]
+} ], contentType=application/json}]
      
      - parameter tags: (query) Tags to filter by 
 
@@ -297,7 +297,7 @@ open class PetAPI: APIBase {
      - API Key:
        - type: apiKey api_key 
        - name: api_key
-     - examples: [{contentType=application/xml, example=<Pet>
+     - examples: [{example=<Pet>
   <id>123456789</id>
   <name>doggie</name>
   <photoUrls>
@@ -306,21 +306,21 @@ open class PetAPI: APIBase {
   <tags>
   </tags>
   <status>aeiou</status>
-</Pet>}, {contentType=application/json, example={
-  "photoUrls" : [ "aeiou" ],
-  "name" : "doggie",
+</Pet>, contentType=application/xml}, {example={
+  "tags" : [ {
+    "id" : 1,
+    "name" : "aeiou"
+  } ],
   "id" : 0,
   "category" : {
-    "name" : "aeiou",
-    "id" : 6
+    "id" : 6,
+    "name" : "aeiou"
   },
-  "tags" : [ {
-    "name" : "aeiou",
-    "id" : 1
-  } ],
-  "status" : "available"
-}}]
-     - examples: [{contentType=application/xml, example=<Pet>
+  "status" : "available",
+  "name" : "doggie",
+  "photoUrls" : [ "aeiou" ]
+}, contentType=application/json}]
+     - examples: [{example=<Pet>
   <id>123456789</id>
   <name>doggie</name>
   <photoUrls>
@@ -329,20 +329,20 @@ open class PetAPI: APIBase {
   <tags>
   </tags>
   <status>aeiou</status>
-</Pet>}, {contentType=application/json, example={
-  "photoUrls" : [ "aeiou" ],
-  "name" : "doggie",
+</Pet>, contentType=application/xml}, {example={
+  "tags" : [ {
+    "id" : 1,
+    "name" : "aeiou"
+  } ],
   "id" : 0,
   "category" : {
-    "name" : "aeiou",
-    "id" : 6
+    "id" : 6,
+    "name" : "aeiou"
   },
-  "tags" : [ {
-    "name" : "aeiou",
-    "id" : 1
-  } ],
-  "status" : "available"
-}}]
+  "status" : "available",
+  "name" : "doggie",
+  "photoUrls" : [ "aeiou" ]
+}, contentType=application/json}]
      
      - parameter petId: (path) ID of pet to return 
 
@@ -471,11 +471,11 @@ open class PetAPI: APIBase {
      - OAuth:
        - type: oauth2
        - name: petstore_auth
-     - examples: [{contentType=application/json, example={
+     - examples: [{example={
+  "message" : "aeiou",
   "code" : 0,
-  "type" : "aeiou",
-  "message" : "aeiou"
-}}]
+  "type" : "aeiou"
+}, contentType=application/json}]
      
      - parameter petId: (path) ID of pet to update 
      - parameter additionalMetadata: (form) Additional data to pass to server (optional)
