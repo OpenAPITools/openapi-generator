ddf9db3148a77d0807d4eedad19110513e4f82e8
wing328 <wing328hk@gmail.com>
2017-07-14 17:45:56 +0800

    fix window batch scripts after ts angular generator rename

--- bin/security/typescript-angular2.sh
+++ bin/security/typescript-angular2.sh
@@ -26,6 +26,6 @@ fi
 
 # if you've executed sbt assembly previously it will use that instead.
 export JAVA_OPTS="${JAVA_OPTS} -XX:MaxPermSize=256M -Xmx1024M -DloggerPath=conf/log4j.properties"
-ags="$@ generate -t modules/swagger-codegen/src/main/resources/typescript-angular2 -i modules/swagger-codegen/src/test/resources/2_0/petstore-security-test.yaml -l typescript-angular2 -o samples/client/petstore-security-test/typescript-angular2"
+ags="$@ generate -t modules/swagger-codegen/src/main/resources/typescript-angular2 -i modules/swagger-codegen/src/test/resources/2_0/petstore-security-test.yaml -l typescript-angular -o samples/client/petstore-security-test/typescript-angular2"
 
 java $JAVA_OPTS -jar $executable $ags
