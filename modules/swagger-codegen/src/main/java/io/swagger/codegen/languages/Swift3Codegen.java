cbfd0d7b5050492378f22ee392a840cc664973bd
julien-h-webcenter <julien.hamon@axa.fr>
2017-07-23 18:03:33 +0200

    fix add "Data" to reserved Keywords (#6011) (#6021)

--- modules/swagger-codegen/src/main/java/io/swagger/codegen/languages/Swift3Codegen.java
+++ modules/swagger-codegen/src/main/java/io/swagger/codegen/languages/Swift3Codegen.java
@@ -125,7 +125,7 @@ public class Swift3Codegen extends DefaultCodegen implements CodegenConfig {
                     "true", "lazy", "operator", "in", "COLUMN", "left", "private", "return", "FILE", "mutating", "protocol",
                     "switch", "FUNCTION", "none", "public", "where", "LINE", "nonmutating", "static", "while", "optional",
                     "struct", "override", "subscript", "postfix", "typealias", "precedence", "var", "prefix", "Protocol",
-                    "required", "right", "set", "Type", "unowned", "weak")
+                    "required", "right", "set", "Type", "unowned", "weak", "Data")
         );
 
         typeMapping = new HashMap<>();
