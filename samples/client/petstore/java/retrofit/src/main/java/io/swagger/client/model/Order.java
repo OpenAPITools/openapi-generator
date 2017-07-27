927055a681fb405eb202577cd6b0c6805662e3b8
wing328 <wing328hk@gmail.com>
2017-07-27 11:36:29 +0800

    [Java] MIME type "*/*" default to JSON (#6188)
    * update java petstore
    
    * minor fix to docstring
    
    * roll back resttemplate with xml petstore due to errors
    
    * minor fix to resttemplate isjson check

--- samples/client/petstore/java/retrofit/src/main/java/io/swagger/client/model/Order.java
+++ samples/client/petstore/java/retrofit/src/main/java/io/swagger/client/model/Order.java
@@ -196,7 +196,7 @@ public class Order {
    * @return complete
   **/
   @ApiModelProperty(value = "")
-  public Boolean getComplete() {
+  public Boolean isComplete() {
     return complete;
   }
 
--- samples/client/petstore/java/retrofit2rx2/src/main/java/io/swagger/client/model/Cat.java
+++ samples/client/petstore/java/retrofit2rx2/src/main/java/io/swagger/client/model/Cat.java
@@ -42,7 +42,7 @@ public class Cat extends Animal {
    * @return declawed
   **/
   @ApiModelProperty(value = "")
-  public Boolean getDeclawed() {
+  public Boolean isDeclawed() {
     return declawed;
   }
 
