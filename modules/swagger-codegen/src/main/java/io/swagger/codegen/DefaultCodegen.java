1f1fd605dd610fb1606bfbfd5387da79a62bde32
mobreza <matija.obreza@gmail.com>
2017-08-03 16:44:17 +0800

    Improved support for XML wrapped items (#6150)
    * Improved support for XML wrapped items
    
    * XML @annotation whitespace fixed

--- modules/swagger-codegen/src/main/java/io/swagger/codegen/DefaultCodegen.java
+++ modules/swagger-codegen/src/main/java/io/swagger/codegen/DefaultCodegen.java
@@ -1816,13 +1816,19 @@ public class DefaultCodegen {
             property.isListContainer = true;
             property.containerType = "array";
             property.baseType = getSwaggerType(p);
+            if (p.getXml() != null) {
+              property.isXmlWrapped = p.getXml().getWrapped() == null ? false : p.getXml().getWrapped();
+              property.xmlPrefix= p.getXml().getPrefix();
+              property.xmlNamespace = p.getXml().getNamespace();
+              property.xmlName = p.getXml().getName();
+            }
             // handle inner property
             ArrayProperty ap = (ArrayProperty) p;
             property.maxItems = ap.getMaxItems();
             property.minItems = ap.getMinItems();
             String itemName = (String) p.getVendorExtensions().get("x-item-name");
             if (itemName == null) {
-                itemName = property.name;
+              itemName = property.name;
             }
             CodegenProperty cp = fromProperty(itemName, ap.getItems());
             updatePropertyForArray(property, cp);
