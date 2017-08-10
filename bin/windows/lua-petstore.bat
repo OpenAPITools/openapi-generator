8939635f1ece5269d452ff1736bed14889532f32
wing328 <wing328hk@gmail.com>
2017-08-11 01:41:29 +0800

    [Lua] Add new Lua client generator (#6252)
    * 1st commit of lua generator
    
    * add petstore sample
    
    * various fixes based on feedback
    
    * better return handling
    
    * check status code 1st letter
    
    * add support for query parameters, auth, body param
    
    * more fixes based on daurnimator feedback
    
    * more fixes based on daurnimator feedback
    
    * fix a few more issues found by luac -p
    
    * use lower case for http header key name
    
    * more fixes based on feedback
    
    * update lua sample
    
    * skip auto-generated readme, doc, test files
    
    * use tab instead of 2-space
    
    * fix container return type
    
    * fix cast method call
    
    * fix cast function call

---
+++ bin/windows/lua-petstore.bat
@@ -0,0 +1,10 @@
+set executable=.\modules\swagger-codegen-cli\target\swagger-codegen-cli.jar
+
+If Not Exist %executable% (
+  mvn clean package
+)
+
+REM set JAVA_OPTS=%JAVA_OPTS% -Xmx1024M -DloggerPath=conf/log4j.properties
+set ags=generate -i modules\swagger-codegen\src\test\resources\2_0\petstore.yaml -l lua -o samples\client\petstore\lua
+
+java %JAVA_OPTS% -jar %executable% %ags%
