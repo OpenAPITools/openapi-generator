4ee557aa246a2187b0b1c59ce309d90cda36a025
Krzysztof Karol <Krzysztof.Karol@gmail.com>
2017-08-24 04:39:34 +0200

    Fix swagger-api/swagger-codegen#6345. Wrap optional JSDoc parameter (apiClient) in square brackets (#6346)

--- samples/client/petstore/javascript-promise-es6/src/api/FakeApi.js
+++ samples/client/petstore/javascript-promise-es6/src/api/FakeApi.js
@@ -30,7 +30,7 @@ export default class FakeApi {
     * Constructs a new FakeApi. 
     * @alias module:api/FakeApi
     * @class
-    * @param {module:ApiClient} apiClient Optional API client implementation to use,
+    * @param {module:ApiClient} [apiClient] Optional API client implementation to use,
     * default to {@link module:ApiClient#instance} if unspecified.
     */
     constructor(apiClient) {
--- samples/client/petstore/javascript-promise/src/api/UserApi.js
+++ samples/client/petstore/javascript-promise/src/api/UserApi.js
@@ -41,7 +41,7 @@
    * Constructs a new UserApi. 
    * @alias module:api/UserApi
    * @class
-   * @param {module:ApiClient} apiClient Optional API client implementation to use,
+   * @param {module:ApiClient} [apiClient] Optional API client implementation to use,
    * default to {@link module:ApiClient#instance} if unspecified.
    */
   var exports = function(apiClient) {
