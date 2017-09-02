87d6751fff50bdbc476f7d7dfeab607cdb5d4ae6
wing328 <wing328hk@gmail.com>
2017-09-02 17:35:46 +0800

    fix file upload in retrofit2 (without play) (#6425)

--- samples/client/petstore/java/retrofit2/src/main/java/io/swagger/client/api/PetApi.java
+++ samples/client/petstore/java/retrofit2/src/main/java/io/swagger/client/api/PetApi.java
@@ -115,7 +115,7 @@ public interface PetApi {
   @retrofit2.http.Multipart
   @POST("pet/{petId}/uploadImage")
   Call<ModelApiResponse> uploadFile(
-    @retrofit2.http.Path("petId") Long petId, @retrofit2.http.Part("additionalMetadata") String additionalMetadata, @retrofit2.http.Part okhttp3.MultipartBody.Part file
+    @retrofit2.http.Path("petId") Long petId, @retrofit2.http.Part("additionalMetadata") String additionalMetadata, @retrofit2.http.Part("file\"; filename=\"file") RequestBody file
   );
 
 }
