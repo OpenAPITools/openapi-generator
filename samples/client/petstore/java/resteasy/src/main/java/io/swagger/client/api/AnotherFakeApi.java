639f88918a1500a799c360fc4c7ff7ceb3aff25c
wing328 <wing328hk@gmail.com>
2017-09-24 12:49:43 +0800

    [Java][RESTEasy] fix resteasy dependency issue in pom.xml (#6556)
    * fix resteasy dependency issue
    
    * add java resteasy petstore to circleci test

---
+++ samples/client/petstore/java/resteasy/src/main/java/io/swagger/client/api/AnotherFakeApi.java
@@ -0,0 +1,78 @@
+package io.swagger.client.api;
+
+import io.swagger.client.ApiException;
+import io.swagger.client.ApiClient;
+import io.swagger.client.Configuration;
+import io.swagger.client.Pair;
+
+import javax.ws.rs.core.GenericType;
+
+import io.swagger.client.model.Client;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+public class AnotherFakeApi {
+  private ApiClient apiClient;
+
+  public AnotherFakeApi() {
+    this(Configuration.getDefaultApiClient());
+  }
+
+  public AnotherFakeApi(ApiClient apiClient) {
+    this.apiClient = apiClient;
+  }
+
+  public ApiClient getApiClient() {
+    return apiClient;
+  }
+
+  public void setApiClient(ApiClient apiClient) {
+    this.apiClient = apiClient;
+  }
+
+  /**
+   * To test special tags
+   * To test special tags
+   * @param body client model (required)
+   * @return Client
+   * @throws ApiException if fails to make API call
+   */
+  public Client testSpecialTags(Client body) throws ApiException {
+    Object localVarPostBody = body;
+    
+    // verify the required parameter 'body' is set
+    if (body == null) {
+      throw new ApiException(400, "Missing the required parameter 'body' when calling testSpecialTags");
+    }
+    
+    // create path and map variables
+    String localVarPath = "/another-fake/dummy".replaceAll("\\{format\\}","json");
+
+    // query params
+    List<Pair> localVarQueryParams = new ArrayList<Pair>();
+    Map<String, String> localVarHeaderParams = new HashMap<String, String>();
+    Map<String, Object> localVarFormParams = new HashMap<String, Object>();
+
+
+    
+    
+    final String[] localVarAccepts = {
+      "application/json"
+    };
+    final String localVarAccept = apiClient.selectHeaderAccept(localVarAccepts);
+
+    final String[] localVarContentTypes = {
+      "application/json"
+    };
+    final String localVarContentType = apiClient.selectHeaderContentType(localVarContentTypes);
+
+    String[] localVarAuthNames = new String[] {  };
+
+    GenericType<Client> localVarReturnType = new GenericType<Client>() {};
+    return apiClient.invokeAPI(localVarPath, "PATCH", localVarQueryParams, localVarPostBody, localVarHeaderParams, localVarFormParams, localVarAccept, localVarContentType, localVarAuthNames, localVarReturnType);
+      }
+}
