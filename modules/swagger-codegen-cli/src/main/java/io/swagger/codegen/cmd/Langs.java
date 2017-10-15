026426e50cf411631b1ed40fe0852a71f46c1d98
Hugo Mercado <hugo.mario.mercado@gmail.com>
2017-10-15 09:47:20 -0500

    Revert "Merge pull request #6668 from swagger-api/issue-6041"
    This reverts commit 3c399a335c5b8dd50b4a333562a0754e2ee88075, reversing
    changes made to c690ea83d36e094e282b9db561d280f9bf42b682.

--- modules/swagger-codegen-cli/src/main/java/io/swagger/codegen/cmd/Langs.java
+++ modules/swagger-codegen-cli/src/main/java/io/swagger/codegen/cmd/Langs.java
@@ -1,6 +1,7 @@
 package io.swagger.codegen.cmd;
 
 import ch.lambdaj.collection.LambdaIterable;
+import io.airlift.airline.Command;
 import io.swagger.codegen.CodegenConfig;
 
 import static ch.lambdaj.Lambda.on;
@@ -10,6 +11,7 @@ import static java.util.ServiceLoader.load;
 /**
  * User: lanwen Date: 24.03.15 Time: 20:25
  */
+@Command(name = "langs", description = "Shows available langs")
 public class Langs implements Runnable {
     @Override
     public void run() {
