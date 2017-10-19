7ba2e14c851102164a9d83b75340a0c1d6e10093
wing328 <wing328hk@gmail.com>
2017-10-19 20:17:08 +0800

    [Rust] rename rust2 to rust-server (#6747)
    * rename rust2 to rust-server
    
    * update rust-server batch file to use petstore test spec

--- samples/server/petstore/rust-server/src/models.rs
+++ samples/server/petstore/rust-server/src/models.rs
@@ -481,76 +481,6 @@ impl HasOnlyReadOnly {
     }
 }
 
-// Utility function for wrapping list elements when serializing xml
-fn wrap_in_item<S>(item: &Vec<String>, serializer: S) -> Result<S::Ok, S::Error>
-where
-    S: Serializer,
-{
-    serde_xml_rs::wrap_primitives(item, serializer, "item")
-}
-
-#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
-pub struct ItemList(Vec<String>);
-
-impl ::std::convert::From<Vec<String>> for ItemList {
-    fn from(x: Vec<String>) -> Self {
-        ItemList(x)
-    }
-}
-
-impl ::std::convert::From<ItemList> for Vec<String> {
-    fn from(x: ItemList) -> Self {
-        x.0
-    }
-}
-
-impl ::std::iter::FromIterator<String> for ItemList {
-    fn from_iter<U: IntoIterator<Item=String>>(u: U) -> Self {
-        ItemList(Vec::<String>::from_iter(u))
-    }
-}
-
-impl ::std::iter::IntoIterator for ItemList {
-    type Item = String;
-    type IntoIter = ::std::vec::IntoIter<String>;
-
-    fn into_iter(self) -> Self::IntoIter {
-        self.0.into_iter()
-    }
-}
-
-impl<'a> ::std::iter::IntoIterator for &'a ItemList {
-    type Item = &'a String;
-    type IntoIter = ::std::slice::Iter<'a, String>;
-
-    fn into_iter(self) -> Self::IntoIter {
-        (&self.0).into_iter()
-    }
-}
-
-impl<'a> ::std::iter::IntoIterator for &'a mut ItemList {
-    type Item = &'a mut String;
-    type IntoIter = ::std::slice::IterMut<'a, String>;
-
-    fn into_iter(self) -> Self::IntoIter {
-        (&mut self.0).into_iter()
-    }
-}
-
-impl ::std::ops::Deref for ItemList {
-    type Target = Vec<String>;
-    fn deref(&self) -> &Self::Target {
-        &self.0
-    }
-}
-
-impl ::std::ops::DerefMut for ItemList {
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        &mut self.0
-    }
-}
-
-
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
 pub struct List {
     #[serde(rename = "123-list")]
@@ -1057,27 +987,6 @@ impl User {
 }
 
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
-#[serde(rename = "group")]
-pub struct XmlObject {
-    #[serde(rename = "name")]
-    pub name: String,
-
-    #[serde(rename = "itemList")]
-    #[serde(serialize_with = "wrap_in_item")]
-    pub item_list: models::ItemList,
-
-}
-
-impl XmlObject {
-    pub fn new(name: String, item_list: models::ItemList, ) -> XmlObject {
-        XmlObject {
-            name: name,
-            item_list: item_list,
-        }
-    }
-}
-
-#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
 pub struct Cat {
     #[serde(rename = "className")]
     pub class_name: String,
@@ -1124,13 +1033,5 @@ impl Dog {
             color: Some("red".to_string()),
             breed: None,
         }
-    }
-}
-
-//XML namespaces
-pub mod namespaces {
-    lazy_static!{
-        pub static ref XMLOBJECT: String = "http://example.com/schema.xsd".to_string();
-        
     }
 }
