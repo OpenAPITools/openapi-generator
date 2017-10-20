c633ab68c242aa334556e1309bf665f74c277080
Hugo Mercado <hugo.mario.mercado@gmail.com>
2017-10-20 02:09:27 -0500

    fixed reference on jaxrs test classes.

--- modules/swagger-codegen/src/test/java/io/swagger/codegen/jaxrs/JaxrsJava8ModelTest.java
+++ modules/swagger-codegen/src/test/java/io/swagger/codegen/jaxrs/JaxrsJava8ModelTest.java
@@ -1,13 +1,12 @@
 package io.swagger.codegen.jaxrs;
 
 import io.swagger.codegen.CodegenModel;
-
 import io.swagger.codegen.languages.JavaJerseyServerCodegen;
-import io.swagger.models.Model;
-import io.swagger.models.ModelImpl;
-import io.swagger.models.properties.DateProperty;
-import io.swagger.models.properties.DateTimeProperty;
-import io.swagger.models.properties.LongProperty;
+import io.swagger.oas.models.media.DateSchema;
+import io.swagger.oas.models.media.DateTimeSchema;
+import io.swagger.oas.models.media.IntegerSchema;
+import io.swagger.oas.models.media.Schema;
+import io.swagger.parser.v3.util.SchemaTypeUtil;
 import io.swagger.util.Json;
 import org.testng.annotations.Test;
 
@@ -18,13 +17,13 @@ public class JaxrsJava8ModelTest {
     
     @Test(description = "convert a simple java model with java8 types")
     public void simpleModelTest() {
-        final Model model = new ModelImpl()
+        final Schema model = new Schema()
                 .description("a sample model")
-                .property("id", new LongProperty())
-                .property("theDate", new DateProperty())
-                .property("createdAt", new DateTimeProperty())
-                .required("id")
-                .required("name");
+                .addProperties("id", new IntegerSchema().format(SchemaTypeUtil.INTEGER64_FORMAT))
+                .addProperties("theDate", new DateSchema())
+                .addProperties("createdAt", new DateTimeSchema())
+                .addRequiredItem("id")
+                .addRequiredItem("name");
 
         final JavaJerseyServerCodegen codegen = new JavaJerseyServerCodegen();
         codegen.setDateLibrary("java8");
