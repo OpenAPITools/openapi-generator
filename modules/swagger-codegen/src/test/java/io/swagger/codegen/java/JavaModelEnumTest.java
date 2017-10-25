702007660e072b4b853da67594380dce198a4e2b
Hugo Mercado <hugo.mario.mercado@gmail.com>
2017-10-25 15:34:29 -0500

    added test on schema sample object.

--- modules/swagger-codegen/src/test/java/io/swagger/codegen/java/JavaModelEnumTest.java
+++ modules/swagger-codegen/src/test/java/io/swagger/codegen/java/JavaModelEnumTest.java
@@ -22,7 +22,7 @@ public class JavaModelEnumTest {
     public void converterTest() {
         final StringSchema enumSchema = new StringSchema();
         enumSchema.setEnum(Arrays.asList("VALUE1", "VALUE2", "VALUE3"));
-        final Schema model = new Schema().addProperties("name", enumSchema);
+        final Schema model = new Schema().type("object").addProperties("name", enumSchema);
 
         final DefaultCodegen codegen = new JavaClientCodegen();
         final CodegenModel cm = codegen.fromModel("sample", model);
