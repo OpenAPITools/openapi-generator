c3ba7b14c90ae54a8e4f8baf08811764d2e545fc
Kenny Jones <kenjones-cisco@users.noreply.github.com>
2017-10-26 08:38:30 -0400

    [python-flask] Create main function and entrypoint (#6814)
    Enable the creation of the entrypoint so that the server can be started
    using a named command instead of needing to always use `python -m` to
    run the server.

--- samples/server/petstore/flaskConnexion/setup.py
+++ samples/server/petstore/flaskConnexion/setup.py
@@ -26,6 +26,8 @@ setup(
     packages=find_packages(),
     package_data={'': ['swagger/swagger.yaml']},
     include_package_data=True,
+    entry_points={
+        'console_scripts': ['swagger_server=swagger_server.__main__:main']},
     long_description="""\
     This is a sample server Petstore server.  You can find out more about Swagger at [http://swagger.io](http://swagger.io) or on [irc.freenode.net, #swagger](http://swagger.io/irc/).  For this sample, you can use the api key &#x60;special-key&#x60; to test the authorization filters.
     """
