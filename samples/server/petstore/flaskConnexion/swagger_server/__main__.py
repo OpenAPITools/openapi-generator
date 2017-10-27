28d14e34c4180eb943624e8edfde130af035217a
Kenny Jones <kenjones-cisco@users.noreply.github.com>
2017-10-27 10:18:14 -0400

    [python-flask] Apply template tweaks to improve lint results (#6826)
    The linting results for the generated samples are as follows
    where the first number is the BEFORE and the second is AFTER.
    
    flaskConnexion          1843 vs. 20
    flaskConnexion-python2  1841 vs. 19
    
    For the complete details please see the following gist.
    https://gist.github.com/kenjones-cisco/edc9d71ef7fd2bf23714ecbb693d52b3

--- samples/server/petstore/flaskConnexion/swagger_server/__main__.py
+++ samples/server/petstore/flaskConnexion/swagger_server/__main__.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 
 import connexion
-from .encoder import JSONEncoder
+
+from swagger_server import encoder
 
 
 def main():
     app = connexion.App(__name__, specification_dir='./swagger/')
-    app.app.json_encoder = JSONEncoder
-    app.add_api('swagger.yaml', arguments={'title': 'This is a sample server Petstore server.  You can find out more about Swagger at [http://swagger.io](http://swagger.io) or on [irc.freenode.net, #swagger](http://swagger.io/irc/).  For this sample, you can use the api key &#x60;special-key&#x60; to test the authorization filters.'})
+    app.app.json_encoder = encoder.JSONEncoder
+    app.add_api('swagger.yaml', arguments={'title': 'Swagger Petstore'})
     app.run(port=8080)
 
 
