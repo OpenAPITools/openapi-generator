29e5f27b03a832a4f0d2ed69340b86c780a87176
craffael <raffael@casagrande.ch>
2017-11-27 09:46:09 +0100

    [NancyFx] Fix for issue #7024, date field mapped to ZonedDateTime (#7025)
    * add support for async routes/endpoints
    
    you can now set --optional-properties async=true to generate
    a nancyfx server stub that uses asynchronous programming.
    
    (cherry picked from commit 126869cb0b967e8063417e11993cf6326ce8ffd4)
    
    * add nancyfx-petstore-server-async.sh to generate sample of async nancyfx.
    
    * Rename async => asyncServer
    
    * update bin/nancyfx-petstore-server-async.sh
    
    * rename async => asyncServer in api.mustache + small bugfix
    
    * run ./bin/nancyfx-petstore-server.sh and ./bin/nancyfx-petstore-server-async.sh
    
    * remove additional new line in api.mustache + add space after if
    
    * run ./bin/nancyfx-petstore-server.sh and ./bin/nancyfx-petstore-server-async.sh
    
    * Map `date` fields to LocalDate c# type + add a JsonConverter for LocalDate.

--- samples/server/petstore/nancyfx-async/src/IO.Swagger/Modules/PetModule.cs
+++ samples/server/petstore/nancyfx-async/src/IO.Swagger/Modules/PetModule.cs
@@ -15,9 +15,9 @@ namespace IO.Swagger.v2.Modules
     /// </summary>
     public enum FindPetsByStatusStatusEnum
     {
-        available, 
-        pending, 
-        sold
+        available = 1, 
+        pending = 2, 
+        sold = 3
     };
 
 
