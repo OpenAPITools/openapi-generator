64b6495d6bd4d22e06cdfa33bd87200b6e2c3548
Hugo Mercado <hugo.mario.mercado@gmail.com>
2017-12-08 00:00:12 -0500

    allow extension helpers to work with maps.

--- modules/swagger-codegen/src/main/java/io/swagger/codegen/languages/helpers/ExtensionHelper.java
+++ modules/swagger-codegen/src/main/java/io/swagger/codegen/languages/helpers/ExtensionHelper.java
@@ -8,22 +8,32 @@ import io.swagger.codegen.VendorExtendable;
 import java.io.IOException;
 import java.util.Map;
 
-public abstract class ExtensionHelper implements Helper<VendorExtendable> {
+public abstract class ExtensionHelper implements Helper {
 
     public abstract String getPreffix();
 
     @Override
-    public Object apply(VendorExtendable vendor, Options options) throws IOException {
+    public Object apply(Object object, Options options) throws IOException {
         final Buffer buffer = options.buffer();
 
-        if (vendor == null) {
+        if (object == null) {
             buffer.append(options.inverse());
             return buffer;
         }
         final String param = options.param(0);
         String extension = getPreffix() + param;
 
-        if (!getBooleanValue(vendor, extension)) {
+        final boolean extensionValue;
+
+        if (object instanceof VendorExtendable) {
+            extensionValue = getBooleanValue((VendorExtendable)object, extension);
+        } else if (object instanceof Map) {
+            extensionValue = getBooleanValue((Map)object, extension);
+        } else {
+            throw new RuntimeException(String.format("%s is not a valid class for this operation.", object.getClass().getName()));
+        }
+
+        if (!extensionValue) {
             buffer.append(options.inverse());
         } else {
             buffer.append(options.fn());
@@ -35,7 +45,13 @@ public abstract class ExtensionHelper implements Helper<VendorExtendable> {
         if (vendorExtendable == null) {
             return false;
         }
-        Map<String, Object> vendorExtensions = vendorExtendable.getVendorExtensions();
+        return getBooleanValue(vendorExtendable.getVendorExtensions(), extensionKey);
+    }
+
+    public static boolean getBooleanValue(Map<String, Object> vendorExtensions, String extensionKey) {
+        if (vendorExtensions == null ||vendorExtensions.isEmpty()) {
+            return false;
+        }
         if (vendorExtensions.get(extensionKey) == null) {
             return false;
         }
--- modules/swagger-codegen/src/main/java/io/swagger/codegen/languages/helpers/NoneExtensionHelper.java
+++ modules/swagger-codegen/src/main/java/io/swagger/codegen/languages/helpers/NoneExtensionHelper.java
@@ -5,25 +5,36 @@ import com.github.jknack.handlebars.Options;
 import io.swagger.codegen.VendorExtendable;
 
 import java.io.IOException;
+import java.util.Map;
 
 import static io.swagger.codegen.VendorExtendable.PREFIX_IS;
 import static io.swagger.codegen.languages.helpers.ExtensionHelper.getBooleanValue;
 
-public abstract class NoneExtensionHelper implements Helper<VendorExtendable> {
+public abstract class NoneExtensionHelper implements Helper {
 
     public abstract String getPreffix();
 
     @Override
-    public Object apply(VendorExtendable vendor, Options options) throws IOException {
+    public Object apply(Object object, Options options) throws IOException {
         final Options.Buffer buffer = options.buffer();
-        if (vendor == null) {
+        if (object == null) {
             buffer.append(options.fn());
             return buffer;
         }
         final String param = options.param(0);
         String extension = getPreffix() + param;
 
-        if (!getBooleanValue(vendor, extension)) {
+        final boolean extensionValue;
+
+        if (object instanceof VendorExtendable) {
+            extensionValue = getBooleanValue((VendorExtendable)object, extension);
+        } else if (object instanceof Map) {
+            extensionValue = getBooleanValue((Map)object, extension);
+        } else {
+            throw new RuntimeException(String.format("%s is not a valid class for this operation.", object.getClass().getName()));
+        }
+
+        if (!extensionValue) {
             buffer.append(options.fn());
         } else {
             buffer.append(options.inverse());
