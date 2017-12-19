662227cee91cf2c6401dde444c6c5cc7bf7d276d
sanshuman <sanshuman@prokarma.com>
2017-12-19 13:42:44 +0530

    pkmst- Prokarma microservice toolkit for java (#7023)
    * Added pkmst-microservice toolkit
    
    * formatted
    
    * replaces tab with blank spaces
    
    * adding documentation folder
    
    * adding documentation folder
    
    * Update readme.mustache
    
    Added feature set offered by pkmst
    
    * updated readme
    
    * Update io.swagger.codegen.CodegenConfig
    
    Arranged pkmstServerCodeGen and SymfonyServerCodegen in aphabatical order
    
    * Update readme.mustache
    
    * renamed according to convention and added typeinfoannotation
    
    * removed tabs
    
    * updated codegen which extends abstractjavacodegen
    
    * Added the script files and rectified the errors
    
    * folder name changed and commons -lang3 version upgraded
    
    * updated readme.mustache
    
    * updated logging filter
    
    * Added petstore to samples and updated the pom for the same
    
    * Added java-pkmst sample to pom.xml.circleci
    
    * updated readme.mustache

---
+++ bin/java-petstore-pkmst-server.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+SCRIPT="$0"
+
+while [ -h "$SCRIPT" ] ; do
+  ls=`ls -ld "$SCRIPT"`
+  link=`expr "$ls" : '.*-> \(.*\)$'`
+  if expr "$link" : '/.*' > /dev/null; then
+    SCRIPT="$link"
+  else
+    SCRIPT=`dirname "$SCRIPT"`/"$link"
+  fi
+done
+
+if [ ! -d "${APP_DIR}" ]; then
+  APP_DIR=`dirname "$SCRIPT"`/..
+  APP_DIR=`cd "${APP_DIR}"; pwd`
+fi
+
+executable="./modules/swagger-codegen-cli/target/swagger-codegen-cli.jar"
+
+if [ ! -f "$executable" ]
+then
+  mvn clean package
+fi
+
+# if you've executed sbt assembly previously it will use that instead.
+export JAVA_OPTS="${JAVA_OPTS} -XX:MaxPermSize=256M -Xmx1024M -DloggerPath=conf/log4j.properties"
+ags="$@ generate -t modules/swagger-codegen/src/main/resources/pkmst -i modules/swagger-codegen/src/test/resources/2_0/petstore-with-fake-endpoints-models-for-testing.yaml -l pkmst -o samples/server/petstore/java-pkmst/ -DhideGenerationTimestamp=true"
+
+echo "Removing files and folders under samples/server/petstore/java-pkmst/src/main"
+rm -rf samples/server/petstore/java-pkmst/src/main
+find samples/server/petstore/java-pkmst -maxdepth 1 -type f ! -name "README.md" ! -name "pom.xml" ! -name "mvn_test_jdk8_only.sh" ! -name ".swagger-codegen-ignore" -exec rm {} +
+java $JAVA_OPTS -jar $executable $ags
---
+++ samples/server/petstore/java-pkmst/src/test/java/com/prokarma/pkmst/cucumber/Pkmstpackage-info.java
@@ -0,0 +1,2 @@
+package com.prokarma.pkmst.cucumber;
+//Implement the feature file steps in this package.
