6d2d4298df16796c95faa5c95efdf35c67ec1be0
ryandavis84 <ryan.davis@sophos.com>
2018-01-07 04:18:36 -0500

    Gatling generator2 issue #6927 (#6934)
    * Initial commit, Generates everything necessary to run a performnace test against a swagger api.  Just have to fill out the CSV feeder files with your data.
    
    * adding samples and gatling-petstore.sh file
    
    * Extending the AbstractScalaCodeGen
    
    * Checking in the CodegenConfig file as it is needed to generate
    
    * removing escaped reserved words
    
    * Changed model to be able to make all variables utilize an underscore while json fields are still just the variable name
    
    * Changing underscore to var as interpolation can not start with a _ in scala
    
    * Fixing path params
    
    * allow you to pass in a system property to define which config to use as a workload profile, use rate and instance multipliers to scale up and down your test, added ramp down after the test is completed, added global assertions.
    
    * Addressing PR feedback
    
    * missed semi-colon
    
    * Bringing everything up to date with the renames that were suggested

---
+++ samples/client/petstore/scala-gatling/src/gatling/scala/io/swagger/client/model/Pet.scala
@@ -0,0 +1,21 @@
+
+package io.swagger.client.model
+
+
+case class Pet (
+    _id: Option[Long],
+    _category: Option[Category],
+    _name: String,
+    _photoUrls: List[String],
+    _tags: Option[List[Tag]],
+    /* pet status in the store */
+    _status: Option[String]
+)
+object Pet {
+    def toStringBody(var_id: Object, var_category: Object, var_name: Object, var_photoUrls: Object, var_tags: Object, var_status: Object) =
+        s"""
+        | {
+        | "id":$var_id,"category":$var_category,"name":$var_name,"photoUrls":$var_photoUrls,"tags":$var_tags,"status":$var_status
+        | }
+        """.stripMargin
+}
