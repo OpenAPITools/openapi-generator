362625bfa9471c0b4bcb88b34ea287c11765179a
Stephane Carrez <Stephane.Carrez@gmail.com>
2018-01-07 11:44:20 +0100

    [Ada] Add Ada support for server code generator #6680 (#7256)
    * Add Ada client petstore samples
    - Add script to generate Ada client support with swagger-codegen
    - Add files to build the Ada sample
    - Add main program to use the generated client samples API
      and connect to the server to perform some operations
    
    * Add some description for the samples
    
    * Update the documentation to explain how to build, how to use the generated Ada client code
    
    * Add server support for path parameters
    - Update postProcessOperations to scan each path parameter and emit a x-path-index
      vendor attribute to tell the index of the path parameter
    
    * Add and fix Ada server code package declaration
    - fix declaration of operations
    - generate a generic package that must be instantiated with the target server implementation
      and which provides the skeleton (deserialization and serialization of data)
    
    * Implement the Ada server side operations
    - extract body, query parameters, path parameters
    - serialize the result
    - register operations to the server according to the path/routes
    
    * Update the code generation to generate server Ada implementation code
    
    * Improvement of Ada server support: generate the swagger.json template file
    
    * Define toModelName operation to the creation of a model identifier
    
    * Add support for server permission generation
    - collect the security scopes in postProcessAuthMethod() method and make sure
      these scopes have unique identifiers.  Some scopes correspond to URLs
      but others correspond to pseudo identifiers.
    
    * Use the #lambdaAdaComment filter to indent correctly a multi-line description
    
    * Fix model generation to support arrays
    
    * Update the generated GNAT project file
    
    * Refactoring and improvement of server code generation
    - Change the server generated code to pass a Context_Type object
      to allow the server implementation to get/set headers in the request/response
      and control what is put in some responses
    - Generate the security permissions based on the scopes that have been collected
    
    * Server code generation improvement
    - Fix generation of GNAT project
    - Generate the intermediate Ada packages if necessary
    - Generate the server main
    
    * Ada server main template
    
    * Ada server code improvement
    - Add support to generate server permission verification
    - Fix the GNAT project definition
    - Templates for Ada intermediate packages
    
    * Skeleton for the server side implementation
    
    * Generate an empty Ada server implementation
    
    * Templates for the Ada server implementation
    
    * Add a README.md file and a GNAT config.gpr file
    
    * New templates to document the generated Ada server
    
    * Add server configuration file for the Ada server
    
    * Fix the log message in the Ada server to report the correct URI to connect to
    
    * Generate the Ada server configuration file
    
    * Improvement of Ada code model to support nullable types
    
    * Update the Ada server templates
    
    * Refactor the Ada code generator
    - separate the Ada client and Ada server code generators
    - register the Ada server code generator under the name 'ada-server'
      keep 'ada' for the client Ada code generator
    - moved the common Ada code operation supports to the AbstractAdaCodegen
    
    * Improvement and cleanup of Ada client and server code
    - new template for the client main program
    - fix the GNAT project template for client or server programs
    - remove unused options to better use the --model-package option
    
    * Fix the GNAT project file name to use a lower case name
    Fix the default GNAT config
    Fix the headers of intermediate Ada package files
    
    * Regenerate the model and client Ada files
    
    * Update the Ada client sample to take into account the Nullable types
    
    * Regenerate some files with Ada Swagger Codegen
    
    * Ignore generation of petstore.gpr

--- samples/client/petstore/ada/src/samples-petstore.ads
+++ samples/client/petstore/ada/src/samples-petstore.ads
@@ -1,2 +1,14 @@
+--  Swagger Petstore
+--  This is a sample server Petstore server.  You can find out more about Swagger at [http://swagger.io](http://swagger.io) or on [irc.freenode.net, #swagger](http://swagger.io/irc/).  For this sample, you can use the api key `special_key` to test the authorization filters.
+--  ------------ EDIT NOTE ------------
+--  This file was generated with swagger-codegen.  You can modify it to implement
+--  the server.  After you modify this file, you should add the following line
+--  to the .swagger-codegen-ignore file:
+--
+--  src/samples-petstore.ads
+--
+--  Then, you can drop this edit note comment.
+--  ------------ EDIT NOTE ------------
 package Samples.Petstore is
+
 end Samples.Petstore;
