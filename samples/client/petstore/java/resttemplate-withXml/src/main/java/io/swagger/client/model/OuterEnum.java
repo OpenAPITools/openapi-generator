429b96ae7e22a57c2a408a4cf59d7fd87f1909eb
Ben Mordue <benmordue@gmail.com>
2018-01-14 09:39:32 +0000

    [JAVA] equals and hashCode for models with byte[] and binary strings (#7341)
    * equals and hashCode methods for POJOs with byte array vars
    
    * Also fix equals and hashCode for binary vars
    
    * Ran script to update samples

--- samples/client/petstore/java/resttemplate-withXml/src/main/java/io/swagger/client/model/OuterEnum.java
+++ samples/client/petstore/java/resttemplate-withXml/src/main/java/io/swagger/client/model/OuterEnum.java
@@ -14,6 +14,7 @@
 package io.swagger.client.model;
 
 import java.util.Objects;
+import java.util.Arrays;
 import com.fasterxml.jackson.dataformat.xml.annotation.*;
 import javax.xml.bind.annotation.*;
 
