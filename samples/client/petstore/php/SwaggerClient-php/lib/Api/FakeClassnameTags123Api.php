ddd7c2b47dd6466b36e6bfcd40328f9e08f02257
Jens Radtke <jmglsn.github@fin-sn.de>
2018-01-25 17:49:35 +0100

    fix: declare property headerSelector (#7495)
    The property was created dynamically, it's a more clean approach to
    declare it as a member of the class.

--- samples/client/petstore/php/SwaggerClient-php/lib/Api/FakeClassnameTags123Api.php
+++ samples/client/petstore/php/SwaggerClient-php/lib/Api/FakeClassnameTags123Api.php
@@ -60,6 +60,11 @@ class FakeClassnameTags123Api
     protected $config;
 
     /**
+     * @var HeaderSelector
+     */
+    protected $headerSelector;
+
+    /**
      * @param ClientInterface $client
      * @param Configuration   $config
      * @param HeaderSelector  $selector
