47111b3241267d732dc400db2745e61d126ccc87
Benjamin Roedell <benjaminrobot@gmail.com>
2018-01-25 20:52:49 -0500

    Issue 4824 fix gson deserialize format byte (#7473)
    * Build sample prior to code changes so differences are easier to decipher
    
    * Add byte array type adapter for okhttp-gson (java8 only)
    
    * Unit revealed that Gson (or GsonFire) defaults to escape = (equal sign) out of base64 string.  Add disableHtmlEscaping() to gsonbuilder.
    
    * Update specs and samples to include format=byte and include junit test of byte array serialization and deserialization.
    
    * Implement recommendations by @cbornet including: Use okio.ByteString for base64 conversions instead of java 8 lib since the okhttp-gson client already includes okio. Remove setting to disableHtmlEscaping. Rename LocalByteArrayAdapter to ByteArrayAdapter.
    
    * Update spec and sample for java okhttp-gson library client
    
    * Undo addition of profilePhoto property.  A format byte property is already available under the format_test definition.
    
    * Put previously deleted tests back. Modified testFindPetsByTags() to verify exception since the server now returns a 500 for this request.
    
    * Update test to pass when run against docker containerized server swaggerapi/petstore (heads up, this test fails if calling out to the internet published version of http://petstore.swagger.io)

--- bin/windows/java-petstore-okhttp-gson.bat
+++ bin/windows/java-petstore-okhttp-gson.bat
@@ -5,6 +5,6 @@ If Not Exist %executable% (
 )
 
 REM set JAVA_OPTS=%JAVA_OPTS% -Xmx1024M
-set ags=generate -i modules\swagger-codegen\src\test\resources\2_0\petstore-with-fake-endpoints-models-for-testing.yaml -l java -o samples\client\petstore\java --library=okhttp-gson -DdateLibrary=joda -DhideGenerationTimestamp=true
+set ags=generate -t modules\swagger-codegen\src\main\resources\Java\libraries\okhttp-gson -i modules\swagger-codegen\src\test\resources\2_0\petstore-with-fake-endpoints-models-for-testing.yaml -l java -c bin\java-petstore-okhttp-gson.json -o samples\client\petstore\java\okhttp-gson -DhideGenerationTimestamp=true
 
 java %JAVA_OPTS% -jar %executable% %ags%
