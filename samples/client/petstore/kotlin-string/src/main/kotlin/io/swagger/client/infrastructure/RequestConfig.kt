c69925b53af03fa7a1deb6eaa7f5623250be2462
William Cheng <wing328hk@gmail.com>
2018-01-28 21:40:22 +0800

    [Kotlin] Fix issues with threetenbp  (#7516)
    * fix kotlin threetenbp client
    
    * fix typo in kotlin sample path

---
+++ samples/client/petstore/kotlin-string/src/main/kotlin/io/swagger/client/infrastructure/RequestConfig.kt
@@ -0,0 +1,15 @@
+package io.swagger.client.infrastructure
+
+/**
+ * Defines a config object for a given request.
+ * NOTE: This object doesn't include 'body' because it
+ *       allows for caching of the constructed object
+ *       for many request definitions.
+ * NOTE: Headers is a Map<String,String> because rfc2616 defines
+ *       multi-valued headers as csv-only.
+ */
+data class RequestConfig(
+        val method: RequestMethod,
+        val path: String,
+        val headers: Map<String, String> = mapOf(),
+        val query: Map<String, List<String>> = mapOf())
---
+++ samples/client/petstore/kotlin-threetenbp/src/main/kotlin/io/swagger/client/infrastructure/RequestMethod.kt
@@ -0,0 +1,8 @@
+package io.swagger.client.infrastructure
+
+/**
+ * Provides enumerated HTTP verbs
+ */
+enum class RequestMethod {
+    GET, DELETE, HEAD, OPTIONS, PATCH, POST, PUT
+}
