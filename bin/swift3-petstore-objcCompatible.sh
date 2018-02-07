52f606b8dc5ef34a971343548214d169e5f4f748
William Cheng <wing328hk@gmail.com>
2018-02-07 23:13:48 +0800

    Fix Swift3 test cases and add pom.xml, travis config for iOS test (#7608)
    * add pom.xml for testing objc, swift
    
    * add travis config for testing swift, objc
    
    * test swift, objc clients
    
    * use ruby 2.4.2
    
    * remove stack
    
    * update swift3 petstore
    
    * test with xcode9
    
    * remove testing inline additional properties for swift3
    
    * restore travis, pom

--- bin/swift3-petstore-objcCompatible.sh
+++ bin/swift3-petstore-objcCompatible.sh
@@ -26,6 +26,6 @@ fi
 
 # if you've executed sbt assembly previously it will use that instead.
 export JAVA_OPTS="${JAVA_OPTS} -XX:MaxPermSize=256M -Xmx1024M -DloggerPath=conf/log4j.properties"
-ags="generate -v -t modules/swagger-codegen/src/main/resources/swift3 -i modules/swagger-codegen/src/test/resources/2_0/petstore-with-fake-endpoints-models-for-testing.yaml -l swift3 -c ./bin/swift3-petstore-objcCompatible.json -o samples/client/petstore/swift3/objcCompatible $@"
+ags="generate -v -t modules/swagger-codegen/src/main/resources/swift3 -i modules/swagger-codegen/src/test/resources/2_0/swift/petstore-with-fake-endpoints-models-for-testing.yaml -l swift3 -c ./bin/swift3-petstore-objcCompatible.json -o samples/client/petstore/swift3/objcCompatible $@"
 
 java $JAVA_OPTS -jar $executable $ags
--- samples/client/petstore/swift3/rxswift/PetstoreClient.podspec
+++ samples/client/petstore/swift3/rxswift/PetstoreClient.podspec
@@ -2,6 +2,7 @@ Pod::Spec.new do |s|
   s.name = 'PetstoreClient'
   s.ios.deployment_target = '9.0'
   s.osx.deployment_target = '10.11'
+  s.tvos.deployment_target = '9.0'
   s.version = '0.0.1'
   s.source = { :git => 'git@github.com:swagger-api/swagger-mustache.git', :tag => 'v1.0.0' }
   s.authors = ''
