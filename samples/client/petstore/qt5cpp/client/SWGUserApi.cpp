6f0ae50cfdf636f3e72e7863bcf0f3143e0364b1
etherealjoy <33183834+etherealjoy@users.noreply.github.com>
2018-03-21 13:51:17 +0100

    [qt5cpp] delete callback data allocated before signal emission (#7840)
    * Small fixes to prevent crash when empty json body is provided.
    
    * Add deleteLater wrapper for pointers passed to user code to prevent memory leak.
    
    * Updates to move Object Wrapper to separate file
    
    * Add Prefix to class name

--- samples/client/petstore/qt5cpp/client/SWGUserApi.cpp
+++ samples/client/petstore/qt5cpp/client/SWGUserApi.cpp
@@ -13,6 +13,7 @@
 #include "SWGUserApi.h"
 #include "SWGHelpers.h"
 #include "SWGModelFactory.h"
+#include "SWGQObjectWrapper.h"
 
 #include <QJsonArray>
 #include <QJsonDocument>
@@ -284,9 +285,10 @@ SWGUserApi::getUserByNameCallback(SWGHttpRequestWorker * worker) {
         msg = "Error: " + worker->error_str;
     }
 
-
     QString json(worker->response);
     SWGUser* output = static_cast<SWGUser*>(create(json, QString("SWGUser")));
+    auto wrapper = new SWGQObjectWrapper<SWGUser*> (output);
+    wrapper->deleteLater();
     worker->deleteLater();
 
     if (worker->error_type == QNetworkReply::NoError) {
@@ -352,9 +354,10 @@ SWGUserApi::loginUserCallback(SWGHttpRequestWorker * worker) {
         msg = "Error: " + worker->error_str;
     }
 
-
     QString json(worker->response);
     QString* output = static_cast<QString*>(create(json, QString("QString")));
+    auto wrapper = new SWGQObjectWrapper<QString*> (output);
+    wrapper->deleteLater();
     worker->deleteLater();
 
     if (worker->error_type == QNetworkReply::NoError) {
