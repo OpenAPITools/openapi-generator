30dec008d33a8220c001bcba4a0a0a789c05180d
wing328 <wing328hk@gmail.com>
2018-03-26 23:17:33 +0800

    add back ruby tests

---
+++ modules/openapi-generator/src/test/java/org/openapitools/codegen/ruby/RubyClientCodegenTest.java
@@ -0,0 +1,70 @@
+package org.openapitools.codegen.ruby;
+
+import org.openapitools.codegen.ClientOpts;
+import org.openapitools.codegen.ClientOptInput;
+import org.openapitools.codegen.CodegenConfig;
+import org.openapitools.codegen.DefaultGenerator;
+import org.openapitools.codegen.languages.RubyClientCodegen;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.Operation;
+import io.swagger.v3.oas.models.media.*;
+import io.swagger.v3.parser.OpenAPIV3Parser;
+import io.swagger.v3.parser.util.SchemaTypeUtil;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.rules.TemporaryFolder;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.io.File;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+import static org.junit.Assert.fail;
+import static org.testng.Assert.*;
+
+/**
+ * Tests for RubyClientCodegen-generated templates
+ */
+public class RubyClientCodegenTest {
+
+  public TemporaryFolder folder = new TemporaryFolder();
+
+  @BeforeMethod
+  public void setUp() throws Exception {
+      folder.create();
+  }
+
+  @AfterMethod
+  public void tearDown() throws Exception {
+      folder.delete();
+  }
+
+  @Test
+  public void testGenerateRubyClientWithHtmlEntity() throws Exception {
+      final File output = folder.getRoot();
+
+      final OpenAPI openAPI = new OpenAPIV3Parser().read("src/test/resources/2_0/pathWithHtmlEntity.yaml");
+      CodegenConfig codegenConfig = new RubyClientCodegen();
+      codegenConfig.setOutputDir(output.getAbsolutePath());
+
+      ClientOptInput clientOptInput = new ClientOptInput().opts(new ClientOpts()).openAPI(openAPI).config(codegenConfig);
+
+      DefaultGenerator generator = new DefaultGenerator();
+      List<File> files = generator.opts(clientOptInput).generate();
+      boolean apiFileGenerated = false;
+      for (File file : files) {
+        if (file.getName().equals("default_api.rb")) {
+          apiFileGenerated = true;
+          // Ruby client should set the path unescaped in the api file
+          assertTrue(FileUtils.readFileToString(file, StandardCharsets.UTF_8).contains("local_var_path = '/foo=bar'"));
+        }
+      }
+      if (!apiFileGenerated) {
+        fail("Default api file is not generated!");
+      }
+  }
+
+}
