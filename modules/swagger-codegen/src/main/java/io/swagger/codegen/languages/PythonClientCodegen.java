3f610b29e219b4fd244dc0777453901d678bfa06
gs <gustav.sinder@gmail.com>
2018-04-03 18:15:15 +0200

    Additional reserved python keywords (#7956)

--- modules/swagger-codegen/src/main/java/io/swagger/codegen/languages/PythonClientCodegen.java
+++ modules/swagger-codegen/src/main/java/io/swagger/codegen/languages/PythonClientCodegen.java
@@ -107,7 +107,8 @@ public class PythonClientCodegen extends DefaultCodegen implements CodegenConfig
                     "and", "del", "from", "not", "while", "as", "elif", "global", "or", "with",
                     "assert", "else", "if", "pass", "yield", "break", "except", "import",
                     "print", "class", "exec", "in", "raise", "continue", "finally", "is",
-                    "return", "def", "for", "lambda", "try", "self", "nonlocal", "None", "True", "False"));
+                    "return", "def", "for", "lambda", "try", "self", "nonlocal", "None", "True", "nonlocal",
+                    "float", "int", "str", "date", "datetime"));
 
         regexModifiers = new HashMap<Character, String>();
         regexModifiers.put('i', "IGNORECASE");
