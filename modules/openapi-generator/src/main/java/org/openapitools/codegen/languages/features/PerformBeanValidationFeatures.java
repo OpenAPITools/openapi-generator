16ade73c29051eda29f7d116ab5a101201c923bb
wing328 <wing328hk@gmail.com>
2018-04-03 15:45:24 +0800

    add java generator

---
+++ modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/features/PerformBeanValidationFeatures.java
@@ -0,0 +1,10 @@
+package org.openapitools.codegen.languages.features;
+
+public interface PerformBeanValidationFeatures {
+
+    // Language supports performing BeanValidation
+    public static final String PERFORM_BEANVALIDATION = "performBeanValidation";
+
+    public void setPerformBeanValidation(boolean performBeanValidation);
+
+}
