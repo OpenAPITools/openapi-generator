240aad86863d10ccf02c68424a13012511f46d33
gs <gustav.sinder@gmail.com>
2018-04-03 18:15:15 +0200

    Additional reserved python keywords (#7956)

--- modules/swagger-codegen/src/main/java/io/swagger/codegen/languages/FlaskConnexionCodegen.java
+++ modules/swagger-codegen/src/main/java/io/swagger/codegen/languages/FlaskConnexionCodegen.java
@@ -78,7 +78,8 @@ public class FlaskConnexionCodegen extends DefaultCodegen implements CodegenConf
                     "and", "del", "from", "not", "while", "as", "elif", "global", "or", "with",
                     "assert", "else", "if", "pass", "yield", "break", "except", "import",
                     "print", "class", "exec", "in", "raise", "continue", "finally", "is",
-                    "return", "def", "for", "lambda", "try", "self", "None", "True", "False", "nonlocal"));
+                    "return", "def", "for", "lambda", "try", "self", "None", "True", "False", "nonlocal",
+                    "float", "int", "str", "date", "datetime"));
 
         // set the output folder here
         outputFolder = "generated-code/connexion";
