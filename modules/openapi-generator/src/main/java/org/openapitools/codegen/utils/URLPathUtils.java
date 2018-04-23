c90c23d68bf9e290684a9b287d1a8a5997d855e6
William Cheng <wing328hk@gmail.com>
2018-04-23 11:47:16 +0800

    Fix NPE when getting the default value (#196)
    * fix NPE when getting the default value
    
    * rename test case
    
    * better npe handling for repsonse example, default path to localhost
    
    * fix code style

--- modules/openapi-generator/src/main/java/org/openapitools/codegen/utils/URLPathUtils.java
+++ modules/openapi-generator/src/main/java/org/openapitools/codegen/utils/URLPathUtils.java
@@ -13,25 +13,23 @@ import java.util.List;
 public class URLPathUtils {
 
     protected static final Logger LOGGER = LoggerFactory.getLogger(URLPathUtils.class);
-    public static String DEFAULT_PATH = "/";
     public static final String LOCAL_HOST = "http://localhost";
 
     public static URL getServerURL(OpenAPI openAPI) {
         final List<Server> servers = openAPI.getServers();
         if (servers == null || servers.isEmpty()) {
-            return null;
+            LOGGER.warn("Server information seems not defined in the spec. Default to {}.", LOCAL_HOST);
+            return getDefaultUrl();
         }
         // TOOD need a way to obtain all server URLs
         final Server server = servers.get(0);
-        String url = server.getUrl();
-        if (url.equals(DEFAULT_PATH)) {
-            url = LOCAL_HOST;
-        }
+        String url = sanitizeUrl(server.getUrl());
+
         try {
-            return new URL(server.getUrl());
+            return new URL(url);
         } catch (MalformedURLException e) {
-            LOGGER.warn("Not valid URL: " + server.getUrl(), e);
-            return null;
+            LOGGER.warn("Not valid URL: {}. Default to {}.", server.getUrl(), LOCAL_HOST);
+            return getDefaultUrl();
         }
     }
 
@@ -49,11 +47,28 @@ public class URLPathUtils {
         return scheme;
     }
 
-    public static String getHost(OpenAPI openAPI){
+    public static String getHost(OpenAPI openAPI) {
         if (openAPI.getServers() != null && openAPI.getServers().size() > 0) {
-            return openAPI.getServers().get(0).getUrl();
+            return sanitizeUrl(openAPI.getServers().get(0).getUrl());
         }
         return LOCAL_HOST;
+    }
+
+    private static String sanitizeUrl(String url) {
+        if (url.startsWith("/")) {
+            LOGGER.warn("'host' not defined in the spec (2.0). Default to " + LOCAL_HOST);
+            url = LOCAL_HOST + url;
+        }
+
+        return url;
+    }
+
+    private static URL getDefaultUrl() {
+        try {
+            return new URL(LOCAL_HOST);
+        } catch (MalformedURLException e) {
+            return null;
+        }
     }
 
 }
