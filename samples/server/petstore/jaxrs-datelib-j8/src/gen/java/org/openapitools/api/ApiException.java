a6c14596769b80faf4c29db995264c1a49131b9d
Jérémie Bresson <dev@jmini.fr>
2018-04-26 07:54:41 +0200

    Regenerate all jaxrs examples (#234)

---
+++ samples/server/petstore/jaxrs-datelib-j8/src/gen/java/org/openapitools/api/ApiException.java
@@ -0,0 +1,10 @@
+package org.openapitools.api;
+
+
+public class ApiException extends Exception{
+    private int code;
+    public ApiException (int code, String msg) {
+        super(msg);
+        this.code = code;
+    }
+}
