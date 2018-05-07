376d9af9c0c28c0f6777288905d59daec6498aaa
Akihito Nakano <sora.akatsuki@gmail.com>
2018-05-08 00:04:06 +0900

    Rename default packages for php client (#315)
    * Rename default packages for php client
    
    * Generate php client (OAS3)
    
    * Rename namespace for tests
    
    * Delete unnecessary 'use'
    
    * Rename method/variable
    
    * Update php client (OAS3)
    
    * Generate php client (OAS2)
    
    * Rename namespace for tests
    
    * Delete unused files
    
    * Fix tests

---
+++ samples/openapi3/client/petstore/php/OpenAPIToolsClient-php/tests/PetTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace OpenAPITools\Client;
+
+use OpenAPITools\Client\Model\Pet;
+
+class PetTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * test empty object serialization
+     */
+    public function testEmptyPetSerialization()
+    {
+        $new_pet = new Pet;
+        // the empty object should be serialised to {}
+        $this->assertSame("{}", "$new_pet");
+    }
+}
