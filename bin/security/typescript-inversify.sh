2ee05e12d2186e326fb8ff8508056954cdc6264f
Jérémie Bresson <dev@jmini.fr>
2018-05-09 12:13:30 +0200

    Fixes in security examples (#396)
    * Regenerate "samples/client/petstore-security-test/java/okhttp-gson/"
    
    * Use correct jar
    
    * Fix sh scripts
    
    * Fix path: modules/swagger-codegen => modules/openapi-generator/

--- bin/security/typescript-inversify.sh
+++ bin/security/typescript-inversify.sh
@@ -18,7 +18,7 @@ if [ ! -d "${APP_DIR}" ]; then
   APP_DIR=`cd "${APP_DIR}"; pwd`
 fi
 
-executable="./modules/swagger-codegen-cli/target/swagger-codegen-cli.jar"
+executable="./modules/openapi-generator-cli/target/openapi-generator-cli.jar"
 
 if [ ! -f "$executable" ]
 then
@@ -27,6 +27,6 @@ fi
 
 # if you've executed sbt assembly previously it will use that instead.
 export JAVA_OPTS="${JAVA_OPTS} -XX:MaxPermSize=256M -Xmx1024M -DloggerPath=conf/log4j.properties"
-ags="$@ generate -t modules/swagger-codegen/src/main/resources/typescript-inversify -i modules/swagger-codegen/src/test/resources/2_0/petstore-security-test.yaml -l typescript-inversify -o samples/client/petstore-security-test/typescript-inversify"
+ags="$@ generate -t modules/openapi-generator/src/main/resources/typescript-inversify -i modules/openapi-generator/src/test/resources/2_0/petstore-security-test.yaml -l typescript-inversify -o samples/client/petstore-security-test/typescript-inversify"
 
 java $JAVA_OPTS -jar $executable $ags
