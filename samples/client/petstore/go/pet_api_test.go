1b01698c147c9d4e6c347a5e2a75df045f3d6d5f
wing328 <wing328hk@gmail.com>
2018-05-12 19:17:50 +0800

    Fix go test, manually fix go client

--- samples/client/petstore/go/pet_api_test.go
+++ samples/client/petstore/go/pet_api_test.go
@@ -59,7 +59,7 @@ func TestGetPetById(t *testing.T) {
 func TestGetPetByIdWithInvalidID(t *testing.T) {
 	resp, r, err := client.PetApi.GetPetById(context.Background(), 999999999)
 	if r != nil && r.StatusCode == 404 {
-		assertedError, ok := err.(sw.GenericSwaggerError)
+		assertedError, ok := err.(sw.GenericOpenAPIError)
 		a := assert.New(t)
 		a.True(ok)
 		a.Contains(string(assertedError.Body()), "type")
