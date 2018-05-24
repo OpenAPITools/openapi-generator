b76d9e6d9fb5b7c676aa1c3afaf4d802b9bd33b0
Jérémie Bresson <dev@jmini.fr>
2018-05-24 22:11:46 +0200

    Update 'springboot-reactive' in samples folder (#147)

--- samples/server/petstore/springboot-reactive/src/main/java/org/openapitools/api/FakeApiController.java
+++ samples/server/petstore/springboot-reactive/src/main/java/org/openapitools/api/FakeApiController.java
@@ -7,5 +7,4 @@ import java.util.Optional;
 @Controller
 public class FakeApiController implements FakeApi {
 
-
 }
--- samples/server/petstore/springboot-reactive/src/main/java/org/openapitools/api/FakeClassnameTestApiController.java
+++ samples/server/petstore/springboot-reactive/src/main/java/org/openapitools/api/FakeClassnameTestApiController.java
@@ -7,5 +7,4 @@ import java.util.Optional;
 @Controller
 public class FakeClassnameTestApiController implements FakeClassnameTestApi {
 
-
 }
