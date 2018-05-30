47f9683aaa56a0466c46b6f963f2751533d675fa
cbornet <cbornet@hotmail.com>
2018-05-30 17:02:57 +0200

    [Spring] Add apiFirst option

--- samples/server/petstore/springboot-delegate-j8/src/main/java/org/openapitools/configuration/HomeController.java
+++ samples/server/petstore/springboot-delegate-j8/src/main/java/org/openapitools/configuration/HomeController.java
@@ -3,14 +3,17 @@ package org.openapitools.configuration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
 /**
- * Home redirection to swagger api documentation 
+ * Home redirection to OpenAPI api documentation
  */
 @Controller
 public class HomeController {
-    @RequestMapping(value = "/")
+
+    @RequestMapping("/")
     public String index() {
-        System.out.println("swagger-ui.html");
         return "redirect:swagger-ui.html";
     }
+
+
 }
