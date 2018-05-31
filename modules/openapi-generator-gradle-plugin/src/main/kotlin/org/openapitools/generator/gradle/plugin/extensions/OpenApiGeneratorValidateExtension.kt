b2e5576804504819a9982f62399614ce54990ab9
Jim Schubert <james.schubert@gmail.com>
2018-05-31 07:23:05 -0400

    [gradle-plugin] Initial implementation (#162)
    * [gradle-plugin] Initial commit
    
    * Clarify comments on file constraints
    
    When a user sets the models, apis, or supportingFiles environment
    variables, any one of these being set disables generation for the other
    two.  This could be confusing to users, so I've added some clarification
    text in the comments for these properties.
    
    In addition, I've cleaned up the extension on
    Property.ifNotEmpty, to avoid using Suppress annotations where it's not
    necessary. The change creates a local variable of type T?, allowing
    Kotlin to track the variable's nullable state at compile time.
    
    * Move gradle plugin under modules
    
    * Move kt files under kotlin source set. Add sample.
    
    * [gradle] map-like options as maps
    
    * Add tests for gradle validate task
    
    * Apply gradle plugin to mvn install phase
    
    * [gradle] Testing remaining gradle tasks
    
    * Add gradle plugin to the integration doc
    
    * Update gradle plugin README with task options
    
    * Gradle readme formatting

---
+++ modules/openapi-generator-gradle-plugin/src/main/kotlin/org/openapitools/generator/gradle/plugin/extensions/OpenApiGeneratorValidateExtension.kt
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2018 OpenAPI-Generator Contributors (https://openapi-generator.tech)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.openapitools.generator.gradle.plugin.extensions
+
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.property
+
+/**
+ * Gradle project level extension object definition for the generators task
+ *
+ * @author Jim Schubert
+ */
+open class OpenApiGeneratorValidateExtension(project: Project) {
+    /**
+     * The input specification to validate. Supports all formats supported by the Parser.
+     */
+    val inputSpec = project.objects.property<String>()
+}
