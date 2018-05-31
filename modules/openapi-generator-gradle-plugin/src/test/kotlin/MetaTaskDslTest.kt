675f2442963bcf655d5dc5623a1d01ccc46569d7
Jim Schubert <james.schubert@gmail.com>
2018-05-30 21:45:33 -0400

    [gradle] Testing remaining gradle tasks

---
+++ modules/openapi-generator-gradle-plugin/src/test/kotlin/MetaTaskDslTest.kt
@@ -0,0 +1,58 @@
+package org.openapitools.generator.gradle.plugin
+
+import org.gradle.testkit.runner.GradleRunner
+import org.gradle.testkit.runner.TaskOutcome
+import org.testng.annotations.Test
+import java.io.File
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+
+class MetaTaskDslTest : TestBase() {
+    override var temp: File = createTempDir(javaClass.simpleName)
+
+    @Test
+    fun `openApiMeta should generate desired project contents`() {
+        // Arrange
+        val buildDirReplacement = "\$buildDir/meta"
+        withProject("""
+            | plugins {
+            |   id 'org.openapi.generator'
+            | }
+            |
+            | openApiMeta {
+            |     generatorName = "Sample"
+            |     packageName = "org.openapitools.example"
+            |     outputFolder = "$buildDirReplacement".toString()
+            | }
+        """.trimMargin())
+
+        // Act
+        val result = GradleRunner.create()
+                .withProjectDir(temp)
+                .withArguments("openApiMeta")
+                .withPluginClasspath()
+                .build()
+
+        // Assert
+        assertTrue(result.output.contains("Wrote file to"), "User friendly write notice is missing.")
+
+        // To avoid any OS-specific output causing issues with our stdout comparisons, only compare on expected filenames.
+        listOf(
+                "SampleGenerator.java",
+                "README.md",
+                "api.mustache",
+                "model.mustache",
+                "myFile.mustache",
+                "org.openapitools.codegen.CodegenConfig",
+                "pom.xml"
+        ).map {
+            assertTrue(result.output.contains(it), "Expected $it to be listed in gradle stdout.")
+        }
+
+        assertEquals(
+                TaskOutcome.SUCCESS,
+                result.task(":openApiMeta")?.outcome,
+                "Expected a successful run, but found ${result.task(":openApiMeta")?.outcome}"
+        )
+    }
+}
--- modules/openapi-generator-gradle-plugin/src/test/kotlin/ValidateTaskDslTest.kt
+++ modules/openapi-generator-gradle-plugin/src/test/kotlin/ValidateTaskDslTest.kt
@@ -32,15 +32,16 @@ class ValidateTaskDslTest : TestBase() {
                 .buildAndFail()
 
         // Assert
-        assertTrue(result.output.contains("unable to read location `some_location`"))
-        assertEquals(FAILED, result.task(":openApiValidate")?.outcome)
+        assertTrue(result.output.contains("unable to read location `some_location`"), "Unexpected/no message presented to the user for a spec pointing to an invalid URI.")
+        assertEquals(FAILED, result.task(":openApiValidate")?.outcome,
+                "Expected a failed run, but found ${result.task(":openApiValidate")?.outcome}")
     }
 
     @Test
     fun `openApiValidate should succeed on valid spec`() {
         // Arrange
         val projectFiles = mapOf(
-            "spec.yaml" to javaClass.classLoader.getResourceAsStream("specs/petstore-v3.0.yaml")
+                "spec.yaml" to javaClass.classLoader.getResourceAsStream("specs/petstore-v3.0.yaml")
         )
 
         withProject("""
@@ -61,15 +62,16 @@ class ValidateTaskDslTest : TestBase() {
                 .build()
 
         // Assert
-        assertTrue(result.output.contains("Spec is valid."))
-        assertEquals(SUCCESS, result.task(":openApiValidate")?.outcome)
+        assertTrue(result.output.contains("Spec is valid."), "Unexpected/no message presented to the user for a valid spec.")
+        assertEquals(SUCCESS, result.task(":openApiValidate")?.outcome,
+                "Expected a successful run, but found ${result.task(":openApiValidate")?.outcome}")
     }
 
     @Test
     fun `openApiValidate should fail on invalid spec`() {
         // Arrange
         val projectFiles = mapOf(
-            "spec.yaml" to javaClass.classLoader.getResourceAsStream("specs/petstore-v3.0-invalid.yaml")
+                "spec.yaml" to javaClass.classLoader.getResourceAsStream("specs/petstore-v3.0-invalid.yaml")
         )
         withProject("""
             | plugins {
@@ -89,8 +91,9 @@ class ValidateTaskDslTest : TestBase() {
                 .buildAndFail()
 
         // Assert
-        assertTrue(result.output.contains("Spec is invalid."))
-        assertEquals(FAILED, result.task(":openApiValidate")?.outcome)
+        assertTrue(result.output.contains("Spec is invalid."), "Unexpected/no message presented to the user for an invalid spec.")
+        assertEquals(FAILED, result.task(":openApiValidate")?.outcome,
+                "Expected a failed run, but found ${result.task(":openApiValidate")?.outcome}")
     }
 
 }
