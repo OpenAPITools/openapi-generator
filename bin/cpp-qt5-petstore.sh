e26665b4c619a845bd0dea977555d0795bbcf703
Akihito Nakano <sora.akatsuki@gmail.com>
2018-06-07 17:55:43 +0900

    [C++] Adjust the names (script, sample folder, generator) to lang option (#220)
    * Rename script: qt5-petstore.sh -> cpp-qt5-petstore.sh
    
    * Rename sample folder: qt5cpp -> cpp-qt5
    
    * Rename script: cpprest-petstore.sh -> cpp-restsdk-petstore.sh
    
    * Rename sample folder: cpprest -> cpp-restsdk
    
    * Rename generator: CppRestClientCodegen -> CppRestSdkClientCodegen
    
    * Rename script: tizen-petstore.sh -> cpp-tizen-petstore.sh
    
    * Rename sample folder: tizen -> cpp-tizen
    
    * Rename script(security): qt5cpp-petstore.sh -> cpp-qt5-petstore.sh
    
    * Rename sample folder(security): qt5cpp -> cpp-qt5
    
    * Rename script(windows): qt5cpp-petstore.bat -> cpp-qt5-petstore.bat
    
    * Change sample folder
    
    * Rename script(windows): cpprest-petstore.bat -> cpp-restsdk-petstore.bat
    
    * Change sample folder
    
    * Rename script(windows): tizen-petstore.bat -> cpp-tizen-petstore.bat
    
    * Change sample folder
    
    * Change output folder: tizen -> cpp-tizen
    
    * Rename the scripts under bin/openapi3
    
    cpp-restsdk is not exist under bin/openapi3
    
    * Change sample folder

--- bin/cpp-qt5-petstore.sh
+++ bin/cpp-qt5-petstore.sh
@@ -22,11 +22,11 @@ executable="./modules/openapi-generator-cli/target/openapi-generator-cli.jar"
 
 if [ ! -f "$executable" ]
 then
-  mvn clean package
+  mvn -B clean package
 fi
 
 # if you've executed sbt assembly previously it will use that instead.
 export JAVA_OPTS="${JAVA_OPTS} -XX:MaxPermSize=256M -Xmx1024M -DloggerPath=conf/log4j.properties"
-ags="generate -t modules/openapi-generator/src/main/resources/qt5cpp -i modules/openapi-generator/src/test/resources/3_0/petstore.yaml -g cpp-qt5 -o samples/client/petstore/qt5cpp $@"
+ags="generate -t modules/openapi-generator/src/main/resources/qt5cpp -i modules/openapi-generator/src/test/resources/2_0/petstore.yaml -g cpp-qt5 -o samples/client/petstore/cpp-qt5 $@"
 
 java $JAVA_OPTS -jar $executable $ags
--- bin/cpp-restsdk-petstore.sh
+++ bin/cpp-restsdk-petstore.sh
@@ -22,11 +22,11 @@ executable="./modules/openapi-generator-cli/target/openapi-generator-cli.jar"
 
 if [ ! -f "$executable" ]
 then
-  mvn clean package
+  mvn -B clean package
 fi
 
 # if you've executed sbt assembly previously it will use that instead.
 export JAVA_OPTS="${JAVA_OPTS} -XX:MaxPermSize=256M -Xmx1024M -DloggerPath=conf/log4j.properties"
-ags="generate -t modules/openapi-generator/src/main/resources/qt5cpp -i modules/openapi-generator/src/test/resources/2_0/petstore-security-test.yaml -g cpp-qt5 -o samples/client/petstore-security-test/qt5cpp $@"
+ags="generate -t modules/openapi-generator/src/main/resources/cpprest -i modules/openapi-generator/src/test/resources/2_0/petstore.yaml -g cpp-restsdk -o samples/client/petstore/cpp-restsdk $@"
 
 java $JAVA_OPTS -jar $executable $ags
