1fd460a39ec18ff5355ec447cc567fa9ac5564c7
Jim Schubert <james.schubert@gmail.com>
2018-06-07 23:35:06 -0400

    Add piggyback license onto licensed of file borrowed from airlift/airline

--- modules/openapi-generator-cli/src/main/java/org/openapitools/codegen/cmd/CompletionCommand.java
+++ modules/openapi-generator-cli/src/main/java/org/openapitools/codegen/cmd/CompletionCommand.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2010 the original author or authors.
+ * Copyright 2018 OpenAPI-Generator Contributors (https://openapi-generator.tech)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
