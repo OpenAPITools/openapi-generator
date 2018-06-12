5e6d18e0eee428ecef878e2bcbaedde36b9d64f3
PowerOfCreation <questerdesura@gmail.com>
2018-06-12 14:28:16 +0200

    the pet struct now uses no double pointer for it's name anymore and no pointer for the enum status anymore; the pet struct can now be fully converted to json and parsed from json

--- samples/client/c/include/pet.h
+++ samples/client/c/include/pet.h
@@ -3,16 +3,20 @@
 
 typedef enum status_t {available, pending, sold } status_t;
 
+char* status_ToString(status_t status);
+status_t status_FromString(char *status);
+
 typedef struct pet_t {
     long id;
     category_t *category;
-    char **name;
+    char *name;
     list_t *photoUrls;
     list_t *tags;
-    status_t *status;
+    status_t status;
 } pet_t;
 
-pet_t *pet_create(long id, category_t *category, char **name, list_t *photoUrls, list_t *tags, status_t *status);
+pet_t *pet_create(long id, category_t *category, char *name, list_t *photoUrls, list_t *tags, status_t status);
 void pet_free(pet_t* pet);
 
-char *pet_convertToJson(pet_t *pet);
+pet_t* pet_parseFromJSON(char *jsonString);
+char *pet_convertToJSON(pet_t *pet);
--- samples/client/c/unit-tests/test-pet.c
+++ samples/client/c/unit-tests/test-pet.c
@@ -1,5 +1,6 @@
 #include "pet.h"
 #include "category.h"
+#include "tag.h"
 #include <stdlib.h>
 #include <string.h>
 
@@ -7,14 +8,16 @@
 #define EXAMPLE_PET_NAME "Example Pet"
 #define EXAMPLE_URL_1 "http://www.github.com"
 #define EXAMPLE_URL_2 "http://www.gitter.im"
-#define EXAMPLE_TAG_1 "beautiful code"
-#define EXAMPLE_TAG_2 "at least I tried"
+#define EXAMPLE_TAG_1_NAME "beautiful code"
+#define EXAMPLE_TAG_2_NAME "at least I tried"
+#define EXAMPLE_TAG_1_ID 1
+#define EXAMPLE_TAG_2_ID 542353
 
 int main() {
 	char *categoryName = malloc(strlen(EXAMPLE_CATEGORY_NAME) + 1);
 	strcpy(categoryName, EXAMPLE_CATEGORY_NAME);
 
-	category_t *category = category_create(5, &categoryName);
+	category_t *category = category_create(5, categoryName);
 
 	char *petName = malloc(strlen(EXAMPLE_PET_NAME) + 1);
 	strcpy(petName, EXAMPLE_PET_NAME);
@@ -30,11 +33,13 @@ int main() {
 	list_addElement(photoUrls, exampleUrl1);
 	list_addElement(photoUrls, exampleUrl2);
 
-	char *exampleTag1 = malloc(strlen(EXAMPLE_TAG_1) + 1);
-	strcpy(exampleTag1, EXAMPLE_TAG_1);
+	char *exampleTag1Name = malloc(strlen(EXAMPLE_TAG_1_NAME) + 1);
+	strcpy(exampleTag1Name, EXAMPLE_TAG_1_NAME);
+	tag_t *exampleTag1 = tag_create(EXAMPLE_TAG_1_ID, exampleTag1Name);
 
-	char *exampleTag2 = malloc(strlen(EXAMPLE_TAG_2) + 1);
-	strcpy(exampleTag2, EXAMPLE_TAG_2);
+	char *exampleTag2Name = malloc(strlen(EXAMPLE_TAG_2_NAME) + 1);
+	strcpy(exampleTag2Name, EXAMPLE_TAG_2_NAME);
+	tag_t *exampleTag2 = tag_create(EXAMPLE_TAG_2_ID, exampleTag2Name);
 
 	list_t *tags = list_create();
 
@@ -44,19 +49,8 @@ int main() {
 	status_t status = sold;
 
 	pet_t *pet =
-		pet_create(1, category, &petName, photoUrls, tags, &status);
-	free(pet_convertToJson(pet));
+		pet_create(1, category, petName, photoUrls, tags, status);
+	free(pet_convertToJSON(pet));
 
-	free(categoryName);
-	category_free(category);
-
-	free(petName);
 	pet_free(pet);
-	free(exampleUrl1);
-	free(exampleUrl2);
-	free(exampleTag1);
-	free(exampleTag2);
-	list_free(photoUrls);
-
-	list_free(tags);
 }
