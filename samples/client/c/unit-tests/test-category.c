79f48c6cc1a85ecf5104f9790d4888b7b45adf21
PowerOfCreation <questerdesura@gmail.com>
2018-06-12 14:35:43 +0200

    Uncrustified the unit test for category

--- samples/client/c/unit-tests/test-category.c
+++ samples/client/c/unit-tests/test-category.c
@@ -6,8 +6,9 @@
 #define EXAMPLE_CATEGORY_ID 5
 
 int main() {
-    char *exampelCategoryName1  = malloc(strlen(EXAMPLE_CATEGORY_NAME) + 1);
+	char *exampelCategoryName1 = malloc(strlen(EXAMPLE_CATEGORY_NAME) + 1);
 
-    category_t *category = category_create(EXAMPLE_CATEGORY_ID, exampelCategoryName1);
-    category_free(category);
+	category_t *category = category_create(EXAMPLE_CATEGORY_ID,
+	                                       exampelCategoryName1);
+	category_free(category);
 }
