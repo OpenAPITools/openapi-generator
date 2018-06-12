a7ac435bb870e544fe11e9ec629bfc5a25a78bf7
PowerOfCreation <questerdesura@gmail.com>
2018-06-12 15:23:40 +0200

    Added some kind of debug functionality to test-petApi.c

--- samples/client/c/unit-tests/test-petApi.c
+++ samples/client/c/unit-tests/test-petApi.c
@@ -1,4 +1,8 @@
+#define DEBUG
 #include <stdlib.h>
+#ifdef DEBUG
+#include <stdio.h>
+#endif // DEBUG
 #include "apiClient.h"
 #include "cJSON.h"
 #include "pet.h"
@@ -11,5 +15,11 @@
 #define EXAMPLE_OPERATION_PARAMETER 4
 
 int main() {
-	pet_free(getPetById(EXAMPLE_OPERATION_PARAMETER));
+	pet_t *pet = getPetById(EXAMPLE_OPERATION_PARAMETER);
+    #ifdef DEBUG
+	char *petJSON = pet_convertToJSON(pet);
+	puts(petJSON);
+	free(petJSON);
+    #endif // DEBUG
+	pet_free(pet);
 }
