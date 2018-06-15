fcfe8f56c84a575bbacd0266b372a8dbcc98c29b
PowerOfCreation <questerdesura@gmail.com>
2018-06-15 15:40:19 +0200

    Added a convertToJSON method to the category model

--- samples/client/petstore/c/include/category.h
+++ samples/client/petstore/c/include/category.h
@@ -1,5 +1,6 @@
 #ifndef INCLUDE_CATEGORY_H
 #define INCLUDE_CATEGORY_H
+#include "cJSON.h"
 
 typedef struct category_t {
     long id;
@@ -9,4 +10,5 @@ typedef struct category_t {
 category_t *category_create(long id, char *name);
 void category_free(category_t *category);
 
+cJSON *category_convertToJSON(category_t *category);
 #endif // INCLUDE_CATEGORY_H
