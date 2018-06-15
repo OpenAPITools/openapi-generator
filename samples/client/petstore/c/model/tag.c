070599eac6d2f5c77c929553d516ba3d46817875
PowerOfCreation <questerdesura@gmail.com>
2018-06-15 15:40:55 +0200

    Added a convertToJSON method to the tag model

--- samples/client/petstore/c/model/tag.c
+++ samples/client/petstore/c/model/tag.c
@@ -12,3 +12,18 @@ void tag_free(tag_t *tag) {
 	free(tag->name);
 	free(tag);
 }
+
+cJSON *tag_convertToJSON(tag_t *tag) {
+	cJSON *item = cJSON_CreateObject();
+	if(cJSON_AddNumberToObject(item, "id", tag->id) == NULL) {
+		goto fail;
+	}
+	if(cJSON_AddStringToObject(item, "name", tag->name) == NULL) {
+		goto fail;
+	}
+
+	return item;
+fail:
+	cJSON_Delete(item);
+	return NULL;
+}
