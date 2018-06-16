0fd168e5406296683e9e8c7a7815c6f7e6158a29
William Cheng <wing328hk@gmail.com>
2018-06-16 19:07:13 +0800

    [PHP-Lumen] Lumen 5.6 support (#212)
    * Lumen 5.6 support
    
    * recall headlines
    
    * Update composer.mustache
    
    * regenerate lumne php petstore samples
    
    * remove output format from lumen routes
    
    * Fix: "A facade root has not been set"
    
    * Ignore log folder

---
+++ samples/server/petstore/php-lumen/lib/app/Jobs/ExampleJob.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Jobs;
+
+class ExampleJob extends Job
+{
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        //
+    }
+}
