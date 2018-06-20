d4654fc9607edb55a6bf0da08b8614207004e5da
Raphael Ochsenbein <raphael@akehir.com>
2018-06-20 17:07:52 +0200

    Use PhantomJS instead of Chrome, rename all typescript-angular-poms to have the same structure

--- samples/client/petstore/typescript-angular-v6-provided-in-root/tests/default/src/karma.conf.js
+++ samples/client/petstore/typescript-angular-v6-provided-in-root/tests/default/src/karma.conf.js
@@ -8,6 +8,7 @@ module.exports = function (config) {
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
+      require('karma-phantomjs-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
