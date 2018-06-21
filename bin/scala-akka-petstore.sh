65478705d05d9e5b4ce7acc0a345578e31098a85
Akihito Nakano <sora.akatsuki@gmail.com>
2018-06-21 18:28:58 +0900

    [Scala][client] Rename script, sample folder, template dir (#334)
    * Rename scripts akka-scala-petstore.sh -> scala-akka-petstore.sh
    
    * Rename sample folder: akka-scala -> scala-akka
    
    * Rename templateDir: akka-scala -> scala-akka-client
    
    * Rename scripts: scala-petstore.sh -> scala-httpclient-petstore.sh
    
    * Rename sample folder: scala -> scala-httpclient
    
    * Rename templateDir: scala -> scala-httpclient
    
    * update circle pom.xml for new scala path
    
    * remove duplicated scala test

--- bin/scala-akka-petstore.sh
+++ bin/scala-akka-petstore.sh
@@ -22,11 +22,11 @@ executable="./modules/openapi-generator-cli/target/openapi-generator-cli.jar"
 
 if [ ! -f "$executable" ]
 then
-  mvn clean package
+  mvn -B clean package
 fi
 
 # if you've executed sbt assembly previously it will use that instead.
 export JAVA_OPTS="${JAVA_OPTS} -XX:MaxPermSize=256M -Xmx1024M -DloggerPath=conf/log4j.properties"
-ags="generate -t modules/openapi-generator/src/main/resources/scala -i modules/openapi-generator/src/test/resources/3_0/petstore.yaml -g scala -o samples/client/petstore/scala $@"
+ags="generate --artifact-id "scala-akka-petstore-client" -t modules/openapi-generator/src/main/resources/scala-akka-client -i modules/openapi-generator/src/test/resources/2_0/petstore.yaml -g scala-akka -o samples/client/petstore/scala-akka $@"
 
 java $JAVA_OPTS -jar $executable $ags
