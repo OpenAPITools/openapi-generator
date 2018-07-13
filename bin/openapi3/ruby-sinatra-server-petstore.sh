26e70fe6423f8f3ab403b30a508692dd10e6f16a
Akihito Nakano <sora.akatsuki@gmail.com>
2018-07-13 23:28:17 +0900

    [Ruby] Rename files/folders (#534)
    * Rename templateDir: rails5 -> ruby-on-rails-server
    
    * Rename script: rails5-petstore-server.sh -> ruby-on-rails-server-petstore.sh
    
    * Rename sample folder: rails5 -> ruby-on-rails
    
    * Rename script(openapi3): rails5-petstore-server.sh -> ruby-on-rails-server-petstore.sh
    
    * Update ruby-on-rails-server-petstore.sh
    
    * Rename templateDir: sinatra -> ruby-sinatra-server
    
    * Rename script: sinatra-petstore-server.sh -> ruby-sinatra-server-petstore.sh
    
    * Rename sample folder: sinatra -> ruby-sinatra
    
    * Update script
    
    * Rename templateDir: ruby -> ruby-client
    
    * Rename script: ruby-petstore.sh -> ruby-client-petstore.sh
    
    * Update scripts
    
    * Update samples
    
    - bin/ruby-sinatra-server-petstore.sh
    - bin/ruby-on-rails-server-petstore.sh
    - bin/ruby-client-petstore.sh
    - bin/security/ruby-client-petstore.sh

--- bin/openapi3/ruby-sinatra-server-petstore.sh
+++ bin/openapi3/ruby-sinatra-server-petstore.sh
@@ -22,11 +22,11 @@ executable="./modules/openapi-generator-cli/target/openapi-generator-cli.jar"
 
 if [ ! -f "$executable" ]
 then
-  mvn -B clean package
+  mvn clean package
 fi
 
 # if you've executed sbt assembly previously it will use that instead.
 export JAVA_OPTS="${JAVA_OPTS} -XX:MaxPermSize=256M -Xmx1024M -DloggerPath=conf/log4j.properties"
-ags="generate -t modules/openapi-generator/src/main/resources/ruby -i modules/openapi-generator/src/test/resources/2_0/petstore-with-fake-endpoints-models-for-testing.yaml -g ruby -c bin/ruby-petstore.json -o samples/client/petstore/ruby $@"
+ags="generate -t modules/openapi-generator/src/main/resources/ruby-sinatra-server -i modules/openapi-generator/src/test/resources/3_0/petstore.yaml -g ruby-sinatra -o samples/server/petstore/ruby-sinatra $@"
 
 java $JAVA_OPTS -jar $executable $ags
