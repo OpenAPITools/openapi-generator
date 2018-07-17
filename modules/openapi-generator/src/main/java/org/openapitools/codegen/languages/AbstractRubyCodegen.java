afb238814dec4547cdfed3fa9335e6c0b8feae80
Akihito Nakano <sora.akatsuki@gmail.com>
2018-07-17 10:08:42 +0900

    [Ruby] Abstract Ruby Codegen (#562)
    * Add AbstractRubyCodegen
    
    * Refactor constructor
    
    * Move escapeReservedWord() to AbstractRubyCodegen
    
    * Move getTypeDeclaration() to AbstractRubyCodegen
    
    * Move toDefaultValue() to AbstractRubyCodegen
    
    * Move toVarName() to AbstractRubyCodegen
    
    * Move toParamName() to AbstractRubyCodegen
    
    * Move toOperationId() to AbstractRubyCodegen
    
    * Move escapeQuotationMark() to AbstractRubyCodegen
    
    * Move escapeUnsafeCharacters() to AbstractRubyCodegen
    
    * Use super.escapeReservedWord()
    
    * RubyClientCodegen extends AbstractRubyCodegen
    
    * Add the differences with AbstractRubyCodegen to "reservedWords"
    
    * cliOptions.clear() is not a language specific matter
    
    - Rails, Sinatra requires cliOptions.clear()
    - Ruby client doesn't requires that
    
    * Remove duplicated statements with AbstractRubyCodegen
    
    * Remove duplicated methods with AbstractRubyCodegen
    
    * Merge toVarName() into AbstractRubyCodegen
    
    * Merge getTypeDeclaration() into AbstractRubyCodegen
    
    * Merge toDefaultValue() into AbstractRubyCodegen
    
    * Update Ruby related samples
    
    - bin/ruby-client-petstore.sh
    - bin/ruby-on-rails-server-petstore.sh
    - bin/ruby-sinatra-server-petstore.sh
    
    * Remove unnecessary 'import'
    
    * Avoid unnecessary HTML escaping

---
+++ modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/AbstractRubyCodegen.java
@@ -0,0 +1,162 @@
+/*
+ * Copyright 2018 OpenAPI-Generator Contributors (https://openapi-generator.tech)
+ * Copyright 2018 SmartBear Software
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.openapitools.codegen.languages;
+
+import io.swagger.v3.oas.models.media.ArraySchema;
+import io.swagger.v3.oas.models.media.Schema;
+import org.openapitools.codegen.*;
+import org.openapitools.codegen.utils.ModelUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Arrays;
+
+abstract class AbstractRubyCodegen extends DefaultCodegen implements CodegenConfig {
+    private static final Logger LOGGER = LoggerFactory.getLogger(AbstractRubyCodegen.class);
+
+    AbstractRubyCodegen() {
+        super();
+
+        setReservedWordsLowerCase(
+                Arrays.asList(
+                        "__FILE__", "and", "def", "end", "in", "or", "self", "unless", "__LINE__",
+                        "begin", "defined?", "ensure", "module", "redo", "super", "until", "BEGIN",
+                        "break", "do", "false", "next", "rescue", "then", "when", "END", "case",
+                        "else", "for", "nil", "retry", "true", "while", "alias", "class", "elsif",
+                        "if", "not", "return", "undef", "yield")
+        );
+
+        languageSpecificPrimitives.clear();
+        languageSpecificPrimitives.add("String");
+        languageSpecificPrimitives.add("Integer");
+        languageSpecificPrimitives.add("Float");
+        languageSpecificPrimitives.add("Date");
+        languageSpecificPrimitives.add("DateTime");
+        languageSpecificPrimitives.add("Array");
+        languageSpecificPrimitives.add("Hash");
+        languageSpecificPrimitives.add("File");
+        languageSpecificPrimitives.add("Object");
+
+        typeMapping.clear();
+        typeMapping.put("string", "String");
+        typeMapping.put("char", "String");
+        typeMapping.put("int", "Integer");
+        typeMapping.put("integer", "Integer");
+        typeMapping.put("long", "Integer");
+        typeMapping.put("short", "Integer");
+        typeMapping.put("float", "Float");
+        typeMapping.put("double", "Float");
+        typeMapping.put("number", "Float");
+        typeMapping.put("date", "Date");
+        typeMapping.put("DateTime", "DateTime");
+        typeMapping.put("array", "Array");
+        typeMapping.put("List", "Array");
+        typeMapping.put("map", "Hash");
+        typeMapping.put("object", "Object");
+        typeMapping.put("file", "File");
+        typeMapping.put("binary", "String");
+        typeMapping.put("ByteArray", "String");
+        typeMapping.put("UUID", "String");
+    }
+
+    @Override
+    public String escapeReservedWord(String name) {
+        if (this.reservedWordsMappings().containsKey(name)) {
+            return this.reservedWordsMappings().get(name);
+        }
+        return "_" + name;
+    }
+
+    @Override
+    public String getTypeDeclaration(Schema schema) {
+        if (ModelUtils.isArraySchema(schema)) {
+            Schema inner = ((ArraySchema) schema).getItems();
+            return getSchemaType(schema) + "<" + getTypeDeclaration(inner) + ">";
+        } else if (ModelUtils.isMapSchema(schema)) {
+            Schema inner = (Schema) schema.getAdditionalProperties();
+            return getSchemaType(schema) + "<String, " + getTypeDeclaration(inner) + ">";
+        }
+
+        return super.getTypeDeclaration(schema);
+    }
+
+    @Override
+    public String toDefaultValue(Schema p) {
+        if (ModelUtils.isIntegerSchema(p) || ModelUtils.isNumberSchema(p) || ModelUtils.isBooleanSchema(p)) {
+            if (p.getDefault() != null) {
+                return p.getDefault().toString();
+            }
+        } else if (ModelUtils.isStringSchema(p)) {
+            if (p.getDefault() != null) {
+                return "'" + escapeText((String) p.getDefault()) + "'";
+            }
+        }
+
+        return null;
+    }
+
+    @Override
+    public String toVarName(String name) {
+        // sanitize name
+        name = sanitizeName(name); // FIXME: a parameter should not be assigned. Also declare the methods parameters as 'final'.
+        // if it's all uppper case, convert to lower case
+        if (name.matches("^[A-Z_]*$")) {
+            name = name.toLowerCase();
+        }
+
+        // camelize (lower first character) the variable name
+        // petId => pet_id
+        name = underscore(name);
+
+        // for reserved word or word starting with number, append _
+        if (isReservedWord(name) || name.matches("^\\d.*")) {
+            name = escapeReservedWord(name);
+        }
+
+        return name;
+    }
+
+    @Override
+    public String toParamName(String name) {
+        // should be the same as variable name
+        return toVarName(name);
+    }
+
+    @Override
+    public String toOperationId(String operationId) {
+        // method name cannot use reserved keyword, e.g. return
+        if (isReservedWord(operationId)) {
+            String newOperationId = underscore("call_" + operationId);
+            LOGGER.warn(operationId + " (reserved word) cannot be used as method name. Renamed to " + newOperationId);
+            return newOperationId;
+        }
+
+        return underscore(operationId);
+    }
+
+    @Override
+    public String escapeQuotationMark(String input) {
+        // remove ' to avoid code injection
+        return input.replace("'", "");
+    }
+
+    @Override
+    public String escapeUnsafeCharacters(String input) {
+        return input.replace("=end", "=_end").replace("=begin", "=_begin");
+    }
+}
