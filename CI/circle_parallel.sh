8ad47bca2995f3ad8c5210d8709ba3b1e6fa710c
William Cheng <wing328hk@gmail.com>
2018-07-20 13:18:33 +0800

    move mvn install to circle.yml

--- CI/circle_parallel.sh
+++ CI/circle_parallel.sh
@@ -3,15 +3,13 @@
 NODE_INDEX=${CIRCLE_NODE_INDEX:-0}
 
 
-mvn --quiet clean install
-
 if [ "$NODE_INDEX" = "1" ]; then
-  echo "Running node $NODE_INDEX ..."
+  echo "Running node $NODE_INDEX to test CI/pom.xml.circleci ..."
   cp CI/pom.xml.circleci pom.xml
   java -version
   mvn --quiet verify -Psamples
 else
-  echo "Running node $NODE_INDEX ..."
+  echo "Running node $NODE_INDEX to test CI/pom.xml.circleci.java7 ..."
   sudo update-java-alternatives -s java-1.7.0-openjdk-amd64
   java -version
   cp CI/pom.xml.circleci.java7 pom.xml
