740ebbfba777c382764cb7bada7d5f0655d42d73
William Cheng <wing328hk@gmail.com>
2018-07-21 20:57:16 +0800

    add submodule update init

--- samples/server/petstore/cpp-pistache/install_pistache.sh
+++ samples/server/petstore/cpp-pistache/install_pistache.sh
@@ -5,6 +5,7 @@ echo "Installing Pistache ..."
 
 git clone https://github.com/oktal/pistache.git
 cd pistache
+git submodule update --init
 mkdir build
 cd build
 cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release ..
