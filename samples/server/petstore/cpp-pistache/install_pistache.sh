48b041b0de86e96ada3c4323776de916da54e678
William Cheng <wing328hk@gmail.com>
2018-07-21 18:59:45 +0800

    test cpp pistache petstore in travis

---
+++ samples/server/petstore/cpp-pistache/install_pistache.sh
@@ -0,0 +1,12 @@
+#/bin/bash
+# ref: http://pistache.io/quickstart#installing-pistache
+#
+echo "Installing Pistache ..."
+
+git clone https://github.com/oktal/pistache.git
+cd pistache
+mkdir build
+cd build
+cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release ..
+make
+sudo make install
