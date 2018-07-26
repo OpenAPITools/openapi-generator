c7784a9b44132ca4f7a4cfe2e4e1c1649377b1e7
Benjamin Gill <benjamin.gill@metaswitch.com>
2018-07-26 16:46:05 +0100

    Add support for multiple rust-server samples
    Though we only have the one as yet. This will make it easier to move rust-server back on to the main test spec, whilst preserving the ability to have rust-specific test specs.

--- bin/rust-server-petstore.sh
+++ bin/rust-server-petstore.sh
@@ -25,8 +25,10 @@ then
   mvn -B clean package
 fi
 
-# if you've executed sbt assembly previously it will use that instead.
-export JAVA_OPTS="${JAVA_OPTS} -XX:MaxPermSize=256M -Xmx1024M -DloggerPath=conf/log4j.properties"
-ags="generate -t modules/openapi-generator/src/main/resources/rust-server -i modules/openapi-generator/src/test/resources/2_0/rust-server/petstore-with-fake-endpoints-models-for-testing.yaml -g rust-server -o samples/server/petstore/rust-server -DpackageName=petstore_api --additional-properties hideGenerationTimestamp=true $@"
+for spec_path in modules/openapi-generator/src/test/resources/2_0/rust-server/* ; do
+  export JAVA_OPTS="${JAVA_OPTS} -XX:MaxPermSize=256M -Xmx1024M -DloggerPath=conf/log4j.properties"
+  spec=$(basename "$spec_path" | sed 's/.yaml//')
+  ags="generate -t modules/openapi-generator/src/main/resources/rust-server -i $spec_path -g rust-server -o samples/server/petstore/rust-server/$spec -DpackageName=petstore_api --additional-properties hideGenerationTimestamp=true $@"
 
-java $JAVA_OPTS -jar $executable $ags
+  java $JAVA_OPTS -jar $executable $ags
+done
