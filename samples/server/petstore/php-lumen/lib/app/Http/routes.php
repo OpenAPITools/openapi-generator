c9b934147ad7bbf0c1871fe69efc93f95faa5572
Akihito Nakano <sora.akatsuki@gmail.com>
2018-07-26 22:26:01 +0900

    [PHP] Escape media type (#615)
    * Move escapeMediaType() to AbstractPhpCodegen
    
    * Escape media type
    
    * Update samples
    
    - bin/php-lumen-petstore-server.sh
    
    * Refactoring: remove unused 'import'

--- samples/server/petstore/php-lumen/lib/app/Http/routes.php
+++ samples/server/petstore/php-lumen/lib/app/Http/routes.php
@@ -81,28 +81,28 @@ $app->get('/v2/fake/jsonFormData', 'FakeApi@testJsonFormData');
  * post fakeOuterBooleanSerialize
  * Summary: 
  * Notes: Test serialization of outer boolean types
- * Output-Formats: [*/*]
+ * Output-Formats: [*_/_*]
  */
 $app->post('/v2/fake/outer/boolean', 'FakeApi@fakeOuterBooleanSerialize');
 /**
  * post fakeOuterCompositeSerialize
  * Summary: 
  * Notes: Test serialization of object with outer number type
- * Output-Formats: [*/*]
+ * Output-Formats: [*_/_*]
  */
 $app->post('/v2/fake/outer/composite', 'FakeApi@fakeOuterCompositeSerialize');
 /**
  * post fakeOuterNumberSerialize
  * Summary: 
  * Notes: Test serialization of outer number types
- * Output-Formats: [*/*]
+ * Output-Formats: [*_/_*]
  */
 $app->post('/v2/fake/outer/number', 'FakeApi@fakeOuterNumberSerialize');
 /**
  * post fakeOuterStringSerialize
  * Summary: 
  * Notes: Test serialization of outer string types
- * Output-Formats: [*/*]
+ * Output-Formats: [*_/_*]
  */
 $app->post('/v2/fake/outer/string', 'FakeApi@fakeOuterStringSerialize');
 /**
