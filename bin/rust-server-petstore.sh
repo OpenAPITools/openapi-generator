df1f4e5c3cfdf61b66f58adca93f5624833ae1cc
Benjamin Gill <benjamin.gill@metaswitch.com>
2018-07-27 12:34:33 +0100

    Move samples to a dedicated directory
    So that there is nothing else in the folder where they live so that the workspace definition in the root Cargo.toml can be simple.

--- bin/rust-server-petstore.sh
+++ bin/rust-server-petstore.sh
@@ -28,7 +28,7 @@ fi
 for spec_path in modules/openapi-generator/src/test/resources/2_0/rust-server/* ; do
   export JAVA_OPTS="${JAVA_OPTS} -XX:MaxPermSize=256M -Xmx1024M -DloggerPath=conf/log4j.properties"
   spec=$(basename "$spec_path" | sed 's/.yaml//')
-  ags="generate -t modules/openapi-generator/src/main/resources/rust-server -i $spec_path -g rust-server -o samples/server/petstore/rust-server/$spec -DpackageName=$spec --additional-properties hideGenerationTimestamp=true $@"
+  ags="generate -t modules/openapi-generator/src/main/resources/rust-server -i $spec_path -g rust-server -o samples/server/petstore/rust-server/output/$spec -DpackageName=$spec --additional-properties hideGenerationTimestamp=true $@"
 
   java $JAVA_OPTS -jar $executable $ags
 done
