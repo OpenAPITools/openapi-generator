c18634e0030411e12aa59c8a7b24a3a4d8505a5e
William Cheng <wing328hk@gmail.com>
2018-08-01 11:47:16 +0800

    defer camelize in operationid

--- modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/AbstractCSharpCodegen.java
+++ modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/AbstractCSharpCodegen.java
@@ -606,7 +606,7 @@ public abstract class AbstractCSharpCodegen extends DefaultCodegen implements Co
         // operationId starts with a number
         if (operationId.matches("^\\d.*")) {
             LOGGER.warn(operationId + " (starting with a number) cannot be used as method name. Renamed to " + camelize(sanitizeName("call_" + operationId)));
-            operationId = camelize("call_" + operationId);
+            operationId = "call_" + operationId;
         }
 
         return camelize(sanitizeName(operationId));
