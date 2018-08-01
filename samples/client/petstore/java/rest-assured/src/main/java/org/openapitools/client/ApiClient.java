12cd26f0ca981db37d177a55001dac8b6afcd0bf
William Cheng <wing328hk@gmail.com>
2018-08-01 15:09:12 +0800

    trigger build failure

--- samples/client/petstore/java/rest-assured/src/main/java/org/openapitools/client/ApiClient.java
+++ samples/client/petstore/java/rest-assured/src/main/java/org/openapitools/client/ApiClient.java
@@ -1,3 +1,4 @@
+dklfajds;kfjakl;sjd;
 /*
  * OpenAPI Petstore
  * This spec is mainly for testing Petstore server and contains fake endpoints, models. Please do not use this for any other purpose. Special characters: \" \\
