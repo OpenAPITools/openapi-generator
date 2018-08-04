85cb02fce99dffd562d3f69ca86c311077e90917
sunn <sunn.ssb@live.com>
2018-08-05 01:19:30 +0200

    rework router to handle multiple path parameters

--- samples/server/petstore/cpp-qt5-qhttpengine-server/server/src/handlers/OAIApiRouter.cpp
+++ samples/server/petstore/cpp-qt5-qhttpengine-server/server/src/handlers/OAIApiRouter.cpp
@@ -24,29 +24,6 @@
 
 namespace OpenAPI {
 
-static inline QString fromQHttpEngineMethod(QHttpEngine::Socket::Method method){
-
-    switch( method ){
-        case QHttpEngine::Socket::Method::OPTIONS:
-            return QStringLiteral("OPTIONS");
-        case QHttpEngine::Socket::Method::GET:
-            return QStringLiteral("GET");
-        case QHttpEngine::Socket::Method::HEAD:
-            return QStringLiteral("HEAD");
-        case QHttpEngine::Socket::Method::POST:
-            return QStringLiteral("POST");
-        case QHttpEngine::Socket::Method::PUT:
-            return QStringLiteral("PUT");
-        case QHttpEngine::Socket::Method::DELETE:
-            return QStringLiteral("DELETE");
-        case QHttpEngine::Socket::Method::TRACE:
-            return QStringLiteral("TRACE");
-        case QHttpEngine::Socket::Method::CONNECT:
-            return QStringLiteral("CONNECT");
-    }
-    return QStringLiteral("");
-}
-
 OAIApiRouter::OAIApiRouter() { 
     createApiHandlers();   
 }
