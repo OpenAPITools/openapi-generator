629608c573bb73a976fb9d1ff1c2e4ae3aa69f31
Dec12 | Fujigon <dec12.kaz@gmail.com>
2018-08-07 17:45:09 +0900

    [jaxrs-spec*] samples should be tested. repair import and dependency. (#755)
    * sample server projects "jaxrs-spec", "jaxrs-spec-interface", and "jaxrs-spec-interface-response" should be tested
    
    * update jax-rs api to 2.1, in which @PATCH annotation is available
    add jackson annotation dependency
    
    * add joda-time to dependency
    
    * modify mustache template to add @JsonCreate, @JsonValue import
    
    * fix return type of enum value() method
    
    * add InputStream import
    
    * Attachment class is in apache cxf. spec should not depend on it
    
    * re-generate sample jaxrs-spec, jaxrs-spec-interface, and jaxrs-spec-interface-response

--- samples/server/petstore/jaxrs-spec/src/gen/java/org/openapitools/model/OuterEnum.java
+++ samples/server/petstore/jaxrs-spec/src/gen/java/org/openapitools/model/OuterEnum.java
@@ -4,7 +4,6 @@ import java.io.Serializable;
 import javax.validation.constraints.*;
 import javax.validation.Valid;
 
-
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonValue;
 
