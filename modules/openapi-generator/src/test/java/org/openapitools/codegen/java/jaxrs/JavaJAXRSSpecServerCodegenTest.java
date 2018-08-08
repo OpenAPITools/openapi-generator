4e20939976de2bff4e38fa9f9d9fa6e6760e8be4
attrobit <github@attrobit.de>
2018-08-08 18:13:27 +0200

    Path generation for primary resource fixed.

--- modules/openapi-generator/src/test/java/org/openapitools/codegen/java/jaxrs/JavaJAXRSSpecServerCodegenTest.java
+++ modules/openapi-generator/src/test/java/org/openapitools/codegen/java/jaxrs/JavaJAXRSSpecServerCodegenTest.java
@@ -1,12 +1,10 @@
 package org.openapitools.codegen.java.jaxrs;
 
 import io.swagger.v3.oas.models.Operation;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.openapitools.codegen.CodegenOperation;
 import org.openapitools.codegen.languages.JavaJAXRSSpecServerCodegen;
 
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -23,23 +21,48 @@ public class JavaJAXRSSpecServerCodegenTest {
     
     /**
      * Test
-     * {@link org.openapitools.codegen.languages.JavaJAXRSSpecServerCodegen#addOperationToGroup(String, String, Operation, CodegenOperation, Map)} for Resource with path "/".
+     * {@link org.openapitools.codegen.languages.JavaJAXRSSpecServerCodegen#addOperationToGroup(String, String, Operation, CodegenOperation, Map)} for Resource with path "/" and set tag.
      */
     @Test
-    @Ignore
     public void testAddOperationToGroupForRootResource() {
         JavaJAXRSSpecServerCodegen instance = new JavaJAXRSSpecServerCodegen();
         
         CodegenOperation codegenOperation = new CodegenOperation();
+        codegenOperation.operationId = "findArbeitsstoffe";
+        Operation operation = new Operation();
+        Map<String, List<CodegenOperation>> operationList = new HashMap<>();
         
-        instance.addOperationToGroup("Arbeitsstoff", "/", null, codegenOperation, Collections.emptyMap());
+        instance.addOperationToGroup("Arbeitsstoff", "/", operation, codegenOperation, operationList);
         
-        System.out.println(codegenOperation.baseName);
+        assertThat(operationList.size(), is(1));
+        assertThat(operationList.containsKey(""), is(true));
+        assertThat(codegenOperation.baseName, is("Arbeitsstoff"));
     }
     
     /**
      * Test
-     * {@link org.openapitools.codegen.languages.JavaJAXRSSpecServerCodegen#addOperationToGroup(String, String, Operation, CodegenOperation, Map)} for Resource with path "/subresource".
+     * {@link org.openapitools.codegen.languages.JavaJAXRSSpecServerCodegen#addOperationToGroup(String, String, Operation, CodegenOperation, Map)} for Resource with path param.
+     */
+    @Test
+    public void testAddOperationToGroupForRootResourcePathParam() {
+        JavaJAXRSSpecServerCodegen instance = new JavaJAXRSSpecServerCodegen();
+        
+        CodegenOperation codegenOperation = new CodegenOperation();
+        codegenOperation.operationId = "getArbeitsstoff";
+        Operation operation = new Operation();
+        Map<String, List<CodegenOperation>> operationList = new HashMap<>();
+        
+        instance.addOperationToGroup("Arbeitsstoff", "/{uuid}", operation, codegenOperation, operationList);
+        
+        assertThat(operationList.size(), is(1));
+        assertThat(operationList.containsKey(""), is(true));
+        assertThat(codegenOperation.baseName, is("Arbeitsstoff"));
+    }
+    
+    /**
+     * Test
+     * {@link org.openapitools.codegen.languages.JavaJAXRSSpecServerCodegen#addOperationToGroup(String, String,
+     * Operation, CodegenOperation, Map)} for Resource with path "/subresource".
      */
     @Test
     public void testAddOperationToGroupForSubresource() {
@@ -47,11 +70,13 @@ public class JavaJAXRSSpecServerCodegenTest {
         
         CodegenOperation codegenOperation = new CodegenOperation();
         codegenOperation.path = "/subresource";
+        Operation operation = new Operation();
         Map<String, List<CodegenOperation>> operationList = new HashMap<>();
         
-        instance.addOperationToGroup("Subresource", "/subresource", null, codegenOperation, operationList);
+        instance.addOperationToGroup("Default", "/subresource", operation, codegenOperation, operationList);
         
         assertThat(codegenOperation.baseName, is("subresource"));
         assertThat(operationList.size(), is(1));
+        assertThat(operationList.containsKey("subresource"), is(true));
     }
 }
