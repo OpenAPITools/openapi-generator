45d1380a0fbe3b3fb6a021ee27bc161a49d7b9b6
William Cheng <wing328hk@gmail.com>
2018-08-12 21:37:21 +0800

    fix windows batch file

--- bin/openapi3/windows/csharp-petstore.bat
+++ bin/openapi3/windows/csharp-petstore.bat
@@ -9,7 +9,7 @@ set ags=generate -i modules\openapi-generator\src\test/resources\2_0\petstore-wi
 
 java %JAVA_OPTS% -jar %executable% %ags%
 
-# restore csproj file
-echo "restore csproject file: CI\samples\client\petstore\csharp\OpenAPIClient\src\Org.OpenAPITools.Test\Org.OpenAPITools.Test.csproj"
-copy /b/v/y .\CI\samples.ci\client\petstore\csharp\OpenAPIClient\src\Org.OpenAPITools.Test\Org.OpenAPITools.Test.csproj .\samples\openapi3\client\petstore\csharp\OpenAPIClient\src\Org.OpenAPITools.Test\
+REM restore csproj file
+echo "restore csproject file: CI\samples.ci\client\petstore\csharp\OpenAPIClient\src\Org.OpenAPITools.Test\Org.OpenAPITools.Test.csproj"
+copy /b/v/y CI\samples.ci\client\petstore\csharp\OpenAPIClient\src\Org.OpenAPITools.Test\Org.OpenAPITools.Test.csproj samples\openapi3\client\petstore\csharp\OpenAPIClient\src\Org.OpenAPITools.Test\
 
