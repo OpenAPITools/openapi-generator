8684d15f38a277010943d0d6e48783e4ab21d4d9
Michael Kotten <michael.kotten@openknowledge.de>
2018-08-16 09:07:00 +0200

    update Petstore sample for jaxrs so that CIs can verify the change

--- samples/server/petstore/jaxrs-spec-interface-response/src/gen/java/org/openapitools/model/ArrayTest.java
+++ samples/server/petstore/jaxrs-spec-interface-response/src/gen/java/org/openapitools/model/ArrayTest.java
@@ -1,5 +1,7 @@
 package org.openapitools.model;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import java.util.ArrayList;
 import java.util.List;
 import org.openapitools.model.ReadOnlyFirst;
