ddec3d2d76d27ea89795feee178424a499e54ee6
Adam Drakeford <adamdrakeford@gmail.com>
2018-08-22 10:41:45 +0100

    Revert "Small fix for date time mappings (plus sample re-gen)"
    This reverts commit 4152dc78b4813da71c675272ca90fb31a333aea1.

--- samples/server/openapi3/petstore/kotlin-springboot/src/main/kotlin/org/openapitools/api/Exceptions.kt
+++ samples/server/openapi3/petstore/kotlin-springboot/src/main/kotlin/org/openapitools/api/Exceptions.kt
@@ -1,12 +1,11 @@
 package org.openapitools.api
 
 import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
-import javax.servlet.http.HttpServletResponse
-import javax.validation.ConstraintViolationException
+import javax.servlet.http.HttpServletRequest
 
-// TODO Extend ApiException for custom exception handling, e.g. the below NotFound exception
 sealed class ApiException(msg: String, val code: Int) : Exception(msg)
 
 class NotFoundException(msg: String, code: Int = HttpStatus.NOT_FOUND.value()) : ApiException(msg, code)
@@ -15,15 +14,11 @@ class NotFoundException(msg: String, code: Int = HttpStatus.NOT_FOUND.value()) :
 @ControllerAdvice
 class DefaultExceptionHandler {
 
-    @ExceptionHandler(value = [ApiException::class])
-    fun onApiException(ex: ApiException, response: HttpServletResponse): Unit =
-            response.sendError(ex.code, ex.message)
+    @ExceptionHandler(value = [NotFoundException::class])
+    fun onNotFound(ex: NotFoundException, request: HttpServletRequest): ResponseEntity<Unit> =
+        ResponseEntity(HttpStatus.valueOf(ex.code))
 
     @ExceptionHandler(value = [NotImplementedError::class])
-    fun onNotImplemented(ex: NotImplementedError, response: HttpServletResponse): Unit =
-            response.sendError(HttpStatus.NOT_IMPLEMENTED.value())
-
-    @ExceptionHandler(value = [ConstraintViolationException::class])
-    fun onConstraintViolation(ex: ConstraintViolationException, response: HttpServletResponse): Unit =
-            response.sendError(HttpStatus.BAD_REQUEST.value(), ex.constraintViolations.joinToString(", ") { it.message })
+    fun onNotImplemented(ex: NotImplementedError, request: HttpServletRequest): ResponseEntity<Unit> =
+        ResponseEntity(HttpStatus.NOT_IMPLEMENTED)
 }
