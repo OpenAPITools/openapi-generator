ea66c10b5c974e30c861dbe77032b9e93fb31555
rienafairefr <rienafairefr@gmail.com>
2018-08-23 15:04:19 +0200

    add the lang|framework|type filter to generator listing command

--- modules/openapi-generator-cli/src/main/java/org/openapitools/codegen/cmd/ListGenerators.java
+++ modules/openapi-generator-cli/src/main/java/org/openapitools/codegen/cmd/ListGenerators.java
@@ -10,6 +10,11 @@ import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Locale;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.apache.commons.lang3.StringUtils.isEmpty;
+import static org.apache.commons.lang3.StringUtils.isNotEmpty;
 
 // NOTE: List can later have subcommands such as list languages, list types, list frameworks, etc.
 @Command(name = "list", description = "Lists the available generators")
@@ -17,6 +22,18 @@ public class ListGenerators implements Runnable {
 
     @Option(name = {"-s", "--short" }, description = "shortened output (suitable for scripting)")
     private Boolean shortened = false;
+
+    @Option(name = {"-lang"}, title = "language",
+            description = "Only show generators with that language")
+    private String lang;
+
+    @Option(name = {"-framework"}, title = "framework",
+            description = "Only show generators using that framework")
+    private String framework;
+
+    @Option(name = {"-type"}, title = "type",
+            description = "Only show generators of that type")
+    private CodegenType type;
 
     @Override
     public void run() {
@@ -35,22 +52,46 @@ public class ListGenerators implements Runnable {
         } else {
             List<CodegenType> types = Arrays.asList(CodegenType.values());
 
-            sb.append("The following generators are available:");
+            if (type!=null || isNotEmpty(lang) || isNotEmpty(framework)) {
+                if (type != null ) {
+                    sb.append("Filtering by type: ").append(type.name()).append(System.lineSeparator());
+                }
+                if (isNotEmpty(lang)) {
+                    sb.append("Filtering by language: ").append(lang).append(System.lineSeparator());
+                }
+                if (isNotEmpty(framework)) {
+                    sb.append("Filtering by framework: ").append(framework).append(System.lineSeparator());
+                }
 
-            sb.append(System.lineSeparator());
-            sb.append(System.lineSeparator());
-
-            for (CodegenType type : types) {
-                sb.append(type.name()).append(" generators:");
+                sb.append("The following generators are available:");
                 sb.append(System.lineSeparator());
 
-                generators.stream()
-                        .filter(g -> g.getTag().equals(type))
-                        .sorted(Comparator.comparing(CodegenConfig::getName))
-                        .forEach(generator -> sb.append("    - ").append(generator.getName()).append(System.lineSeparator()));
+                for (CodegenType currentType : types) {
+                    generators.stream()
+                            .filter(g -> g.getTag().equals(currentType))
+                            .filter(g -> type == null || type.equals(g.getTag()))
+                            .filter(g -> isEmpty(lang) || lang.equals(g.getLanguage()))
+                            .filter(g -> isEmpty(framework) || framework.equals(g.getFramework()))
+                            .sorted(Comparator.comparing(CodegenConfig::getName))
+                            .forEach(generator -> sb.append("    - ").append(generator.getName()).append(System.lineSeparator()));
+                }
+            } else {
+
+                sb.append("The following generators are available:");
 
                 sb.append(System.lineSeparator());
                 sb.append(System.lineSeparator());
+
+                for (CodegenType type : types) {
+                    sb.append(type.name()).append(" generators:");
+                    sb.append(System.lineSeparator());
+
+                    generators.stream()
+                            .sorted(Comparator.comparing(CodegenConfig::getName))
+                            .forEach(generator -> sb.append("    - ").append(generator.getName()).append(System.lineSeparator()));
+                    sb.append(System.lineSeparator());
+                    sb.append(System.lineSeparator());
+                }
             }
         }
 
