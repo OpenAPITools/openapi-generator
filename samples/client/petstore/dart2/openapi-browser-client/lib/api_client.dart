d4c810988a90fc11477539b2cd008e01e85e7243
William Cheng <wing328hk@gmail.com>
2018-08-24 11:23:39 +0800

    add dart2 samples, update travis

---
+++ samples/client/petstore/dart2/openapi-browser-client/lib/api_client.dart
@@ -0,0 +1,160 @@
+part of openapi.api;
+
+class QueryParam {
+  String name;
+  String value;
+
+  QueryParam(this.name, this.value);
+}
+
+class ApiClient {
+
+  String basePath;
+  var client = new BrowserClient();
+
+  Map<String, String> _defaultHeaderMap = {};
+  Map<String, Authentication> _authentications = {};
+
+  final _RegList = new RegExp(r'^List<(.*)>$');
+  final _RegMap = new RegExp(r'^Map<String,(.*)>$');
+
+  ApiClient({this.basePath: "http://petstore.swagger.io/v2"}) {
+    // Setup authentications (key: authentication name, value: authentication).
+    _authentications['api_key'] = new ApiKeyAuth("header", "api_key");
+    _authentications['petstore_auth'] = new OAuth();
+  }
+
+  void addDefaultHeader(String key, String value) {
+     _defaultHeaderMap[key] = value;
+  }
+
+  dynamic _deserialize(dynamic value, String targetType) {
+    try {
+      switch (targetType) {
+        case 'String':
+          return '$value';
+        case 'int':
+          return value is int ? value : int.parse('$value');
+        case 'bool':
+          return value is bool ? value : '$value'.toLowerCase() == 'true';
+        case 'double':
+          return value is double ? value : double.parse('$value');
+        case 'ApiResponse':
+          return new ApiResponse.fromJson(value);
+        case 'Category':
+          return new Category.fromJson(value);
+        case 'Order':
+          return new Order.fromJson(value);
+        case 'Pet':
+          return new Pet.fromJson(value);
+        case 'Tag':
+          return new Tag.fromJson(value);
+        case 'User':
+          return new User.fromJson(value);
+        default:
+          {
+            Match match;
+            if (value is List &&
+                (match = _RegList.firstMatch(targetType)) != null) {
+              var newTargetType = match[1];
+              return value.map((v) => _deserialize(v, newTargetType)).toList();
+            } else if (value is Map &&
+                (match = _RegMap.firstMatch(targetType)) != null) {
+              var newTargetType = match[1];
+              return new Map.fromIterables(value.keys,
+                  value.values.map((v) => _deserialize(v, newTargetType)));
+            }
+          }
+      }
+    } catch (e, stack) {
+      throw new ApiException.withInner(500, 'Exception during deserialization.', e, stack);
+    }
+    throw new ApiException(500, 'Could not find a suitable class for deserialization');
+  }
+
+  dynamic deserialize(String json, String targetType) {
+    // Remove all spaces.  Necessary for reg expressions as well.
+    targetType = targetType.replaceAll(' ', '');
+
+    if (targetType == 'String') return json;
+
+    var decodedJson = JSON.decode(json);
+    return _deserialize(decodedJson, targetType);
+  }
+
+  String serialize(Object obj) {
+    String serialized = '';
+    if (obj == null) {
+      serialized = '';
+    } else {
+      serialized = JSON.encode(obj);
+    }
+    return serialized;
+  }
+
+  // We don't use a Map<String, String> for queryParams.
+  // If collectionFormat is 'multi' a key might appear multiple times.
+  Future<Response> invokeAPI(String path,
+                             String method,
+                             Iterable<QueryParam> queryParams,
+                             Object body,
+                             Map<String, String> headerParams,
+                             Map<String, String> formParams,
+                             String contentType,
+                             List<String> authNames) async {
+
+    _updateParamsForAuth(authNames, queryParams, headerParams);
+
+    var ps = queryParams.where((p) => p.value != null).map((p) => '${p.name}=${p.value}');
+    String queryString = ps.isNotEmpty ?
+                         '?' + ps.join('&') :
+                         '';
+
+    String url = basePath + path + queryString;
+
+    headerParams.addAll(_defaultHeaderMap);
+    headerParams['Content-Type'] = contentType;
+
+    if(body is MultipartRequest) {
+      var request = new MultipartRequest(method, Uri.parse(url));
+      request.fields.addAll(body.fields);
+      request.files.addAll(body.files);
+      request.headers.addAll(body.headers);
+      request.headers.addAll(headerParams);
+      var response = await client.send(request);
+      return Response.fromStream(response);
+    } else {
+      var msgBody = contentType == "application/x-www-form-urlencoded" ? formParams : serialize(body);
+      switch(method) {
+        case "POST":
+          return client.post(url, headers: headerParams, body: msgBody);
+        case "PUT":
+          return client.put(url, headers: headerParams, body: msgBody);
+        case "DELETE":
+          return client.delete(url, headers: headerParams);
+        case "PATCH":
+          return client.patch(url, headers: headerParams, body: msgBody);
+        default:
+          return client.get(url, headers: headerParams);
+      }
+    }
+  }
+
+  /// Update query and header parameters based on authentication settings.
+  /// @param authNames The authentications to apply
+  void _updateParamsForAuth(List<String> authNames, List<QueryParam> queryParams, Map<String, String> headerParams) {
+    authNames.forEach((authName) {
+      Authentication auth = _authentications[authName];
+      if (auth == null) throw new ArgumentError("Authentication undefined: " + authName);
+      auth.applyToParams(queryParams, headerParams);
+    });
+  }
+
+  void setAccessToken(String accessToken) {
+    _authentications.forEach((key, auth) {
+      if (auth is OAuth) {
+        auth.setAccessToken(accessToken);
+      }
+    });
+  }
+}
