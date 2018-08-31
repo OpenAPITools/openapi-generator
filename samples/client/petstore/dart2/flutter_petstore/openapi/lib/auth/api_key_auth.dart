bbcd7bd46653d558fe0f7db1a7bf48c9012dba5d
Yimin Lin <ylin@swipesight.com>
2018-09-01 01:49:18 +0800

    [Dart 2] Add support for Dart 2 (#754)
    * Add an option for Dart2
    
    * add dart2 samples, update travis
    
    * fix dart installation
    
    * Upper constraints on the SDK version
    
    * Update dependencies
    
    * supportDart2 option can now be passed through --additional-properties
    
    * Update petstore tests
    
    * Update dart2-petstore.sh
    
    * Running tests on Dart VM
    
    * Fixed JSON deserialization bugs
    
    * Fixed missing initialization of postBody
    
    * Run bin/dart2-petstore.sh to regenerate libraries
    
    * Update pom.xml
    
    * Added SDK version constraints in pubspec.mustache
    
    * Run bin/dart2-petstore.sh to regenerate libraries
    
    * move dart2 test to the end

---
+++ samples/client/petstore/dart2/flutter_petstore/openapi/lib/auth/api_key_auth.dart
@@ -0,0 +1,27 @@
+part of openapi.api;
+
+class ApiKeyAuth implements Authentication {
+
+  final String location;
+  final String paramName;
+  String apiKey;
+  String apiKeyPrefix;
+
+  ApiKeyAuth(this.location, this.paramName);
+
+  @override
+  void applyToParams(List<QueryParam> queryParams, Map<String, String> headerParams) {
+    String value;
+    if (apiKeyPrefix != null) {
+      value = '$apiKeyPrefix $apiKey';
+    } else {
+      value = apiKey;
+    }
+
+    if (location == 'query' && value != null) {
+      queryParams.add(QueryParam(paramName, value));
+    } else if (location == 'header' && value != null) {
+      headerParams[paramName] = value;
+    }
+  }
+}
