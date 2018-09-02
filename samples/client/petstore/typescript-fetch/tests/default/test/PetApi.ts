c8a9ce24cd46a3a0d023b0de461bc0bae94dd93f
James Batt <getjamesbatt@gmail.com>
2018-09-02 14:04:36 +1000

    updated tests

--- samples/client/petstore/typescript-fetch/tests/default/test/PetApi.ts
+++ samples/client/petstore/typescript-fetch/tests/default/test/PetApi.ts
@@ -3,7 +3,7 @@ import { PetApi, Pet, Category } from '@swagger/typescript-fetch-petstore';
 
 describe('PetApi', () => {
 
-    function runSuite(description: string, requestOptions?: any): void {
+    function runSuite(description: string): void {
 
         describe(description, () => {
 
@@ -15,21 +15,21 @@ describe('PetApi', () => {
             });
 
             it('should add and delete Pet', () => {
-                return api.addPet(fixture, requestOptions).then(() => {
+                return api.addPet({ pet: fixture }).then(() => {
                 });
             });
 
             it('should get Pet by ID', () => {
-                return api.getPetById(fixture.id, requestOptions).then((result: Pet) => {
+                return api.getPetById({ petId: fixture.id }).then((result: Pet) => {
                     return expect(result).to.deep.equal(fixture);
                 });
             });
 
             it('should update Pet by ID', () => {
-                return api.getPetById(fixture.id, requestOptions).then((result: Pet) => {
+                return api.getPetById({ petId: fixture.id }).then((result: Pet) => {
                     result.name = 'newname';
-                    return api.updatePet(result, requestOptions).then(() => {
-                        return api.getPetById(fixture.id, requestOptions).then((result: Pet) => {
+                    return api.updatePet({ pet: result }).then(() => {
+                        return api.getPetById({ petId: fixture.id }).then((result: Pet) => {
                             return expect(result.name).to.deep.equal('newname');
                         });
                     });
@@ -37,11 +37,11 @@ describe('PetApi', () => {
             });
 
             it('should delete Pet', () => {
-                return api.deletePet(fixture.id, requestOptions);
+                return api.deletePet({ petId: fixture.id });
             });
 
             it('should not contain deleted Pet', () => {
-                return api.getPetById(fixture.id, requestOptions).then((result: Pet) => {
+                return api.getPetById({ petId: fixture.id }).then((result: Pet) => {
                     return expect(result).to.not.exist;
                 }, (err: any) => {
                     return expect(err).to.exist;
@@ -52,11 +52,6 @@ describe('PetApi', () => {
     }
 
     runSuite('without custom request options');
-
-    runSuite('with custom request options', {
-        credentials: 'include',
-        mode: 'cors'
-    });
 
 });
 
