c5953495663efa8f3780f9849360639d8a710961
William Cheng <wing328hk@gmail.com>
2018-09-12 21:17:12 +0800

    fix import in test files

--- samples/client/petstore/C/unit-test/test-PetAPI.c
+++ samples/client/petstore/C/unit-test/test-PetAPI.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include "apiClient.h"
 #include "cJSON.h"
-#include "{import=#include "api_response.h", classname=api_response}.h"
-#include "{import=#include "pet.h", classname=pet}.h"
+#include "api_response.h"
+#include "pet.h"
 
 #define MAX_BUFFER_LENGTH 9
--- samples/client/petstore/C/unit-test/test-StoreAPI.c
+++ samples/client/petstore/C/unit-test/test-StoreAPI.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include "apiClient.h"
 #include "cJSON.h"
-#include "{import=#include "map.h", classname=map}.h"
-#include "{import=#include "order.h", classname=order}.h"
+#include "map.h"
+#include "order.h"
 
 #define MAX_BUFFER_LENGTH 9
