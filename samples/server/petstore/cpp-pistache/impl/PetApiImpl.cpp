634defbc7e3e045b28171522ae3938844e0fc152
sunn <33183834+etherealjoy@users.noreply.github.com>
2018-09-12 09:53:01 +0200

    [c++ pistache server] Support basic query handling (#943)
    Support basic query handling
    
    Add  helpers for primitive de-serialization
    Remove warnings due to unneeded commas
    Deserialize basic types in queries
    Add dependencies chain for external libraries
    Fixes wrong parameter passed to API

--- samples/server/petstore/cpp-pistache/impl/PetApiImpl.cpp
+++ samples/server/petstore/cpp-pistache/impl/PetApiImpl.cpp
@@ -29,10 +29,10 @@ void PetApiImpl::add_pet(const Pet &pet, Pistache::Http::ResponseWriter &respons
 void PetApiImpl::delete_pet(const int64_t &petId, const Pistache::Optional<Pistache::Http::Header::Raw> &apiKey, Pistache::Http::ResponseWriter &response) {
     response.send(Pistache::Http::Code::Ok, "Do some magic\n");
 }
-void PetApiImpl::find_pets_by_status(const Pistache::Optional<std::string> &status, Pistache::Http::ResponseWriter &response) {
+void PetApiImpl::find_pets_by_status(const Pistache::Optional<std::vector<std::string>> &status, Pistache::Http::ResponseWriter &response) {
     response.send(Pistache::Http::Code::Ok, "Do some magic\n");
 }
-void PetApiImpl::find_pets_by_tags(const Pistache::Optional<std::string> &tags, Pistache::Http::ResponseWriter &response) {
+void PetApiImpl::find_pets_by_tags(const Pistache::Optional<std::vector<std::string>> &tags, Pistache::Http::ResponseWriter &response) {
     response.send(Pistache::Http::Code::Ok, "Do some magic\n");
 }
 void PetApiImpl::get_pet_by_id(const int64_t &petId, Pistache::Http::ResponseWriter &response) {
