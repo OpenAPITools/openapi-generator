18308b3e2a20bbc892ca31aeb29803d28e40cdcf
Hemant Zope <hemant.zope@fokus.fraunhofer.de>
2018-09-12 11:29:54 +0200

    updated sample

--- samples/client/petstore/C/include/apiClient.h
+++ samples/client/petstore/C/include/apiClient.h
@@ -8,32 +8,25 @@ typedef int bool;
 #define false 0
 
 typedef struct apiClient_t {
-	char *basePath;
-	void *dataReceived;
-	// this would only be generated for basic authentication
+    char *basePath;
+    void *dataReceived;
+    // this would only be generated for basic authentication
     #ifdef BASIC_AUTH
-	char *username;
-	char *password;
-    #endif // BASIC_AUTH
-	// this would only be generated for OAUTH2 authentication
+    char *username;
+    char *password;
+    #endif //BASIC_AUTH
+    // this would only be generated for OAUTH2 authentication
     #ifdef OAUTH2
-	char *accessToken;
+    char *accessToken;
     #endif // OAUTH2
     #ifdef API_KEY
-	// this would only be generated for apiKey authentication
-	list_t *apiKeys;
+    //this would only be generated for apiKey authentication
+    list_t *apiKeys;
     #endif // API_KEY
 } apiClient_t;
 
-apiClient_t *apiClient_create();
+apiClient_t* apiClient_create();
 void apiClient_free(apiClient_t *apiClient);
-void apiClient_invoke(apiClient_t	*apiClient,
-                      char		*operationName,
-                      char		*operationParameter,
-                      list_t		*queryParameters,
-                      list_t		*headerParameters,
-                      list_t		*formParameters,
-                      char		*bodyParameters,
-                      char		*requestType);
+void apiClient_invoke(apiClient_t *apiClient, char* operationName, char* operationParameter, list_t *queryParameters, list_t *headerParameters, list_t *formParameters, char *bodyParameters, char *requestType);
 
 #endif // INCLUDE_API_CLIENT_H
