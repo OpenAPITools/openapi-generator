a9956cb1c7c32ab6a16c344a4258bc9a70c147cf
ircecho <anton.wimmer@calasoul.com>
2018-09-14 12:40:37 +0200

    [DART2] Maps could not be deserialized in dart 2 (#1007)
    * fix[dart2]: due to stronger type checking in dart 2, maps could not be deserialized
    
    * rebuild dart2 petstore

--- samples/client/petstore/dart2/openapi/lib/model/user.dart
+++ samples/client/petstore/dart2/openapi/lib/model/user.dart
@@ -54,11 +54,11 @@ class User {
         : json.map((value) => new User.fromJson(value)).toList();
   }
 
-  static Map<String, User> mapFromJson(Map<String, Map<String, dynamic>> json) {
+  static Map<String, User> mapFromJson(Map<String, dynamic> json) {
     var map = new Map<String, User>();
     if (json != null && json.length > 0) {
-      json.forEach((String key, Map<String, dynamic> value) =>
-          map[key] = new User.fromJson(value));
+      json.forEach(
+          (String key, dynamic value) => map[key] = new User.fromJson(value));
     }
     return map;
   }
