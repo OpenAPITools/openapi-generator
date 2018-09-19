01e5d4de450fefca441c73242896b5941d082bca
kemokemo <t2wonderland@gmail.com>
2018-09-19 22:43:53 +0900

    delete unnecessary comments (#1048)

--- modules/openapi-generator/src/test/java/org/openapitools/codegen/options/GoGinServerOptionsProvider.java
+++ modules/openapi-generator/src/test/java/org/openapitools/codegen/options/GoGinServerOptionsProvider.java
@@ -1,7 +1,4 @@
 /*
- * Copyright 2018 OpenAPI-Generator Contributors (https://openapi-generator.tech)
- * Copyright 2018 SmartBear Software
- *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
