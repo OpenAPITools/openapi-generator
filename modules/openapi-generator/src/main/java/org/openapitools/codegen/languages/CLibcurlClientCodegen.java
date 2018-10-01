6d5d6bffecb09f54b8b1b129f4838ad52bd293d4
Hemant Zope <hemant.zope@fokus.fraunhofer.de>
2018-10-01 11:42:44 +0200

    renamed manually written test files

--- modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/CLibcurlClientCodegen.java
+++ modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/CLibcurlClientCodegen.java
@@ -203,11 +203,11 @@ public class CLibcurlClientCodegen extends DefaultCodegen implements CodegenConf
         supportingFiles.add(new SupportingFile("cJSON.h.mustache", "external" + File.separator + "include", "cJSON.h"));
 
         // test files should not be overwritten
-       writeOptional(outputFolder, new SupportingFile("api_store_test.mustache", "unit-tests", "StoreAPI.c"));
-       writeOptional(outputFolder, new SupportingFile("api_user_test.mustache", "unit-tests", "UserAPI.c"));
-       writeOptional(outputFolder, new SupportingFile("api_test.mustache", "unit-tests", "PetAPI.c"));
-       writeOptional(outputFolder, new SupportingFile("model_order_test.mustache", "unit-tests", "order.c"));
-       writeOptional(outputFolder, new SupportingFile("model_user_test.mustache", "unit-tests", "user.c"));
+       writeOptional(outputFolder, new SupportingFile("api_store_test.mustache", "unit-tests", "manual-StoreAPI.c"));
+       writeOptional(outputFolder, new SupportingFile("api_user_test.mustache", "unit-tests", "manual-UserAPI.c"));
+       writeOptional(outputFolder, new SupportingFile("api_pet_test.mustache", "unit-tests", "manual-PetAPI.c"));
+       writeOptional(outputFolder, new SupportingFile("model_order_test.mustache", "unit-tests", "manual-order.c"));
+       writeOptional(outputFolder, new SupportingFile("model_user_test.mustache", "unit-tests", "manual-user.c"));
        //writeOptional(outputFolder, new SupportingFile("apiKey.c.mustache", "unit-tests", "apiKey.c"));
     }
 
