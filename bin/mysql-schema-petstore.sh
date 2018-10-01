3a5ec0a69d4cc4e6b45af2eefe8d274a791ae712
William Cheng <wing328hk@gmail.com>
2018-10-01 19:46:33 +0800

    Minor enhancements to the MySQL generator (#1148)
    * minor enhancements to the mysql generator
    
    * add ybelenko to the README

--- bin/mysql-schema-petstore.sh
+++ bin/mysql-schema-petstore.sh
@@ -26,7 +26,7 @@ then
 fi
 
 # if you've executed sbt assembly previously it will use that instead.
-export JAVA_OPTS="${JAVA_OPTS} -XX:MaxPermSize=256M -Xmx1024M -DloggerPath=conf/log4j.properties"
+export JAVA_OPTS="${JAVA_OPTS} -Xmx1024M -DloggerPath=conf/log4j.properties"
 ags="generate -t modules/openapi-generator/src/main/resources/mysql-schema -i modules/openapi-generator/src/test/resources/2_0/petstore-with-fake-endpoints-models-for-testing.yaml -g mysql-schema -o samples/schema/petstore/mysql $@"
 
 java $JAVA_OPTS -jar $executable $ags
