970f996566a740045f2a986fd70fc70f11952925
Benoît Courtine <benoit.courtine@gmail.com>
2018-10-22 10:23:38 +0200

    Merge scripts generating rust-hyper and rust-reqwest samples.

--- bin/rust-petstore.sh
+++ bin/rust-petstore.sh
@@ -26,7 +26,12 @@ then
 fi
 
 # if you've executed sbt assembly previously it will use that instead.
-export JAVA_OPTS="${JAVA_OPTS} -XX:MaxPermSize=256M -Xmx1024M -DloggerPath=conf/log4j.properties"
-ags="generate -t modules/openapi-generator/src/main/resources/rust -i modules/openapi-generator/src/test/resources/2_0/petstore.yaml -g rust -o samples/client/petstore/rust -DpackageName=petstore_client --library=hyper $@"
+export JAVA_OPTS="${JAVA_OPTS} -Xmx1024M -DloggerPath=conf/log4j.properties"
 
+# Generate client using hyper lib.
+ags="generate -t modules/openapi-generator/src/main/resources/rust -i modules/openapi-generator/src/test/resources/2_0/petstore.yaml -g rust -o samples/client/petstore/rust -DpackageName=petstore_client --library=hyper $@"
+java ${JAVA_OPTS} -jar ${executable} ${ags}
+
+# Generate client using reqwest lib.
+ags="generate -t modules/openapi-generator/src/main/resources/rust -i modules/openapi-generator/src/test/resources/2_0/petstore.yaml -g rust -o samples/client/petstore/rust-reqwest -DpackageName=petstore_client --library=reqwest $@"
 java ${JAVA_OPTS} -jar ${executable} ${ags}
