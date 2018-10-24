206a7201ea68687947b136bdaec2ad07a66cc059
William Cheng <wing328hk@gmail.com>
2018-10-24 17:27:53 +0800

    fix javadoc for JDK10

--- run-in-docker.sh
+++ run-in-docker.sh
@@ -15,4 +15,4 @@ docker run --rm -it \
         -v "${PWD}:/gen" \
         -v "${maven_cache_repo}:/var/maven/.m2/repository" \
         --entrypoint /gen/docker-entrypoint.sh \
-        maven:3-jdk-8 "$@"
+        maven:3-jdk-9 "$@"
