b82042a4e5f504d19e7b354edf97be06e1dcfaa8
Benoît Courtine <benoit.courtine@gmail.com>
2018-10-26 11:24:14 +0200

    [Rust] Client library choice between hyper and reqwest (#1258)
    * Port of PR https://github.com/swagger-api/swagger-codegen/pull/8804.
    
    * Correction of conflict with PR #528 (missing template file).
    
    * Add rust-reqwest samples to Circle CI tests.
    
    * Add integration test pom.xml file with launcher to trigger cargo execution.
    
    * Deduplicate Maven project name.
    
    * Fix "api_key" header for Petstore.
    
    * Better API key management.
    
    * Fix query param for lists of objects other than strings (numbers, etc.).
    
    * Update to reqwest 0.9, and refactor of header management (using reqwest transition feature).
    
    * Merge scripts generating rust-hyper and rust-reqwest samples.
    
    * Consistent full stops.
    
    * Use raw variables in all Rust mustache templates.
    
    * Replace production build in CI with a quick simple check.
    
    * Update samples.
    
    * Finish Reqwest 0.9 migration (removing "hyper 0.11" transition feature).
    
    * Configuration implements Default trait.
    
    * API template reorganized: HashMap is not required anymore.
    
    * Revert "Merge scripts generating rust-hyper and rust-reqwest samples."
    
    This reverts commit 970f996566a740045f2a986fd70fc70f11952925.
    
    * Remove deprecated "-XX:MaxPermSize" java arg.

--- samples/client/petstore/rust-reqwest/src/lib.rs
+++ samples/client/petstore/rust-reqwest/src/lib.rs
@@ -1,11 +1,10 @@
 #[macro_use]
 extern crate serde_derive;
 
-extern crate hyper;
 extern crate serde;
 extern crate serde_json;
-extern crate futures;
 extern crate url;
+extern crate reqwest;
 
 pub mod apis;
 pub mod models;
