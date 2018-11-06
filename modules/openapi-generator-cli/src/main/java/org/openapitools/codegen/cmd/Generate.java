32a2b105c667efc855105e87fc54e8c61c4e70c5
John Lilley <john.lilley@redpoint.net>
2018-11-06 10:22:39 -0700

    Implement "overwrite only changed files" feature with --minimal-update option. Add unit test.

--- modules/openapi-generator-cli/src/main/java/org/openapitools/codegen/cmd/Generate.java
+++ modules/openapi-generator-cli/src/main/java/org/openapitools/codegen/cmd/Generate.java
@@ -211,6 +211,11 @@ public class Generate implements Runnable {
     @Option(name = {"--enable-post-process-file"}, title = "enable post-process file", description = CodegenConstants.ENABLE_POST_PROCESS_FILE)
     private Boolean enablePostProcessFile;
 
+    @Option(name = {"--minimal-update"},
+        title = "Minimal update",
+        description = "Only write output files that have changed.")
+    private Boolean minimalUpdate;
+
     @Override
     public void run() {
         if (logToStderr != null) {
@@ -334,6 +339,10 @@ public class Generate implements Runnable {
 
         if (enablePostProcessFile != null) {
             configurator.setEnablePostProcessFile(enablePostProcessFile);
+        }
+
+        if (minimalUpdate != null) {
+            configurator.setEnableMinimalUpdate(minimalUpdate);
         }
 
         applySystemPropertiesKvpList(systemProperties, configurator);
