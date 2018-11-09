b798306423925ac6e12a6270b78fa3deae8a545b
sunn <33183834+etherealjoy@users.noreply.github.com>
2018-11-09 16:49:47 +0100

    Fixes double prefixing during model import (#1398)

--- modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/CppQt5ClientCodegen.java
+++ modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/CppQt5ClientCodegen.java
@@ -253,7 +253,7 @@ public class CppQt5ClientCodegen extends AbstractCppCodegen implements CodegenCo
         if (!folder.isEmpty())
             folder += File.separator;
 
-        return "#include \"" + folder + toModelName(name) + ".h\"";
+        return "#include \"" + folder + sanitizeName(name) + ".h\"";
     }
 
     /**
