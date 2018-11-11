01bf14371c7d4ec733d8505a9f8714ba056f66e2
William Cheng <wing328hk@gmail.com>
2018-11-11 11:10:14 +0800

    remove duplicated imports

--- samples/client/petstore/java/okhttp-gson/src/main/java/org/openapitools/client/JSON.java
+++ samples/client/petstore/java/okhttp-gson/src/main/java/org/openapitools/client/JSON.java
@@ -27,7 +27,6 @@ import org.threeten.bp.LocalDate;
 import org.threeten.bp.OffsetDateTime;
 import org.threeten.bp.format.DateTimeFormatter;
 
-import org.openapitools.client.model.*;
 import okio.ByteString;
 
 import java.io.IOException;
--- samples/client/petstore/java/retrofit2rx/src/main/java/org/openapitools/client/JSON.java
+++ samples/client/petstore/java/retrofit2rx/src/main/java/org/openapitools/client/JSON.java
@@ -27,7 +27,6 @@ import org.threeten.bp.LocalDate;
 import org.threeten.bp.OffsetDateTime;
 import org.threeten.bp.format.DateTimeFormatter;
 
-import org.openapitools.client.model.*;
 
 import java.io.IOException;
 import java.io.StringReader;
