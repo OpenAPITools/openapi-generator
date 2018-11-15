0c1754c259cf658fc5e8f9957367fdedd0cfd499
Christoph, René Pardon <pardon@vimp.com>
2018-11-15 08:09:49 +0100

    remove MaxPermSize form bash script as it will be removed from later Java versions; add isNullable utility helper method; Adjust TODOs for graphql implementation

--- bin/openapi3/graphql-petstore-server.sh
+++ bin/openapi3/graphql-petstore-server.sh
@@ -36,7 +36,7 @@ then
 fi
 
 # if you've executed sbt assembly previously it will use that instead.
-export JAVA_OPTS="${JAVA_OPTS} -XX:MaxPermSize=256M -Xmx1024M -DloggerPath=conf/log4j.properties"
+export JAVA_OPTS="${JAVA_OPTS} -Xmx1024M -DloggerPath=conf/log4j.properties"
 ags="generate -t $SCRIPT_DIR/../../modules/openapi-generator/src/main/resources/graphql-server -i $SCRIPT_DIR/../../modules/openapi-generator/src/test/resources/3_0/petstore.yaml -g graphql-server -o $TARGET_DIR $@"
 
 java $JAVA_OPTS -jar $executable $ags
--- samples/server/petstore/graphql-server/openapi3graphql-server/README.md
+++ samples/server/petstore/graphql-server/openapi3graphql-server/README.md
@@ -13,7 +13,6 @@ This is a sample server Petstore server. For this sample, you can use the api ke
 
 # TODOs
 
-- adjust express/apollo v2* update https://medium.com/@jeffrey.allen.lewis/graphql-migrating-from-apollo-server-express-1-0-to-2-0-be80f5c61bee
-- support for union types
-- use `ID`-type for unique identifiers instead of `Int`
+- use `ID`-type for unique identifiers instead of `Int` (detect UUID)
 - add example responses to resolvers.mustache
+- support for union types
