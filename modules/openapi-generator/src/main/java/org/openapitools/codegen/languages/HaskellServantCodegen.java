9e5d92052e553fa76161f90faa508a9eba0f96fe
adamConnerSax <adam_conner_sax@yahoo.com>
2018-11-16 15:24:31 -0500

    changed date related mappings in haskell-servant backend.  date -> Date.Time.Calendar.Day and date-time -> Date.Time.LocalTime.LocalTime.  Also updated the import of BaseUrl(..) and fixed an issue with underscores in field names.

--- modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/HaskellServantCodegen.java
+++ modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/HaskellServantCodegen.java
@@ -82,7 +82,7 @@ public class HaskellServantCodegen extends DefaultCodegen implements CodegenConf
      * @return A string value for the help message
      */
     public String getHelp() {
-        return "Generates a Haskell server and client library.";
+        return "Generates a Haskell Servant server and client library.";
     }
 
     public HaskellServantCodegen() {
@@ -98,6 +98,9 @@ public class HaskellServantCodegen extends DefaultCodegen implements CodegenConf
         specialCharReplacements.remove("\"");
         specialCharReplacements.put("\\\\", "Back_Slash");
         specialCharReplacements.put("\\\"", "Double_Quote");
+
+        // Allow underscores?
+        specialCharReplacements.remove("_");
 
         // set the output folder here
         outputFolder = "generated-code/haskell-servant";
@@ -179,7 +182,11 @@ public class HaskellServantCodegen extends DefaultCodegen implements CodegenConf
         typeMapping.put("char", "Char");
         typeMapping.put("float", "Float");
         typeMapping.put("double", "Double");
-        typeMapping.put("DateTime", "Integer");
+        typeMapping.put("date-time", "LocalTime");
+        typeMapping.put("datetime", "LocalTime");
+        typeMapping.put("dateTime", "LocalTime");
+        typeMapping.put("DateTime", "LocalTime");
+        typeMapping.put("date", "Day");
         typeMapping.put("file", "FilePath");
         typeMapping.put("binary", "FilePath");
         typeMapping.put("number", "Double");
