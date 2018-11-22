c775169e13adb097d8aa74c4d90292d39e4f95d8
kingofdisasterr <sandro@kojan.net>
2018-11-22 12:34:58 +0100

    fix #1421: [Java][jaxrs-jersey] avoid unnecessary multipart imports in generated code - detect usage of multipart features in api spec input. (remove '\t')

--- modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/features/MultipartFeatures.java
+++ modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/features/MultipartFeatures.java
@@ -1,8 +1,8 @@
 package org.openapitools.codegen.languages.features;
 
 public interface MultipartFeatures {
-	
-	// Language supports usage of Multipart Features
+
+    // Language supports usage of Multipart Features
     public static final String USE_MULTIPART_FEATURE = "useMultipartFeature";
 
     public void setUseMultipartFeature(boolean useMultipartFeature);
