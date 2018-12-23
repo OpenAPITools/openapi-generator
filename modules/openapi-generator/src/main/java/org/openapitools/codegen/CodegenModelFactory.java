99779d17c45138dafc3fb7a80ef379960f51d782
akihito.nakano <sora.akatsuki@gmail.com>
2018-12-23 11:48:20 +0900

    Delete unused method

--- modules/openapi-generator/src/main/java/org/openapitools/codegen/CodegenModelFactory.java
+++ modules/openapi-generator/src/main/java/org/openapitools/codegen/CodegenModelFactory.java
@@ -25,24 +25,6 @@ public final class CodegenModelFactory {
 
     private static final Map<CodegenModelType, Class<?>> typeMapping = new HashMap<CodegenModelType, Class<?>>();
 
-    /**
-     * Configure a different implementation class.
-     *
-     * @param type           the type that shall be replaced
-     * @param implementation the implementation class must extend the default class and must provide a public no-arg constructor
-     */
-    public static void setTypeMapping(CodegenModelType type, Class<?> implementation) {
-        if (!type.getDefaultImplementation().isAssignableFrom(implementation)) {
-            throw new IllegalArgumentException(implementation.getSimpleName() + " doesn't extend " + type.getDefaultImplementation().getSimpleName());
-        }
-        try {
-            implementation.getDeclaredConstructor().newInstance();
-        } catch (Exception e) {
-            throw new IllegalArgumentException(e);
-        }
-        typeMapping.put(type, implementation);
-    }
-
     @SuppressWarnings("unchecked")
     public static <T> T newInstance(CodegenModelType type) {
         Class<?> classType = typeMapping.get(type);
