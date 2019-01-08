b3a97b61137a85dd9a97be7fdd5d28c94f41c06b
d3v-cl <christian.loitsch@calasoul.com>
2019-01-08 17:26:46 +0100

    [DART] fix: set fields to null if json value is null. (#1798)
    * fix: set fields to null if json value is null.
    
    * rebuild dart2 petstore
    
    * rebuild dart petstore
    
    * rebuild petstore
    
    * [DART]fix: set fields to null if json value is null.

--- samples/client/petstore/dart/openapi/lib/model/pet.dart
+++ samples/client/petstore/dart/openapi/lib/model/pet.dart
@@ -1,14 +1,15 @@
 part of openapi.api;
 
 class Pet {
+  
   int id = null;
-
+  
   Category category = null;
-
+  
   String name = null;
-
+  
   List<String> photoUrls = [];
-
+  
   List<Tag> tags = [];
   /* pet status in the store */
   String status = null;
@@ -22,13 +23,36 @@ class Pet {
 
   Pet.fromJson(Map<String, dynamic> json) {
     if (json == null) return;
-    id = json['id'];
-    category = new Category.fromJson(json['category']);
-    name = json['name'];
-    photoUrls =
-        (json['photoUrls'] as List).map((item) => item as String).toList();
-    tags = Tag.listFromJson(json['tags']);
-    status = json['status'];
+    if (json['id'] == null) {
+      id = null;
+    } else {
+      id = json['id'];
+    }
+    if (json['category'] == null) {
+      category = null;
+    } else {
+      category = new Category.fromJson(json['category']);
+    }
+    if (json['name'] == null) {
+      name = null;
+    } else {
+      name = json['name'];
+    }
+    if (json['photoUrls'] == null) {
+      photoUrls = null;
+    } else {
+      photoUrls = (json['photoUrls'] as List).map((item) => item as String).toList();
+    }
+    if (json['tags'] == null) {
+      tags = null;
+    } else {
+      tags = Tag.listFromJson(json['tags']);
+    }
+    if (json['status'] == null) {
+      status = null;
+    } else {
+      status = json['status'];
+    }
   }
 
   Map<String, dynamic> toJson() {
@@ -43,17 +67,15 @@ class Pet {
   }
 
   static List<Pet> listFromJson(List<dynamic> json) {
-    return json == null
-        ? new List<Pet>()
-        : json.map((value) => new Pet.fromJson(value)).toList();
+    return json == null ? new List<Pet>() : json.map((value) => new Pet.fromJson(value)).toList();
   }
 
   static Map<String, Pet> mapFromJson(Map<String, Map<String, dynamic>> json) {
     var map = new Map<String, Pet>();
     if (json != null && json.length > 0) {
-      json.forEach((String key, Map<String, dynamic> value) =>
-          map[key] = new Pet.fromJson(value));
+      json.forEach((String key, Map<String, dynamic> value) => map[key] = new Pet.fromJson(value));
     }
     return map;
   }
 }
+
