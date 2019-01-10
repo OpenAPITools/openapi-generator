ccde71956635ca3461117c5eb8f21db22cbbef54
Silas Boyd-Wickizer <silasbw@gmail.com>
2019-01-10 10:04:47 -0800

    Update Petstore sample

--- samples/client/petstore/typescript-node/default/api/storeApi.ts
+++ samples/client/petstore/typescript-node/default/api/storeApi.ts
@@ -77,7 +77,7 @@ export class StoreApi {
      * @summary Delete purchase order by ID
      * @param orderId ID of the order that needs to be deleted
      */
-    public deleteOrder (orderId: string, options: any = {}) : Promise<{ response: http.ClientResponse; body?: any;  }> {
+    public deleteOrder (orderId: string, options: {headerParams: {[name: string]: value}} = {headerParams: {}}) : Promise<{ response: http.ClientResponse; body?: any;  }> {
         const localVarPath = this.basePath + '/store/order/{orderId}'
             .replace('{' + 'orderId' + '}', encodeURIComponent(String(orderId)));
         let localVarQueryParameters: any = {};
@@ -89,7 +89,7 @@ export class StoreApi {
             throw new Error('Required parameter orderId was null or undefined when calling deleteOrder.');
         }
 
-        (<any>Object).assign(localVarHeaderParams, options.headers);
+        (<any>Object).assign(localVarHeaderParams, options.headerParams);
 
         let localVarUseFormData = false;
 
@@ -129,13 +129,13 @@ export class StoreApi {
      * Returns a map of status codes to quantities
      * @summary Returns pet inventories by status
      */
-    public getInventory (options: any = {}) : Promise<{ response: http.ClientResponse; body: { [key: string]: number; };  }> {
+    public getInventory (options: {headerParams: {[name: string]: value}} = {headerParams: {}}) : Promise<{ response: http.ClientResponse; body: { [key: string]: number; };  }> {
         const localVarPath = this.basePath + '/store/inventory';
         let localVarQueryParameters: any = {};
         let localVarHeaderParams: any = (<any>Object).assign({}, this.defaultHeaders);
         let localVarFormParams: any = {};
 
-        (<any>Object).assign(localVarHeaderParams, options.headers);
+        (<any>Object).assign(localVarHeaderParams, options.headerParams);
 
         let localVarUseFormData = false;
 
@@ -179,7 +179,7 @@ export class StoreApi {
      * @summary Find purchase order by ID
      * @param orderId ID of pet that needs to be fetched
      */
-    public getOrderById (orderId: number, options: any = {}) : Promise<{ response: http.ClientResponse; body: Order;  }> {
+    public getOrderById (orderId: number, options: {headerParams: {[name: string]: value}} = {headerParams: {}}) : Promise<{ response: http.ClientResponse; body: Order;  }> {
         const localVarPath = this.basePath + '/store/order/{orderId}'
             .replace('{' + 'orderId' + '}', encodeURIComponent(String(orderId)));
         let localVarQueryParameters: any = {};
@@ -191,7 +191,7 @@ export class StoreApi {
             throw new Error('Required parameter orderId was null or undefined when calling getOrderById.');
         }
 
-        (<any>Object).assign(localVarHeaderParams, options.headers);
+        (<any>Object).assign(localVarHeaderParams, options.headerParams);
 
         let localVarUseFormData = false;
 
@@ -233,7 +233,7 @@ export class StoreApi {
      * @summary Place an order for a pet
      * @param body order placed for purchasing the pet
      */
-    public placeOrder (body: Order, options: any = {}) : Promise<{ response: http.ClientResponse; body: Order;  }> {
+    public placeOrder (body: Order, options: {headerParams: {[name: string]: value}} = {headerParams: {}}) : Promise<{ response: http.ClientResponse; body: Order;  }> {
         const localVarPath = this.basePath + '/store/order';
         let localVarQueryParameters: any = {};
         let localVarHeaderParams: any = (<any>Object).assign({}, this.defaultHeaders);
@@ -244,7 +244,7 @@ export class StoreApi {
             throw new Error('Required parameter body was null or undefined when calling placeOrder.');
         }
 
-        (<any>Object).assign(localVarHeaderParams, options.headers);
+        (<any>Object).assign(localVarHeaderParams, options.headerParams);
 
         let localVarUseFormData = false;
 
