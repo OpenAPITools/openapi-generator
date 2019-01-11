2172cc277e6e2159a8dc5981294f60368d9cd5cf
Jyhess <jyhess@gmail.com>
2019-01-11 16:35:21 +0100

    Python AIOHTTP server generator (#1470)
    * Astract factory for generators based on connexion
    
    * Add aiohttp server generator
    
    * Fix flask tests
    
    * Normalize python-flask folder names

--- bin/python-server-aiohttp-petstore.sh
+++ bin/python-server-aiohttp-petstore.sh
@@ -22,29 +22,23 @@ executable="./modules/openapi-generator-cli/target/openapi-generator-cli.jar"
 
 if [ ! -f "$executable" ]
 then
-  mvn clean package
+  mvn -B clean package
 fi
 
-# if you've executed sbt assembly previously it will use that instead.
-input=modules/openapi-generator/src/test/resources/3_0/petstore.yaml
-out_folder=samples/server/openapi3/petstore/python-flask
-resources=modules/openapi-generator/src/main/resources/python-flask
+generator=python-aiohttp
+input=modules/openapi-generator/src/test/resources/2_0/petstore.yaml
+out_folder=samples/server/petstore/$generator
+resources=modules/openapi-generator/src/main/resources/$generator
 
 # if you've executed sbt assembly previously it will use that instead.
 export JAVA_OPTS="${JAVA_OPTS} -Xmx1024M -DloggerPath=conf/log4j.properties"
-ags="generate -t $resources -i $input -g python-flask -o $out_folder -Dservice $@"
+ags="generate -t $resources -i $input -g $generator -o $out_folder -Dservice $@"
 
 rm -rf $out_folder/.openapi*
 rm -rf $out_folder/openapi_server
-rm $out_folder/.dockerignore
-rm $out_folder/.gitignore
-rm $out_folder/.travis.yml
-rm $out_folder/Dockerfile
-rm $out_folder/git_push.sh
+rm -rf $out_folder/tests*
 rm $out_folder/README.md
 rm $out_folder/requirements.txt
-rm $out_folder/setup.py
 rm $out_folder/test-requirements.txt
-rm $out_folder/tox.ini
 
 java $JAVA_OPTS -jar $executable $ags
