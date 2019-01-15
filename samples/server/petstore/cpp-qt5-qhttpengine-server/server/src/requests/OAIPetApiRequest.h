963c0028302e2edb02a93fd5bee4fb8d0a9c002b
sunn <33183834+etherealjoy@users.noreply.github.com>
2019-01-15 13:58:52 +0100

    Update Readme for qt5 server (#1912)

--- samples/server/petstore/cpp-qt5-qhttpengine-server/server/src/requests/OAIPetApiRequest.h
+++ samples/server/petstore/cpp-qt5-qhttpengine-server/server/src/requests/OAIPetApiRequest.h
@@ -77,12 +77,12 @@ public:
     void setResponseHeaders(const QMultiMap<QString,QString>& headers);
 
 signals:
-    void addPet(OAIPet oai_pet);
+    void addPet(OAIPet body);
     void deletePet(qint64 pet_id, QString api_key);
     void findPetsByStatus(QList<QString> status);
     void findPetsByTags(QList<QString> tags);
     void getPetById(qint64 pet_id);
-    void updatePet(OAIPet oai_pet);
+    void updatePet(OAIPet body);
     void updatePetWithForm(qint64 pet_id, QString name, QString status);
     void uploadFile(qint64 pet_id, QString additional_metadata, QIODevice* file);
     
