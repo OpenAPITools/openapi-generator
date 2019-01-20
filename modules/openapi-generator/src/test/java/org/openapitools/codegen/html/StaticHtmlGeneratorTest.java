06824622bfc62ca03e4be9db6142d1755d7992aa
Jérémie Bresson <dev@jmini.fr>
2019-01-20 17:51:18 +0100

    [html] Use ModelUtils to avoid NullPointerException (#1948)

--- modules/openapi-generator/src/test/java/org/openapitools/codegen/html/StaticHtmlGeneratorTest.java
+++ modules/openapi-generator/src/test/java/org/openapitools/codegen/html/StaticHtmlGeneratorTest.java
@@ -17,10 +17,13 @@
 
 package org.openapitools.codegen.html;
 
+import io.swagger.parser.OpenAPIParser;
+import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.media.IntegerSchema;
 import io.swagger.v3.oas.models.media.ObjectSchema;
 import io.swagger.v3.oas.models.media.Schema;
 import io.swagger.v3.oas.models.media.StringSchema;
+import io.swagger.v3.parser.core.models.ParseOptions;
 
 import org.openapitools.codegen.CodegenModel;
 import org.openapitools.codegen.languages.StaticHtmlGenerator;
@@ -41,6 +44,16 @@ public class StaticHtmlGeneratorTest {
                 .addProperties("name", new StringSchema());
         CodegenModel cm = codegen.fromModel("test", schema, Collections.emptyMap());
         Assert.assertNotNull(cm);
+    }
+
+    @Test
+    public void testSpecWithoutSchema() throws Exception {
+        final OpenAPI openAPI = new OpenAPIParser().readLocation("src/test/resources/3_0/ping.yaml", null, new ParseOptions()).getOpenAPI();
+
+        final StaticHtmlGenerator codegen = new StaticHtmlGenerator();
+        codegen.preprocessOpenAPI(openAPI);
+
+        Assert.assertEquals(openAPI.getInfo().getTitle(), "ping test");
     }
 
 }
