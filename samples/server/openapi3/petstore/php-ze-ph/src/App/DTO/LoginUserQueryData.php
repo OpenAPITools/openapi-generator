75fd0261555c48404a521dc8e574493d6d8b784c
Arthur Mogliev <articusw@gmail.com>
2019-01-30 11:36:00 +0400

    [PHP][php-ze-ph] Support for PHP 7.1+, Zend Expressive 3.2 and PathHander 0.4  (#1902)
    * - support for PHP 7.1, Zend Expressive 3.2 and PathHander 0.4 for php-ze-ph generator
    
    * - fixed mess with petstore samples (added new files, removed obsolete files)
    
    * php-ze-ph:
    - overwriting "*/*" media type for producers with "n/a" (PathHandler does not support that cause it makes no sense to return response with "Content-Type: */*")
    - "array" return type declaration for handler methods with ambiguous "container" return type
    - better way to generate attribute annotation stub for request body data with ambiguous "container" type
    - fixed missing dependency in composer.json
    - minor optimization for container.php
    - samples for OAS3 petstore spec
    
    * php-ze-ph:
    - note about ext-yaml in stub README
    - updated .gitignore
    
    * php-ze-ph:
    - logging '*/*' replacement as warning

---
+++ samples/server/openapi3/petstore/php-ze-ph/src/App/DTO/LoginUserQueryData.php
@@ -0,0 +1,29 @@
+<?php
+declare(strict_types=1);
+
+namespace App\DTO;
+
+use Articus\DataTransfer\Annotation as DTA;
+
+/**
+ * Query parameters for loginUser
+ */
+class LoginUserQueryData
+{
+    /**
+     * The password for login in clear text
+     * @DTA\Data(field="password")
+     * @DTA\Strategy(name="QueryParameter", options={"type":"string"})
+     * @DTA\Validator(name="QueryParameterType", options={"type":"string"})
+     * @var string
+     */
+    public $password;
+    /**
+     * The user name for login
+     * @DTA\Data(field="username")
+     * @DTA\Strategy(name="QueryParameter", options={"type":"string"})
+     * @DTA\Validator(name="QueryParameterType", options={"type":"string"})
+     * @var string
+     */
+    public $username;
+}
--- samples/server/petstore/php-ze-ph/src/App/Handler/User.php
+++ samples/server/petstore/php-ze-ph/src/App/Handler/User.php
@@ -1,8 +1,8 @@
 <?php
+declare(strict_types=1);
 
 namespace App\Handler;
 
-use Articus\PathHandler\Operation;
 use Articus\PathHandler\Annotation as PHA;
 use Articus\PathHandler\Consumer as PHConsumer;
 use Articus\PathHandler\Producer as PHProducer;
@@ -13,20 +13,21 @@ use Psr\Http\Message\ServerRequestInterface;
 /**
  * @PHA\Route(pattern="/user")
  */
-class User implements Operation\PostInterface
+class User
 {
     /**
      * Create user
+     * @PHA\Post()
      * @PHA\Attribute(name=PHAttribute\Transfer::class, options={"type":\App\DTO\User::class,"objectAttr":"bodyData"})
      * @param ServerRequestInterface $request
      *
-     * @throws PHException\HttpCode 500 if the method is not implemented
+     * @throws PHException\HttpCode 501 if the method is not implemented
      */
-    public function handlePost(ServerRequestInterface $request)
+    public function createUser(ServerRequestInterface $request)
     {
         //TODO implement method
         /** @var \App\DTO\User $bodyData */
         $bodyData = $request->getAttribute("bodyData");
-        throw new PHException\HttpCode(500, "Not implemented");
+        throw new PHException\HttpCode(501, "Not implemented");
     }
 }
