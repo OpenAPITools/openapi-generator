fd0847864a6734fb627983b2bc806296ed0197c3
Sven Panne <svenpanne@gmail.com>
2019-02-12 05:20:34 +0100

    Fixed handling of dotted module names in python generator. (#2016)
    Previously, if you used a packageName of the form "foo.bar.baz", half of the
    generated files of the python generator went into a subdirectory
    "foo/bar/baz" (correct), the other half went into a subdirectory
    "foo.bar.baz" (incorrect).

--- modules/openapi-generator/src/test/java/org/openapitools/codegen/python/PythonClientOptionsTest.java
+++ modules/openapi-generator/src/test/java/org/openapitools/codegen/python/PythonClientOptionsTest.java
@@ -24,6 +24,8 @@ import org.openapitools.codegen.CodegenConfig;
 import org.openapitools.codegen.languages.PythonClientCodegen;
 import org.openapitools.codegen.options.PythonClientOptionsProvider;
 
+import java.io.File;
+
 public class PythonClientOptionsTest extends AbstractOptionsTest {
 
     @Tested
@@ -43,11 +45,20 @@ public class PythonClientOptionsTest extends AbstractOptionsTest {
     protected void setExpectations() {
         new Expectations(clientCodegen) {{
             clientCodegen.setPackageName(PythonClientOptionsProvider.PACKAGE_NAME_VALUE);
-            clientCodegen.setProjectName(PythonClientOptionsProvider.PROJECT_NAME_VALUE);
-            clientCodegen.setPackageVersion(PythonClientOptionsProvider.PACKAGE_VERSION_VALUE);
-            clientCodegen.setPackageUrl(PythonClientOptionsProvider.PACKAGE_URL_VALUE);
-            // clientCodegen.setLibrary(PythonClientCodegen.DEFAULT_LIBRARY);
             times = 1;
+
+            clientCodegen.setProjectName(PythonClientOptionsProvider.PROJECT_NAME_VALUE);
+            times = 1;
+
+            clientCodegen.setPackageVersion(PythonClientOptionsProvider.PACKAGE_VERSION_VALUE);
+            times = 1;
+
+            clientCodegen.setPackageUrl(PythonClientOptionsProvider.PACKAGE_URL_VALUE);
+            times = 1;
+
+            clientCodegen.packagePath();
+            result = PythonClientOptionsProvider.PACKAGE_NAME_VALUE.replace('.', File.separatorChar);
+            minTimes = 1;
         }};
     }
 }
