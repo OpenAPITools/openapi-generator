b58421ec3d7ade2234fd764ba8671f807b2cf31b
Rainer Schlönvoigt <rainer.schlonvoigt@philips.com>
2019-03-06 15:41:41 +0100

    [kotlin-ktor-client] removing dependency on Java BigDecimal and Java File to make sure the code would also work for an iOS project

--- modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/KotlinKtorClientCodegen.java
+++ modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/KotlinKtorClientCodegen.java
@@ -5,9 +5,10 @@ import org.openapitools.codegen.CodegenType;
 import org.openapitools.codegen.SupportingFile;
 
 import java.io.File;
+import java.util.HashMap;
 
 public class KotlinKtorClientCodegen extends AbstractKotlinCodegen {
-    
+
     public KotlinKtorClientCodegen() {
         super();
 
@@ -23,6 +24,18 @@ public class KotlinKtorClientCodegen extends AbstractKotlinCodegen {
         //These should already be taken care off by AbstractKotlinCodegen TODO: double-check
         apiPackage = packageName + ".apis";
         modelPackage = packageName + ".models";
+
+        typeMapping.put("number", "kotlin.Double");
+        //TODO: find a date library/solution for kotlin native
+        typeMapping.put("date-time", "kotlin.String");
+        typeMapping.put("date", "kotlin.String");
+        typeMapping.put("Date", "kotlin.String");
+        typeMapping.put("DateTime", "kotlin.String");
+
+        //TODO: change to something else
+        typeMapping.put("file", "kotlin.String");
+
+        importMapping = new HashMap<>();
     }
 
     @Override
@@ -47,15 +60,9 @@ public class KotlinKtorClientCodegen extends AbstractKotlinCodegen {
         //At the point of writing this plugin kotlinx serialization does not yet support enum names that aren't exact matches.
         enumPropertyNaming = CodegenConstants.ENUM_PROPERTY_NAMING_TYPE.original;
 
-        //TODO: find a date library/solution for kotlin native
-        typeMapping.put("date-time", "kotlin.String");
-        typeMapping.put("date", "kotlin.String");
-        typeMapping.put("Date", "kotlin.String");
-        typeMapping.put("DateTime", "kotlin.String");
-
         supportingFiles.add(new SupportingFile("README.mustache", "", "README.md"));
 
-        final String infrastructureFolder = (sourceFolder + File.separator + packageName + File.separator + "infrastructure").replace(".", "/");
+//        final String infrastructureFolder = (sourceFolder + File.separator + packageName + File.separator + "infrastructure").replace(".", "/");
 
         //TODO: Add infrastructure classes
 
