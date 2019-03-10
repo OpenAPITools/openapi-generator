970462ff8cd9c88dfde92ab254060e17539bff29
William Cheng <wing328hk@gmail.com>
2019-03-10 22:50:06 +0800

    fix r installation error

--- samples/client/petstore/R/test_petstore.bash
+++ samples/client/petstore/R/test_petstore.bash
@@ -2,8 +2,14 @@
 
 set -e
 
-sudo Rscript -e 'install.packages("jsonlite", repos="https://cran.rstudio.com")'
-sudo Rscript -e 'install.packages("httr", repos="https://cran.rstudio.com")'
+sudo Rscript -e 'install.packages("jsonlite", repos="https://cran.cnr.berkeley.edu/")'
+sudo Rscript -e 'install.packages("httr", repos="https://cran.cnr.berkeley.edu/")'
+sudo Rscript -e 'install.packages("testthat", repos="https://cran.cnr.berkeley.edu/")'
+sudo Rscript -e 'install.packages("R6", repos="https://cran.cnr.berkeley.edu/")'
+sudo Rscript -e 'install.packages("caTools", repos="https://cran.cnr.berkeley.edu/")'
+
+#Packages required but not available: ‘jsonlite’ ‘httr’ ‘R6’ ‘caTools’
+#Package suggested but not available: ‘testthat’
 
 R CMD build .
 R CMD check *tar.gz
