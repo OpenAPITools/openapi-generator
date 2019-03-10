cdf3960ac65e4e1734bdbf23c030891627271acb
William Cheng <wing328hk@gmail.com>
2019-03-10 21:50:10 +0800

    fix permission error

--- samples/client/petstore/R/test_petstore.bash
+++ samples/client/petstore/R/test_petstore.bash
@@ -2,8 +2,8 @@
 
 set -e
 
-Rscript -e 'install.packages("jsonlite", repos="https://cran.rstudio.com")'
-Rscript -e 'install.packages("httr", repos="https://cran.rstudio.com")'
+sudo Rscript -e 'install.packages("jsonlite", repos="https://cran.rstudio.com")'
+sudo Rscript -e 'install.packages("httr", repos="https://cran.rstudio.com")'
 
 R CMD build .
 R CMD check *tar.gz
