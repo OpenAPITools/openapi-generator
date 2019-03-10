7950ef3c650cafb08e153d9c227aefcaf528fcd5
William Cheng <wing328hk@gmail.com>
2019-03-10 16:15:31 +0800

    fix r bash test

--- samples/client/petstore/R/test_petstore.bash
+++ samples/client/petstore/R/test_petstore.bash
@@ -2,8 +2,8 @@
 
 set -e
 
-Rscript -e 'install.packages("jsonlite")'
-Rscript -e 'install.packages("httr")'
+Rscript -e 'install.packages("jsonlite", repos="https://cran.rstudio.com")'
+Rscript -e 'install.packages("httr", repos="https://cran.rstudio.com")'
 
 R CMD build .
 R CMD check *tar.gz
