22a7cd9932dca58af48c4480eb5930a8084a6fe3
William Cheng <wing328hk@gmail.com>
2019-03-11 14:47:37 +0800

    update lib location

--- samples/client/petstore/R/test_petstore.bash
+++ samples/client/petstore/R/test_petstore.bash
@@ -8,11 +8,11 @@ export R_LIBS_USER=$HOME/R
 
 echo "R lib directory: $R_LIBS_USER"
 
-Rscript -e "install.packages('jsonlite', repos='$REPO')"
-Rscript -e "install.packages('httr', repos='$REPO')"
-Rscript -e "install.packages('testthat', repos='$REPO')"
-Rscript -e "install.packages('R6', repos='$REPO')"
-Rscript -e "install.packages('caTools', repos='$REPO')"
+Rscript -e "install.packages('jsonlite', repos='$REPO', lib='$R_LIBS_USER')"
+Rscript -e "install.packages('httr', repos='$REPO', lib='$R_LIBS_USER')"
+Rscript -e "install.packages('testthat', repos='$REPO', lib='$R_LIBS_USER')"
+Rscript -e "install.packages('R6', repos='$REPO', lib='$R_LIBS_USER')"
+Rscript -e "install.packages('caTools', repos='$REPO', lib='$R_LIBS_USER')"
 
 R CMD build .
 R CMD check *tar.gz --no-manual
