f92c562530ff89b2546fbdc1f0e5235fc0a23d12
William Cheng <wing328hk@gmail.com>
2019-03-11 13:28:49 +0800

    better caching

--- samples/client/petstore/R/test_petstore.bash
+++ samples/client/petstore/R/test_petstore.bash
@@ -4,12 +4,14 @@ set -e
 
 REPO=http://cran.revolutionanalytics.com
 
-sudo Rscript -e "install.packages('jsonlite', repos='$REPO')"
-sudo Rscript -e "install.packages('httr', repos='$REPO')"
-sudo Rscript -e "install.packages('testthat', repos='$REPO')"
-sudo Rscript -e "install.packages('R6', repos='$REPO')"
-sudo Rscript -e "install.packages('caTools', repos='$REPO')"
+R_LIBS_USER=$HOME/R
+
+Rscript -e "install.packages('jsonlite', repos='$REPO')"
+Rscript -e "install.packages('httr', repos='$REPO')"
+Rscript -e "install.packages('testthat', repos='$REPO')"
+Rscript -e "install.packages('R6', repos='$REPO')"
+Rscript -e "install.packages('caTools', repos='$REPO')"
 
 R CMD build .
 R CMD check *tar.gz --no-manual
-sudo R CMD INSTALL *tar.gz
+R CMD INSTALL *tar.gz
