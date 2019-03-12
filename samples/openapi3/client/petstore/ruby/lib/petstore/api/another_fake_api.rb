fcfe687f192a98e7a7c15c8463afab3fbd7a1b4e
ota42y <ota42y+github@gmail.com>
2019-03-13 01:17:49 +0900

    Outputs dataType to YARD tag  (#2329)
    * output dataType to YARD tag
    
    YARD supports parameter type tag.
    https://www.rubydoc.info/gems/yard/file/docs/Tags.md#param
    
    We can use `dataType` as parameter type.
    So it's very useful to output `dataType`.
    
    * update samples

--- samples/openapi3/client/petstore/ruby/lib/petstore/api/another_fake_api.rb
+++ samples/openapi3/client/petstore/ruby/lib/petstore/api/another_fake_api.rb
@@ -21,7 +21,7 @@ module Petstore
     end
     # To test special tags
     # To test special tags and operation ID starting with number
-    # @param client client model
+    # @param client [Client] client model
     # @param [Hash] opts the optional parameters
     # @return [Client]
     def call_123_test_special_tags(client, opts = {})
@@ -31,7 +31,7 @@ module Petstore
 
     # To test special tags
     # To test special tags and operation ID starting with number
-    # @param client client model
+    # @param client [Client] client model
     # @param [Hash] opts the optional parameters
     # @return [Array<(Client, Fixnum, Hash)>] Client data, response status code and response headers
     def call_123_test_special_tags_with_http_info(client, opts = {})
