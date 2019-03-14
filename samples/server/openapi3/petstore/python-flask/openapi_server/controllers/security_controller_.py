809da58165db006c190dc13463a904743e3f40d0
Tom Ghyselinck <tom.ghyselinck@linbox.be>
2019-03-14 09:33:35 +0100

    Python apikey cookie (#2367)
    * The `ApiClient` will now have support to store and use HTTP Cookies (as APIKey auth).
    
    * Use Cookie authentication for user management.
    
    * - Updated Python related samples.

--- samples/server/openapi3/petstore/python-flask/openapi_server/controllers/security_controller_.py
+++ samples/server/openapi3/petstore/python-flask/openapi_server/controllers/security_controller_.py
@@ -17,6 +17,22 @@ def info_from_api_key(api_key, required_scopes):
     return {'uid': 'user_id'}
 
 
+def info_from_auth_cookie(api_key, required_scopes):
+    """
+    Check and retrieve authentication information from api_key.
+    Returned value will be passed in 'token_info' parameter of your operation function, if there is one.
+    'sub' or 'uid' will be set in 'user' parameter of your operation function, if there is one.
+
+    :param api_key API key provided by Authorization header
+    :type api_key: str
+    :param required_scopes Always None. Used for other authentication method
+    :type required_scopes: None
+    :return: Information attached to provided api_key or None if api_key is invalid or does not allow access to called API
+    :rtype: dict | None
+    """
+    return {'uid': 'user_id'}
+
+
 def info_from_petstore_auth(token):
     """
     Validate and decode token.
