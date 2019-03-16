e5a0d183744c3a7d331b77af4d451ff36cc3da16
Jason Henriksen <jason.henriksen@gmail.com>
2019-03-15 20:53:45 -0700

    PR for ability to easily debug newly created codegen classes. (#2388)
    * 2nd attempt to make the automatic checkin testing work
    
    * trying to submit the samples in hopes that the CI matches them and passes
    
    * found some samples I hadn't updated.  Maybe this is it?

---
+++ samples/client/petstore/kotlin/bin/org/openapitools/client/infrastructure/ApiAbstractions.kt
@@ -0,0 +1,20 @@
+package org.openapitools.client.infrastructure
+
+typealias MultiValueMap = Map<String,List<String>>
+
+fun collectionDelimiter(collectionFormat: String) = when(collectionFormat) {
+    "csv" -> ","
+    "tsv" -> "\t"
+    "pipes" -> "|"
+    "ssv" -> " "
+    else -> ""
+}
+
+val defaultMultiValueConverter: (item: Any?) -> String = { item -> "$item" }
+
+fun <T: Any?> toMultiValue(items: List<T>, collectionFormat: String, map: (item: Any?) -> String = defaultMultiValueConverter): List<String> {
+    return when(collectionFormat) {
+        "multi" -> items.map(map)
+        else -> listOf(items.map(map).joinToString(separator = collectionDelimiter(collectionFormat)))
+    }
+}
