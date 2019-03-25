6bec233f6e63d6d7ef8001bd835eae2fab998409
Rainer Schlönvoigt <rainer.schlonvoigt@philips.com>
2019-03-25 11:38:19 +0100

    [kotlin-ktor-client]: adding API key authentication

--- modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/KotlinKtorClientCodegen.java
+++ modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/KotlinKtorClientCodegen.java
@@ -80,6 +80,7 @@ public class KotlinKtorClientCodegen extends AbstractKotlinCodegen {
         supportingFiles.add(new SupportingFile("infrastructure/authentication/AuthProvider.kt.mustache", infrastructureFolder+"/authentication", "AuthProvider.kt"));
         supportingFiles.add(new SupportingFile("infrastructure/authentication/Base64.kt.mustache", infrastructureFolder+"/authentication", "Base64.kt"));
         supportingFiles.add(new SupportingFile("infrastructure/authentication/HttpBasicAuth.kt.mustache", infrastructureFolder+"/authentication", "HttpBasicAuth.kt"));
+        supportingFiles.add(new SupportingFile("infrastructure/authentication/ApiKeyAuth.kt.mustache", infrastructureFolder+"/authentication", "ApiKeyAuth.kt"));
     }
 
     @Override
