e612f6f11a1a44bef2c1e7927bfbfbd693181bcc
Vincent Devos <46601673+karismann@users.noreply.github.com>
2019-03-26 17:03:24 +0100

    [TypeScript Client] fix npm version when snapshot is true and npmversion is snapshot too (#2401)

--- modules/openapi-generator/src/test/java/org/openapitools/codegen/typescript/typescriptnode/TypeScriptNodeClientCodegenTest.java
+++ modules/openapi-generator/src/test/java/org/openapitools/codegen/typescript/typescriptnode/TypeScriptNodeClientCodegenTest.java
@@ -17,4 +17,45 @@ public class TypeScriptNodeClientCodegenTest {
        Assert.assertEquals(codegen.toVarName("user|name"), "userName");
        Assert.assertEquals(codegen.toVarName("user !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~name"), "user$Name");
    }
+
+   @Test
+   public void testSnapshotVersion() {
+      TypeScriptNodeClientCodegen codegen = new TypeScriptNodeClientCodegen();
+      codegen.additionalProperties().put("npmName", "@openapi/typescript-angular-petstore");
+      codegen.additionalProperties().put("snapshot", true);
+      codegen.additionalProperties().put("npmVersion", "1.0.0-SNAPSHOT");
+      codegen.processOpts();
+
+      org.junit.Assert.assertTrue(codegen.getNpmVersion().matches("^1.0.0-SNAPSHOT.[0-9]{12}$"));
+
+      codegen = new TypeScriptNodeClientCodegen();
+      codegen.additionalProperties().put("npmName", "@openapi/typescript-angular-petstore");
+      codegen.additionalProperties().put("snapshot", true);
+      codegen.additionalProperties().put("npmVersion", "3.0.0-M1");
+      codegen.processOpts();
+
+      org.junit.Assert.assertTrue(codegen.getNpmVersion().matches("^3.0.0-M1-SNAPSHOT.[0-9]{12}$"));
+
+   }
+
+   @Test
+   public void testWithoutSnapshotVersion() {
+      TypeScriptNodeClientCodegen codegen = new TypeScriptNodeClientCodegen();
+      codegen.additionalProperties().put("npmName", "@openapi/typescript-angular-petstore");
+      codegen.additionalProperties().put("snapshot", false);
+      codegen.additionalProperties().put("npmVersion", "1.0.0-SNAPSHOT");
+      codegen.processOpts();
+
+      org.junit.Assert.assertTrue(codegen.getNpmVersion().matches("^1.0.0-SNAPSHOT$"));
+
+      codegen = new TypeScriptNodeClientCodegen();
+      codegen.additionalProperties().put("npmName", "@openapi/typescript-angular-petstore");
+      codegen.additionalProperties().put("snapshot", false);
+      codegen.additionalProperties().put("npmVersion", "3.0.0-M1");
+      codegen.processOpts();
+
+      org.junit.Assert.assertTrue(codegen.getNpmVersion().matches("^3.0.0-M1$"));
+
+   }
+
 }
