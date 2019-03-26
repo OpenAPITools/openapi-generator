603b4912f104ea68401989ff1b9b1389c38feca5
William Cheng <wing328hk@gmail.com>
2019-03-26 11:21:44 +0800

    update java vertx client dep

--- samples/client/petstore/java/vertx/src/main/java/org/openapitools/client/model/Order.java
+++ samples/client/petstore/java/vertx/src/main/java/org/openapitools/client/model/Order.java
@@ -20,7 +20,7 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonValue;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
-import org.threeten.bp.OffsetDateTime;
+import java.time.OffsetDateTime;
 
 /**
  * Order
