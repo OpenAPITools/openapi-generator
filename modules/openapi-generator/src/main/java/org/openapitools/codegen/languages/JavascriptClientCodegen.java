823b84fe01a312237eece6a5489a985017859867
William Cheng <wing328hk@gmail.com>
2019-03-31 12:41:37 +0800

    update travis for nodejs

--- modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/JavascriptClientCodegen.java
+++ modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/JavascriptClientCodegen.java
@@ -205,7 +205,7 @@ public class JavascriptClientCodegen extends DefaultCodegen implements CodegenCo
 
     @Override
     public String getHelp() {
-        return "Generates a Javascript client library.";
+        return "Generates a JavaScript client library.";
     }
 
     @Override
@@ -340,6 +340,7 @@ public class JavascriptClientCodegen extends DefaultCodegen implements CodegenCo
 
         supportingFiles.add(new SupportingFile("index.mustache", createPath(sourceFolder, invokerPackage), "index.js"));
         supportingFiles.add(new SupportingFile("ApiClient.mustache", createPath(sourceFolder, invokerPackage), "ApiClient.js"));
+
     }
 
     @Override
@@ -980,7 +981,7 @@ public class JavascriptClientCodegen extends DefaultCodegen implements CodegenCo
 
                 if (hasOptionalParams) {
                     argList.add("opts");
-                } 
+                }
 
                 if (!usePromises) {
                     argList.add("callback");
@@ -1123,17 +1124,6 @@ public class JavascriptClientCodegen extends DefaultCodegen implements CodegenCo
 
         return codegenModel;
     }
-
-    /*
-    private static String sanitizePackageName(String packageName) { // FIXME parameter should not be assigned. Also declare it as "final"
-        packageName = packageName.trim();
-        packageName = packageName.replaceAll("[^a-zA-Z0-9_\\.]", "_");
-        if (Strings.isNullOrEmpty(packageName)) {
-            return "invalidPackageName";
-        }
-        return packageName;
-    }
-    */
 
     @Override
     public String toEnumName(CodegenProperty property) {
