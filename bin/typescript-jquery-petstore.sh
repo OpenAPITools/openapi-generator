2a6cd8b521630ed1d9eb271354258d0865e428de
Richard Whitehouse <github@richardwhiuk.com>
2019-04-23 04:34:01 +0100

    Remove `-XX:MaxPermSize` (#2712)
    This was removed in JDK 8 and produces a warning

--- bin/typescript-jquery-petstore.sh
+++ bin/typescript-jquery-petstore.sh
@@ -26,7 +26,7 @@ then
 fi
 
 # if you've executed sbt assembly previously it will use that instead.
-export JAVA_OPTS="${JAVA_OPTS} -XX:MaxPermSize=256M -Xmx1024M -DloggerPath=conf/log4j.properties"
+export JAVA_OPTS="${JAVA_OPTS} -Xmx1024M -DloggerPath=conf/log4j.properties"
 ags="generate -i modules/openapi-generator/src/test/resources/2_0/petstore.yaml -g typescript-jquery -o samples/client/petstore/typescript-jquery/default $@"
 
 java $JAVA_OPTS -jar $executable $ags
--- new.sh
+++ new.sh
@@ -248,7 +248,7 @@ then
 fi
 
 # if you've executed sbt assembly previously it will use that instead.
-export JAVA_OPTS="\${JAVA_OPTS} -XX:MaxPermSize=256M -Xmx1024M -DloggerPath=conf/log4j.properties"
+export JAVA_OPTS="\${JAVA_OPTS} -Xmx1024M -DloggerPath=conf/log4j.properties"
 ags="\$@ generate -i modules/openapi-generator/src/test/resources/2_0/petstore.yaml -g ${gen_name_camel} -o samples/${gen_type}/petstore/${gen_name_camel_path}"
 
 java \${JAVA_OPTS} -jar \${executable} \${ags}
