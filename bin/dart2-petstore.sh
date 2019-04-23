a4be2c0bb40e5cc44310483665b258e56c7cebde
Richard Whitehouse <github@richardwhiuk.com>
2019-04-23 04:34:01 +0100

    Remove `-XX:MaxPermSize` (#2712)
    This was removed in JDK 8 and produces a warning

--- bin/dart2-petstore.sh
+++ bin/dart2-petstore.sh
@@ -26,7 +26,7 @@ then
 fi
 
 # if you've executed sbt assembly previously it will use that instead.
-export JAVA_OPTS="${JAVA_OPTS} -XX:MaxPermSize=256M -Xmx1024M -DloggerPath=conf/log4j.properties"
+export JAVA_OPTS="${JAVA_OPTS} -Xmx1024M -DloggerPath=conf/log4j.properties"
 
 # Generate non-browserClient
 ags="generate -t modules/openapi-generator/src/main/resources/dart -i modules/openapi-generator/src/test/resources/2_0/petstore.yaml -g dart -o samples/client/petstore/dart2/openapi -DhideGenerationTimestamp=true -DbrowserClient=false  $@"
