8e906983295e7e25cdb895fe4a6f68cd1e41a351
William Cheng <wing328hk@gmail.com>
2019-05-07 16:14:22 +0800

    create nodejs express esrver

---
+++ modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/NodeJSExpressServerCodegen.java
@@ -0,0 +1,422 @@
+/*
+ * Copyright 2018 OpenAPI-Generator Contributors (https://openapi-generator.tech)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.openapitools.codegen.languages;
+
+import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Multimap;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.Operation;
+import io.swagger.v3.oas.models.PathItem;
+import io.swagger.v3.oas.models.PathItem.HttpMethod;
+import io.swagger.v3.oas.models.Paths;
+import io.swagger.v3.oas.models.info.Info;
+import org.openapitools.codegen.*;
+import org.openapitools.codegen.config.GeneratorProperties;
+import org.openapitools.codegen.utils.URLPathUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.net.URL;
+import java.util.*;
+import java.util.Map.Entry;
+import java.util.regex.Pattern;
+
+import static org.openapitools.codegen.utils.StringUtils.*;
+
+public class NodeJSExpressServerCodegen extends DefaultCodegen implements CodegenConfig {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(NodeJSExpressServerCodegen.class);
+    protected String implFolder = "service";
+    public static final String EXPORTED_NAME = "exportedName";
+    public static final String SERVER_PORT = "serverPort";
+
+    protected String apiVersion = "1.0.0";
+    protected String projectName = "openapi-server";
+    protected String defaultServerPort = "8080";
+
+    protected boolean googleCloudFunctions;
+    protected String exportedName;
+
+    public NodeJSExpressServerCodegen() {
+        super();
+
+        // set the output folder here
+        outputFolder = "generated-code/nodejs";
+
+        /*
+         * Models.  You can write model files using the modelTemplateFiles map.
+         * if you want to create one template for file, you can do so here.
+         * for multiple files for model, just put another entry in the `modelTemplateFiles` with
+         * a different extension
+         */
+        modelTemplateFiles.clear();
+
+        /*
+         * Api classes.  You can write classes for each Api file with the apiTemplateFiles map.
+         * as with models, add multiple entries with different extensions for multiple files per
+         * class
+         */
+        apiTemplateFiles.put(
+                "controller.mustache",   // the template to use
+                ".js");       // the extension for each file to write
+
+        /*
+         * Template Location.  This is the location which templates will be read from.  The generator
+         * will use the resource stream to attempt to read the templates.
+         */
+        embeddedTemplateDir = templateDir = "nodejs";
+
+        /*
+         * Reserved words.  Override this with reserved words specific to your language
+         */
+        setReservedWordsLowerCase(
+                Arrays.asList(
+                        "break", "case", "class", "catch", "const", "continue", "debugger",
+                        "default", "delete", "do", "else", "export", "extends", "finally",
+                        "for", "function", "if", "import", "in", "instanceof", "let", "new",
+                        "return", "super", "switch", "this", "throw", "try", "typeof", "var",
+                        "void", "while", "with", "yield")
+        );
+
+        /*
+         * Additional Properties.  These values can be passed to the templates and
+         * are available in models, apis, and supporting files
+         */
+        additionalProperties.put("apiVersion", apiVersion);
+        additionalProperties.put("implFolder", implFolder);
+
+        supportingFiles.add(new SupportingFile("writer.mustache", ("utils").replace(".", File.separator), "writer.js"));
+
+        cliOptions.add(new CliOption(EXPORTED_NAME,
+                "When the generated code will be deployed to Google Cloud Functions, this option can be "
+                        + "used to update the name of the exported function. By default, it refers to the "
+                        + "basePath. This does not affect normal standalone nodejs server code."));
+        cliOptions.add(new CliOption(SERVER_PORT,
+                "TCP port to listen on."));
+    }
+
+    @Override
+    public String apiPackage() {
+        return "controllers";
+    }
+
+    /**
+     * Configures the type of generator.
+     *
+     * @return the CodegenType for this generator
+     * @see org.openapitools.codegen.CodegenType
+     */
+    @Override
+    public CodegenType getTag() {
+        return CodegenType.SERVER;
+    }
+
+    /**
+     * Configures a friendly name for the generator.  This will be used by the generator
+     * to select the library with the -g flag.
+     *
+     * @return the friendly name for the generator
+     */
+    @Override
+    public String getName() {
+        return "nodejs-express-server";
+    }
+
+    /**
+     * Returns human-friendly help for the generator.  Provide the consumer with help
+     * tips, parameters here
+     *
+     * @return A string value for the help message
+     */
+    @Override
+    public String getHelp() {
+        return "Generates a NodeJS Express server (beta).";
+    }
+
+    @Override
+    public String toApiName(String name) {
+        if (name.length() == 0) {
+            return "DefaultController";
+        }
+        return camelize(name);
+    }
+
+    @Override
+    public String toApiFilename(String name) {
+        return toApiName(name);
+    }
+
+
+    @Override
+    public String apiFilename(String templateName, String tag) {
+        String result = super.apiFilename(templateName, tag);
+
+        if (templateName.equals("service.mustache")) {
+            String stringToMatch = File.separator + "controllers" + File.separator;
+            String replacement = File.separator + implFolder + File.separator;
+            result = result.replaceAll(Pattern.quote(stringToMatch), replacement);
+        }
+        return result;
+    }
+
+    private String implFileFolder(String output) {
+        return outputFolder + File.separator + output + File.separator + apiPackage().replace('.', File.separatorChar);
+    }
+
+    /**
+     * Escapes a reserved word as defined in the `reservedWords` array. Handle escaping
+     * those terms here.  This logic is only called if a variable matches the reserved words
+     *
+     * @return the escaped term
+     */
+    @Override
+    public String escapeReservedWord(String name) {
+        if (this.reservedWordsMappings().containsKey(name)) {
+            return this.reservedWordsMappings().get(name);
+        }
+        return "_" + name;
+    }
+
+    /**
+     * Location to write api files.  You can use the apiPackage() as defined when the class is
+     * instantiated
+     */
+    @Override
+    public String apiFileFolder() {
+        return outputFolder + File.separator + apiPackage().replace('.', File.separatorChar);
+    }
+
+    public boolean getGoogleCloudFunctions() {
+        return googleCloudFunctions;
+    }
+
+    public void setGoogleCloudFunctions(boolean value) {
+        googleCloudFunctions = value;
+    }
+
+    public String getExportedName() {
+        return exportedName;
+    }
+
+    public void setExportedName(String name) {
+        exportedName = name;
+    }
+
+    @Override
+    public Map<String, Object> postProcessOperationsWithModels(Map<String, Object> objs, List<Object> allModels) {
+        @SuppressWarnings("unchecked")
+        Map<String, Object> objectMap = (Map<String, Object>) objs.get("operations");
+        @SuppressWarnings("unchecked")
+        List<CodegenOperation> operations = (List<CodegenOperation>) objectMap.get("operation");
+        for (CodegenOperation operation : operations) {
+            operation.httpMethod = operation.httpMethod.toLowerCase(Locale.ROOT);
+
+            List<CodegenParameter> params = operation.allParams;
+            if (params != null && params.size() == 0) {
+                operation.allParams = null;
+            }
+            List<CodegenResponse> responses = operation.responses;
+            if (responses != null) {
+                for (CodegenResponse resp : responses) {
+                    if ("0".equals(resp.code)) {
+                        resp.code = "default";
+                    }
+                }
+            }
+            if (operation.examples != null && !operation.examples.isEmpty()) {
+                // Leave application/json* items only
+                for (Iterator<Map<String, String>> it = operation.examples.iterator(); it.hasNext(); ) {
+                    final Map<String, String> example = it.next();
+                    final String contentType = example.get("contentType");
+                    if (contentType == null || !contentType.startsWith("application/json")) {
+                        it.remove();
+                    }
+                }
+            }
+        }
+        return objs;
+    }
+
+    @SuppressWarnings("unchecked")
+    private static List<Map<String, Object>> getOperations(Map<String, Object> objs) {
+        List<Map<String, Object>> result = new ArrayList<Map<String, Object>>();
+        Map<String, Object> apiInfo = (Map<String, Object>) objs.get("apiInfo");
+        List<Map<String, Object>> apis = (List<Map<String, Object>>) apiInfo.get("apis");
+        for (Map<String, Object> api : apis) {
+            result.add((Map<String, Object>) api.get("operations"));
+        }
+        return result;
+    }
+
+    private static List<Map<String, Object>> sortOperationsByPath(List<CodegenOperation> ops) {
+        Multimap<String, CodegenOperation> opsByPath = ArrayListMultimap.create();
+
+        for (CodegenOperation op : ops) {
+            opsByPath.put(op.path, op);
+        }
+
+        List<Map<String, Object>> opsByPathList = new ArrayList<Map<String, Object>>();
+        for (Entry<String, Collection<CodegenOperation>> entry : opsByPath.asMap().entrySet()) {
+            Map<String, Object> opsByPathEntry = new HashMap<String, Object>();
+            opsByPathList.add(opsByPathEntry);
+            opsByPathEntry.put("path", entry.getKey());
+            opsByPathEntry.put("operation", entry.getValue());
+            List<CodegenOperation> operationsForThisPath = Lists.newArrayList(entry.getValue());
+            operationsForThisPath.get(operationsForThisPath.size() - 1).hasMore = false;
+            if (opsByPathList.size() < opsByPath.asMap().size()) {
+                opsByPathEntry.put("hasMore", "true");
+            }
+        }
+
+        return opsByPathList;
+    }
+
+    @Override
+    public void processOpts() {
+        super.processOpts();
+
+        if (additionalProperties.containsKey(EXPORTED_NAME)) {
+            setExportedName((String) additionalProperties.get(EXPORTED_NAME));
+        }
+
+        /*
+         * Supporting Files.  You can write single files for the generator with the
+         * entire object tree available.  If the input file has a suffix of `.mustache
+         * it will be processed by the template engine.  Otherwise, it will be copied
+         */
+        // supportingFiles.add(new SupportingFile("controller.mustache",
+        //   "controllers",
+        //   "controller.js")
+        // );
+        supportingFiles.add(new SupportingFile("openapi.mustache",
+                "api",
+                "openapi.yaml")
+        );
+        if (getGoogleCloudFunctions()) {
+            writeOptional(outputFolder, new SupportingFile("index-gcf.mustache", "", "index.js"));
+        } else {
+            writeOptional(outputFolder, new SupportingFile("index.mustache", "", "index.js"));
+        }
+        writeOptional(outputFolder, new SupportingFile("package.mustache", "", "package.json"));
+        writeOptional(outputFolder, new SupportingFile("README.mustache", "", "README.md"));
+        if (GeneratorProperties.getProperty("noservice") == null) {
+            apiTemplateFiles.put(
+                    "service.mustache",   // the template to use
+                    "Service.js");       // the extension for each file to write
+        }
+    }
+
+    @Override
+    public void preprocessOpenAPI(OpenAPI openAPI) {
+        URL url = URLPathUtils.getServerURL(openAPI);
+        String host =  URLPathUtils.getProtocolAndHost(url);
+        String port = URLPathUtils.getPort(url, defaultServerPort) ;
+        String basePath = url.getPath();
+
+        if (additionalProperties.containsKey(SERVER_PORT)) {
+            port = additionalProperties.get(SERVER_PORT).toString();
+        }
+        this.additionalProperties.put(SERVER_PORT, port);
+
+        if (openAPI.getInfo() != null) {
+            Info info = openAPI.getInfo();
+            if (info.getTitle() != null) {
+                // when info.title is defined, use it for projectName
+                // used in package.json
+                projectName = info.getTitle()
+                        .replaceAll("[^a-zA-Z0-9]", "-")
+                        .replaceAll("^[-]*", "")
+                        .replaceAll("[-]*$", "")
+                        .replaceAll("[-]{2,}", "-")
+                        .toLowerCase(Locale.ROOT);
+                this.additionalProperties.put("projectName", projectName);
+            }
+        }
+
+        if (getGoogleCloudFunctions()) {
+            // Note that Cloud Functions don't allow customizing port name, simply checking host
+            // is good enough.
+            if (!host.endsWith(".cloudfunctions.net")) {
+                LOGGER.warn("Host " + host + " seems not matching with cloudfunctions.net URL.");
+            }
+            if (!additionalProperties.containsKey(EXPORTED_NAME)) {
+                if (basePath == null || basePath.equals("/")) {
+                    LOGGER.warn("Cannot find the exported name properly. Using 'openapi' as the exported name");
+                    basePath = "/openapi";
+                }
+                additionalProperties.put(EXPORTED_NAME, basePath.substring(1));
+            }
+        }
+
+        // need vendor extensions for x-swagger-router-controller
+        Paths paths = openAPI.getPaths();
+        if (paths != null) {
+            for (String pathname : paths.keySet()) {
+                PathItem path = paths.get(pathname);
+                Map<HttpMethod, Operation> operationMap = path.readOperationsMap();
+                if (operationMap != null) {
+                    for (HttpMethod method : operationMap.keySet()) {
+                        Operation operation = operationMap.get(method);
+                        String tag = "default";
+                        if (operation.getTags() != null && operation.getTags().size() > 0) {
+                            tag = toApiName(operation.getTags().get(0));
+                        }
+                        if (operation.getOperationId() == null) {
+                            operation.setOperationId(getOrGenerateOperationId(operation, pathname, method.toString()));
+                        }
+                        if (operation.getExtensions() == null ||
+                                operation.getExtensions().get("x-swagger-router-controller") == null) {
+                            operation.addExtension("x-swagger-router-controller", sanitizeTag(tag));
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    @Override
+    public Map<String, Object> postProcessSupportingFileData(Map<String, Object> objs) {
+        generateYAMLSpecFile(objs);
+
+        for (Map<String, Object> operations : getOperations(objs)) {
+            @SuppressWarnings("unchecked")
+            List<CodegenOperation> ops = (List<CodegenOperation>) operations.get("operation");
+
+            List<Map<String, Object>> opsByPathList = sortOperationsByPath(ops);
+            operations.put("operationsByPath", opsByPathList);
+        }
+        return super.postProcessSupportingFileData(objs);
+    }
+
+    @Override
+    public String removeNonNameElementToCamelCase(String name) {
+        return removeNonNameElementToCamelCase(name, "[-:;#]");
+    }
+
+    @Override
+    public String escapeUnsafeCharacters(String input) {
+        return input.replace("*/", "*_/").replace("/*", "/_*");
+    }
+
+    @Override
+    public String escapeQuotationMark(String input) {
+        // remove " to avoid code injection
+        return input.replace("\"", "");
+    }
+}
---
+++ samples/server/petstore/nodejs-express-server/controllers/User.js
@@ -0,0 +1,93 @@
+'use strict';
+
+var utils = require('../utils/writer.js');
+var User = require('../service/UserService');
+
+module.exports.createUser = function createUser (req, res, next) {
+  var body = req.swagger.params['body'].value;
+  User.createUser(body)
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
+
+module.exports.createUsersWithArrayInput = function createUsersWithArrayInput (req, res, next) {
+  var body = req.swagger.params['body'].value;
+  User.createUsersWithArrayInput(body)
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
+
+module.exports.createUsersWithListInput = function createUsersWithListInput (req, res, next) {
+  var body = req.swagger.params['body'].value;
+  User.createUsersWithListInput(body)
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
+
+module.exports.deleteUser = function deleteUser (req, res, next) {
+  var username = req.swagger.params['username'].value;
+  User.deleteUser(username)
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
+
+module.exports.getUserByName = function getUserByName (req, res, next) {
+  var username = req.swagger.params['username'].value;
+  User.getUserByName(username)
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
+
+module.exports.loginUser = function loginUser (req, res, next) {
+  var username = req.swagger.params['username'].value;
+  var password = req.swagger.params['password'].value;
+  User.loginUser(username,password)
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
+
+module.exports.logoutUser = function logoutUser (req, res, next) {
+  User.logoutUser()
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
+
+module.exports.updateUser = function updateUser (req, res, next) {
+  var username = req.swagger.params['username'].value;
+  var body = req.swagger.params['body'].value;
+  User.updateUser(username,body)
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
