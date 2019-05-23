e5179b097700c7cdb1f9eb455c4dad5207ee017a
Johan Brandhorst <johan.brandhorst@gmail.com>
2019-05-23 12:29:11 +0100

    [Go][Client] Ensure test errors don't crash
    Previously we did not interrupt execution of a test case
    when an error was returned from an API call. This was
    causing the tests to crash as soon as we try to
    dereference the response.
    
    We now fail the tests as soon as the first API error
    is received.

--- samples/client/petstore/go/pet_api_test.go
+++ samples/client/petstore/go/pet_api_test.go
@@ -32,8 +32,7 @@ func TestAddPet(t *testing.T) {
 	r, err := client.PetApi.AddPet(context.Background(), newPet)
 
 	if err != nil {
-		t.Errorf("Error while adding pet")
-		t.Log(err)
+		t.Fatalf("Error while adding pet: %v", err)
 	}
 	if r.StatusCode != 200 {
 		t.Log(r)
@@ -44,8 +43,7 @@ func TestFindPetsByStatusWithMissingParam(t *testing.T) {
 	_, r, err := client.PetApi.FindPetsByStatus(context.Background(), nil)
 
 	if err != nil {
-		t.Errorf("Error while testing TestFindPetsByStatusWithMissingParam")
-		t.Log(err)
+		t.Fatalf("Error while testing TestFindPetsByStatusWithMissingParam: %v", err)
 	}
 	if r.StatusCode != 200 {
 		t.Log(r)
@@ -66,8 +64,7 @@ func TestGetPetByIdWithInvalidID(t *testing.T) {
 
 		a.Contains(assertedError.Error(), "Not Found")
 	} else if err != nil {
-		t.Errorf("Error while getting pet by invalid id")
-		t.Log(err)
+		t.Fatalf("Error while getting pet by invalid id: %v", err)
 		t.Log(r)
 	} else {
 		t.Log(resp)
@@ -80,8 +77,7 @@ func TestUpdatePetWithForm(t *testing.T) {
 		Status: optional.NewString("available"),
 	})
 	if err != nil {
-		t.Errorf("Error while updating pet by id")
-		t.Log(err)
+		t.Fatalf("Error while updating pet by id: %v", err)
 		t.Log(r)
 	}
 	if r.StatusCode != 200 {
@@ -96,8 +92,7 @@ func TestFindPetsByTag(t *testing.T) {
 	var found = false
 	resp, r, err := client.PetApi.FindPetsByTags(context.Background(), []string{"tag2"})
 	if err != nil {
-		t.Errorf("Error while getting pet by tag")
-		t.Log(err)
+		t.Fatalf("Error while getting pet by tag: %v", err)
 		t.Log(r)
 	} else {
 		if len(resp) == 0 {
@@ -126,8 +121,7 @@ func TestFindPetsByTag(t *testing.T) {
 func TestFindPetsByStatus(t *testing.T) {
 	resp, r, err := client.PetApi.FindPetsByStatus(context.Background(), []string{"available"})
 	if err != nil {
-		t.Errorf("Error while getting pet by id")
-		t.Log(err)
+		t.Fatalf("Error while getting pet by id: %v", err)
 		t.Log(r)
 	} else {
 		if len(resp) == 0 {
@@ -154,8 +148,7 @@ func TestUploadFile(t *testing.T) {
 	})
 
 	if err != nil {
-		t.Errorf("Error while uploading file")
-		t.Log(err)
+		t.Fatalf("Error while uploading file: %v", err)
 	}
 
 	if r.StatusCode != 200 {
@@ -174,8 +167,7 @@ func TestUploadFileRequired(t *testing.T) {
 		})
 
 	if err != nil {
-		t.Errorf("Error while uploading file")
-		t.Log(err)
+		t.Fatalf("Error while uploading file: %v", err)
 	}
 
 	if r.StatusCode != 200 {
@@ -187,8 +179,7 @@ func TestDeletePet(t *testing.T) {
 	r, err := client.PetApi.DeletePet(context.Background(), 12830, nil)
 
 	if err != nil {
-		t.Errorf("Error while deleting pet by id")
-		t.Log(err)
+		t.Fatalf("Error while deleting pet by id: %v", err)
 	}
 	if r.StatusCode != 200 {
 		t.Log(r)
@@ -269,8 +260,7 @@ func waitOnFunctions(t *testing.T, errc chan error, n int) {
 	for i := 0; i < n; i++ {
 		err := <-errc
 		if err != nil {
-			t.Errorf("Error performing concurrent test")
-			t.Log(err)
+			t.Fatalf("Error performing concurrent test: %v", err)
 		}
 	}
 }
@@ -279,8 +269,7 @@ func deletePet(t *testing.T, id int64) {
 	r, err := client.PetApi.DeletePet(context.Background(), id, nil)
 
 	if err != nil {
-		t.Errorf("Error while deleting pet by id")
-		t.Log(err)
+		t.Fatalf("Error while deleting pet by id: %v", err)
 	}
 	if r.StatusCode != 200 {
 		t.Log(r)
@@ -291,8 +280,7 @@ func isPetCorrect(t *testing.T, id int64, name string, status string) {
 	assert := assert.New(t)
 	resp, r, err := client.PetApi.GetPetById(context.Background(), id)
 	if err != nil {
-		t.Errorf("Error while getting pet by id")
-		t.Log(err)
+		t.Fatalf("Error while getting pet by id: %v", err)
 	} else {
 		assert.Equal(resp.Id, int64(id), "Pet id should be equal")
 		assert.Equal(resp.Name, name, fmt.Sprintf("Pet name should be %s", name))
