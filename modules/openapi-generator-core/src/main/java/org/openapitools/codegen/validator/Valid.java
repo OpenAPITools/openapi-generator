9439e4187a1e39ae070dadc9075a425a1ece4ec5
Jim Schubert <james.schubert@gmail.com>
2019-05-29 08:55:40 -0400

    Initial implementation of a validation framework in core
    Print summary of failures in maven-surefire-plugin, use project.parent.version for generator->core

---
+++ modules/openapi-generator-core/src/main/java/org/openapitools/codegen/validator/Valid.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2019 OpenAPI-Generator Contributors (https://openapi-generator.tech)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.openapitools.codegen.validator;
+
+/**
+ * Represents a {@link Validated} state which is "valid" according to the defined rule.
+ */
+public final class Valid extends Validated {
+    private ValidationRule rule;
+
+    /**
+     * Constructs a new {@link Valid} instance.
+     *
+     * @param rule The rule which was evaluated and resulted in this state.
+     */
+    Valid(ValidationRule rule) {
+        this.rule = rule;
+    }
+
+    @Override
+    public String getMessage() {
+        return null;
+    }
+
+    @Override
+    public ValidationRule getRule() {
+        return rule;
+    }
+}
