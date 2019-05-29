f5c1ed200b40050be7997864a32993e34e12ae9c
Jan Buchar <Teyras@gmail.com>
2019-05-29 12:50:29 +0200

    Update samples/client/petstore/typescript-fetch/tests/default/test/PetApi.ts
    Co-Authored-By: Esteban Marin <estebanmarin@gmx.ch>

--- samples/client/petstore/typescript-fetch/tests/default/test/PetApi.ts
+++ samples/client/petstore/typescript-fetch/tests/default/test/PetApi.ts
@@ -29,7 +29,7 @@ describe('PetApi', () => {
             it('should update Pet by ID', () => {
                 return api.getPetById({ petId: fixture.id }).then((result: Pet) => {
                     result.name = 'newname';
-                    return api.updatePet({ pet: result }).then(() => {
+                    return api.updatePet({ body: result }).then(() => {
                         return api.getPetById({ petId: fixture.id }).then((result: Pet) => {
                             return expect(result.name).to.deep.equal('newname');
                         });
