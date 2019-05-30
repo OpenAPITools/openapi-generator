aaf63bf9de854e5405781286d4b817bd92486108
wooyme <867653608@qq.com>
2019-05-30 14:27:27 +0800

    update

--- modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/KotlinVertxServerCodegen.java
+++ modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/KotlinVertxServerCodegen.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2018 OpenAPI-Generator Contributors (https://openapi-generator.tech)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.openapitools.codegen.languages;
 
 import io.swagger.v3.oas.models.OpenAPI;
@@ -60,7 +76,7 @@ public class KotlinVertxServerCodegen extends AbstractJavaCodegen {
 
         apiTestTemplateFiles.clear();
         modelDocTemplateFiles.clear();
-        apiDocTemplateFiles.clear();
+//        apiDocTemplateFiles.clear();
         supportingFiles.clear();
 
         apiTemplateFiles.clear();
@@ -75,7 +91,6 @@ public class KotlinVertxServerCodegen extends AbstractJavaCodegen {
         modelPackage = rootPackage + ".model";
         artifactId = "openapi-java-vertx-server";
         artifactVersion = apiVersion;
-//        supportingFiles.add(new SupportingFile("README.mustache", "", "README.md"));
 
         updateOption(CodegenConstants.ARTIFACT_ID, this.getArtifactId());
         updateOption(CodegenConstants.ARTIFACT_VERSION, this.getArtifactVersion());
@@ -85,6 +100,8 @@ public class KotlinVertxServerCodegen extends AbstractJavaCodegen {
         additionalProperties.put(ROOT_PACKAGE, rootPackage);
 
         supportingFiles.add(new SupportingFile("pom.mustache", "", "pom.xml"));
+        supportingFiles.add(new SupportingFile("README.mustache", "", "README.md"));
+
     }
 
     @Override
