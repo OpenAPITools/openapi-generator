53df04bc88573c3ab259ce0e3db05ec43b3454dc
Vincent Devos <46601673+karismann@users.noreply.github.com>
2019-06-03 09:59:20 +0200

    fix indent

--- samples/server/petstore/kotlin-springboot/src/main/kotlin/org/openapitools/api/PetApiService.kt
+++ samples/server/petstore/kotlin-springboot/src/main/kotlin/org/openapitools/api/PetApiService.kt
@@ -6,17 +6,17 @@ interface PetApiService {
 
 	fun addPet(body: Pet): Unit
 
-    fun deletePet(petId: kotlin.Long, apiKey: kotlin.String?): Unit
+	fun deletePet(petId: kotlin.Long, apiKey: kotlin.String?): Unit
 
-    fun findPetsByStatus(status: kotlin.collections.List<kotlin.String>): List<Pet>
+	fun findPetsByStatus(status: kotlin.collections.List<kotlin.String>): List<Pet>
 
-    fun findPetsByTags(tags: kotlin.collections.List<kotlin.String>): List<Pet>
+	fun findPetsByTags(tags: kotlin.collections.List<kotlin.String>): List<Pet>
 
-    fun getPetById(petId: kotlin.Long): Pet
+	fun getPetById(petId: kotlin.Long): Pet
 
 	fun updatePet(body: Pet): Unit
 
-    fun updatePetWithForm(petId: kotlin.Long, name: kotlin.String?, status: kotlin.String?): Unit
+	fun updatePetWithForm(petId: kotlin.Long, name: kotlin.String?, status: kotlin.String?): Unit
 
-    fun uploadFile(petId: kotlin.Long, additionalMetadata: kotlin.String?, file: org.springframework.core.io.Resource?): ModelApiResponse
+	fun uploadFile(petId: kotlin.Long, additionalMetadata: kotlin.String?, file: org.springframework.core.io.Resource?): ModelApiResponse
 }
