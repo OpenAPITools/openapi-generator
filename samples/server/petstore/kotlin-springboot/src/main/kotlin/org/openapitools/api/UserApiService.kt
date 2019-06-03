038b025e3abc447a7e823c3f07e37f4f02e9f9af
Vincent Devos <46601673+karismann@users.noreply.github.com>
2019-06-03 10:00:45 +0200

    fix indent

--- samples/server/petstore/kotlin-springboot/src/main/kotlin/org/openapitools/api/UserApiService.kt
+++ samples/server/petstore/kotlin-springboot/src/main/kotlin/org/openapitools/api/UserApiService.kt
@@ -5,17 +5,17 @@ interface UserApiService {
 
 	fun createUser(body: User): Unit
 
-    fun createUsersWithArrayInput(body: kotlin.collections.List<User>): Unit
+	fun createUsersWithArrayInput(body: kotlin.collections.List<User>): Unit
 
-    fun createUsersWithListInput(body: kotlin.collections.List<User>): Unit
+	fun createUsersWithListInput(body: kotlin.collections.List<User>): Unit
 
-    fun deleteUser(username: kotlin.String): Unit
+	fun deleteUser(username: kotlin.String): Unit
 
-    fun getUserByName(username: kotlin.String): User
+	fun getUserByName(username: kotlin.String): User
 
-    fun loginUser(username: kotlin.String, password: kotlin.String): kotlin.String
+	fun loginUser(username: kotlin.String, password: kotlin.String): kotlin.String
 
 	fun logoutUser(): Unit
 
-    fun updateUser(username: kotlin.String, body: User): Unit
+	fun updateUser(username: kotlin.String, body: User): Unit
 }
