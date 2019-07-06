78551d01807518a5e809955eaa92f3ac7115c440
Nathan Broadbent <git@ndbroadbent.com>
2019-07-06 21:21:41 +0700

    Set error_reporting(E_ALL) in phpunit.xml settings, to crash on undefined variables (#3266)
    * Set error_reporting(E_ALL) in phpunit.xml settings, so that undefined variables will always cause a test failure
    
    * Update petstore sample for php-laravel

--- samples/server/petstore/php-laravel/lib/app/Models/AdditionalPropertiesClass.php
+++ samples/server/petstore/php-laravel/lib/app/Models/AdditionalPropertiesClass.php
@@ -9,10 +9,37 @@ namespace app\Models;
  */
 class AdditionalPropertiesClass {
 
-    /** @var map[string,string] $mapProperty */
-    private $mapProperty;
+    /** @var map[string,string] $mapString */
+    private $mapString;
 
-    /** @var map[string,map[string,string]] $mapOfMapProperty */
-    private $mapOfMapProperty;
+    /** @var map[string,float] $mapNumber */
+    private $mapNumber;
+
+    /** @var map[string,int] $mapInteger */
+    private $mapInteger;
+
+    /** @var map[string,bool] $mapBoolean */
+    private $mapBoolean;
+
+    /** @var map[string,int[]] $mapArrayInteger */
+    private $mapArrayInteger;
+
+    /** @var map[string,object[]] $mapArrayAnytype */
+    private $mapArrayAnytype;
+
+    /** @var map[string,map[string,string]] $mapMapString */
+    private $mapMapString;
+
+    /** @var map[string,map[string,object]] $mapMapAnytype */
+    private $mapMapAnytype;
+
+    /** @var object $anytype1 */
+    private $anytype1;
+
+    /** @var object $anytype2 */
+    private $anytype2;
+
+    /** @var object $anytype3 */
+    private $anytype3;
 
 }
