06619ac531db4ea0ed135a8db4d7095e767bc29c
Christophe <christophe.gensoul@gmail.com>
2019-07-18 22:12:18 +0200

    Prise en compte du fait que Yojson représente les variants constants sous forme d'un tableau JSON contenant une unique string.

--- samples/client/petstore/ocaml-client/src/support/enums.ml
+++ samples/client/petstore/ocaml-client/src/support/enums.ml
@@ -7,6 +7,13 @@ type status =
 | Delivered [@printer fun fmt _ -> Format.pp_print_string fmt "delivered"] [@name "delivered"]
 [@@deriving yojson, show { with_path = false }];;
 
+let status_of_yojson json = status_of_yojson (`List [json])
+let status_to_yojson e =
+    match status_to_yojson e with
+    | `List [json] -> json
+    | json -> json
+
+
 
 (**  *)
 type pet_status =
@@ -14,3 +21,10 @@ type pet_status =
 | Pending [@printer fun fmt _ -> Format.pp_print_string fmt "pending"] [@name "pending"]
 | Sold [@printer fun fmt _ -> Format.pp_print_string fmt "sold"] [@name "sold"]
 [@@deriving yojson, show { with_path = false }];;
+
+let pet_status_of_yojson json = pet_status_of_yojson (`List [json])
+let pet_status_to_yojson e =
+    match pet_status_to_yojson e with
+    | `List [json] -> json
+    | json -> json
+
