4053224f33c75522317157e684ace7939e13f2a9
Christophe Gensoul <christophe.gensoul.mre@gmail.com>
2019-07-19 19:03:30 +0200

    More tests.

--- samples/client/petstore/ocaml-client/bin/test.ml
+++ samples/client/petstore/ocaml-client/bin/test.ml
@@ -1,10 +1,19 @@
+open Petstore_client
+open Lwt
+
+let lift f r = Ppx_deriving_yojson_runtime.(>|=) r f
+
 let main () =
-  let open Petstore_client in
-  let open Lwt in
   let p = Pet.create "Bob" [] in
-  Pet_api.add_pet p >>= fun (_r, b) ->
-    b |> Cohttp_lwt.Body.to_string >|= print_endline
-    
+  Pet_api.add_pet p >|= Yojson.Safe.to_string >|= print_endline
+
+let find_pet () =
+  let pet_id = 9199424981609334637L in
+  Pet_api.get_pet_by_id pet_id >|= lift Pet.show >|= lift print_endline
+
+let find_pets_by_tags () =
+  let tags = ["dog"; "cat"] in
+  Pet_api.find_pets_by_tags tags >|= List.map (lift Pet.show) >|= List.map (lift print_endline)
+
 let _ =
-  print_endline "coucou";
-  Lwt_main.run (main ())
+  Lwt_main.run (find_pets_by_tags ())
