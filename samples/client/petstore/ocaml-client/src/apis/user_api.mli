aa978f61b9dbc7a337570d46b8ac4ccbb5f5bf01
Christophe Gensoul <christophe.gensoul.mre@gmail.com>
2019-07-23 18:09:26 +0200

    Make apis calls without a return type return unit rather than Yojson.Safe.t.

--- samples/client/petstore/ocaml-client/src/apis/user_api.mli
+++ samples/client/petstore/ocaml-client/src/apis/user_api.mli
@@ -5,11 +5,11 @@
  *
  *)
 
-val create_user : User.t -> Yojson.Safe.t Lwt.t
-val create_users_with_array_input : User.t list -> Yojson.Safe.t Lwt.t
-val create_users_with_list_input : User.t list -> Yojson.Safe.t Lwt.t
-val delete_user : string -> Yojson.Safe.t Lwt.t
+val create_user : User.t -> unit Lwt.t
+val create_users_with_array_input : User.t list -> unit Lwt.t
+val create_users_with_list_input : User.t list -> unit Lwt.t
+val delete_user : string -> unit Lwt.t
 val get_user_by_name : string -> User.t Lwt.t
 val login_user : string -> string -> string Lwt.t
-val logout_user : unit -> Yojson.Safe.t Lwt.t
-val update_user : string -> User.t -> Yojson.Safe.t Lwt.t
+val logout_user : unit -> unit Lwt.t
+val update_user : string -> User.t -> unit Lwt.t
