391de4ed22b96a6942633796a6e785ae9a7ccfa9
Christophe Gensoul <christophe.gensoul.mre@gmail.com>
2019-07-24 18:33:52 +0200

    Deleted commented code.

--- bin/ocaml-client-petstore.sh
+++ bin/ocaml-client-petstore.sh
@@ -29,7 +29,6 @@ fi
 export JAVA_OPTS="${JAVA_OPTS} -Xmx1024M -DdebugOperations -DloggerPath=conf/log4j.properties"
 
 args="generate -t modules/openapi-generator/src/main/resources/ocaml-client -i modules/openapi-generator/src/test/resources/2_0/petstore.yaml -g ocaml-client -o samples/client/petstore/ocaml-client --additional-properties packageName=petstore_client $@"
-#args="debugModels -t modules/openapi-generator/src/main/resources/ocaml-client -i modules/openapi-generator/src/test/resources/2_0/petstore.yaml -g ocaml-client -o samples/client/petstore/ocaml-client --additional-properties packageName=petstore_client $@"
 
 echo "java ${JAVA_OPTS} -jar ${executable} ${args}"
 java $JAVA_OPTS -jar $executable $args
