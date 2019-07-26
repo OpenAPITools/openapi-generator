87303b1536b083ffef0a131085d191251497defb
Steven Masala <me@smasala.com>
2019-07-26 12:36:36 +0200

    format

--- modules/openapi-generator/src/test/java/org/openapitools/codegen/typescript/typescriptnode/TypeScriptNodeClientCodegenTest.java
+++ modules/openapi-generator/src/test/java/org/openapitools/codegen/typescript/typescriptnode/TypeScriptNodeClientCodegenTest.java
@@ -10,62 +10,62 @@ public class TypeScriptNodeClientCodegenTest {
 
     @Test
     public void convertVarName() throws Exception {
-       TypeScriptNodeClientCodegen codegen = new TypeScriptNodeClientCodegen();
-       Assert.assertEquals(codegen.toVarName("name"), "name");
-       Assert.assertEquals(codegen.toVarName("$name"), "$name");
-       Assert.assertEquals(codegen.toVarName("nam$$e"), "nam$$e");
-       Assert.assertEquals(codegen.toVarName("user-name"), "userName");
-       Assert.assertEquals(codegen.toVarName("user_name"), "userName");
-       Assert.assertEquals(codegen.toVarName("user|name"), "userName");
-       Assert.assertEquals(codegen.toVarName("user !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~name"), "user$Name");
-   }
+        TypeScriptNodeClientCodegen codegen = new TypeScriptNodeClientCodegen();
+        Assert.assertEquals(codegen.toVarName("name"), "name");
+        Assert.assertEquals(codegen.toVarName("$name"), "$name");
+        Assert.assertEquals(codegen.toVarName("nam$$e"), "nam$$e");
+        Assert.assertEquals(codegen.toVarName("user-name"), "userName");
+        Assert.assertEquals(codegen.toVarName("user_name"), "userName");
+        Assert.assertEquals(codegen.toVarName("user|name"), "userName");
+        Assert.assertEquals(codegen.toVarName("user !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~name"), "user$Name");
+    }
 
-   @Test
-   public void testSnapshotVersion() {
-      OpenAPI api = TestUtils.createOpenAPI();
-      TypeScriptNodeClientCodegen codegen = new TypeScriptNodeClientCodegen();
+    @Test
+    public void testSnapshotVersion() {
+        OpenAPI api = TestUtils.createOpenAPI();
+        TypeScriptNodeClientCodegen codegen = new TypeScriptNodeClientCodegen();
 
-      codegen.additionalProperties().put("npmName", "@openapi/typescript-angular-petstore");
-      codegen.additionalProperties().put("snapshot", true);
-      codegen.additionalProperties().put("npmVersion", "1.0.0-SNAPSHOT");
-      codegen.processOpts();
-      codegen.preprocessOpenAPI(api);
+        codegen.additionalProperties().put("npmName", "@openapi/typescript-angular-petstore");
+        codegen.additionalProperties().put("snapshot", true);
+        codegen.additionalProperties().put("npmVersion", "1.0.0-SNAPSHOT");
+        codegen.processOpts();
+        codegen.preprocessOpenAPI(api);
 
-      Assert.assertTrue(codegen.getNpmVersion().matches("^1.0.0-SNAPSHOT.[0-9]{12}$"));
+        Assert.assertTrue(codegen.getNpmVersion().matches("^1.0.0-SNAPSHOT.[0-9]{12}$"));
 
-      codegen = new TypeScriptNodeClientCodegen();
-      codegen.additionalProperties().put("npmName", "@openapi/typescript-angular-petstore");
-      codegen.additionalProperties().put("snapshot", true);
-      codegen.additionalProperties().put("npmVersion", "3.0.0-M1");
-      codegen.processOpts();
-      codegen.preprocessOpenAPI(api);
+        codegen = new TypeScriptNodeClientCodegen();
+        codegen.additionalProperties().put("npmName", "@openapi/typescript-angular-petstore");
+        codegen.additionalProperties().put("snapshot", true);
+        codegen.additionalProperties().put("npmVersion", "3.0.0-M1");
+        codegen.processOpts();
+        codegen.preprocessOpenAPI(api);
 
-      Assert.assertTrue(codegen.getNpmVersion().matches("^3.0.0-M1-SNAPSHOT.[0-9]{12}$"));
+        Assert.assertTrue(codegen.getNpmVersion().matches("^3.0.0-M1-SNAPSHOT.[0-9]{12}$"));
 
-   }
+    }
 
-   @Test
-   public void testWithoutSnapshotVersion() {
-      OpenAPI api = TestUtils.createOpenAPI();
-      TypeScriptNodeClientCodegen codegen = new TypeScriptNodeClientCodegen();
+    @Test
+    public void testWithoutSnapshotVersion() {
+        OpenAPI api = TestUtils.createOpenAPI();
+        TypeScriptNodeClientCodegen codegen = new TypeScriptNodeClientCodegen();
 
-      codegen.additionalProperties().put("npmName", "@openapi/typescript-angular-petstore");
-      codegen.additionalProperties().put("snapshot", false);
-      codegen.additionalProperties().put("npmVersion", "1.0.0-SNAPSHOT");
-      codegen.processOpts();
-      codegen.preprocessOpenAPI(api);
+        codegen.additionalProperties().put("npmName", "@openapi/typescript-angular-petstore");
+        codegen.additionalProperties().put("snapshot", false);
+        codegen.additionalProperties().put("npmVersion", "1.0.0-SNAPSHOT");
+        codegen.processOpts();
+        codegen.preprocessOpenAPI(api);
 
-      Assert.assertTrue(codegen.getNpmVersion().matches("^1.0.0-SNAPSHOT$"));
+        Assert.assertTrue(codegen.getNpmVersion().matches("^1.0.0-SNAPSHOT$"));
 
-      codegen = new TypeScriptNodeClientCodegen();
-      codegen.additionalProperties().put("npmName", "@openapi/typescript-angular-petstore");
-      codegen.additionalProperties().put("snapshot", false);
-      codegen.additionalProperties().put("npmVersion", "3.0.0-M1");
-      codegen.processOpts();
-      codegen.preprocessOpenAPI(api);
+        codegen = new TypeScriptNodeClientCodegen();
+        codegen.additionalProperties().put("npmName", "@openapi/typescript-angular-petstore");
+        codegen.additionalProperties().put("snapshot", false);
+        codegen.additionalProperties().put("npmVersion", "3.0.0-M1");
+        codegen.processOpts();
+        codegen.preprocessOpenAPI(api);
 
-      Assert.assertTrue(codegen.getNpmVersion().matches("^3.0.0-M1$"));
+        Assert.assertTrue(codegen.getNpmVersion().matches("^3.0.0-M1$"));
 
-   }
+    }
 
 }
