OpenAPITools/openapi-generator

commit 2e81e612fc6a6d7f54381b833869927ae6132b6c

Fix enum types in structs (#3309)
I already contributed proper enum supports a few weeks ago, but somehow missed actually using them when they're used as a property. This PR fixes that.

I also took the liberty of fixing a bunch of unused warnings.

* Make sure we use the enum types in Rust

* Fix unused warnings in Rust and regenerate samples

* Use crate:: import path that works both with Rust 2015 and 2018

* Derive PartialEq and Eq in generated Rust structs and enums

modules/openapi-generator/src/main/resources/rust/model.mustache
@@ -20,14 +20,14 @@ pub enum {{classname}} {
 
 {{!-- for non-enum schemas --}}
 {{^isEnum}}
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, PartialEq, Eq, Serialize, Deserialize)]
 pub struct {{{classname}}} {
 {{#vars}}
     {{#description}}
     /// {{{description}}}
     {{/description}}
     #[serde(rename = "{{{baseName}}}"{{^required}}, skip_serializing_if = "Option::is_none"{{/required}})]
-    pub {{{name}}}: {{#required}}{{#isNullable}}Option<{{/isNullable}}{{/required}}{{^required}}Option<{{/required}}{{{dataType}}}{{#required}}{{#isNullable}}>{{/isNullable}}{{/required}}{{^required}}>{{/required}},
+    pub {{{name}}}: {{#required}}{{#isNullable}}Option<{{/isNullable}}{{/required}}{{^required}}Option<{{/required}}{{#isEnum}}{{{enumName}}}{{/isEnum}}{{^isEnum}}{{{dataType}}}{{/isEnum}}{{#required}}{{#isNullable}}>{{/isNullable}}{{/required}}{{^required}}>{{/required}},
 {{/vars}}
 }
 
@@ -35,7 +35,7 @@ impl {{{classname}}} {
     {{#description}}
     /// {{{description}}}
     {{/description}}
-    pub fn new({{#requiredVars}}{{{name}}}: {{#isNullable}}Option<{{/isNullable}}{{{dataType}}}{{#isNullable}}>{{/isNullable}}{{^-last}}, {{/-last}}{{/requiredVars}}) -> {{{classname}}} {
+    pub fn new({{#requiredVars}}{{{name}}}: {{#isNullable}}Option<{{/isNullable}}{{#isEnum}}{{{enumName}}}{{/isEnum}}{{^isEnum}}{{{dataType}}}{{/isEnum}}{{#isNullable}}>{{/isNullable}}{{^-last}}, {{/-last}}{{/requiredVars}}) -> {{{classname}}} {
         {{{classname}}} {
             {{#vars}}
             {{{name}}}: {{#required}}{{{name}}}{{/required}}{{^required}}{{#isListContainer}}None{{/isListContainer}}{{#isMapContainer}}None{{/isMapContainer}}{{^isContainer}}None{{/isContainer}}{{/required}},
@@ -49,7 +49,7 @@ impl {{{classname}}} {
 {{#vars}}
 {{#isEnum}}
 /// {{{description}}}
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, PartialEq, Eq, Serialize, Deserialize)]
 pub enum {{enumName}} {
 {{#allowableValues}}
 {{#enumVars}}

samples/client/petstore/rust-reqwest/src/models/api_response.rs
@@ -11,7 +11,7 @@
 /// ApiResponse : Describes the result of uploading an image resource
 
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, PartialEq, Eq, Serialize, Deserialize)]
 pub struct ApiResponse {
     #[serde(rename = "code", skip_serializing_if = "Option::is_none")]
     pub code: Option<i32>,

samples/client/petstore/rust-reqwest/src/models/category.rs
@@ -11,7 +11,7 @@
 /// Category : A category for a pet
 
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, PartialEq, Eq, Serialize, Deserialize)]
 pub struct Category {
     #[serde(rename = "id", skip_serializing_if = "Option::is_none")]
     pub id: Option<i64>,

samples/client/petstore/rust-reqwest/src/models/order.rs
@@ -11,7 +11,7 @@
 /// Order : An order for a pets from the pet store
 
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, PartialEq, Eq, Serialize, Deserialize)]
 pub struct Order {
     #[serde(rename = "id", skip_serializing_if = "Option::is_none")]
     pub id: Option<i64>,
@@ -23,7 +23,7 @@ pub struct Order {
     pub ship_date: Option<String>,
     /// Order Status
     #[serde(rename = "status", skip_serializing_if = "Option::is_none")]
-    pub status: Option<String>,
+    pub status: Option<Status>,
     #[serde(rename = "complete", skip_serializing_if = "Option::is_none")]
     pub complete: Option<bool>,
 }
@@ -43,7 +43,7 @@ impl Order {
 }
 
 /// Order Status
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, PartialEq, Eq, Serialize, Deserialize)]
 pub enum Status {
     #[serde(rename = "placed")]
     Placed,

samples/client/petstore/rust-reqwest/src/models/pet.rs
@@ -11,7 +11,7 @@
 /// Pet : A pet for sale in the pet store
 
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, PartialEq, Eq, Serialize, Deserialize)]
 pub struct Pet {
     #[serde(rename = "id", skip_serializing_if = "Option::is_none")]
     pub id: Option<i64>,
@@ -25,7 +25,7 @@ pub struct Pet {
     pub tags: Option<Vec<crate::models::Tag>>,
     /// pet status in the store
     #[serde(rename = "status", skip_serializing_if = "Option::is_none")]
-    pub status: Option<String>,
+    pub status: Option<Status>,
 }
 
 impl Pet {
@@ -43,7 +43,7 @@ impl Pet {
 }
 
 /// pet status in the store
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, PartialEq, Eq, Serialize, Deserialize)]
 pub enum Status {
     #[serde(rename = "available")]
     Available,

samples/client/petstore/rust-reqwest/src/models/tag.rs
@@ -11,7 +11,7 @@
 /// Tag : A tag for a pet
 
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, PartialEq, Eq, Serialize, Deserialize)]
 pub struct Tag {
     #[serde(rename = "id", skip_serializing_if = "Option::is_none")]
     pub id: Option<i64>,

samples/client/petstore/rust-reqwest/src/models/user.rs
@@ -11,7 +11,7 @@
 /// User : A User who is purchasing from the pet store
 
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, PartialEq, Eq, Serialize, Deserialize)]
 pub struct User {
     #[serde(rename = "id", skip_serializing_if = "Option::is_none")]
     pub id: Option<i64>,

samples/client/petstore/rust/src/models/api_response.rs
@@ -11,7 +11,7 @@
 /// ApiResponse : Describes the result of uploading an image resource
 
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, PartialEq, Eq, Serialize, Deserialize)]
 pub struct ApiResponse {
     #[serde(rename = "code", skip_serializing_if = "Option::is_none")]
     pub code: Option<i32>,

commit c684505cba2a821e4754c19c383dd8dbaacdd09e

Stop deriving Eq for models (#3463)
It fails if the model contains a float, which don't implement Eq.

Fix for bug introduced in #3309.

We really need to improve the testing of the rust generator to catch this sort of mistake in future. I don't have time to do this now, though.

modules/openapi-generator/src/main/resources/rust/model.mustache
@@ -20,7 +20,7 @@ pub enum {{classname}} {
 
 {{!-- for non-enum schemas --}}
 {{^isEnum}}
-#[derive(Debug, PartialEq, Eq, Serialize, Deserialize)]
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
 pub struct {{{classname}}} {
 {{#vars}}
     {{#description}}
@@ -49,7 +49,7 @@ impl {{{classname}}} {
 {{#vars}}
 {{#isEnum}}
 /// {{{description}}}
-#[derive(Debug, PartialEq, Eq, Serialize, Deserialize)]
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
 pub enum {{enumName}} {
 {{#allowableValues}}
 {{#enumVars}}

samples/client/petstore/rust-reqwest/src/models/api_response.rs
@@ -11,7 +11,7 @@
 /// ApiResponse : Describes the result of uploading an image resource
 
 
-#[derive(Debug, PartialEq, Eq, Serialize, Deserialize)]
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
 pub struct ApiResponse {
     #[serde(rename = "code", skip_serializing_if = "Option::is_none")]
     pub code: Option<i32>,

samples/client/petstore/rust-reqwest/src/models/category.rs
@@ -11,7 +11,7 @@
 /// Category : A category for a pet
 
 
-#[derive(Debug, PartialEq, Eq, Serialize, Deserialize)]
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
 pub struct Category {
     #[serde(rename = "id", skip_serializing_if = "Option::is_none")]
     pub id: Option<i64>,

samples/client/petstore/rust-reqwest/src/models/order.rs
@@ -11,7 +11,7 @@
 /// Order : An order for a pets from the pet store
 
 
-#[derive(Debug, PartialEq, Eq, Serialize, Deserialize)]
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
 pub struct Order {
     #[serde(rename = "id", skip_serializing_if = "Option::is_none")]
     pub id: Option<i64>,
@@ -43,7 +43,7 @@ impl Order {
 }
 
 /// Order Status
-#[derive(Debug, PartialEq, Eq, Serialize, Deserialize)]
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
 pub enum Status {
     #[serde(rename = "placed")]
     Placed,

samples/client/petstore/rust-reqwest/src/models/pet.rs
@@ -11,7 +11,7 @@
 /// Pet : A pet for sale in the pet store
 
 
-#[derive(Debug, PartialEq, Eq, Serialize, Deserialize)]
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
 pub struct Pet {
     #[serde(rename = "id", skip_serializing_if = "Option::is_none")]
     pub id: Option<i64>,
@@ -43,7 +43,7 @@ impl Pet {
 }
 
 /// pet status in the store
-#[derive(Debug, PartialEq, Eq, Serialize, Deserialize)]
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
 pub enum Status {
     #[serde(rename = "available")]
     Available,

samples/client/petstore/rust-reqwest/src/models/tag.rs
@@ -11,7 +11,7 @@
 /// Tag : A tag for a pet
 
 
-#[derive(Debug, PartialEq, Eq, Serialize, Deserialize)]
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
 pub struct Tag {
     #[serde(rename = "id", skip_serializing_if = "Option::is_none")]
     pub id: Option<i64>,

samples/client/petstore/rust-reqwest/src/models/user.rs
@@ -11,7 +11,7 @@
 /// User : A User who is purchasing from the pet store
 
 
-#[derive(Debug, PartialEq, Eq, Serialize, Deserialize)]
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
 pub struct User {
     #[serde(rename = "id", skip_serializing_if = "Option::is_none")]
     pub id: Option<i64>,

samples/client/petstore/rust/src/models/api_response.rs
@@ -11,7 +11,7 @@
 /// ApiResponse : Describes the result of uploading an image resource
 
 
-#[derive(Debug, PartialEq, Eq, Serialize, Deserialize)]
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
 pub struct ApiResponse {
     #[serde(rename = "code", skip_serializing_if = "Option::is_none")]
     pub code: Option<i32>,

samples/client/petstore/rust/src/models/category.rs
@@ -11,7 +11,7 @@
 /// Category : A category for a pet
 
 
-#[derive(Debug, PartialEq, Eq, Serialize, Deserialize)]
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
 pub struct Category {
     #[serde(rename = "id", skip_serializing_if = "Option::is_none")]
     pub id: Option<i64>,

samples/client/petstore/rust/src/models/order.rs
@@ -11,7 +11,7 @@
 /// Order : An order for a pets from the pet store
 
 
-#[derive(Debug, PartialEq, Eq, Serialize, Deserialize)]
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
 pub struct Order {
     #[serde(rename = "id", skip_serializing_if = "Option::is_none")]
     pub id: Option<i64>,
@@ -43,7 +43,7 @@ impl Order {
 }
 
 /// Order Status
-#[derive(Debug, PartialEq, Eq, Serialize, Deserialize)]
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
 pub enum Status {
     #[serde(rename = "placed")]
     Placed,

samples/client/petstore/rust/src/models/pet.rs
@@ -11,7 +11,7 @@
 /// Pet : A pet for sale in the pet store
 
 
-#[derive(Debug, PartialEq, Eq, Serialize, Deserialize)]
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
 pub struct Pet {
     #[serde(rename = "id", skip_serializing_if = "Option::is_none")]
     pub id: Option<i64>,
@@ -43,7 +43,7 @@ impl Pet {
 }
 
 /// pet status in the store
-#[derive(Debug, PartialEq, Eq, Serialize, Deserialize)]
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
 pub enum Status {
     #[serde(rename = "available")]
     Available,

samples/client/petstore/rust/src/models/tag.rs
@@ -11,7 +11,7 @@
 /// Tag : A tag for a pet
 
 
-#[derive(Debug, PartialEq, Eq, Serialize, Deserialize)]
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
 pub struct Tag {
     #[serde(rename = "id", skip_serializing_if = "Option::is_none")]
     pub id: Option<i64>,

samples/client/petstore/rust/src/models/user.rs
@@ -11,7 +11,7 @@
 /// User : A User who is purchasing from the pet store
 
 
-#[derive(Debug, PartialEq, Eq, Serialize, Deserialize)]
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
 pub struct User {
     #[serde(rename = "id", skip_serializing_if = "Option::is_none")]
     pub id: Option<i64>,