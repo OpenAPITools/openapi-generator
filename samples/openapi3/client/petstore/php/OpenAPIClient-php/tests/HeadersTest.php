cbb17b2c61c91dcdb116e011d5e7782730d165ef
githubERIK <githubERIK@users.noreply.github.com>
2019-08-01 12:59:18 +0300

    Add sample pet to v3 headers test [ci skip] [skip ci]

--- samples/openapi3/client/petstore/php/OpenAPIClient-php/tests/HeadersTest.php
+++ samples/openapi3/client/petstore/php/OpenAPIClient-php/tests/HeadersTest.php
@@ -3,17 +3,37 @@
 namespace OpenAPI\Client;
 
 use PHPUnit\Framework\TestCase;
+use GuzzleHttp\Psr7\Response;
 
-require_once __DIR__ . '/FakeHttpClient.php';
+require_once __DIR__.'/FakeHttpClient.php';
 
 class HeadersTest extends TestCase
 {
-    /** @var  FakeHttpClient */
+    const SAMPLE_PET = [
+        'id' => 1,
+        'category' => [
+            'id' => 1,
+            'name' => 'any category name',
+        ],
+        'name' => 'any pet name',
+        'photoUrls' => ['example.com'],
+        'tags' => [
+            ['id' => 1,
+            'name' => 'any tag name',
+            ],
+        ],
+        'status' => 'pending',
+    ];
+
+    /** @var FakeHttpClient */
     private $fakeHttpClient;
 
     public function setUp()
     {
         $this->fakeHttpClient = new FakeHttpClient();
+        $this->fakeHttpClient->setResponse(
+            new Response(200, [], json_encode(self::SAMPLE_PET))
+        );
     }
 
     public function testUserAgent()
