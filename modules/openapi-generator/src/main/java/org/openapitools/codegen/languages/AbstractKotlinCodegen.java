fe67428c319a040580ba4c766cc7336a2c7885f8
frecco75 <fabien.recco@gmail.com>
2019-08-15 21:27:38 +0200

    [kotlin-spring] Add default value to RequestHeader

--- modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/AbstractKotlinCodegen.java
+++ modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/AbstractKotlinCodegen.java
@@ -841,7 +841,7 @@ public abstract class AbstractKotlinCodegen extends DefaultCodegen implements Co
             }
         } else if (ModelUtils.isStringSchema(p)) {
             if (p.getDefault() != null) {
-                return "'" + p.getDefault() + "'";
+                return p.getDefault().toString();
             }
         }
 
