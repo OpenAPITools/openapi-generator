1adb0ba5884453e447475643e868466026e4dd4a
Martin Delille <martin@delille.org>
2019-08-19 17:58:03 +0200

    cpp-qt5-client: Improve tests (#3667)

--- samples/client/petstore/cpp-qt5/PetStore/StoreApiTests.h
+++ samples/client/petstore/cpp-qt5/PetStore/StoreApiTests.h
@@ -1,30 +1,16 @@
-#ifndef STOREAPITESTS_H
-#define STOREAPITESTS_H
-#include <QtTest/QtTest>
-#include <QTimer>
+#pragma once
 
 #include "../client/OAIStoreApi.h"
 
 using namespace OpenAPI;
 
 class StoreApiTests: public QObject {
-Q_OBJECT
-public:
-    StoreApiTests();
-    virtual ~StoreApiTests();
+    Q_OBJECT
 
-    static void runTests();
-
-private:
     OAIStoreApi* getApi();
-signals:
-    void quit();
-    bool success();
 
 private slots:
     void placeOrderTest();
     void getOrderByIdTest();
     void getInventoryTest();
 };
-
-#endif // STOREAPITESTS_H
