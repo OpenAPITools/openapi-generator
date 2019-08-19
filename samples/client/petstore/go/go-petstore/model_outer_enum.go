e086ad1b6ea5232ef5063e466bbc40a0f6218a83
Kris Raney <github@junk.kraney.com>
2019-08-18 23:34:26 -0700

    Guarantee a unique name for the constants that represent enum values (#3644)
    * Guarantee a unique name for the constants that represent enum values
    
    * update samples
    
    * Use all-caps

--- samples/client/petstore/go/go-petstore/model_outer_enum.go
+++ samples/client/petstore/go/go-petstore/model_outer_enum.go
@@ -12,7 +12,7 @@ type OuterEnum string
 
 // List of OuterEnum
 const (
-	PLACED OuterEnum = "placed"
-	APPROVED OuterEnum = "approved"
-	DELIVERED OuterEnum = "delivered"
+	OUTERENUM_PLACED OuterEnum = "placed"
+	OUTERENUM_APPROVED OuterEnum = "approved"
+	OUTERENUM_DELIVERED OuterEnum = "delivered"
 )
