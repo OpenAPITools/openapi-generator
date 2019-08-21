d89d4ab79501eed8977784582ed780f199c8aeea
Saber GADOUAR <saber.gadouar@mymoneybank.com>
2019-08-21 17:27:08 +0200

    doc/update copyright for avro generator

--- modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/AvroCodegen.java
+++ modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/AvroCodegen.java
@@ -1,6 +1,5 @@
 /*
- * Copyright 2018 OpenAPI-Generator Contributors (https://openapi-generator.tech)
- * Copyright 2018 SmartBear Software
+ * Copyright 2019 OpenAPI-Generator Contributors (https://openapi-generator.tech)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
