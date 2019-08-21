bb5cd4c42d460b4e3368e01f058821c57ef260c1
William Cheng <wing328hk@gmail.com>
2019-08-21 18:19:38 +0800

    [Dart] test Dart petstore clients in drone.io (#3717)
    * test dart 1x in drone
    
    * update dart image
    
    * trigger build failure
    
    * Revert "trigger build failure"
    
    This reverts commit d565f348ba120d9111b7ad2c6f7e3ad077197a26.
    
    * add dart 2 test
    
    * update build runner version
    
    * test dart2
    
    * only test dart jaguar
    
    * remove pubspec.lock
    
    * better wording
    
    * use pub run test
    
    * revert changes in build_runner
    
    * update test in pom.xml
    
    * update samples
    
    * add dart scripts to ensure up-to-date

--- samples/client/petstore/dart/flutter_petstore/swagger/lib/model/api_response.dart
+++ samples/client/petstore/dart/flutter_petstore/swagger/lib/model/api_response.dart
@@ -34,21 +34,24 @@ class ApiResponse {
   }
 
   Map<String, dynamic> toJson() {
-    return {
-      'code': code,
-      'type': type,
-      'message': message
-    };
+    Map <String, dynamic> json = {};
+    if (code != null)
+      json['code'] = code;
+    if (type != null)
+      json['type'] = type;
+    if (message != null)
+      json['message'] = message;
+    return json;
   }
 
   static List<ApiResponse> listFromJson(List<dynamic> json) {
-    return json == null ? new List<ApiResponse>() : json.map((value) => new ApiResponse.fromJson(value)).toList();
+    return json == null ? List<ApiResponse>() : json.map((value) => ApiResponse.fromJson(value)).toList();
   }
 
   static Map<String, ApiResponse> mapFromJson(Map<String, dynamic> json) {
-    var map = new Map<String, ApiResponse>();
+    var map = Map<String, ApiResponse>();
     if (json != null && json.isNotEmpty) {
-      json.forEach((String key, dynamic value) => map[key] = new ApiResponse.fromJson(value));
+      json.forEach((String key, dynamic value) => map[key] = ApiResponse.fromJson(value));
     }
     return map;
   }
