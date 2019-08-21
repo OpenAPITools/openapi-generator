8b2fa3c1185bcf7cfbd4b3411e42d22fafa5f8e0
Josh Giles <jgiles@paxos.com>
2019-08-21 11:18:08 -0400

    Emulate with-npm-version, adding package.json.

--- bin/typescript-fetch-petstore-namespace-parameter-interfaces.sh
+++ bin/typescript-fetch-petstore-namespace-parameter-interfaces.sh
@@ -27,6 +27,8 @@ fi
 
 # if you've executed sbt assembly previously it will use that instead.
 export JAVA_OPTS="${JAVA_OPTS} -Xmx1024M -DloggerPath=conf/log4j.properties"
-ags="generate -i modules/openapi-generator/src/test/resources/2_0/petstore.yaml -g typescript-fetch -o samples/client/petstore/typescript-fetch/builds/namespace-parameter-interfaces --additional-properties namespaceParameterInterfaces=true $@"
+ags="generate -i modules/openapi-generator/src/test/resources/2_0/petstore.yaml -g typescript-fetch -c bin/typescript-fetch-petstore-namespace-parameter-interfaces.json -o samples/client/petstore/typescript-fetch/builds/namespace-parameter-interfaces $@"
 
 java $JAVA_OPTS -jar $executable $ags
+
+cp CI/samples.ci/client/petstore/typescript-fetch/builds/namespace-parameter-interfaces/pom.xml samples/client/petstore/typescript-fetch/builds/namespace-parameter-interfaces/pom.xml
