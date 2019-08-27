15e8ceb2d4202dc3246a5314270e89097372e923
man.from.home <man.from.home@gmail.com>
2019-08-28 00:57:16 +0200

    asciidoc markup include processing with mustache filter

--- modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/AsciidocDocumentationCodegen.java
+++ modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/AsciidocDocumentationCodegen.java
@@ -3,25 +3,82 @@ package org.openapitools.codegen.languages;
 import org.openapitools.codegen.*;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.Writer;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.HashSet;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-/** basic asciidoc generator.
+import com.samskivert.mustache.Mustache;
+import com.samskivert.mustache.Template;
+
+import io.swagger.v3.oas.models.OpenAPI;
+
+/** basic asciidoc markup generator.
  * @see <a href="https://asciidoctor.org">asciidoctor</a> 
   */
 public class AsciidocDocumentationCodegen extends DefaultCodegen implements CodegenConfig {
-    public static final String PROJECT_NAME = "projectName";
 
-    static Logger LOGGER = LoggerFactory.getLogger(AsciidocDocumentationCodegen.class);
+	private static final Logger LOGGER = LoggerFactory.getLogger(AsciidocDocumentationCodegen.class);
     
+	/**
+	 * Lambda emitting an asciidoc "include::filename.adoc[]" if file is found in path. 
+	 * Use:
+	 * <pre>{{#includeMarkup}}{{name}}/description.adoc{{/includeMarkup}}</pre>
+	 */
+	public class IncludeMarkupLambda implements Mustache.Lambda {
+
+		/** {{includeMarkup}} */
+	    public static final String includeMarkupTag = "includemarkup";
+	    
+		private CodegenConfig config;
+		
+		private long includeCount = 0;
+		private long notFoundCount = 0;
+	    
+	    public IncludeMarkupLambda(final CodegenConfig config) {
+	    	this.config = config;
+		}
+	    
+	    public String resetCounter( ) {
+	    	String msg = "included:" + includeCount + " notFound: " + notFoundCount;
+	    	includeCount = 0;
+	    	notFoundCount = 0;
+	    	return msg;
+	    }
+
+	    @Override
+	    public void execute(final Template.Fragment frag, final Writer out) throws IOException {
+	        
+	    	String basePath = config.templateDir() != null ?  config.templateDir() : ".";
+	    	if( !Files.isDirectory(Paths.get(config.templateDir()))) {
+	    		LOGGER.warn("base part for include markup lambda not found: " + basePath);
+	    	};
+	    	
+	    	final String relativeFileName = frag.execute();
+	    	final Path filePathToInclude = Paths.get(basePath, relativeFileName);
+	        
+	        if(Files.isRegularFile(filePathToInclude)) {
+		        LOGGER.debug("including " + ++includeCount + ". file into markup: " + filePathToInclude.toAbsolutePath().toString());
+		        out.write("\ninclude::" + relativeFileName + "[]\n");	
+	        } else {
+		        LOGGER.debug(++notFoundCount + ". file not found, skip include into markup: " + filePathToInclude.toAbsolutePath().toString());
+		        out.write("\n// markup not included, not found: include::" + relativeFileName + "[]\n");	
+	        }	        
+	    }
+	}
+	
     protected String invokerPackage = "org.openapitools.client";
     protected String groupId = "org.openapitools";
     protected String artifactId = "openapi-client";
     protected String artifactVersion = "1.0.0";
-
+    
+    private IncludeMarkupLambda includeMarkupLambda;
 
     public CodegenType getTag() {
         return CodegenType.DOCUMENTATION;
@@ -32,13 +89,13 @@ public class AsciidocDocumentationCodegen extends DefaultCodegen implements Code
     }
 
     public String getHelp() {
-        return "Generates a asciidoc documentation.";
+        return "Generates asciidoc markup based documentation.";
     }
 
     public AsciidocDocumentationCodegen() {
         super();
 
-        LOGGER.info("start asciidoc codegen");
+        LOGGER.trace("start asciidoc codegen");
         
         outputFolder = "generated-code" + File.separator + "asciidoc";
         embeddedTemplateDir = templateDir = "asciidoc-documentation";
@@ -84,4 +141,24 @@ public class AsciidocDocumentationCodegen extends DefaultCodegen implements Code
     public String escapeUnsafeCharacters(String input) {
         return input; // just return the original string
     }
+    
+    @Override
+    public void processOpts() {
+        super.processOpts();
+        // addMustacheLambdas() does not work.
+        this.includeMarkupLambda = new IncludeMarkupLambda(this);
+        additionalProperties.put(IncludeMarkupLambda.includeMarkupTag, this.includeMarkupLambda );
+    }
+
+    @Override
+    public void processOpenAPI(OpenAPI openAPI) {
+    	if(this.includeMarkupLambda != null) {
+    		LOGGER.info(
+    				IncludeMarkupLambda.includeMarkupTag + 
+    				": " + 
+    				this.includeMarkupLambda.resetCounter());
+    	}
+		super.processOpenAPI(openAPI);
+	}
+
 }
