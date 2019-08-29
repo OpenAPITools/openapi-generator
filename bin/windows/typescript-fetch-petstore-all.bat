0aa4262ef6b2926b74851922107b696bdb192e7e
Malhotra <someone1@gmail.com>
2019-08-29 11:09:02 -0400

    typescript-fetch: add option for TypeScript 3.6+ compatible generation

--- bin/windows/typescript-fetch-petstore-all.bat
+++ bin/windows/typescript-fetch-petstore-all.bat
@@ -6,3 +6,4 @@ call bin\windows\typescript-fetch-petstore-with-npm-version.bat
 call bin\windows\typescript-fetch-petstore-interfaces.bat
 call bin\windows\typescript-fetch-petstore-multiple-parameters.bat
 call bin\windows\typescript-fetch-petstore-prefix-parameter-interfaces.bat
+call bin\windows\typescript-fetch-petstore-typescript-three-plus.bat
