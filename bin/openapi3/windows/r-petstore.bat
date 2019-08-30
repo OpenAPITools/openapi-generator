cd39f4ad041cb6dae9a36e75902ca2097ef76cb4
William Cheng <wing328hk@gmail.com>
2019-08-30 11:55:27 +0800

    add new filies

---
+++ bin/openapi3/windows/r-petstore.bat
@@ -0,0 +1,10 @@
+set executable=.\modules\openapi-generator-cli\target\openapi-generator-cli.jar
+
+If Not Exist %executable% (
+  mvn clean package
+)
+
+REM set JAVA_OPTS=%JAVA_OPTS% -Xmx1024M -DloggerPath=conf/log4j.properties
+set ags=generate -i modules\openapi-generator\src\test\resources\3_0\petstore.yaml -g r -o samples\client\petstore\R
+
+java %JAVA_OPTS% -jar %executable% %ags%
