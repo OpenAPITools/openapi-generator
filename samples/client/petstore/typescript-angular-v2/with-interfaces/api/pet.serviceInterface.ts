f25a5a0293d3c2cc342df33ad943c0137e276b4d
Michael Nahkies <michael@nahkies.co.nz>
2019-08-31 11:37:18 +0100

    issue #2845: run ./bin/openapi3/typescript-angular-petstore-all.sh

--- samples/client/petstore/typescript-angular-v2/with-interfaces/api/pet.serviceInterface.ts
+++ samples/client/petstore/typescript-angular-v2/with-interfaces/api/pet.serviceInterface.ts
@@ -28,9 +28,9 @@ export interface PetServiceInterface {
     /**
     * Add a new pet to the store
     * 
-    * @param body Pet object that needs to be added to the store
+    * @param pet Pet object that needs to be added to the store
     */
-    addPet(body: Pet, extraHttpRequestParams?: any): Observable<{}>;
+    addPet(pet: Pet, extraHttpRequestParams?: any): Observable<{}>;
 
     /**
     * Deletes a pet
@@ -64,9 +64,9 @@ export interface PetServiceInterface {
     /**
     * Update an existing pet
     * 
-    * @param body Pet object that needs to be added to the store
+    * @param pet Pet object that needs to be added to the store
     */
-    updatePet(body: Pet, extraHttpRequestParams?: any): Observable<{}>;
+    updatePet(pet: Pet, extraHttpRequestParams?: any): Observable<{}>;
 
     /**
     * Updates a pet in the store with form data
