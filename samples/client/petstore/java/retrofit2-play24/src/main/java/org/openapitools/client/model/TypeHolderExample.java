221443ac208e588dcfaf604bafd7401bac2ab99f
Benjamin Simpson <ben@bensimpson.ch>
2019-09-07 06:36:19 +0200

    [Java][okhttp-gson] fix failure to deserialize floats (#3846)
    * fixed bug where nullApi.java would be generated.  Instead, generated DefaultApi.java to match the default path /{pathParam}
    
    * fix to bug #3157
    
    * update samples

--- samples/client/petstore/java/retrofit2-play24/src/main/java/org/openapitools/client/model/TypeHolderExample.java
+++ samples/client/petstore/java/retrofit2-play24/src/main/java/org/openapitools/client/model/TypeHolderExample.java
@@ -34,6 +34,7 @@ import javax.validation.Valid;
 @JsonPropertyOrder({
   TypeHolderExample.JSON_PROPERTY_STRING_ITEM,
   TypeHolderExample.JSON_PROPERTY_NUMBER_ITEM,
+  TypeHolderExample.JSON_PROPERTY_FLOAT_ITEM,
   TypeHolderExample.JSON_PROPERTY_INTEGER_ITEM,
   TypeHolderExample.JSON_PROPERTY_BOOL_ITEM,
   TypeHolderExample.JSON_PROPERTY_ARRAY_ITEM
@@ -45,6 +46,9 @@ public class TypeHolderExample {
 
   public static final String JSON_PROPERTY_NUMBER_ITEM = "number_item";
   private BigDecimal numberItem;
+
+  public static final String JSON_PROPERTY_FLOAT_ITEM = "float_item";
+  private Float floatItem;
 
   public static final String JSON_PROPERTY_INTEGER_ITEM = "integer_item";
   private Integer integerItem;
@@ -106,6 +110,32 @@ public class TypeHolderExample {
 
   public void setNumberItem(BigDecimal numberItem) {
     this.numberItem = numberItem;
+  }
+
+
+  public TypeHolderExample floatItem(Float floatItem) {
+    
+    this.floatItem = floatItem;
+    return this;
+  }
+
+   /**
+   * Get floatItem
+   * @return floatItem
+  **/
+  @NotNull
+  @ApiModelProperty(example = "1.234", required = true, value = "")
+  @JsonProperty(JSON_PROPERTY_FLOAT_ITEM)
+  @JsonInclude(value = JsonInclude.Include.ALWAYS)
+
+  public Float getFloatItem() {
+    return floatItem;
+  }
+
+
+
+  public void setFloatItem(Float floatItem) {
+    this.floatItem = floatItem;
   }
 
 
@@ -203,6 +233,7 @@ public class TypeHolderExample {
     TypeHolderExample typeHolderExample = (TypeHolderExample) o;
     return Objects.equals(this.stringItem, typeHolderExample.stringItem) &&
         Objects.equals(this.numberItem, typeHolderExample.numberItem) &&
+        Objects.equals(this.floatItem, typeHolderExample.floatItem) &&
         Objects.equals(this.integerItem, typeHolderExample.integerItem) &&
         Objects.equals(this.boolItem, typeHolderExample.boolItem) &&
         Objects.equals(this.arrayItem, typeHolderExample.arrayItem);
@@ -210,7 +241,7 @@ public class TypeHolderExample {
 
   @Override
   public int hashCode() {
-    return Objects.hash(stringItem, numberItem, integerItem, boolItem, arrayItem);
+    return Objects.hash(stringItem, numberItem, floatItem, integerItem, boolItem, arrayItem);
   }
 
 
@@ -220,6 +251,7 @@ public class TypeHolderExample {
     sb.append("class TypeHolderExample {\n");
     sb.append("    stringItem: ").append(toIndentedString(stringItem)).append("\n");
     sb.append("    numberItem: ").append(toIndentedString(numberItem)).append("\n");
+    sb.append("    floatItem: ").append(toIndentedString(floatItem)).append("\n");
     sb.append("    integerItem: ").append(toIndentedString(integerItem)).append("\n");
     sb.append("    boolItem: ").append(toIndentedString(boolItem)).append("\n");
     sb.append("    arrayItem: ").append(toIndentedString(arrayItem)).append("\n");
