dc2907aced112b947543f6d50dc118d7a44c28c8
aeb-sia <50743092+aeb-sia@users.noreply.github.com>
2019-09-12 14:44:56 +0200

    typescript-node: Use HttpError class when rejecting promises (#3876)
    * Use HttpError class when rejecting promises
    
    Fixes #3872
    
    * Update samples
    
    * Test the new code in client.ts

--- samples/client/petstore/typescript-node/npm/api/apis.ts
+++ samples/client/petstore/typescript-node/npm/api/apis.ts
@@ -4,4 +4,11 @@ export * from './storeApi';
 import { StoreApi } from './storeApi';
 export * from './userApi';
 import { UserApi } from './userApi';
+import http = require('http');
+export class HttpError extends Error {
+    constructor (public response: http.ClientResponse, public body: any, public statusCode?: number) {
+        super('HTTP request failed');
+        this.name = 'HttpError';
+    }
+}
 export const APIS = [PetApi, StoreApi, UserApi];
