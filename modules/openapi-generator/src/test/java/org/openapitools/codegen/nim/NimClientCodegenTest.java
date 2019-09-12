2db44c71f0376b98da9f7ec57cd95e32ec8c23d2
Hideki Okamoto <okamoto@fab34.com>
2019-09-12 20:23:17 +0900

    Add a test snippet

--- modules/openapi-generator/src/test/java/org/openapitools/codegen/nim/NimClientCodegenTest.java
+++ modules/openapi-generator/src/test/java/org/openapitools/codegen/nim/NimClientCodegenTest.java
@@ -2,18 +2,37 @@ package org.openapitools.codegen.nim;
 
 import org.openapitools.codegen.*;
 import org.openapitools.codegen.languages.NimClientCodegen;
-import io.swagger.models.*;
-import io.swagger.parser.SwaggerParser;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class NimClientCodegenTest {
 
-    NimClientCodegen codegen = new NimClientCodegen();
+  @Test
+  public void testInitialConfigValues() throws Exception {
+    final NimClientCodegen codegen = new NimClientCodegen();
+    codegen.processOpts();
 
-    @Test
-    public void shouldSucceed() throws Exception {
-        // TODO: Complete this test.
-        Assert.fail("Not implemented.");
-    }
+    Assert.assertEquals(codegen.additionalProperties().get(CodegenConstants.HIDE_GENERATION_TIMESTAMP), Boolean.TRUE);
+    Assert.assertEquals(codegen.isHideGenerationTimestamp(), true);
+  }
+
+  @Test
+  public void testSettersForConfigValues() throws Exception {
+    final NimClientCodegen codegen = new NimClientCodegen();
+    codegen.setHideGenerationTimestamp(false);
+    codegen.processOpts();
+
+    Assert.assertEquals(codegen.additionalProperties().get(CodegenConstants.HIDE_GENERATION_TIMESTAMP), Boolean.FALSE);
+    Assert.assertEquals(codegen.isHideGenerationTimestamp(), false);
+  }
+
+  @Test
+  public void testAdditionalPropertiesPutForConfigValues() throws Exception {
+    final NimClientCodegen codegen = new NimClientCodegen();
+    codegen.additionalProperties().put(CodegenConstants.HIDE_GENERATION_TIMESTAMP, false);
+    codegen.processOpts();
+
+    Assert.assertEquals(codegen.additionalProperties().get(CodegenConstants.HIDE_GENERATION_TIMESTAMP), Boolean.FALSE);
+    Assert.assertEquals(codegen.isHideGenerationTimestamp(), false);
+  }
 }
