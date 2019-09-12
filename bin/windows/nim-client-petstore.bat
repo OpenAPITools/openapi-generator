bface471029f659d28bffe993a16211d601a1d5d
Hideki Okamoto <okamoto@fab34.com>
2019-09-12 19:02:57 +0900

    Update the batch for Windows

--- bin/windows/nim-client-petstore.bat
+++ bin/windows/nim-client-petstore.bat
@@ -5,6 +5,6 @@ If Not Exist %executable% (
 )
 
 REM set JAVA_OPTS=%JAVA_OPTS% -Xmx1024M -DloggerPath=conf/log4j.properties
-set ags=generate  --artifact-id "nim-petstore-client" -i modules\openapi-generator\src\test\resources\2_0\petstore.yaml -g nim -o samples\client\petstore\nim
+set ags=generate  --artifact-id "nim-petstore-client" -i modules\openapi-generator\src\test\resources\2_0\petstore.yaml --additional-properties packageName=petstore -g nim -o samples\client\petstore\nim
 
 java %JAVA_OPTS% -jar %executable% %ags%
