29b2ecede5db9d00e1026a3a67b17e22bbc1aeee
Jim Schubert <james.schubert@gmail.com>
2019-10-16 13:37:34 -0400

    [android] FeatureSet definitions

--- modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/AndroidClientCodegen.java
+++ modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/AndroidClientCodegen.java
@@ -21,7 +21,7 @@ import io.swagger.v3.oas.models.media.ArraySchema;
 import io.swagger.v3.oas.models.media.Schema;
 import org.apache.commons.lang3.StringUtils;
 import org.openapitools.codegen.*;
-import org.openapitools.codegen.meta.features.DocumentationFeature;
+import org.openapitools.codegen.meta.features.*;
 import org.openapitools.codegen.utils.ModelUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -60,8 +60,33 @@ public class AndroidClientCodegen extends DefaultCodegen implements CodegenConfi
     public AndroidClientCodegen() {
         super();
 
+        // TODO: Android client maintainer review.
         featureSet = getFeatureSet().modify()
                 .includeDocumentationFeature(DocumentationFeature.Readme)
+                .excludeWireFormatFeatures(
+                        WireFormatFeature.PROTOBUF
+                )
+                .excludeSecurityFeatures(
+                        SecurityFeature.OpenIDConnect,
+                        SecurityFeature.OAuth2_Password,
+                        SecurityFeature.OAuth2_AuthorizationCode,
+                        SecurityFeature.OAuth2_ClientCredentials,
+                        SecurityFeature.OAuth2_Implicit,
+                        SecurityFeature.BearerToken
+                )
+                .excludeGlobalFeatures(
+                        GlobalFeature.XMLStructureDefinitions,
+                        GlobalFeature.Callbacks,
+                        GlobalFeature.LinkObjects,
+                        GlobalFeature.ParameterStyling
+                )
+                .includeSchemaSupportFeature(
+                        SchemaSupportFeature.Polymorphism
+                )
+                .excludeParameterFeatures(
+                        ParameterFeature.Cookie
+                )
+                .includeClientModificationFeature(ClientModificationFeature.BasePath)
                 .build();
 
         outputFolder = "generated-code/android";
