6b65436167b07cb280c8951be0464aadf54cfdca
wing328 <wing328hk@gmail.com>
2019-10-16 18:16:31 +0800

    use dartmft

--- samples/client/petstore/dart-dio/lib/model/api_response.dart
+++ samples/client/petstore/dart-dio/lib/model/api_response.dart
@@ -1,33 +1,24 @@
-        import 'package:built_value/built_value.dart';
+import 'package:built_value/built_value.dart';
 import 'package:built_value/serializer.dart';
 
 part 'api_response.g.dart';
 
 abstract class ApiResponse implements Built<ApiResponse, ApiResponseBuilder> {
+  @nullable
+  @BuiltValueField(wireName: 'code')
+  int get code;
 
-    
-        @nullable
+  @nullable
+  @BuiltValueField(wireName: 'type')
+  String get type;
 
-    
-    @BuiltValueField(wireName: 'code')
-    int get code;
-    
-        @nullable
+  @nullable
+  @BuiltValueField(wireName: 'message')
+  String get message;
 
-    
-    @BuiltValueField(wireName: 'type')
-    String get type;
-    
-        @nullable
+  // Boilerplate code needed to wire-up generated code
+  ApiResponse._();
 
-    
-    @BuiltValueField(wireName: 'message')
-    String get message;
-
-    // Boilerplate code needed to wire-up generated code
-    ApiResponse._();
-
-    factory ApiResponse([updates(ApiResponseBuilder b)]) = _$ApiResponse;
-    static Serializer<ApiResponse> get serializer => _$apiResponseSerializer;
-
+  factory ApiResponse([updates(ApiResponseBuilder b)]) = _$ApiResponse;
+  static Serializer<ApiResponse> get serializer => _$apiResponseSerializer;
 }
--- samples/client/petstore/dart-dio/lib/model/order.dart
+++ samples/client/petstore/dart-dio/lib/model/order.dart
@@ -1,52 +1,39 @@
-        import 'package:built_value/built_value.dart';
+import 'package:built_value/built_value.dart';
 import 'package:built_value/serializer.dart';
 
 part 'order.g.dart';
 
 abstract class Order implements Built<Order, OrderBuilder> {
+  @nullable
+  @BuiltValueField(wireName: 'id')
+  int get id;
 
-    
-        @nullable
+  @nullable
+  @BuiltValueField(wireName: 'petId')
+  int get petId;
 
-    
-    @BuiltValueField(wireName: 'id')
-    int get id;
-    
-        @nullable
+  @nullable
+  @BuiltValueField(wireName: 'quantity')
+  int get quantity;
 
-    
-    @BuiltValueField(wireName: 'petId')
-    int get petId;
-    
-        @nullable
+  @nullable
+  @BuiltValueField(wireName: 'shipDate')
+  DateTime get shipDate;
+  /* Order Status */
+  @nullable
 
-    
-    @BuiltValueField(wireName: 'quantity')
-    int get quantity;
-    
-        @nullable
+  /* Order Status */
+  @BuiltValueField(wireName: 'status')
+  String get status;
+  //enum statusEnum {  placed,  approved,  delivered,  };{
 
-    
-    @BuiltValueField(wireName: 'shipDate')
-    DateTime get shipDate;
-    /* Order Status */
-        @nullable
+  @nullable
+  @BuiltValueField(wireName: 'complete')
+  bool get complete;
 
-    /* Order Status */
-    @BuiltValueField(wireName: 'status')
-    String get status;
-        //enum statusEnum {  placed,  approved,  delivered,  };{
-    
-        @nullable
+  // Boilerplate code needed to wire-up generated code
+  Order._();
 
-    
-    @BuiltValueField(wireName: 'complete')
-    bool get complete;
-
-    // Boilerplate code needed to wire-up generated code
-    Order._();
-
-    factory Order([updates(OrderBuilder b)]) = _$Order;
-    static Serializer<Order> get serializer => _$orderSerializer;
-
+  factory Order([updates(OrderBuilder b)]) = _$Order;
+  static Serializer<Order> get serializer => _$orderSerializer;
 }
