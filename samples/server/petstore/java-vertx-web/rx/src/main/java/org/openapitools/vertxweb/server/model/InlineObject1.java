e3046b403549558532806adfee204232a0929d51
likwei <lwlee2608@gmail.com>
2019-10-26 20:11:34 +0800

    add readme and sample

---
+++ samples/server/petstore/java-vertx-web/rx/src/main/java/org/openapitools/vertxweb/server/model/InlineObject1.java
@@ -0,0 +1,83 @@
+package org.openapitools.vertxweb.server.model;
+
+import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import java.io.File;
+
+@JsonInclude(JsonInclude.Include.NON_NULL) 
+public class InlineObject1   {
+  
+  private String additionalMetadata;
+  private File file;
+
+  public InlineObject1 () {
+
+  }
+
+  public InlineObject1 (String additionalMetadata, File file) {
+    this.additionalMetadata = additionalMetadata;
+    this.file = file;
+  }
+
+    
+  @JsonProperty("additionalMetadata")
+  public String getAdditionalMetadata() {
+    return additionalMetadata;
+  }
+  public void setAdditionalMetadata(String additionalMetadata) {
+    this.additionalMetadata = additionalMetadata;
+  }
+
+    
+  @JsonProperty("file")
+  public File getFile() {
+    return file;
+  }
+  public void setFile(File file) {
+    this.file = file;
+  }
+
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    InlineObject1 inlineObject1 = (InlineObject1) o;
+    return Objects.equals(additionalMetadata, inlineObject1.additionalMetadata) &&
+        Objects.equals(file, inlineObject1.file);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(additionalMetadata, file);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class InlineObject1 {\n");
+    
+    sb.append("    additionalMetadata: ").append(toIndentedString(additionalMetadata)).append("\n");
+    sb.append("    file: ").append(toIndentedString(file)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
