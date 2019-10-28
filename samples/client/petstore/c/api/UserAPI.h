e339baa52ac21ee952a55e20cea2e55b65c71457
michelealbano <michele.albano@gmail.com>
2019-10-27 20:14:02 -0700

    changed test files to support additionalProperties for C generator

--- samples/client/petstore/c/api/UserAPI.h
+++ samples/client/petstore/c/api/UserAPI.h
@@ -11,43 +11,53 @@
 //
 // This can only be done by the logged in user.
 //
-void UserAPI_createUser(apiClient_t *apiClient, user_t *body);
+void
+UserAPI_createUser(apiClient_t *apiClient ,user_t * body);
 
 
 // Creates list of users with given input array
 //
-void UserAPI_createUsersWithArrayInput(apiClient_t *apiClient, list_t *body);
+void
+UserAPI_createUsersWithArrayInput(apiClient_t *apiClient ,list_t * body);
 
 
 // Creates list of users with given input array
 //
-void UserAPI_createUsersWithListInput(apiClient_t *apiClient, list_t *body);
+void
+UserAPI_createUsersWithListInput(apiClient_t *apiClient ,list_t * body);
 
 
 // Delete user
 //
 // This can only be done by the logged in user.
 //
-void UserAPI_deleteUser(apiClient_t *apiClient, char *username);
+void
+UserAPI_deleteUser(apiClient_t *apiClient ,char * username);
 
 
 // Get user by user name
 //
-user_t *UserAPI_getUserByName(apiClient_t *apiClient, char *username);
+user_t*
+UserAPI_getUserByName(apiClient_t *apiClient ,char * username);
 
 
 // Logs user into the system
 //
-char *UserAPI_loginUser(apiClient_t *apiClient, char *username, char *password);
+char*
+UserAPI_loginUser(apiClient_t *apiClient ,char * username ,char * password);
 
 
 // Logs out current logged in user session
 //
-void UserAPI_logoutUser(apiClient_t *apiClient);
+void
+UserAPI_logoutUser(apiClient_t *apiClient);
 
 
 // Updated user
 //
 // This can only be done by the logged in user.
 //
-void UserAPI_updateUser(apiClient_t *apiClient, char *username, user_t *body);
+void
+UserAPI_updateUser(apiClient_t *apiClient ,char * username ,user_t * body);
+
+
