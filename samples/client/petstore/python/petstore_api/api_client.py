a9dda78731c1656c508a82d54e43ebba297944eb
Colossal Code <54778962+colossal-code@users.noreply.github.com>
2019-10-30 08:39:42 +0530

    [Python] Avoid pep8 violation

--- samples/client/petstore/python/petstore_api/api_client.py
+++ samples/client/petstore/python/petstore_api/api_client.py
@@ -11,6 +11,7 @@
 from __future__ import absolute_import
 
 import datetime
+from dateutil.parser import parse
 import json
 import mimetypes
 from multiprocessing.pool import ThreadPool
@@ -577,7 +578,6 @@ class ApiClient(object):
         :return: date.
         """
         try:
-            from dateutil.parser import parse
             return parse(string).date()
         except ImportError:
             return string
@@ -596,7 +596,6 @@ class ApiClient(object):
         :return: datetime.
         """
         try:
-            from dateutil.parser import parse
             return parse(string)
         except ImportError:
             return string
