594ba066176f6d6f665b316d5ce02fe2129a1cc0
Bruno Coelho <4brunu@gmail.com>
2019-10-31 09:57:21 +0000

    [kotlin][client] fix JsonMediaType and update petstores

--- samples/client/petstore/kotlin-gson/src/main/kotlin/org/openapitools/client/infrastructure/ApiClient.kt
+++ samples/client/petstore/kotlin-gson/src/main/kotlin/org/openapitools/client/infrastructure/ApiClient.kt
@@ -50,7 +50,7 @@ open class ApiClient(val baseUrl: String) {
                     }
                 }.build()
             }
-            mediaType == JsonMediaType -> MediaType.parse(mediaType), Serializer.gson.toJson(content, T::class.java).toRequestBody(
+            mediaType == JsonMediaType -> Serializer.gson.toJson(content, T::class.java).toRequestBody(
                 mediaType.toMediaTypeOrNull()
             )
             mediaType == XmlMediaType -> throw UnsupportedOperationException("xml not currently supported.")
