3a6a3579a0947a9d582c037b8e557d42648984bc
Bruno Coelho <4brunu@gmail.com>
2019-11-07 12:01:26 +0000

    try to fix build

--- bin/kotlin-client-nonpublic.sh
+++ bin/kotlin-client-nonpublic.sh
@@ -30,5 +30,3 @@ export JAVA_OPTS="${JAVA_OPTS} -Xmx1024M -DloggerPath=conf/log4j.properties"
 ags="generate -t modules/openapi-generator/src/main/resources/kotlin-client -i modules/openapi-generator/src/test/resources/2_0/petstore.yaml -g kotlin --artifact-id kotlin-petstore-nonpublic --additional-properties nonPublicApi=true -o samples/client/petstore/kotlin-nonpublic $@"
 
 java ${JAVA_OPTS} -jar ${executable} ${ags}
-
-cp CI/samples.ci/client/petstore/kotlin-nonpublic/pom.xml samples/client/petstore/kotlin-nonpublic/pom.xml
