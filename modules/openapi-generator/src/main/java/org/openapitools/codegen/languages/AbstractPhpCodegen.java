d83006a1f7d74479d8a33dd07469f032d573fe92
Yuriy Belenko <yura-bely@mail.ru>
2019-11-08 23:33:45 +0500

    Revert "[PHP] Don't suppress warning from default codegen"
    This reverts commit d9d6716bf28f52725b4cb268b4ccb3474ce6ed1d.

--- modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/AbstractPhpCodegen.java
+++ modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/AbstractPhpCodegen.java
@@ -666,15 +666,14 @@ public abstract class AbstractPhpCodegen extends DefaultCodegen implements Codeg
 
     @Override
     public Map<String, Object> postProcessOperationsWithModels(Map<String, Object> objs, List<Object> allModels) {
-        Map<String, Object> superClassObjs = super.postProcessOperationsWithModels(objs, allModels);
-        Map<String, Object> operations = (Map<String, Object>) superClassObjs.get("operations");
+        Map<String, Object> operations = (Map<String, Object>) objs.get("operations");
         List<CodegenOperation> operationList = (List<CodegenOperation>) operations.get("operation");
         for (CodegenOperation op : operationList) {
             // for API test method name
             // e.g. public function test{{vendorExtensions.x-testOperationId}}()
             op.vendorExtensions.put("x-testOperationId", camelize(op.operationId));
         }
-        return superClassObjs;
+        return objs;
     }
 
     @Override
