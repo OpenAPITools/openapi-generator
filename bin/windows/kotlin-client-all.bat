ad41599c1e05cc92bfd3271421efb63a40515b29
Alexander Eggers <alex@domesticcat.com.au>
2019-11-12 15:58:47 +1100

    Reverted the mediaType addition
    In this commit I have also made some changes to the kotlin-client-all.bat to make this file a bit more robust regarding different folder structures.

--- bin/windows/kotlin-client-all.bat
+++ bin/windows/kotlin-client-all.bat
@@ -1,8 +1,8 @@
-call %~dp0\kotlin-client-gson.bat
-call %~dp0\kotlin-client-nonpublic.bat
-call %~dp0\kotlin-client-okhttp3.bat
-call %~dp0\kotlin-client-petstore-multiplatform.bat
-call %~dp0\kotlin-client-petstore.bat
-call %~dp0\kotlin-client-string.bat
-call %~dp0\kotlin-client-threetenbp.bat
-call %~dp0\kotlin-client-nullable.bat
+call powershell -command "& '%~dp0\kotlin-client-gson.bat'"
+call powershell -command "& '%~dp0\kotlin-client-nonpublic.bat'"
+call powershell -command "& '%~dp0\kotlin-client-okhttp3.bat'"
+call powershell -command "& '%~dp0\kotlin-client-petstore-multiplatform.bat'"
+call powershell -command "& '%~dp0\kotlin-client-petstore.bat'"
+call powershell -command "& '%~dp0\kotlin-client-string.bat'"
+call powershell -command "& '%~dp0\kotlin-client-threetenbp.bat'"
+call powershell -command "& '%~dp0\kotlin-client-nullable.bat'"
