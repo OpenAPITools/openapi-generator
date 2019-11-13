0dc039e7a64e42b8d0ca1d73799994ee216777b8
Jim Schubert <james.schubert@gmail.com>
2019-11-12 21:34:04 -0500

    [kotlin] Fix new bin/kotlin-client-nullable.sh script (#1)

--- bin/kotlin-client-nullable.sh
+++ bin/kotlin-client-nullable.sh
@@ -27,6 +27,6 @@ fi
 
 # if you've executed sbt assembly previously it will use that instead.
 export JAVA_OPTS="${JAVA_OPTS} -Xmx1024M -DloggerPath=conf/log4j.properties"
-ags="generate -t modules/openapi-generator/src/main/resources/kotlin-client -i modules/openapi-generator/src/test/resources/2_0/petstore.yaml -g kotlin --artifact-id "kotlin-petstore-nullable" --additional-properties nullableReturnType=true,serializableModel=true -o samples\client\petstore\kotlin-nullable $@"
+ags="generate -t modules/openapi-generator/src/main/resources/kotlin-client -i modules/openapi-generator/src/test/resources/2_0/petstore.yaml -g kotlin --artifact-id kotlin-petstore-nullable --additional-properties nullableReturnType=true,serializableModel=true -o samples/client/petstore/kotlin-nullable $@"
 
 java ${JAVA_OPTS} -jar ${executable} ${ags}
