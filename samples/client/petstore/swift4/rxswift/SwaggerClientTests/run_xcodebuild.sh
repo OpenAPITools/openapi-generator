bcdd5ce182eb1c01ecd3bed0d18dbcb114abacaa
Bruno Coelho <4brunu@gmail.com>
2019-11-18 21:08:45 +0000

    [swift] improve testing in sample projects

--- samples/client/petstore/swift4/rxswift/SwaggerClientTests/run_xcodebuild.sh
+++ samples/client/petstore/swift4/rxswift/SwaggerClientTests/run_xcodebuild.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-xcodebuild -workspace "SwaggerClient.xcworkspace" -scheme "SwaggerClient" test -destination "platform=iOS Simulator,name=iPhone 8,OS=13.1" | xcpretty && exit ${PIPESTATUS[0]}
+pod install && xcodebuild -workspace "SwaggerClient.xcworkspace" -scheme "SwaggerClient" test -destination "platform=iOS Simulator,name=iPhone 8,OS=latest" | xcpretty && exit ${PIPESTATUS[0]}
