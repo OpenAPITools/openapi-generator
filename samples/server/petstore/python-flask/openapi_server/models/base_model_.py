bb7d554dbbf1321e49cf6f3a35db770b9926142a
Sven Kraus <mail@svenkraus.de>
2019-11-24 12:18:26 +0100

    ran python-server-flask-petstore.sh

--- samples/server/petstore/python-flask/openapi_server/models/base_model_.py
+++ samples/server/petstore/python-flask/openapi_server/models/base_model_.py
@@ -24,7 +24,6 @@ class Model(object):
 
     def to_dict(self, json_keys=True):
         """Returns the model properties as a dict
-
         :param json_keys: Defines if json_keys are used in dict
         :type: bool
         :rtype: dict
@@ -34,7 +33,6 @@ class Model(object):
         for attr, _ in six.iteritems(self.openapi_types):
             value = getattr(self, attr)
             dict_attr = self.attribute_map[attr] if json_keys else attr
-
             if isinstance(value, list):
                 result[dict_attr] = list(map(
                     lambda x: x.to_dict() if hasattr(x, "to_dict") else x,
