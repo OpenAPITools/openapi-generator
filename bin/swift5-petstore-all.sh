82dbb17761535163718e793b5fbbb2dc4c3324f8
Andrés Pizá <andres.piza@scrm.eu>
2019-12-03 18:24:59 +0100

    Implement Swift 5 support

---
+++ bin/swift5-petstore-all.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+./bin/swift5-petstore.sh
+./bin/swift5-petstore-promisekit.sh
+./bin/swift5-petstore-result.sh
+./bin/swift5-petstore-rxswift.sh
+./bin/swift5-petstore-objcCompatible.sh
+./bin/swift5-petstore-unwrapRequired.sh
+./bin/swift5-petstore-nonPublicApi.sh
