f951f020acb2fcd4cd3a331de0b1f352bac3874d
Bruno Coelho <4brunu@gmail.com>
2019-12-25 20:05:15 +0000

    [swift] update windows bat scripts

--- bin/windows/swift5-petstore-all.bat
+++ bin/windows/swift5-petstore-all.bat
@@ -1,3 +1,8 @@
 call .\bin\windows\swift5-petstore.bat
 call .\bin\windows\swift5-petstore-promisekit.bat
+call .\bin\windows\swift5-petstore-result.bat
 call .\bin\windows\swift5-petstore-rxswift.bat
+call .\bin\windows\swift5-petstore-objcCompatible.bat
+call .\bin\windows\swift5-petstore-nonPublicApi.bat
+call .\bin\windows\swift5-petstore-urlsession.bat
+call .\bin\windows\swift5-petstore-combine.bat
