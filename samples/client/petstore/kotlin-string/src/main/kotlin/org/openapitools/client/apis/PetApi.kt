9ef299c82ffa8452dfcd90305f55332ccbec8339
Artem Shubovych <ashubovych@atlassian.com>
2020-01-08 14:35:41 +1100

    Update Kotlin samples :shrug:

--- samples/client/petstore/kotlin-string/src/main/kotlin/org/openapitools/client/apis/PetApi.kt
+++ samples/client/petstore/kotlin-string/src/main/kotlin/org/openapitools/client/apis/PetApi.kt
@@ -71,15 +71,15 @@ class PetApi(basePath: kotlin.String = "http://petstore.swagger.io/v2") : ApiCli
     /**
     * Deletes a pet
     * 
-    * @param petId Pet id to delete 
     * @param apiKey  (optional)
+    * @param petId Pet id to delete 
     * @return void
     * @throws UnsupportedOperationException If the API returns an informational or redirection response
     * @throws ClientException If the API returns a client error response
     * @throws ServerException If the API returns a server error response
     */
     @Throws(UnsupportedOperationException::class, ClientException::class, ServerException::class)
-    fun deletePet(petId: kotlin.Long, apiKey: kotlin.String?) : Unit {
+    fun deletePet(apiKey: kotlin.String?, petId: kotlin.Long) : Unit {
         val localVariableBody: kotlin.Any? = null
         val localVariableQuery: MultiValueMap = mutableMapOf()
         val localVariableHeaders: MutableMap<String, String> = mutableMapOf("api_key" to apiKey.toString())
