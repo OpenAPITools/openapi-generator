9167ba76188d4e05ffa5085087e4e716d9114f0f
david1601 <59955469+david1601@users.noreply.github.com>
2020-01-17 10:43:00 +0100

    JavaSpring Add Spring pagination with vendor extension x-spring-paginated

---
+++ bin/spring-cloud-petstore-feign-spring-pageable.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+SCRIPT="$0"
+echo "# START SCRIPT: $SCRIPT"
+
+while [ -h "$SCRIPT" ] ; do
+  ls=`ls -ld "$SCRIPT"`
+  link=`expr "$ls" : '.*-> \(.*\)$'`
+  if expr "$link" : '/.*' > /dev/null; then
+    SCRIPT="$link"
+  else
+    SCRIPT=`dirname "$SCRIPT"`/"$link"
+  fi
+done
+
+if [ ! -d "${APP_DIR}" ]; then
+  APP_DIR=`dirname "$SCRIPT"`/..
+  APP_DIR=`cd "${APP_DIR}"; pwd`
+fi
+
+executable="./modules/openapi-generator-cli/target/openapi-generator-cli.jar"
+
+if [ ! -f "$executable" ]
+then
+  mvn -B clean package
+fi
+
+# if you've executed sbt assembly previously it will use that instead.
+export JAVA_OPTS="${JAVA_OPTS} -Xmx1024M -DloggerPath=conf/log4j.properties"
+ags="generate -t modules/openapi-generator/src/main/resources/JavaSpring -i modules/openapi-generator/src/test/resources/2_0/petstore-with-spring-pageable.yaml -g spring -c bin/spring-cloud-petstore-feign-spring-pageable.json -o samples/client/petstore/spring-cloud-spring-pageable --additional-properties hideGenerationTimestamp=true $@"
+
+echo "Removing files and folders under samples/client/spring-cloud-spring-pageable/src/main"
+rm -rf samples/client/petstore/spring-cloud-spring-pageable/src/main
+find samples/client/petstore/spring-cloud-spring-pageable -maxdepth 1 -type f ! -name "README.md" -exec rm {} +
+java $JAVA_OPTS -jar $executable $ags
