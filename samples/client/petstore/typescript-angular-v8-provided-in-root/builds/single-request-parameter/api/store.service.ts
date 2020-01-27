d111e3a35920b8475043aa5548f95b938ee7baa4
aanno2 <33512482+aanno2@users.noreply.github.com>
2020-01-27 09:40:59 +0100

    [REQ][typescript-angular]: support for object as query parameters (#4407)
    * https://github.com/OpenAPITools/openapi-generator/pull/4407 squashed from original, and better author (I failed to refer the right PR the first time)
    
    * removed isArrayLike in favour of Array.isArray
    
    * support collectionFormat the old way
    
    * fixed type and rebuild samples
    
    * revert samples/client/petstore/typescript-angular-v8-provided-in-root/builds/with-npm/package.json
    
    * reverted samples/client/petstore/typescript-angular-v8-provided-in-root/builds/with-npm/tsconfig.json
    
    * rebase-to-master-fix: isDateTime stuff is now handled within this.addToHttpParams
    
    * run bin/typescript-angular-petstore-all.sh on the rebased PR
    
    * applying proposed fix of @macjohnny
    
    * run bin/typescript-angular-petstore-all.sh to regenerate samples
    
    Co-authored-by: aanno <aanno@users.noreply.github.com>

--- samples/client/petstore/typescript-angular-v8-provided-in-root/builds/single-request-parameter/api/store.service.ts
+++ samples/client/petstore/typescript-angular-v8-provided-in-root/builds/single-request-parameter/api/store.service.ts
@@ -61,6 +61,38 @@ export class StoreService {
 
 
 
+    private addToHttpParams(httpParams: HttpParams, value: any, key?: string): HttpParams {
+        if (typeof value === "object") {
+            httpParams = this.addToHttpParamsRecursive(httpParams, value);
+        } else {
+            httpParams = this.addToHttpParamsRecursive(httpParams, value, key);
+        }
+        return httpParams;
+    }
+
+    private addToHttpParamsRecursive(httpParams: HttpParams, value: any, key?: string): HttpParams {
+        if (typeof value === "object") {
+            if (Array.isArray(value)) {
+                (value as any[]).forEach( elem => httpParams = this.addToHttpParamsRecursive(httpParams, elem, key));
+            } else if (value instanceof Date) {
+                if (key != null) {
+                    httpParams = httpParams.append(key,
+                        (value as Date).toISOString().substr(0, 10));
+                } else {
+                   throw Error("key may not be null if value is Date");
+                }
+            } else {
+                Object.keys(value).forEach( k => httpParams = this.addToHttpParamsRecursive(
+                    httpParams, value[k], key != null ? `${key}.${k}` : k));
+            }
+        } else if (key != null) {
+            httpParams = httpParams.append(key, value);
+        } else {
+            throw Error("key may not be null if value is not object or array");
+        }
+        return httpParams;
+    }
+
     /**
      * Delete purchase order by ID
      * For valid response try integer IDs with value &lt; 1000. Anything above 1000 or nonintegers will generate API errors
