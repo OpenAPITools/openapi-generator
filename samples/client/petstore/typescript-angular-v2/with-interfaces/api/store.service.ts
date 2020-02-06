7799ddebe12404851e410bcbda840de773552471
Dheeraj Nalluri <djnalluri@gmail.com>
2020-02-06 03:20:30 -0600

    [typescript-angular] Use security scheme name for API key lookup (#5220)
    * [typescript-angular] Use security scheme name when looking up API keys.
    
    * [typescript-angular] Updated samples.

--- samples/client/petstore/typescript-angular-v2/with-interfaces/api/store.service.ts
+++ samples/client/petstore/typescript-angular-v2/with-interfaces/api/store.service.ts
@@ -202,8 +202,11 @@ export class StoreService implements StoreServiceInterface {
         let headers = new Headers(this.defaultHeaders.toJSON()); // https://github.com/angular/angular/issues/6845
 
         // authentication (api_key) required
-        if (this.configuration.apiKeys && this.configuration.apiKeys["api_key"]) {
-            headers.set('api_key', this.configuration.apiKeys["api_key"]);
+        if (this.configuration.apiKeys) {
+            const key: string | undefined = this.configuration.apiKeys["api_key"] || this.configuration.apiKeys["api_key"];
+            if (key) {
+                headers.set('api_key', key);
+            }
         }
 
         let httpHeaderAcceptSelected: string | undefined = options && options.httpHeaderAccept;
