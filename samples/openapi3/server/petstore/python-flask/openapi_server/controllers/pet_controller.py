910ce9fa895c63472c9c892478a9de337376864e
LoveIsGrief <LoveIsGrief@users.noreply.github.com>
2020-02-17 15:36:21 +0100

    python: Add gitlab-ci.mustache (#5342)
    * python: Add gitlab-ci.mustache
    
    It does the same stuff as in the .travis.yml but just for Gitlab CI
    
    #5340 - Python .gitlab-ci.yml
    
    * python: Run all scripts in bin
    
    find bin/ -type f -name 'python*.sh' -exec {} \;
    
    Had to update all samples

--- samples/openapi3/server/petstore/python-flask/openapi_server/controllers/pet_controller.py
+++ samples/openapi3/server/petstore/python-flask/openapi_server/controllers/pet_controller.py
@@ -14,7 +14,7 @@ def add_pet(pet):  # noqa: E501
     :param pet: Pet object that needs to be added to the store
     :type pet: dict | bytes
 
-    :rtype: None
+    :rtype: Pet
     """
     if connexion.request.is_json:
         pet = Pet.from_dict(connexion.request.get_json())  # noqa: E501
@@ -83,7 +83,7 @@ def update_pet(pet):  # noqa: E501
     :param pet: Pet object that needs to be added to the store
     :type pet: dict | bytes
 
-    :rtype: None
+    :rtype: Pet
     """
     if connexion.request.is_json:
         pet = Pet.from_dict(connexion.request.get_json())  # noqa: E501
