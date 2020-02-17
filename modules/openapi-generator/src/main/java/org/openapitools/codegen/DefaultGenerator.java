527c118210b33c1d2c9acde7344619879b7639cb
Sebastien Rosset <serosset@cisco.com>
2020-02-16 17:23:00 -0800

    [codegen] Improve java code comments and argument documentation; Fix issue with ComposedSchema that has undeclared properties (#5316)
    * improve documentation
    
    * Add use case of composed schema with additional properties

--- modules/openapi-generator/src/main/java/org/openapitools/codegen/DefaultGenerator.java
+++ modules/openapi-generator/src/main/java/org/openapitools/codegen/DefaultGenerator.java
@@ -450,7 +450,10 @@ public class DefaultGenerator extends AbstractGenerator implements Generator {
                     LOGGER.info("Model " + name + " not generated since it's a free-form object");
                     continue;
                 } else if (ModelUtils.isMapSchema(schema)) { // check to see if it's a "map" model
-                    if (!ModelUtils.isGenerateAliasAsModel() && (schema.getProperties() == null || schema.getProperties().isEmpty())) {
+                    // A composed schema (allOf, oneOf, anyOf) is considered a Map schema if the additionalproperties attribute is set
+                    // for that composed schema. However, in the case of a composed schema, the properties are defined or referenced
+                    // in the inner schemas, and the outer schema does not have properties.
+                    if (!ModelUtils.isGenerateAliasAsModel() && !ModelUtils.isComposedSchema(schema) && (schema.getProperties() == null || schema.getProperties().isEmpty())) {
                         // schema without property, i.e. alias to map
                         LOGGER.info("Model " + name + " not generated since it's an alias to map (without property) and `generateAliasAsModel` is set to false (default)");
                         continue;
