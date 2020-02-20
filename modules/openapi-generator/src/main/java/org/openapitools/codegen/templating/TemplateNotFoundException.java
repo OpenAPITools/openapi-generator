e4c522d43937928e9d8e1fb2732ea823a4f92c88
Jim Schubert <james.schubert@gmail.com>
2020-02-19 22:34:30 -0500

    Fix javadoc for Java 11

--- modules/openapi-generator/src/main/java/org/openapitools/codegen/templating/TemplateNotFoundException.java
+++ modules/openapi-generator/src/main/java/org/openapitools/codegen/templating/TemplateNotFoundException.java
@@ -30,7 +30,7 @@ public class TemplateNotFoundException extends RuntimeException {
      * @param message the detail message (which is saved for later retrieval
      *                by the {@link #getMessage()} method).
      * @param cause   the cause (which is saved for later retrieval by the
-     *                {@link #getCause()} method).  (A <tt>null</tt> value is
+     *                {@link #getCause()} method).  (A <code>null</code> value is
      *                permitted, and indicates that the cause is nonexistent or
      *                unknown.)
      * @since 1.4
@@ -41,13 +41,13 @@ public class TemplateNotFoundException extends RuntimeException {
 
     /**
      * Constructs a new runtime exception with the specified cause and a
-     * detail message of <tt>(cause==null ? null : cause.toString())</tt>
+     * detail message of <code>(cause==null ? null : cause.toString())</code>
      * (which typically contains the class and detail message of
-     * <tt>cause</tt>).  This constructor is useful for runtime exceptions
+     * <code>cause</code>).  This constructor is useful for runtime exceptions
      * that are little more than wrappers for other throwables.
      *
      * @param cause the cause (which is saved for later retrieval by the
-     *              {@link #getCause()} method).  (A <tt>null</tt> value is
+     *              {@link #getCause()} method).  (A <code>null</code> value is
      *              permitted, and indicates that the cause is nonexistent or
      *              unknown.)
      * @since 1.4
