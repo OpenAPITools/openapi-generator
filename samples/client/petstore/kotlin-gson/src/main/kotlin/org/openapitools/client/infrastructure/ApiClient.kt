1cd7221a7dff3871ebbc8ec210cb5e7010f8b04f
Adam Kaplan <adkapx@gmail.com>
2020-03-02 10:34:13 -0500

    Update Kotlin tests

--- samples/client/petstore/kotlin-gson/src/main/kotlin/org/openapitools/client/infrastructure/ApiClient.kt
+++ samples/client/petstore/kotlin-gson/src/main/kotlin/org/openapitools/client/infrastructure/ApiClient.kt
@@ -173,12 +173,6 @@ open class ApiClient(val baseUrl: String) {
     }
 
     protected inline fun <reified T: Any> parseDateToQueryString(value : T): String {
-        /*
-        .replace("\"", "") converts the json object string to an actual string for the query parameter.
-        The moshi or gson adapter allows a more generic solution instead of trying to use a native
-        formatter. It also easily allows to provide a simple way to define a custom date format pattern
-        inside a gson/moshi adapter.
-        */
-        return Serializer.gson.toJson(value, T::class.java).replace("\"", "")
+        return value.toString()
     }
 }
