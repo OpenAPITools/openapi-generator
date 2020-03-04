64a94d6d99465d3538ebe9b1995ba4f3fd067b58
William Cheng <wing328hk@gmail.com>
2020-03-04 14:57:10 +0800

    update php openapi3 petstore sample

--- samples/openapi3/client/petstore/php/OpenAPIClient-php/lib/Api/PetApi.php
+++ samples/openapi3/client/petstore/php/OpenAPIClient-php/lib/Api/PetApi.php
@@ -804,7 +804,7 @@ class PetApi
 
         // query params
         if (is_array($status)) {
-            $status = ObjectSerializer::serializeCollection($status, 'csv', true);
+            $status = ObjectSerializer::serializeCollection($status, 'form', true);
         }
         if ($status !== null) {
             $queryParams['status'] = $status;
@@ -1082,7 +1082,7 @@ class PetApi
 
         // query params
         if (is_array($tags)) {
-            $tags = ObjectSerializer::serializeCollection($tags, 'csv', true);
+            $tags = ObjectSerializer::serializeCollection($tags, 'form', true);
         }
         if ($tags !== null) {
             $queryParams['tags'] = $tags;
