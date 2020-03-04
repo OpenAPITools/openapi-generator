3e5310c45faf5c5fbedf8342ba0872cd35251062
Victor Frank <vfrank@lumeris.com>
2020-03-03 20:41:01 -0600

    Revert "remove extra files from building"
    This reverts commit 7f80f961ef0cd0e50b9d2bb856a3703d5b821640.

---
+++ samples/client/petstore/kotlin/bin/org/openapitools/client/infrastructure/Errors.kt
@@ -0,0 +1,42 @@
+@file:Suppress("unused")
+package org.openapitools.client.infrastructure
+
+import java.lang.RuntimeException
+
+open class ClientException : RuntimeException {
+
+    /**
+     * Constructs an [ClientException] with no detail message.
+     */
+    constructor() : super()
+
+    /**
+     * Constructs an [ClientException] with the specified detail message.
+
+     * @param   message   the detail message.
+     */
+    constructor(message: kotlin.String) : super(message)
+
+    companion object {
+        private const val serialVersionUID: Long = 123L
+    }
+}
+
+open class ServerException : RuntimeException {
+
+    /**
+     * Constructs an [ServerException] with no detail message.
+     */
+    constructor() : super()
+
+    /**
+     * Constructs an [ServerException] with the specified detail message.
+
+     * @param   message   the detail message.
+     */
+    constructor(message: kotlin.String) : super(message)
+
+    companion object {
+        private const val serialVersionUID: Long = 456L
+    }
+}
