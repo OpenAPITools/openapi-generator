0d842d3a7fcb1abee507da03a68f2914f1169ec2
William Cheng <wing328hk@gmail.com>
2020-03-06 18:30:21 +0800

    update package

--- CI/circle_parallel.sh
+++ CI/circle_parallel.sh
@@ -55,6 +55,7 @@ else
   java -version
 
   # install dart2
+  sudo apt-get update
   sudo apt-get install apt-transport-https
   sudo sh -c 'wget -qO- https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -'
   sudo sh -c 'wget -qO- https://storage.googleapis.com/download.dartlang.org/linux/debian/dart_stable.list > /etc/apt/sources.list.d/dart_stable.list'
