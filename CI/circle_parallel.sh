e0a3a436829a60c42ee8d152552d57b5fac71f8d
William Cheng <wing328hk@gmail.com>
2020-03-06 21:16:09 +0800

    fix dart installation

--- CI/circle_parallel.sh
+++ CI/circle_parallel.sh
@@ -59,6 +59,7 @@ else
   sudo apt-get install apt-transport-https
   sudo sh -c 'wget -qO- https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -'
   sudo sh -c 'wget -qO- https://storage.googleapis.com/download.dartlang.org/linux/debian/dart_stable.list > /etc/apt/sources.list.d/dart_stable.list'
+  sudo apt-get update
   sudo apt-get install dart
   export PATH="$PATH:/usr/lib/dart/bin"
 
