1db9be5e1c3bd4c08156ec4c42544e20b69c1334
Sebastien Rosset (serosset) <serosset@cisco.com>
2020-03-09 08:13:08 -0700

    Add more troubleshooting information

--- bin/utils/test-fake-petstore-for-all.sh
+++ bin/utils/test-fake-petstore-for-all.sh
@@ -13,16 +13,18 @@ do
     if eval java -jar ${executable} generate -i modules/openapi-generator/src/test/resources/2_0/petstore-with-fake-endpoints-models-for-testing.yaml -g ${GENERATOR} -o /tmp/openapi-generator-test-fake-petstore/2.0/${GENERATOR} > /dev/null 2>&1; then
       echo "[OAS 2.0] Executed ${GENERATOR} successfully!"
     else
-      echo "ERROR: Failed to run ${GENERATOR}"
+      echo "ERROR: Failed to run ${GENERATOR}. Running command again with logging enabled"
       echo "java -jar ${executable} generate -i modules/openapi-generator/src/test/resources/2_0/petstore-with-fake-endpoints-models-for-testing.yaml -g ${GENERATOR} -o /tmp/openapi-generator-test-fake-petstore/2.0/${GENERATOR}"
+      echo "Exit status: $?"
       exit 1
     fi
 
     if eval java -jar ${executable} generate -i modules/openapi-generator/src/test/resources/3_0/petstore-with-fake-endpoints-models-for-testing.yaml -g ${GENERATOR} -o /tmp/openapi-generator-test-fake-petstore/3.0/${GENERATOR} > /dev/null 2>&1; then
       echo "[OAS 3.0] Executed ${GENERATOR} successfully!"
     else
-      echo "ERROR: Failed to run ${GENERATOR}"
+      echo "ERROR: Failed to run ${GENERATOR}. Running command again with logging enabled"
       echo "java -jar ${executable} generate -i modules/openapi-generator/src/test/resources/3_0/petstore-with-fake-endpoints-models-for-testing.yaml -g ${GENERATOR} -o /tmp/openapi-generator-test-fake-petstore/3.0/${GENERATOR}"
+      echo "Exit status: $?"
       exit 1
     fi
 done
