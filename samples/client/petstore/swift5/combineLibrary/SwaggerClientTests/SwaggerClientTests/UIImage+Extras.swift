f2385aa683de89f7d6b213f4e1580f26e0c734c3
Bruno Coelho <4brunu@gmail.com>
2020-03-11 16:08:30 +0000

    [swift] add unit tests for file upload

---
+++ samples/client/petstore/swift5/combineLibrary/SwaggerClientTests/SwaggerClientTests/UIImage+Extras.swift
@@ -0,0 +1,23 @@
+//
+//  UIImage+Extras.swift
+//  SwaggerClientTests
+//
+//  Created by Bruno Coelho on 11/03/2020.
+//  Copyright © 2020 Swagger. All rights reserved.
+//
+
+import UIKit
+
+extension UIImage {
+    convenience init?(color: UIColor, size: CGSize = CGSize(width: 1, height: 1)) {
+        let rect = CGRect(origin: .zero, size: size)
+        UIGraphicsBeginImageContextWithOptions(rect.size, false, 0.0)
+        color.setFill()
+        UIRectFill(rect)
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        guard let cgImage = image?.cgImage else { return nil }
+        self.init(cgImage: cgImage)
+    }    
+}
