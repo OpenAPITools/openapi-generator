79f034309835ee717a8ded693bdd96b254f2b3eb
larshp <larshp@hotmail.com>
2020-03-17 14:02:13 +0100

    typescript-fetch: interfaces, first draft

--- samples/client/petstore/typescript-fetch/builds/with-interfaces/apis/PetApi.ts
+++ samples/client/petstore/typescript-fetch/builds/with-interfaces/apis/PetApi.ts
@@ -61,9 +61,145 @@ export interface UploadFileRequest {
 }
 
 /**
+ * PetApi - interface
+ * @export
+ * @interface PetApi
+ */
+export interface PetApiInterface {
+    /**
+     * 
+     * @summary Add a new pet to the store
+     * @param {Pet} body Pet object that needs to be added to the store
+     * @param {*} [options] Override http request option.
+     * @throws {RequiredError}
+     * @memberof PetApiInterface
+     */
+    addPetRaw(requestParameters: AddPetRequest): Promise<runtime.ApiResponse<void>>;
+
+    /**
+     * Add a new pet to the store
+     */
+    addPet(requestParameters: AddPetRequest): Promise<void>;
+
+    /**
+     * 
+     * @summary Deletes a pet
+     * @param {number} petId Pet id to delete
+     * @param {string} [apiKey] 
+     * @param {*} [options] Override http request option.
+     * @throws {RequiredError}
+     * @memberof PetApiInterface
+     */
+    deletePetRaw(requestParameters: DeletePetRequest): Promise<runtime.ApiResponse<void>>;
+
+    /**
+     * Deletes a pet
+     */
+    deletePet(requestParameters: DeletePetRequest): Promise<void>;
+
+    /**
+     * Multiple status values can be provided with comma separated strings
+     * @summary Finds Pets by status
+     * @param {Array<'available' | 'pending' | 'sold'>} status Status values that need to be considered for filter
+     * @param {*} [options] Override http request option.
+     * @throws {RequiredError}
+     * @memberof PetApiInterface
+     */
+    findPetsByStatusRaw(requestParameters: FindPetsByStatusRequest): Promise<runtime.ApiResponse<Array<Pet>>>;
+
+    /**
+     * Multiple status values can be provided with comma separated strings
+     * Finds Pets by status
+     */
+    findPetsByStatus(requestParameters: FindPetsByStatusRequest): Promise<Array<Pet>>;
+
+    /**
+     * Multiple tags can be provided with comma separated strings. Use tag1, tag2, tag3 for testing.
+     * @summary Finds Pets by tags
+     * @param {Array<string>} tags Tags to filter by
+     * @param {*} [options] Override http request option.
+     * @throws {RequiredError}
+     * @memberof PetApiInterface
+     */
+    findPetsByTagsRaw(requestParameters: FindPetsByTagsRequest): Promise<runtime.ApiResponse<Array<Pet>>>;
+
+    /**
+     * Multiple tags can be provided with comma separated strings. Use tag1, tag2, tag3 for testing.
+     * Finds Pets by tags
+     */
+    findPetsByTags(requestParameters: FindPetsByTagsRequest): Promise<Array<Pet>>;
+
+    /**
+     * Returns a single pet
+     * @summary Find pet by ID
+     * @param {number} petId ID of pet to return
+     * @param {*} [options] Override http request option.
+     * @throws {RequiredError}
+     * @memberof PetApiInterface
+     */
+    getPetByIdRaw(requestParameters: GetPetByIdRequest): Promise<runtime.ApiResponse<Pet>>;
+
+    /**
+     * Returns a single pet
+     * Find pet by ID
+     */
+    getPetById(requestParameters: GetPetByIdRequest): Promise<Pet>;
+
+    /**
+     * 
+     * @summary Update an existing pet
+     * @param {Pet} body Pet object that needs to be added to the store
+     * @param {*} [options] Override http request option.
+     * @throws {RequiredError}
+     * @memberof PetApiInterface
+     */
+    updatePetRaw(requestParameters: UpdatePetRequest): Promise<runtime.ApiResponse<void>>;
+
+    /**
+     * Update an existing pet
+     */
+    updatePet(requestParameters: UpdatePetRequest): Promise<void>;
+
+    /**
+     * 
+     * @summary Updates a pet in the store with form data
+     * @param {number} petId ID of pet that needs to be updated
+     * @param {string} [name] Updated name of the pet
+     * @param {string} [status] Updated status of the pet
+     * @param {*} [options] Override http request option.
+     * @throws {RequiredError}
+     * @memberof PetApiInterface
+     */
+    updatePetWithFormRaw(requestParameters: UpdatePetWithFormRequest): Promise<runtime.ApiResponse<void>>;
+
+    /**
+     * Updates a pet in the store with form data
+     */
+    updatePetWithForm(requestParameters: UpdatePetWithFormRequest): Promise<void>;
+
+    /**
+     * 
+     * @summary uploads an image
+     * @param {number} petId ID of pet to update
+     * @param {string} [additionalMetadata] Additional data to pass to server
+     * @param {Blob} [file] file to upload
+     * @param {*} [options] Override http request option.
+     * @throws {RequiredError}
+     * @memberof PetApiInterface
+     */
+    uploadFileRaw(requestParameters: UploadFileRequest): Promise<runtime.ApiResponse<ModelApiResponse>>;
+
+    /**
+     * uploads an image
+     */
+    uploadFile(requestParameters: UploadFileRequest): Promise<ModelApiResponse>;
+
+}
+
+/**
  * no description
  */
-export class PetApi extends runtime.BaseAPI {
+export class PetApi extends runtime.BaseAPI implements PetApiInterface {
 
     /**
      * Add a new pet to the store
