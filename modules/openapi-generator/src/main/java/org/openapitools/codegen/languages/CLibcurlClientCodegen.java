5e9959529c8034912439611e1a3f4b73bf61cb41
Michele Albano <michele.albano@gmail.com>
2020-03-23 20:26:35 -0700

    First try to generate unit tests for the models of the C-libcurl client. Models into models are not supported yet.

--- modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/CLibcurlClientCodegen.java
+++ modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/CLibcurlClientCodegen.java
@@ -18,6 +18,7 @@ package org.openapitools.codegen.languages;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.media.ArraySchema;
 import io.swagger.v3.oas.models.media.Schema;
 import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.lang3.StringUtils;
@@ -87,7 +88,7 @@ public class CLibcurlClientCodegen extends DefaultCodegen implements CodegenConf
         embeddedTemplateDir = templateDir = "C-libcurl";
 
         // TODO add auto-generated test files
-        //modelTestTemplateFiles.put("model_test.mustache", ".c");
+        modelTestTemplateFiles.put("model_test.mustache", ".c");
         //apiTestTemplateFiles.put("api_test.mustache", ".c");
 
         // default HIDE_GENERATION_TIMESTAMP to true
@@ -319,6 +320,104 @@ public class CLibcurlClientCodegen extends DefaultCodegen implements CodegenConf
         }
 
         return null;
+    }
+
+    @Override
+    public String toExampleValue(Schema schema) {
+        String example = super.toExampleValue(schema);
+
+        if (ModelUtils.isNullType(schema) && null != example) {
+            // The 'null' type is allowed in OAS 3.1 and above. It is not supported by OAS 3.0.x,
+            // though this tooling supports it.
+            return "NULL";
+        }
+        // correct "&#39;"s into "'"s after toString()
+        if (ModelUtils.isStringSchema(schema) && schema.getDefault() != null) {
+            example = (String) schema.getDefault();
+        }
+        if (StringUtils.isNotBlank(example) && !"null".equals(example)) {
+            if (ModelUtils.isStringSchema(schema)) {
+                example = "\"" + example + "\"";
+            }
+            return example;
+        }
+
+        if (schema.getEnum() != null && !schema.getEnum().isEmpty()) {
+        // Enum case:
+            example = schema.getEnum().get(0).toString();
+/*            if (ModelUtils.isStringSchema(schema)) {
+                example = "'" + escapeText(example) + "'";
+            }*/
+            if (null == example)
+                LOGGER.warn("Empty enum. Cannot built an example!");
+
+            return example;
+        } else if (null != schema.get$ref()) {
+        // $ref case:
+            Map<String, Schema> allDefinitions = ModelUtils.getSchemas(this.openAPI);
+            String ref = ModelUtils.getSimpleRef(schema.get$ref());
+            if (allDefinitions != null) {
+                Schema refSchema = allDefinitions.get(ref);
+                if (null == refSchema) {
+                    return "None";
+                } else {
+                    String refTitle = refSchema.getTitle();
+                    if (StringUtils.isBlank(refTitle) || "null".equals(refTitle)) {
+                        refSchema.setTitle(ref);
+                    }
+                    return toExampleValue(refSchema);
+                }
+            } else {
+                LOGGER.warn("allDefinitions not defined in toExampleValue!\n");
+            }
+        }
+        if (ModelUtils.isDateSchema(schema)) {
+            example = "datetime.datetime.strptime('1975-12-30', '%Y-%m-%d').date()";
+            return example;
+        } else if (ModelUtils.isDateTimeSchema(schema)) {
+            example = "datetime.datetime.strptime('2013-10-20 19:20:30.00', '%Y-%m-%d %H:%M:%S.%f')";
+            return example;
+        } else if (ModelUtils.isBinarySchema(schema)) {
+            example = "bytes(b'blah')";
+            return example;
+        } else if (ModelUtils.isByteArraySchema(schema)) {
+            example = "YQ==";
+        } else if (ModelUtils.isStringSchema(schema)) {
+            // a BigDecimal:
+            if ("Number".equalsIgnoreCase(schema.getFormat())) {return "1";}
+            if (StringUtils.isNotBlank(schema.getPattern())) return "'a'"; // I cheat here, since it would be too complicated to generate a string from a regexp
+            int len = 0;
+            if (null != schema.getMinLength()) len = schema.getMinLength().intValue();
+            if (len < 1) len = 1;
+            example = "";
+            for (int i=0;i<len;i++) example += i;
+        } else if (ModelUtils.isIntegerSchema(schema)) {
+            if (schema.getMinimum() != null)
+                example = schema.getMinimum().toString();
+            else
+                example = "56";
+        } else if (ModelUtils.isNumberSchema(schema)) {
+            if (schema.getMinimum() != null)
+                example = schema.getMinimum().toString();
+            else
+                example = "1.337";
+        } else if (ModelUtils.isBooleanSchema(schema)) {
+            example = "1";
+        } else if (ModelUtils.isArraySchema(schema)) {
+            example = "list_create()";
+        } else if (ModelUtils.isMapSchema(schema)) {
+            example = "list_create()";
+        } else if (ModelUtils.isObjectSchema(schema)) {
+            return null; // models are managed at moustache level
+        } else {
+            LOGGER.warn("Type " + schema.getType() + " not handled properly in toExampleValue");
+        }
+
+        if (ModelUtils.isStringSchema(schema)) {
+            example = "\"" + escapeText(example) + "\"";
+        }
+
+        return example;
     }
 
     @Override
