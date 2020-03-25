5d4796c150c2ff67ba0d7498c00ea2d39cfd34f0
Sebastien Rosset (serosset) <serosset@cisco.com>
2020-03-25 15:06:33 -0700

    fix compilation error of generated code

--- modules/openapi-generator/src/main/java/org/openapitools/codegen/CodegenProperty.java
+++ modules/openapi-generator/src/main/java/org/openapitools/codegen/CodegenProperty.java
@@ -258,20 +258,20 @@ public class CodegenProperty implements Cloneable, IJsonSchemaValidationProperti
         this.name = name;
     }
 
-    public String getMin() {  
-        return min; 
-    } 
+    public String getMin() {
+        return min;
+    }
 
-    public void setMin(String min) {  
-        this.min = min; 
-    } 
+    public void setMin(String min) {
+        this.min = min;
+    }
 
-    public String getMax() {  
-        return max; 
-    } 
+    public String getMax() {
+        return max;
+    }
 
-    public void setMax(String max) {  
-        this.max = max; 
+    public void setMax(String max) {
+        this.max = max;
     }
 
     public String getDefaultValue() {
@@ -601,7 +601,7 @@ public class CodegenProperty implements Cloneable, IJsonSchemaValidationProperti
         sb.append(", datatypeWithEnum='").append(datatypeWithEnum).append('\'');
         sb.append(", dataFormat='").append(dataFormat).append('\'');
         sb.append(", name='").append(name).append('\'');
-        sb.append(", min='").append(min).append('\'');  
+        sb.append(", min='").append(min).append('\'');
         sb.append(", max='").append(max).append('\'');
         sb.append(", defaultValue='").append(defaultValue).append('\'');
         sb.append(", defaultValueWithParam='").append(defaultValueWithParam).append('\'');
@@ -731,7 +731,7 @@ public class CodegenProperty implements Cloneable, IJsonSchemaValidationProperti
                 Objects.equals(datatypeWithEnum, that.datatypeWithEnum) &&
                 Objects.equals(dataFormat, that.dataFormat) &&
                 Objects.equals(name, that.name) &&
-                Objects.equals(min, that.min) &&  
+                Objects.equals(min, that.min) &&
                 Objects.equals(max, that.max) &&
                 Objects.equals(defaultValue, that.defaultValue) &&
                 Objects.equals(defaultValueWithParam, that.defaultValueWithParam) &&
