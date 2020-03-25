625c734cfe024f0e0ee2675514e32b11e1f84e8d
Tomasz Prus <tomasz.prus@gmail.com>
2020-03-25 15:59:27 +0100

    [python/asyncio] explicitly close client session via async context manager (#5621)

--- samples/client/petstore/python-asyncio/tests/test_pet_api.py
+++ samples/client/petstore/python-asyncio/tests/test_pet_api.py
@@ -18,22 +18,13 @@ import petstore_api
 from petstore_api import Configuration
 from petstore_api.rest import ApiException
 
-from .util import id_gen
+from .util import id_gen, async_test
 
 import json
 
 import urllib3
 
 HOST = 'http://localhost:80/v2'
-
-
-def async_test(f):
-    def wrapper(*args, **kwargs):
-        coro = asyncio.coroutine(f)
-        future = coro(*args, **kwargs)
-        loop = asyncio.get_event_loop()
-        loop.run_until_complete(future)
-    return wrapper
 
 
 class TestPetApiTests(unittest.TestCase):
