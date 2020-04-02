42b0abb4369b97de53ee41ec60c121bd58fdd81f
Nikita Karnaukh <nikita.karnaukh@gmail.com>
2020-04-02 17:19:11 +0300

    Update and refactoring scripts for sample generation

--- bin/openapi3/kotlin-client-petstore-retrofit2-rx.sh
+++ bin/openapi3/kotlin-client-petstore-retrofit2-rx.sh
@@ -25,11 +25,13 @@ then
   mvn clean package
 fi
 
-export JAVA_OPTS="${JAVA_OPTS} -Xmx1024M -DloggerPath=conf/log4j.properties"
-ags="generate -i modules/openapi-generator/src/test/resources/3_0/petstore-with-fake-endpoints-models-for-testing.yaml -t modules/openapi-generator/src/main/resources/kotlin-client -g kotlin --artifact-id kotlin-petstore-rx2-client --library jvm-retrofit2 --additional-properties serializationLibrary=gson,dateLibrary=java8,serializableModel=true,useRxJava=true -o samples/openapi3/client/petstore/kotlin-jvm-retrofit2-rx $@"
+samplePath="samples/openapi3/client/petstore/kotlin-jvm-retrofit2-rx"
 
-echo "Cleaning previously generated files if any from samples/openapi3/client/petstore/kotlin"
-rm -rf samples/openapi3/client/petstore/kotlin
+export JAVA_OPTS="${JAVA_OPTS} -Xmx1024M -DloggerPath=conf/log4j.properties"
+ags="generate -i modules/openapi-generator/src/test/resources/3_0/petstore-with-fake-endpoints-models-for-testing.yaml -t modules/openapi-generator/src/main/resources/kotlin-client -g kotlin --artifact-id kotlin-petstore-rx-client --library jvm-retrofit2 --additional-properties serializationLibrary=gson,dateLibrary=java8,serializableModel=true,useRxJava=true -o $samplePath $@"
+
+echo "Cleaning previously generated files if any from samplePath"
+rm -rf $samplePath
 
 echo "Generating Kotling client..."
 java $JAVA_OPTS -jar $executable $ags
