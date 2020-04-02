6e6247e0fe237a52d6acdbf55303795abf1189b6
jonfreedman <jon.freedman@zoho.com>
2020-04-02 15:55:11 +0100

    need to iterate over authMethods in order to pull out name

--- samples/client/petstore/java/jersey1/src/main/java/org/openapitools/client/ApiClient.java
+++ samples/client/petstore/java/jersey1/src/main/java/org/openapitools/client/ApiClient.java
@@ -105,7 +105,7 @@ public class ApiClient {
 
     // Setup authentications (key: authentication name, value: authentication).
     authentications = new HashMap<String, Authentication>();
-    authentications.put("", new OAuth());
+    authentications.put("petstore_auth", new OAuth());
     // Prevent the authentications from being modified.
     authentications = Collections.unmodifiableMap(authentications);
 
