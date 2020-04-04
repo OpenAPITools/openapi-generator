37a1e4660cf03565e0bb0f2595bde7b7e3e7765f
William Cheng <wing328hk@gmail.com>
2020-04-04 23:37:09 +0800

    minor improvements to scala akka server

--- bin/windows/scala-akka-http-server-petstore.bat
+++ bin/windows/scala-akka-http-server-petstore.bat
@@ -5,6 +5,6 @@ If Not Exist %executable% (
 )
 
 REM set JAVA_OPTS=%JAVA_OPTS% -Xmx1024M -DloggerPath=conf/log4j.properties
-set ags=generate  --artifact-id "scala-akka-http-petstore-server" -i modules\openapi-generator\src\test\resources\2_0\petstore.yaml -g scala-akka-http -o samples\server\petstore\scala-akka-http
+set ags=generate  --artifact-id "scala-akka-http-petstore-server" -i modules\openapi-generator\src\test\resources\2_0\petstore.yaml -g scala-akka-http-server -o samples\server\petstore\scala-akka-http-server
 
 java %JAVA_OPTS% -jar %executable% %ags%
