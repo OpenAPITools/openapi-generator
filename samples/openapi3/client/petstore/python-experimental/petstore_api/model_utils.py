bad077997f95f4feb8587d9f442dab25bfffe458
Sebastien Rosset (serosset) <serosset@cisco.com>
2020-04-14 18:01:38 -0700

    use list instead of set

--- samples/openapi3/client/petstore/python-experimental/petstore_api/model_utils.py
+++ samples/openapi3/client/petstore/python-experimental/petstore_api/model_utils.py
@@ -147,10 +147,6 @@ class ModelSimple(OpenApiModel):
         """Returns the string representation of the model"""
         return str(self.value)
 
-    def __hash__(self):
-        """Returns the hash value of this object."""
-        return hash(self._data_store['value'])
-
     def __eq__(self, other):
         """Returns true if both objects are equal"""
         if not isinstance(other, self.__class__):
@@ -207,10 +203,6 @@ class ModelNormal(OpenApiModel):
     def to_str(self):
         """Returns the string representation of the model"""
         return pprint.pformat(self.to_dict())
-
-    def __hash__(self):
-        """Returns the hash value of this object."""
-        return hash(frozenset(self._data_store.items()))
 
     def __eq__(self, other):
         """Returns true if both objects are equal"""
@@ -312,10 +304,6 @@ class ModelComposed(OpenApiModel):
     def to_str(self):
         """Returns the string representation of the model"""
         return pprint.pformat(self.to_dict())
-
-    def __hash__(self):
-        """Returns the hash value of this object."""
-        return hash(frozenset(self._data_store.items()))
 
     def __eq__(self, other):
         """Returns true if both objects are equal"""
