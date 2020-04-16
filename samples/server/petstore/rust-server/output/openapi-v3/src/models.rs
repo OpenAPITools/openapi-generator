f80830fe81230b003cb11ccffede0a6a86cd141c
Andy.Yang <andy.yang.cn@outlook.com>
2020-04-16 17:12:29 +0800

    [Rust Server] Add test case yaml new generated code after fix self reference issue.

--- samples/server/petstore/rust-server/output/openapi-v3/src/models.rs
+++ samples/server/petstore/rust-server/output/openapi-v3/src/models.rs
@@ -550,7 +550,7 @@ pub struct Error {
 
     #[serde(rename = "innerError")]
     #[serde(skip_serializing_if="Option::is_none")]
-    pub inner_error: Option<models::Error>,
+    pub inner_error: Box<Option<models::Error>>,
 
 }
 
@@ -560,7 +560,7 @@ impl Error {
             code: code,
             retryable: retryable,
             message: message,
-            inner_error: None,
+            inner_error: Box::new(None),
         }
     }
 }
@@ -636,7 +636,7 @@ impl std::str::FromStr for Error {
             code: intermediate_rep.code.into_iter().next().ok_or("code missing in Error".to_string())?,
             retryable: intermediate_rep.retryable.into_iter().next().ok_or("retryable missing in Error".to_string())?,
             message: intermediate_rep.message.into_iter().next().ok_or("message missing in Error".to_string())?,
-            inner_error: intermediate_rep.inner_error.into_iter().next(),
+            inner_error: Box::new(intermediate_rep.inner_error.into_iter().next()),
         })
     }
 }
