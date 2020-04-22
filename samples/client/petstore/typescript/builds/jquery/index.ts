90f2fbbe9b5e680f93bd299f9283d5a4c7e41f6f
Bodo Graumann <mail@bodograumann.de>
2020-04-22 11:09:01 +0200

    Export server configurations

--- samples/client/petstore/typescript/builds/jquery/index.ts
+++ samples/client/petstore/typescript/builds/jquery/index.ts
@@ -3,6 +3,7 @@ export * from './auth/auth';
 export * from './models/all';
 export { Configuration} from './configuration'
 export * from './apis/exception';
+export * from './servers';
 
   export * from './types/PromiseAPI';
   export { PromiseMiddleware as Middleware } from './middleware';
