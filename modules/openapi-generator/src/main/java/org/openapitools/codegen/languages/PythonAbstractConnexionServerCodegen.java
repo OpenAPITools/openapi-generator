70c65c6536652ace2c0d4c58cb87467ace47332d
Mark Wilby <mark.wilby@gmail.com>
2020-04-27 08:35:23 +0200

    Moved definitions and common base functions into PythonAbstractCodegen.
    Changed imports so pyImport is no longer present and now the imports are
    managed by postProcessAllModels, which places them at the same level as
    model.

--- modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/PythonAbstractConnexionServerCodegen.java
+++ modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/PythonAbstractConnexionServerCodegen.java
@@ -24,12 +24,10 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.Operation;
 import io.swagger.v3.oas.models.PathItem;
 import io.swagger.v3.oas.models.PathItem.HttpMethod;
-import io.swagger.v3.oas.models.media.ArraySchema;
 import io.swagger.v3.oas.models.media.Schema;
 import io.swagger.v3.oas.models.parameters.Parameter;
 import io.swagger.v3.oas.models.parameters.RequestBody;
 import io.swagger.v3.oas.models.security.SecurityScheme;
-import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.openapitools.codegen.*;
 import org.openapitools.codegen.meta.features.DocumentationFeature;
@@ -43,92 +41,34 @@ import java.util.*;
 import static org.openapitools.codegen.utils.StringUtils.camelize;
 import static org.openapitools.codegen.utils.StringUtils.underscore;
 
-public class PythonAbstractConnexionServerCodegen extends DefaultCodegen implements CodegenConfig {
+public class PythonAbstractConnexionServerCodegen extends PythonAbstractCodegen {
     private static final Logger LOGGER = LoggerFactory.getLogger(PythonAbstractConnexionServerCodegen.class);
 
     public static final String CONTROLLER_PACKAGE = "controllerPackage";
     public static final String DEFAULT_CONTROLLER = "defaultController";
     public static final String SUPPORT_PYTHON2 = "supportPython2";
-    // nose is a python testing framework, we use pytest if USE_NOSE is unset
-    public static final String USE_NOSE = "useNose";
     public static final String PYTHON_SRC_ROOT = "pythonSrcRoot";
     static final String MEDIA_TYPE = "mediaType";
 
     protected int serverPort = 8080;
-    protected String packageName;
-    protected String packageVersion;
     protected String controllerPackage;
     protected String defaultController;
-    protected Map<Character, String> regexModifiers;
     protected boolean fixBodyName;
-    protected boolean useNose = Boolean.FALSE;
     protected String pythonSrcRoot;
 
     public PythonAbstractConnexionServerCodegen(String templateDirectory, boolean fixBodyNameValue) {
-        super();
+        super(templateDirectory);
 
         modifyFeatureSet(features -> features.includeDocumentationFeatures(DocumentationFeature.Readme));
 
         fixBodyName = fixBodyNameValue;
-        modelPackage = "models";
         testPackage = "test";
-
-        languageSpecificPrimitives.clear();
-        languageSpecificPrimitives.add("int");
-        languageSpecificPrimitives.add("float");
-        languageSpecificPrimitives.add("List");
-        languageSpecificPrimitives.add("Dict");
-        languageSpecificPrimitives.add("bool");
-        languageSpecificPrimitives.add("str");
-        languageSpecificPrimitives.add("datetime");
-        languageSpecificPrimitives.add("date");
-        languageSpecificPrimitives.add("file");
-        languageSpecificPrimitives.add("object");
-        languageSpecificPrimitives.add("byte");
-        languageSpecificPrimitives.add("bytearray");
-
-        typeMapping.clear();
-        typeMapping.put("integer", "int");
-        typeMapping.put("float", "float");
-        typeMapping.put("number", "float");
-        typeMapping.put("long", "int");
-        typeMapping.put("double", "float");
-        typeMapping.put("array", "List");
-        typeMapping.put("map", "Dict");
-        typeMapping.put("boolean", "bool");
-        typeMapping.put("string", "str");
-        typeMapping.put("date", "date");
-        typeMapping.put("DateTime", "datetime");
-        typeMapping.put("object", "object");
-        typeMapping.put("file", "file");
-        typeMapping.put("UUID", "str");
-        typeMapping.put("URI", "str");
-        typeMapping.put("byte", "bytearray");
-        typeMapping.put("ByteArray", "bytearray");
-
-        // from https://docs.python.org/3/reference/lexical_analysis.html#keywords
-        setReservedWordsLowerCase(
-                Arrays.asList(
-                        // @property
-                        "property",
-                        // python reserved words
-                        "and", "del", "from", "not", "while", "as", "elif", "global", "or", "with",
-                        "assert", "else", "if", "pass", "yield", "break", "except", "import",
-                        "print", "class", "exec", "in", "raise", "continue", "finally", "is",
-                        "return", "def", "for", "lambda", "try", "self", "None", "True", "False", "nonlocal"));
 
         // set the output folder here
         outputFolder = "generated-code/connexion";
 
         apiTemplateFiles.put("controller.mustache", ".py");
-        modelTemplateFiles.put("model.mustache", ".py");
         apiTestTemplateFiles().put("controller_test.mustache", ".py");
-
-        /*
-         * Template Location.  This is the location which templates will be read from.  The generator
-         * will use the resource stream to attempt to read the templates.
-         */
-        embeddedTemplateDir = templateDir = templateDirectory;
 
         /*
          * Additional Properties.  These values can be passed to the templates and
@@ -144,14 +84,6 @@ public class PythonAbstractConnexionServerCodegen extends DefaultCodegen impleme
         supportingFiles.add(new SupportingFile("README.mustache", "", "README.md"));
         supportingFiles.add(new SupportingFile("test-requirements.mustache", "", "test-requirements.txt"));
         supportingFiles.add(new SupportingFile("requirements.mustache", "", "requirements.txt"));
-
-        regexModifiers = new HashMap<Character, String>();
-        regexModifiers.put('i', "IGNORECASE");
-        regexModifiers.put('l', "LOCALE");
-        regexModifiers.put('m', "MULTILINE");
-        regexModifiers.put('s', "DOTALL");
-        regexModifiers.put('u', "UNICODE");
-        regexModifiers.put('x', "VERBOSE");
 
         cliOptions.add(new CliOption(CodegenConstants.PACKAGE_NAME, "python package name (convention: snake_case).")
                 .defaultValue("openapi_server"));
@@ -236,10 +168,6 @@ public class PythonAbstractConnexionServerCodegen extends DefaultCodegen impleme
         controllerPackage = packageName + "." + controllerPackage;
     }
 
-    public void setUseNose(String val) {
-        this.useNose = Boolean.valueOf(val);
-    }
-
     public void setPythonSrcRoot(String val) {
         String pySrcRoot;
         if (val == null) {
@@ -259,11 +187,11 @@ public class PythonAbstractConnexionServerCodegen extends DefaultCodegen impleme
         return outputFolder + File.separator + pythonSrcRoot;
     }
 
-    private static String packageToPath(String pkg) {
+    protected static String packageToPath(String pkg) {
         return pkg.replace(".", File.separator);
     }
 
-    private static String dropDots(String str) {
+    protected static String dropDots(String str) {
         return str.replaceAll("\\.", "_");
     }
 
@@ -308,25 +236,6 @@ public class PythonAbstractConnexionServerCodegen extends DefaultCodegen impleme
         return underscore(toApiName(name));
     }
 
-    @Override
-    public String toApiTestFilename(String name) {
-        return "test_" + toApiFilename(name);
-    }
-
-    /**
-     * Escapes a reserved word as defined in the `reservedWords` array. Handle escaping
-     * those terms here.  This logic is only called if a variable matches the reserved words
-     *
-     * @return the escaped term
-     */
-    @Override
-    public String escapeReservedWord(String name) {
-        if (this.reservedWordsMappings().containsKey(name)) {
-            return this.reservedWordsMappings().get(name);
-        }
-        return "_" + name; // add an underscore to the name
-    }
-
     /**
      * Location to write api files.  You can use the apiPackage() as defined when the class is
      * instantiated
@@ -341,34 +250,6 @@ public class PythonAbstractConnexionServerCodegen extends DefaultCodegen impleme
     public String modelFileFolder() {
         String pkgPath = modelPackage().replace('.', File.separatorChar);
         return pythonSrcOutputFolder() + pkgPath;
-    }
-
-    @Override
-    public String getTypeDeclaration(Schema p) {
-        if (ModelUtils.isArraySchema(p)) {
-            ArraySchema ap = (ArraySchema) p;
-            Schema inner = ap.getItems();
-            return getSchemaType(p) + "[" + getTypeDeclaration(inner) + "]";
-        } else if (ModelUtils.isMapSchema(p)) {
-            Schema inner = ModelUtils.getAdditionalProperties(p);
-            return getSchemaType(p) + "[str, " + getTypeDeclaration(inner) + "]";
-        }
-        return super.getTypeDeclaration(p);
-    }
-
-    @Override
-    public String getSchemaType(Schema p) {
-        String schemaType = super.getSchemaType(p);
-        String type = null;
-        if (typeMapping.containsKey(schemaType)) {
-            type = typeMapping.get(schemaType);
-            if (languageSpecificPrimitives.contains(type)) {
-                return type;
-            }
-        } else {
-            type = toModelName(schemaType);
-        }
-        return type;
     }
 
     @Override
@@ -607,83 +488,6 @@ public class PythonAbstractConnexionServerCodegen extends DefaultCodegen impleme
     }
 
     @Override
-    public String toVarName(String name) {
-        // sanitize name
-        name = sanitizeName(name); // FIXME: a parameter should not be assigned. Also declare the methods parameters as 'final'.
-
-        // remove dollar sign
-        name = name.replaceAll("$", "");
-
-        // if it's all uppper case, convert to lower case
-        if (name.matches("^[A-Z_]*$")) {
-            name = name.toLowerCase(Locale.ROOT);
-        }
-
-        // underscore the variable name
-        // petId => pet_id
-        name = underscore(name);
-
-        // remove leading underscore
-        name = name.replaceAll("^_*", "");
-
-        // for reserved word or word starting with number, append _
-        if (isReservedWord(name) || name.matches("^\\d.*")) {
-            name = escapeReservedWord(name);
-        }
-
-        return name;
-    }
-
-    @Override
-    public String toParamName(String name) {
-        // to avoid conflicts with 'callback' parameter for async call
-        if ("callback".equals(name)) {
-            return "param_callback";
-        }
-
-        // should be the same as variable name
-        return toVarName(name);
-    }
-
-    @Override
-    public String toModelFilename(String name) {
-        // underscore the model file name
-        // PhoneNumber => phone_number
-        return underscore(dropDots(toModelName(name)));
-    }
-
-    @Override
-    public String toModelName(String name) {
-        name = sanitizeName(name); // FIXME: a parameter should not be assigned. Also declare the methods parameters as 'final'.
-        // remove dollar sign
-        name = name.replaceAll("$", "");
-
-        // model name cannot use reserved keyword, e.g. return
-        if (isReservedWord(name)) {
-            LOGGER.warn(name + " (reserved word) cannot be used as model name. Renamed to " + camelize("model_" + name));
-            name = "model_" + name; // e.g. return => ModelReturn (after camelize)
-        }
-
-        // model name starts with number
-        if (name.matches("^\\d.*")) {
-            LOGGER.warn(name + " (model name starts with number) cannot be used as model name. Renamed to " + camelize("model_" + name));
-            name = "model_" + name; // e.g. 200Response => Model200Response (after camelize)
-        }
-
-        if (!StringUtils.isEmpty(modelNamePrefix)) {
-            name = modelNamePrefix + "_" + name;
-        }
-
-        if (!StringUtils.isEmpty(modelNameSuffix)) {
-            name = name + "_" + modelNameSuffix;
-        }
-
-        // camelize the model name
-        // phone_number => PhoneNumber
-        return camelize(name);
-    }
-
-    @Override
     public String toOperationId(String operationId) {
         // throw exception if method name is empty (should not occur as an auto-generated method name will be used)
         if (StringUtils.isEmpty(operationId)) {
@@ -826,92 +630,6 @@ public class PythonAbstractConnexionServerCodegen extends DefaultCodegen impleme
         p.example = example;
     }
 
-    public void setPackageName(String packageName) {
-        this.packageName = packageName;
-    }
-
-    public void setPackageVersion(String packageVersion) {
-        this.packageVersion = packageVersion;
-    }
-
-    public String packagePath() {
-        String pkgPath = packageName.replace('.', File.separatorChar);
-        return pythonSrcRoot + pkgPath;
-    }
-
-    @Override
-    public String escapeQuotationMark(String input) {
-        // remove ' to avoid code injection
-        return input.replace("'", "");
-    }
-
-    @Override
-    public String escapeUnsafeCharacters(String input) {
-        // remove multiline comment
-        return input.replace("'''", "'_'_'");
-    }
-
-    @Override
-    public String toModelImport(String name) {
-        String modelImport;
-        if (StringUtils.startsWithAny(name, "import", "from")) {
-            modelImport = name;
-        } else {
-            modelImport = "from ";
-            if (!"".equals(modelPackage())) {
-                modelImport += modelPackage() + ".";
-            }
-            modelImport += toModelFilename(name) + " import " + name;
-        }
-        return modelImport;
-    }
-
-    @Override
-    public void postProcessModelProperty(CodegenModel model, CodegenProperty property) {
-        if (StringUtils.isNotEmpty(property.pattern)) {
-            addImport(model, "import re");
-        }
-        postProcessPattern(property.pattern, property.vendorExtensions);
-    }
-
-    @Override
-    public Map<String, Object> postProcessModels(Map<String, Object> objs) {
-        // process enum in models
-        return postProcessModelsEnum(objs);
-    }
-
-    @Override
-    public Map<String, Object> postProcessAllModels(Map<String, Object> objs) {
-        Map<String, Object> result = super.postProcessAllModels(objs);
-        for (Map.Entry<String, Object> entry : result.entrySet()) {
-            Map<String, Object> inner = (Map<String, Object>) entry.getValue();
-            List<Map<String, Object>> models = (List<Map<String, Object>>) inner.get("models");
-            for (Map<String, Object> mo : models) {
-                CodegenModel cm = (CodegenModel) mo.get("model");
-                // Add additional filename information for imports
-                mo.put("pyImports", toPyImports(cm, cm.imports));
-            }
-        }
-        return result;
-    }
-
-    private List<Map<String, String>> toPyImports(CodegenModel cm, Set<String> imports) {
-        List<Map<String, String>> pyImports = new ArrayList<>();
-        for (String im : imports) {
-            if (!im.equals(cm.classname)) {
-                HashMap<String, String> pyImport = new HashMap<>();
-                pyImport.put("import", toModelImport(im));
-                pyImports.add(pyImport);
-            }
-        }
-        return pyImports;
-    }
-
-    @Override
-    public void postProcessParameter(CodegenParameter parameter) {
-        postProcessPattern(parameter.pattern, parameter.vendorExtensions);
-    }
-
     @Override
     public Map<String, Object> postProcessOperationsWithModels(Map<String, Object> objs, List<Object> allModels) {
         Map<String, Object> operations = (Map<String, Object>) objs.get("operations");
@@ -970,71 +688,4 @@ public class PythonAbstractConnexionServerCodegen extends DefaultCodegen impleme
         }
         return objs;
     }
-
-    /*
-     * The openapi pattern spec follows the Perl convention and style of modifiers. Python
-     * does not support this in as natural a way so it needs to convert it. See
-     * https://docs.python.org/2/howto/regex.html#compilation-flags for details.
-     */
-    public void postProcessPattern(String pattern, Map<String, Object> vendorExtensions) {
-        if (pattern != null) {
-            int i = pattern.lastIndexOf('/');
-
-            //Must follow Perl /pattern/modifiers convention
-            if (pattern.charAt(0) != '/' || i < 2) {
-                throw new IllegalArgumentException("Pattern must follow the Perl "
-                        + "/pattern/modifiers convention. " + pattern + " is not valid.");
-            }
-
-            String regex = pattern.substring(1, i).replace("'", "\\'");
-            List<String> modifiers = new ArrayList<String>();
-
-            for (char c : pattern.substring(i).toCharArray()) {
-                if (regexModifiers.containsKey(c)) {
-                    String modifier = regexModifiers.get(c);
-                    modifiers.add(modifier);
-                }
-            }
-
-            vendorExtensions.put("x-regex", regex);
-            vendorExtensions.put("x-modifiers", modifiers);
-        }
-    }
-
-    @Override
-    public void postProcessFile(File file, String fileType) {
-        if (file == null) {
-            return;
-        }
-        String pythonPostProcessFile = System.getenv("PYTHON_POST_PROCESS_FILE");
-        if (StringUtils.isEmpty(pythonPostProcessFile)) {
-            return; // skip if PYTHON_POST_PROCESS_FILE env variable is not defined
-        }
-
-        // only process files with py extension
-        if ("py".equals(FilenameUtils.getExtension(file.toString()))) {
-            String command = pythonPostProcessFile + " " + file.toString();
-            try {
-                Process p = Runtime.getRuntime().exec(command);
-                int exitValue = p.waitFor();
-                if (exitValue != 0) {
-                    LOGGER.error("Error running the command ({}). Exit value: {}", command, exitValue);
-                } else {
-                    LOGGER.info("Successfully executed: " + command);
-                }
-            } catch (Exception e) {
-                LOGGER.error("Error running the command ({}). Exception: {}", command, e.getMessage());
-            }
-        }
-    }
-
-    /*
-     * We don't want to run `escapeText` on the pattern
-     * but forward it directly to the Python implementation.
-     */
-    @Override
-    public String toRegularExpression(String pattern) {
-        return addRegularExpressionDelimiter(pattern);
-    }
-
 }
