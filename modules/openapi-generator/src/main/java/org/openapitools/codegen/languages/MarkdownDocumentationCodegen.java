b22f250d43f2c9022992b15d7f57de69f7fab3f3
TetianaServirog <tetiana.servirog@gmail.com>
2020-04-28 19:56:52 +0300

    [BUG][MARKDOWN] fix parameter and property names (#6089)

--- modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/MarkdownDocumentationCodegen.java
+++ modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/MarkdownDocumentationCodegen.java
@@ -1,20 +1,17 @@
 package org.openapitools.codegen.languages;
 
-import org.openapitools.codegen.*;
-import io.swagger.models.properties.ArrayProperty;
-import io.swagger.models.properties.MapProperty;
-import io.swagger.models.properties.Property;
-import io.swagger.models.parameters.Parameter;
-
-import java.io.File;
-import java.util.*;
-
-import org.apache.commons.lang3.StringUtils;
-
+import org.openapitools.codegen.CodegenConfig;
+import org.openapitools.codegen.CodegenType;
+import org.openapitools.codegen.DefaultCodegen;
+import org.openapitools.codegen.SupportingFile;
 import org.openapitools.codegen.meta.GeneratorMetadata;
 import org.openapitools.codegen.meta.Stability;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.File;
+
+import static org.openapitools.codegen.utils.StringUtils.escape;
 
 public class MarkdownDocumentationCodegen extends DefaultCodegen implements CodegenConfig {
     public static final String PROJECT_NAME = "projectName";
@@ -48,5 +45,38 @@ public class MarkdownDocumentationCodegen extends DefaultCodegen implements Code
         modelPackage = File.separator + "Models";
         supportingFiles.add(new SupportingFile("README.mustache", "", "README.md"));
         // TODO: Fill this out.
+    }
+
+    @Override
+    protected void initalizeSpecialCharacterMapping() {
+        // escape only those symbols that can mess up markdown
+        specialCharReplacements.put("\\", "\\\\");
+        specialCharReplacements.put("/", "\\/");
+        specialCharReplacements.put("`", "\\`");
+        specialCharReplacements.put("*", "\\*");
+        specialCharReplacements.put("_", "\\_");
+        specialCharReplacements.put("[", "\\[");
+        specialCharReplacements.put("]", "\\]");
+
+        // todo Current markdown api and model mustache templates display properties and parameters in tables. It can be
+        // commonly escaped with a backslash (e.g. GFM supports this). However, in some cases it may be necessary to
+        // choose a different approach.
+        specialCharReplacements.put("|", "\\|");
+    }
+
+    /**
+     * Works identically to {@link DefaultCodegen#toParamName(String)} but doesn't camelize.
+     *
+     * @param name Codegen property object
+     * @return the sanitized parameter name
+     */
+    @Override
+    public String toParamName(String name) {
+        if (reservedWords.contains(name)) {
+            return escapeReservedWord(name);
+        } else if (((CharSequence) name).chars().anyMatch(character -> specialCharReplacements.keySet().contains("" + ((char) character)))) {
+            return escape(name, specialCharReplacements, null, null);
+        }
+        return name;
     }
 }
