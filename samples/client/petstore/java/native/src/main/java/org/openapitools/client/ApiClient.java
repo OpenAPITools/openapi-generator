a4e39a87e33b1ee526c7e3c328defe1811e8dc9f
William Cheng <wing328hk@gmail.com>
2020-05-03 12:32:26 +0800

    Revert "trigger build failure"
    This reverts commit e52ad39ec1c6508864fd7c3c8513e56a2eef6539.

--- samples/client/petstore/java/native/src/main/java/org/openapitools/client/ApiClient.java
+++ samples/client/petstore/java/native/src/main/java/org/openapitools/client/ApiClient.java
@@ -1,4 +1,3 @@
-fjakl;sdjfkl;adsfj
 /*
  * OpenAPI Petstore
  * This spec is mainly for testing Petstore server and contains fake endpoints, models. Please do not use this for any other purpose. Special characters: \" \\
