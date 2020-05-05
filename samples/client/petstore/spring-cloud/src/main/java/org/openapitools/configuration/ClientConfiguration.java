89278e4d5b3e41d0b00cbdc59dbbc3a40ab1d454
Melzer Dennis (BCI/ESW17) <dennis.melzer@de.bosch.com>
2020-05-05 15:08:14 +0200

    Add import for test
    #6171

--- samples/client/petstore/spring-cloud/src/main/java/org/openapitools/configuration/ClientConfiguration.java
+++ samples/client/petstore/spring-cloud/src/main/java/org/openapitools/configuration/ClientConfiguration.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.cloud.security.oauth2.client.feign.OAuth2FeignRequestInterceptor;
 import org.springframework.security.oauth2.client.DefaultOAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
 import org.springframework.security.oauth2.client.token.grant.implicit.ImplicitResourceDetails;
 
 @Configuration
