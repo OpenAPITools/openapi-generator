b587d59ceea3e2dce0378d28c9e681ee603bbcf2
Sebastien Rosset (serosset) <serosset@cisco.com>
2020-05-08 14:26:26 -0700

    fix formatting issues. Finally figured out how to check formatting in local workspace

--- samples/client/petstore/python-experimental/petstore_api/model_utils.py
+++ samples/client/petstore/python-experimental/petstore_api/model_utils.py
@@ -138,8 +138,9 @@ class OpenApiModel(object):
         # Build a list containing all oneOf and anyOf descendants.
         oneof_anyof_classes = None
         if cls._composed_schemas is not None:
-            oneof_anyof_classes = (cls._composed_schemas.get('oneOf', ()) +
-                    cls._composed_schemas.get('anyOf', ()))
+            oneof_anyof_classes = (
+                cls._composed_schemas.get('oneOf', ()) +
+                cls._composed_schemas.get('anyOf', ()))
         if (oneof_anyof_classes and none_type in oneof_anyof_classes and
                 len(args) == 1 and args[0] is None):
             # The input data is the 'null' value AND one of the oneOf/anyOf children
