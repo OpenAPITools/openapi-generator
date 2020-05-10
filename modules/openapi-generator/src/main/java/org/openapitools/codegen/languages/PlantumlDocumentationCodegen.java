ad7b711e39a44a0a799955b22ab19ff4e7cc00f3
Patrick Burls <pburls@gmail.com>
2020-05-10 19:58:15 +0100

    add new entities data to SupportingFileData

--- modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/PlantumlDocumentationCodegen.java
+++ modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/PlantumlDocumentationCodegen.java
@@ -1,16 +1,10 @@
 package org.openapitools.codegen.languages;
 
 import org.openapitools.codegen.*;
-import io.swagger.models.properties.ArrayProperty;
-import io.swagger.models.properties.MapProperty;
-import io.swagger.models.properties.Property;
-import io.swagger.models.parameters.Parameter;
 
 import java.io.File;
 import java.util.*;
 import java.util.stream.Collectors;
-
-import org.apache.commons.lang3.StringUtils;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -40,26 +34,63 @@ public class PlantumlDocumentationCodegen extends DefaultCodegen implements Code
         supportingFiles.add(new SupportingFile("schemas.mustache", "", "schemas.plantuml"));
     }
 
+//    @Override
+//    public Map<String, Object> postProcessModels(Map<String, Object> objs) {
+//        List<Map<String, Object>> models = (List<Map<String, Object>>) objs.get("models");
+//        for (Map<String, Object> model : models) {
+//            Object v = model.get("model");
+//            if (v instanceof CodegenModel) {
+//                CodegenModel m = (CodegenModel) v;
+//                if (m.interfaces != null) {
+//                    m.interfaces.removeIf(interfaceName -> interfaceName.endsWith("AllOf"));
+//                }
+//            }
+//        }
+//        return objs;
+//    }
+
+//    @Override
+//    public Map<String, Object> postProcessAllModels(Map<String, Object> objs) {
+//        List<String> keysToRemove = objs.keySet().stream().filter(key -> key.endsWith("_allOf")).collect(Collectors.toList());
+//        keysToRemove.forEach(key -> objs.remove(key));
+//
+//        return super.postProcessAllModels(objs);
+//    }
+
+    @SuppressWarnings("unchecked")
     @Override
-    public Map<String, Object> postProcessModels(Map<String, Object> objs) {
-        List<Map<String, Object>> models = (List<Map<String, Object>>) objs.get("models");
-        for (Map<String, Object> model : models) {
-            Object v = model.get("model");
-            if (v instanceof CodegenModel) {
-                CodegenModel m = (CodegenModel) v;
-                if (m.interfaces != null) {
-                    m.interfaces.removeIf(interfaceName -> interfaceName.endsWith("AllOf"));
-                }
-            }
-        }
-        return objs;
+    public Map<String, Object> postProcessSupportingFileData(Map<String, Object> objs) {
+        Object models = objs.get("models");
+        List<Object> modelsList = (List<Object>) models;
+        List<CodegenModel> codegenModelList = modelsList.stream()
+                .filter(listItem -> listItem instanceof HashMap<?, ?>)
+                .map(listItem -> (CodegenModel)((HashMap<?, ?>)listItem).get("model"))
+                .collect(Collectors.toList());
+
+        List<Object> entities = codegenModelList.stream().map(cm -> createEntityFor(cm)).collect(Collectors.toList());
+        objs.put("entities", entities);
+
+        return super.postProcessSupportingFileData(objs);
     }
 
-    @Override
-    public Map<String, Object> postProcessAllModels(Map<String, Object> objs) {
-        List<String> keysToRemove = objs.keySet().stream().filter(key -> key.endsWith("_allOf")).collect(Collectors.toList());
-        keysToRemove.forEach(key -> objs.remove(key));
+    private Object createEntityFor(CodegenModel codegenModel) {
+        Map<String, Object> entity = new HashMap<>();
+        entity.put("name", codegenModel.getClassname());
 
-        return super.postProcessAllModels(objs);
+        List<Object> fields = codegenModel.getAllVars().stream()
+                .map(var -> createFieldFor(var))
+                .collect(Collectors.toList());
+        entity.put("fields", fields);
+
+        return entity;
+    }
+
+    private Object createFieldFor(CodegenProperty codegenProperty) {
+        Map<String, Object> field = new HashMap<>();
+        field.put("name", codegenProperty.getBaseName());
+        field.put("isRequired", codegenProperty.getRequired());
+        field.put("dataType", codegenProperty.getDataType());
+
+        return field;
     }
 }
---
+++ modules/openapi-generator/src/test/java/org/openapitools/codegen/plantuml/PlantumlDocumentationCodegenTest.java
@@ -0,0 +1,67 @@
+package org.openapitools.codegen.plantuml;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.media.DateTimeSchema;
+import io.swagger.v3.oas.models.media.IntegerSchema;
+import io.swagger.v3.oas.models.media.Schema;
+import io.swagger.v3.oas.models.media.StringSchema;
+import io.swagger.v3.parser.util.SchemaTypeUtil;
+import org.openapitools.codegen.CodegenModel;
+import org.openapitools.codegen.TestUtils;
+import org.openapitools.codegen.languages.PlantumlDocumentationCodegen;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class PlantumlDocumentationCodegenTest {
+    private PlantumlDocumentationCodegen plantumlDocumentationCodegen = new PlantumlDocumentationCodegen();
+
+    @Test
+    public void simpleEntityTest() {
+        final Schema model = new Schema()
+                .description("a sample model")
+                .addProperties("id", new IntegerSchema().format(SchemaTypeUtil.INTEGER64_FORMAT))
+                .addProperties("name", new StringSchema())
+                .addRequiredItem("id")
+                .addRequiredItem("name");
+        OpenAPI openAPI = TestUtils.createOpenAPIWithOneSchema("sample", model);
+        plantumlDocumentationCodegen.setOpenAPI(openAPI);
+        final CodegenModel cm = plantumlDocumentationCodegen.fromModel("sample", model);
+
+        Map<String, Object> modelsMap = new HashMap<>();
+        modelsMap.put("model", cm);
+        List<Map<?,?>> modelsList = new ArrayList();
+        modelsList.add(modelsMap);
+        Map<String, Object> objs = new HashMap<>();
+        objs.put("models", modelsList);
+
+        plantumlDocumentationCodegen.postProcessSupportingFileData(objs);
+
+        Object entities = objs.get("entities");
+        Assert.assertNotNull(entities);
+
+        Assert.assertTrue(entities instanceof List<?>);
+        List<?> entityList = (List<?>)entities;
+
+        Assert.assertFalse(entityList.isEmpty(), "empty entity list");
+        Object firstEntityItem = entityList.get(0);
+
+        Assert.assertTrue(firstEntityItem instanceof HashMap<?, ?>, "first item in the entity list is a HashMap");
+        HashMap<String, Object> firstEntity = (HashMap<String, Object>)firstEntityItem;
+
+        Assert.assertEquals(firstEntity.get("name"), "Sample");
+
+        List<Object> fieldList = (List<Object>)firstEntity.get("fields");
+        Assert.assertEquals(fieldList.size(), 2, "size of field list");
+
+        HashMap<String, Object> firstField = (HashMap<String, Object>)fieldList.get(0);
+
+        Assert.assertEquals((String)firstField.get("name"), "id");
+        Assert.assertTrue((boolean)firstField.get("isRequired"));
+        Assert.assertEquals((String)firstField.get("dataType"), "Long");
+    }
+}
