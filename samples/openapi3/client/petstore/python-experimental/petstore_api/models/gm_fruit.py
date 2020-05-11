4488db60b3e752c42e21c3392948091dc9e51fa8
Sebastien Rosset <serosset@cisco.com>
2020-05-11 03:18:34 -0700

    [python-experimental] Improve error message for regex matching, add unit tests. (#6226)
    * Improve error message
    
    * Add unit tests for regular expressions

--- samples/openapi3/client/petstore/python-experimental/petstore_api/models/gm_fruit.py
+++ samples/openapi3/client/petstore/python-experimental/petstore_api/models/gm_fruit.py
@@ -72,6 +72,17 @@ class GmFruit(ModelComposed):
     }
 
     validations = {
+        ('cultivar',): {
+            'regex': {
+                'pattern': r'^[a-zA-Z\s]*$',  # noqa: E501
+            },
+        },
+        ('origin',): {
+            'regex': {
+                'pattern': r'^[A-Z\s]*$',  # noqa: E501
+                'flags': (re.IGNORECASE)
+            },
+        },
     }
 
     additional_properties_type = None
@@ -89,6 +100,7 @@ class GmFruit(ModelComposed):
         return {
             'color': (str,),  # noqa: E501
             'cultivar': (str,),  # noqa: E501
+            'origin': (str,),  # noqa: E501
             'length_cm': (float,),  # noqa: E501
         }
 
@@ -99,6 +111,7 @@ class GmFruit(ModelComposed):
     attribute_map = {
         'color': 'color',  # noqa: E501
         'cultivar': 'cultivar',  # noqa: E501
+        'origin': 'origin',  # noqa: E501
         'length_cm': 'lengthCm',  # noqa: E501
     }
 
@@ -149,6 +162,7 @@ class GmFruit(ModelComposed):
                                 _visited_composed_classes = (Animal,)
             color (str): [optional]  # noqa: E501
             cultivar (str): [optional]  # noqa: E501
+            origin (str): [optional]  # noqa: E501
             length_cm (float): [optional]  # noqa: E501
         """
 
