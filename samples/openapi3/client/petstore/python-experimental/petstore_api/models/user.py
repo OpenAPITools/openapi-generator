5e48c724f2ddf5fb14b8a4772c53f67199c300a4
Sebastien Rosset (serosset) <serosset@cisco.com>
2020-05-12 11:20:34 -0700

    run samples scripts

--- samples/openapi3/client/petstore/python-experimental/petstore_api/models/user.py
+++ samples/openapi3/client/petstore/python-experimental/petstore_api/models/user.py
@@ -87,8 +87,8 @@ class User(ModelNormal):
             'user_status': (int,),  # noqa: E501
             'object_with_no_declared_props': (bool, date, datetime, dict, float, int, list, str,),  # noqa: E501
             'object_with_no_declared_props_nullable': (bool, date, datetime, dict, float, int, list, str, none_type,),  # noqa: E501
-            'any_type_prop': (object,),  # noqa: E501
-            'any_type_prop_nullable': (object, none_type,),  # noqa: E501
+            'any_type_prop': (bool, date, datetime, dict, float, int, list, str, none_type,),  # noqa: E501
+            'any_type_prop_nullable': (bool, date, datetime, dict, float, int, list, str, none_type,),  # noqa: E501
         }
 
     @cached_property
@@ -164,8 +164,8 @@ class User(ModelNormal):
             user_status (int): User Status. [optional]  # noqa: E501
             object_with_no_declared_props (bool, date, datetime, dict, float, int, list, str): test code generation for objects Value must be a map of strings to values. It cannot be the &#39;null&#39; value.. [optional]  # noqa: E501
             object_with_no_declared_props_nullable (bool, date, datetime, dict, float, int, list, str, none_type): test code generation for nullable objects. Value must be a map of strings to values or the &#39;null&#39; value.. [optional]  # noqa: E501
-            any_type_prop (object): test code generation for any type Here the &#39;type&#39; attribute is not specified, which means the value can be anything, including the null value, string, number, boolean, array or object. See https://github.com/OAI/OpenAPI-Specification/issues/1389. [optional]  # noqa: E501
-            any_type_prop_nullable (object, none_type): test code generation for any type Here the &#39;type&#39; attribute is not specified, which means the value can be anything, including the null value, string, number, boolean, array or object. The &#39;nullable&#39; attribute does not change the allowed values.. [optional]  # noqa: E501
+            any_type_prop (bool, date, datetime, dict, float, int, list, str, none_type): test code generation for any type Here the &#39;type&#39; attribute is not specified, which means the value can be anything, including the null value, string, number, boolean, array or object. See https://github.com/OAI/OpenAPI-Specification/issues/1389. [optional]  # noqa: E501
+            any_type_prop_nullable (bool, date, datetime, dict, float, int, list, str, none_type): test code generation for any type Here the &#39;type&#39; attribute is not specified, which means the value can be anything, including the null value, string, number, boolean, array or object. The &#39;nullable&#39; attribute does not change the allowed values.. [optional]  # noqa: E501
         """
 
         self._data_store = {}
