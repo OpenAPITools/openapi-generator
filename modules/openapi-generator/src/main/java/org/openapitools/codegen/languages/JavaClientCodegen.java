75c991dda59396e9652610c1c64c78026c116007
William Cheng <wing328hk@gmail.com>
2020-05-14 15:13:59 +0800

    fix oauth check

--- modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/JavaClientCodegen.java
+++ modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/JavaClientCodegen.java
@@ -107,6 +107,7 @@ public class JavaClientCodegen extends AbstractJavaCodegen
     protected String serializationLibrary = null;
 
     protected boolean addOAuthSupportingFiles = false;
+    protected boolean addOAuthRetrySupportingFiles = false;
 
     public JavaClientCodegen() {
         super();
@@ -590,19 +591,22 @@ public class JavaClientCodegen extends AbstractJavaCodegen
             }
         }
 
-        // for okhttp-gson (default), check to see if OAuth is defined and included OAuth-related files accordingly
-        if ((OKHTTP_GSON.equals(getLibrary()) || StringUtils.isEmpty(getLibrary())) && ProcessUtils.hasOAuthMethods(objs) && !addOAuthSupportingFiles) {
-            supportingFiles.add(new SupportingFile("auth/OAuthOkHttpClient.mustache", authFolder, "OAuthOkHttpClient.java"));
-            supportingFiles.add(new SupportingFile("auth/RetryingOAuth.mustache", authFolder, "RetryingOAuth.java"));
-            addOAuthSupportingFiles = true; // add only once
-        }
+        // has OAuth defined
+        if (ProcessUtils.hasOAuthMethods(objs)) {
+            // for okhttp-gson (default), check to see if OAuth is defined and included OAuth-related files accordingly
+            if ((OKHTTP_GSON.equals(getLibrary()) || StringUtils.isEmpty(getLibrary())) && !addOAuthRetrySupportingFiles) {
+                supportingFiles.add(new SupportingFile("auth/OAuthOkHttpClient.mustache", authFolder, "OAuthOkHttpClient.java"));
+                supportingFiles.add(new SupportingFile("auth/RetryingOAuth.mustache", authFolder, "RetryingOAuth.java"));
+                addOAuthRetrySupportingFiles = true; // add only once
+            }
 
-        // google-api-client doesn't use the OpenAPI auth, because it uses Google Credential directly (HttpRequestInitializer)
-        if ((!(GOOGLE_API_CLIENT.equals(getLibrary()) || REST_ASSURED.equals(getLibrary()) || usePlayWS
-                || NATIVE.equals(getLibrary()) || MICROPROFILE.equals(getLibrary()))) && ProcessUtils.hasOAuthMethods(objs) && !addOAuthSupportingFiles) {
-            supportingFiles.add(new SupportingFile("auth/OAuth.mustache", authFolder, "OAuth.java"));
-            supportingFiles.add(new SupportingFile("auth/OAuthFlow.mustache", authFolder, "OAuthFlow.java"));
-            addOAuthSupportingFiles = true; // add only once
+            // google-api-client doesn't use the OpenAPI auth, because it uses Google Credential directly (HttpRequestInitializer)
+            if (!(GOOGLE_API_CLIENT.equals(getLibrary()) || REST_ASSURED.equals(getLibrary()) || usePlayWS
+                    || NATIVE.equals(getLibrary()) || MICROPROFILE.equals(getLibrary())) && !addOAuthSupportingFiles) {
+                supportingFiles.add(new SupportingFile("auth/OAuth.mustache", authFolder, "OAuth.java"));
+                supportingFiles.add(new SupportingFile("auth/OAuthFlow.mustache", authFolder, "OAuthFlow.java"));
+                addOAuthSupportingFiles = true; // add only once
+            }
         }
 
         if (MICROPROFILE.equals(getLibrary())) {
--- modules/openapi-generator/src/main/java/org/openapitools/codegen/utils/ProcessUtils.java
+++ modules/openapi-generator/src/main/java/org/openapitools/codegen/utils/ProcessUtils.java
@@ -15,7 +15,7 @@ public class ProcessUtils {
     /**
      * Add x-index extension to the model's properties
      *
-     * @param models List of models
+     * @param models       List of models
      * @param initialIndex starting index to use
      */
     public static void addIndexToProperties(List<Object> models, int initialIndex) {
@@ -55,27 +55,20 @@ public class ProcessUtils {
      * @return True if at least one operation has OAuth security schema defined
      */
     public static boolean hasOAuthMethods(Map<String, Object> objs) {
-        if (hasOAuthMethods == null) {
-            Map<String, Object> operations = (Map<String, Object>) objs.get("operations");
-            if (operations != null) {
-                List<CodegenOperation> ops = (List<CodegenOperation>) operations.get("operation");
-                for (CodegenOperation operation : ops) {
-                    if (operation.authMethods != null && !operation.authMethods.isEmpty()) {
-                        for (CodegenSecurity cs : operation.authMethods) {
-                            if (Boolean.TRUE.equals(cs.isOAuth)) {
-                                hasOAuthMethods = true;
-                                return true;
-                            }
+        Map<String, Object> operations = (Map<String, Object>) objs.get("operations");
+        if (operations != null) {
+            List<CodegenOperation> ops = (List<CodegenOperation>) operations.get("operation");
+            for (CodegenOperation operation : ops) {
+                if (operation.authMethods != null && !operation.authMethods.isEmpty()) {
+                    for (CodegenSecurity cs : operation.authMethods) {
+                        if (Boolean.TRUE.equals(cs.isOAuth)) {
+                            return true;
                         }
                     }
                 }
             }
-
-            hasOAuthMethods = false;
-            return false;
-        } else {
-            return hasOAuthMethods;
         }
+        return false;
     }
 
     /**
@@ -105,34 +98,34 @@ public class ProcessUtils {
     /**
      * Returns true if the specified OAS model has at least one operation with the HTTP basic
      * security scheme.
-     * 
+     *
      * @param authMethods List of auth methods.
      * @return True if at least one operation has HTTP basic security scheme defined
      */
     public static boolean hasHttpBasicMethods(List<CodegenSecurity> authMethods) {
         if (authMethods != null && !authMethods.isEmpty()) {
-          for (CodegenSecurity cs : authMethods) {
-              if (Boolean.TRUE.equals(cs.isBasicBasic)) {
-                  return true;
-              }
-          }
+            for (CodegenSecurity cs : authMethods) {
+                if (Boolean.TRUE.equals(cs.isBasicBasic)) {
+                    return true;
+                }
+            }
         }
         return false;
     }
 
     /**
      * Returns true if the specified OAS model has at least one operation with API keys.
-     * 
+     *
      * @param authMethods List of auth methods.
      * @return True if at least one operation has API key security scheme defined
      */
     public static boolean hasApiKeyMethods(List<CodegenSecurity> authMethods) {
         if (authMethods != null && !authMethods.isEmpty()) {
-          for (CodegenSecurity cs : authMethods) {
-              if (Boolean.TRUE.equals(cs.isApiKey)) {
-                  return true;
-              }
-          }
+            for (CodegenSecurity cs : authMethods) {
+                if (Boolean.TRUE.equals(cs.isApiKey)) {
+                    return true;
+                }
+            }
         }
         return false;
     }
@@ -141,17 +134,17 @@ public class ProcessUtils {
      * Returns true if the specified OAS model has at least one operation with the HTTP signature
      * security scheme.
      * The HTTP signature scheme is defined in https://datatracker.ietf.org/doc/draft-cavage-http-signatures/
-     * 
+     *
      * @param authMethods List of auth methods.
      * @return True if at least one operation has HTTP signature security scheme defined
      */
     public static boolean hasHttpSignatureMethods(List<CodegenSecurity> authMethods) {
         if (authMethods != null && !authMethods.isEmpty()) {
-          for (CodegenSecurity cs : authMethods) {
-              if (Boolean.TRUE.equals(cs.isHttpSignature)) {
-                  return true;
-              }
-          }
+            for (CodegenSecurity cs : authMethods) {
+                if (Boolean.TRUE.equals(cs.isHttpSignature)) {
+                    return true;
+                }
+            }
         }
         return false;
     }
