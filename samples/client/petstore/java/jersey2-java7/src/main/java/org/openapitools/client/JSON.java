7c036893a276bd12f18bba0fff9c2f883a6e6650
Slavek Kabrda <slavek.kabrda@datadoghq.com>
2020-05-15 15:54:27 +0200

    [Java][jersey2] Allow unknown properties returned by server.

--- samples/client/petstore/java/jersey2-java7/src/main/java/org/openapitools/client/JSON.java
+++ samples/client/petstore/java/jersey2-java7/src/main/java/org/openapitools/client/JSON.java
@@ -17,7 +17,7 @@ public class JSON implements ContextResolver<ObjectMapper> {
   public JSON() {
     mapper = new ObjectMapper();
     mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
-    mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, true);
+    mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
     mapper.configure(DeserializationFeature.FAIL_ON_INVALID_SUBTYPE, true);
     mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
     mapper.enable(SerializationFeature.WRITE_ENUMS_USING_TO_STRING);
