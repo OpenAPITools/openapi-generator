29a68f9cd5b2284435dba3d6996816a0ad8a5f81
William Cheng <wing328hk@gmail.com>
2020-05-18 16:50:26 +0800

    migrate ocaml petstore to use oas3

--- bin/windows/ocaml-petstore.bat
+++ bin/windows/ocaml-petstore.bat
@@ -5,6 +5,6 @@ If Not Exist %executable% (
 )
 
 REM set JAVA_OPTS=%JAVA_OPTS% -Xmx1024M -DloggerPath=conf/log4j.properties
-set ags=generate -i modules\openapi-generator\src\test\resources\2_0\petstore.yaml -g ocaml -o samples\client\petstore\ocaml
+set ags=generate -i modules\openapi-generator\src\test\resources\3_0\petstore.yaml -g ocaml -o samples\client\petstore\ocaml
 
 java %JAVA_OPTS% -jar %executable% %ags%
