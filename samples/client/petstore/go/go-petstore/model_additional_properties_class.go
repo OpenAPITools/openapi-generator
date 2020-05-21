598b0e489c516bd775a2585a9dc89a2ce0137ebc
Sebastien Rosset (serosset) <serosset@cisco.com>
2020-05-20 20:21:22 -0700

    run samples scripts

--- samples/client/petstore/go/go-petstore/model_additional_properties_class.go
+++ samples/client/petstore/go/go-petstore/model_additional_properties_class.go
@@ -18,8 +18,6 @@ type AdditionalPropertiesClass struct {
 	MapArrayAnytype map[string][]map[string]interface{} `json:"map_array_anytype,omitempty"`
 	MapMapString map[string]map[string]string `json:"map_map_string,omitempty"`
 	MapMapAnytype map[string]map[string]map[string]interface{} `json:"map_map_anytype,omitempty"`
-	MapWithAdditionalProperties map[string]interface{} `json:"map_with_additional_properties,omitempty"`
-	MapWithoutAdditionalProperties map[string]interface{} `json:"map_without_additional_properties,omitempty"`
 	Anytype1 map[string]interface{} `json:"anytype_1,omitempty"`
 	Anytype2 map[string]interface{} `json:"anytype_2,omitempty"`
 	Anytype3 map[string]interface{} `json:"anytype_3,omitempty"`
