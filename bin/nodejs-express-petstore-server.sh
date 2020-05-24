60d5ed350c1ba6c17d250a26e30eb9ca1352f421
Sebastien Rosset <serosset@cisco.com>
2020-05-24 01:48:23 -0700

    [nodejs] Fix deprecation notice when running sample nodejs script (#6412)
    * Mustache template should use invokerPackage tag to generate import
    
    * Remove deprecation notice

--- bin/nodejs-express-petstore-server.sh
+++ bin/nodejs-express-petstore-server.sh
@@ -27,6 +27,6 @@ fi
 
 # if you've executed sbt assembly previously it will use that instead.
 export JAVA_OPTS="${JAVA_OPTS} -Xmx1024M -DloggerPath=conf/log4j.properties"
-ags="generate -t modules/openapi-generator/src/main/resources/nodejs-express-server -i modules/openapi-generator/src/test/resources/2_0/petstore.yaml -g nodejs-express-server -o samples/server/petstore/nodejs-express-server -Dservice $@"
+ags="generate -t modules/openapi-generator/src/main/resources/nodejs-express-server -i modules/openapi-generator/src/test/resources/2_0/petstore.yaml -g nodejs-express-server -o samples/server/petstore/nodejs-express-server $@"
 
 java $JAVA_OPTS -jar $executable $ags
