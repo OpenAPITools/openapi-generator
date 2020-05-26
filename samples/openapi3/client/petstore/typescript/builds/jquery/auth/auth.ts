e3936fcb5a5900610059bd8ad2a7e196a54ab88b
Bodo Graumann <mail@bodograumann.de>
2020-05-26 16:28:49 +0200

    Generate a class for each auth method

--- samples/openapi3/client/petstore/typescript/builds/jquery/auth/auth.ts
+++ samples/openapi3/client/petstore/typescript/builds/jquery/auth/auth.ts
@@ -1,84 +1,20 @@
 import {RequestContext} from '../http/http';
 // typings for btoa are incorrect
 /**
- * Base class for all authentication schemes.
- *
+ * Interface authentication schemes.
  */
-export abstract class SecurityAuthentication {
+export interface SecurityAuthentication {
+    /*
+     * @return returns the name of the security authentication as specified in OAI
+     */
+    getName(): string;
 
-	public constructor(private name: string) {
-	
-	}
-	
-	/*
-	 * 
-	 * @return returns the name of the security authentication as specified in OAI
-	 */
-	public getName(): string {
-		return this.name;
-	}
-	
-	/**
-	 * Applies the authentication scheme to the request context
-	 * 
-	 * @params context the request context which should use this authentication scheme
-	 */
-   public abstract applySecurityAuthentication(context: RequestContext): void | Promise<void>;
-
-}
-
-/**
- * Applies an api key to the request context. 
- *
- */
-export class APIKeyAuthentication extends SecurityAuthentication {
-	
-	/**
-	 * Configures this api key authentication with the necessary properties
-	 *
-	 * @param authName: name of this authentication scheme as specified in the swagger.json
-	 * @param paramName: Parameter name used for the api key
-	 * @param keyLocation: Parameter location, either query, header or cookie.
-	 * @param apiKey: The api key to be used for every request
-	 */
-	public constructor(authName: string, private paramName: string, private keyLocation: "query" | "header" | "cookie", private apiKey: string) {
-		super(authName);
-	}
-	
-	public applySecurityAuthentication(context: RequestContext) {
-		if (this.keyLocation === "header") {
-			context.setHeaderParam(this.paramName, this.apiKey);
-		} else if (this.keyLocation === "cookie") {
-			context.addCookie(this.paramName, this.apiKey);
-		} else if (this.keyLocation === "query") {
-			context.setQueryParam(this.paramName, this.apiKey);
-		}
-	}
-}
-
-
-/**
- * Applies basic http authentication to a request.
- *
- */
-export class HttpBasicAuthentication extends SecurityAuthentication {
-	
-	/**
-	 * Configures the http authentication with the required details.
-	 *
-	 *
-	 * @param authName name of the authentication scheme as defined in swagger json
-	 * @param username username for http basic authentication
-	 * @param password password for http basic authentication
-	 */
-	public constructor(authName: string, private username: string, private password: string) {
-		super(authName);
-	}
-	
-	public applySecurityAuthentication(context: RequestContext) {
-		let comb = this.username + ":" + this.password;
-		context.setHeaderParam("Authorization", "Basic " + btoa(comb));
-	}
+    /**
+     * Applies the authentication scheme to the request context
+     *
+     * @params context the request context which should use this authentication scheme
+     */
+    applySecurityAuthentication(context: RequestContext): void | Promise<void>;
 }
 
 export interface TokenProvider {
@@ -86,40 +22,45 @@ export interface TokenProvider {
 }
 
 /**
- * Applies http bearer authentication to a request.
- *
+ * Applies apiKey authentication to the request context.
  */
-export class HttpBearerAuthentication extends SecurityAuthentication {
+export class ApiKeyAuthentication implements SecurityAuthentication {
     /**
-     * Configures the http authentication with the required details.
+     * Configures this api key authentication with the necessary properties
      *
-     *
-     * @param authName name of the authentication scheme as defined in openapi specification
-     * @param tokenProvider service that can provide the up-to-date token when needed
+     * @param apiKey: The api key to be used for every request
      */
-    public constructor(authName: string, private tokenProvider: TokenProvider) {
-        super(authName);
+    public constructor(private apiKey: string) {}
+
+    public getName(): string {
+        return "api_key";
     }
 
-    public async applySecurityAuthentication(context: RequestContext) {
-        context.setHeaderParam("Authorization", "Bearer " + await this.tokenProvider.getToken());
+    public applySecurityAuthentication(context: RequestContext) {
+        context.setHeaderParam("api_key", this.apiKey);
     }
 }
 
-// TODO: How to handle oauth2 authentication!
-export class OAuth2Authentication extends SecurityAuthentication {
-	public constructor(authName: string) {
-		super(authName);
-	}
-	
-	public applySecurityAuthentication(context: RequestContext) {
-		// TODO
-	}
+/**
+ * Applies oauth2 authentication to the request context.
+ */
+export class PetstoreAuthAuthentication implements SecurityAuthentication {
+    // TODO: How to handle oauth2 authentication!
+    public constructor() {}
+
+    public getName(): string {
+        return "petstore_auth";
+    }
+
+    public applySecurityAuthentication(context: RequestContext) {
+        // TODO
+    }
 }
+
 
 export type AuthMethods = {
-		"api_key"?: APIKeyAuthentication,
-		"petstore_auth"?: OAuth2Authentication,
+    "api_key"?: ApiKeyAuthentication,
+    "petstore_auth"?: PetstoreAuthAuthentication
 }
 
 export type ApiKeyConfiguration = string;
@@ -127,7 +68,10 @@ export type HttpBasicConfiguration = { "username": string, "password": string };
 export type HttpBearerConfiguration = { tokenProvider: TokenProvider };
 export type OAuth2Configuration = string;
 
-export type AuthMethodsConfiguration = { "api_key"?:ApiKeyConfiguration,  "petstore_auth"?:OAuth2Configuration,   }
+export type AuthMethodsConfiguration = {
+    "api_key"?: ApiKeyConfiguration,
+    "petstore_auth"?: OAuth2Configuration
+}
 
 /**
  * Creates the authentication methods from a swagger description.
@@ -141,12 +85,15 @@ export function configureAuthMethods(config: AuthMethodsConfiguration | undefine
     }
 
     if (config["api_key"]) {
-        authMethods["api_key"] = new APIKeyAuthentication("api_key",  "api_key", "header", <string> config["api_key"]);
-	}
+        authMethods["api_key"] = new ApiKeyAuthentication(
+            config["api_key"]
+        );
+    }
 
     if (config["petstore_auth"]) {
-		authMethods["petstore_auth"] = new OAuth2Authentication("petstore_auth");
-	}
+        authMethods["petstore_auth"] = new PetstoreAuthAuthentication(
+        );
+    }
 
-	return authMethods;
+    return authMethods;
 }
--- samples/openapi3/client/petstore/typescript/tests/default/test/auth/auth.test.ts
+++ samples/openapi3/client/petstore/typescript/tests/default/test/auth/auth.test.ts
@@ -1,5 +1,5 @@
 import * as petstore from 'ts-petstore-client';
-import { expect} from "chai";
+import { expect } from "chai";
 
 
 
@@ -8,35 +8,14 @@ describe("Security Authentication", () => {
         // TODO: make all params const variables
         it("Header API Key", () => {
             let ctx = new petstore.RequestContext("http://google.com", petstore.HttpMethod.GET);
-            let auth = new petstore.APIKeyAuthentication("my_name", "paramName", "header", "apiKey");
+            let auth = new petstore.ApiKeyAuthentication("apiKey");
             auth.applySecurityAuthentication(ctx);
-    
-            expect(ctx.getUrl()).to.equal("http://google.com");
-            expect(ctx.getHeaders()).to.have.property("paramName");
-            expect(ctx.getHeaders()["paramName"]).to.equal("apiKey");
-            expect(ctx.getBody()).to.equal(undefined);
-        });    
 
-        it("Query API Key", () => {
-            let ctx = new petstore.RequestContext("http://google.com?a=b", petstore.HttpMethod.GET);
-            let auth = new petstore.APIKeyAuthentication("my_name", "paramName", "query", "apiKey",);
-            auth.applySecurityAuthentication(ctx);
-    
-            expect(ctx.getUrl()).to.contain("paramName=apiKey");
-            expect(ctx.getHeaders()).to.deep.equal({});
-            expect(ctx.getBody()).to.equal(undefined);
-        });    
-
-        it("Cookie API Key", () => {
-            let ctx = new petstore.RequestContext("http://google.com", petstore.HttpMethod.GET);
-            let auth = new petstore.APIKeyAuthentication("my_name", "paramName", "cookie", "apiKey",);
-            auth.applySecurityAuthentication(ctx);
-    
             expect(ctx.getUrl()).to.equal("http://google.com");
-            expect(ctx.getHeaders()).to.have.property("Cookie");
-            expect(ctx.getHeaders()["Cookie"]).to.contain("paramName=apiKey; ");
+            expect(ctx.getHeaders()).to.have.property("api_key");
+            expect(ctx.getHeaders()["api_key"]).to.equal("apiKey");
             expect(ctx.getBody()).to.equal(undefined);
-        });    
+        });
 
     })
 });
