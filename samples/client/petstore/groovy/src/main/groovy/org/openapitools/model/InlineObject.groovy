ddcda1ee50721da63166ff0463b5b198a06593ba
William Cheng <wing328hk@gmail.com>
2020-05-27 10:59:53 +0800

    Migrate Groovy samples to oas3 (#6435)
    * migrate groovy samples to oas3
    
    * update samples

---
+++ samples/client/petstore/groovy/src/main/groovy/org/openapitools/model/InlineObject.groovy
@@ -0,0 +1,13 @@
+package org.openapitools.model;
+
+import groovy.transform.Canonical
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+@Canonical
+class InlineObject {
+    /* Updated name of the pet */
+    String name
+    /* Updated status of the pet */
+    String status
+}
