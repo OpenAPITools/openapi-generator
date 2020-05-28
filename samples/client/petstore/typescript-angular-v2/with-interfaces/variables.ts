ec39005ae528f88522caedd91db91c0b59624d94
Esteban Gehring <esteban.gehring@bithost.ch>
2020-05-28 11:28:11 +0200

    [typescript-angular] drop support of angular below 6.0.0 (#6360)
    * typescript-angular: drop support for angular below 6.0.0
    
    * typescript-angular: re-generate samples
    
    * typescript-angular: re-generate docs

--- samples/client/petstore/typescript-angular-v2/with-interfaces/variables.ts
+++
@@ -1,9 +0,0 @@
-import { OpaqueToken } from '@angular/core';
-
-export const BASE_PATH = new OpaqueToken('basePath');
-export const COLLECTION_FORMATS = {
-    'csv': ',',
-    'tsv': '   ',
-    'ssv': ' ',
-    'pipes': '|'
-}
