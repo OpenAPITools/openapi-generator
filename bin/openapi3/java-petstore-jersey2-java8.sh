c2c636448bdaad4283116e8cd0368ce8057daa95
Sebastien Rosset (serosset) <serosset@cisco.com>
2020-05-28 19:00:18 -0700

    Add unit test for oneOf schema deserialization

--- bin/openapi3/java-petstore-jersey2-java8.sh
+++ bin/openapi3/java-petstore-jersey2-java8.sh
@@ -35,3 +35,10 @@ rm -rf samples/openapi3/client/petstore/java/jersey2-java8/src/main
 find samples/openapi3/client/petstore/java/jersey2-java8 -maxdepth 1 -type f ! -name "README.md" -exec rm {} +
 java $JAVA_OPTS -jar $executable $ags
 
+# copy additional manually written unit-tests
+mkdir -p samples/client/petstore/java/jersey2-java8/src/test/java/org/openapitools/client
+mkdir -p samples/client/petstore/java/jersey2-java8/src/test/java/org/openapitools/client/auth
+mkdir -p samples/client/petstore/java/jersey2-java8/src/test/java/org/openapitools/client/model
+
+cp CI/samples.ci/client/petstore/java/test-manual/jersey2-java8/JSONTest.java samples/openapi3/client/petstore/java/jersey2-java8/src/test/java/org/openapitools/client/JSONTest.java
+cp CI/samples.ci/client/petstore/java/test-manual/jersey2-java8/JSONComposedSchemaTest.java samples/openapi3/client/petstore/java/jersey2-java8/src/test/java/org/openapitools/client/JSONComposedSchemaTest.java
