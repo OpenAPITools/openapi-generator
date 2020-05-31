21861bd27f82111ec99f8fd10911476c3be0d86e
Tino Fuhrmann <tino-fuhrmann@web.de>
2020-06-01 01:28:11 +0200

    Added file to generate TS samples on Windows

---
+++ bin/windows/typescript.bat
@@ -0,0 +1,13 @@
+set executable=.\modules\openapi-generator-cli\target\openapi-generator-cli.jar
+
+If Not Exist %executable% (
+  mvn clean package
+)
+
+REM set JAVA_OPTS=%JAVA_OPTS% -Xmx1024M
+
+set ags=generate -i modules\openapi-generator\src\test\resources\3_0\petstore.yaml -g typescript -o samples\openapi3\client\petstore\typescript\builds\default --additional-properties=platform=node,npmName=ts-petstore-client
+java %JAVA_OPTS% -jar %executable% %ags%
+
+ags=generate -i modules\openapi-generator\src\test\resources\3_0\petstore.yaml  -g typescript -o samples\openapi3\client\petstore\typescript\builds\jquery --additional-properties=framework=jquery,npmName=ts-petstore-client
+java %JAVA_OPTS% -jar %executable% %ags%
