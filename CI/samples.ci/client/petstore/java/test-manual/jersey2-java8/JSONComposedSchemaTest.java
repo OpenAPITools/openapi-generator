410a19d067271e4266303cd006659b0d4ada0dfd
Sebastien Rosset (serosset) <serosset@cisco.com>
2020-06-01 20:05:45 -0700

    Add more unit tests

--- CI/samples.ci/client/petstore/java/test-manual/jersey2-java8/JSONComposedSchemaTest.java
+++ CI/samples.ci/client/petstore/java/test-manual/jersey2-java8/JSONComposedSchemaTest.java
@@ -24,9 +24,49 @@ public class JSONComposedSchemaTest {
     public void testOneOfSchemaWithoutDiscriminator() throws Exception {
         // BananaReq and AppleReq have explicitly defined properties that are different by name.
         // There is no discriminator property.
-        String str = "{ \"cultivar\": \"golden delicious\", \"mealy\": false }";
-        FruitReq o = json.getContext(null).readValue(str, FruitReq.class);
-        assertTrue(o.getActualInstance() instanceof AppleReq);
+        {
+            String str = "{ \"cultivar\": \"golden delicious\", \"mealy\": false }";
+            FruitReq o = json.getContext(null).readValue(str, FruitReq.class);
+            assertTrue(o.getActualInstance() instanceof AppleReq);
+            AppleReq inst = (AppleReq) o.getActualInstance();
+            assertEquals(inst.getCultivar(), "golden delicious");
+            assertEquals(inst.getMealy(), false);
+        }
+        {
+            // Same test, but this time with additional (undeclared) properties.
+            // Since FruitReq has additionalProperties: false, deserialization should fail.
+            String str = "{ \"cultivar\": \"golden delicious\", \"mealy\": false, \"garbage_prop\": \"abc\" }";
+            Exception exception = assertThrows(JsonMappingException.class, () -> {
+                FruitReq o = json.getContext(null).readValue(str, FruitReq.class);
+            });
+            assertTrue(exception.getMessage().contains("Failed deserialization for FruitReq: 0 classes match result"));
+        }
+        {
+            String str = "{ \"lengthCm\": 17 }";
+            FruitReq o = json.getContext(null).readValue(str, FruitReq.class);
+            assertTrue(o.getActualInstance() instanceof BananaReq);
+            BananaReq inst = (BananaReq) o.getActualInstance();
+            assertEquals(inst.getLengthCm(), new java.math.BigDecimal(17));
+        }
+        {
+            // Try to deserialize empty object. This should fail 'oneOf' because that will match
+            // both AppleReq and BananaReq.
+            String str = "{ }";
+            Exception exception = assertThrows(JsonMappingException.class, () -> {
+                json.getContext(null).readValue(str, FruitReq.class);
+            });
+            assertTrue(exception.getMessage().contains("Failed deserialization for FruitReq: 2 classes match result"));
+            // TODO: add a similar unit test where the oneOf child schemas have required properties.
+            // If the implementation is correct, the unmarshaling should take the "required" keyword
+            // into consideration, which it is not doing currently.
+        }
+        {
+            // Deserialize the null value. This should be allowed because the 'FruitReq' schema
+            // has nullable: true.
+            String str = "null";
+            FruitReq o = json.getContext(null).readValue(str, FruitReq.class);
+            assertNull(o);
+        }        
     }
 
     /**
@@ -37,26 +77,65 @@ public class JSONComposedSchemaTest {
     public void testOneOfSchemaWithDiscriminator() throws Exception {
         // Mammal can be one of whale, pig and zebra.
         // pig has sub-classes.
-        String str = "{ \"className\": \"whale\", \"hasBaleen\": true, \"hasTeeth\": false }";
-        
-        // Note that the 'zebra' schema does not have any explicit property defined AND
-        // it has additionalProperties: true. Hence without a discriminator the above
-        // JSON payload would match both 'whale' and 'zebra'. This is because the 'hasBaleen'
-        // and 'hasTeeth' would be considered additional (undeclared) properties for 'zebra'.
-        AbstractOpenApiSchema o = json.getContext(null).readValue(str, Mammal.class);
-        assertNotNull(o);
-        assertTrue(o.getActualInstance() instanceof Whale);
-
-        str = "{ \"className\": \"zebra\" }";
-        o = json.getContext(null).readValue(str, Mammal.class);
-        assertNotNull(o);
-        assertTrue(o.getActualInstance() instanceof Zebra);
-
-        // Deserialization test with indirections of 'oneOf' child schemas.
-        // Mammal is oneOf whale, zebra and pig, and pig is itself one of BasquePig, DanishPig.
-        str = "{ \"className\": \"BasquePig\" }";
-        o = json.getContext(null).readValue(str, Mammal.class);
-        assertTrue(o.getActualInstance() instanceof BasquePig);
+        {
+            String str = "{ \"className\": \"whale\", \"hasBaleen\": true, \"hasTeeth\": false }";
+            
+            // Note that the 'zebra' schema does not have any explicit property defined AND
+            // it has additionalProperties: true. Hence without a discriminator the above
+            // JSON payload would match both 'whale' and 'zebra'. This is because the 'hasBaleen'
+            // and 'hasTeeth' would be considered additional (undeclared) properties for 'zebra'.
+            AbstractOpenApiSchema o = json.getContext(null).readValue(str, Mammal.class);
+            assertNotNull(o);
+            assertTrue(o.getActualInstance() instanceof Whale);
+        }
+        {
+            String str = "{ \"className\": \"zebra\", \"type\": \"plains\" }";
+            AbstractOpenApiSchema o = json.getContext(null).readValue(str, Mammal.class);
+            assertNotNull(o);
+            assertTrue(o.getActualInstance() instanceof Zebra);
+            Zebra z = (Zebra)o.getActualInstance();
+            // TODO: this is incorrect: assert the value is Zebra.TypeEnum.PLAINS
+            assertNull(z.getType());
+            //assertEquals(Zebra.TypeEnum.PLAINS, z.getType());
+        }
+        {
+            // The discriminator value is valid but the 'type' value is invalid.
+            // TODO: the current deserialization code is incorrectly accepting the input data.
+            // The unit test code below should be rewritten to assert an exception.
+            String str = "{ \"className\": \"zebra\", \"type\": \"garbage_value\" }";
+            AbstractOpenApiSchema o = json.getContext(null).readValue(str, Mammal.class);
+            assertNotNull(o);
+            assertTrue(o.getActualInstance() instanceof Zebra);
+            Zebra z = (Zebra)o.getActualInstance();
+            assertNull(z.getType());
+            //Exception exception = assertThrows(JsonMappingException.class, () -> {
+            //json.getContext(null).readValue(str, Mammal.class);
+            //});
+        }
+        {
+            // The discriminator value is zebra but the properties belong to Whale.
+            // The 'whale' properties are considered to be additional (undeclared) properties
+            // because in the 'zebra' schema, the 'additionalProperties' keyword has been set
+            // to true.
+            // TODO: The outcome should depend on the value of the 'useOneOfDiscriminatorLookup' CLI.
+            String str = "{ \"className\": \"zebra\", \"hasBaleen\": true, \"hasTeeth\": false }";
+            AbstractOpenApiSchema o = json.getContext(null).readValue(str, Mammal.class);
+            assertNotNull(o);
+            assertTrue(o.getActualInstance() instanceof Zebra);
+        }
+        {
+            String str = "{ \"className\": \"zebra\" }";
+            AbstractOpenApiSchema o = json.getContext(null).readValue(str, Mammal.class);
+            assertNotNull(o);
+            assertTrue(o.getActualInstance() instanceof Zebra);
+        }
+        {
+            // Deserialization test with indirections of 'oneOf' child schemas.
+            // Mammal is oneOf whale, zebra and pig, and pig is itself one of BasquePig, DanishPig.
+            String str = "{ \"className\": \"BasquePig\" }";
+            AbstractOpenApiSchema o = json.getContext(null).readValue(str, Mammal.class);
+            assertTrue(o.getActualInstance() instanceof BasquePig);
+        }
     }
 
     @Test
@@ -76,7 +155,7 @@ public class JSONComposedSchemaTest {
         Exception exception = assertThrows(JsonMappingException.class, () -> {
             json.getContext(null).readValue(str, Shape.class);
         });
-        assertEquals(exception.getMessage(), "Shape cannot be null");
+        assertEquals("Shape cannot be null", exception.getMessage());
     }
 
     /**
@@ -157,7 +236,7 @@ public class JSONComposedSchemaTest {
             // We should be able to deserialize a dog into a Dog.
             Dog d = json.getContext(null).readValue(str, Dog.class);            
             assertNotNull(d);
-            assertEquals(d.getColor(), "white");
+            assertEquals("white", d.getColor());
         }
     }
 }
