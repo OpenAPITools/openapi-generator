d433c2d42fd79fa1bb3a8fa0671599315ca152fc
Chris Tankersley <chris@ctankersley.com>
2020-06-30 05:22:32 -0400

    [php-laravel] Upgrade `php-laravel` to 7.x (#6346)
    * Update laravel from 5.6 to 7.0
    
    * Updated petstore example, removed missing files
    
    * Updated PHP version to match composer.json
    
    * Add samples generation config with OAS3 input spec
    
    * Fixed gitignore compile errors, updated to newest petstore example
    
    * Add composer.lock to .gitignore
    
    * Fix generator gitignore location
    
    * Add integration tests
    
    * Refresh samples
    
    Co-authored-by: Yuriy Belenko <yura-bely@mail.ru>

--- modules/openapi-generator/src/main/resources/php-laravel/database/factories/UserFactory.php
+++ modules/openapi-generator/src/main/resources/php-laravel/database/factories/UserFactory.php
@@ -1,6 +1,10 @@
 <?php
 
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\User;
 use Faker\Generator as Faker;
+use Illuminate\Support\Str;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,11 +17,12 @@ use Faker\Generator as Faker;
 |
 */
 
-$factory->define(App\User::class, function (Faker $faker) {
+$factory->define(User::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
-        'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
-        'remember_token' => str_random(10),
+        'email_verified_at' => now(),
+        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        'remember_token' => Str::random(10),
     ];
 });
