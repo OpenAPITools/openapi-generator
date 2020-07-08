0be0a06d8417db7694840f699f346e95c2c9183b
jekkel <joerg.eichhorn@kiwigrid.com>
2020-07-08 10:30:31 +0200

    [Java][Client][vert.x] Support per-call authentication and JsonNullable in client request bodies, fix path parameter encoding (#5732)
    * Support per-call authentication and JsonNullable in client request bodies
    
    Extensions
    ----------
    Since vertx is an asynchronous/reactive toolkit usage of mutable fields
    of `ApiClient` to store authentication prohibits delegation of
    authentication from incoming call without recreating the client
    everytime (which is prohibitively expensive due to instantiation of objectmapper).
    This commit adds a per call authentication override which takes precedence
    over the mutable fields so a single client can be safely reused. To ease
    usage a simple builder-style auxiliary `AuthInfo` object is provided
    providing methods for each specified authentication.
    
    A new configuration option for `ApiClient` has been added: `timeout`. It gets
    applied as timeout for the vertx http request.
    
    Fixes
    -----
    Request bodies have been wrongly serialized using vert.x built-in
    objectmapper instead of the embedded instance thus usage of beans with
    `JsonNullable` wrapped fields in request bodies led to bad requests.
    
    * update vertx samples
    
    * Client Java Vertx: Add url encode for path parameters.
    
    * update vertx samples

--- samples/client/petstore/java/vertx/src/main/java/org/openapitools/client/api/StoreApiImpl.java
+++ samples/client/petstore/java/vertx/src/main/java/org/openapitools/client/api/StoreApiImpl.java
@@ -10,6 +10,9 @@ import io.vertx.core.json.JsonObject;
 import com.fasterxml.jackson.core.type.TypeReference;
 
 import java.util.*;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 
 import org.openapitools.client.ApiClient;
 import org.openapitools.client.ApiException;
@@ -38,12 +41,23 @@ public class StoreApiImpl implements StoreApi {
     }
 
     /**
-     * Delete purchase order by ID
-     * For valid response try integer IDs with value &lt; 1000. Anything above 1000 or nonintegers will generate API errors
-     * @param orderId ID of the order that needs to be deleted (required)
-     * @param resultHandler Asynchronous result handler
-     */
+    * Delete purchase order by ID
+    * For valid response try integer IDs with value &lt; 1000. Anything above 1000 or nonintegers will generate API errors
+        * @param orderId ID of the order that needs to be deleted (required)
+    * @param resultHandler Asynchronous result handler
+    */
     public void deleteOrder(String orderId, Handler<AsyncResult<Void>> resultHandler) {
+        deleteOrder(orderId, null, resultHandler);
+    }
+
+    /**
+    * Delete purchase order by ID
+    * For valid response try integer IDs with value &lt; 1000. Anything above 1000 or nonintegers will generate API errors
+    * @param orderId ID of the order that needs to be deleted (required)
+    * @param authInfo per call authentication override.
+    * @param resultHandler Asynchronous result handler
+    */
+    public void deleteOrder(String orderId, ApiClient.AuthInfo authInfo, Handler<AsyncResult<Void>> resultHandler) {
         Object localVarBody = null;
         
         // verify the required parameter 'orderId' is set
@@ -53,7 +67,7 @@ public class StoreApiImpl implements StoreApi {
         }
         
         // create path and map variables
-        String localVarPath = "/store/order/{order_id}".replaceAll("\\{" + "order_id" + "\\}", orderId.toString());
+        String localVarPath = "/store/order/{order_id}".replaceAll("\\{" + "order_id" + "\\}", encodeParameter(orderId.toString()));
 
         // query params
         List<Pair> localVarQueryParams = new ArrayList<>();
@@ -72,14 +86,24 @@ public class StoreApiImpl implements StoreApi {
         String[] localVarContentTypes = {  };
         String[] localVarAuthNames = new String[] {  };
 
-        apiClient.invokeAPI(localVarPath, "DELETE", localVarQueryParams, localVarBody, localVarHeaderParams, localVarCookieParams, localVarFormParams, localVarAccepts, localVarContentTypes, localVarAuthNames, null, resultHandler);
+        apiClient.invokeAPI(localVarPath, "DELETE", localVarQueryParams, localVarBody, localVarHeaderParams, localVarCookieParams, localVarFormParams, localVarAccepts, localVarContentTypes, localVarAuthNames, authInfo, null, resultHandler);
     }
     /**
-     * Returns pet inventories by status
-     * Returns a map of status codes to quantities
-     * @param resultHandler Asynchronous result handler
-     */
+    * Returns pet inventories by status
+    * Returns a map of status codes to quantities
+    * @param resultHandler Asynchronous result handler
+    */
     public void getInventory(Handler<AsyncResult<Map<String, Integer>>> resultHandler) {
+        getInventory(null, resultHandler);
+    }
+
+    /**
+    * Returns pet inventories by status
+    * Returns a map of status codes to quantities
+    * @param authInfo per call authentication override.
+    * @param resultHandler Asynchronous result handler
+    */
+    public void getInventory(ApiClient.AuthInfo authInfo, Handler<AsyncResult<Map<String, Integer>>> resultHandler) {
         Object localVarBody = null;
         
         // create path and map variables
@@ -102,15 +126,26 @@ public class StoreApiImpl implements StoreApi {
         String[] localVarContentTypes = {  };
         String[] localVarAuthNames = new String[] { "api_key" };
         TypeReference<Map<String, Integer>> localVarReturnType = new TypeReference<Map<String, Integer>>() {};
-        apiClient.invokeAPI(localVarPath, "GET", localVarQueryParams, localVarBody, localVarHeaderParams, localVarCookieParams, localVarFormParams, localVarAccepts, localVarContentTypes, localVarAuthNames, localVarReturnType, resultHandler);
+        apiClient.invokeAPI(localVarPath, "GET", localVarQueryParams, localVarBody, localVarHeaderParams, localVarCookieParams, localVarFormParams, localVarAccepts, localVarContentTypes, localVarAuthNames, authInfo, localVarReturnType, resultHandler);
     }
     /**
-     * Find purchase order by ID
-     * For valid response try integer IDs with value &lt;&#x3D; 5 or &gt; 10. Other values will generated exceptions
-     * @param orderId ID of pet that needs to be fetched (required)
-     * @param resultHandler Asynchronous result handler
-     */
+    * Find purchase order by ID
+    * For valid response try integer IDs with value &lt;&#x3D; 5 or &gt; 10. Other values will generated exceptions
+        * @param orderId ID of pet that needs to be fetched (required)
+    * @param resultHandler Asynchronous result handler
+    */
     public void getOrderById(Long orderId, Handler<AsyncResult<Order>> resultHandler) {
+        getOrderById(orderId, null, resultHandler);
+    }
+
+    /**
+    * Find purchase order by ID
+    * For valid response try integer IDs with value &lt;&#x3D; 5 or &gt; 10. Other values will generated exceptions
+    * @param orderId ID of pet that needs to be fetched (required)
+    * @param authInfo per call authentication override.
+    * @param resultHandler Asynchronous result handler
+    */
+    public void getOrderById(Long orderId, ApiClient.AuthInfo authInfo, Handler<AsyncResult<Order>> resultHandler) {
         Object localVarBody = null;
         
         // verify the required parameter 'orderId' is set
@@ -120,7 +155,7 @@ public class StoreApiImpl implements StoreApi {
         }
         
         // create path and map variables
-        String localVarPath = "/store/order/{order_id}".replaceAll("\\{" + "order_id" + "\\}", orderId.toString());
+        String localVarPath = "/store/order/{order_id}".replaceAll("\\{" + "order_id" + "\\}", encodeParameter(orderId.toString()));
 
         // query params
         List<Pair> localVarQueryParams = new ArrayList<>();
@@ -139,15 +174,26 @@ public class StoreApiImpl implements StoreApi {
         String[] localVarContentTypes = {  };
         String[] localVarAuthNames = new String[] {  };
         TypeReference<Order> localVarReturnType = new TypeReference<Order>() {};
-        apiClient.invokeAPI(localVarPath, "GET", localVarQueryParams, localVarBody, localVarHeaderParams, localVarCookieParams, localVarFormParams, localVarAccepts, localVarContentTypes, localVarAuthNames, localVarReturnType, resultHandler);
+        apiClient.invokeAPI(localVarPath, "GET", localVarQueryParams, localVarBody, localVarHeaderParams, localVarCookieParams, localVarFormParams, localVarAccepts, localVarContentTypes, localVarAuthNames, authInfo, localVarReturnType, resultHandler);
     }
     /**
-     * Place an order for a pet
-     * 
-     * @param body order placed for purchasing the pet (required)
-     * @param resultHandler Asynchronous result handler
-     */
+    * Place an order for a pet
+    * 
+        * @param body order placed for purchasing the pet (required)
+    * @param resultHandler Asynchronous result handler
+    */
     public void placeOrder(Order body, Handler<AsyncResult<Order>> resultHandler) {
+        placeOrder(body, null, resultHandler);
+    }
+
+    /**
+    * Place an order for a pet
+    * 
+    * @param body order placed for purchasing the pet (required)
+    * @param authInfo per call authentication override.
+    * @param resultHandler Asynchronous result handler
+    */
+    public void placeOrder(Order body, ApiClient.AuthInfo authInfo, Handler<AsyncResult<Order>> resultHandler) {
         Object localVarBody = body;
         
         // verify the required parameter 'body' is set
@@ -176,6 +222,14 @@ public class StoreApiImpl implements StoreApi {
         String[] localVarContentTypes = {  };
         String[] localVarAuthNames = new String[] {  };
         TypeReference<Order> localVarReturnType = new TypeReference<Order>() {};
-        apiClient.invokeAPI(localVarPath, "POST", localVarQueryParams, localVarBody, localVarHeaderParams, localVarCookieParams, localVarFormParams, localVarAccepts, localVarContentTypes, localVarAuthNames, localVarReturnType, resultHandler);
+        apiClient.invokeAPI(localVarPath, "POST", localVarQueryParams, localVarBody, localVarHeaderParams, localVarCookieParams, localVarFormParams, localVarAccepts, localVarContentTypes, localVarAuthNames, authInfo, localVarReturnType, resultHandler);
+    }
+
+    private String encodeParameter(String parameter) {
+        try {
+            return URLEncoder.encode(parameter, StandardCharsets.UTF_8.name());
+        } catch (UnsupportedEncodingException e) {
+            return parameter;
+        }
     }
 }
