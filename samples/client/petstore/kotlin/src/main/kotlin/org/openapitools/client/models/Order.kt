dfcdee308b2be117f8d183a700e6ae7aa00fb594
Nikita Karnaukh <nikita.karnaukh@gmail.com>
2020-07-13 09:11:09 +0300

    update sample #3

--- samples/client/petstore/kotlin/src/main/kotlin/org/openapitools/client/models/Order.kt
+++ samples/client/petstore/kotlin/src/main/kotlin/org/openapitools/client/models/Order.kt
@@ -49,7 +49,7 @@ data class Order (
     * Values: placed,approved,delivered
     */
     
-    enum class Status(val value: kotlin.String){
+    enum class Status(val serialName: kotlin.String) {
         @Json(name = "placed") placed("placed"),
         @Json(name = "approved") approved("approved"),
         @Json(name = "delivered") delivered("delivered");
