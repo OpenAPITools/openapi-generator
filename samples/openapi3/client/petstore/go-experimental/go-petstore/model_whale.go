362958e74cac92780aa38572c5baedc4535b3f92
stephane guillemot <sguillemot@b-com.com>
2020-07-22 11:35:12 +0200

    generate config

--- samples/openapi3/client/petstore/go-experimental/go-petstore/model_whale.go
+++ samples/openapi3/client/petstore/go-experimental/go-petstore/model_whale.go
@@ -18,10 +18,7 @@ type Whale struct {
 	HasBaleen *bool `json:"hasBaleen,omitempty"`
 	HasTeeth *bool `json:"hasTeeth,omitempty"`
 	ClassName string `json:"className"`
-	AdditionalProperties map[string]interface{}
 }
-
-type _Whale Whale
 
 // NewWhale instantiates a new Whale object
 // This constructor will assign default values to properties that have it defined,
@@ -140,31 +137,7 @@ func (o Whale) MarshalJSON() ([]byte, error) {
 	if true {
 		toSerialize["className"] = o.ClassName
 	}
-
-	for key, value := range o.AdditionalProperties {
-		toSerialize[key] = value
-	}
-
 	return json.Marshal(toSerialize)
-}
-
-func (o *Whale) UnmarshalJSON(bytes []byte) (err error) {
-	varWhale := _Whale{}
-
-	if err = json.Unmarshal(bytes, &varWhale); err == nil {
-		*o = Whale(varWhale)
-	}
-
-	additionalProperties := make(map[string]interface{})
-
-	if err = json.Unmarshal(bytes, &additionalProperties); err == nil {
-		delete(additionalProperties, "hasBaleen")
-		delete(additionalProperties, "hasTeeth")
-		delete(additionalProperties, "className")
-		o.AdditionalProperties = additionalProperties
-	}
-
-	return err
 }
 
 type NullableWhale struct {
