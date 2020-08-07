2b770737af754331bf3d790ec668ded136774cc5
Siret, Loic <lsiret@nuglif.com>
2020-08-07 16:15:47 -0400

    Running  ./bin/generate-samples.sh

--- samples/client/petstore/kotlin-gson/src/main/kotlin/org/openapitools/client/infrastructure/Serializer.kt
+++ samples/client/petstore/kotlin-gson/src/main/kotlin/org/openapitools/client/infrastructure/Serializer.kt
@@ -2,23 +2,47 @@ package org.openapitools.client.infrastructure
 
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
+import com.google.gson.JsonElement
+import io.gsonfire.GsonFireBuilder
+import io.gsonfire.TypeSelector
 import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.OffsetDateTime
 import java.util.UUID
+import org.openapitools.client.models.*
+import kotlin.collections.HashMap
+import kotlin.collections.MutableMap
+import kotlin.collections.get
+import kotlin.collections.set
+import kotlin.collections.toTypedArray
 import java.util.Date
 
 object Serializer {
     @JvmStatic
-    val gsonBuilder: GsonBuilder = GsonBuilder()
+    var gsonBuilder = GsonFireBuilder()
+        .createGsonBuilder()
         .registerTypeAdapter(Date::class.java, DateAdapter())
         .registerTypeAdapter(OffsetDateTime::class.java, OffsetDateTimeAdapter())
         .registerTypeAdapter(LocalDateTime::class.java, LocalDateTimeAdapter())
         .registerTypeAdapter(LocalDate::class.java, LocalDateAdapter())
         .registerTypeAdapter(ByteArray::class.java, ByteArrayAdapter())
-    
+
     @JvmStatic
     val gson: Gson by lazy {
         gsonBuilder.create()
+    }
+
+    fun getDiscriminatorValue(readElement: JsonElement, discriminatorField: String?): String? {
+        val element: JsonElement = readElement.getAsJsonObject().get(discriminatorField)
+            ?: throw IllegalArgumentException("missing discriminator field: <$discriminatorField>")
+        return element.getAsString()
+    }
+
+    fun getClassByDiscriminator(
+        classByDiscriminatorValue: Map<*, *>,
+        discriminatorValue: String?
+    ): Class<*>? {
+        return classByDiscriminatorValue[discriminatorValue?.toUpperCase()] as Class<*>?
+            ?: throw IllegalArgumentException("cannot determine model class of name: <$discriminatorValue>")
     }
 }
