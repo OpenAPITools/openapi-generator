319b8d6c7832b80319b738e32619a986112473b6
William Cheng <wing328hk@gmail.com>
2020-08-07 16:35:44 +0800

    restore test in pet api test

--- samples/client/petstore/java/jersey2-java8/src/test/java/org/openapitools/client/api/PetApiTest.java
+++ samples/client/petstore/java/jersey2-java8/src/test/java/org/openapitools/client/api/PetApiTest.java
@@ -159,6 +159,27 @@ public class PetApiTest {
      */
     @Test
     public void getPetByIdTest() throws ApiException {
+        ApiClient defaultClient = Configuration.getDefaultApiClient();
+        defaultClient.setBasePath("http://petstore.swagger.io:80/v2");
+
+        // Configure API key authorization: api_key
+        ApiKeyAuth api_key = (ApiKeyAuth) defaultClient.getAuthentication("api_key");
+        api_key.setApiKey("YOUR API KEY");
+        // Uncomment the following line to set a prefix for the API key, e.g. "Token" (defaults to null)
+        //api_key.setApiKeyPrefix("Token");
+
+        PetApi apiInstance = new PetApi(defaultClient);
+        Long petId = 56L; // Long | ID of pet to return
+        try {
+            Pet result = apiInstance.getPetById(petId);
+            System.out.println(result);
+        } catch (ApiException e) {
+            System.err.println("Exception when calling PetApi#getPetById");
+            System.err.println("Status code: " + e.getCode());
+            System.err.println("Reason: " + e.getResponseBody());
+            System.err.println("Response headers: " + e.getResponseHeaders());
+        }
+
         //Long petId = null;
         //Pet response = api.getPetById(petId);
         // TODO: test validations
