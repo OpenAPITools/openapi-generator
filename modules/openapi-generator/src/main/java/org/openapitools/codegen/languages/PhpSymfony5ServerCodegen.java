b96b387c0b1a6aed7402891a9906ec2fdb4f41eb
Yuriy Belenko <yura-bely@mail.ru>
2020-08-14 14:22:56 +0300

    Generate composer file only

--- modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/PhpSymfony5ServerCodegen.java
+++ modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/PhpSymfony5ServerCodegen.java
@@ -17,8 +17,10 @@
 
 package org.openapitools.codegen.languages;
 
+import org.openapitools.codegen.SupportingFile;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import java.io.File;
 
 public class PhpSymfony5ServerCodegen extends PhpSymfonyServerCodegen {
     private static final Logger LOGGER = LoggerFactory.getLogger(PhpSymfony5ServerCodegen.class);
@@ -40,5 +42,47 @@ public class PhpSymfony5ServerCodegen extends PhpSymfonyServerCodegen {
     @Override
     public String getHelp() {
         return "Generates a PHP Symfony 5 server bundle.";
+    }
+
+    @Override
+    public void processOpts() {
+        super.processOpts();
+
+        final String configDir = "Resources" + File.separator + "config";
+        final String dependencyInjectionDir = "DependencyInjection";
+
+        supportingFiles.clear();
+        // supportingFiles.add(new SupportingFile("Controller.mustache", toSrcPath(controllerPackage, srcBasePath), "Controller.php"));
+        // supportingFiles.add(new SupportingFile("Bundle.mustache", "", bundleClassName + ".php"));
+        // supportingFiles.add(new SupportingFile("Extension.mustache", dependencyInjectionDir, bundleExtensionName + ".php"));
+        // supportingFiles.add(new SupportingFile("ApiPass.mustache", dependencyInjectionDir + File.separator + "Compiler", bundleName + "ApiPass.php"));
+        // supportingFiles.add(new SupportingFile("ApiServer.mustache", toSrcPath(apiPackage, srcBasePath), "ApiServer.php"));
+
+        // Serialization components
+        // supportingFiles.add(new SupportingFile("serialization/SerializerInterface.mustache", toSrcPath(servicePackage, srcBasePath), "SerializerInterface.php"));
+        // supportingFiles.add(new SupportingFile("serialization/JmsSerializer.mustache", toSrcPath(servicePackage, srcBasePath), "JmsSerializer.php"));
+        // supportingFiles.add(new SupportingFile("serialization/StrictJsonDeserializationVisitor.mustache", toSrcPath(servicePackage, srcBasePath), "StrictJsonDeserializationVisitor.php"));
+        // supportingFiles.add(new SupportingFile("serialization/TypeMismatchException.mustache", toSrcPath(servicePackage, srcBasePath), "TypeMismatchException.php"));
+        // Validation components
+        // supportingFiles.add(new SupportingFile("validation/ValidatorInterface.mustache", toSrcPath(servicePackage, srcBasePath), "ValidatorInterface.php"));
+        // supportingFiles.add(new SupportingFile("validation/SymfonyValidator.mustache", toSrcPath(servicePackage, srcBasePath), "SymfonyValidator.php"));
+
+        // Testing components
+        // supportingFiles.add(new SupportingFile("testing/phpunit.xml.mustache", "", "phpunit.xml.dist"));
+        // supportingFiles.add(new SupportingFile("testing/pom.xml", "", "pom.xml"));
+        // supportingFiles.add(new SupportingFile("testing/AppKernel.mustache", toSrcPath(testsPackage, srcBasePath), "AppKernel.php"));
+        // supportingFiles.add(new SupportingFile("testing/ControllerTest.mustache", toSrcPath(controllerTestsPackage, srcBasePath), "ControllerTest.php"));
+        // supportingFiles.add(new SupportingFile("testing/test_config.yml", toSrcPath(testsPackage, srcBasePath), "test_config.yml"));
+
+        // supportingFiles.add(new SupportingFile("routing.mustache", configDir, "routing.yml"));
+        // supportingFiles.add(new SupportingFile("services.mustache", configDir, "services.yml"));
+        supportingFiles.add(new SupportingFile("composer.mustache", "", "composer.json"));
+        // supportingFiles.add(new SupportingFile("autoload.mustache", "", "autoload.php"));
+        // supportingFiles.add(new SupportingFile("README.mustache", "", "README.md"));
+
+        // supportingFiles.add(new SupportingFile(".travis.yml", "", ".travis.yml"));
+        // supportingFiles.add(new SupportingFile(".php_cs.dist", "", ".php_cs.dist"));
+        // supportingFiles.add(new SupportingFile(".coveralls.yml", "", ".coveralls.yml"));
+        // supportingFiles.add(new SupportingFile("git_push.sh.mustache", "", "git_push.sh"));
     }
 }
