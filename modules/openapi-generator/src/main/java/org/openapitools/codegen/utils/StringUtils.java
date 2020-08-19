2279ba8113d5ebc2f4b48a40f84e8ac7e46f7e02
William Cheng <wing328hk@gmail.com>
2020-08-19 14:03:17 +0800

    Revert "[codegen][Python] Add model cache to speed up code generation (#7220)"
    This reverts commit bee03685f06ba60cf4ef2677a9b15bbc730e42af.

--- modules/openapi-generator/src/main/java/org/openapitools/codegen/utils/StringUtils.java
+++ modules/openapi-generator/src/main/java/org/openapitools/codegen/utils/StringUtils.java
@@ -7,13 +7,10 @@ import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.apache.commons.lang3.tuple.Pair;
 import org.openapitools.codegen.config.GlobalSettings;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import java.util.HashMap;
-import java.util.Objects;
-import java.util.Set;
 import java.util.concurrent.TimeUnit;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -21,11 +18,11 @@ import java.util.regex.Pattern;
 public class StringUtils {
 
     /**
-     * Set the cache size (entry count) of the sanitizedNameCache, camelizedWordsCache and underscoreWordsCache.
+     * Set the cache size (entry count) of the sanitizedNameCache, camelizedWordsCache and underscoreWords.
      */
     public static final String NAME_CACHE_SIZE_PROPERTY = "org.openapitools.codegen.utils.namecache.cachesize";
     /**
-     * Set the cache expiry (in seconds) of the sanitizedNameCache, camelizedWordsCache and underscoreWordsCache.
+     * Set the cache expiry (in seconds) of the sanitizedNameCache, camelizedWordsCache and underscoreWords.
      */
     public static final String NAME_CACHE_EXPIRY_PROPERTY = "org.openapitools.codegen.utils.namecache.expireafter.seconds";
 
@@ -34,11 +31,7 @@ public class StringUtils {
     private static Cache<Pair<String, Boolean>, String> camelizedWordsCache;
 
     // A cache of underscored words, used to optimize the performance of the underscore() method.
-    private static Cache<String, String> underscoreWordsCache;
-
-    // A cache of escaped words, used to optimize the performance of the escape() method.
-    private static Cache<EscapedNameOptions, String> escapedWordsCache;
-
+    private static Cache<String, String> underscoreWords;
     static {
         int cacheSize = Integer.parseInt(GlobalSettings.getProperty(NAME_CACHE_SIZE_PROPERTY, "200"));
         int cacheExpiry = Integer.parseInt(GlobalSettings.getProperty(NAME_CACHE_EXPIRY_PROPERTY, "5"));
@@ -48,23 +41,12 @@ public class StringUtils {
                 .ticker(Ticker.systemTicker())
                 .build();
 
-        escapedWordsCache = Caffeine.newBuilder()
-                .maximumSize(cacheSize)
-                .expireAfterAccess(cacheExpiry, TimeUnit.SECONDS)
-                .ticker(Ticker.systemTicker())
-                .build();
-
-        underscoreWordsCache = Caffeine.newBuilder()
+        underscoreWords = Caffeine.newBuilder()
                 .maximumSize(cacheSize)
                 .expireAfterAccess(cacheExpiry, TimeUnit.SECONDS)
                 .ticker(Ticker.systemTicker())
                 .build();
     }
-
-    private static Pattern capitalLetterPattern = Pattern.compile("([A-Z]+)([A-Z][a-z])");
-    private static Pattern lowercasePattern = Pattern.compile("([a-z\\d])([A-Z])");
-    private static Pattern pkgSeparatorPattern = Pattern.compile("\\.");
-    private static Pattern dollarPattern = Pattern.compile("\\$");
 
     /**
      * Underscore the given word.
@@ -75,16 +57,18 @@ public class StringUtils {
      * @return The underscored version of the word
      */
     public static String underscore(final String word) {
-        return underscoreWordsCache.get(word, wordToUnderscore -> {
+        return underscoreWords.get(word, wordToUnderscore -> {
             String result;
+            String firstPattern = "([A-Z]+)([A-Z][a-z])";
+            String secondPattern = "([a-z\\d])([A-Z])";
             String replacementPattern = "$1_$2";
             // Replace package separator with slash.
-            result = pkgSeparatorPattern.matcher(wordToUnderscore).replaceAll("/");
+            result = wordToUnderscore.replaceAll("\\.", "/");
             // Replace $ with two underscores for inner classes.
-            result = dollarPattern.matcher(result).replaceAll("__");
+            result = result.replaceAll("\\$", "__");
             // Replace capital letter with _ plus lowercase letter.
-            result = capitalLetterPattern.matcher(result).replaceAll(replacementPattern);
-            result = lowercasePattern.matcher(result).replaceAll(replacementPattern);
+            result = result.replaceAll(firstPattern, replacementPattern);
+            result = result.replaceAll(secondPattern, replacementPattern);
             result = result.replace('-', '_');
             // replace space with underscore
             result = result.replace(' ', '_');
@@ -119,8 +103,6 @@ public class StringUtils {
     private static Pattern camelizeUppercasePattern = Pattern.compile("(\\.?)(\\w)([^\\.]*)$");
     private static Pattern camelizeUnderscorePattern = Pattern.compile("(_)(.)");
     private static Pattern camelizeHyphenPattern = Pattern.compile("(-)(.)");
-    private static Pattern camelizeDollarPattern = Pattern.compile("\\$");
-    private static Pattern camelizeSimpleUnderscorePattern = Pattern.compile("_");
 
     /**
      * Camelize name (parameter, property, method, etc)
@@ -162,7 +144,7 @@ public class StringUtils {
             m = camelizeUppercasePattern.matcher(word);
             if (m.find()) {
                 String rep = m.group(1) + m.group(2).toUpperCase(Locale.ROOT) + m.group(3);
-                rep = camelizeDollarPattern.matcher(rep).replaceAll("\\\\\\$");
+                rep = rep.replaceAll("\\$", "\\\\\\$");
                 word = m.replaceAll(rep);
             }
 
@@ -172,7 +154,7 @@ public class StringUtils {
                 String original = m.group(2);
                 String upperCase = original.toUpperCase(Locale.ROOT);
                 if (original.equals(upperCase)) {
-                    word = camelizeSimpleUnderscorePattern.matcher(word).replaceFirst("");
+                    word = word.replaceFirst("_", "");
                 } else {
                     word = m.replaceFirst(upperCase);
                 }
@@ -198,47 +180,9 @@ public class StringUtils {
             }
 
             // remove all underscore
-            word = camelizeSimpleUnderscorePattern.matcher(word).replaceAll("");
+            word = word.replaceAll("_", "");
             return word;
         });
-    }
-
-    private static class EscapedNameOptions {
-        public EscapedNameOptions(String name, Set<String> specialChars, List<String> charactersToAllow, String appendToReplacement) {
-            this.name = name;
-            this.appendToReplacement = appendToReplacement;
-            if (specialChars != null) {
-                this.specialChars = Collections.unmodifiableSet(specialChars);
-            } else {
-                this.specialChars = Collections.emptySet();
-            }
-            if (charactersToAllow != null) {
-                this.charactersToAllow = Collections.unmodifiableList(charactersToAllow);
-            } else {
-                this.charactersToAllow = Collections.emptyList();
-            }
-        }
-
-        private String name;
-        private String appendToReplacement;
-        private Set<String> specialChars;
-        private List<String> charactersToAllow;
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            EscapedNameOptions that = (EscapedNameOptions) o;
-            return Objects.equals(name, that.name) &&
-                    Objects.equals(appendToReplacement, that.appendToReplacement) &&
-                    Objects.equals(specialChars, that.specialChars) &&
-                    Objects.equals(charactersToAllow, that.charactersToAllow);
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(name, appendToReplacement, specialChars, charactersToAllow);
-        }
     }
 
     /**
@@ -252,23 +196,20 @@ public class StringUtils {
      * <p>
      * throws Runtime exception as word is not escaped properly.
      */
-    public static String escape(final String name, final Map<String, String> replacementMap,
-                                final List<String> charactersToAllow, final String appendToReplacement) {
-        EscapedNameOptions ns = new EscapedNameOptions(name, replacementMap.keySet(), charactersToAllow, appendToReplacement);
-        return escapedWordsCache.get(ns, wordToEscape -> {
-            String result = name.chars().mapToObj(c -> {
-                String character = "" + (char) c;
-                if (charactersToAllow != null && charactersToAllow.contains(character)) {
-                    return character;
-                } else if (replacementMap.containsKey(character)) {
-                    return replacementMap.get(character) + (appendToReplacement != null ? appendToReplacement: "");
-                } else {
-                    return character;
-                }
-            }).reduce( (c1, c2) -> "" + c1 + c2).orElse(null);
-    
-            if (result != null) return result;
-            throw new RuntimeException("Word '" + name + "' could not be escaped.");    
-        });
+    public static String escape(String name, Map<String, String> replacementMap,
+                                List<String> charactersToAllow, String appendToReplacement) {
+        String result = name.chars().mapToObj(c -> {
+            String character = "" + (char) c;
+            if (charactersToAllow != null && charactersToAllow.contains(character)) {
+                return character;
+            } else if (replacementMap.containsKey(character)) {
+                return replacementMap.get(character) + (appendToReplacement != null ? appendToReplacement: "");
+            } else {
+                return character;
+            }
+        }).reduce( (c1, c2) -> "" + c1 + c2).orElse(null);
+
+        if (result != null) return result;
+        throw new RuntimeException("Word '" + name + "' could not be escaped.");
     }
 }
