1f95199f822e6adb23c1920ff5b67d5879a7a9e0
William Cheng <wing328hk@gmail.com>
2020-08-28 19:17:20 +0800

    update spring config to use java8 (#7308)

--- samples/server/petstore/spring-mvc/src/main/java/org/openapitools/model/Order.java
+++ samples/server/petstore/spring-mvc/src/main/java/org/openapitools/model/Order.java
@@ -6,7 +6,7 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonValue;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
-import org.threeten.bp.OffsetDateTime;
+import java.time.OffsetDateTime;
 import org.openapitools.jackson.nullable.JsonNullable;
 import javax.validation.Valid;
 import javax.validation.constraints.*;
