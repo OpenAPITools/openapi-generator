aa6dcdd20dadbc325aaa0a6d776dd9c937545489
Katherine Papcun <kpapcun@mimsoftware.com>
2020-09-16 11:31:44 -0400

    add profile and generate samples

---
+++ samples/client/petstore/java/retrofit2-jackson/src/main/java/org/openapitools/client/api/FakeClassnameTags123Api.java
@@ -0,0 +1,34 @@
+package org.openapitools.client.api;
+
+import org.openapitools.client.CollectionFormats.*;
+
+import retrofit2.Call;
+import retrofit2.http.*;
+
+import okhttp3.RequestBody;
+import okhttp3.ResponseBody;
+import okhttp3.MultipartBody;
+
+import org.openapitools.client.model.Client;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public interface FakeClassnameTags123Api {
+  /**
+   * To test class name in snake case
+   * To test class name in snake case
+   * @param body client model (required)
+   * @return Call&lt;Client&gt;
+   */
+  @Headers({
+    "Content-Type:application/json"
+  })
+  @PATCH("fake_classname_test")
+  Call<Client> testClassname(
+    @retrofit2.http.Body Client body
+  );
+
+}
