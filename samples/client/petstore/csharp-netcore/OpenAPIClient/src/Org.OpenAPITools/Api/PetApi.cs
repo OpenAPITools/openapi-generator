9d9a64bc25228bf19a6ae493a7ac8e458f7f7779
William Cheng <wing328hk@gmail.com>
2020-09-17 17:14:18 +0800

    update samples

--- samples/client/petstore/csharp-netcore/OpenAPIClient/src/Org.OpenAPITools/Api/PetApi.cs
+++ samples/client/petstore/csharp-netcore/OpenAPIClient/src/Org.OpenAPITools/Api/PetApi.cs
@@ -603,22 +603,6 @@ namespace Org.OpenAPITools.Api
 
             localVarRequestOptions.Data = pet;
 
-            // authentication (http_signature_test) required
-            if(this.Configuration.HTTPSigningConfiguration != null)
-			{
-				var HttpSigningHeaders = this.Configuration.HTTPSigningConfiguration.GetHttpSignedHeader(this.Configuration.BasePath, "POST", "/pet", localVarRequestOptions);
-				foreach (var headerItem in HttpSigningHeaders)
-				{
-					if (localVarRequestOptions.HeaderParameters.ContainsKey(headerItem.Key))
-					{
-						localVarRequestOptions.HeaderParameters[headerItem.Key] = new List<string>() { headerItem.Value };
-					}
-					else
-					{
-						localVarRequestOptions.HeaderParameters.Add(headerItem.Key, headerItem.Value);
-					}
-				}
-			}
             // authentication (petstore_auth) required
             // oauth required
             if (!String.IsNullOrEmpty(this.Configuration.AccessToken))
@@ -684,7 +668,6 @@ namespace Org.OpenAPITools.Api
 
             localVarRequestOptions.Data = pet;
 
-            // authentication (http_signature_test) required
             // authentication (petstore_auth) required
             // oauth required
             if (!String.IsNullOrEmpty(this.Configuration.AccessToken))
@@ -875,22 +858,6 @@ namespace Org.OpenAPITools.Api
 
             localVarRequestOptions.QueryParameters.Add(Org.OpenAPITools.Client.ClientUtils.ParameterToMultiMap("csv", "status", status));
 
-            // authentication (http_signature_test) required
-            if(this.Configuration.HTTPSigningConfiguration != null)
-			{
-				var HttpSigningHeaders = this.Configuration.HTTPSigningConfiguration.GetHttpSignedHeader(this.Configuration.BasePath, "GET", "/pet/findByStatus", localVarRequestOptions);
-				foreach (var headerItem in HttpSigningHeaders)
-				{
-					if (localVarRequestOptions.HeaderParameters.ContainsKey(headerItem.Key))
-					{
-						localVarRequestOptions.HeaderParameters[headerItem.Key] = new List<string>() { headerItem.Value };
-					}
-					else
-					{
-						localVarRequestOptions.HeaderParameters.Add(headerItem.Key, headerItem.Value);
-					}
-				}
-			}
             // authentication (petstore_auth) required
             // oauth required
             if (!String.IsNullOrEmpty(this.Configuration.AccessToken))
@@ -957,7 +924,6 @@ namespace Org.OpenAPITools.Api
 
             localVarRequestOptions.QueryParameters.Add(Org.OpenAPITools.Client.ClientUtils.ParameterToMultiMap("csv", "status", status));
 
-            // authentication (http_signature_test) required
             // authentication (petstore_auth) required
             // oauth required
             if (!String.IsNullOrEmpty(this.Configuration.AccessToken))
@@ -1021,22 +987,6 @@ namespace Org.OpenAPITools.Api
 
             localVarRequestOptions.QueryParameters.Add(Org.OpenAPITools.Client.ClientUtils.ParameterToMultiMap("csv", "tags", tags));
 
-            // authentication (http_signature_test) required
-            if(this.Configuration.HTTPSigningConfiguration != null)
-			{
-				var HttpSigningHeaders = this.Configuration.HTTPSigningConfiguration.GetHttpSignedHeader(this.Configuration.BasePath, "GET", "/pet/findByTags", localVarRequestOptions);
-				foreach (var headerItem in HttpSigningHeaders)
-				{
-					if (localVarRequestOptions.HeaderParameters.ContainsKey(headerItem.Key))
-					{
-						localVarRequestOptions.HeaderParameters[headerItem.Key] = new List<string>() { headerItem.Value };
-					}
-					else
-					{
-						localVarRequestOptions.HeaderParameters.Add(headerItem.Key, headerItem.Value);
-					}
-				}
-			}
             // authentication (petstore_auth) required
             // oauth required
             if (!String.IsNullOrEmpty(this.Configuration.AccessToken))
@@ -1103,7 +1053,6 @@ namespace Org.OpenAPITools.Api
 
             localVarRequestOptions.QueryParameters.Add(Org.OpenAPITools.Client.ClientUtils.ParameterToMultiMap("csv", "tags", tags));
 
-            // authentication (http_signature_test) required
             // authentication (petstore_auth) required
             // oauth required
             if (!String.IsNullOrEmpty(this.Configuration.AccessToken))
@@ -1285,22 +1234,6 @@ namespace Org.OpenAPITools.Api
 
             localVarRequestOptions.Data = pet;
 
-            // authentication (http_signature_test) required
-            if(this.Configuration.HTTPSigningConfiguration != null)
-			{
-				var HttpSigningHeaders = this.Configuration.HTTPSigningConfiguration.GetHttpSignedHeader(this.Configuration.BasePath, "PUT", "/pet", localVarRequestOptions);
-				foreach (var headerItem in HttpSigningHeaders)
-				{
-					if (localVarRequestOptions.HeaderParameters.ContainsKey(headerItem.Key))
-					{
-						localVarRequestOptions.HeaderParameters[headerItem.Key] = new List<string>() { headerItem.Value };
-					}
-					else
-					{
-						localVarRequestOptions.HeaderParameters.Add(headerItem.Key, headerItem.Value);
-					}
-				}
-			}
             // authentication (petstore_auth) required
             // oauth required
             if (!String.IsNullOrEmpty(this.Configuration.AccessToken))
@@ -1366,7 +1299,6 @@ namespace Org.OpenAPITools.Api
 
             localVarRequestOptions.Data = pet;
 
-            // authentication (http_signature_test) required
             // authentication (petstore_auth) required
             // oauth required
             if (!String.IsNullOrEmpty(this.Configuration.AccessToken))
