79e83eed87a42ba8e6afdffc4ad7ae03c5c57689
William Cheng <wing328hk@gmail.com>
2020-09-23 19:01:39 +0800

    add option to set recursion limit

--- modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/PythonClientCodegen.java
+++ modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/PythonClientCodegen.java
@@ -43,6 +43,7 @@ public class PythonClientCodegen extends DefaultCodegen implements CodegenConfig
     public static final String DEFAULT_LIBRARY = "urllib3";
     // nose is a python testing framework, we use pytest if USE_NOSE is unset
     public static final String USE_NOSE = "useNose";
+    public static final String SET_RECURSION_LIMIT = "setRecursionLimit";
 
     protected String packageName = "openapi_client";
     protected String packageVersion = "1.0.0";
@@ -184,6 +185,7 @@ public class PythonClientCodegen extends DefaultCodegen implements CodegenConfig
                 .defaultValue(Boolean.FALSE.toString()));
         cliOptions.add(CliOption.newBoolean(USE_NOSE, "use the nose test framework").
                 defaultValue(Boolean.FALSE.toString()));
+        cliOptions.add(new CliOption(SET_RECURSION_LIMIT, "Set recursion limit. If not, using the system default value."));
 
         supportedLibraries.put("urllib3", "urllib3-based client");
         supportedLibraries.put("asyncio", "Asyncio-based client (python 3.5+)");
@@ -251,6 +253,15 @@ public class PythonClientCodegen extends DefaultCodegen implements CodegenConfig
 
         if (additionalProperties.containsKey(USE_NOSE)) {
             setUseNose((String) additionalProperties.get(USE_NOSE));
+        }
+
+        // check to see if setRecursionLimit is set and whether it's an integer
+        if (additionalProperties.containsKey(SET_RECURSION_LIMIT)) {
+            try {
+                Integer.parseInt((String)additionalProperties.get(SET_RECURSION_LIMIT));
+            } catch(NumberFormatException | NullPointerException e) {
+                throw new IllegalArgumentException("setRecursionLimit must be an integer, e.g. 2000.");
+            }
         }
 
         String readmePath = "README.md";
