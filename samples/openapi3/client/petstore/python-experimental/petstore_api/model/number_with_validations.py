336521b464d9edf42002a89d956499a4f73986e4
Slavek Kabrda <slavek.kabrda@datadoghq.com>
2020-09-29 19:56:52 +0200

    [Python-experimental] Don't mandate passing in required value as argument (#7424)
    * [Python-experimental] Don't mandate passing in required value as argument
    
    * Remove usage of have_value
    
    * Address additional review comments
    
    * Fix tests according to the code change
    
    * Does not set None as value initial value, removes hasRequired tag usage, updates docstring
    
    Co-authored-by: Justin Black <justin.a.black@gmail.com>

--- samples/openapi3/client/petstore/python-experimental/petstore_api/model/number_with_validations.py
+++ samples/openapi3/client/petstore/python-experimental/petstore_api/model/number_with_validations.py
@@ -98,13 +98,16 @@ class NumberWithValidations(ModelSimple):
     ])
 
     @convert_js_args_to_python_args
-    def __init__(self, value, *args, **kwargs):
+    def __init__(self, *args, **kwargs):
         """NumberWithValidations - a model defined in OpenAPI
 
+        Note that value can be passed either in args or in kwargs, but not in both.
+
         Args:
-            value (float):  # noqa: E501
+            args[0] (float):  # noqa: E501
 
         Keyword Args:
+            value (float):  # noqa: E501
             _check_type (bool): if True, values for parameters in openapi_types
                                 will be type checked and a TypeError will be
                                 raised if the wrong type is input.
@@ -136,6 +139,18 @@ class NumberWithValidations(ModelSimple):
                                 through its discriminator because we passed in
                                 _visited_composed_classes = (Animal,)
         """
+
+        if 'value' in kwargs:
+            value = kwargs.pop('value')
+        elif args:
+            args = list(args)
+            value = args.pop(0)
+        else:
+            raise ApiTypeError(
+                "value is required, but not passed in args or kwargs and doesn't have default",
+                path_to_item=_path_to_item,
+                valid_classes=(self.__class__,),
+            )
 
         _check_type = kwargs.pop('_check_type', True)
         _spec_property_naming = kwargs.pop('_spec_property_naming', False)
