3d36d491dfe5f76619823ff03e6b037af99d4ed2
William Cheng <wing328hk@gmail.com>
2020-10-08 23:00:36 +0800

    replace tab with spaces (#7625)

--- samples/client/petstore/java/microprofile-rest-client/src/main/java/org/openapitools/client/model/Order.java
+++ samples/client/petstore/java/microprofile-rest-client/src/main/java/org/openapitools/client/model/Order.java
@@ -43,7 +43,7 @@ public class Order  {
   @JsonbTypeDeserializer(StatusEnum.Deserializer.class)
   public enum StatusEnum {
 
-    PLACED(String.valueOf("placed")), APPROVED(String.valueOf("approved")), DELIVERED(String.valueOf("delivered"));	
+    PLACED(String.valueOf("placed")), APPROVED(String.valueOf("approved")), DELIVERED(String.valueOf("delivered"));    
 
 
     String value;
