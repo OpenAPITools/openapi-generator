56d221c6eca662b911d7b7f700ba0a07f9470c6a
Martin Goldhahn <mgoldhahn@gmail.com>
2020-10-09 18:39:02 +0200

    rebased changes from synchronizing branches

--- samples/server/petstore/springboot-reactive/src/main/java/org/openapitools/api/FakeApi.java
+++ samples/server/petstore/springboot-reactive/src/main/java/org/openapitools/api/FakeApi.java
@@ -19,12 +19,14 @@ import org.openapitools.model.XmlItem;
 import io.swagger.annotations.*;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import org.springframework.http.codec.multipart.Part;
+import springfox.documentation.annotations.ApiIgnore;
 
 import javax.validation.Valid;
 import javax.validation.constraints.*;
