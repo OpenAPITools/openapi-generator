65764c35a7467a28262ccfbc9ce0b53654a9f06e
berlysia <berlysia@gmail.com>
2020-10-19 04:12:20 +0900

    [typescript-fetch] add sample

---
+++ samples/client/petstore/typescript-fetch/builds/default-v3.0/apis/index.ts
@@ -0,0 +1,7 @@
+export * from './AnotherFakeApi';
+export * from './DefaultApi';
+export * from './FakeApi';
+export * from './FakeClassnameTags123Api';
+export * from './PetApi';
+export * from './StoreApi';
+export * from './UserApi';
