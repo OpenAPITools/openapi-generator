afd3deb59aa483fdada66efcef2554fc2b9768e4
Yuriy Belenko <yura-bely@mail.ru>
2020-10-20 22:13:15 +0300

    Refresh samples

--- samples/client/petstore/php/OpenAPIClient-php/lib/Model/ModelReturn.php
+++ samples/client/petstore/php/OpenAPIClient-php/lib/Model/ModelReturn.php
@@ -44,7 +44,7 @@ use \OpenAPI\Client\ObjectSerializer;
  * @template TKey int|null
  * @template TValue mixed|null  
  */
-class ModelReturn implements ModelInterface, ArrayAccess
+class ModelReturn implements ModelInterface, ArrayAccess, \JsonSerializable
 {
     public const DISCRIMINATOR = null;
 
@@ -284,6 +284,18 @@ class ModelReturn implements ModelInterface, ArrayAccess
     public function offsetUnset($offset)
     {
         unset($this->container[$offset]);
+    }
+
+    /**
+     * Serializes the object to a value that can be serialized natively by json_encode().
+     * @link https://www.php.net/manual/en/jsonserializable.jsonserialize.php
+     *
+     * @return mixed Returns data which can be serialized by json_encode(), which is a value
+     * of any type other than a resource.
+     */
+    public function jsonSerialize()
+    {
+       return ObjectSerializer::sanitizeForSerialization($this);
     }
 
     /**
