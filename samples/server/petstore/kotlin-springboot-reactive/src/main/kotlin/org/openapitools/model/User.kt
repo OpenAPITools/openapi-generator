71dcd043a280556610bc9a1aafb270871b70dfac
Dmitry Polyakov <dmitry.polyakov987@gmail.com>
2020-11-27 10:01:13 +0300

    fix kotlin-spring generator does not add @Valid annotation to nested classes

--- samples/server/petstore/kotlin-springboot-reactive/src/main/kotlin/org/openapitools/model/User.kt
+++ samples/server/petstore/kotlin-springboot-reactive/src/main/kotlin/org/openapitools/model/User.kt
@@ -9,6 +9,7 @@ import javax.validation.constraints.Min
 import javax.validation.constraints.NotNull
 import javax.validation.constraints.Pattern
 import javax.validation.constraints.Size
+import javax.validation.Valid
 import io.swagger.annotations.ApiModelProperty
 
 /**
