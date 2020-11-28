835bf472de89592c9241fe5866cc2e4ac6a283a3
Ülgen Sarıkavak <ulgensrkvk@gmail.com>
2020-11-28 07:43:11 +0300

    Update petstore example

--- samples/openapi3/client/features/dynamic-servers/python/setup.py
+++ samples/openapi3/client/features/dynamic-servers/python/setup.py
@@ -1,5 +1,3 @@
-# coding: utf-8
-
 """
     OpenAPI Extension with dynamic servers
 
@@ -36,7 +34,7 @@ setup(
     author_email="team@openapitools.org",
     url="",
     keywords=["OpenAPI", "OpenAPI-Generator", "OpenAPI Extension with dynamic servers"],
-    python_requires=">=3.5",
+    python_requires=">=3.6",
     install_requires=REQUIRES,
     packages=find_packages(exclude=["test", "tests"]),
     include_package_data=True,
