ca0004f8b8f198b1a3b1904f441e9af74df0e4bd
Bruno Coelho <4brunu@gmail.com>
2020-12-31 14:41:06 +0000

    [swift] create a sample of bearer token authentication with URLSession

--- samples/client/petstore/swift5/urlsessionLibrary/SwaggerClientTests/SwaggerClient/BearerDecodableRequestBuilder.swift
+++ samples/client/petstore/swift5/urlsessionLibrary/SwaggerClientTests/SwaggerClient/BearerDecodableRequestBuilder.swift
@@ -21,30 +21,40 @@ class BearerRequestBuilderFactory: RequestBuilderFactory {
 
 class BearerRequestBuilder<T>: URLSessionRequestBuilder<T> {
     override func execute(_ apiResponseQueue: DispatchQueue = PetstoreClientAPI.apiResponseQueue, _ completion: @escaping (Result<Response<T>, Error>) -> Void) {
-
+        
+        // Before making the request, we can validate if we have a bearer token to be able to make a request
         BearerTokenHandler.refreshTokenIfDoesntExist {
             
+            // Here we make the request
             super.execute(apiResponseQueue) { result in
                 
                 switch result {
                 case .success:
+                    // If we got a successful response, we send the response to the completion block
                     completion(result)
                     
                 case let .failure(error):
                     
+                    // If we got a failure response, we will analyse the error to see what we should do with it
                     if case let ErrorResponse.error(_, data, response, error) = error {
+                        
+                        // If the error is an ErrorResponse.error() we will analyse it to see if it's a 401, and if it's a 401, we will refresh the token and retry the request
                         BearerTokenHandler.refreshTokenIfUnauthorizedRequestResponse(
                             data: data,
                             response: response,
                             error: error
-                        ) { isTokenRefreshed in
-                            if isTokenRefreshed {
+                        ) { wasTokenRefreshed in
+                            
+                            if wasTokenRefreshed {
+                                // If the token was refreshed, it's because it was a 401 error, so we refreshed the token, and we are going to retry the request by calling self.execute()
                                 self.execute(apiResponseQueue, completion)
                             } else {
+                                // If the token was not refreshed, it's because it was not a 401 error, so we send the response to the completion block
                                 completion(result)
                             }
                         }
                     } else {
+                        // If it's an unknown error, we send the response to the completion block
                         completion(result)
                     }
                     
@@ -56,30 +66,40 @@ class BearerRequestBuilder<T>: URLSessionRequestBuilder<T> {
 
 class BearerDecodableRequestBuilder<T: Decodable>: URLSessionDecodableRequestBuilder<T> {
     override func execute(_ apiResponseQueue: DispatchQueue = PetstoreClientAPI.apiResponseQueue, _ completion: @escaping (Result<Response<T>, Error>) -> Void) {
-
+        
+        // Before making the request, we can validate if we have a bearer token to be able to make a request
         BearerTokenHandler.refreshTokenIfDoesntExist {
             
+            // Here we make the request
             super.execute(apiResponseQueue) { result in
                 
                 switch result {
                 case .success:
+                    // If we got a successful response, we send the response to the completion block
                     completion(result)
                     
                 case let .failure(error):
                     
+                    // If we got a failure response, we will analyse the error to see what we should do with it
                     if case let ErrorResponse.error(_, data, response, error) = error {
+                        
+                        // If the error is an ErrorResponse.error() we will analyse it to see if it's a 401, and if it's a 401, we will refresh the token and retry the request
                         BearerTokenHandler.refreshTokenIfUnauthorizedRequestResponse(
                             data: data,
                             response: response,
                             error: error
-                        ) { isTokenRefreshed in
-                            if isTokenRefreshed {
+                        ) { wasTokenRefreshed in
+                            
+                            if wasTokenRefreshed {
+                                // If the token was refreshed, it's because it was a 401 error, so we refreshed the token, and we are going to retry the request by calling self.execute()
                                 self.execute(apiResponseQueue, completion)
                             } else {
+                                // If the token was not refreshed, it's because it was not a 401 error, so we send the response to the completion block
                                 completion(result)
                             }
                         }
                     } else {
+                        // If it's an unknown error, we send the response to the completion block
                         completion(result)
                     }
                     
