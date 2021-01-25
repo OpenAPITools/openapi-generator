ec60f603598abf0f57dfdb6ca55b4d5c1ee25edf
nanashi0x74 <rian.lindenberger@mpq.se>
2021-01-25 22:01:37 +0100

    regenerate samples, make shebangs portable

--- samples/openapi3/client/petstore/dart2/petstore_client_lib_fake/lib/api_client.dart
+++ samples/openapi3/client/petstore/dart2/petstore_client_lib_fake/lib/api_client.dart
@@ -45,6 +45,12 @@ class ApiClient {
      _defaultHeaderMap[key] = value;
   }
 
+  Map<String,String> get defaultHeaderMap => _defaultHeaderMap;
+  /// returns an unmodifiable view of the authentications, since none should be added
+  /// nor deleted
+  Map<String, Authentication> get authentications =>
+      Map.unmodififiable(_authentications);
+
   dynamic deserialize(String json, String targetType, {bool growable}) {
     // Remove all spaces.  Necessary for reg expressions as well.
     targetType = targetType.replaceAll(' ', '');
