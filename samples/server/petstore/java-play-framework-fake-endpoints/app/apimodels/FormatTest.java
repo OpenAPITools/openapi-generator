fd9bd206cd7614fcc9bfa825805689b7597debf5
Lavanya <y509418@softwareag.com>
2021-02-09 11:23:35 +0530

    Updating samples 2nd try

--- samples/server/petstore/java-play-framework-fake-endpoints/app/apimodels/FormatTest.java
+++ samples/server/petstore/java-play-framework-fake-endpoints/app/apimodels/FormatTest.java
@@ -59,7 +59,7 @@ public class FormatTest   {
 
   @JsonProperty("byte")
   @NotNull
-@Pattern(regexp="^(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=)?$")
+@Pattern(regexp="/^(?:[A-Za-z0-9+\/]{4})*(?:[A-Za-z0-9+\/]{2}==|[A-Za-z0-9+\/]{3}=)?$/")
 
   private byte[] _byte;
 
