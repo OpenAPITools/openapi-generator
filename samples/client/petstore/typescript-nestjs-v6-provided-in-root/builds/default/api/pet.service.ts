34c77c2807b29412f4c6fd625dd6a017652ad0bd
Rafael Ceron <rfceron@gmail.com>
2021-02-25 02:43:21 -0500

    typescript-nestjs: removed unused basePath from api.service template (#8811)
    * (nestjs) removed unused basePath from api.service template 8810
    
    * Update modules/openapi-generator/src/main/resources/typescript-nestjs/api.service.mustache
    
    Co-authored-by: Esteban Gehring <esteban.gehring@gmail.com>
    
    * added missing parenthesis on Global() decorator
    
    Co-authored-by: Esteban Gehring <esteban.gehring@gmail.com>

--- samples/client/petstore/typescript-nestjs-v6-provided-in-root/builds/default/api/pet.service.ts
+++ samples/client/petstore/typescript-nestjs-v6-provided-in-root/builds/default/api/pet.service.ts
@@ -11,13 +11,12 @@
  */
 /* tslint:disable:no-unused-variable member-ordering */
 
-import { HttpService, Inject, Injectable }                      from '@nestjs/common';
+import { HttpService, Inject, Injectable, Optional } from '@nestjs/common';
 import { AxiosResponse } from 'axios';
-import { Observable }                                        from 'rxjs';
+import { Observable } from 'rxjs';
 import { ApiResponse } from '../model/apiResponse';
 import { Pet } from '../model/pet';
-import { Configuration }                                     from '../configuration';
-import { COLLECTION_FORMATS }                     from '../variables';
+import { Configuration } from '../configuration';
 
 
 @Injectable()
@@ -27,9 +26,9 @@ export class PetService {
     public defaultHeaders = new Map()
     public configuration = new Configuration();
 
-    constructor(protected httpClient: HttpService, configuration: Configuration) {
-        this.configuration = configuration;
-        this.basePath = basePath || configuration.basePath || this.basePath;
+    constructor(protected httpClient: HttpService, @Optional() configuration: Configuration) {
+        this.configuration = configuration || this.configuration;
+        this.basePath = configuration?.basePath || this.basePath;
     }
 
     /**
