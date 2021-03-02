7fd2b4a55e71b96f14776f74d83d4eae75c5f358
Peter Leibiger <kuhnroyal@gmail.com>
2021-03-02 15:30:27 +0100

    Fix import in testcase

--- samples/openapi3/client/petstore/dart-dio-next/petstore_client_lib_fake_tests/test/api_util_test.dart
+++ samples/openapi3/client/petstore/dart-dio-next/petstore_client_lib_fake_tests/test/api_util_test.dart
@@ -2,6 +2,7 @@ import 'package:built_collection/built_collection.dart';
 import 'package:built_value/serializer.dart';
 import 'package:dio/dio.dart';
 import 'package:openapi/openapi.dart';
+import 'package:openapi/src/api_util.dart';
 import 'package:test/test.dart';
 
 void main() {
