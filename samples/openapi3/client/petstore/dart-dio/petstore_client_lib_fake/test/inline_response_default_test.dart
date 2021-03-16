c26a06ad7e9a95f00acb7b338a965c8ea3f57c92
Peter Leibiger <kuhnroyal@gmail.com>
2021-03-17 00:36:00 +0100

    [dart] Ensure tests compile with Dart 2.12+

--- samples/openapi3/client/petstore/dart-dio/petstore_client_lib_fake/test/inline_response_default_test.dart
+++ samples/openapi3/client/petstore/dart-dio/petstore_client_lib_fake/test/inline_response_default_test.dart
@@ -1,3 +1,10 @@
+//
+// AUTO-GENERATED FILE, DO NOT MODIFY!
+//
+// @dart=2.7
+
+// ignore_for_file: unused_import
+
 import 'package:openapi/model/inline_response_default.dart';
 import 'package:test/test.dart';
 
