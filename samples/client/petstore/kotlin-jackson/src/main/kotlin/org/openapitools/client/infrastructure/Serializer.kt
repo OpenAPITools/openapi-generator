a2abdfbf6a9522ea10bf3dd9e493d27aec07658c
Sondre Eikanger Kvalø <zapodot@gmail.com>
2021-03-16 16:20:11 +0100

    Add generated sample from previous commit

--- samples/client/petstore/kotlin-jackson/src/main/kotlin/org/openapitools/client/infrastructure/Serializer.kt
+++ samples/client/petstore/kotlin-jackson/src/main/kotlin/org/openapitools/client/infrastructure/Serializer.kt
@@ -1,9 +1,9 @@
 package org.openapitools.client.infrastructure
 
-    import com.fasterxml.jackson.databind.ObjectMapper
-    import com.fasterxml.jackson.databind.SerializationFeature
-    import com.fasterxml.jackson.annotation.JsonInclude
-    import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import java.util.Date
 
 object Serializer {
