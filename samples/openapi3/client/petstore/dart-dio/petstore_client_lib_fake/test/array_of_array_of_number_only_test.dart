a307ab38b04cd98d4d67fe0a5ff68c226378a1b8
Peter Leibiger <kuhnroyal@gmail.com>
2021-03-17 14:53:48 +0100

    [dart] Ensure tests compile with Dart 2.12+ (#8984)

--- samples/openapi3/client/petstore/dart-dio/petstore_client_lib_fake/test/array_of_array_of_number_only_test.dart
+++ samples/openapi3/client/petstore/dart-dio/petstore_client_lib_fake/test/array_of_array_of_number_only_test.dart
@@ -1,3 +1,10 @@
+//
+// AUTO-GENERATED FILE, DO NOT MODIFY!
+//
+// @dart=2.7
+
+// ignore_for_file: unused_import
+
 import 'package:openapi/model/array_of_array_of_number_only.dart';
 import 'package:test/test.dart';
 
