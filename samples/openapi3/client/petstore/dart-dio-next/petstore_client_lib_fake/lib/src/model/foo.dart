3d4a845bc9b857bdd41b8868bd3482c75148b36b
William Cheng <wing328hk@gmail.com>
2021-03-20 16:45:05 +0800

    Prepare v5.1.0 release (#9017)
    * prepare v5.1.0 release
    
    * update samples (dart-dio-next)

--- samples/openapi3/client/petstore/dart-dio-next/petstore_client_lib_fake/lib/src/model/foo.dart
+++ samples/openapi3/client/petstore/dart-dio-next/petstore_client_lib_fake/lib/src/model/foo.dart
@@ -7,57 +7,61 @@ import 'package:built_value/serializer.dart';
 
 part 'foo.g.dart';
 
+
+
 abstract class Foo implements Built<Foo, FooBuilder> {
-  @BuiltValueField(wireName: r'bar')
-  String? get bar;
+    @BuiltValueField(wireName: r'bar')
+    String? get bar;
 
-  Foo._();
+    Foo._();
 
-  static void _initializeBuilder(FooBuilder b) => b..bar = 'bar';
+    static void _initializeBuilder(FooBuilder b) => b
+        ..bar = 'bar';
 
-  factory Foo([void updates(FooBuilder b)]) = _$Foo;
+    factory Foo([void updates(FooBuilder b)]) = _$Foo;
 
-  @BuiltValueSerializer(custom: true)
-  static Serializer<Foo> get serializer => _$FooSerializer();
+    @BuiltValueSerializer(custom: true)
+    static Serializer<Foo> get serializer => _$FooSerializer();
 }
 
 class _$FooSerializer implements StructuredSerializer<Foo> {
-  @override
-  final Iterable<Type> types = const [Foo, _$Foo];
+    @override
+    final Iterable<Type> types = const [Foo, _$Foo];
 
-  @override
-  final String wireName = r'Foo';
+    @override
+    final String wireName = r'Foo';
 
-  @override
-  Iterable<Object?> serialize(Serializers serializers, Foo object,
-      {FullType specifiedType = FullType.unspecified}) {
-    final result = <Object?>[];
-    if (object.bar != null) {
-      result
-        ..add(r'bar')
-        ..add(serializers.serialize(object.bar,
-            specifiedType: const FullType(String)));
+    @override
+    Iterable<Object?> serialize(Serializers serializers, Foo object,
+        {FullType specifiedType = FullType.unspecified}) {
+        final result = <Object?>[];
+        if (object.bar != null) {
+            result
+                ..add(r'bar')
+                ..add(serializers.serialize(object.bar,
+                    specifiedType: const FullType(String)));
+        }
+        return result;
     }
-    return result;
-  }
 
-  @override
-  Foo deserialize(Serializers serializers, Iterable<Object?> serialized,
-      {FullType specifiedType = FullType.unspecified}) {
-    final result = FooBuilder();
+    @override
+    Foo deserialize(Serializers serializers, Iterable<Object?> serialized,
+        {FullType specifiedType = FullType.unspecified}) {
+        final result = FooBuilder();
 
-    final iterator = serialized.iterator;
-    while (iterator.moveNext()) {
-      final key = iterator.current as String;
-      iterator.moveNext();
-      final Object? value = iterator.current;
-      switch (key) {
-        case r'bar':
-          result.bar = serializers.deserialize(value,
-              specifiedType: const FullType(String)) as String;
-          break;
-      }
+        final iterator = serialized.iterator;
+        while (iterator.moveNext()) {
+            final key = iterator.current as String;
+            iterator.moveNext();
+            final Object? value = iterator.current;
+            switch (key) {
+                case r'bar':
+                    result.bar = serializers.deserialize(value,
+                        specifiedType: const FullType(String)) as String;
+                    break;
+            }
+        }
+        return result.build();
     }
-    return result.build();
-  }
 }
+
