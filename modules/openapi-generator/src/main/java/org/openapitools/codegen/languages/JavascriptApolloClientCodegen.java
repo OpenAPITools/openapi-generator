9419de573e3b1d922dd7fe4186b1a15af8668e45
cal <cal101@users.noreply.github.com>
2021-03-25 14:21:21 +0100

    [cleanup] erefactor/EclipseJdt - Remove unused imports (#9075)
    EclipseJdt cleanup 'RemoveUnusedImport' applied by erefactor.
    
    For EclipseJdt see https://www.eclipse.org/eclipse/news/4.19/jdt.php
    For erefactor see https://github.com/cal101/erefactor

--- modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/JavascriptApolloClientCodegen.java
+++ modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/JavascriptApolloClientCodegen.java
@@ -32,7 +32,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.*;
 
 import static org.openapitools.codegen.utils.StringUtils.*;
--- modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/RustServerCodegen.java
+++ modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/RustServerCodegen.java
@@ -39,7 +39,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
-import java.io.IOException;
 import java.net.URL;
 import java.util.*;
 import java.util.Map.Entry;
