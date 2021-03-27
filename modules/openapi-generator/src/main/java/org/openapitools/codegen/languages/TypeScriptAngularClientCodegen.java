dd2f21b2c0af91aba9595a0c0f8efda1f3cff6c7
cal101 <cal101@users.noreply.github.com>
2021-03-27 16:58:09 +0000

    [cleanup] erefactor/EclipseJdt - Use primitive parse methods instead of wrapper.
    EclipseJdt cleanup 'ParsePrimitive' applied by erefactor.
    
    For EclipseJdt see https://www.eclipse.org/eclipse/news/4.19/jdt.php
    For erefactor see https://github.com/cal101/erefactor

--- modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/TypeScriptAngularClientCodegen.java
+++ modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/TypeScriptAngularClientCodegen.java
@@ -177,7 +177,7 @@ public class TypeScriptAngularClientCodegen extends AbstractTypeScriptClientCode
         }
 
         if (additionalProperties.containsKey(STRING_ENUMS)) {
-            setStringEnums(Boolean.valueOf(additionalProperties.get(STRING_ENUMS).toString()));
+            setStringEnums(Boolean.parseBoolean(additionalProperties.get(STRING_ENUMS).toString()));
             additionalProperties.put("stringEnums", getStringEnums());
             if (getStringEnums()) {
                 classEnumSeparator = "";
