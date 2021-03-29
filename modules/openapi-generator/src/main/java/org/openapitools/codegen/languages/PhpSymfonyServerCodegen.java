4f552b638aa59400e76eb050142d7439e9694401
lerminou <nicolas.dubut@laposte.net>
2021-03-29 18:20:33 +0200

    [php-symfony] fix double quoting string defaults values
    quoting for default values is already done in:
    https://github.com/OpenAPITools/openapi-generator/blob/master/modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/AbstractPhpCodegen.java#L533

--- modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/PhpSymfonyServerCodegen.java
+++ modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/PhpSymfonyServerCodegen.java
@@ -416,14 +416,6 @@ public class PhpSymfonyServerCodegen extends AbstractPhpCodegen implements Codeg
                 if (param.isContainer) {
                     param.vendorExtensions.put("x-comment-type", param.dataType + "[]");
                 }
-
-                // Quote default values for strings
-                // @todo: The default values for headers, forms and query params are handled
-                //        in DefaultCodegen fromParameter with no real possibility to override
-                //        the functionality. Thus we are handling quoting of string values here
-                if ("string".equals(param.dataType) && param.defaultValue != null && !param.defaultValue.isEmpty()) {
-                    param.defaultValue = "'" + param.defaultValue + "'";
-                }
             }
 
             // Create a variable to display the correct return type in comments for interfaces
