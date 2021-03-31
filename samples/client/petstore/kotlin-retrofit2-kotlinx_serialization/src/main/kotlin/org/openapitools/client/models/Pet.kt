097031792b7587f5409e31c6e5c5baa3652f85f8
Kevin Stieglitz <kevin.stieglitz@jambit.com>
2021-03-31 13:19:26 +0200

    change samples

--- samples/client/petstore/kotlin-retrofit2-kotlinx_serialization/src/main/kotlin/org/openapitools/client/models/Pet.kt
+++ samples/client/petstore/kotlin-retrofit2-kotlinx_serialization/src/main/kotlin/org/openapitools/client/models/Pet.kt
@@ -52,6 +52,7 @@ data class Pet (
      * pet status in the store
      * Values: AVAILABLE,PENDING,SOLD
      */
+    @KSerializable
     enum class Status(val value: kotlin.String) {
         @SerialName(value = "available") AVAILABLE("available"),
         @SerialName(value = "pending") PENDING("pending"),
