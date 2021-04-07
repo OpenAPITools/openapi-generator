f8a9ea0c6557dc7f01e467c84287793731b4edc1
Peter Leibiger <kuhnroyal@gmail.com>
2021-04-07 17:37:51 +0200

    Fix lists of dates not working

--- samples/openapi3/client/petstore/dart-dio-next/petstore_client_lib_fake/lib/src/serializers.dart
+++ samples/openapi3/client/petstore/dart-dio-next/petstore_client_lib_fake/lib/src/serializers.dart
@@ -2,12 +2,15 @@
 // AUTO-GENERATED FILE, DO NOT MODIFY!
 //
 
+// ignore_for_file: unused_import
+
 import 'package:built_collection/built_collection.dart';
 import 'package:built_value/json_object.dart';
 import 'package:built_value/serializer.dart';
 import 'package:built_value/standard_json_plugin.dart';
 import 'package:built_value/iso_8601_date_time_serializer.dart';
 import 'package:openapi/src/date_serializer.dart';
+import 'package:openapi/src/model/date.dart';
 
 import 'package:openapi/src/model/additional_properties_class.dart';
 import 'package:openapi/src/model/animal.dart';
