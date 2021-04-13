b1a1ab976e4a9c04e0965f66892fabf8d4c39e3f
Simon Bullik <sbu@wbt-solutions.de>
2021-04-13 19:08:58 +0200

    Move to AbstractDartCodegen

--- modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/AbstractDartCodegen.java
+++ modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/AbstractDartCodegen.java
@@ -5,6 +5,7 @@ import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
 import io.swagger.v3.oas.models.Operation;
 import io.swagger.v3.oas.models.media.ArraySchema;
+import io.swagger.v3.oas.models.media.ComposedSchema;
 import io.swagger.v3.oas.models.media.Schema;
 import io.swagger.v3.oas.models.media.StringSchema;
 import io.swagger.v3.oas.models.servers.Server;
@@ -487,6 +488,11 @@ public abstract class AbstractDartCodegen extends DefaultCodegen {
 
     @Override
     public String getSchemaType(Schema p) {
+        if (p instanceof ComposedSchema) {
+            if (((ComposedSchema) p).getOneOf() != null) {
+                return "dynamic";
+            }
+        }
         String openAPIType = super.getSchemaType(p);
         if (openAPIType == null) {
             LOGGER.error("No Type defined for Schema {}", p);
