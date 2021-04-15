90572c5ebcff2d3e452e6fc0c11c9624bdb9d4c0
Melzer Dennis (BCI/ESW17) <dennis.melzer@de.bosch.com>
2021-04-15 15:58:14 +0200

    Trigger build

--- samples/client/petstore/spring-cloud/src/test/java/org/openapitools/api/StoreApiTest.java
+++ samples/client/petstore/spring-cloud/src/test/java/org/openapitools/api/StoreApiTest.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
 import java.time.OffsetDateTime;
 import java.util.Map;
 
