139e9e458fd4f21426c10e0a843b7c724c54269e
William Cheng <wing328hk@gmail.com>
2021-04-21 21:11:05 +0800

    Minor improvements to go-echo server (#9303)
    * minor improvements to go-echo server
    
    * update go tech comm

--- modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/GoEchoServerCodegen.java
+++ modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/GoEchoServerCodegen.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2018 OpenAPI-Generator Contributors (https://openapi-generator.tech)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.openapitools.codegen.languages;
 
 import org.openapitools.codegen.*;
@@ -6,6 +22,8 @@ import java.io.File;
 import java.util.*;
 
 import org.openapitools.codegen.meta.features.*;
+import org.openapitools.codegen.meta.GeneratorMetadata;
+import org.openapitools.codegen.meta.Stability;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -35,6 +53,10 @@ public class GoEchoServerCodegen extends AbstractGoCodegen {
 
     public GoEchoServerCodegen() {
         super();
+
+        generatorMetadata = GeneratorMetadata.newBuilder(generatorMetadata)
+                .stability(Stability.BETA)
+                .build();
 
         modifyFeatureSet(features -> features
                 .includeDocumentationFeatures(DocumentationFeature.Readme)
