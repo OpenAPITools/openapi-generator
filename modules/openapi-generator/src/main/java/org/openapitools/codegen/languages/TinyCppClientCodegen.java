9966916b1b10b3f83cceb56f1f317cd07d0592cb
Anders Aaen Springborg <aaspringborg@gmail.com>
2021-04-22 11:11:53 +0200

    initil tiny client generator

---
+++ modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/TinyCppClientCodegen.java
@@ -0,0 +1,46 @@
+package org.openapitools.codegen.languages;
+
+import org.openapitools.codegen.*;
+import io.swagger.models.properties.ArrayProperty;
+import io.swagger.models.properties.MapProperty;
+import io.swagger.models.properties.Property;
+import io.swagger.models.parameters.Parameter;
+
+import java.io.File;
+import java.util.*;
+
+import org.apache.commons.lang3.StringUtils;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TinyCppClientCodegen extends AbstractCppCodegen implements CodegenConfig {
+    public static final String PROJECT_NAME = "SHOLOSOSOSO";
+
+    static final Logger LOGGER = LoggerFactory.getLogger(TinyCppClientCodegen.class);
+
+    public CodegenType getTag() {
+        return CodegenType.CLIENT;
+    }
+
+    public String getName() {
+        return "tiny-cpp";
+    }
+
+    public String getHelp() {
+        return "Generates a tiny-cpp client.";
+    }
+
+    public TinyCppClientCodegen() {
+        super();
+
+        outputFolder = "generated-code" + File.separator + "tiny-cpp";
+        modelTemplateFiles.put("model.mustache", ".md");
+        apiTemplateFiles.put("api.mustache", ".md");
+        embeddedTemplateDir = templateDir = "tiny-cpp-client";
+        apiPackage = "Apis";
+        modelPackage = "Models";
+        supportingFiles.add(new SupportingFile("README.mustache", "", "README.md"));
+        // TODO: Fill this out.
+    }
+}
