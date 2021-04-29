a7d8057126136297b5a0ba50f8510058de874875
kaareHH <kaare.hattel@gmail.com>
2021-04-29 09:44:37 +0200

    Added mapping configurations

--- modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/TinyCppClientCodegen.java
+++ modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/TinyCppClientCodegen.java
@@ -1,6 +1,8 @@
 package org.openapitools.codegen.languages;
 
 import org.openapitools.codegen.*;
+import io.swagger.v3.oas.models.media.Schema;
+import org.openapitools.codegen.utils.ModelUtils;
 import io.swagger.models.properties.ArrayProperty;
 import io.swagger.models.properties.MapProperty;
 import io.swagger.models.properties.Property;
@@ -47,5 +49,106 @@ public class TinyCppClientCodegen extends AbstractCppCodegen implements CodegenC
         supportingFiles.add(new SupportingFile("helpers-header.mustache", "", "Helpers.h"));
         supportingFiles.add(new SupportingFile("helpers-body.mustache", "", "Helpers.cpp"));
         // TODO: Fill this out.
+
+        defaultIncludes = new HashSet<String>(
+                Arrays.asList(
+                        "bool",
+                        "int",
+                        "long",
+                        "double",
+                        "float")
+        );
+        languageSpecificPrimitives = new HashSet<String>(
+                Arrays.asList(
+                        "bool",
+                        "int",
+                        "long",
+                        "double",
+                        "float",
+                        "std::string")
+        );
+
+    
+
+        super.typeMapping = new HashMap<String, String>();
+        typeMapping.put("string", "std::string");
+        typeMapping.put("integer", "int");
+        typeMapping.put("boolean", "bool");
+        typeMapping.put("array", "std::list");
+        typeMapping.put("DateTime", "std::string");
+        
     }
+
+
+    @Override
+    public String toInstantiationType(Schema p) {
+        if (ModelUtils.isMapSchema(p)) {
+            return instantiationTypes.get("map");
+        } else if (ModelUtils.isArraySchema(p)) {
+            return instantiationTypes.get("array");
+        } else {
+            return null;
+        }
+    }
+
+    @Override
+    public String getTypeDeclaration(String name) {
+        if (languageSpecificPrimitives.contains(name)) {
+            return name;
+        } else {
+            return name + "";
+        }
+    }
+
+    @Override
+    public String getSchemaType(Schema p) {
+        String openAPIType = super.getSchemaType(p);
+        String type = null;
+        if (typeMapping.containsKey(openAPIType)) {
+            type = typeMapping.get(openAPIType);
+            if (languageSpecificPrimitives.contains(type)) {
+                return toModelName(type);
+            }
+        } else {
+            type = openAPIType;
+        }
+        return toModelName(type);
+    }
+
+    @Override
+    public String getTypeDeclaration(Schema p) {
+        String openAPIType = getSchemaType(p);
+        if (languageSpecificPrimitives.contains(openAPIType)) {
+            return toModelName(openAPIType);
+        } else {
+            return openAPIType + "";
+        }
+    }
+
+    @Override
+    public String toModelName(String type) {
+        if (typeMapping.keySet().contains(type) ||
+                typeMapping.values().contains(type) ||
+                defaultIncludes.contains(type) ||
+                languageSpecificPrimitives.contains(type)) {
+            return type;
+        } else {
+            return Character.toUpperCase(type.charAt(0)) + type.substring(1);
+        }
+    }
+
+    @Override
+    public String toModelImport(String name) {
+        if (name.equals("std::string")) {
+            return "#include <string>";
+        } else if (name.equals("std::map")) {
+            return "#include <map>";
+        } else if (name.equals("std::list")) {
+            return "#include <list>";
+        }
+        return "#include \"" + name + ".h\"";
+    }
+    
+
+
 }
