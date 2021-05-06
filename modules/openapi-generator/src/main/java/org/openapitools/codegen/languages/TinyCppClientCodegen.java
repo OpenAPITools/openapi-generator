6c369243cd78b62bf6c00a9c54e37fb86bc2fd12
Anders Aaen Springborg <aaspringborg@gmail.com>
2021-05-06 09:20:12 +0200

    typemappings and new paths

--- modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/TinyCppClientCodegen.java
+++ modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/TinyCppClientCodegen.java
@@ -1,15 +1,9 @@
 package org.openapitools.codegen.languages;
 
 import org.openapitools.codegen.*;
-import io.swagger.models.properties.ArrayProperty;
-import io.swagger.models.properties.MapProperty;
-import io.swagger.models.properties.Property;
-import io.swagger.models.parameters.Parameter;
 
 import java.io.File;
 import java.util.*;
-
-import org.apache.commons.lang3.StringUtils;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -53,16 +47,65 @@ public class TinyCppClientCodegen extends AbstractCppCodegen implements CodegenC
 
     public TinyCppClientCodegen() {
         super();
-        outputFolder = "generated-code" + File.separator + "tiny-cpp";
-        modelTemplateFiles.put("model.mustache", ".md");
-        apiTemplateFiles.put("service/api-header.mustache", ".h");
-        apiTemplateFiles.put("service/api-body.mustache", ".cpp");
+        outputFolder = "generated-code" + File.separator + "tiny-cpp" + File.separator + sourceFolder;
+
+
+        modelPackage = sourceFolder + File.separator + "models";
+        modelTemplateFiles.put("model.mustache", ".h");
+
+        apiPackage = sourceFolder + File.separator + "service";
+        apiTemplateFiles.put("service/api-header.mustache".replace('/', File.separatorChar), ".h");
+        apiTemplateFiles.put("service/api-body.mustache".replace('/', File.separatorChar), ".cpp");
+
         embeddedTemplateDir = templateDir = "tiny-cpp-client";
-        apiPackage = "Apis";
-        modelPackage = "Models";
+
         supportingFiles.add(new SupportingFile("README.mustache", "", "README.md"));
-        supportingFiles.add(new SupportingFile("service/Response.h.mustache", "service", "Response.h")); // TODO find right function for folder
-        supportingFiles.add(new SupportingFile("service/AbstractService.h.mustache", "service", "AbstractService.h")); // TODO find right function for folder
-        // TODO: Fill this out.
+        supportingFiles.add(new SupportingFile("service/Response.h.mustache", serviceFolder, "Response.h")); // TODO find right function for folder
+        supportingFiles.add(new SupportingFile("service/AbstractService.h.mustache", serviceFolder, "AbstractService.h")); // TODO find right function for folder
+        supportingFiles.add(new SupportingFile("service/AbstractService.cpp.mustache", serviceFolder, "AbstractService.cpp")); // TODO find right function for folder
+
+        makeTypeMappings();
+
+    }
+
+
+    @Override
+    public String toModelImport(String name) {
+        if (name.equals("std::string")) {
+            return "#include <string>";
+        //} else if (name.equals(cpp_hashmap_type)) {
+        //    return "#include <map>";
+        } else if (name.equals(cpp_array_type)) {
+            return "#include <list>";
+        }
+        return "#include \"" + name + ".h\"";
+    }
+
+    // FilePaths
+    private static final String sourceFolder = "lib";
+    private static final String serviceFolder = sourceFolder + File.separator + "service";
+
+    // Types
+    private static final String cpp_array_type = "std::list";
+    //private static final String cpp_hashmap_type = "std::map";
+
+    private void makeTypeMappings() {
+        super.typeMapping = new HashMap<>();
+
+        typeMapping.put("string", "std::string");
+        typeMapping.put("integer", "int");
+        typeMapping.put("float", "float");
+        typeMapping.put("long", "long long");
+        typeMapping.put("boolean", "bool");
+        typeMapping.put("double", "double");
+        typeMapping.put("array", cpp_array_type);
+        typeMapping.put("number", "long long");
+        typeMapping.put("binary", "std::string");
+        typeMapping.put("password", "std::string");
+        typeMapping.put("file", "std::string");
+        typeMapping.put("DateTime", "std::string");
+        typeMapping.put("Date", "std::string");
+        typeMapping.put("UUID", "std::string");
+        typeMapping.put("URI", "std::string");
     }
 }
