96cb5432595be50db04545dfd9dc61eed8a7b5af
Anders Aaen Springborg <aaspringborg@gmail.com>
2021-05-15 14:16:51 +0200

    new main example ❣️

--- samples/client/petstore/tiny/cpp/src/main.cpp
+++ samples/client/petstore/tiny/cpp/src/main.cpp
@@ -1,36 +1,41 @@
 #include "PetApi.h"
-const char* ssid = "your wifi name"; // TODO Change wifi name
-const char* password = "Your wifi password"; //TODO Change wifi password
+const char* ssid = "NOKIA-CE21"; //Enter SSID
+const char* password = "R4f6U4pEeS"; //Enter Password
 
 
 void setup(){
     //Initialize serial and wait for port to open:
     Serial.begin(9600);
     delay(100);
-
+    
     Serial.print("Attempting to connect to SSID: ");
     Serial.println(ssid);
     WiFi.begin(ssid, password);
 
     // attempt to connect to Wifi network:
     while (WiFi.status() != WL_CONNECTED) {
-    Serial.print(".");
-    // wait 1 second for re-trying
-    delay(1000);
+        Serial.print(".");
+        // wait 1 second for re-trying
+        delay(1000);
     }
 
     Serial.print("Connected to ");
     Serial.println(ssid);
 
-    //Print LAN IP.
+    //Print LAN IP.  
     Serial.print("IP address set: ");
     Serial.println(WiFi.localIP());
 
     Tiny::PetApi petapi;
     auto resp = petapi.getPetById(1);
     Serial.println(resp.code);
-    Pet pet = resp.obj;
-    Serial.println(pet.toJson().dump().c_str());
+    Serial.println(resp.obj.toJson().dump().c_str());
 }
 
 void loop(){}
+
+#else
+int main(){
+    return 1;
+}
+#endif
