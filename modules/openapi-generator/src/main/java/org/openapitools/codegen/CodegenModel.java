af6c4d59d7b9c1330ea8c6b4c92950c7f31f1f52
Justin Black <justin.a.black@gmail.com>
2021-05-18 10:15:29 -0700

    Adds isShortInteger to CodegenModel

--- modules/openapi-generator/src/main/java/org/openapitools/codegen/CodegenModel.java
+++ modules/openapi-generator/src/main/java/org/openapitools/codegen/CodegenModel.java
@@ -64,7 +64,7 @@ public class CodegenModel implements IJsonSchemaValidationProperties {
     public String defaultValue;
     public String arrayModelType;
     public boolean isAlias; // Is this effectively an alias of another simple type
-    public boolean isString, isInteger, isLong, isNumber, isNumeric, isFloat, isDouble, isDate, isDateTime;
+    public boolean isString, isInteger, isLong, isNumber, isNumeric, isFloat, isDouble, isDate, isDateTime, isShortInteger;
     private boolean additionalPropertiesIsAnyType;
     public List<CodegenProperty> vars = new ArrayList<CodegenProperty>(); // all properties (without parent's properties)
     public List<CodegenProperty> allVars = new ArrayList<CodegenProperty>(); // all properties (with parent's properties)
@@ -607,6 +607,14 @@ public class CodegenModel implements IJsonSchemaValidationProperties {
     }
 
     @Override
+    public boolean getIsShortInteger() { return isShortInteger; }
+
+    @Override
+    public void setIsShortInteger(boolean isShortInteger)   {
+        this.isShortInteger = isShortInteger;
+    }
+
+    @Override
     public CodegenProperty getAdditionalProperties() { return additionalProperties; }
 
     @Override
@@ -752,6 +760,7 @@ public class CodegenModel implements IJsonSchemaValidationProperties {
         return isAlias == that.isAlias &&
                 isString == that.isString &&
                 isInteger == that.isInteger &&
+                isShortInteger == that.isShortInteger &&
                 isLong == that.isLong &&
                 isNumber == that.isNumber &&
                 isNumeric == that.isNumeric &&
@@ -839,7 +848,7 @@ public class CodegenModel implements IJsonSchemaValidationProperties {
                 getDescription(), getClassVarName(), getModelJson(), getDataType(), getXmlPrefix(), getXmlNamespace(),
                 getXmlName(), getClassFilename(), getUnescapedDescription(), getDiscriminator(), getDefaultValue(),
                 getArrayModelType(), isAlias, isString, isInteger, isLong, isNumber, isNumeric, isFloat, isDouble,
-                isDate, isDateTime, isNull, hasValidation,
+                isDate, isDateTime, isNull, hasValidation, isShortInteger,
                 getVars(), getAllVars(), getRequiredVars(), getOptionalVars(), getReadOnlyVars(), getReadWriteVars(),
                 getParentVars(), getAllowableValues(), getMandatory(), getAllMandatory(), getImports(), hasVars,
                 isEmptyVars(), hasMoreModels, hasEnums, isEnum, isNullable, hasRequired, hasOptional, isArray,
@@ -882,6 +891,7 @@ public class CodegenModel implements IJsonSchemaValidationProperties {
         sb.append(", isString=").append(isString);
         sb.append(", isInteger=").append(isInteger);
         sb.append(", isLong=").append(isLong);
+        sb.append(", isShortInteger=").append(isShortInteger);
         sb.append(", isNumber=").append(isNumber);
         sb.append(", isNumeric=").append(isNumeric);
         sb.append(", isFloat=").append(isFloat);
