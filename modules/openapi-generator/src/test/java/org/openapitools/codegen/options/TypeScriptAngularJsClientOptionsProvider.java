8b24f3578d04b9b973d0cf4f4b19e0194f5c04f3
Mike Marchetti <mike@agilicus.com>
2021-05-21 11:56:56 -0400

    fix: add paramPropertyNaming
    Model naming conforms to the property: modelPropertyNaming, however the
    parameter naming scheme is currently hardcoded to camelcase. Since the
    parameter names are closely tied to the model properties names, is maybe
    desirable to use a similar naming approach.
    
    The toParamName cannot directly use the paramPropertyNaming since that
    will cause issues with current generated code, since the
    modelPropertyNaming currently defaults to "original"
    
    A new property has been created to override this behavior, named
    "paramPropertyNaming", and its default is set to "camelcase".

--- modules/openapi-generator/src/test/java/org/openapitools/codegen/options/TypeScriptAngularJsClientOptionsProvider.java
+++ modules/openapi-generator/src/test/java/org/openapitools/codegen/options/TypeScriptAngularJsClientOptionsProvider.java
@@ -31,6 +31,7 @@ public class TypeScriptAngularJsClientOptionsProvider implements OptionsProvider
     public static final String SORT_MODEL_PROPERTIES_VALUE = "false";
     public static final String ENSURE_UNIQUE_PARAMS_VALUE = "true";
     public static final String MODEL_PROPERTY_NAMING_VALUE = "camelCase";
+    public static final String PARAM_NAMING_VALUE = "camelCase";
     public static final String ENUM_PROPERTY_NAMING_VALUE = "PascalCase";
     public static final String ALLOW_UNICODE_IDENTIFIERS_VALUE = "false";
     public static final String PREPEND_FORM_OR_BODY_PARAMETERS_VALUE = "true";
@@ -51,6 +52,7 @@ public class TypeScriptAngularJsClientOptionsProvider implements OptionsProvider
                 .put(CodegenConstants.ENSURE_UNIQUE_PARAMS, ENSURE_UNIQUE_PARAMS_VALUE)
                 .put(CodegenConstants.ENUM_PROPERTY_NAMING, ENUM_PROPERTY_NAMING_VALUE)
                 .put(CodegenConstants.MODEL_PROPERTY_NAMING, MODEL_PROPERTY_NAMING_VALUE)
+                .put(CodegenConstants.PARAM_NAMING, PARAM_NAMING_VALUE)
                 .put(CodegenConstants.ALLOW_UNICODE_IDENTIFIERS, ALLOW_UNICODE_IDENTIFIERS_VALUE)
                 .put(CodegenConstants.PREPEND_FORM_OR_BODY_PARAMETERS, PREPEND_FORM_OR_BODY_PARAMETERS_VALUE)
                 .put(CodegenConstants.LEGACY_DISCRIMINATOR_BEHAVIOR, "true")
