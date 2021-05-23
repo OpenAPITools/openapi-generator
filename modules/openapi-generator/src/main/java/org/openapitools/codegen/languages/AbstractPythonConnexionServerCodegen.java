97e07803fd67d34b38cffc4d8f18b1fb2f78b8fb
Troy Patterson <troy.patterson@utoronto.ca>
2021-05-23 01:18:52 -0700

    Adding usePythonSrcRootInImports logic to AbstractPythonConnexionServerCodegen. (#9558)

--- modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/AbstractPythonConnexionServerCodegen.java
+++ modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/AbstractPythonConnexionServerCodegen.java
@@ -52,6 +52,7 @@ public abstract class AbstractPythonConnexionServerCodegen extends AbstractPytho
     // nose is a python testing framework, we use pytest if USE_NOSE is unset
     public static final String USE_NOSE = "useNose";
     public static final String PYTHON_SRC_ROOT = "pythonSrcRoot";
+    public static final String USE_PYTHON_SRC_ROOT_IN_IMPORTS = "usePythonSrcRootInImports";
     static final String MEDIA_TYPE = "mediaType";
 
     protected int serverPort = 8080;
@@ -62,6 +63,7 @@ public abstract class AbstractPythonConnexionServerCodegen extends AbstractPytho
     protected boolean featureCORS = Boolean.FALSE;
     protected boolean useNose = Boolean.FALSE;
     protected String pythonSrcRoot;
+    protected boolean usePythonSrcRootInImports = Boolean.FALSE;
 
     public AbstractPythonConnexionServerCodegen(String templateDirectory, boolean fixBodyNameValue) {
         super();
@@ -79,7 +81,7 @@ public abstract class AbstractPythonConnexionServerCodegen extends AbstractPytho
         typeMapping.put("map", "Dict");
 
         // set the output folder here
-        outputFolder = "generated-code/connexion";
+        outputFolder = "generated-code" + File.separatorChar + "connexion";
 
         apiTemplateFiles.put("controller.mustache", ".py");
         modelTemplateFiles.put("model.mustache", ".py");
@@ -132,6 +134,8 @@ public abstract class AbstractPythonConnexionServerCodegen extends AbstractPytho
                 defaultValue(Boolean.FALSE.toString()));
         cliOptions.add(new CliOption(PYTHON_SRC_ROOT, "put python sources in this subdirectory of output folder (defaults to \"\" for). Use this for src/ layout.").
                 defaultValue(""));
+        cliOptions.add(new CliOption(USE_PYTHON_SRC_ROOT_IN_IMPORTS, "include pythonSrcRoot in import namespaces.").
+                defaultValue("false"));
     }
 
     protected void addSupportingFiles() {
@@ -147,7 +151,6 @@ public abstract class AbstractPythonConnexionServerCodegen extends AbstractPytho
             setPackageName((String) additionalProperties.get(CodegenConstants.PACKAGE_NAME));
         } else {
             setPackageName("openapi_server");
-            additionalProperties.put(CodegenConstants.PACKAGE_NAME, this.packageName);
         }
         if (additionalProperties.containsKey(CodegenConstants.PACKAGE_VERSION)) {
             setPackageVersion((String) additionalProperties.get(CodegenConstants.PACKAGE_VERSION));
@@ -177,9 +180,19 @@ public abstract class AbstractPythonConnexionServerCodegen extends AbstractPytho
         if (additionalProperties.containsKey(USE_NOSE)) {
             setUseNose((String) additionalProperties.get(USE_NOSE));
         }
+        if (additionalProperties.containsKey(USE_PYTHON_SRC_ROOT_IN_IMPORTS)) {
+            setUsePythonSrcRootInImports((String) additionalProperties.get(USE_PYTHON_SRC_ROOT_IN_IMPORTS));
+        }
         if (additionalProperties.containsKey(PYTHON_SRC_ROOT)) {
-            setPythonSrcRoot((String) additionalProperties.get(PYTHON_SRC_ROOT));
-            additionalProperties.put(PYTHON_SRC_ROOT, pythonSrcRoot);
+            String pythonSrcRoot = (String) additionalProperties.get(PYTHON_SRC_ROOT);
+            if (usePythonSrcRootInImports) {
+                // if we prepend the package name if the pythonSrcRoot we get the desired effect.
+                // but we also need to set pythonSrcRoot itself to "" to ensure all the paths are
+                // what we expect.
+                setPackageName(pythonSrcRoot + "." + packageName);
+                pythonSrcRoot = "";
+            }
+            setPythonSrcRoot(pythonSrcRoot);
         } else {
             setPythonSrcRoot("");
         }
@@ -218,6 +231,11 @@ public abstract class AbstractPythonConnexionServerCodegen extends AbstractPytho
         } else {
             this.pythonSrcRoot = pySrcRoot + File.separator;
         }
+        additionalProperties.put(PYTHON_SRC_ROOT, StringUtils.defaultIfBlank(this.pythonSrcRoot, null));
+    }
+
+    public void setUsePythonSrcRootInImports(String val) {
+        this.usePythonSrcRootInImports = Boolean.parseBoolean(val);
     }
 
 
