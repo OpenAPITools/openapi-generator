0da409986889dcfab1420fb1410ae9509a5834c8
Nikita Vakula <52108696+krjakbrjak@users.noreply.github.com>
2021-05-30 11:43:31 +0200

    New python-fastapi generator (#9611)
    * [python-fastapi] Added new generator
    
    See https://fastapi.tiangolo.com/ for more details about FastAPI
    
    Signed-off-by: Nikita Vakula <programmistov.programmist@gmail.com>
    
    * [python-fastapi] Added samples
    
    Signed-off-by: Nikita Vakula <programmistov.programmist@gmail.com>

---
+++ samples/server/petstore/python-fastapi/tests/conftest.py
@@ -0,0 +1,20 @@
+import contextlib
+from typing import Any
+
+import pytest
+from fastapi import FastAPI
+from fastapi.testclient import TestClient
+
+from openapi_server.main import app as application
+
+
+@pytest.fixture
+def app() -> FastAPI:
+    application.dependency_overrides = {}
+
+    return application
+
+
+@pytest.fixture
+def client(app) -> TestClient:
+    return TestClient(app)
