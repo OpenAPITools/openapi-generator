d52f753033a6fbac13fb2760fbe2aa970e7694a8
Bruno Coelho <4brunu@gmail.com>
2021-05-31 12:15:27 +0100

    [swift5][client] update sample projects

--- samples/client/petstore/swift5/alamofireLibrary/Package.swift
+++ samples/client/petstore/swift5/alamofireLibrary/Package.swift
@@ -19,7 +19,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/Flight-School/AnyCodable", .exact("0.4.0")),
+        .package(url: "https://github.com/Flight-School/AnyCodable", .exact("0.6.0")),
         .package(url: "https://github.com/Alamofire/Alamofire.git", from: "4.9.1"),
     ],
     targets: [
