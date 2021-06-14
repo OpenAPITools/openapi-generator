180f9876bf4e1bee8522915e30fa9d2ed95a8340
Sem Schilder <sem@tropical.email>
2021-06-14 11:21:45 +0200

    Fix other snapshot

--- samples/client/petstore/java/feign/src/main/java/org/openapitools/client/ApiClient.java
+++ samples/client/petstore/java/feign/src/main/java/org/openapitools/client/ApiClient.java
@@ -126,6 +126,7 @@ public class ApiClient {
     return objectMapper;
   }
 
+
   public ObjectMapper getObjectMapper(){
     return objectMapper;
   }
@@ -209,7 +210,7 @@ public class ApiClient {
   }
 
   /**
-   * Helper method to configure the client credentials for Oauth
+   * Helper method to configure the client credentials for Oauth 
    * @param username Username
    * @param password Password
    */
