f3ff4c2f6555ba263a6c716e50af4d3492c5a4b5
Anders Aaen Springborg <aaspringborg@gmail.com>
2021-06-16 13:10:01 +0200

    moved network logic to abstract service

--- samples/client/petstore/cpp-tiny/lib/service/AbstractService.cpp
+++ samples/client/petstore/cpp-tiny/lib/service/AbstractService.cpp
@@ -2,7 +2,18 @@
 #include "Arduino.h"
 
 
+void Tiny::AbstractService::addHeader(std::string key, std::string value){
+    http.addHeader(String(key.c_str()), String(value.c_str()));
+}
 
+void Tiny::AbstractService::addQueryParam(std::string key, std::string value){
+    queryParams[key] = value;
+}
+
+int Tiny::AbstractService::sendRequest(const char * type, Stream * stream, size_t size){
+    int httpCode = http.sendRequest(type, stream, size);
+    return httpCode;
+}
 void Tiny::AbstractService::begin(std::string url){
     http.begin(String(url.c_str()), test_root_ca); //HTTPS connection
 }
--- samples/client/petstore/cpp-tiny/lib/service/AbstractService.h
+++ samples/client/petstore/cpp-tiny/lib/service/AbstractService.h
@@ -3,6 +3,7 @@
 
 #include "HTTPClient.h"
 #include "Response.h"
+#include <map>
 namespace Tiny {
 
 /**
@@ -10,17 +11,22 @@ namespace Tiny {
 * Generated with openapi::tiny-cpp-client
 */
 class AbstractService {
-public:
-HTTPClient http;
-std::string basepath = "https://petstore3.swagger.io/api/v3"; // TODO: change to your url
+    public:
+        HTTPClient http;
+        std::string basepath = "https://petstore3.swagger.io/api/v3"; // TODO: change to your url
+        std::map<std::string, std::string> queryParams;
+        void begin(std::string url);
 
-void begin(std::string url);
+        int sendRequest(const char * type, Stream * stream, size_t size);
 
-// Go and comment out a certificate in root.cert, if you get an error here
-// Certificate from file
-const char* test_root_ca =
-#include "../../root.cert"
-;
+        void addQueryParam(std::string key, std::string value);
+        void addHeader(std::string key, std::string value);
+
+        // Go and comment out a certificate in root.cert, if you get an error here
+        // Certificate from file
+        const char* test_root_ca =
+        #include "../../root.cert"
+        ;
 
 }; // end class
 }// namespace Tinyclient
