10d1c988aabdbf275bc7c387a59be06d4569928e
Ghufz <18732053+Ghufz@users.noreply.github.com>
2021-06-18 16:46:48 +0530

    conditional serialization add only those property which user wants to configure.

--- modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/AbstractCSharpCodegen.java
+++ modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/AbstractCSharpCodegen.java
@@ -41,6 +41,7 @@ public abstract class AbstractCSharpCodegen extends DefaultCodegen implements Co
 
     protected boolean optionalAssemblyInfoFlag = true;
     protected boolean optionalEmitDefaultValuesFlag = false;
+    protected boolean conditionalSerialization = false;
     protected boolean optionalProjectFileFlag = true;
     protected boolean optionalMethodArgumentFlag = true;
     protected boolean useDateTimeOffsetFlag = false;
