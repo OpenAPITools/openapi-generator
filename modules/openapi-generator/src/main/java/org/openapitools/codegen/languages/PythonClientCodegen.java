3967b2aca92830a8ed58e783c6725f2c89934568
Justin Black <justin.a.black@gmail.com>
2021-07-09 09:08:27 -0700

    Passes needed locale argument to toUpperCase

--- modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/PythonClientCodegen.java
+++ modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/PythonClientCodegen.java
@@ -575,7 +575,7 @@ public class PythonClientCodegen extends PythonLegacyClientCodegen {
         if ("int".equals(datatype) || "float".equals(datatype)) {
             return value;
         } else if ("bool".equals(datatype)) {
-            return value.substring(0, 1).toUpperCase() + value.substring(1);
+            return value.substring(0, 1).toUpperCase(Locale.ROOT) + value.substring(1);
         } else {
             return ensureQuotes(value);
         }
