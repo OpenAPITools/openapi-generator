08c3fc1056f9028cdd2539ebf96b6af96f4bdd4f
Ilya Samartsev <ignz@yandex-team.ru>
2021-07-16 15:53:51 +0300

    generate samples

--- samples/client/petstore/java/webclient/src/main/java/org/openapitools/client/ApiClient.java
+++ samples/client/petstore/java/webclient/src/main/java/org/openapitools/client/ApiClient.java
@@ -84,6 +84,8 @@ public class ApiClient extends JavaTimeFormatter {
         }
     }
 
+    private static final String URI_TEMPLATE_ATTRIBUTE = WebClient.class.getName() + ".uriTemplate";
+
     private HttpHeaders defaultHeaders = new HttpHeaders();
     private MultiValueMap<String, String> defaultCookies = new LinkedMultiValueMap<String, String>();
 
@@ -615,6 +617,8 @@ public class ApiClient extends JavaTimeFormatter {
         addHeadersToRequest(defaultHeaders, requestBuilder);
         addCookiesToRequest(cookieParams, requestBuilder);
         addCookiesToRequest(defaultCookies, requestBuilder);
+
+        requestBuilder.attribute(URI_TEMPLATE_ATTRIBUTE, path);
 
         requestBuilder.body(selectBody(body, formParams, contentType));
         return requestBuilder;
