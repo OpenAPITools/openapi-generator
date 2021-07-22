ad756b61b32d9bd8d939369faf258f0da63371fe
TNM Technologies <tnmtechnologies@users.noreply.github.com>
2021-07-22 19:56:25 +0200

    Revert "feat(jaxrs-resteasy): update related samples"
    This reverts commit 32bb7144bd096765eb9fb077431dd48ac4f38799.

--- samples/server/petstore/jaxrs-resteasy/default-value/src/gen/java/org/openapitools/api/TestQueryParamsApi.java
+++ samples/server/petstore/jaxrs-resteasy/default-value/src/gen/java/org/openapitools/api/TestQueryParamsApi.java
@@ -3,7 +3,8 @@ package org.openapitools.api;
 import org.openapitools.model.*;
 import org.openapitools.api.TestQueryParamsApiService;
 
-
+import io.swagger.annotations.ApiParam;
+import io.swagger.jaxrs.*;
 
 import java.math.BigDecimal;
 import org.openapitools.model.TestResponse;
@@ -26,6 +27,7 @@ import javax.validation.Valid;
 @Path("/test-query-params")
 
 
+@io.swagger.annotations.Api(description = "the test-query-params API")
 @javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaResteasyServerCodegen")
 public class TestQueryParamsApi  {
 
@@ -35,6 +37,9 @@ public class TestQueryParamsApi  {
     
     
     @Produces({ "application/json" })
+    @io.swagger.annotations.ApiOperation(value = "test query params", notes = "desc", response = TestResponse.class, tags={ "verify-default-value", })
+    @io.swagger.annotations.ApiResponses(value = { 
+        @io.swagger.annotations.ApiResponse(code = 200, message = "default response", response = TestResponse.class) })
     public Response queryParamsTest(  @DefaultValue("11.2") @QueryParam("queryNumber") BigDecimal queryNumber,  @DefaultValue("qwerty") @QueryParam("queryString") String queryString,  @DefaultValue("qwerty") @QueryParam("queryStringWrapped") String queryStringWrapped,  @DefaultValue("qwerty\"with quotes\" test") @QueryParam("queryStringQuotes") String queryStringQuotes,  @DefaultValue("qwerty\"with quotes\" test") @QueryParam("queryStringQuotesWrapped") String queryStringQuotesWrapped,  @DefaultValue("true") @QueryParam("queryBoolean") Boolean queryBoolean,@Context SecurityContext securityContext)
     throws NotFoundException {
         return service.queryParamsTest(queryNumber,queryString,queryStringWrapped,queryStringQuotes,queryStringQuotesWrapped,queryBoolean,securityContext);
