1d4a3433dfccf5bed8f3841107748b43ace7a47a
Richard Moorhead <richard.moorhead@gmail.com>
2021-07-24 15:15:47 -0500

    fix java8 datetime jaxrs-resteasy

---
+++ samples/server/petstore/jaxrs-resteasy/java8/src/gen/java/org/openapitools/api/ApiException.java
@@ -0,0 +1,32 @@
+package org.openapitools.api;
+
+/**
+ * The exception that can be used to store the HTTP status code returned by an API response.
+ */
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaResteasyServerCodegen")
+public class ApiException extends Exception {
+
+    /** The HTTP status code. */
+    private int code;
+
+    /**
+     * Constructor.
+     *
+     * @param code The HTTP status code.
+     * @param msg The error message.
+     */
+    public ApiException(int code, String msg) {
+        super(msg);
+        this.code = code;
+    }
+
+    /**
+     * Get the HTTP status code.
+     *
+     * @return The HTTP status code.
+     */
+    public int getCode() {
+        return code;
+    }
+
+}
