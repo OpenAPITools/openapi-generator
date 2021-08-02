673699c091da6cd313bd6e111e1f028958c0adb9
TNM Technologies <tnmtechnologies@users.noreply.github.com>
2021-08-02 10:05:25 +0200

    feat(jaxrs-resteasy): update samples

--- samples/server/petstore/jaxrs-resteasy/default-value/src/gen/java/org/openapitools/api/TestHeadersApi.java
+++ samples/server/petstore/jaxrs-resteasy/default-value/src/gen/java/org/openapitools/api/TestHeadersApi.java
@@ -3,8 +3,7 @@ package org.openapitools.api;
 import org.openapitools.model.*;
 import org.openapitools.api.TestHeadersApiService;
 
-import io.swagger.annotations.ApiParam;
-import io.swagger.jaxrs.*;
+
 
 import java.math.BigDecimal;
 import org.openapitools.model.TestResponse;
@@ -27,7 +26,6 @@ import javax.validation.Valid;
 @Path("/test-headers")
 
 
-@io.swagger.annotations.Api(description = "the test-headers API")
 @javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaResteasyServerCodegen")
 public class TestHeadersApi  {
 
@@ -37,10 +35,7 @@ public class TestHeadersApi  {
     
     
     @Produces({ "application/json" })
-    @io.swagger.annotations.ApiOperation(value = "test headers", notes = "desc", response = TestResponse.class, tags={ "verify-default-value", })
-    @io.swagger.annotations.ApiResponses(value = { 
-        @io.swagger.annotations.ApiResponse(code = 200, message = "default response", response = TestResponse.class) })
-    public Response headersTest(  @ApiParam(value = "" , defaultValue="11.2") @HeaderParam("headerNumber") BigDecimal headerNumber,  @ApiParam(value = "" , defaultValue="qwerty") @HeaderParam("headerString") String headerString,  @ApiParam(value = "" , defaultValue="qwerty") @HeaderParam("headerStringWrapped") String headerStringWrapped,  @ApiParam(value = "" , defaultValue="qwerty\"with quotes\" test") @HeaderParam("headerStringQuotes") String headerStringQuotes,  @ApiParam(value = "" , defaultValue="qwerty\"with quotes\" test") @HeaderParam("headerStringQuotesWrapped") String headerStringQuotesWrapped,  @ApiParam(value = "" , defaultValue="true") @HeaderParam("headerBoolean") Boolean headerBoolean,@Context SecurityContext securityContext)
+    public Response headersTest(   @HeaderParam("headerNumber") BigDecimal headerNumber,   @HeaderParam("headerString") String headerString,   @HeaderParam("headerStringWrapped") String headerStringWrapped,   @HeaderParam("headerStringQuotes") String headerStringQuotes,   @HeaderParam("headerStringQuotesWrapped") String headerStringQuotesWrapped,   @HeaderParam("headerBoolean") Boolean headerBoolean,@Context SecurityContext securityContext)
     throws NotFoundException {
         return service.headersTest(headerNumber,headerString,headerStringWrapped,headerStringQuotes,headerStringQuotesWrapped,headerBoolean,securityContext);
     }
