808bbfab09f30b326a5667f0358fefa22a1cff11
Steve Telford <steven.telford@greater.com.au>
2021-08-03 17:10:05 +1000

    Add request factory interface

--- modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/KotlinClientCodegen.java
+++ modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/KotlinClientCodegen.java
@@ -484,6 +484,7 @@ public class KotlinClientCodegen extends AbstractKotlinCodegen {
 
         supportingFiles.add(new SupportingFile("infrastructure/ApiClient.kt.mustache", infrastructureFolder, "ApiClient.kt"));
         supportingFiles.add(new SupportingFile("request/GsonRequest.mustache", requestFolder, "GsonRequest.kt"));
+        supportingFiles.add(new SupportingFile("request/IRequestFactory.mustache", requestFolder, "IRequestFactory.kt"));
         supportingFiles.add(new SupportingFile("request/RequestFactory.mustache", requestFolder, "RequestFactory.kt"));
 
         addSupportingSerializerAdapters(infrastructureFolder);
