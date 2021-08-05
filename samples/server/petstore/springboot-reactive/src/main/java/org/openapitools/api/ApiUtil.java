64a2c60f32a6cfa4ae341b459ce0ac69c6d5b2f3
Joshua Bridge <jbridg12@jaguarlandrover.com>
2021-08-05 14:24:21 +0100

    Return content type on default spring reactive responses

--- samples/server/petstore/springboot-reactive/src/main/java/org/openapitools/api/ApiUtil.java
+++ samples/server/petstore/springboot-reactive/src/main/java/org/openapitools/api/ApiUtil.java
@@ -1,12 +1,18 @@
 package org.openapitools.api;
 
 import java.nio.charset.StandardCharsets;
+import org.springframework.core.io.buffer.DefaultDataBuffer;
 import org.springframework.core.io.buffer.DefaultDataBufferFactory;
+import org.springframework.http.MediaType;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
 public class ApiUtil {
-    public static Mono<Void> getExampleResponse(ServerWebExchange exchange, String example) {
-        return exchange.getResponse().writeWith(Mono.just(new DefaultDataBufferFactory().wrap(example.getBytes(StandardCharsets.UTF_8))));
+    public static Mono<Void> getExampleResponse(ServerWebExchange exchange, String contentType, String example) {
+        DefaultDataBuffer data = new DefaultDataBufferFactory().wrap(example.getBytes(StandardCharsets.UTF_8));
+        ServerHttpResponse response = exchange.getResponse();
+        response.getHeaders().setContentType(MediaType.valueOf(contentType));
+        return response.writeWith(Mono.just(data));
     }
 }
