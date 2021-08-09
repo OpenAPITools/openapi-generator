559ca95d6f2d4bf7297b74bfd406beb734d8cd48
Przemyslaw KADEJ <przemyslaw.kadej@bluesoft.com>
2021-08-09 10:44:19 +0200

    useOneOfInterfaces and addOneOfInterfaceImports via additional-properties

--- modules/openapi-generator/src/main/java/org/openapitools/codegen/DefaultCodegen.java
+++ modules/openapi-generator/src/main/java/org/openapitools/codegen/DefaultCodegen.java
@@ -370,6 +370,14 @@ public class DefaultCodegen implements CodegenConfig {
             this.setDisallowAdditionalPropertiesIfNotPresent(Boolean.parseBoolean(additionalProperties
                     .get(CodegenConstants.DISALLOW_ADDITIONAL_PROPERTIES_IF_NOT_PRESENT).toString()));
         }
+        if (additionalProperties.containsKey(CodegenConstants.USE_ONE_OF_INTERFACES)) {
+            this.setUseOneOfInterfaces(Boolean.valueOf(additionalProperties
+                    .get(CodegenConstants.USE_ONE_OF_INTERFACES).toString()));
+        }
+        if (additionalProperties.containsKey(CodegenConstants.ADD_ONE_OF_INTERFACE_IMPORTS)) {
+            this.setAddOneOfInterfaceImports(Boolean.valueOf(additionalProperties
+                    .get(CodegenConstants.ADD_ONE_OF_INTERFACE_IMPORTS).toString()));
+        }
     }
 
     /***
@@ -1280,6 +1288,10 @@ public class DefaultCodegen implements CodegenConfig {
 
     public void setUseOneOfInterfaces(Boolean useOneOfInterfaces) {
         this.useOneOfInterfaces = useOneOfInterfaces;
+    }
+
+    public void setAddOneOfInterfaceImports(Boolean addOneOfInterfaceImports) {
+        this.addOneOfInterfaceImports = addOneOfInterfaceImports;
     }
 
     /**
