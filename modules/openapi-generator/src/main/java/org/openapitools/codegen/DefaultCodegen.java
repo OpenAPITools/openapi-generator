993ddf91fed799a8060147919478c981c547e68b
William Cheng <wing328hk@gmail.com>
2021-08-23 10:25:52 +0800

    add oneOfProps, allOfProps, anyOfProps to codegen model

--- modules/openapi-generator/src/main/java/org/openapitools/codegen/DefaultCodegen.java
+++ modules/openapi-generator/src/main/java/org/openapitools/codegen/DefaultCodegen.java
@@ -2572,9 +2572,14 @@ public class DefaultCodegen implements CodegenConfig {
                 }
             }
 
+            m.oneOfProps = oneOfProps;
+            m.allOfProps = allOfProps;
+            m.anyOfProps = anyOfProps;
+/*
             m.vendorExtensions.put("x-oneOf-as-properties", oneOfProps);
             m.vendorExtensions.put("x-allOf-as-properties", allOfProps);
             m.vendorExtensions.put("x-anyOf-as-properties", anyOfProps);
+*/
 
             if (parent != null && composed.getAllOf() != null) { // set parent for allOf only
                 m.parentSchema = parentName;
