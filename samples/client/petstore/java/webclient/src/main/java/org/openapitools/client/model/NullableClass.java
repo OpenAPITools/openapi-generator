4ebcd4352da614096b2cd20d98a31359e9e680e6
Oleh Kurpiak <oleh.kurpiak@gmail.com>
2021-08-24 12:08:17 +0300

    [Java][Spring] fix imports for nullable helpers

--- samples/client/petstore/java/webclient/src/main/java/org/openapitools/client/model/NullableClass.java
+++ samples/client/petstore/java/webclient/src/main/java/org/openapitools/client/model/NullableClass.java
@@ -585,7 +585,7 @@ public class NullableClass extends HashMap<String, Object> {
   }
 
   private static <T> boolean equalsNullable(JsonNullable<T> a, JsonNullable<T> b) {
-    return a == b || (a != null && b != null && a.isPresent() && b.isPresent() && a.get().getClass().isArray() ? Arrays.equals((T[])a.get(), (T[])b.get()) : Objects.equals(a.get(), b.get()));
+    return a == b || (a != null && b != null && a.isPresent() && b.isPresent() && Objects.deepEquals(a.get(), b.get()));
   }
 
   @Override
@@ -597,9 +597,7 @@ public class NullableClass extends HashMap<String, Object> {
     if (a == null) {
       return 1;
     }
-    return a.isPresent()
-      ? (a.get().getClass().isArray() ? Arrays.hashCode((T[])a.get()) : Objects.hashCode(a.get()))
-      : 31;
+    return a.isPresent() ? Arrays.deepHashCode(new Object[]{a.get()}) : 31;
   }
 
   @Override
