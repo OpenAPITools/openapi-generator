25373ca40eb6d12815d4e55ae564c8a9deb4bcd9
Alexander <aleksandr.kostousov@telekom.com>
2021-09-01 20:15:31 +0300

    TAP-655 Put the func back

--- modules/openapi-generator-gradle-plugin/src/main/kotlin/org/openapitools/generator/gradle/plugin/tasks/GenerateTask.kt
+++ modules/openapi-generator-gradle-plugin/src/main/kotlin/org/openapitools/generator/gradle/plugin/tasks/GenerateTask.kt
@@ -459,6 +459,23 @@ open class GenerateTask : DefaultTask() {
     @Input
     val engine = project.objects.property<String?>()
 
+    private fun <T : Any?> Property<T>.ifNotEmpty(block: Property<T>.(T) -> Unit) {
+        if (isPresent) {
+            val item: T? = get()
+            if (item != null) {
+                when (get()) {
+                    is String -> if ((get() as String).isNotEmpty()) {
+                        block(get())
+                    }
+                    is String? -> if (true == (get() as String?)?.isNotEmpty()) {
+                        block(get())
+                    }
+                    else -> block(get())
+                }
+            }
+        }
+    }
+
     @Suppress("unused")
     @TaskAction
     fun doWork() {
