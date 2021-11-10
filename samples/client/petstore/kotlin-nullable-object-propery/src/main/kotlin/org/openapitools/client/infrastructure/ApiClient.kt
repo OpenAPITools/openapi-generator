3978ee01014c93e777ce3f9202c73a74d43d2cea
Eric Durand-Tremblay <etremblay@kronostechnologies.com>
2021-11-10 16:55:20 -0500

    Regenerate samples after rebase on master

--- samples/client/petstore/kotlin-nullable-object-propery/src/main/kotlin/org/openapitools/client/infrastructure/ApiClient.kt
+++ samples/client/petstore/kotlin-nullable-object-propery/src/main/kotlin/org/openapitools/client/infrastructure/ApiClient.kt
@@ -44,6 +44,7 @@ open class ApiClient(val baseUrl: String) {
         var username: String? = null
         var password: String? = null
         var accessToken: String? = null
+        const val baseUrlKey = "org.openapitools.client.baseUrl"
 
         @JvmStatic
         val client: OkHttpClient by lazy {
