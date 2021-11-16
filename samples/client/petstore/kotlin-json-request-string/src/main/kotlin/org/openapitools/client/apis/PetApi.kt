e8754c842bc3b1bc5e7059c407b80dcafd1f6d0f
WILLIAM CHENG <wing328hk@gmail.com>
2021-11-16 22:25:42 +0800

    update samples

--- samples/client/petstore/kotlin-json-request-string/src/main/kotlin/org/openapitools/client/apis/PetApi.kt
+++ samples/client/petstore/kotlin-json-request-string/src/main/kotlin/org/openapitools/client/apis/PetApi.kt
@@ -288,7 +288,7 @@ class PetApi(basePath: kotlin.String = defaultBasePath) : ApiClient(basePath) {
     /**
     * Get all pets
     * 
-    * @param lastUpdated When this endpoint was hit last to help indentify if the client already has the latest copy. (optional)
+    * @param lastUpdated When this endpoint was hit last to help identify if the client already has the latest copy. (optional)
     * @return ApiInfrastructureResponse<kotlin.collections.List<Pet>?>
     * @throws UnsupportedOperationException If the API returns an informational or redirection response
     * @throws ClientException If the API returns a client error response
