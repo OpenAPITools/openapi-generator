8caa3360c0bc369c867f19ea54ed25e5e42f959f
Tomáš Čermák <t-cermak@seznam.cz>
2021-11-16 12:23:23 +0100

    [Protobuf-Schema] Add enum as model support

--- modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/ProtobufSchemaCodegen.java
+++ modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/ProtobufSchemaCodegen.java
@@ -183,6 +183,14 @@ public class ProtobufSchemaCodegen extends DefaultCodegen implements CodegenConf
         return camelize(sanitizeName(operationId));
     }
 
+    public void addModelEnumIndexes(List<Map<String, Object>> enumVars){
+        int enumIndex = 0;
+        for (Map<String, Object> enumVar : enumVars) {
+            enumVar.put("protobuf-enum-index", enumIndex);
+            enumIndex++;
+        }
+    }
+
     @Override
     public Map<String, Object> postProcessModels(Map<String, Object> objs) {
         objs = postProcessModelsEnum(objs);
@@ -191,6 +199,16 @@ public class ProtobufSchemaCodegen extends DefaultCodegen implements CodegenConf
         for (Object _mo : models) {
             Map<String, Object> mo = (Map<String, Object>) _mo;
             CodegenModel cm = (CodegenModel) mo.get("model");
+
+            if(cm.isEnum) {
+                cm.setClassname(StringUtils.capitalize(cm.getName()) + "Enum");
+
+                Map<String, Object> allowableValues = cm.getAllowableValues();
+                if (allowableValues.containsKey("enumVars")) {
+                    List<Map<String, Object>> enumVars = (List<Map<String, Object>>)allowableValues.get("enumVars");
+                    addModelEnumIndexes(enumVars);
+                }
+            }
 
             for (CodegenProperty var : cm.vars) {
                 // add x-protobuf-type: repeated if it's an array
@@ -209,12 +227,8 @@ public class ProtobufSchemaCodegen extends DefaultCodegen implements CodegenConf
                 }
 
                 if (var.isEnum && var.allowableValues.containsKey("enumVars")) {
-                    List<Map<String, Object>> enumVars = (List<Map<String, Object>>) var.allowableValues.get("enumVars");
-                    int enumIndex = 0;
-                    for (Map<String, Object> enumVar : enumVars) {
-                        enumVar.put("protobuf-enum-index", enumIndex);
-                        enumIndex++;
-                    }
+                    List<Map<String, Object>> enumVars = (List<Map<String, Object>>)var.allowableValues.get("enumVars");
+                    addModelEnumIndexes(enumVars);
                 }
 
                 // Add x-protobuf-index, unless already specified
