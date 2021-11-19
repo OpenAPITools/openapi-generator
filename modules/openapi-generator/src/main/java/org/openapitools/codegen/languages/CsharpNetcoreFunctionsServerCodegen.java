6a5c2251f651bdd646584261da6b8c294fbd9c58
J Abraham <james.abraham@aqovia.com>
2021-11-19 16:39:31 +0000

    Removes sln and csproj

--- modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/CsharpNetcoreFunctionsServerCodegen.java
+++ modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/CsharpNetcoreFunctionsServerCodegen.java
@@ -14,7 +14,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class CsharpNetcoreFunctionsServerCodegen extends CSharpNetCoreClientCodegen  {
+public class CsharpNetcoreFunctionsServerCodegen extends CSharpNetCoreReducedClientCodegen  {
     public static final String PROJECT_NAME = "projectName";
 
     final Logger LOGGER = LoggerFactory.getLogger(CsharpNetcoreFunctionsServerCodegen.class);
