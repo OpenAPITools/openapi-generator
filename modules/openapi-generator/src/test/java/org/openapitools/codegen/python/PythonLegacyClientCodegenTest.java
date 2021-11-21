2bd43deacde77ed00767a8f58c138fbef1947c6f
Vidya Anjur <vidya.anjur@gmail.com>
2021-11-21 12:50:53 -0600

    update JUnit test for 6675

--- modules/openapi-generator/src/test/java/org/openapitools/codegen/python/PythonLegacyClientCodegenTest.java
+++ modules/openapi-generator/src/test/java/org/openapitools/codegen/python/PythonLegacyClientCodegenTest.java
@@ -91,6 +91,9 @@ public class PythonLegacyClientCodegenTest {
         Assert.assertEquals(op.allParams.get(4).pattern, "/^pattern\\/\\d{3}$/");
         // pattern_with_modifiers '/^pattern\d{3}$/i
         Assert.assertEquals(op.allParams.get(5).pattern, "/^pattern\\d{3}$/i");
+        // pattern_with_backslash_after_bracket '/^[\pattern\d{3}$/i'
+        // added to test fix for issue #6675
+        Assert.assertEquals(op.allParams.get(6).pattern, "/^[\\pattern\\d{3}$/i");
     }
 
 
