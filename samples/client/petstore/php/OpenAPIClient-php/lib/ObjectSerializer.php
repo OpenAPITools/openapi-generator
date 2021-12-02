ec1f69b3609e7a452001f0d54bfbd3843ac9a2e2
Carmen Quan <cquan@launchdarkly.com>
2021-12-02 10:26:32 -0800

    fix php precision bug

--- samples/client/petstore/php/OpenAPIClient-php/lib/ObjectSerializer.php
+++ samples/client/petstore/php/OpenAPIClient-php/lib/ObjectSerializer.php
@@ -324,8 +324,9 @@ class ObjectSerializer
                     // Some API's return a date-time with too high nanosecond
                     // precision for php's DateTime to handle. This conversion
                     // (string -> unix timestamp -> DateTime) is a workaround
-                    // for the problem.
-                    return (new \DateTime())->setTimestamp(strtotime($data));
+                    // for the problem. Note that strtotime only handles precision
+                    // up to a second, so trimming does not result in data loss.
+                    return (new \DateTime())->setTimestamp(strtotime(substr($data, 0, strpos($data, '.'))));
                 }
             } else {
                 return null;
