bb49ccb8007078f5b271837c59943da12755ce40
Bruno Coelho <4brunu@gmail.com>
2021-12-16 12:54:55 +0000

    [DefaultCodegen] generate unknown default case

--- samples/client/petstore/kotlin-enum-default-value/src/main/kotlin/org/openapitools/client/infrastructure/Serializer.kt
+++ samples/client/petstore/kotlin-enum-default-value/src/main/kotlin/org/openapitools/client/infrastructure/Serializer.kt
@@ -2,7 +2,6 @@ package org.openapitools.client.infrastructure
 
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.adapters.EnumJsonAdapter
-import org.openapitools.client.models.*
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 
 object Serializer {
@@ -17,8 +16,8 @@ object Serializer {
         .add(KotlinJsonAdapterFactory())
         .add(BigDecimalAdapter())
         .add(BigIntegerAdapter())
-        .add(ModelWithEnumPropertyHavingDefault.PropertyName::class.java, EnumJsonAdapter.create(ModelWithEnumPropertyHavingDefault.PropertyName::class.java)
-            .withUnknownFallback(ModelWithEnumPropertyHavingDefault.PropertyName.unknownDefaultOpenApi))
+        .add(org.openapitools.client.models.ModelWithEnumPropertyHavingDefault.PropertyName::class.java, EnumJsonAdapter.create(org.openapitools.client.models.ModelWithEnumPropertyHavingDefault.PropertyName::class.java)
+            .withUnknownFallback(org.openapitools.client.models.ModelWithEnumPropertyHavingDefault.PropertyName.unknownDefaultOpenApi))
 
     @JvmStatic
     val moshi: Moshi by lazy {
