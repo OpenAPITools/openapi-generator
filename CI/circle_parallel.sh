fa7c31ea86387229af6be2abb21ddb417d9cd877
Justin Black <justin.a.black@gmail.com>
2022-01-07 12:00:43 -0800

    Removes sudo

--- CI/circle_parallel.sh
+++ CI/circle_parallel.sh
@@ -92,7 +92,7 @@ elif [ "$NODE_INDEX" = "3" ]; then
 
   # Note: it may be possible to run openapitools/openapi-petstore as a second docker image
   # I wasn't abe to get it working so run the server in the image
-  sudo systemctl start docker
+  systemctl start docker
   printf '127.0.0.1       petstore.swagger.io' | tee -a /etc/hosts
   docker pull openapitools/openapi-petstore
   docker run -d -e OPENAPI_BASE_PATH=/v3 -e DISABLE_API_KEY=1 -e DISABLE_OAUTH=1 -p 80:8080 openapitools/openapi-petstore
