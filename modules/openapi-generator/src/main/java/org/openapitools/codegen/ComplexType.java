ff4f9eb9757577f3fc23d16f2bac0b8bb5f43102
ethan92429 <ekeller@ptc.com>
2022-01-07 09:56:09 -0500

    add javadoc comments

--- modules/openapi-generator/src/main/java/org/openapitools/codegen/ComplexType.java
+++ modules/openapi-generator/src/main/java/org/openapitools/codegen/ComplexType.java
@@ -7,20 +7,37 @@ import java.util.Set;
 import java.util.stream.Stream;
 
 public interface ComplexType {
+
+    /**
+      * @return basic type - no generics supported.
+     */
     String getBaseType();
+
+    /**
+     * @return complex type that can contain type parameters - like {@code List<Items>} for Java
+     */
     String getComplexType();
 
+    /**
+     * @return true if set, map or list.
+     */
     boolean isContainer();
 
+    /**
+     * @return the composed schemas that might make up this type, null otherwise.
+     */
     CodegenComposedSchemas getComposedSchemas();
 
+    /**
+     * @return the inner type in the case of a type with type parameters
+     */
     ComplexType getInner();
 
     /**
-     * Recursively collect all necessary imports to include in order to resolve this type.
+     * Recursively collect all necessary imports to include so that the type may be resolved.
      *
      * @param includeContainerTypes
-     * @return
+     * @return all of the imports
      */
     default Set<String> getImports(boolean includeContainerTypes) {
         Set<String> imports = new HashSet<>();
