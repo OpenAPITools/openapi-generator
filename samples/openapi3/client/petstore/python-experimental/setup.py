680d3bfb146d49cf3fc91a2a1304ce52e1bff78d
Justin Black <justin.a.black@gmail.com>
2022-01-10 15:48:58 -0800

    Fixes the python-experimental setup.py file so it works with generatorLanguageVersion

--- samples/openapi3/client/petstore/python-experimental/setup.py
+++ samples/openapi3/client/petstore/python-experimental/setup.py
@@ -37,7 +37,7 @@ setup(
     author_email="team@openapitools.org",
     url="",
     keywords=["OpenAPI", "OpenAPI-Generator", "OpenAPI Petstore"],
-    python_requires="&gt;&#x3D;3.9",
+    python_requires=">=3.9",
     install_requires=REQUIRES,
     packages=find_packages(exclude=["test", "tests"]),
     include_package_data=True,
