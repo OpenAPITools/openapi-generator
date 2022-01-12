5407c96f2e64ba58ca302498991cc89e8fcadbe4
Géry THRASIBULE <g.thrasibule@lecomptoirdespharmacies.fr>
2022-01-12 09:38:53 +0100

    LDS-2166 : fix test
    Envoyé depuis mon iPhone.
    P.S. : Ce commit est certifié sans gluten

--- samples/client/petstore/python/test/test_fake_api.py
+++ samples/client/petstore/python/test/test_fake_api.py
@@ -127,6 +127,7 @@ class TestFakeApi(unittest.TestCase):
                 _request_timeout=None,
                 _return_http_data_only=True,
                 _spec_property_naming=False,
+                _request_auth=None,
                 async_req=False,
                 header_number=1.234,
                 path_integer=34,
