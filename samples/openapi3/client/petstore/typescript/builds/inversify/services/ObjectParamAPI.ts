d38609e0ceedfd8a33d41fdd0085df7734391544
David Gamero <david340804@gmail.com>
2022-01-18 14:56:34 -0500

    remove extra space

--- samples/openapi3/client/petstore/typescript/builds/inversify/services/ObjectParamAPI.ts
+++ samples/openapi3/client/petstore/typescript/builds/inversify/services/ObjectParamAPI.ts
@@ -15,52 +15,52 @@ export abstract class AbstractObjectPetApi {
      * Add a new pet to the store
      * @param param the request object
      */
-    public abstract addPet(param: req.PetApiAddPetRequest , options?: Configuration): Promise<Pet>;
+    public abstract addPet(param: req.PetApiAddPetRequest, options?: Configuration): Promise<Pet>;
 
     /**
      * Deletes a pet
      * @param param the request object
      */
-    public abstract deletePet(param: req.PetApiDeletePetRequest , options?: Configuration): Promise<void>;
+    public abstract deletePet(param: req.PetApiDeletePetRequest, options?: Configuration): Promise<void>;
 
     /**
      * Multiple status values can be provided with comma separated strings
      * Finds Pets by status
      * @param param the request object
      */
-    public abstract findPetsByStatus(param: req.PetApiFindPetsByStatusRequest , options?: Configuration): Promise<Array<Pet>>;
+    public abstract findPetsByStatus(param: req.PetApiFindPetsByStatusRequest, options?: Configuration): Promise<Array<Pet>>;
 
     /**
      * Multiple tags can be provided with comma separated strings. Use tag1, tag2, tag3 for testing.
      * Finds Pets by tags
      * @param param the request object
      */
-    public abstract findPetsByTags(param: req.PetApiFindPetsByTagsRequest , options?: Configuration): Promise<Array<Pet>>;
+    public abstract findPetsByTags(param: req.PetApiFindPetsByTagsRequest, options?: Configuration): Promise<Array<Pet>>;
 
     /**
      * Returns a single pet
      * Find pet by ID
      * @param param the request object
      */
-    public abstract getPetById(param: req.PetApiGetPetByIdRequest , options?: Configuration): Promise<Pet>;
+    public abstract getPetById(param: req.PetApiGetPetByIdRequest, options?: Configuration): Promise<Pet>;
 
     /**
      * Update an existing pet
      * @param param the request object
      */
-    public abstract updatePet(param: req.PetApiUpdatePetRequest , options?: Configuration): Promise<Pet>;
+    public abstract updatePet(param: req.PetApiUpdatePetRequest, options?: Configuration): Promise<Pet>;
 
     /**
      * Updates a pet in the store with form data
      * @param param the request object
      */
-    public abstract updatePetWithForm(param: req.PetApiUpdatePetWithFormRequest , options?: Configuration): Promise<void>;
+    public abstract updatePetWithForm(param: req.PetApiUpdatePetWithFormRequest, options?: Configuration): Promise<void>;
 
     /**
      * uploads an image
      * @param param the request object
      */
-    public abstract uploadFile(param: req.PetApiUploadFileRequest , options?: Configuration): Promise<ApiResponse>;
+    public abstract uploadFile(param: req.PetApiUploadFileRequest, options?: Configuration): Promise<ApiResponse>;
 
 }
 
@@ -71,27 +71,27 @@ export abstract class AbstractObjectStoreApi {
      * Delete purchase order by ID
      * @param param the request object
      */
-    public abstract deleteOrder(param: req.StoreApiDeleteOrderRequest , options?: Configuration): Promise<void>;
+    public abstract deleteOrder(param: req.StoreApiDeleteOrderRequest, options?: Configuration): Promise<void>;
 
     /**
      * Returns a map of status codes to quantities
      * Returns pet inventories by status
      * @param param the request object
      */
-    public abstract getInventory(param: req.StoreApiGetInventoryRequest  = {}, options?: Configuration): Promise<{ [key: string]: number; }>;
+    public abstract getInventory(param: req.StoreApiGetInventoryRequest = {}, options?: Configuration): Promise<{ [key: string]: number; }>;
 
     /**
      * For valid response try integer IDs with value <= 5 or > 10. Other values will generated exceptions
      * Find purchase order by ID
      * @param param the request object
      */
-    public abstract getOrderById(param: req.StoreApiGetOrderByIdRequest , options?: Configuration): Promise<Order>;
+    public abstract getOrderById(param: req.StoreApiGetOrderByIdRequest, options?: Configuration): Promise<Order>;
 
     /**
      * Place an order for a pet
      * @param param the request object
      */
-    public abstract placeOrder(param: req.StoreApiPlaceOrderRequest , options?: Configuration): Promise<Order>;
+    public abstract placeOrder(param: req.StoreApiPlaceOrderRequest, options?: Configuration): Promise<Order>;
 
 }
 
@@ -102,50 +102,50 @@ export abstract class AbstractObjectUserApi {
      * Create user
      * @param param the request object
      */
-    public abstract createUser(param: req.UserApiCreateUserRequest , options?: Configuration): Promise<void>;
+    public abstract createUser(param: req.UserApiCreateUserRequest, options?: Configuration): Promise<void>;
 
     /**
      * Creates list of users with given input array
      * @param param the request object
      */
-    public abstract createUsersWithArrayInput(param: req.UserApiCreateUsersWithArrayInputRequest , options?: Configuration): Promise<void>;
+    public abstract createUsersWithArrayInput(param: req.UserApiCreateUsersWithArrayInputRequest, options?: Configuration): Promise<void>;
 
     /**
      * Creates list of users with given input array
      * @param param the request object
      */
-    public abstract createUsersWithListInput(param: req.UserApiCreateUsersWithListInputRequest , options?: Configuration): Promise<void>;
+    public abstract createUsersWithListInput(param: req.UserApiCreateUsersWithListInputRequest, options?: Configuration): Promise<void>;
 
     /**
      * This can only be done by the logged in user.
      * Delete user
      * @param param the request object
      */
-    public abstract deleteUser(param: req.UserApiDeleteUserRequest , options?: Configuration): Promise<void>;
+    public abstract deleteUser(param: req.UserApiDeleteUserRequest, options?: Configuration): Promise<void>;
 
     /**
      * Get user by user name
      * @param param the request object
      */
-    public abstract getUserByName(param: req.UserApiGetUserByNameRequest , options?: Configuration): Promise<User>;
+    public abstract getUserByName(param: req.UserApiGetUserByNameRequest, options?: Configuration): Promise<User>;
 
     /**
      * Logs user into the system
      * @param param the request object
      */
-    public abstract loginUser(param: req.UserApiLoginUserRequest , options?: Configuration): Promise<string>;
+    public abstract loginUser(param: req.UserApiLoginUserRequest, options?: Configuration): Promise<string>;
 
     /**
      * Logs out current logged in user session
      * @param param the request object
      */
-    public abstract logoutUser(param: req.UserApiLogoutUserRequest  = {}, options?: Configuration): Promise<void>;
+    public abstract logoutUser(param: req.UserApiLogoutUserRequest = {}, options?: Configuration): Promise<void>;
 
     /**
      * This can only be done by the logged in user.
      * Updated user
      * @param param the request object
      */
-    public abstract updateUser(param: req.UserApiUpdateUserRequest , options?: Configuration): Promise<void>;
+    public abstract updateUser(param: req.UserApiUpdateUserRequest, options?: Configuration): Promise<void>;
 
 }
