12831b1a1a50d12db21484d041991e7248acd5a4
Anton Davydov <dydus0x14@gmail.com>
2022-01-22 18:55:23 +0300

    swift-alt v0.5.0

--- modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/SwiftAltClientCodegen.java
+++ modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/SwiftAltClientCodegen.java
@@ -755,7 +755,7 @@ public class SwiftAltClientCodegen extends DefaultCodegen implements CodegenConf
         System.out.println("################################################################################");
         System.out.println("# Thanks for using OpenAPI Generator.                                          #");
         System.out.println("# swift alternative generator is contributed by @dydus0x14 and @ptiz.          #");
-        System.out.println("# swift alternative generator v0.4.0                                           #");
+        System.out.println("# swift alternative generator v0.5.0                                           #");
         System.out.println("################################################################################");
     }
 }
