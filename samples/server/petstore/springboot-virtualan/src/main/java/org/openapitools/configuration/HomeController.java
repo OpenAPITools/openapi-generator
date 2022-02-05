97e847c82948db140b9f5c7b9fb7e6122bda048f
Lars Uffmann <lars.uffmann@gmail.com>
2022-02-05 19:20:36 +0100

    Generate Samples

--- samples/server/petstore/springboot-virtualan/src/main/java/org/openapitools/configuration/HomeController.java
+++ samples/server/petstore/springboot-virtualan/src/main/java/org/openapitools/configuration/HomeController.java
@@ -1,8 +1,10 @@
 package org.openapitools.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.GetMapping;
 
 /**
  * Home redirection to OpenAPI api documentation
@@ -10,10 +12,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 
+    @GetMapping(value = "/swagger-config.yaml", produces = "text/plain")
+    @ResponseBody
+    public String swaggerConfig() {
+        return "url: /v2/api-docs\n";
+    }
     @RequestMapping("/")
     public String index() {
         return "redirect:swagger-ui.html";
     }
-
 
 }
