2982b6d5ff59de40f6ba858fcc77112048c3ba2b
Lars Uffmann <lars.uffmann@gmail.com>
2022-02-08 08:14:56 +0100

    Add more test coverage.

--- modules/openapi-generator/src/test/java/org/openapitools/codegen/java/AbstractJavaCodegenTest.java
+++ modules/openapi-generator/src/test/java/org/openapitools/codegen/java/AbstractJavaCodegenTest.java
@@ -20,8 +20,11 @@ package org.openapitools.codegen.java;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.media.*;
+import java.util.HashSet;
+import java.util.Set;
 import org.openapitools.codegen.CodegenConstants;
 import org.openapitools.codegen.CodegenModel;
+import org.openapitools.codegen.CodegenParameter;
 import org.openapitools.codegen.CodegenType;
 import org.openapitools.codegen.TestUtils;
 import org.openapitools.codegen.languages.AbstractJavaCodegen;
@@ -592,6 +595,44 @@ public class AbstractJavaCodegenTest {
         numberSchema.setFormat("double");
         defaultValue = codegen.toDefaultValue(numberSchema);
         Assert.assertEquals(defaultValue, doubleValue + "d");
+    }
+
+    @Test
+    public void dateDefaultValueIsIsoDate() {
+        final OpenAPI openAPI = TestUtils.parseFlattenSpec("src/test/resources/3_0/spring/date-time-parameter-types-for-testing.yml");
+        final P_AbstractJavaCodegen codegen = new P_AbstractJavaCodegen();
+        codegen.setOpenAPI(openAPI);
+
+        Set<String> imports = new HashSet<>();
+        CodegenParameter parameter = codegen.fromParameter(openAPI.getPaths().get("/thingy/{date}").getGet().getParameters().get(2), imports);
+
+        Assert.assertEquals(parameter.dataType, "Date");
+        Assert.assertEquals(parameter.isDate, true);
+        Assert.assertEquals(parameter.defaultValue, "1974-01-01");
+        Assert.assertEquals(imports.size(), 1);
+        Assert.assertEquals(imports.iterator().next(), "Date");
+
+        Assert.assertNotNull(parameter.getSchema());
+        Assert.assertEquals(parameter.getSchema().baseType, "Date");
+    }
+
+    @Test
+    public void dateDefaultValueIsIsoDateTime() {
+        final OpenAPI openAPI = TestUtils.parseFlattenSpec("src/test/resources/3_0/spring/date-time-parameter-types-for-testing.yml");
+        final P_AbstractJavaCodegen codegen = new P_AbstractJavaCodegen();
+        codegen.setOpenAPI(openAPI);
+
+        Set<String> imports = new HashSet<>();
+        CodegenParameter parameter = codegen.fromParameter(openAPI.getPaths().get("/thingy/{date}").getGet().getParameters().get(1), imports);
+
+        Assert.assertEquals(parameter.dataType, "Date");
+        Assert.assertEquals(parameter.isDateTime, true);
+        Assert.assertEquals(parameter.defaultValue, "1973-12-19T03:39:57-08:00");
+        Assert.assertEquals(imports.size(), 1);
+        Assert.assertEquals(imports.iterator().next(), "Date");
+
+        Assert.assertNotNull(parameter.getSchema());
+        Assert.assertEquals(parameter.getSchema().baseType, "Date");
     }
 
     @Test
