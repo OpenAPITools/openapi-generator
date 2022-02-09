a5c154ff5a5ab52d9e1558efc2ccfb9d2f0b96ef
William Cheng <wing328hk@gmail.com>
2022-02-09 20:49:59 +0800

    fix java inflector

--- samples/server/petstore/java-inflector/src/main/java/org/openapitools/controllers/FakeController.java
+++ samples/server/petstore/java-inflector/src/main/java/org/openapitools/controllers/FakeController.java
@@ -23,7 +23,7 @@ import org.openapitools.model.XmlItem;
 
 @javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaInflectorServerCodegen")
 public class FakeController  {
-  /** 
+  /**
    * Uncomment and implement as you see fit.  These operations will map
    * Directly to operation calls from the routing logic.  Because the inflector
    * Code allows you to implement logic incrementally, they are disabled.
@@ -120,4 +120,3 @@ public class FakeController  {
   */
 
 }
-
--- samples/server/petstore/jaxrs-spec-interface-response/src/gen/java/org/openapitools/model/EnumArrays.java
+++ samples/server/petstore/jaxrs-spec-interface-response/src/gen/java/org/openapitools/model/EnumArrays.java
@@ -87,7 +87,7 @@ public enum ArrayEnumEnum {
     }
 }
 
-  private @Valid List<ArrayEnumEnum> arrayEnum = new ArrayList<ArrayEnumEnum>();
+  private @Valid List<ArrayEnumEnum> arrayEnum = new ArrayList<>();
 
   /**
    **/
@@ -131,6 +131,22 @@ public enum ArrayEnumEnum {
     this.arrayEnum = arrayEnum;
   }
 
+  public EnumArrays addArrayEnumItem(ArrayEnumEnum arrayEnumItem) {
+    if (this.arrayEnum == null) {
+      this.arrayEnum = new ArrayList<>();
+    }
+
+    this.arrayEnum.add(arrayEnumItem);
+    return this;
+  }
+
+  public EnumArrays removeArrayEnumItem(ArrayEnumEnum arrayEnumItem) {
+    if (arrayEnumItem != null && this.arrayEnum != null) {
+      this.arrayEnum.remove(arrayEnumItem);
+    }
+
+    return this;
+  }
 
   @Override
   public boolean equals(Object o) {
