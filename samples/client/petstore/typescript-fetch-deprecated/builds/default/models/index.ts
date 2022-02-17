901e4f2ddc18a3c747995886a5e97787bea8d8ca
Ran Halprin <ran@myhippo.com>
2022-02-17 14:14:36 -0800

    Generate samples for deprecated typescript fields

---
+++ samples/client/petstore/typescript-fetch-deprecated/builds/default/models/index.ts
@@ -0,0 +1,8 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './Category';
+export * from './ModelApiResponse';
+export * from './Order';
+export * from './Pet';
+export * from './Tag';
+export * from './User';
