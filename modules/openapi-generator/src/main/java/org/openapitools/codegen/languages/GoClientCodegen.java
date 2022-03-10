6f37409d2be846d542e37ea496ac0c807a7c7cf5
Noah Fontes <noah.fontes@puppet.com>
2022-03-10 00:31:44 -0800

    Fix nullable models embedded in allOf (#11804)
    It is possible, though contrived, for a property to have both isAnyType
    and isModel set. In this case, when the outer type is nullable, the Go
    template expects a Nullable* type to be provided by the generator, but
    it is skipped over instead. This change aligns the generator with the
    template's expectations.

--- modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/GoClientCodegen.java
+++ modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/GoClientCodegen.java
@@ -403,8 +403,8 @@ public class GoClientCodegen extends AbstractGoCodegen {
 
                 for (CodegenProperty param : Iterables.concat(model.vars, model.allVars, model.requiredVars, model.optionalVars)) {
                     param.vendorExtensions.put("x-go-base-type", param.dataType);
-                    if (!param.isNullable || param.isMap || param.isArray ||
-                            param.isFreeFormObject || param.isAnyType) {
+                    if (!param.isNullable || param.isContainer || param.isFreeFormObject
+                            || (param.isAnyType && !param.isModel)) {
                         continue;
                     }
                     if (param.isDateTime) {
