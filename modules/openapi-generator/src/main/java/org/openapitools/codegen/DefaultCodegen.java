a4e2dde745d7eef974334388471d65d9c56f1a7a
Justin Black <spacether@users.noreply.github.com>
2022-03-30 15:30:26 -0700

    Fixes issue where object in query parameter generates a NPE (#12010)
    * Fixes issue 11946
    
    * Fixes items assignment for deep object query params
    
    * Adds fix for inline query param object schemas
    
    * Fixes keyword name, changes it from mapBean[keyword]
    
    * Reverts version file

--- modules/openapi-generator/src/main/java/org/openapitools/codegen/DefaultCodegen.java
+++ modules/openapi-generator/src/main/java/org/openapitools/codegen/DefaultCodegen.java
@@ -269,6 +269,8 @@ public class DefaultCodegen implements CodegenConfig {
     // A cache to efficiently lookup schema `toModelName()` based on the schema Key
     private final Map<String, String> schemaKeyToModelNameCache = new HashMap<>();
 
+    protected boolean loadDeepObjectIntoItems = true;
+
     @Override
     public List<CliOption> cliOptions() {
         return cliOptions;
@@ -4748,10 +4750,12 @@ public class DefaultCodegen implements CodegenConfig {
         }
         codegenParameter.pattern = toRegularExpression(parameterSchema.getPattern());
 
-        if (codegenParameter.isQueryParam && codegenParameter.isDeepObject) {
-            Schema schema = ModelUtils.getSchema(openAPI, codegenParameter.dataType);
+        if (codegenParameter.isQueryParam && codegenParameter.isDeepObject && loadDeepObjectIntoItems) {
+            Schema schema = parameterSchema;
+            if (schema.get$ref() != null) {
+                schema = ModelUtils.getReferencedSchema(openAPI, schema);
+            }
             codegenParameter.items = fromProperty(codegenParameter.paramName, schema);
-            // TODO Check why schema is actually null for a schema of type object defined inline
             // https://swagger.io/docs/specification/serialization/
             if (schema != null) {
                 Map<String, Schema<?>> properties = schema.getProperties();
