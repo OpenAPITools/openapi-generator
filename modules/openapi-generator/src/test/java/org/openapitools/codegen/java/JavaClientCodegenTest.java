c21d2cde195136a92a79f1f9b1d8f9f64b83424a
Mikko Maunu <mikko@maunu.org>
2022-04-03 10:53:29 +0200

    Added space between @NotNull-annotation and Java type to produce code that can be compiled. fix #11340 (#12034)

--- modules/openapi-generator/src/test/java/org/openapitools/codegen/java/JavaClientCodegenTest.java
+++ modules/openapi-generator/src/test/java/org/openapitools/codegen/java/JavaClientCodegenTest.java
@@ -37,6 +37,7 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import java.util.Optional;
+import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -53,8 +54,10 @@ import org.openapitools.codegen.CodegenSecurity;
 import org.openapitools.codegen.DefaultGenerator;
 import org.openapitools.codegen.TestUtils;
 import org.openapitools.codegen.config.CodegenConfigurator;
+import org.openapitools.codegen.java.assertions.JavaFileAssert;
 import org.openapitools.codegen.languages.AbstractJavaCodegen;
 import org.openapitools.codegen.languages.JavaClientCodegen;
+import org.openapitools.codegen.languages.features.BeanValidationFeatures;
 import org.openapitools.codegen.languages.features.CXFServerFeatures;
 import org.testng.Assert;
 import org.testng.annotations.Ignore;
@@ -1408,5 +1411,37 @@ public class JavaClientCodegenTest {
 
         TestUtils.assertExtraAnnotationFiles(outputPath + "/src/main/java/org/openapitools/client/model");
 
+    }
+
+    /**
+     * See https://github.com/OpenAPITools/openapi-generator/issues/11340
+     */
+    @Test
+    public void testReferencedHeader2() throws Exception {
+        File output = Files.createTempDirectory("test").toFile().getCanonicalFile();
+        output.deleteOnExit();
+        Map<String, Object> additionalProperties = new HashMap<>();
+        additionalProperties.put(BeanValidationFeatures.USE_BEANVALIDATION, "true");
+        final CodegenConfigurator configurator = new CodegenConfigurator().setGeneratorName("java")
+                .setAdditionalProperties(additionalProperties)
+                .setInputSpec("src/test/resources/3_0/issue-11340.yaml")
+                .setOutputDir(output.getAbsolutePath()
+                        .replace("\\", "/"));
+
+        final ClientOptInput clientOptInput = configurator.toClientOptInput();
+        DefaultGenerator generator = new DefaultGenerator();
+
+        Map<String, File> files = generator.opts(clientOptInput).generate().stream()
+                .collect(Collectors.toMap(File::getName, Function.identity()));
+
+        JavaFileAssert.assertThat(files.get("DefaultApi.java"))
+                .assertMethod("operationWithHttpInfo")
+                    .hasParameter("requestBody")
+                    .assertParameterAnnotations()
+                    .containsWithName("NotNull")
+                .toParameter().toMethod()
+                    .hasParameter("xNonNullHeaderParameter")
+                    .assertParameterAnnotations()
+                    .containsWithName("NotNull");
     }
 }
