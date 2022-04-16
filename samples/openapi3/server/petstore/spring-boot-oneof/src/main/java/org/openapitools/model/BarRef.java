f195a83aaf6bcf1f1c22179725946638f69fada7
cachescrubber <5127753+cachescrubber@users.noreply.github.com>
2022-04-16 10:48:14 +0200

    [Java/Spring] all-of and one-of Improvements and Fixes (was #12075) (#12089)
    * Fix Bug in OneOfImplementorAdditionalData pulling in wrong vars to one-of-implementors.
    Support parentVars in order to support fluent setter with inherited properties.
    
    Squashed commit of the following:
    
    commit f945c943777a1a496d7de8fc0a188842d9efb1ac
    Author: Lars Uffmann <lars.uffmann@gmail.com>
    Date:   Thu Apr 7 18:22:54 2022 +0200
    
        Polishing
    
    commit 23ce1d0ff1faff53e85ca4362f33660962aa6a92
    Author: Lars Uffmann <lars.uffmann@vitroconnect.de>
    Date:   Thu Apr 7 17:15:28 2022 +0200
    
        Add JavaDoc
    
    commit fee70fde5709afa67f3aabd4f48ba496df63a884
    Author: Lars Uffmann <lars.uffmann@vitroconnect.de>
    Date:   Thu Apr 7 17:11:17 2022 +0200
    
        Add imports for inherited Properties
    
    commit 29509aaac51750fbd33c00a57d32cac34cbcbb90
    Author: Lars Uffmann <lars.uffmann@vitroconnect.de>
    Date:   Thu Apr 7 13:40:36 2022 +0200
    
        Generate Samples
    
    commit 1d19d5465137d3af712f2fd3b4ae4474c58af15e
    Author: Lars Uffmann <lars.uffmann@vitroconnect.de>
    Date:   Thu Apr 7 13:21:23 2022 +0200
    
        SpringCodegen: Support parentVars in order to support fluent setter with inherited properties.
    
    commit 2217a77bb747d0b07ef17407a6b5dd5c624a2551
    Author: Lars Uffmann <lars.uffmann@gmail.com>
    Date:   Thu Apr 7 07:18:50 2022 +0200
    
        Add allVars to omit list in OneOfImplementorAdditionalData
    
    commit 90499a3b0a187971bfe25deb6355c3444dcf89a7
    Author: Lars Uffmann <lars.uffmann@gmail.com>
    Date:   Wed Apr 6 16:40:23 2022 +0200
    
        Works exactly as needed for oneOf/allOf in Java/Spring
    
    commit b6d496d772e0d0a8d87a3b8cdba8fd3ca4db7f3f
    Author: Lars Uffmann <lars.uffmann@gmail.com>
    Date:   Wed Apr 6 15:16:27 2022 +0200
    
        Debug Session: identify critical codep path
    
    commit 85722360038107f15841d5acc448d93dee513a06
    Author: Lars Uffmann <lars.uffmann@vitroconnect.de>
    Date:   Tue Apr 5 09:56:38 2022 +0200
    
        Add test case to reproduce issue.
    
    commit 14acc5cd974bb5260f3751015558807e2eb1a8a1
    Author: Lars Uffmann <lars.uffmann@gmail.com>
    Date:   Tue Apr 5 06:57:30 2022 +0200
    
        Add config to reproduce the issue
    
    * Adjust indentation.
    
    Co-authored-by: Lars Uffmann <lars@wintermute.local>

--- samples/openapi3/server/petstore/spring-boot-oneof/src/main/java/org/openapitools/model/BarRef.java
+++ samples/openapi3/server/petstore/spring-boot-oneof/src/main/java/org/openapitools/model/BarRef.java
@@ -7,56 +7,57 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
-import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
-import org.openapitools.model.Animal;
-import org.openapitools.model.DogAllOf;
+import org.openapitools.model.EntityRef;
 import org.openapitools.jackson.nullable.JsonNullable;
 import java.time.OffsetDateTime;
 import javax.validation.Valid;
 import javax.validation.constraints.*;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 
 import java.util.*;
 import javax.annotation.Generated;
 
 /**
- * Dog
+ * BarRef
  */
 
 
 @Generated(value = "org.openapitools.codegen.languages.SpringCodegen")
-public class Dog extends Animal {
+public class BarRef extends EntityRef implements BarRefOrValue {
 
-  @JsonProperty("breed")
-  private String breed;
-
-  public Dog breed(String breed) {
-    this.breed = breed;
+  public BarRef name(String name) {
+    super.setName(name);
     return this;
   }
 
-  /**
-   * Get breed
-   * @return breed
-  */
-  
-  @ApiModelProperty(value = "")
-  public String getBreed() {
-    return breed;
-  }
-
-  public void setBreed(String breed) {
-    this.breed = breed;
-  }
-
-  public Dog className(String className) {
-    super.setClassName(className);
+  public BarRef atReferredType(String atReferredType) {
+    super.setAtReferredType(atReferredType);
     return this;
   }
 
-  public Dog color(String color) {
-    super.setColor(color);
+  public BarRef href(String href) {
+    super.setHref(href);
+    return this;
+  }
+
+  public BarRef id(String id) {
+    super.setId(id);
+    return this;
+  }
+
+  public BarRef atSchemaLocation(String atSchemaLocation) {
+    super.setAtSchemaLocation(atSchemaLocation);
+    return this;
+  }
+
+  public BarRef atBaseType(String atBaseType) {
+    super.setAtBaseType(atBaseType);
+    return this;
+  }
+
+  public BarRef atType(String atType) {
+    super.setAtType(atType);
     return this;
   }
 
@@ -68,22 +69,19 @@ public class Dog extends Animal {
     if (o == null || getClass() != o.getClass()) {
       return false;
     }
-    Dog dog = (Dog) o;
-    return Objects.equals(this.breed, dog.breed) &&
-        super.equals(o);
+    return true;
   }
 
   @Override
   public int hashCode() {
-    return Objects.hash(breed, super.hashCode());
+    return Objects.hash(super.hashCode());
   }
 
   @Override
   public String toString() {
     StringBuilder sb = new StringBuilder();
-    sb.append("class Dog {\n");
+    sb.append("class BarRef {\n");
     sb.append("    ").append(toIndentedString(super.toString())).append("\n");
-    sb.append("    breed: ").append(toIndentedString(breed)).append("\n");
     sb.append("}");
     return sb.toString();
   }
