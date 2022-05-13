472b52776d0a4cd37d6435d857d07fe8d3ec8b70
Ahmed Fwela <ahmednfwela@bdaya-dev.com>
2022-05-13 11:09:10 +0200

    resolve conflicts

--- samples/openapi3/client/petstore/dart-dio/petstore_client_lib_fake/lib/src/model/foo_get_default_response.dart
+++ samples/openapi3/client/petstore/dart-dio/petstore_client_lib_fake/lib/src/model/foo_get_default_response.dart
@@ -5,6 +5,9 @@
 import 'package:openapi/src/model/foo.dart';
 import 'package:built_value/built_value.dart';
 import 'package:built_value/serializer.dart';
+import 'package:one_of/one_of.dart';
+import 'package:one_of/any_of.dart';
+// ignore_for_file: unused_element, unused_import
 
 part 'foo_get_default_response.g.dart';
 
@@ -12,61 +15,77 @@ part 'foo_get_default_response.g.dart';
 ///
 /// Properties:
 /// * [string] 
+@BuiltValue()
 abstract class FooGetDefaultResponse implements Built<FooGetDefaultResponse, FooGetDefaultResponseBuilder> {
     @BuiltValueField(wireName: r'string')
     Foo? get string;
 
+
     FooGetDefaultResponse._();
+    
+    factory FooGetDefaultResponse([void updates(FooGetDefaultResponseBuilder b)]) = _$FooGetDefaultResponse;
 
     @BuiltValueHook(initializeBuilder: true)
     static void _defaults(FooGetDefaultResponseBuilder b) => b;
 
-    factory FooGetDefaultResponse([void updates(FooGetDefaultResponseBuilder b)]) = _$FooGetDefaultResponse;
-
     @BuiltValueSerializer(custom: true)
     static Serializer<FooGetDefaultResponse> get serializer => _$FooGetDefaultResponseSerializer();
+
+
 }
 
-class _$FooGetDefaultResponseSerializer implements StructuredSerializer<FooGetDefaultResponse> {
+class _$FooGetDefaultResponseSerializer implements PrimitiveSerializer<FooGetDefaultResponse> {
     @override
     final Iterable<Type> types = const [FooGetDefaultResponse, _$FooGetDefaultResponse];
 
     @override
     final String wireName = r'FooGetDefaultResponse';
 
-    @override
-    Iterable<Object?> serialize(Serializers serializers, FooGetDefaultResponse object,
-        {FullType specifiedType = FullType.unspecified}) {
-        final result = <Object?>[];
+    Iterable<Object?> _serializeProperties(Serializers serializers, FooGetDefaultResponse object,
+        {FullType specifiedType = FullType.unspecified}) sync* {        
         if (object.string != null) {
-            result
-                ..add(r'string')
-                ..add(serializers.serialize(object.string,
-                    specifiedType: const FullType(Foo)));
+            yield r'string';
+            yield serializers.serialize(object.string,
+                    specifiedType: const FullType(Foo));
         }
-        return result;
     }
 
     @override
-    FooGetDefaultResponse deserialize(Serializers serializers, Iterable<Object?> serialized,
+    Object serialize(Serializers serializers, FooGetDefaultResponse object,
         {FullType specifiedType = FullType.unspecified}) {
-        final result = FooGetDefaultResponseBuilder();
+        return _serializeProperties(serializers, object, specifiedType: specifiedType).toList();
+    }
 
-        final iterator = serialized.iterator;
-        while (iterator.moveNext()) {
-            final key = iterator.current as String;
-            iterator.moveNext();
-            final Object? value = iterator.current;
-            
+    void _deserializeProperties(Serializers serializers, Object serialized,
+        {FullType specifiedType = FullType.unspecified, required List<Object?> serializedList,required FooGetDefaultResponseBuilder result, required List<Object?> unhandled}) {
+        for (var i = 0; i < serializedList.length; i += 2) {
+            final key = serializedList[i] as String;
+            final value = serializedList[i + 1];
             switch (key) {
-                case r'string':
+                 case r'string':
                     final valueDes = serializers.deserialize(value,
                         specifiedType: const FullType(Foo)) as Foo;
                     result.string.replace(valueDes);
                     break;
+                default:
+                  unhandled.add(key);
+                  unhandled.add(value);
+                  break;
             }
         }
+    }
+    
+    @override
+    FooGetDefaultResponse deserialize(Serializers serializers, Object serialized,
+        {FullType specifiedType = FullType.unspecified}) {
+        final result = FooGetDefaultResponseBuilder();
+        final serializedList = (serialized as Iterable<Object?>).toList();        
+        final unhandled = <Object?>[];
+        _deserializeProperties(serializers, serialized, specifiedType: specifiedType, serializedList: serializedList, unhandled: unhandled, result: result);        
         return result.build();
     }
 }
+
+
+
 
