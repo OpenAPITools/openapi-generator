1eab4ddf62ac693af72d34bc58667a479b70e2e9
devhl <shawnkanyer@gmail.com>
2022-05-13 20:55:22 -0400

    fixed a bug

--- samples/server/petstore/aspnetcore-6.0/src/Org.OpenAPITools/Controllers/StoreApi.cs
+++ samples/server/petstore/aspnetcore-6.0/src/Org.OpenAPITools/Controllers/StoreApi.cs
@@ -69,8 +69,8 @@ namespace Org.OpenAPITools.Controllers
             string exampleJson = null;
             
             var example = exampleJson != null
-            ? JsonConvert.DeserializeObject<Dictionary<Dictionary<string, int>>>(exampleJson)
-            : new Dictionary<Dictionary<string, int>>();
+            ? JsonConvert.DeserializeObject<Dictionary<string, int>>(exampleJson)
+            : new Dictionary<string, int>();
             //TODO: Change the data returned
             return new ObjectResult(example);
         }
