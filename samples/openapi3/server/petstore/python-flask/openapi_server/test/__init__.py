f08006261a52ee4df8f2ea570c944eb96c2e5ffa
Oliver Ford <dev@ojford.com>
2022-05-20 16:59:06 +0100

    Create mock implementation for tests

--- samples/openapi3/server/petstore/python-flask/openapi_server/test/__init__.py
+++ samples/openapi3/server/petstore/python-flask/openapi_server/test/__init__.py
@@ -1,4 +1,5 @@
 import logging
+from unittest.mock import patch
 
 import connexion
 from flask_testing import TestCase
@@ -7,10 +8,14 @@ from openapi_server.encoder import JSONEncoder
 
 
 class BaseTestCase(TestCase):
-
     def create_app(self):
-        logging.getLogger('connexion.operation').setLevel('ERROR')
-        app = connexion.App(__name__, specification_dir='../openapi/')
+        logging.getLogger("connexion.operation").setLevel("ERROR")
+        app = connexion.App(__name__, specification_dir="../openapi/")
         app.app.json_encoder = JSONEncoder
-        app.add_api('openapi.yaml', pythonic_params=True)
+        app.add_api("openapi.yaml", pythonic_params=True)
         return app.app
+
+    def setUp(self):
+        impl_patcher = patch("openapi_server.impl", create=True)
+        impl_patcher.start()
+        self.addCleanup(impl_patcher)
