980d5d17aacabaa915881f08306ce1ef2dbf65c8
Stephan Pelikan <stephan.pelikan@phactum.at>
2022-05-29 20:05:43 +0200

    #12490: Fix format 'date'

--- samples/client/petstore/typescript-fetch/builds/default-v3.0/models/NullableClass.ts
+++ samples/client/petstore/typescript-fetch/builds/default-v3.0/models/NullableClass.ts
@@ -134,7 +134,7 @@ export function NullableClassToJSON(value?: NullableClass | null): any {
         'number_prop': value.numberProp,
         'boolean_prop': value.booleanProp,
         'string_prop': value.stringProp,
-        'date_prop': value.dateProp === undefined ? undefined : (value.dateProp === null ? null : value.dateProp.toISOString().substr(0,10)),
+        'date_prop': value.dateProp === undefined ? undefined : (value.dateProp === null ? null : new Date(value.dateProp.getTime() - value.dateProp.getTimezoneOffset()*60000).toISOString().substr(0,10)),
         'datetime_prop': value.datetimeProp === undefined ? undefined : (value.datetimeProp === null ? null : value.datetimeProp.toISOString()),
         'array_nullable_prop': value.arrayNullableProp,
         'array_and_items_nullable_prop': value.arrayAndItemsNullableProp,
