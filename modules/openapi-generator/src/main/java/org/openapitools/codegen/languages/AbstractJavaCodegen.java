cdc2d926090d1c4bdd21c7fbf648fed540e49cfd
William Cheng <wing328hk@gmail.com>
2022-06-12 22:36:10 +0800

    [Java] fix bugs when property name, datatype are the same (#12581)
    * fix bugs when property name, datatype are the same
    
    * add null check
    
    * add Locale.ROOT

--- modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/AbstractJavaCodegen.java
+++ modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/AbstractJavaCodegen.java
@@ -1330,6 +1330,11 @@ public abstract class AbstractJavaCodegen extends DefaultCodegen implements Code
         if (property.isReadOnly) {
             model.getVendorExtensions().put("x-has-readonly-properties", true);
         }
+
+        // if data type happens to be the same as the property name and both are upper case
+        if (property.dataType != null && property.dataType.equals(property.name) && property.dataType.toUpperCase(Locale.ROOT).equals(property.name)) {
+            property.name = property.name.toLowerCase(Locale.ROOT);
+        }
     }
 
     @Override
