c370ac9210157b2e6fe188d5f907ad5fd8f8ea3b
William Cheng <wing328hk@gmail.com>
2022-06-12 21:22:37 +0800

    add Locale.ROOT

--- modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/AbstractJavaCodegen.java
+++ modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/AbstractJavaCodegen.java
@@ -1332,8 +1332,8 @@ public abstract class AbstractJavaCodegen extends DefaultCodegen implements Code
         }
 
         // if data type happens to be the same as the property name and both are upper case
-        if (property.dataType != null && property.dataType.equals(property.name) && property.dataType.toUpperCase().equals(property.name)) {
-            property.name = property.name.toLowerCase();
+        if (property.dataType != null && property.dataType.equals(property.name) && property.dataType.toUpperCase(Locale.ROOT).equals(property.name)) {
+            property.name = property.name.toLowerCase(Locale.ROOT);
         }
     }
 
