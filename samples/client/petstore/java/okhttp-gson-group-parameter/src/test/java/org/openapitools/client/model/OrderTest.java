849f71f1f1a28659b0fd375986d7e66e9c6d04ac
William Cheng <wing328hk@gmail.com>
2022-06-12 13:12:53 +0800

    trigger build failure

--- samples/client/petstore/java/okhttp-gson-group-parameter/src/test/java/org/openapitools/client/model/OrderTest.java
+++ samples/client/petstore/java/okhttp-gson-group-parameter/src/test/java/org/openapitools/client/model/OrderTest.java
@@ -1,3 +1,5 @@
+sdklfadskl;fjakls;
+klafjkl;sdjfl;adks
 /*
  * OpenAPI Petstore
  * This is a sample server Petstore server. For this sample, you can use the api key `special-key` to test the authorization filters.
