a00ae4631d5d4a2b86dcc4886d72ce4f565168f7
Justin Black <spacether@users.noreply.github.com>
2022-06-13 07:45:15 -0700

    Fixes double serialization bug in python-experimental (#12561)
    * Partial template update, simplifies ref6570 serialization
    
    * Updated serialization
    
    * Typo fixes
    
    * Fixes test_query_style_form_serialization
    
    * Fixes test_cookie_style_form_serialization
    
    * FIxes test_style_simple_in_path_serialization
    
    * Fixes test_style_simple_in_header_serialization
    
    * Fixes many parameter tests
    
    * Deletes duplicate method
    
    * Fixes TestParameter.test_path_params_no_style
    
    * Fixes test_style_pipe_delimited_serialization
    
    * Fixes test_header_params_no_style
    
    * Fixes test_style_space_delimited_serialization
    
    * Fixes test_query_style_form_serialization
    
    * Fixes test_cookie_style_form_serialization
    
    * Fixes test_query_params_no_style
    
    * Fixes test_cookie_params_no_style
    
    * Fixes test_delete_endpoint_without_request_body
    
    * Fixes all tests
    
    * Reverts version file
    
    * Adds test_get_user_by_name to test path param url encoding
    
    * Adds test_body_with_query_params
    
    * Samples regenerated

--- samples/openapi3/client/petstore/python-experimental/petstore_api/api/fake_api_endpoints/inline_composition.py
+++ samples/openapi3/client/petstore/python-experimental/petstore_api/api/fake_api_endpoints/inline_composition.py
@@ -524,8 +524,9 @@ class InlineComposition(api_client.Api):
             class instances
         """
         self._verify_typed_dict_inputs(RequestQueryParams, query_params)
+        used_path = _path
 
-        _query_params = []
+        prefix_separator_iterator = None
         for parameter in (
             request_query_composition_at_root,
             request_query_composition_in_property,
@@ -533,8 +534,11 @@ class InlineComposition(api_client.Api):
             parameter_data = query_params.get(parameter.name, unset)
             if parameter_data is unset:
                 continue
-            serialized_data = parameter.serialize(parameter_data)
-            _query_params.extend(serialized_data)
+            if prefix_separator_iterator is None:
+                prefix_separator_iterator = parameter.get_prefix_separator_iterator()
+            serialized_data = parameter.serialize(parameter_data, prefix_separator_iterator)
+            for serialized_value in serialized_data.values():
+                used_path += serialized_value
 
         _headers = HTTPHeaderDict()
         # TODO add cookie handling
@@ -552,9 +556,8 @@ class InlineComposition(api_client.Api):
             elif 'body' in serialized_data:
                 _body = serialized_data['body']
         response = self.api_client.call_api(
-            resource_path=_path,
+            resource_path=used_path,
             method=_method,
-            query_params=tuple(_query_params),
             headers=_headers,
             fields=_fields,
             body=_body,
--- samples/openapi3/client/petstore/python-experimental/petstore_api/api/user_api_endpoints/update_user.py
+++ samples/openapi3/client/petstore/python-experimental/petstore_api/api/user_api_endpoints/update_user.py
@@ -155,6 +155,7 @@ class UpdateUser(api_client.Api):
             class instances
         """
         self._verify_typed_dict_inputs(RequestPathParams, path_params)
+        used_path = _path
 
         _path_params = {}
         for parameter in (
@@ -165,6 +166,9 @@ class UpdateUser(api_client.Api):
                 continue
             serialized_data = parameter.serialize(parameter_data)
             _path_params.update(serialized_data)
+
+        for k, v in _path_params.items():
+            used_path = used_path.replace('{%s}' % k, v)
 
         _headers = HTTPHeaderDict()
         # TODO add cookie handling
@@ -181,9 +185,8 @@ class UpdateUser(api_client.Api):
         elif 'body' in serialized_data:
             _body = serialized_data['body']
         response = self.api_client.call_api(
-            resource_path=_path,
+            resource_path=used_path,
             method=_method,
-            path_params=_path_params,
             headers=_headers,
             fields=_fields,
             body=_body,
