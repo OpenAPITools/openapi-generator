4eed12f850cbc008404b0dc2284245f3d3ce4a15
William Cheng <wing328hk@gmail.com>
2022-07-05 16:58:37 +0800

    trigger build failure

--- samples/server/petstore/jaxrs-cxf/src/gen/java/org/openapitools/model/OuterComposite.java
+++ samples/server/petstore/jaxrs-cxf/src/gen/java/org/openapitools/model/OuterComposite.java
@@ -1,8 +1,5 @@
 package org.openapitools.model;
 
-import java.math.BigDecimal;
-import javax.validation.constraints.*;
-import javax.validation.Valid;
 
 import io.swagger.annotations.ApiModelProperty;
 import java.util.Objects;
