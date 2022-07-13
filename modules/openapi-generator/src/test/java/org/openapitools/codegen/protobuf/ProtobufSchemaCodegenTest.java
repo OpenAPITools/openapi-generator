7c17bdadc53e56e67da36aee54b2701c941215cf
Loris Leong <loris.leong@amadeus.com>
2022-07-13 16:48:50 +0200

    handle x-ama-enum-extension

--- modules/openapi-generator/src/test/java/org/openapitools/codegen/protobuf/ProtobufSchemaCodegenTest.java
+++ modules/openapi-generator/src/test/java/org/openapitools/codegen/protobuf/ProtobufSchemaCodegenTest.java
@@ -353,6 +353,48 @@ public class ProtobufSchemaCodegenTest {
     }
 
     @Test
+    public void testExtensionAmaEnum() throws IOException {
+        Map<String, Object> properties = new HashMap<>();
+        Map<String, String> globalProperties = new HashMap<>();
+
+        File output = Files.createTempDirectory("test").toFile();
+        List<File> files = generate(output, properties, globalProperties, "src/test/resources/3_0/protobuf-schema/extension-ama-enum.yaml");
+        TestUtils.ensureContainsFile(files, output, "models/pet.proto");
+        Path path = Paths.get(output + "/models/pet.proto");
+        assertFileEquals(path, Paths.get("src/test/resources/3_0/protobuf-schema/extension-ama-enum.proto"));
+
+        output.delete();
+    }
+
+    @Test
+    public void testExtensionAmaEnumNonMatchingItem() throws IOException {
+        Map<String, Object> properties = new HashMap<>();
+        Map<String, String> globalProperties = new HashMap<>();
+
+        File output = Files.createTempDirectory("test").toFile();
+        List<File> files = generate(output, properties, globalProperties, "src/test/resources/3_0/protobuf-schema/extension-ama-enum-non-matching-item.yaml");
+        TestUtils.ensureContainsFile(files, output, "models/pet.proto");
+        Path path = Paths.get(output + "/models/pet.proto");
+        assertFileEquals(path, Paths.get("src/test/resources/3_0/protobuf-schema/extension-ama-enum-non-matching-item.proto"));
+
+        output.delete();
+    }
+
+    @Test
+    public void testExtensionAmaEnumDuplicateValues() throws IOException {
+        try {
+            Map<String, Object> properties = new HashMap<>();
+            Map<String, String> globalProperties = new HashMap<>();
+            File output = Files.createTempDirectory("test").toFile();
+            List<File> files = generate(output, properties, globalProperties, "src/test/resources/3_0/protobuf-schema/extension-ama-enum-duplicate-values.yaml");
+            fail("No exception thrown!");
+        }
+        catch (RuntimeException e) {
+            assertEquals(e.getCause().getMessage(), "Duplicate value in x-ama-enum.values");
+        }        
+    }
+
+    @Test
     public void testPropertyAnyType1() throws IOException {
         Map<String, Object> properties = new HashMap<>();
         Map<String, String> globalProperties = new HashMap<>();
