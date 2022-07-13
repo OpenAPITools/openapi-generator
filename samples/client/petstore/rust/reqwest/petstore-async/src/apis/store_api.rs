5aa0e0a45632d88ab378ca02401e2e94aa9135f0
William Cheng <wing328hk@gmail.com>
2022-07-13 17:32:31 +0800

    update samples

--- samples/client/petstore/rust/reqwest/petstore-async/src/apis/store_api.rs
+++ samples/client/petstore/rust/reqwest/petstore-async/src/apis/store_api.rs
@@ -18,14 +18,14 @@ use super::{Error, configuration};
 #[derive(Clone, Debug, Default)]
 pub struct DeleteOrderParams {
     /// ID of the order that needs to be deleted
-    pub orderid: String
+    pub order_id: String
 }
 
 /// struct for passing parameters to the method [`get_order_by_id`]
 #[derive(Clone, Debug, Default)]
 pub struct GetOrderByIdParams {
     /// ID of pet that needs to be fetched
-    pub orderid: i64
+    pub order_id: i64
 }
 
 /// struct for passing parameters to the method [`place_order`]
@@ -106,12 +106,12 @@ pub async fn delete_order(configuration: &configuration::Configuration, params: 
     let local_var_configuration = configuration;
 
     // unbox the parameters
-    let orderid = params.orderid;
+    let order_id = params.order_id;
 
 
     let local_var_client = &local_var_configuration.client;
 
-    let local_var_uri_str = format!("{}/store/order/{orderId}", local_var_configuration.base_path, orderId=crate::apis::urlencode(orderid));
+    let local_var_uri_str = format!("{}/store/order/{orderId}", local_var_configuration.base_path, orderId=crate::apis::urlencode(order_id));
     let mut local_var_req_builder = local_var_client.request(reqwest::Method::DELETE, local_var_uri_str.as_str());
 
     if let Some(ref local_var_user_agent) = local_var_configuration.user_agent {
@@ -181,12 +181,12 @@ pub async fn get_order_by_id(configuration: &configuration::Configuration, param
     let local_var_configuration = configuration;
 
     // unbox the parameters
-    let orderid = params.orderid;
+    let order_id = params.order_id;
 
 
     let local_var_client = &local_var_configuration.client;
 
-    let local_var_uri_str = format!("{}/store/order/{orderId}", local_var_configuration.base_path, orderId=orderid);
+    let local_var_uri_str = format!("{}/store/order/{orderId}", local_var_configuration.base_path, orderId=order_id);
     let mut local_var_req_builder = local_var_client.request(reqwest::Method::GET, local_var_uri_str.as_str());
 
     if let Some(ref local_var_user_agent) = local_var_configuration.user_agent {
