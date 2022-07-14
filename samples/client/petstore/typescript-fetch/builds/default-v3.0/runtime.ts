e5370b2598eb287828d161d730e8562dca8965a5
Thomas Hansen <thomas@thomasphansen.dk>
2022-07-14 15:49:21 +0200

    [Typescript] Update samples

--- samples/client/petstore/typescript-fetch/builds/default-v3.0/runtime.ts
+++ samples/client/petstore/typescript-fetch/builds/default-v3.0/runtime.ts
@@ -13,7 +13,7 @@
  */
 
 
-export const BASE_PATH = "http://petstore.swagger.io:80/v2".replace(/\/+$/, "");
+export const BASE_PATH = "https://petstore.swagger.io:443/v2".replace(/\/+$/, "");
 
 export interface ConfigurationParameters {
     basePath?: string; // override base path
