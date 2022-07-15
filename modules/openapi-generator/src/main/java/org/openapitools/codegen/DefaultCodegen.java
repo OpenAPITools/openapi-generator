cf8fa5169f2037da8bf527812407545a293e62cb
Loris Leong <loris.leong@amadeus.com>
2022-07-15 14:07:15 +0200

    fix handling of ama-enum extension

--- modules/openapi-generator/src/main/java/org/openapitools/codegen/DefaultCodegen.java
+++ modules/openapi-generator/src/main/java/org/openapitools/codegen/DefaultCodegen.java
@@ -6179,7 +6179,7 @@ public class DefaultCodegen implements CodegenConfig {
                     for(Map<String, Object> amaEnumValue : amaEnumValues) {
                         //enum values have additional "\"" at start and end
                         if (amaEnumValue.containsKey("value") && 
-                        amaEnumValue.get("value").equals(value.replace("\"", ""))) {
+                        toEnumValue((String) amaEnumValue.get("value"), dataType).equals(value)) {
                             //update enum
                             if (amaEnumValue.containsKey(extensionKey)) {
                                 String extension = (String) amaEnumValue.get(extensionKey);
