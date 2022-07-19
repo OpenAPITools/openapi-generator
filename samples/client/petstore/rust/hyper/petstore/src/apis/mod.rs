b88666b87acab1d59f5a1502c2445bf390ae8dfe
William Cheng <wing328hk@gmail.com>
2022-07-19 11:22:29 +0800

    add uuid test, better hyper test (#12916)

--- samples/client/petstore/rust/hyper/petstore/src/apis/mod.rs
+++ samples/client/petstore/rust/hyper/petstore/src/apis/mod.rs
@@ -47,6 +47,8 @@ impl From<serde_json::Error> for Error {
 
 mod request;
 
+mod fake_api;
+pub use self::fake_api::{ FakeApi, FakeApiClient };
 mod pet_api;
 pub use self::pet_api::{ PetApi, PetApiClient };
 mod store_api;
