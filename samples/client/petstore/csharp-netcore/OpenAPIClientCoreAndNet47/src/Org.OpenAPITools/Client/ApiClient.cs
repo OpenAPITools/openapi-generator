1bd04683893b71456140e02c48a4e6db91796643
Emanuele Saccomandi <emanuele.saccomandi@gmail.com>
2022-07-19 15:49:06 +0200

    fix: errors were not getting thrown

--- samples/client/petstore/csharp-netcore/OpenAPIClientCoreAndNet47/src/Org.OpenAPITools/Client/ApiClient.cs
+++ samples/client/petstore/csharp-netcore/OpenAPIClientCoreAndNet47/src/Org.OpenAPITools/Client/ApiClient.cs
@@ -515,7 +515,10 @@ namespace Org.OpenAPITools.Client
             {
                 response.Data = (T)(object)response.RawBytes;
             }
-            else response.Data = (T)(object)response.Content;
+            else if (typeof(T).Name == "String") // for string response
+            {
+                response.Data = (T)(object)response.Content;
+            }
 
             InterceptResponse(req, response);
 
@@ -633,8 +636,10 @@ namespace Org.OpenAPITools.Client
             {
                 response.Data = (T)(object)response.RawBytes;
             }
-            else response.Data = (T)(object)response.Content;
-            
+            else if (typeof(T).Name == "String") // for string response
+            {
+                response.Data = (T)(object)response.Content;
+            }
             InterceptResponse(req, response);
 
             var result = ToApiResponse(response);
