0d0c68c87a3949bf759c1d8d7c5a779385d12901
ry99 <ryan@ostrich-emulators.com>
2022-07-21 07:28:20 -0400

    updated samples

--- samples/client/petstore/typescript-angular-v11-oneOf/builds/default/api/default.service.ts
+++ samples/client/petstore/typescript-angular-v11-oneOf/builds/default/api/default.service.ts
@@ -37,11 +37,15 @@ export class DefaultService {
     public configuration = new Configuration();
     public encoder: HttpParameterCodec;
 
-    constructor(protected httpClient: HttpClient, @Optional()@Inject(BASE_PATH) basePath: string, @Optional() configuration: Configuration) {
+    constructor(protected httpClient: HttpClient, @Optional()@Inject(BASE_PATH) basePath: string|string[], @Optional() configuration: Configuration) {
         if (configuration) {
             this.configuration = configuration;
         }
         if (typeof this.configuration.basePath !== 'string') {
+            if (Array.isArray(basePath) && basePath.length > 0) {
+                basePath = basePath[0];
+            }
+
             if (typeof basePath !== 'string') {
                 basePath = this.basePath;
             }
