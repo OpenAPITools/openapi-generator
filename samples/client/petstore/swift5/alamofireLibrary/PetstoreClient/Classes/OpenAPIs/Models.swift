fecd82b3c21ed175a72c78f2c9cb092ac3689bc7
Bruno Coelho <4brunu@gmail.com>
2022-07-22 11:47:38 +0100

    [swift][client] reduce unnecessary public api

--- samples/client/petstore/swift5/alamofireLibrary/PetstoreClient/Classes/OpenAPIs/Models.swift
+++ samples/client/petstore/swift5/alamofireLibrary/PetstoreClient/Classes/OpenAPIs/Models.swift
@@ -19,7 +19,7 @@ extension CaseIterableDefaultsLast {
     /// Initializes an enum such that if a known raw value is found, then it is decoded.
     /// Otherwise the last case is used.
     /// - Parameter decoder: A decoder.
-    init(from decoder: Decoder) throws {
+    public init(from decoder: Decoder) throws {
         if let value = try Self(rawValue: decoder.singleValueContainer().decode(RawValue.self)) {
             self = value
         } else if let lastValue = Self.allCases.last {
