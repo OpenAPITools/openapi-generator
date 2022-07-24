2b9a1ad0fc47296053590c29e5620c3e260fb353
Justin Black <spacether@users.noreply.github.com>
2022-07-24 14:02:34 -0700

    [python-experimental] Consolidates model tests into a subfolder (#13006)
    * Consolidates model tests into a subfolder
    
    * Regenerates samples

--- samples/openapi3/client/petstore/python-experimental/test/test_user_api.py
+++ samples/openapi3/client/petstore/python-experimental/test/test_user_api.py
@@ -10,16 +10,24 @@
 """
 
 import unittest
+from unittest.mock import patch
+
+import urllib3
 
 import petstore_api
 from petstore_api.api.user_api import UserApi  # noqa: E501
+from petstore_api import configuration, schemas, api_client
+
+from . import ApiTestMixin
 
 
-class TestUserApi(unittest.TestCase):
+class TestUserApi(ApiTestMixin, unittest.TestCase):
     """UserApi unit test stubs"""
+    _configuration = configuration.Configuration()
 
     def setUp(self):
-        self.api = UserApi()  # noqa: E501
+        used_api_client = api_client.ApiClient(configuration=self._configuration)
+        self.api = UserApi(api_client=used_api_client)  # noqa: E501
 
     def tearDown(self):
         pass
@@ -29,27 +37,45 @@ class TestUserApi(unittest.TestCase):
 
         Create user  # noqa: E501
         """
-        pass
+        from petstore_api.api.user_api_endpoints import create_user as endpoint_module
+        response_status = 0
+        response_body = ''
+        content_type = 'application/json'
+
+
 
     def test_create_users_with_array_input(self):
         """Test case for create_users_with_array_input
 
         Creates list of users with given input array  # noqa: E501
         """
-        pass
+        from petstore_api.api.user_api_endpoints import create_users_with_array_input as endpoint_module
+        response_status = 0
+        response_body = ''
+        content_type = 'application/json'
+
+
 
     def test_create_users_with_list_input(self):
         """Test case for create_users_with_list_input
 
         Creates list of users with given input array  # noqa: E501
         """
-        pass
+        from petstore_api.api.user_api_endpoints import create_users_with_list_input as endpoint_module
+        response_status = 0
+        response_body = ''
+        content_type = 'application/json'
+
+
 
     def test_delete_user(self):
         """Test case for delete_user
 
         Delete user  # noqa: E501
         """
+        from petstore_api.api.user_api_endpoints import delete_user as endpoint_module
+        response_status = 400
+        response_body = ''
         pass
 
     def test_get_user_by_name(self):
@@ -57,6 +83,16 @@ class TestUserApi(unittest.TestCase):
 
         Get user by user name  # noqa: E501
         """
+        from petstore_api.api.user_api_endpoints import get_user_by_name as endpoint_module
+        response_status = 200
+        accept_content_type = 'application/xml'
+
+
+
+        accept_content_type = 'application/json'
+
+
+
         pass
 
     def test_login_user(self):
@@ -64,6 +100,16 @@ class TestUserApi(unittest.TestCase):
 
         Logs user into the system  # noqa: E501
         """
+        from petstore_api.api.user_api_endpoints import login_user as endpoint_module
+        response_status = 200
+        accept_content_type = 'application/xml'
+
+
+
+        accept_content_type = 'application/json'
+
+
+
         pass
 
     def test_logout_user(self):
@@ -71,6 +117,9 @@ class TestUserApi(unittest.TestCase):
 
         Logs out current logged in user session  # noqa: E501
         """
+        from petstore_api.api.user_api_endpoints import logout_user as endpoint_module
+        response_status = 0
+        response_body = ''
         pass
 
     def test_update_user(self):
@@ -78,7 +127,12 @@ class TestUserApi(unittest.TestCase):
 
         Updated user  # noqa: E501
         """
-        pass
+        from petstore_api.api.user_api_endpoints import update_user as endpoint_module
+        response_status = 400
+        response_body = ''
+        content_type = 'application/json'
+
+
 
 
 if __name__ == '__main__':
