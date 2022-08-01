c2cbdd018bf29468ca55b235cbf91b785d9d3c05
Justin Black <justin.a.black@gmail.com>
2022-08-01 14:57:13 -0700

    Samples regenerated

--- samples/openapi3/client/petstore/python-experimental/petstore_api/apis/tag_to_api.py
+++ samples/openapi3/client/petstore/python-experimental/petstore_api/apis/tag_to_api.py
@@ -4,6 +4,10 @@ from petstore_api.apis.tags import TagValues
 from petstore_api.apis.tags.pet_api import PetApi
 from petstore_api.apis.tags.store_api import StoreApi
 from petstore_api.apis.tags.user_api import UserApi
+from petstore_api.apis.tags.another_fake_api import AnotherFakeApi
+from petstore_api.apis.tags.default_api import DefaultApi
+from petstore_api.apis.tags.fake_api import FakeApi
+from petstore_api.apis.tags.fake_classname_tags123_api import FakeClassnameTags123Api
 
 TagToApi = typing.TypedDict(
     'TagToApi',
@@ -11,6 +15,10 @@ TagToApi = typing.TypedDict(
         TagValues.PET: PetApi,
         TagValues.STORE: StoreApi,
         TagValues.USER: UserApi,
+        TagValues.ANOTHERFAKE: AnotherFakeApi,
+        TagValues.DEFAULT: DefaultApi,
+        TagValues.FAKE: FakeApi,
+        TagValues.FAKE_CLASSNAME_TAGS_123: FakeClassnameTags123Api,
     }
 )
 
@@ -19,5 +27,9 @@ tag_to_api = TagToApi(
         TagValues.PET: PetApi,
         TagValues.STORE: StoreApi,
         TagValues.USER: UserApi,
+        TagValues.ANOTHERFAKE: AnotherFakeApi,
+        TagValues.DEFAULT: DefaultApi,
+        TagValues.FAKE: FakeApi,
+        TagValues.FAKE_CLASSNAME_TAGS_123: FakeClassnameTags123Api,
     }
 )
