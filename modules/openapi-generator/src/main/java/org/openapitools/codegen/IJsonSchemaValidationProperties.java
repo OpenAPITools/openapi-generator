8d82fed777d5efb386d979da1c7ab64a2beae347
Justin Black <justin.a.black@gmail.com>
2022-08-05 11:57:05 -0700

    Fixes imports

--- modules/openapi-generator/src/main/java/org/openapitools/codegen/IJsonSchemaValidationProperties.java
+++ modules/openapi-generator/src/main/java/org/openapitools/codegen/IJsonSchemaValidationProperties.java
@@ -1,6 +1,10 @@
 package org.openapitools.codegen;
 
-import java.util.*;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.stream.Stream;
 
 import io.swagger.v3.oas.models.media.Schema;
