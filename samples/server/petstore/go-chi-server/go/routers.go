7c89c4f4bbe65bd615c12874ba4144921117e96b
Dwyer <dodwyer@ptc.com>
2022-08-08 12:58:20 -0400

    Fix file on server

--- samples/server/petstore/go-chi-server/go/routers.go
+++ samples/server/petstore/go-chi-server/go/routers.go
@@ -76,22 +76,22 @@ func EncodeJSONResponse(i interface{}, status *int, headers map[string][]string,
 }
 
 // ReadFormFileToTempFile reads file data from a request form and writes it to a temporary file
-func ReadFormFileToTempFile(r *http.Request, key string) (*os.File, error) {
+func ReadFormFileToTempFile(r *http.Request, key string) (os.File, error) {
 	_, fileHeader, err := r.FormFile(key)
 	if err != nil {
-		return nil, err
+		return os.File{}, err
 	}
 
 	return readFileHeaderToTempFile(fileHeader)
 }
 
 // ReadFormFilesToTempFiles reads files array data from a request form and writes it to a temporary files
-func ReadFormFilesToTempFiles(r *http.Request, key string) ([]*os.File, error) {
+func ReadFormFilesToTempFiles(r *http.Request, key string) ([]os.File, error) {
 	if err := r.ParseMultipartForm(32 << 20); err != nil {
 		return nil, err
 	}
 
-	files := make([]*os.File, 0, len(r.MultipartForm.File[key]))
+	files := make([]os.File, 0, len(r.MultipartForm.File[key]))
 
 	for _, fileHeader := range r.MultipartForm.File[key] {
 		file, err := readFileHeaderToTempFile(fileHeader)
@@ -106,29 +106,29 @@ func ReadFormFilesToTempFiles(r *http.Request, key string) ([]*os.File, error) {
 }
 
 // readFileHeaderToTempFile reads multipart.FileHeader and writes it to a temporary file
-func readFileHeaderToTempFile(fileHeader *multipart.FileHeader) (*os.File, error) {
+func readFileHeaderToTempFile(fileHeader *multipart.FileHeader) (os.File, error) {
 	formFile, err := fileHeader.Open()
 	if err != nil {
-		return nil, err
+		return os.File{}, err
 	}
 
 	defer formFile.Close()
 
 	fileBytes, err := ioutil.ReadAll(formFile)
 	if err != nil {
-		return nil, err
+		return os.File{}, err
 	}
 
 	file, err := ioutil.TempFile("", fileHeader.Filename)
 	if err != nil {
-		return nil, err
+		return os.File{}, err
 	}
 
 	defer file.Close()
 
 	file.Write(fileBytes)
 
-	return file, nil
+	return *file, nil
 }
 
 // parseInt64Parameter parses a string parameter to an int64.
