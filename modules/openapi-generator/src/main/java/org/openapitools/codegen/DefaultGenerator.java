9a041132587f915e9b25a555cd539cfa78469492
Laurens-W <laurens.westerlaken@mapcreator.eu>
2022-08-09 01:04:13 +0200

    Allow useInlineModelResolver to be influenced by generator options (#13056)
    * Move logic so that it can dynamically be influenced by opts
    
    * Or should it be here?
    
    * We handle the useInlineModelResolver logic in configureGeneratorProperties after processOpts
    
    Co-authored-by: Westerlaken, H.L. (Laurens) <laurens.westerlaken@devolksbank.nl>

--- modules/openapi-generator/src/main/java/org/openapitools/codegen/DefaultGenerator.java
+++ modules/openapi-generator/src/main/java/org/openapitools/codegen/DefaultGenerator.java
@@ -254,6 +254,15 @@ public class DefaultGenerator implements Generator {
         }
 
         config.processOpts();
+
+        // resolve inline models
+        if (config.getUseInlineModelResolver()) {
+            InlineModelResolver inlineModelResolver = new InlineModelResolver();
+            inlineModelResolver.setInlineSchemaNameMapping(config.inlineSchemaNameMapping());
+            inlineModelResolver.setInlineSchemaNameDefaults(config.inlineSchemaNameDefault());
+            inlineModelResolver.flatten(openAPI);
+        }
+
         config.preprocessOpenAPI(openAPI);
 
         // set OpenAPI to make these available to all methods
@@ -887,14 +896,6 @@ public class DefaultGenerator implements Generator {
             } else {
                 LOGGER.info(stabilityMessage);
             }
-        }
-
-        // resolve inline models
-        if (config.getUseInlineModelResolver()) {
-            InlineModelResolver inlineModelResolver = new InlineModelResolver();
-            inlineModelResolver.setInlineSchemaNameMapping(config.inlineSchemaNameMapping());
-            inlineModelResolver.setInlineSchemaNameDefaults(config.inlineSchemaNameDefault());
-            inlineModelResolver.flatten(openAPI);
         }
 
         configureGeneratorProperties();
