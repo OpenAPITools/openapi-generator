fa2b9e4b4ed1594a5b0616cb4e89e61008bd1539
Nikos <n.epping@evosec.de>
2022-08-17 10:29:19 +0200

    [typescript-axios] Add // @ts-ignore to imports in models if withSeparateModelsAndApi is set

--- samples/client/others/typescript-axios/with-separate-models-and-api-inheritance/model/abstract-user-dto.ts
+++ samples/client/others/typescript-axios/with-separate-models-and-api-inheritance/model/abstract-user-dto.ts
@@ -13,6 +13,8 @@
  */
 
 
+// May contain unused imports in some cases
+// @ts-ignore
 import { BranchDto } from './branch-dto';
 
 /**
--- samples/client/others/typescript-axios/with-separate-models-and-api-inheritance/model/remote-authenticated-user-dto.ts
+++ samples/client/others/typescript-axios/with-separate-models-and-api-inheritance/model/remote-authenticated-user-dto.ts
@@ -13,7 +13,11 @@
  */
 
 
+// May contain unused imports in some cases
+// @ts-ignore
 import { AbstractUserDto } from './abstract-user-dto';
+// May contain unused imports in some cases
+// @ts-ignore
 import { BranchDto } from './branch-dto';
 
 /**
