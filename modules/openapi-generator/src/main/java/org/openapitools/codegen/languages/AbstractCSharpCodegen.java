6f103a2eaa77400e4c96eea3a4ff83dc50fc300a
devhl <shawnkanyer@gmail.com>
2022-09-20 21:15:31 -0400

    fighting line endings

--- modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/AbstractCSharpCodegen.java
+++ modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/AbstractCSharpCodegen.java
@@ -118,7 +118,7 @@ public abstract class AbstractCSharpCodegen extends DefaultCodegen implements Co
         );
 
         mapTypes = new HashSet<>(
-                Arrays.asList("IDictionary") //, "Dictionary")
+                Arrays.asList("IDictionary", "Dictionary")
         );
 
         // NOTE: C# uses camel cased reserved words, while models are title cased. We don't want lowercase comparisons.
@@ -722,7 +722,7 @@ public abstract class AbstractCSharpCodegen extends DefaultCodegen implements Co
                             }
                         } else {
                             operation.returnContainer = operation.returnType;
-                            // operation.isMap = this.mapTypes.stream().anyMatch(t -> typeMapping.startsWith(t));
+                            operation.isMap = this.mapTypes.stream().anyMatch(t -> typeMapping.startsWith(t));
                         }
                     }
 
--- samples/server/petstore/aspnetcore/src/Org.OpenAPITools/Controllers/StoreApi.cs
+++ samples/server/petstore/aspnetcore/src/Org.OpenAPITools/Controllers/StoreApi.cs
@@ -68,13 +68,9 @@ namespace Org.OpenAPITools.Controllers
             // return StatusCode(200, default(Dictionary<string, int>));
             string exampleJson = null;
             
-            // var example = exampleJson != null
-            // ? JsonConvert.DeserializeObject<Dictionary<string, int>>(exampleJson)
-            // : new Dictionary<string, int>();
-
             var example = exampleJson != null
-            ? JsonConvert.DeserializeObject<Dictionary<Dictionary<string, int>>>(exampleJson)
-            : new Dictionary<Dictionary<string, int>>();
+            ? JsonConvert.DeserializeObject<Dictionary<string, int>>(exampleJson)
+            : new Dictionary<string, int>();
 
             //TODO: Change the data returned
             return new ObjectResult(example);
