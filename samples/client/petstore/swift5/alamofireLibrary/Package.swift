6ba97321ff494ef6f7825505159f55cce251fb44
Bruno Coelho <4brunu@users.noreply.github.com>
2022-10-12 09:07:32 +0100

    [swift5] update new minimum OS supported versions (#13667)
    * [swift5] update new minimum supported versions
    
    * [swift5] update new minimum supported versions
    
    * [swift5] update new minimum supported versions
    
    * [swift5] update new minimum supported versions

--- samples/client/petstore/swift5/alamofireLibrary/Package.swift
+++ samples/client/petstore/swift5/alamofireLibrary/Package.swift
@@ -5,10 +5,10 @@ import PackageDescription
 let package = Package(
     name: "PetstoreClient",
     platforms: [
-        .iOS(.v10),
-        .macOS(.v10_12),
-        .tvOS(.v10),
-        .watchOS(.v3),
+        .iOS(.v11),
+        .macOS(.v10_13),
+        .tvOS(.v11),
+        .watchOS(.v4),
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
