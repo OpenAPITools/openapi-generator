dcf5ec3c6a87a734d032f55a8f3b818140faf6e8
William Cheng <wing328hk@gmail.com>
2022-11-07 22:17:38 +0800

    go client regenerate test

--- samples/openapi3/client/petstore/go/go-petstore/test/api_fake_test.go
+++ samples/openapi3/client/petstore/go/go-petstore/test/api_fake_test.go
@@ -14,7 +14,7 @@ import (
     "github.com/stretchr/testify/assert"
     "github.com/stretchr/testify/require"
     "testing"
-    openapiclient "./openapi"
+    openapiclient "github.com/GIT_USER_ID/GIT_REPO_ID"
 )
 
 func Test_petstore_FakeApiService(t *testing.T) {
