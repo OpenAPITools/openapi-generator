b5b6d5a41d38b2e1ead46ab47a1e130a7502815f
Beppe Catanese <gcatanese@yahoo.com>
2022-12-04 22:48:38 +0100

    Add examples map and overload constructor

--- modules/openapi-generator/src/main/java/org/openapitools/codegen/CodegenMediaType.java
+++ modules/openapi-generator/src/main/java/org/openapitools/codegen/CodegenMediaType.java
@@ -1,13 +1,17 @@
 package org.openapitools.codegen;
 
+import io.swagger.v3.oas.models.examples.Example;
+
 import java.util.HashMap;
 import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.Objects;
 
 public class CodegenMediaType {
     private CodegenProperty schema;
     private LinkedHashMap<String, CodegenEncoding> encoding;
     private HashMap<String, SchemaTestCase> testCases = new HashMap<>();
+    private Map<String, Example> examples = null;
 
     public CodegenMediaType(CodegenProperty schema, LinkedHashMap<String, CodegenEncoding> encoding, HashMap<String, SchemaTestCase> testCases) {
         this.schema = schema;
@@ -17,6 +21,10 @@ public class CodegenMediaType {
         }
     }
 
+    public CodegenMediaType(CodegenProperty schema, LinkedHashMap<String, CodegenEncoding> encoding, HashMap<String, SchemaTestCase> testCases, Map<String, Example> examples) {
+        this(schema, encoding, testCases);
+        this.examples = examples;
+    }
     public CodegenProperty getSchema() {
         return schema;
     }
@@ -26,6 +34,10 @@ public class CodegenMediaType {
     }
 
     public HashMap<String, SchemaTestCase> getTestCases() { return testCases; }
+
+    public Map<String, Example> getExamples() {
+        return examples;
+    }
 
     public String toString() {
         final StringBuilder sb = new StringBuilder("CodegenMediaType{");
