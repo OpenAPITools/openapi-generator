921199bba7fd71b1a5acc60d69e8f2113dd60cb6
Julien Herr <julien@herr.fr>
2023-01-02 16:32:57 +0100

    [kotlin-spring] Move Jackson annotation from field to getter (#10825)
    * Move Jackson annotation from field to getter
    
    In some case, Jackson will duplicate entries when the annotation is on field
    See https://github.com/FasterXML/jackson-databind/issues/1609
    
    * Update samples

--- samples/server/petstore/kotlin-springboot-modelMutable/src/main/kotlin/org/openapitools/model/Category.kt
+++ samples/server/petstore/kotlin-springboot-modelMutable/src/main/kotlin/org/openapitools/model/Category.kt
@@ -21,10 +21,10 @@ import io.swagger.v3.oas.annotations.media.Schema
 data class Category(
 
     @Schema(example = "null", description = "")
-    @field:JsonProperty("id") var id: kotlin.Long? = null,
+    @get:JsonProperty("id") var id: kotlin.Long? = null,
 
     @Schema(example = "null", description = "")
-    @field:JsonProperty("name") var name: kotlin.String? = null
+    @get:JsonProperty("name") var name: kotlin.String? = null
 ) {
 
 }
