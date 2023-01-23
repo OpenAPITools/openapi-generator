ba664316f4639951d5b55704f4da6d4a8fc37755
Paul Parenko <paul.parenko@gmail.com>
2023-01-23 23:26:45 +0100

    new config kotlin-spring-boot-3

---
+++ samples/server/petstore/kotlin-springboot-3/src/main/kotlin/org/openapitools/model/Pet.kt
@@ -0,0 +1,50 @@
+package org.openapitools.model
+
+import java.util.Objects
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.annotation.JsonValue
+import org.openapitools.model.Category
+import org.openapitools.model.Tag
+import jakarta.validation.constraints.*
+import jakarta.validation.Valid
+
+/**
+ * A pet for sale in the pet store
+ * @param name 
+ * @param photoUrls 
+ * @param id 
+ * @param category 
+ * @param tags 
+ * @param status pet status in the store
+ */
+data class Pet(
+
+    @get:JsonProperty("name", required = true) val name: kotlin.String,
+
+    @get:JsonProperty("photoUrls", required = true) val photoUrls: kotlin.collections.List<kotlin.String>,
+
+    @get:JsonProperty("id") val id: kotlin.Long? = null,
+
+    @field:Valid
+    @get:JsonProperty("category") val category: Category? = null,
+
+    @field:Valid
+    @get:JsonProperty("tags") val tags: kotlin.collections.List<Tag>? = null,
+
+    @Deprecated(message = "")
+    @get:JsonProperty("status") val status: Pet.Status? = null
+) {
+
+    /**
+    * pet status in the store
+    * Values: available,pending,sold
+    */
+    enum class Status(val value: kotlin.String) {
+
+        @JsonProperty("available") available("available"),
+        @JsonProperty("pending") pending("pending"),
+        @JsonProperty("sold") sold("sold")
+    }
+
+}
+
