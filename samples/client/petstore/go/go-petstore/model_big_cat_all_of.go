447948306cef72d55204f67982e20585eb818821
Beppe Catanese <gcatanese@yahoo.com>
2023-02-05 17:59:34 +0100

    Regenerate files

--- samples/client/petstore/go/go-petstore/model_big_cat_all_of.go
+++ samples/client/petstore/go/go-petstore/model_big_cat_all_of.go
@@ -41,7 +41,7 @@ func NewBigCatAllOfWithDefaults() *BigCatAllOf {
 
 // GetKind returns the Kind field value if set, zero value otherwise.
 func (o *BigCatAllOf) GetKind() string {
-	if o == nil || isNil(o.Kind) {
+	if o == nil || IsNil(o.Kind) {
 		var ret string
 		return ret
 	}
@@ -51,7 +51,7 @@ func (o *BigCatAllOf) GetKind() string {
 // GetKindOk returns a tuple with the Kind field value if set, nil otherwise
 // and a boolean to check if the value has been set.
 func (o *BigCatAllOf) GetKindOk() (*string, bool) {
-	if o == nil || isNil(o.Kind) {
+	if o == nil || IsNil(o.Kind) {
 		return nil, false
 	}
 	return o.Kind, true
@@ -59,7 +59,7 @@ func (o *BigCatAllOf) GetKindOk() (*string, bool) {
 
 // HasKind returns a boolean if a field has been set.
 func (o *BigCatAllOf) HasKind() bool {
-	if o != nil && !isNil(o.Kind) {
+	if o != nil && !IsNil(o.Kind) {
 		return true
 	}
 
@@ -81,7 +81,7 @@ func (o BigCatAllOf) MarshalJSON() ([]byte, error) {
 
 func (o BigCatAllOf) ToMap() (map[string]interface{}, error) {
 	toSerialize := map[string]interface{}{}
-	if !isNil(o.Kind) {
+	if !IsNil(o.Kind) {
 		toSerialize["kind"] = o.Kind
 	}
 	return toSerialize, nil
