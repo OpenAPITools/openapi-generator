0dbc5391512382fbf3548125abf332c5bb9caf76
Jorge Rodríguez Martín <jorgerma@ext.inditex.com>
2023-02-17 15:03:32 +0100

    upgrade samples

--- samples/server/petstore/springboot/src/main/java/org/openapitools/model/AnimalDto.java
+++ samples/server/petstore/springboot/src/main/java/org/openapitools/model/AnimalDto.java
@@ -37,7 +37,6 @@ import javax.annotation.Generated;
   @JsonSubTypes.Type(value = DogDto.class, name = "Dog")
 })
 
-@JsonTypeName("Animal")
 @Generated(value = "org.openapitools.codegen.languages.SpringCodegen")
 public class AnimalDto {
 
