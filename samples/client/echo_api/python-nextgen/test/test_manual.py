51afc7dcfb016e44546efa13a871d3d96d908b1d
William Cheng <wing328hk@gmail.com>
2023-02-18 10:52:58 +0800

    python better datetime test

--- samples/client/echo_api/python-nextgen/test/test_manual.py
+++ samples/client/echo_api/python-nextgen/test/test_manual.py
@@ -41,14 +41,14 @@ class TestManual(unittest.TestCase):
 
     def testDateTimeQueryWithString(self):
         api_instance = openapi_client.QueryApi()
-        datetime_query = '2013-10-20T19:20:30+01:00' # datetime |  (optional)
+        datetime_query = '19:20:30 2013-10-20' # datetime |  (optional)
         date_query = '2013-10-20' # date |  (optional)
         string_query = 'string_query_example' # str |  (optional)
     
         # Test query parameter(s)
         api_response = api_instance.test_query_datetime_date_string(datetime_query=datetime_query, date_query=date_query, string_query=string_query)
         e = EchoServerResponseParser(api_response)
-        self.assertEqual(e.path, "/query/datetime/date/string?datetime_query=2013-10-20T19:20:30+01:00&date_query=2013-10-20&string_query=string_query_example")
+        self.assertEqual(e.path, "/query/datetime/date/string?datetime_query=19:20:30%202013-10-20&date_query=2013-10-20&string_query=string_query_example")
 
 class EchoServerResponseParser():
     def __init__(self, http_response):
