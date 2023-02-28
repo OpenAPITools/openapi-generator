144f8bc82f1465e08e78fcaee49a170c9b5edf37
Volker Suschke <dev@volsus.de>
2023-02-28 13:53:08 +0100

    [Kotlin-Spring] Update sample files [#14652]

--- samples/server/petstore/kotlin-springboot-reactive/src/main/kotlin/org/openapitools/api/PetApiController.kt
+++ samples/server/petstore/kotlin-springboot-reactive/src/main/kotlin/org/openapitools/api/PetApiController.kt
@@ -16,8 +16,15 @@ import org.springframework.validation.annotation.Validated
 import org.springframework.web.context.request.NativeWebRequest
 import org.springframework.beans.factory.annotation.Autowired
 
-import javax.validation.constraints.*
 import javax.validation.Valid
+import javax.validation.constraints.DecimalMax
+import javax.validation.constraints.DecimalMin
+import javax.validation.constraints.Email
+import javax.validation.constraints.Max
+import javax.validation.constraints.Min
+import javax.validation.constraints.NotNull
+import javax.validation.constraints.Pattern
+import javax.validation.constraints.Size
 
 import kotlinx.coroutines.flow.Flow
 import kotlin.collections.List
