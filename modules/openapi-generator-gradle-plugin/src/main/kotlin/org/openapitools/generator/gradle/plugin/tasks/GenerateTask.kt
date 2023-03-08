0ee81bd209f4528c41517b3cbd6096fb1f973333
Daniel Imber <dimber@caisgroup.com>
2023-03-08 18:51:46 +0000

    [Enhancement] [Gradle Plugin] Allow templates from classpath

--- modules/openapi-generator-gradle-plugin/src/main/kotlin/org/openapitools/generator/gradle/plugin/tasks/GenerateTask.kt
+++ modules/openapi-generator-gradle-plugin/src/main/kotlin/org/openapitools/generator/gradle/plugin/tasks/GenerateTask.kt
@@ -128,6 +128,13 @@ open class GenerateTask : DefaultTask() {
     val templateDir = project.objects.property<String?>()
 
     /**
+     * Resource path containing template files.
+     */
+    @Optional
+    @Input
+    val templateResourcePath = project.objects.property<String?>()
+
+    /**
      * Adds authorization headers when fetching the OpenAPI definitions remotely.
      * Pass in a URL-encoded string of name:header with a comma separating multiple values
      */
@@ -647,6 +654,13 @@ open class GenerateTask : DefaultTask() {
             }
 
             templateDir.ifNotEmpty { value ->
+                configurator.setTemplateDir(value)
+            }
+
+            templateResourcePath.ifNotEmpty { value ->
+                templateDir.ifNotEmpty {
+                    logger.warn("Both templateDir and templateResourcePath were configured. templateResourcePath overwrites templateDir.")
+                }
                 configurator.setTemplateDir(value)
             }
 
