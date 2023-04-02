f0d9b91acdb51aa51fc761460158aefc9128ab24
Robin Karlsson <snago86@gmail.com>
2023-04-02 09:22:38 +0200

    [java] Add public static version field

--- samples/openapi3/client/petstore/java/jersey2-java8-swagger1/src/main/java/org/openapitools/client/Configuration.java
+++ samples/openapi3/client/petstore/java/jersey2-java8-swagger1/src/main/java/org/openapitools/client/Configuration.java
@@ -15,6 +15,8 @@ package org.openapitools.client;
 
 @javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen")
 public class Configuration {
+    public static final String VERSION = "1.0.0";
+
     private static ApiClient defaultApiClient = new ApiClient();
 
     /**
