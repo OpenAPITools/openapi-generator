fefbfd6aa24e8b3d67b3ef5f6e9eb3a2e0f42e49
Noor Dawod <noor@fine47.com>
2023-05-02 13:41:33 +0200

    Drop default value when unnecessary. (#15368)
    * Drop default value when unnecessary.
    
    * Simplify code.
    
    * Simplify code.
    
    * Generate Petstore sample code.

--- samples/openapi3/client/petstore/dart2/petstore_client_lib_fake/lib/model/model200_response.dart
+++ samples/openapi3/client/petstore/dart2/petstore_client_lib_fake/lib/model/model200_response.dart
@@ -88,7 +88,7 @@ class Model200Response {
     return null;
   }
 
-  static List<Model200Response>? listFromJson(dynamic json, {bool growable = false,}) {
+  static List<Model200Response> listFromJson(dynamic json, {bool growable = false,}) {
     final result = <Model200Response>[];
     if (json is List && json.isNotEmpty) {
       for (final row in json) {
@@ -119,12 +119,10 @@ class Model200Response {
   static Map<String, List<Model200Response>> mapListFromJson(dynamic json, {bool growable = false,}) {
     final map = <String, List<Model200Response>>{};
     if (json is Map && json.isNotEmpty) {
-      json = json.cast<String, dynamic>(); // ignore: parameter_assignments
+      // ignore: parameter_assignments
+      json = json.cast<String, dynamic>();
       for (final entry in json.entries) {
-        final value = Model200Response.listFromJson(entry.value, growable: growable,);
-        if (value != null) {
-          map[entry.key] = value;
-        }
+        map[entry.key] = Model200Response.listFromJson(entry.value, growable: growable,);
       }
     }
     return map;
