4f46768dd44906c86826b1ba4676565615b230b3
William Cheng <wing328hk@gmail.com>
2023-05-03 14:34:09 +0800

    add copyright, remove unused import

--- modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/XojoClientCodegen.java
+++ modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/XojoClientCodegen.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2018 OpenAPI-Generator Contributors (https://openapi-generator.tech)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.openapitools.codegen.languages;
 
 import io.swagger.v3.oas.models.media.ArraySchema;
@@ -9,10 +25,6 @@ import org.openapitools.codegen.meta.Stability;
 import org.openapitools.codegen.model.ModelMap;
 import org.openapitools.codegen.model.ModelsMap;
 import org.openapitools.codegen.model.OperationsMap;
-import io.swagger.models.properties.ArrayProperty;
-import io.swagger.models.properties.MapProperty;
-import io.swagger.models.properties.Property;
-import io.swagger.models.parameters.Parameter;
 import org.openapitools.codegen.utils.ModelUtils;
 
 import java.io.File;
@@ -21,7 +33,6 @@ import java.util.function.Consumer;
 
 import static org.openapitools.codegen.utils.CamelizeOption.LOWERCASE_FIRST_LETTER;
 import static org.openapitools.codegen.utils.StringUtils.camelize;
-import static org.openapitools.codegen.utils.StringUtils.underscore;
 
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.text.WordUtils;
