746bdbde5e59362e20577454682100d959a95897
mmews <marcus.mews@numberfour.eu>
2023-05-03 10:11:40 +0200

    replace tabs by spaces

--- modules/openapi-generator/src/test/java/org/openapitools/codegen/n4js/N4jsClientCodegenOptionsTest.java
+++ modules/openapi-generator/src/test/java/org/openapitools/codegen/n4js/N4jsClientCodegenOptionsTest.java
@@ -19,28 +19,28 @@ import org.openapitools.codegen.languages.N4jsClientCodegen;
 import org.openapitools.codegen.options.N4jsClientCodegenOptionsProvider;
 
 public class N4jsClientCodegenOptionsTest extends AbstractOptionsTest {
-	private N4jsClientCodegen codegen = mock(N4jsClientCodegen.class, mockSettings);
+    private N4jsClientCodegen codegen = mock(N4jsClientCodegen.class, mockSettings);
 
-	public N4jsClientCodegenOptionsTest() {
-		super(new N4jsClientCodegenOptionsProvider());
-	}
+    public N4jsClientCodegenOptionsTest() {
+        super(new N4jsClientCodegenOptionsProvider());
+    }
 
-	@Override
-	protected CodegenConfig getCodegenConfig() {
-		return codegen;
-	}
+    @Override
+    protected CodegenConfig getCodegenConfig() {
+        return codegen;
+    }
 
-	@Override
-	protected void verifyOptions() {
-		assertEquals(parseBoolean(CHECK_REQUIRED_PARAMS_NOT_NULL__VALUE),
-				codegen.additionalProperties().get(CHECK_REQUIRED_PARAMS_NOT_NULL));
-		assertEquals(parseBoolean(CHECK_SUPERFLUOUS_BODY_PROPS__VALUE),
-				codegen.additionalProperties().get(CHECK_SUPERFLUOUS_BODY_PROPS));
-		assertEquals(parseBoolean(GENERATE_DEFAULT_API_EXECUTER__VALUE),
-				codegen.additionalProperties().get(GENERATE_DEFAULT_API_EXECUTER));
+    @Override
+    protected void verifyOptions() {
+        assertEquals(parseBoolean(CHECK_REQUIRED_PARAMS_NOT_NULL__VALUE),
+                codegen.additionalProperties().get(CHECK_REQUIRED_PARAMS_NOT_NULL));
+        assertEquals(parseBoolean(CHECK_SUPERFLUOUS_BODY_PROPS__VALUE),
+                codegen.additionalProperties().get(CHECK_SUPERFLUOUS_BODY_PROPS));
+        assertEquals(parseBoolean(GENERATE_DEFAULT_API_EXECUTER__VALUE),
+                codegen.additionalProperties().get(GENERATE_DEFAULT_API_EXECUTER));
 
-		assertEquals("", codegen.additionalProperties().get(API_PACKAGE));
-		assertEquals("", codegen.additionalProperties().get(MODEL_PACKAGE));
-		assertEquals("", codegen.additionalProperties().get(API_NAME_PREFIX));
-	}
+        assertEquals("", codegen.additionalProperties().get(API_PACKAGE));
+        assertEquals("", codegen.additionalProperties().get(MODEL_PACKAGE));
+        assertEquals("", codegen.additionalProperties().get(API_NAME_PREFIX));
+    }
 }
