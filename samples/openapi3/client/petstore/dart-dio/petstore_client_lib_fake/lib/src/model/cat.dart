7e8fb00049782685c663d9509ebe896030366f84
Neil Wilkie <neil.wilkie@amiciprocurement.com>
2023-06-05 12:40:28 +0100

    [aspnetcore] #9621 Updates samples and documentation

--- samples/openapi3/client/petstore/dart-dio/petstore_client_lib_fake/lib/src/model/cat.dart
+++ samples/openapi3/client/petstore/dart-dio/petstore_client_lib_fake/lib/src/model/cat.dart
@@ -42,11 +42,6 @@ class _$CatSerializer implements PrimitiveSerializer<Cat> {
     Cat object, {
     FullType specifiedType = FullType.unspecified,
   }) sync* {
-    yield r'className';
-    yield serializers.serialize(
-      object.className,
-      specifiedType: const FullType(String),
-    );
     if (object.color != null) {
       yield r'color';
       yield serializers.serialize(
@@ -61,6 +56,11 @@ class _$CatSerializer implements PrimitiveSerializer<Cat> {
         specifiedType: const FullType(bool),
       );
     }
+    yield r'className';
+    yield serializers.serialize(
+      object.className,
+      specifiedType: const FullType(String),
+    );
   }
 
   @override
@@ -84,13 +84,6 @@ class _$CatSerializer implements PrimitiveSerializer<Cat> {
       final key = serializedList[i] as String;
       final value = serializedList[i + 1];
       switch (key) {
-        case r'className':
-          final valueDes = serializers.deserialize(
-            value,
-            specifiedType: const FullType(String),
-          ) as String;
-          result.className = valueDes;
-          break;
         case r'color':
           final valueDes = serializers.deserialize(
             value,
@@ -104,6 +97,13 @@ class _$CatSerializer implements PrimitiveSerializer<Cat> {
             specifiedType: const FullType(bool),
           ) as bool;
           result.declawed = valueDes;
+          break;
+        case r'className':
+          final valueDes = serializers.deserialize(
+            value,
+            specifiedType: const FullType(String),
+          ) as String;
+          result.className = valueDes;
           break;
         default:
           unhandled.add(key);
