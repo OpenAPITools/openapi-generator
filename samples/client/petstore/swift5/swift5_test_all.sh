e81bd725cc24ed26884ea568377cf768deeeb283
William Cheng <wing328hk@gmail.com>
2023-06-08 12:18:01 +0800

    avoid using mvn

--- samples/client/petstore/swift5/swift5_test_all.sh
+++ samples/client/petstore/swift5/swift5_test_all.sh
@@ -4,10 +4,11 @@ set -e
 DIRECTORY=`dirname $0`
 
 # example project with unit tests
-mvn -f $DIRECTORY/alamofireLibrary/SwaggerClientTests/pom.xml integration-test
-mvn -f $DIRECTORY/asyncAwaitLibrary/SwaggerClientTests/pom.xml integration-test
+(cd $DIRECTORY/alamofireLibrary/SwaggerClientTests/ && ./run_xcodebuild.sh)
+#mvn -f $DIRECTORY/alamofireLibrary/SwaggerClientTests/pom.xml integration-test
+(cd $DIRECTORY/asyncAwaitLibrary/SwaggerClientTests/ && ./run_xcodebuild.sh)
 mvn -f $DIRECTORY/combineLibrary/SwaggerClientTests/pom.xml integration-test
-#mvn -f $DIRECTORY/default/SwaggerClientTests/pom.xml integration-test
+mvn -f $DIRECTORY/default/SwaggerClientTests/pom.xml integration-test
 mvn -f $DIRECTORY/promisekitLibrary/SwaggerClientTests/pom.xml integration-test
 mvn -f $DIRECTORY/rxswiftLibrary/SwaggerClientTests/pom.xml integration-test
 mvn -f $DIRECTORY/urlsessionLibrary/SwaggerClientTests/pom.xml integration-test
@@ -17,7 +18,7 @@ mvn -f $DIRECTORY/alamofireLibrary/pom.xml integration-test
 mvn -f $DIRECTORY/anycodable/pom.xml integration-test
 mvn -f $DIRECTORY/asyncAwaitLibrary/pom.xml integration-test
 mvn -f $DIRECTORY/combineLibrary/pom.xml integration-test
-#mvn -f $DIRECTORY/default/pom.xml integration-test
+mvn -f $DIRECTORY/default/pom.xml integration-test
 mvn -f $DIRECTORY/deprecated/pom.xml integration-test
 mvn -f $DIRECTORY/frozenEnums/pom.xml integration-test
 mvn -f $DIRECTORY/nonPublicApi/pom.xml integration-test
