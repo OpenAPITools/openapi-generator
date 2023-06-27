4bec09f7b9e1551f87bd964f12587e9f1725131b
martin-mfg <2026226+martin-mfg@users.noreply.github.com>
2023-06-27 04:23:10 +0200

    [PHP] fix error on circular references (#15900)
    * fix php generators
    
    * generate samples and docs
    
    * fix samples

--- samples/server/petstore/php-mezzio-ph/src/App/DTO/Collection9.php
+++ samples/server/petstore/php-mezzio-ph/src/App/DTO/Collection9.php
@@ -6,9 +6,9 @@ namespace App\DTO;
 use Articus\DataTransfer\Annotation as DTA;
 
 /**
- * @DTA\Strategy(name="ObjectList", options={"type":\App\DTO\Tag::class})
+ * @DTA\Strategy(name="ObjectList", options={"type":\App\DTO\User::class})
  * @DTA\Validator(name="Collection", options={"validators":{
- *     {"name":"TypeCompliant", "options":{"type":\App\DTO\Tag::class}}
+ *     {"name":"TypeCompliant", "options":{"type":\App\DTO\User::class}}
  * }})
  */
 class Collection9 extends \ArrayObject
