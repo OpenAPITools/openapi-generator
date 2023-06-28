3ed9ebc3bb115f71e90786209b88f37904d0064f
y-chan <y-chan@y-chan.dev>
2023-06-28 10:30:24 +0900

    fix comment miss

--- modules/openapi-generator/src/main/java/org/openapitools/codegen/InlineModelResolver.java
+++ modules/openapi-generator/src/main/java/org/openapitools/codegen/InlineModelResolver.java
@@ -997,7 +997,7 @@ public class InlineModelResolver {
         }
 
         // If found name in components, return the name
-        // This condition occurs when allOf/anyOf/allOf/not contains exactly one schema
+        // This condition occurs when allOf/anyOf/oneOf/not contains exactly one schema
         if (openAPI.getComponents().getSchemas().get(name) != null) {
             return name;
         }
