d5c53b98127a345d82d2aec6ae36664005dfac78
Noor Dawod <noor@fine47.com>
2023-06-29 18:06:12 +0200

    Fix epoch deserialization and parsing numbers (#15948)
    * Fix 2 bugs with epoch deserialization and parsing numbers.
    
    * Generate samples.

--- samples/openapi3/client/petstore/dart2/petstore_client_lib_fake/lib/model/outer_composite.dart
+++ samples/openapi3/client/petstore/dart2/petstore_client_lib_fake/lib/model/outer_composite.dart
@@ -97,9 +97,7 @@ class OuterComposite {
       }());
 
       return OuterComposite(
-        myNumber: json[r'my_number'] == null
-            ? null
-            : num.parse(json[r'my_number'].toString()),
+        myNumber: num.parse('${json[r'my_number']}'),
         myString: mapValueOfType<String>(json, r'my_string'),
         myBoolean: mapValueOfType<bool>(json, r'my_boolean'),
       );
