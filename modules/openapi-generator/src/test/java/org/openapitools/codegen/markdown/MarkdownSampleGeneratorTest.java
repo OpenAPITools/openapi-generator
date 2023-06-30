4aab8b663451ad1659f755e97e6a529cbdf4ffe6
William Cheng <wing328hk@gmail.com>
2023-06-30 11:22:50 +0800

    add comment

--- modules/openapi-generator/src/test/java/org/openapitools/codegen/markdown/MarkdownSampleGeneratorTest.java
+++ modules/openapi-generator/src/test/java/org/openapitools/codegen/markdown/MarkdownSampleGeneratorTest.java
@@ -13,13 +13,15 @@ public class MarkdownSampleGeneratorTest {
 
     @Test(description = "test special characters in MIME type")
     public void testSpecialCharactersInMimeType() throws Exception {
+        // for https://github.com/OpenAPITools/openapi-generator/issues/15923
+        // without the fix, the following will throw exception "IllegalArgument character to be escaped is missing"
         final OpenAPI openAPI = TestUtils.parseFlattenSpec("src/test/resources/3_0/issue_15923.yaml");
         final MarkdownDocumentationCodegen codegen = new MarkdownDocumentationCodegen();
         codegen.setOpenAPI(openAPI);
 
         final String requestPath = "/v1/MyRequest";
         Operation textOperation = openAPI.getPaths().get(requestPath).getPut();
-        CodegenOperation operation = codegen.fromOperation(requestPath, "ptu", textOperation, null);
+        CodegenOperation operation = codegen.fromOperation(requestPath, "put", textOperation, null);
         CodegenParameter codegenParameter = operation.allParams.get(0);
         Assert.assertNotNull(codegenParameter);
     }
