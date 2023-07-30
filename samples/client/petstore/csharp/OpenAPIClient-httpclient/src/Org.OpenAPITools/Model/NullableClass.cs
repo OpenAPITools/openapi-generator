07d4f7d5340d2b6f9cb21874720f24ea54766eac
William Cheng <wing328hk@gmail.com>
2023-07-30 16:20:45 +0800

    update template and samples

--- samples/client/petstore/csharp/OpenAPIClient-httpclient/src/Org.OpenAPITools/Model/NullableClass.cs
+++ samples/client/petstore/csharp/OpenAPIClient-httpclient/src/Org.OpenAPITools/Model/NullableClass.cs
@@ -62,6 +62,7 @@ namespace Org.OpenAPITools.Model
             this.ObjectNullableProp = objectNullableProp;
             this.ObjectAndItemsNullableProp = objectAndItemsNullableProp;
             this.ObjectItemsNullable = objectItemsNullable;
+            this.AdditionalProperties = new Dictionary<string, object>();
         }
 
         /// <summary>
@@ -138,6 +139,12 @@ namespace Org.OpenAPITools.Model
         public Dictionary<string, Object> ObjectItemsNullable { get; set; }
 
         /// <summary>
+        /// Gets or Sets additional properties
+        /// </summary>
+        [JsonExtensionData]
+        public IDictionary<string, object> AdditionalProperties { get; set; }
+
+        /// <summary>
         /// Returns the string presentation of the object
         /// </summary>
         /// <returns>String presentation of the object</returns>
@@ -158,6 +165,7 @@ namespace Org.OpenAPITools.Model
             sb.Append("  ObjectNullableProp: ").Append(ObjectNullableProp).Append("\n");
             sb.Append("  ObjectAndItemsNullableProp: ").Append(ObjectAndItemsNullableProp).Append("\n");
             sb.Append("  ObjectItemsNullable: ").Append(ObjectItemsNullable).Append("\n");
+            sb.Append("  AdditionalProperties: ").Append(AdditionalProperties).Append("\n");
             sb.Append("}\n");
             return sb.ToString();
         }
@@ -247,6 +255,10 @@ namespace Org.OpenAPITools.Model
                 if (this.ObjectItemsNullable != null)
                 {
                     hashCode = (hashCode * 59) + this.ObjectItemsNullable.GetHashCode();
+                }
+                if (this.AdditionalProperties != null)
+                {
+                    hashCode = (hashCode * 59) + this.AdditionalProperties.GetHashCode();
                 }
                 return hashCode;
             }
