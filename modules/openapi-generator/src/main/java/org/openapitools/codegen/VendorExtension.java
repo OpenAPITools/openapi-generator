d955ff6988470c051eb71d83d0e0e9a7c5f8570c
Rodrigo de Almeida - RMA3 <rodrigo.ma3@gmail.com>
2023-08-09 11:21:57 -0300

    Add annotations to the operation - case permission validation - Fix 12219 15822 (#16259)
    * fix #12219 #15822
    
    * add test with array
    
    * update samples
    
    ---------
    
    Co-authored-by: Rodrigo Maciel de Almeida <rodrigo.almeida@wefin.com.br>

--- modules/openapi-generator/src/main/java/org/openapitools/codegen/VendorExtension.java
+++ modules/openapi-generator/src/main/java/org/openapitools/codegen/VendorExtension.java
@@ -15,6 +15,7 @@ public enum VendorExtension {
     X_CONTENT_TYPE("x-content-type", ExtensionLevel.OPERATION, "Specify custom value for 'Content-Type' header for operation", null),
     X_CLASS_EXTRA_ANNOTATION("x-class-extra-annotation", ExtensionLevel.MODEL, "List of custom annotations to be added to model", null),
     X_FIELD_EXTRA_ANNOTATION("x-field-extra-annotation", ExtensionLevel.FIELD, "List of custom annotations to be added to property", null),
+    X_OPERATION_EXTRA_ANNOTATION("x-operation-extra-annotation", ExtensionLevel.OPERATION, "List of custom annotations to be added to operation", null),
     X_VERSION_PARAM("x-version-param", ExtensionLevel.OPERATION_PARAMETER, "Marker property that tells that this parameter would be used for endpoint versioning. Applicable for headers & query params. true/false", null),
     X_PATTERN_MESSAGE("x-pattern-message", ExtensionLevel.FIELD, "Add this property whenever you need to customize the invalidation error message for the regex pattern of a variable", null),
     ;
