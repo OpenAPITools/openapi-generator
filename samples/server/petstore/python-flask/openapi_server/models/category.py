57d627b893628d25e4cd8f7e668eb83cab1967c7
Baptiste LE MORLEC <baptiste.lemorlec@stryker.com>
2023-08-23 08:54:25 +0200

    Adding model type hints for Python Flask

--- samples/server/petstore/python-flask/openapi_server/models/category.py
+++ samples/server/petstore/python-flask/openapi_server/models/category.py
@@ -45,7 +45,7 @@ class Category(Model):
         return util.deserialize_model(dikt, cls)
 
     @property
-    def id(self):
+    def id(self) -> int:
         """Gets the id of this Category.
 
 
@@ -55,7 +55,7 @@ class Category(Model):
         return self._id
 
     @id.setter
-    def id(self, id):
+    def id(self, id: int):
         """Sets the id of this Category.
 
 
@@ -66,7 +66,7 @@ class Category(Model):
         self._id = id
 
     @property
-    def name(self):
+    def name(self) -> str:
         """Gets the name of this Category.
 
 
@@ -76,7 +76,7 @@ class Category(Model):
         return self._name
 
     @name.setter
-    def name(self, name):
+    def name(self, name: str):
         """Sets the name of this Category.
 
 
