29e4f89dc3789ad64cdfe7b1a62f732fd000e6e2
Jesse Myers <jesse.myers@lettuce.co>
2023-09-01 08:10:32 -0700

    Update generated files

--- samples/client/echo_api/python/openapi_client/api/query_api.py
+++ samples/client/echo_api/python/openapi_client/api/query_api.py
@@ -17,7 +17,8 @@ import re  # noqa: F401
 import io
 import warnings
 
-from pydantic import validate_arguments
+from pydantic import validate_arguments, ValidationError
+from typing_extensions import Annotated
 
 from datetime import date, datetime
 
