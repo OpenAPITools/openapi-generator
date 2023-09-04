cb3ea1c937ae5e614b67949b34c1b1d9bd031376
habib erfani <hbberfani@gmail.com>
2023-09-04 15:22:14 +0200

    indentation perfect accomplished !!!

--- modules/openapi-generator/src/main/java/org/openapitools/codegen/utils/MetariscVisibilityTrait.java
+++ modules/openapi-generator/src/main/java/org/openapitools/codegen/utils/MetariscVisibilityTrait.java
@@ -1,11 +1,9 @@
 package org.openapitools.codegen.utils;
 
-
 import java.util.Arrays;
 import java.util.Map;
 
-public class VisibilityTrait {
-
+public class MetariscVisibilityTrait {
     public static boolean visibilityCheck(Map<String, Object> vendorExtensions, String[] visibility) {
         String extensionName = "x-extension-metarisc-codegen-visibility";
         Object extensionValue = vendorExtensions.get(extensionName);
@@ -21,6 +19,7 @@ public class VisibilityTrait {
                 vendorExtensions.put(extensionName, "public");
             }
         }
+
         return false;
     }
 }
