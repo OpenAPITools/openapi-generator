c354bf86ffec8b01206af23053e2817f21a3fa90
Julian Vennen <julian@aternos.org>
2023-09-09 08:46:49 +0200

    Set minimal PHP Version to ^8.1 (#16542)

--- samples/client/petstore/php-nextgen/OpenAPIClient-php/src/Model/FakeBigDecimalMap200Response.php
+++ samples/client/petstore/php-nextgen/OpenAPIClient-php/src/Model/FakeBigDecimalMap200Response.php
@@ -2,7 +2,7 @@
 /**
  * FakeBigDecimalMap200Response
  *
- * PHP version 8.0
+ * PHP version 8.1
  *
  * @category Class
  * @package  OpenAPI\Client
--- samples/client/petstore/php-nextgen/OpenAPIClient-php/src/Model/FileSchemaTestClass.php
+++ samples/client/petstore/php-nextgen/OpenAPIClient-php/src/Model/FileSchemaTestClass.php
@@ -2,7 +2,7 @@
 /**
  * FileSchemaTestClass
  *
- * PHP version 8.0
+ * PHP version 8.1
  *
  * @category Class
  * @package  OpenAPI\Client
