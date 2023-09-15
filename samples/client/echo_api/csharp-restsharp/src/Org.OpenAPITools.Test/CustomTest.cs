3a84292f433e6f447796619871044692971cf5e8
Oliver Krug <Oliver.Krug@ptvgroup.com>
2023-09-15 17:24:56 +0200

    add and fix tests

--- samples/client/echo_api/csharp-restsharp/src/Org.OpenAPITools.Test/CustomTest.cs
+++ samples/client/echo_api/csharp-restsharp/src/Org.OpenAPITools.Test/CustomTest.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 using Xunit;
 
 namespace Org.OpenAPITools.Test
@@ -29,6 +30,40 @@ namespace Org.OpenAPITools.Test
             Assert.Null(p2);
         }
 
+        /**
+ * Test query parameter(s)
+ * <p>
+ * Test query parameter(s)
+ *
+ * @throws ApiException if the Api call fails
+ */
+        [Fact]
+        public void TestQueryStyleFormExplodeTrueObjectTest()
+        {
+            Pet queryObject = new Pet(12345L, "Hello World", new Category(987L, "new category"), new List<string> { "http://a.com", "http://b.com" });
+            String response = api.TestQueryStyleFormExplodeTrueObject(queryObject);
+            EchoServerResponseParser p = new EchoServerResponseParser(response);
+            Assert.Equal("/query/style_form/explode_true/object?query_object=class%20Pet%20%7b%0a%20%20Id%3a%2012345%0a%20%20Name%3a%20Hello%20World%0a%20%20Category%3a%20class%20Category%20%7b%0a%20%20Id%3a%20987%0a%20%20Name%3a%20new%20category%0a%7d%0a%0a%20%20PhotoUrls%3a%20System.Collections.Generic.List%601%5bSystem.String%5d%0a%20%20Tags%3a%20%0a%20%20Status%3a%20%0a%7d%0a", p.path);
+        }
+
+        /**
+         * Test query parameter(s)
+         * <p>
+         * Test query parameter(s)
+         *
+         * @throws ApiException if the Api call fails
+         */
+        [Fact]
+        public void TestQueryStyleFormExplodeTrueObjectAllOfTest()
+        {
+            DataQuery queryObject = new DataQuery() { Text = "Hello World" };
+            queryObject.Id = 3487L;
+            queryObject.Outcomes = new List<Query.OutcomesEnum>() { Query.OutcomesEnum.SKIPPED, Query.OutcomesEnum.FAILURE };
+            String response = api.TestQueryStyleFormExplodeTrueObjectAllOf(queryObject);
+            EchoServerResponseParser p = new EchoServerResponseParser(response);
+            Assert.Equal("/query/style_form/explode_true/object/allOf?query_object=class%20DataQuery%20%7b%0a%20%20class%20Query%20%7b%0a%20%20%20%20Id%3a%203487%0a%20%20%20%20Outcomes%3a%20System.Collections.Generic.List%601%5bOrg.OpenAPITools.Model.Query%2bOutcomesEnum%5d%0a%20%20%7d%0a%20%20%0a%20%20Suffix%3a%20%0a%20%20Text%3a%20Hello%20World%0a%20%20Date%3a%2001.01.0001%2000%3a00%3a00%0a%7d%0a", p.path);
+        }
+
         [Fact]
         public void testQueryStyleDeepObjectExplodeTrueObjectTest()
         {
@@ -45,7 +80,7 @@ namespace Org.OpenAPITools.Test
             Pet queryObject = new Pet(12345L, "Hello World", new Category(987L, "new category"), new List<string> { "http://a.com", "http://b.com" });
             Task<String> responseTask = api.TestQueryStyleDeepObjectExplodeTrueObjectAsync(queryObject);
             EchoServerResponseParser p = new EchoServerResponseParser(responseTask.Result);
-            Assert.Equal("/query/style_deepObject/explode_true/object?query_object=class%20Pet%20%7b%0a%20%20Id%3a%2012345%0a%20%20Name%3a%20Hello%20World%0a%20%20Category%3a%20class%20Category%20%7b%0a%20%20Id%3a%20987%0a%20%20Name%3a%20new%20category%0a%7d%0a%0a%20%20PhotoUrls%3a%20System.Collections.Generic.List%601%5bSystem.String%5d%0a%20%20Tags%3a%20%0a%20%20Status%3a%20%0a%7d%0a", p.path);
+            Assert.Equal("/query/style_deepObject/explode_true/object?queryObject%5bid%5d=12345&queryObject%5bname%5d=Hello%20World&queryObject%5bcategory%5d=class%20Category%20%7b%0a%20%20Id%3a%20987%0a%20%20Name%3a%20new%20category%0a%7d%0a&queryObject%5bphotoUrls%5d=http%3a%2f%2fa.com%2chttp%3a%2f%2fb.com", p.path);
         }
     }
 }
