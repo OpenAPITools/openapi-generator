a2f6b8eae5cccec0748c8f19c5b700e0fdc365b6
Jonathan Ballet <jon@multani.info>
2023-09-20 04:48:31 +0200

    python: remove non-async code path from the aiohttp generator (#16601)
    * python: remove non-async code path from the aiohttp generator
    
    This removes all the non-async code from the aiohttp generator:
    
    * all the methods that should be asynchronous are marked as `async`
    * the `async_req` parameter is gone; calls are directly awaitables now
    * the async calls into a thread pool are gone and the thread pool is
      gone too (now useless)
    
    Closes: #15824
    Closes: #5539
    Related: #763
    Related: #3696
    
    * Fix empty line
    
    * remove more

--- samples/openapi3/client/petstore/python-aiohttp/tests/test_api_client.py
+++ samples/openapi3/client/petstore/python-aiohttp/tests/test_api_client.py
@@ -15,13 +15,8 @@ class TestApiClient(unittest.TestCase):
     async def test_context_manager_closes_client(self):
 
         async with petstore_api.ApiClient() as client:
-            # thread pool
-            self.assertIsNotNone(client.pool)
-            pool_ref = weakref.ref(client._pool)
-            self.assertIsNotNone(pool_ref())
             # pool_manager
             self.assertFalse(client.rest_client.pool_manager.closed)
             rest_pool_ref = client.rest_client.pool_manager
 
-        self.assertIsNone(pool_ref())
         self.assertTrue(rest_pool_ref.closed)
