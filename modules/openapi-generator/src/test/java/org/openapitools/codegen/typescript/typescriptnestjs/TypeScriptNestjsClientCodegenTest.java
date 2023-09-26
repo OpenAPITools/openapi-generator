177aa0d8dcdf3b2c0a73760063ebf0324208e24b
Thomas Trapanese <thomas.trapanese@maggioli.it>
2023-09-26 08:37:11 +0200

    fix(nestjs) fix bug in path param generator with hyphen (16668)

--- modules/openapi-generator/src/test/java/org/openapitools/codegen/typescript/typescriptnestjs/TypeScriptNestjsClientCodegenTest.java
+++ modules/openapi-generator/src/test/java/org/openapitools/codegen/typescript/typescriptnestjs/TypeScriptNestjsClientCodegenTest.java
@@ -7,9 +7,15 @@ import io.swagger.v3.oas.models.media.ComposedSchema;
 import io.swagger.v3.oas.models.media.Schema;
 import io.swagger.v3.oas.models.responses.ApiResponse;
 import io.swagger.v3.oas.models.responses.ApiResponses;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import org.openapitools.codegen.CodegenOperation;
+import org.openapitools.codegen.CodegenParameter;
 import org.openapitools.codegen.TestUtils;
 import org.openapitools.codegen.languages.TypeScriptNestjsClientCodegen;
+import org.openapitools.codegen.model.OperationMap;
+import org.openapitools.codegen.model.OperationsMap;
 import org.openapitools.codegen.typescript.TypeScriptGroups;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -17,121 +23,158 @@ import org.testng.annotations.Test;
 
 @Test(groups = {TypeScriptGroups.TYPESCRIPT, TypeScriptGroups.TYPESCRIPT_NESTJS})
 public class TypeScriptNestjsClientCodegenTest {
-    @Test
-    public void testModelFileSuffix() {
-        TypeScriptNestjsClientCodegen codegen = new TypeScriptNestjsClientCodegen();
-        codegen.additionalProperties().put("modelFileSuffix", "MySuffix");
-        codegen.additionalProperties().put("modelSuffix", "MySuffix");
-        codegen.processOpts();
 
-        Assert.assertEquals("testNameMySuffix", codegen.toModelFilename("testName"));
-    }
+  @Test
+  public void testModelFileSuffix() {
+    TypeScriptNestjsClientCodegen codegen = new TypeScriptNestjsClientCodegen();
+    codegen.additionalProperties().put("modelFileSuffix", "MySuffix");
+    codegen.additionalProperties().put("modelSuffix", "MySuffix");
+    codegen.processOpts();
 
-    @Test
-    public void testOperationIdParser() {
-        OpenAPI openAPI = TestUtils.createOpenAPI();
-        Operation operation1 = new Operation().operationId("123_test_@#$%_special_tags").responses(new ApiResponses().addApiResponse("201", new ApiResponse().description("OK")));
-        openAPI.path("another-fake/dummy/", new PathItem().get(operation1));
-        final TypeScriptNestjsClientCodegen codegen = new TypeScriptNestjsClientCodegen();
-        codegen.setOpenAPI(openAPI);
+    Assert.assertEquals("testNameMySuffix", codegen.toModelFilename("testName"));
+  }
 
-        CodegenOperation co1 = codegen.fromOperation("/another-fake/dummy/", "get", operation1, null);
-        org.testng.Assert.assertEquals(co1.operationId, "_123testSpecialTags");
+  @Test
+  public void testOperationIdParser() {
+    OpenAPI openAPI = TestUtils.createOpenAPI();
+    Operation operation1 = new Operation().operationId("123_test_@#$%_special_tags")
+        .responses(new ApiResponses().addApiResponse("201", new ApiResponse().description("OK")));
+    openAPI.path("another-fake/dummy/", new PathItem().get(operation1));
+    final TypeScriptNestjsClientCodegen codegen = new TypeScriptNestjsClientCodegen();
+    codegen.setOpenAPI(openAPI);
 
-    }
+    CodegenOperation co1 = codegen.fromOperation("/another-fake/dummy/", "get", operation1, null);
+    org.testng.Assert.assertEquals(co1.operationId, "_123testSpecialTags");
 
-    @Test
-    public void testSnapshotVersion() {
-        OpenAPI openAPI = TestUtils.createOpenAPI();
+  }
 
-        TypeScriptNestjsClientCodegen codegen = new TypeScriptNestjsClientCodegen();
-        codegen.additionalProperties().put("npmName", "@openapi/typescript-nestjs-petstore");
-        codegen.additionalProperties().put("snapshot", true);
-        codegen.additionalProperties().put("npmVersion", "1.0.0-SNAPSHOT");
-        codegen.processOpts();
-        codegen.preprocessOpenAPI(openAPI);
+  @Test
+  public void testSnapshotVersion() {
+    OpenAPI openAPI = TestUtils.createOpenAPI();
 
-        Assert.assertTrue(codegen.getNpmVersion().matches("^1.0.0-SNAPSHOT.[0-9]{12}$"));
+    TypeScriptNestjsClientCodegen codegen = new TypeScriptNestjsClientCodegen();
+    codegen.additionalProperties().put("npmName", "@openapi/typescript-nestjs-petstore");
+    codegen.additionalProperties().put("snapshot", true);
+    codegen.additionalProperties().put("npmVersion", "1.0.0-SNAPSHOT");
+    codegen.processOpts();
+    codegen.preprocessOpenAPI(openAPI);
 
-        codegen = new TypeScriptNestjsClientCodegen();
-        codegen.additionalProperties().put("npmName", "@openapi/typescript-nestjs-petstore");
-        codegen.additionalProperties().put("snapshot", true);
-        codegen.additionalProperties().put("npmVersion", "3.0.0-M1");
-        codegen.processOpts();
-        codegen.preprocessOpenAPI(openAPI);
+    Assert.assertTrue(codegen.getNpmVersion().matches("^1.0.0-SNAPSHOT.[0-9]{12}$"));
 
-        Assert.assertTrue(codegen.getNpmVersion().matches("^3.0.0-M1-SNAPSHOT.[0-9]{12}$"));
+    codegen = new TypeScriptNestjsClientCodegen();
+    codegen.additionalProperties().put("npmName", "@openapi/typescript-nestjs-petstore");
+    codegen.additionalProperties().put("snapshot", true);
+    codegen.additionalProperties().put("npmVersion", "3.0.0-M1");
+    codegen.processOpts();
+    codegen.preprocessOpenAPI(openAPI);
 
-    }
+    Assert.assertTrue(codegen.getNpmVersion().matches("^3.0.0-M1-SNAPSHOT.[0-9]{12}$"));
 
-    @Test
-    public void testWithoutSnapshotVersion() {
-        OpenAPI openAPI = TestUtils.createOpenAPI();
+  }
 
-        TypeScriptNestjsClientCodegen codegen = new TypeScriptNestjsClientCodegen();
-        codegen.additionalProperties().put("npmName", "@openapi/typescript-nestjs-petstore");
-        codegen.additionalProperties().put("snapshot", false);
-        codegen.additionalProperties().put("npmVersion", "1.0.0-SNAPSHOT");
-        codegen.processOpts();
-        codegen.preprocessOpenAPI(openAPI);
+  @Test
+  public void testWithoutSnapshotVersion() {
+    OpenAPI openAPI = TestUtils.createOpenAPI();
 
-        Assert.assertTrue(codegen.getNpmVersion().matches("^1.0.0-SNAPSHOT$"));
+    TypeScriptNestjsClientCodegen codegen = new TypeScriptNestjsClientCodegen();
+    codegen.additionalProperties().put("npmName", "@openapi/typescript-nestjs-petstore");
+    codegen.additionalProperties().put("snapshot", false);
+    codegen.additionalProperties().put("npmVersion", "1.0.0-SNAPSHOT");
+    codegen.processOpts();
+    codegen.preprocessOpenAPI(openAPI);
 
-        codegen = new TypeScriptNestjsClientCodegen();
-        codegen.additionalProperties().put("npmName", "@openapi/typescript-nestjs-petstore");
-        codegen.additionalProperties().put("snapshot", false);
-        codegen.additionalProperties().put("npmVersion", "3.0.0-M1");
-        codegen.processOpts();
-        codegen.preprocessOpenAPI(openAPI);
+    Assert.assertTrue(codegen.getNpmVersion().matches("^1.0.0-SNAPSHOT$"));
 
-        Assert.assertTrue(codegen.getNpmVersion().matches("^3.0.0-M1$"));
+    codegen = new TypeScriptNestjsClientCodegen();
+    codegen.additionalProperties().put("npmName", "@openapi/typescript-nestjs-petstore");
+    codegen.additionalProperties().put("snapshot", false);
+    codegen.additionalProperties().put("npmVersion", "3.0.0-M1");
+    codegen.processOpts();
+    codegen.preprocessOpenAPI(openAPI);
 
-    }
+    Assert.assertTrue(codegen.getNpmVersion().matches("^3.0.0-M1$"));
 
-    @Test
-    public void testRemovePrefixSuffix() {
-        TypeScriptNestjsClientCodegen codegen = new TypeScriptNestjsClientCodegen();
+  }
 
-        // simple noop test
-        Assert.assertEquals("TestName", codegen.removeModelPrefixSuffix("TestName"));
+  @Test
+  public void testRemovePrefixSuffix() {
+    TypeScriptNestjsClientCodegen codegen = new TypeScriptNestjsClientCodegen();
 
-        codegen.setModelNamePrefix("abc");
-        codegen.setModelNameSuffix("def");
-        codegen.additionalProperties().put("modelSuffix", "Ghi");
-        codegen.processOpts();
+    // simple noop test
+    Assert.assertEquals("TestName", codegen.removeModelPrefixSuffix("TestName"));
 
-        Assert.assertEquals("TestName", codegen.removeModelPrefixSuffix("TestName"));
-        Assert.assertEquals("TestName", codegen.removeModelPrefixSuffix("TestNameGhi"));
-        Assert.assertEquals("TestNameghi", codegen.removeModelPrefixSuffix("TestNameghi"));
-        Assert.assertEquals("abcTestName", codegen.removeModelPrefixSuffix("abcTestName"));
-        Assert.assertEquals("TestName", codegen.removeModelPrefixSuffix("AbcTestName"));
-        Assert.assertEquals("AbcTestName", codegen.removeModelPrefixSuffix("AbcAbcTestName"));
-        Assert.assertEquals("TestName", codegen.removeModelPrefixSuffix("TestNameDef"));
-        Assert.assertEquals("TestNamedef", codegen.removeModelPrefixSuffix("TestNamedef"));
-        Assert.assertEquals("TestNamedefghi", codegen.removeModelPrefixSuffix("TestNamedefghi"));
-        Assert.assertEquals("TestNameDefghi", codegen.removeModelPrefixSuffix("TestNameDefghi"));
-        Assert.assertEquals("TestName", codegen.removeModelPrefixSuffix("TestNameDefGhi"));
-    }
+    codegen.setModelNamePrefix("abc");
+    codegen.setModelNameSuffix("def");
+    codegen.additionalProperties().put("modelSuffix", "Ghi");
+    codegen.processOpts();
 
-    @Test
-    public void testSchema() {
-        TypeScriptNestjsClientCodegen codegen = new TypeScriptNestjsClientCodegen();
+    Assert.assertEquals("TestName", codegen.removeModelPrefixSuffix("TestName"));
+    Assert.assertEquals("TestName", codegen.removeModelPrefixSuffix("TestNameGhi"));
+    Assert.assertEquals("TestNameghi", codegen.removeModelPrefixSuffix("TestNameghi"));
+    Assert.assertEquals("abcTestName", codegen.removeModelPrefixSuffix("abcTestName"));
+    Assert.assertEquals("TestName", codegen.removeModelPrefixSuffix("AbcTestName"));
+    Assert.assertEquals("AbcTestName", codegen.removeModelPrefixSuffix("AbcAbcTestName"));
+    Assert.assertEquals("TestName", codegen.removeModelPrefixSuffix("TestNameDef"));
+    Assert.assertEquals("TestNamedef", codegen.removeModelPrefixSuffix("TestNamedef"));
+    Assert.assertEquals("TestNamedefghi", codegen.removeModelPrefixSuffix("TestNamedefghi"));
+    Assert.assertEquals("TestNameDefghi", codegen.removeModelPrefixSuffix("TestNameDefghi"));
+    Assert.assertEquals("TestName", codegen.removeModelPrefixSuffix("TestNameDefGhi"));
+  }
 
-        ComposedSchema composedSchema = new ComposedSchema();
+  @Test
+  public void testSchema() {
+    TypeScriptNestjsClientCodegen codegen = new TypeScriptNestjsClientCodegen();
 
-        Schema<Object> schema1 = new Schema<>();
-        schema1.set$ref("SchemaOne");
-        Schema<Object> schema2 = new Schema<>();
-        schema2.set$ref("SchemaTwo");
-        Schema<Object> schema3 = new Schema<>();
-        schema3.set$ref("SchemaThree");
+    ComposedSchema composedSchema = new ComposedSchema();
 
-        composedSchema.addAnyOfItem(schema1);
-        composedSchema.addAnyOfItem(schema2);
-        composedSchema.addAnyOfItem(schema3);
+    Schema<Object> schema1 = new Schema<>();
+    schema1.set$ref("SchemaOne");
+    Schema<Object> schema2 = new Schema<>();
+    schema2.set$ref("SchemaTwo");
+    Schema<Object> schema3 = new Schema<>();
+    schema3.set$ref("SchemaThree");
 
-        String schemaType = codegen.getSchemaType(composedSchema);
-        Assert.assertEquals(schemaType, "SchemaOne | SchemaTwo | SchemaThree");
-    }
+    composedSchema.addAnyOfItem(schema1);
+    composedSchema.addAnyOfItem(schema2);
+    composedSchema.addAnyOfItem(schema3);
+
+    String schemaType = codegen.getSchemaType(composedSchema);
+    Assert.assertEquals(schemaType, "SchemaOne | SchemaTwo | SchemaThree");
+  }
+
+  @Test
+  public void testPathPropertyName() {
+    TypeScriptNestjsClientCodegen codegen = new TypeScriptNestjsClientCodegen();
+
+    CodegenOperation codegenOperation = new CodegenOperation();
+
+    CodegenParameter pathParam1 = createPathParam("path-param1", "pathParam1");
+    CodegenParameter pathParam2 = createPathParam("pathParam2", "pathParam2");
+
+    codegenOperation.httpMethod = "GET";
+    codegenOperation.path = "test/{pathParam1}/{pathParam2}";
+    codegenOperation.allParams.addAll(Arrays.asList(pathParam1, pathParam2));
+
+    OperationMap operations = new OperationMap();
+    operations.setOperation(codegenOperation);
+    operations.setClassname("TestService");
+
+    OperationsMap objs = new OperationsMap();
+    objs.setOperation(operations);
+    objs.setImports(List.of());
+
+    var res = codegen.postProcessOperationsWithModels(objs, Collections.emptyList());
+
+    Assert.assertEquals(res.getOperations().getOperation().get(0).path, "test/${encodeURIComponent(String(pathParam1))}/${encodeURIComponent(String(pathParam2))}");
+  }
+
+  private CodegenParameter createPathParam(String paramName, String baseName) {
+    CodegenParameter codegenParameter = new CodegenParameter();
+    codegenParameter.paramName = paramName;
+    codegenParameter.baseName = baseName;
+    codegenParameter.dataType = "String";
+    codegenParameter.isPathParam = true;
+    return codegenParameter;
+  }
 
 }
