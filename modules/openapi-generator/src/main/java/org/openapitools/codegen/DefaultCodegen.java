25e9c48a07ef1e0a42e7248212d897a2e1fe76c8
Martin <2026226+martin-mfg@users.noreply.github.com>
2023-10-05 09:34:40 +0200

    try fix

--- modules/openapi-generator/src/main/java/org/openapitools/codegen/DefaultCodegen.java
+++ modules/openapi-generator/src/main/java/org/openapitools/codegen/DefaultCodegen.java
@@ -7247,7 +7247,7 @@ public class DefaultCodegen implements CodegenConfig {
                     codegenParameter.description = codegenModelDescription;
                     imports.add(codegenParameter.baseType);
 
-                    if (codegenProperty.complexType != null) {
+                    if (codegenProperty.complexType != null && codegenProperty.getComposedSchemas() == null) {
                         imports.add(codegenProperty.complexType);
                     }
                 }
