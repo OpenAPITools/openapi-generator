7af459396c19e90988dcc3a8fd37e3092a984f8c
Robert Schweizer <robert.schweizer@astrazeneca.com>
2023-10-11 07:51:37 +0200

    docs: Improve docstring in to_dict() method (#16777)
    It was often confusing to me why this method is even there.
    
    Also using more line breaks and matching how black would format this.

--- samples/openapi3/client/petstore/python/petstore_api/models/array_test.py
+++ samples/openapi3/client/petstore/python/petstore_api/models/array_test.py
@@ -59,13 +59,24 @@ class ArrayTest(BaseModel):
         """Create an instance of ArrayTest from a JSON string"""
         return cls.from_dict(json.loads(json_str))
 
-    def to_dict(self):
-        """Returns the dictionary representation of the model using alias"""
-        _dict = self.model_dump(by_alias=True,
-                          exclude={
-                            "additional_properties"
-                          },
-                          exclude_none=True)
+    def to_dict(self) -> Dict[str, Any]:
+        """Return the dictionary representation of the model using alias.
+
+        This has the following differences from calling pydantic's
+        `self.model_dump(by_alias=True)`:
+
+        * `None` is only added to the output dict for nullable fields that
+          were set at model initialization. Other fields with value `None`
+          are ignored.
+        * Fields in `self.additional_properties` are added to the output dict.
+        """
+        _dict = self.model_dump(
+            by_alias=True,
+            exclude={
+                "additional_properties",
+            },
+            exclude_none=True,
+        )
         # override the default output from pydantic by calling `to_dict()` of each item in array_array_of_model (list of list)
         _items = []
         if self.array_array_of_model:
