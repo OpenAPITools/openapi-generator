80b4af40067ef975967266176f94e0782c546902
Yuriy Belenko <yura-bely@mail.ru>
2023-10-14 20:58:02 +0300

    Refresh samples

--- samples/client/petstore/php-nextgen/OpenAPIClient-php/src/Api/FakeClassnameTags123Api.php
+++ samples/client/petstore/php-nextgen/OpenAPIClient-php/src/Api/FakeClassnameTags123Api.php
@@ -3,7 +3,6 @@
  * FakeClassnameTags123Api
  * PHP version 8.1
  *
- * @category Class
  * @package  OpenAPI\Client
  * @author   OpenAPI Generator team
  * @link     https://openapi-generator.tech
@@ -44,7 +43,6 @@ use OpenAPI\Client\ObjectSerializer;
 /**
  * FakeClassnameTags123Api Class Doc Comment
  *
- * @category Class
  * @package  OpenAPI\Client
  * @author   OpenAPI Generator team
  * @link     https://openapi-generator.tech
