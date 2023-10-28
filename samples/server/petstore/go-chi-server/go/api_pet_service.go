677b6e64c148abc0b544ada012f1ee3b89078619
Ian Cubbon <ianc@spectralogic.com>
2023-10-28 05:32:04 -0700

    If parsing a boolean fails, return a `ParsingError` instead of a straight 500 (#16910)
    Add an example endpoint that covers Required, Default, and Not Required cases

--- samples/server/petstore/go-chi-server/go/api_pet_service.go
+++ samples/server/petstore/go-chi-server/go/api_pet_service.go
@@ -130,6 +130,17 @@ func (s *PetAPIService) GetPetImageById(ctx context.Context, petId int64) (ImplR
 	return Response(http.StatusNotImplemented, nil), errors.New("GetPetImageById method not implemented")
 }
 
+// GetPetsUsingBooleanQueryParameters - Get the pets by only using boolean query parameters
+func (s *PetAPIService) GetPetsUsingBooleanQueryParameters(ctx context.Context, expr bool, grouping bool, inactive bool) (ImplResponse, error) {
+	// TODO - update GetPetsUsingBooleanQueryParameters with the required logic for this service method.
+	// Add api_pet_service.go to the .openapi-generator-ignore to avoid overwriting this service implementation when updating open api generation.
+
+	// TODO: Uncomment the next line to return response Response(200, ApiResponse{}) or use other options such as http.Ok ...
+	// return Response(200, ApiResponse{}), nil
+
+	return Response(http.StatusNotImplemented, nil), errors.New("GetPetsUsingBooleanQueryParameters method not implemented")
+}
+
 // UpdatePet - Update an existing pet
 func (s *PetAPIService) UpdatePet(ctx context.Context, pet Pet) (ImplResponse, error) {
 	// TODO - update UpdatePet with the required logic for this service method.
