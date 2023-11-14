38a53c46cb527d1f4fb699fbaa271f537ad92161
Javier Puerto <javier.puerto@movementsciences.ch>
2023-11-14 09:28:53 +0100

    #17040: Add support of 'x-spring-provide-args' add sample project.

---
+++ samples/server/petstore/springboot-spring-provide-args-delegate/src/main/java/org/openapitools/configuration/SpringDocConfiguration.java
@@ -0,0 +1,48 @@
+package org.openapitools.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
+@Configuration
+public class SpringDocConfiguration {
+
+    @Bean(name = "org.openapitools.configuration.SpringDocConfiguration.apiInfo")
+    OpenAPI apiInfo() {
+        return new OpenAPI()
+                .info(
+                        new Info()
+                                .title("Swagger Petstore")
+                                .description("This is a sample server Petstore server.  You can find out more about Swagger at [http://swagger.io](http://swagger.io) or on [irc.freenode.net, #swagger](http://swagger.io/irc/).  For this sample, you can use the api key `special-key` to test the authorization filters.")
+                                .termsOfService("http://swagger.io/terms/")
+                                .contact(
+                                        new Contact()
+                                                .email("apiteam@swagger.io")
+                                )
+                                .license(
+                                        new License()
+                                                .name("Apache 2.0")
+                                                .url("http://www.apache.org/licenses/LICENSE-2.0.html")
+                                )
+                                .version("1.0.6")
+                )
+                .components(
+                        new Components()
+                                .addSecuritySchemes("api_key", new SecurityScheme()
+                                        .type(SecurityScheme.Type.APIKEY)
+                                        .in(SecurityScheme.In.HEADER)
+                                        .name("api_key")
+                                )
+                                .addSecuritySchemes("petstore_auth", new SecurityScheme()
+                                        .type(SecurityScheme.Type.OAUTH2)
+                                )
+                )
+        ;
+    }
+}
