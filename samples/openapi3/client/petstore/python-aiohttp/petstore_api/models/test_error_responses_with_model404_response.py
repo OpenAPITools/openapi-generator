0c3a0a36865b2fdc42002d153e6469bb9b26195a
William Cheng <wing328hk@gmail.com>
2023-11-16 00:56:05 +0800

    update samples

--- samples/openapi3/client/petstore/python-aiohttp/petstore_api/models/test_error_responses_with_model404_response.py
+++ samples/openapi3/client/petstore/python-aiohttp/petstore_api/models/test_error_responses_with_model404_response.py
@@ -34,7 +34,8 @@ class TestErrorResponsesWithModel404Response(BaseModel):
 
     model_config = {
         "populate_by_name": True,
-        "validate_assignment": True
+        "validate_assignment": True,
+        "protected_namespaces": (),
     }
 
 
