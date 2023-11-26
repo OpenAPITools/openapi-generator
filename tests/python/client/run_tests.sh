51ca9a96cfa629e563bca21066e13e4f64294de1
Robert Schweizer <robert.schweizer@astrazeneca.com>
2023-11-26 22:04:57 +0100

    chore: Remove true from sample folder name
    python-disallowAdditionalPropertiesIfNotPresent-true
    -> python-disallowAdditionalPropertiesIfNotPresent

--- tests/python/client/run_tests.sh
+++ tests/python/client/run_tests.sh
@@ -7,7 +7,7 @@ SCRIPT_DIR=$(dirname "$0")
 ROOT_DIR=$(dirname "$0")/../../..
 # Note: These cannot run in one process, because some tests have the same import paths
 pytest -v "$ROOT_DIR/samples/client/echo_api/python"
-pytest -v "$ROOT_DIR/samples/client/echo_api/python-disallowAdditionalPropertiesIfNotPresent-true"
+pytest -v "$ROOT_DIR/samples/client/echo_api/python-disallowAdditionalPropertiesIfNotPresent"
 pytest -v "$ROOT_DIR/samples/openapi3/client/petstore/python"
 pytest -v "$ROOT_DIR/samples/openapi3/client/petstore/python-aiohttp"
 pytest -v "$SCRIPT_DIR"
