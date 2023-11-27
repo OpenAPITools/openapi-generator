7072ca52d0f3b7bc01e0939000a04b8cc2aade3f
Mann <mannpatel7555@gmail.com>
2023-11-26 22:42:57 -0400

    Refactoring

--- modules/openapi-generator-online/src/main/java/org/openapitools/codegen/online/service/Generator.java
+++ modules/openapi-generator-online/src/main/java/org/openapitools/codegen/online/service/Generator.java
@@ -5,7 +5,6 @@
   * Licensed under the Apache License, Version 2.0 (the "License");
   * you may not use this file except in compliance with the License.
   * You may obtain a copy of the License at
-  *
   *     https://www.apache.org/licenses/LICENSE-2.0
   *
   * Unless required by applicable law or agreed to in writing, software
