78b612200bc8a2cb8db9de0cfbfa199bc294334d
Beppe Catanese <gcatanese@yahoo.com>
2023-12-20 13:20:50 +0100

    Add tests to verify generated Postman samples

--- samples/schema/postman-collection/python/test/test_parameters.py
+++ samples/schema/postman-collection/python/test/test_parameters.py
@@ -25,7 +25,25 @@ class TestParameters(unittest.TestCase):
         self.assertEqual(request['url']['query'][0]['key'], 'pUserId')
         self.assertEqual(request['url']['query'][0]['value'], '888')
         self.assertEqual(request['url']['query'][0]['description'], 'Query Id.')
+
+    def test_request_parameter_required(self):
+        # request url
+        request = self.json_data['item'][2]['item'][1]['item'][0]['request']
+        self.assertEqual(request['url']['raw'], '{{baseUrl}}/users/')
+        # first query parameter
         self.assertEqual(request['url']['query'][0]['disabled'], False)
+
+    def test_request_header(self):
+        # request url
+        request = self.json_data['item'][2]['item'][1]['item'][0]['request']
+        self.assertEqual(request['url']['raw'], '{{baseUrl}}/users/')
+        # headers
+        self.assertEqual(request['header'][0]['key'], 'Accept')
+        self.assertEqual(request['header'][0]['disabled'], False)
+        self.assertEqual(request['header'][1]['key'], 'Custom-Header')
+        self.assertEqual(request['header'][1]['disabled'], True)
+        self.assertEqual(request['header'][2]['key'], 'Another-Custom-Header')
+        self.assertEqual(request['header'][2]['disabled'], False)
 
 
 if __name__ == '__main__':
