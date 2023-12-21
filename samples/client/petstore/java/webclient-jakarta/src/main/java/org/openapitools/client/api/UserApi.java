cbb6d1526055363b13f4253897545678a89887b3
nikita.shmakov <nikshmakov@gmail.com>
2023-12-21 11:56:32 +0300

    webclient explode query parameters support

--- samples/client/petstore/java/webclient-jakarta/src/main/java/org/openapitools/client/api/UserApi.java
+++ samples/client/petstore/java/webclient-jakarta/src/main/java/org/openapitools/client/api/UserApi.java
@@ -439,7 +439,7 @@ public class UserApi {
 
         queryParams.putAll(apiClient.parameterToMultiValueMap(null, "username", username));
         queryParams.putAll(apiClient.parameterToMultiValueMap(null, "password", password));
-
+        
         final String[] localVarAccepts = { 
             "application/xml", "application/json"
         };
