da8b6db73257cc6256bcf245f18d9e870ece53b2
Linh Tran Tuan <linhduonggnu@gmail.com>
2024-01-08 01:17:26 +0900

    Rust Server - Axum based (#5)

---
+++ samples/server/petstore/rust-axum/output/ops-v3/src/models.rs
@@ -0,0 +1,47 @@
+#![allow(unused_qualifications)]
+
+use http::HeaderValue;
+use validator::Validate;
+
+#[cfg(feature = "server")]
+use crate::header;
+use crate::{models, types::*};
+
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+
