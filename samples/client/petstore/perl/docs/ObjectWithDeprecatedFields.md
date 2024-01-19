e9e0ca5fb8f7061f9f734391451f4c4359132eab
William Cheng <wing328hk@gmail.com>
2024-01-19 16:56:23 +0800

    undo spec

--- samples/client/petstore/perl/docs/ObjectWithDeprecatedFields.md
+++ samples/client/petstore/perl/docs/ObjectWithDeprecatedFields.md
@@ -11,7 +11,6 @@ Name | Type | Description | Notes
 **uuid** | **string** |  | [optional] 
 **id** | **double** |  | [optional] 
 **deprecated_ref** | [**DeprecatedModel**](DeprecatedModel.md) |  | [optional] 
-**any_type_property** | **object** | Any type property | [optional] 
 **bars** | **ARRAY[string]** |  | [optional] 
 
 [[Back to Model list]](../README.md#documentation-for-models) [[Back to API list]](../README.md#documentation-for-api-endpoints) [[Back to README]](../README.md)
--- samples/client/petstore/perl/lib/WWW/OpenAPIClient/Object/ObjectWithDeprecatedFields.pm
+++ samples/client/petstore/perl/lib/WWW/OpenAPIClient/Object/ObjectWithDeprecatedFields.pm
@@ -241,13 +241,6 @@ __PACKAGE__->method_documentation({
         format => '',
         read_only => '',
             },
-    'any_type_property' => {
-        datatype => 'object',
-        base_name => 'any_type_property',
-        description => 'Any type property',
-        format => '',
-        read_only => '',
-            },
     'bars' => {
         datatype => 'ARRAY[string]',
         base_name => 'bars',
@@ -261,7 +254,6 @@ __PACKAGE__->openapi_types( {
     'uuid' => 'string',
     'id' => 'double',
     'deprecated_ref' => 'DeprecatedModel',
-    'any_type_property' => 'object',
     'bars' => 'ARRAY[string]'
 } );
 
@@ -269,7 +261,6 @@ __PACKAGE__->attribute_map( {
     'uuid' => 'uuid',
     'id' => 'id',
     'deprecated_ref' => 'deprecatedRef',
-    'any_type_property' => 'any_type_property',
     'bars' => 'bars'
 } );
 
