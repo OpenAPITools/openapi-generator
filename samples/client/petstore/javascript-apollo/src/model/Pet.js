963e16a0dd0a06689a35ed93031821bbc483da2a
William Cheng <wing328hk@gmail.com>
2024-02-06 17:00:49 +0800

    update js samples

--- samples/client/petstore/javascript-apollo/src/model/Pet.js
+++ samples/client/petstore/javascript-apollo/src/model/Pet.js
@@ -83,7 +83,7 @@ class Pet {
     static validateJSON(data) {
         // check to make sure all required properties are present in the JSON string
         for (const property of Pet.RequiredProperties) {
-            if (!data[property]) {
+            if (!data.hasOwnProperty(property)) {
                 throw new Error("The required field `" + property + "` is not found in the JSON data: " + JSON.stringify(data));
             }
         }
