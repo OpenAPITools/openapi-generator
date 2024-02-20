c99a5cfeb6b1f6f9d7c3fca2cf6a21a821237dbe
ふぁ <34892635+fa0311@users.noreply.github.com>
2024-02-20 18:53:13 +0900

    [python] update model_config from dict to ConfigDict (#17900)
    * [python] rewrite dict as ConfigDict
    
    * [python] update sample
    
    * [python] undo unwanted changes
    
    * [python] update sample

--- samples/openapi3/client/petstore/python/petstore_api/models/order.py
+++ samples/openapi3/client/petstore/python/petstore_api/models/order.py
@@ -18,7 +18,7 @@ import re  # noqa: F401
 import json
 
 from datetime import datetime
-from pydantic import BaseModel, Field, StrictBool, StrictInt, StrictStr, field_validator
+from pydantic import BaseModel, ConfigDict, Field, StrictBool, StrictInt, StrictStr, field_validator
 from typing import Any, ClassVar, Dict, List, Optional
 from typing import Optional, Set
 from typing_extensions import Self
@@ -46,11 +46,11 @@ class Order(BaseModel):
             raise ValueError("must be one of enum values ('placed', 'approved', 'delivered')")
         return value
 
-    model_config = {
-        "populate_by_name": True,
-        "validate_assignment": True,
-        "protected_namespaces": (),
-    }
+    model_config = ConfigDict(
+        populate_by_name=True,
+        validate_assignment=True,
+        protected_namespaces=(),
+    )
 
 
     def to_str(self) -> str:
--- samples/server/petstore/python-fastapi/src/openapi_server/models/category.py
+++ samples/server/petstore/python-fastapi/src/openapi_server/models/category.py
@@ -20,7 +20,7 @@ import json
 
 
 
-from pydantic import BaseModel, Field, StrictInt, field_validator
+from pydantic import BaseModel, ConfigDict, Field, StrictInt, field_validator
 from typing import Any, ClassVar, Dict, List, Optional
 from typing_extensions import Annotated
 try:
