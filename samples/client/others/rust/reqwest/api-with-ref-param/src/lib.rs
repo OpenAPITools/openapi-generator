8f158067049f8b1e9327fa23190d3ecc9de0ecae
Artem Medvedev <artem.medvedev@workato.com>
2024-02-26 22:15:02 +0100

    fix(rust): wrong path to mode used as parameter of api
    Closes #16974

---
+++ samples/client/others/rust/reqwest/api-with-ref-param/src/lib.rs
@@ -0,0 +1,12 @@
+#![allow(unused_imports)]
+
+#[macro_use]
+extern crate serde_derive;
+
+extern crate serde;
+extern crate serde_json;
+extern crate url;
+extern crate reqwest;
+
+pub mod apis;
+pub mod models;
