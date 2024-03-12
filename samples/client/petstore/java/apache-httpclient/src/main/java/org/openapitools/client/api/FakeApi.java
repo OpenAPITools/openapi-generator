de50f42fc09e48e694db0b29f821bb3c7667dabc
Ross Bender <l3ender@users.noreply.github.com>
2024-03-11 20:16:00 -0500

    update httpclient/resttemplate samples

--- samples/client/petstore/java/apache-httpclient/src/main/java/org/openapitools/client/api/FakeApi.java
+++ samples/client/petstore/java/apache-httpclient/src/main/java/org/openapitools/client/api/FakeApi.java
@@ -1808,4 +1808,122 @@ if (param2 != null)
     );
   }
 
+  /**
+   * Directly invoke the API for the given URL. Useful if the API returns direct links/URLs for subsequent requests.
+   * @param url The URL for the request, either full URL or only the path.
+   * @param method The HTTP method for the request.
+   * @throws ApiException if fails to make API call.
+   */
+  public void invokeAPI(String url, String method) throws ApiException {
+    invokeAPI(url, method, null, null, Collections.emptyMap());
+  }
+
+  /**
+   * Directly invoke the API for the given URL. Useful if the API returns direct links/URLs for subsequent requests.
+   * @param url The URL for the request, either full URL or only the path.
+   * @param method The HTTP method for the request.
+   * @param additionalHeaders Additional headers for the request.
+   * @throws ApiException if fails to make API call.
+   */
+  public void invokeAPI(String url, String method, Map<String, String> additionalHeaders) throws ApiException {
+    invokeAPI(url, method, null, null, additionalHeaders);
+  }
+
+  /**
+   * Directly invoke the API for the given URL. Useful if the API returns direct links/URLs for subsequent requests.
+   * @param url The URL for the request, either full URL or only the path.
+   * @param method The HTTP method for the request.
+   * @param request The request object.
+   * @throws ApiException if fails to make API call.
+   */
+  public void invokeAPI(String url, String method, Object request) throws ApiException {
+    invokeAPI(url, method, request, null, Collections.emptyMap());
+  }
+
+  /**
+   * Directly invoke the API for the given URL. Useful if the API returns direct links/URLs for subsequent requests.
+   * @param url The URL for the request, either full URL or only the path.
+   * @param method The HTTP method for the request.
+   * @param request The request object.
+   * @param additionalHeaders Additional headers for the request.
+   * @throws ApiException if fails to make API call.
+   */
+  public void invokeAPI(String url, String method, Object request, Map<String, String> additionalHeaders) throws ApiException {
+    invokeAPI(url, method, request, null, additionalHeaders);
+  }
+
+  /**
+   * Directly invoke the API for the given URL. Useful if the API returns direct links/URLs for subsequent requests.
+   * @param url The URL for the request, either full URL or only the path.
+   * @param method The HTTP method for the request.
+   * @param returnType The return type.
+   * @return The API response in the specified type.
+   * @throws ApiException if fails to make API call.
+   */
+  public <T> T invokeAPI(String url, String method, TypeReference<T> returnType) throws ApiException {
+    return invokeAPI(url, method, null, returnType, Collections.emptyMap());
+  }
+
+  /**
+   * Directly invoke the API for the given URL. Useful if the API returns direct links/URLs for subsequent requests.
+   * @param url The URL for the request, either full URL or only the path.
+   * @param method The HTTP method for the request.
+   * @param request The request object.
+   * @param returnType The return type.
+   * @return The API response in the specified type.
+   * @throws ApiException if fails to make API call.
+   */
+  public <T> T invokeAPI(String url, String method, Object request, TypeReference<T> returnType) throws ApiException {
+    return invokeAPI(url, method, request, returnType, Collections.emptyMap());
+  }
+
+  /**
+   * Directly invoke the API for the given URL. Useful if the API returns direct links/URLs for subsequent requests.
+   * @param url The URL for the request, either full URL or only the path.
+   * @param method The HTTP method for the request.
+   * @param request The request object.
+   * @param returnType The return type.
+   * @param additionalHeaders Additional headers for the request.
+   * @return The API response in the specified type.
+   * @throws ApiException if fails to make API call.
+   */
+  public <T> T invokeAPI(String url, String method, Object request, TypeReference<T> returnType, Map<String, String> additionalHeaders) throws ApiException {
+    String localVarPath = url.replace(apiClient.getBaseURL(), "");
+    StringJoiner localVarQueryStringJoiner = new StringJoiner("&");
+    List<Pair> localVarQueryParams = new ArrayList<Pair>();
+    List<Pair> localVarCollectionQueryParams = new ArrayList<Pair>();
+    Map<String, String> localVarHeaderParams = new HashMap<String, String>();
+    Map<String, String> localVarCookieParams = new HashMap<String, String>();
+    Map<String, Object> localVarFormParams = new HashMap<String, Object>();
+
+    localVarHeaderParams.putAll(additionalHeaders);
+
+    final String[] localVarAccepts = {
+      
+    };
+    final String localVarAccept = apiClient.selectHeaderAccept(localVarAccepts);
+
+    final String[] localVarContentTypes = {
+      "application/json"
+    };
+    final String localVarContentType = apiClient.selectHeaderContentType(localVarContentTypes);
+
+    String[] localVarAuthNames = new String[] {  };
+
+    return apiClient.invokeAPI(
+      localVarPath,
+        method,
+        localVarQueryParams,
+        localVarCollectionQueryParams,
+        localVarQueryStringJoiner.toString(),
+        request,
+        localVarHeaderParams,
+        localVarCookieParams,
+        localVarFormParams,
+        localVarAccept,
+        localVarContentType,
+        localVarAuthNames,
+        returnType
+    );
+  }
 }
