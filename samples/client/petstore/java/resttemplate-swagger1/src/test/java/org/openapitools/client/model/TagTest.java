ad1ab48faa1ec536a108a1d8397c70f112a81ef7
timo <1398557+timo-a@users.noreply.github.com>
2024-03-25 01:37:20 +0100

    update(samples): regenerate resttemplate-swagger1

--- samples/client/petstore/java/resttemplate-swagger1/src/test/java/org/openapitools/client/model/TagTest.java
+++ samples/client/petstore/java/resttemplate-swagger1/src/test/java/org/openapitools/client/model/TagTest.java
@@ -20,22 +20,21 @@ import com.fasterxml.jackson.annotation.JsonTypeName;
 import com.fasterxml.jackson.annotation.JsonValue;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
-import org.junit.Assert;
-import org.junit.Ignore;
-import org.junit.Test;
-
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
 
 /**
  * Model tests for Tag
  */
-public class TagTest {
+class TagTest {
     private final Tag model = new Tag();
 
     /**
      * Model tests for Tag
      */
     @Test
-    public void testTag() {
+    void testTag() {
         // TODO: test Tag
     }
 
@@ -43,7 +42,7 @@ public class TagTest {
      * Test the property 'id'
      */
     @Test
-    public void idTest() {
+    void idTest() {
         // TODO: test id
     }
 
@@ -51,7 +50,7 @@ public class TagTest {
      * Test the property 'name'
      */
     @Test
-    public void nameTest() {
+    void nameTest() {
         // TODO: test name
     }
 
