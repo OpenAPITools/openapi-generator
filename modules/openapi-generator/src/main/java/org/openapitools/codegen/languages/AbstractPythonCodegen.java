388f99e0127dabe431cc273d69c9b27fe10312d3
William Cheng <wing328hk@gmail.com>
2024-04-03 10:08:25 +0800

    remove tabs

--- modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/AbstractPythonCodegen.java
+++ modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/AbstractPythonCodegen.java
@@ -105,8 +105,8 @@ public abstract class AbstractPythonCodegen extends DefaultCodegen implements Co
         // from https://docs.python.org/3/reference/lexical_analysis.html#keywords
         setReservedWordsLowerCase(
                 Arrays.asList(
-			// pydantic
-			"field",
+                        // pydantic
+                        "field",
                         // local variable name used in API methods (endpoints)
                         "all_params", "resource_path", "path_params", "query_params",
                         "header_params", "form_params", "local_var_files", "body_params", "auth_settings",
