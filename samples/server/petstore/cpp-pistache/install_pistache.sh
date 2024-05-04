fde8c772fb9442b31bd348f7c0847f56f1df7631
Marc Le Bihan <mlebihan29@gmail.com>
2024-05-04 15:59:49 +0200

    [BUG] [C++][Pistache] cpp-pistache-server generating API include unde… (#18553)
    * [BUG] [C++][Pistache] cpp-pistache-server generating API include undefined "Object.h" (#2769)
    
    Should handle Object.h, AnyType.h correctly. Set.h also tested.
    
       - #include Object.h removed and replaced by a typeMapping.put(object, nlohmann::json) like suggested in other issues
       - object had an invalid syntax: ':' instead of '::' in types with namespace
       - extra include of #include nlohmann/json.h removed when there's already #include <nlohmann/json.hpp>
       - nlohmann::json is excluded from model namespace
    
    Tested with custom petstore played, with suggested openapi specs coming from issues #2769, #10266, #14234
    
       ```bash
       rm -rf samples/server/petstore/cpp-pistache-everything/ && ./bin/generate-samples.sh ./bin/configs/cpp-pistache-server-cpp-pistache-everything.yaml  && cd samples/server/petstore/cpp-pistache-everything/ && mkdir build && cd build && cmake .. && cmake --build . --parallel
       ```
    
    * - Adding to samples/server/petstore cpp-pistache-everything
    
    * - .md and FILES missing

--- samples/server/petstore/cpp-pistache/install_pistache.sh
+++ samples/server/petstore/cpp-pistache/install_pistache.sh
@@ -1,13 +1,14 @@
-#/bin/bash
+#!/bin/bash
 # ref: http://pistache.io/quickstart#installing-pistache
 #
 echo "Installing Pistache ..."
 
 git clone https://github.com/oktal/pistache.git || true
-cd pistache
+cd pistache || (echo "Cannot find git clone pistache directory"; exit 1)
+
 git submodule update --init
 mkdir -p build
-cd build
+cd build || (echo "Cannot find build directory"; exit 1)
+
 cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release ..
-make
-sudo make install
+meson
