9aee644d939103d6136784cacc31112f530b5627
Tim Quinn <tim.quinn@oracle.com>
2024-05-11 13:44:57 -0500

    Correct the copyright notice
    Signed-off-by: Tim Quinn <tim.quinn@oracle.com>

--- modules/openapi-generator/src/test/java/org/openapitools/codegen/languages/HelidonCommonCodegenTest.java
+++ modules/openapi-generator/src/test/java/org/openapitools/codegen/languages/HelidonCommonCodegenTest.java
@@ -1,4 +1,5 @@
 /*
+ * Copyright 2024 OpenAPI-Generator Contributors (https://openapi-generator.tech)
  * Copyright (c) 2024 Oracle and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
