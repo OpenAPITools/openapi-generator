OpenAPITools/openapi-generator

commit 296a6ac5163b226015511b4bea42b78f0d2966f3

[typescript-fetch] Restore pre-es2017 compatibility in modelEnum template (#18418)
* [typescript-fetch] Restore pre-es2017 compatibility in modelEnum template

Restore pre-es2017 compatibility in modelEnum template by not using Object.values in instanceOf{{classname}} function

* [typescript-fetch] Regenerate samples

* [typescript-fetch] Regenerate samples documentation

* [typescript-fetch] Add curly brackets

* [typescript-fetch] Regenerate samples

* [typescript-fetch] Use typed comparison

Co-authored-by: Esteban Gehring <esteban.gehring@gmail.com>

* [typescript-fetch] Regenerate samples

---------

Co-authored-by: Esteban Gehring <esteban.gehring@gmail.com>

modules/openapi-generator/src/main/resources/typescript-fetch/modelEnum.mustache
@@ -1,7 +1,14 @@
 {{>modelEnumInterfaces}}
 
 export function instanceOf{{classname}}(value: any): boolean {
-    return Object.values({{classname}}).includes(value);
+    for (const key in {{classname}}) {
+        if (Object.prototype.hasOwnProperty.call({{classname}}, key)) {
+            if ({{classname}}[key] === value) {
+                return true;
+            }
+        }
+    }
+    return false;
 }
 
 export function {{classname}}FromJSON(json: any): {{classname}} {

samples/client/petstore/typescript-fetch/builds/default-v3.0/models/EnumClass.ts
@@ -26,7 +26,14 @@ export type EnumClass = typeof EnumClass[keyof typeof EnumClass];
 
 
 export function instanceOfEnumClass(value: any): boolean {
-    return Object.values(EnumClass).includes(value);
+    for (const key in EnumClass) {
+        if (Object.prototype.hasOwnProperty.call(EnumClass, key)) {
+            if (EnumClass[key] === value) {
+                return true;
+            }
+        }
+    }
+    return false;
 }
 
 export function EnumClassFromJSON(json: any): EnumClass {

samples/client/petstore/typescript-fetch/builds/default-v3.0/models/OuterEnum.ts
@@ -26,7 +26,14 @@ export type OuterEnum = typeof OuterEnum[keyof typeof OuterEnum];
 
 
 export function instanceOfOuterEnum(value: any): boolean {
-    return Object.values(OuterEnum).includes(value);
+    for (const key in OuterEnum) {
+        if (Object.prototype.hasOwnProperty.call(OuterEnum, key)) {
+            if (OuterEnum[key] === value) {
+                return true;
+            }
+        }
+    }
+    return false;
 }
 
 export function OuterEnumFromJSON(json: any): OuterEnum {

samples/client/petstore/typescript-fetch/builds/default-v3.0/models/OuterEnumDefaultValue.ts
@@ -26,7 +26,14 @@ export type OuterEnumDefaultValue = typeof OuterEnumDefaultValue[keyof typeof Ou
 
 
 export function instanceOfOuterEnumDefaultValue(value: any): boolean {
-    return Object.values(OuterEnumDefaultValue).includes(value);
+    for (const key in OuterEnumDefaultValue) {
+        if (Object.prototype.hasOwnProperty.call(OuterEnumDefaultValue, key)) {
+            if (OuterEnumDefaultValue[key] === value) {
+                return true;
+            }
+        }
+    }
+    return false;
 }
 
 export function OuterEnumDefaultValueFromJSON(json: any): OuterEnumDefaultValue {

samples/client/petstore/typescript-fetch/builds/default-v3.0/models/OuterEnumInteger.ts
@@ -26,7 +26,14 @@ export type OuterEnumInteger = typeof OuterEnumInteger[keyof typeof OuterEnumInt
 
 
 export function instanceOfOuterEnumInteger(value: any): boolean {
-    return Object.values(OuterEnumInteger).includes(value);
+    for (const key in OuterEnumInteger) {
+        if (Object.prototype.hasOwnProperty.call(OuterEnumInteger, key)) {
+            if (OuterEnumInteger[key] === value) {
+                return true;
+            }
+        }
+    }
+    return false;
 }
 
 export function OuterEnumIntegerFromJSON(json: any): OuterEnumInteger {

samples/client/petstore/typescript-fetch/builds/default-v3.0/models/OuterEnumIntegerDefaultValue.ts
@@ -26,7 +26,14 @@ export type OuterEnumIntegerDefaultValue = typeof OuterEnumIntegerDefaultValue[k
 
 
 export function instanceOfOuterEnumIntegerDefaultValue(value: any): boolean {
-    return Object.values(OuterEnumIntegerDefaultValue).includes(value);
+    for (const key in OuterEnumIntegerDefaultValue) {
+        if (Object.prototype.hasOwnProperty.call(OuterEnumIntegerDefaultValue, key)) {
+            if (OuterEnumIntegerDefaultValue[key] === value) {
+                return true;
+            }
+        }
+    }
+    return false;
 }
 
 export function OuterEnumIntegerDefaultValueFromJSON(json: any): OuterEnumIntegerDefaultValue {

samples/client/petstore/typescript-fetch/builds/default-v3.0/models/SingleRefType.ts
@@ -25,7 +25,14 @@ export type SingleRefType = typeof SingleRefType[keyof typeof SingleRefType];
 
 
 export function instanceOfSingleRefType(value: any): boolean {
-    return Object.values(SingleRefType).includes(value);
+    for (const key in SingleRefType) {
+        if (Object.prototype.hasOwnProperty.call(SingleRefType, key)) {
+            if (SingleRefType[key] === value) {
+                return true;
+            }
+        }
+    }
+    return false;
 }
 
 export function SingleRefTypeFromJSON(json: any): SingleRefType {

samples/client/petstore/typescript-fetch/builds/enum/models/NumberEnum.ts
@@ -26,7 +26,14 @@ export type NumberEnum = typeof NumberEnum[keyof typeof NumberEnum];
 
 
 export function instanceOfNumberEnum(value: any): boolean {
-    return Object.values(NumberEnum).includes(value);
+    for (const key in NumberEnum) {
+        if (Object.prototype.hasOwnProperty.call(NumberEnum, key)) {
+            if (NumberEnum[key] === value) {
+                return true;
+            }
+        }
+    }
+    return false;
 }
 
 export function NumberEnumFromJSON(json: any): NumberEnum {

commit edaace9323970a4e5df805efe2f1cda70e121fde

Fix TS7053 errors in generated instanceOf* methods (#18770)
* Fix bug referenced in #18746 , introduced in #18418

* Generate examples

modules/openapi-generator/src/main/resources/typescript-fetch/modelEnum.mustache
@@ -3,7 +3,7 @@
 export function instanceOf{{classname}}(value: any): boolean {
     for (const key in {{classname}}) {
         if (Object.prototype.hasOwnProperty.call({{classname}}, key)) {
-            if ({{classname}}[key] === value) {
+            if (({{classname}} as Record<string, {{classname}}>)[key] === value) {
                 return true;
             }
         }

samples/client/petstore/typescript-fetch/builds/default-v3.0/models/EnumClass.ts
@@ -28,7 +28,7 @@ export type EnumClass = typeof EnumClass[keyof typeof EnumClass];
 export function instanceOfEnumClass(value: any): boolean {
     for (const key in EnumClass) {
         if (Object.prototype.hasOwnProperty.call(EnumClass, key)) {
-            if (EnumClass[key] === value) {
+            if ((EnumClass as Record<string, EnumClass>)[key] === value) {
                 return true;
             }
         }

samples/client/petstore/typescript-fetch/builds/default-v3.0/models/OuterEnum.ts
@@ -28,7 +28,7 @@ export type OuterEnum = typeof OuterEnum[keyof typeof OuterEnum];
 export function instanceOfOuterEnum(value: any): boolean {
     for (const key in OuterEnum) {
         if (Object.prototype.hasOwnProperty.call(OuterEnum, key)) {
-            if (OuterEnum[key] === value) {
+            if ((OuterEnum as Record<string, OuterEnum>)[key] === value) {
                 return true;
             }
         }

samples/client/petstore/typescript-fetch/builds/default-v3.0/models/OuterEnumDefaultValue.ts
@@ -28,7 +28,7 @@ export type OuterEnumDefaultValue = typeof OuterEnumDefaultValue[keyof typeof Ou
 export function instanceOfOuterEnumDefaultValue(value: any): boolean {
     for (const key in OuterEnumDefaultValue) {
         if (Object.prototype.hasOwnProperty.call(OuterEnumDefaultValue, key)) {
-            if (OuterEnumDefaultValue[key] === value) {
+            if ((OuterEnumDefaultValue as Record<string, OuterEnumDefaultValue>)[key] === value) {
                 return true;
             }
         }

samples/client/petstore/typescript-fetch/builds/default-v3.0/models/OuterEnumInteger.ts
@@ -28,7 +28,7 @@ export type OuterEnumInteger = typeof OuterEnumInteger[keyof typeof OuterEnumInt
 export function instanceOfOuterEnumInteger(value: any): boolean {
     for (const key in OuterEnumInteger) {
         if (Object.prototype.hasOwnProperty.call(OuterEnumInteger, key)) {
-            if (OuterEnumInteger[key] === value) {
+            if ((OuterEnumInteger as Record<string, OuterEnumInteger>)[key] === value) {
                 return true;
             }
         }

samples/client/petstore/typescript-fetch/builds/default-v3.0/models/OuterEnumIntegerDefaultValue.ts
@@ -28,7 +28,7 @@ export type OuterEnumIntegerDefaultValue = typeof OuterEnumIntegerDefaultValue[k
 export function instanceOfOuterEnumIntegerDefaultValue(value: any): boolean {
     for (const key in OuterEnumIntegerDefaultValue) {
         if (Object.prototype.hasOwnProperty.call(OuterEnumIntegerDefaultValue, key)) {
-            if (OuterEnumIntegerDefaultValue[key] === value) {
+            if ((OuterEnumIntegerDefaultValue as Record<string, OuterEnumIntegerDefaultValue>)[key] === value) {
                 return true;
             }
         }

samples/client/petstore/typescript-fetch/builds/default-v3.0/models/SingleRefType.ts
@@ -27,7 +27,7 @@ export type SingleRefType = typeof SingleRefType[keyof typeof SingleRefType];
 export function instanceOfSingleRefType(value: any): boolean {
     for (const key in SingleRefType) {
         if (Object.prototype.hasOwnProperty.call(SingleRefType, key)) {
-            if (SingleRefType[key] === value) {
+            if ((SingleRefType as Record<string, SingleRefType>)[key] === value) {
                 return true;
             }
         }

samples/client/petstore/typescript-fetch/builds/enum/models/NumberEnum.ts
@@ -28,7 +28,7 @@ export type NumberEnum = typeof NumberEnum[keyof typeof NumberEnum];
 export function instanceOfNumberEnum(value: any): boolean {
     for (const key in NumberEnum) {
         if (Object.prototype.hasOwnProperty.call(NumberEnum, key)) {
-            if (NumberEnum[key] === value) {
+            if ((NumberEnum as Record<string, NumberEnum>)[key] === value) {
                 return true;
             }
         }

samples/client/petstore/typescript-fetch/builds/enum/models/StringEnum.ts
@@ -28,7 +28,7 @@ export type StringEnum = typeof StringEnum[keyof typeof StringEnum];
 export function instanceOfStringEnum(value: any): boolean {
     for (const key in StringEnum) {
         if (Object.prototype.hasOwnProperty.call(StringEnum, key)) {
-            if (StringEnum[key] === value) {
+            if ((StringEnum as Record<string, StringEnum>)[key] === value) {
                 return true;
             }
         }

samples/client/petstore/typescript-fetch/builds/sagas-and-records/src/models/BehaviorType.ts
@@ -28,7 +28,7 @@ export type BehaviorType = typeof BehaviorType[keyof typeof BehaviorType];
 export function instanceOfBehaviorType(value: any): boolean {
     for (const key in BehaviorType) {
         if (Object.prototype.hasOwnProperty.call(BehaviorType, key)) {
-            if (BehaviorType[key] === value) {
+            if ((BehaviorType as Record<string, BehaviorType>)[key] === value) {
                 return true;
             }
         }

samples/client/petstore/typescript-fetch/builds/sagas-and-records/src/models/DeploymentRequestStatus.ts
@@ -37,7 +37,7 @@ export type DeploymentRequestStatus = typeof DeploymentRequestStatus[keyof typeo
 export function instanceOfDeploymentRequestStatus(value: any): boolean {
     for (const key in DeploymentRequestStatus) {
         if (Object.prototype.hasOwnProperty.call(DeploymentRequestStatus, key)) {
-            if (DeploymentRequestStatus[key] === value) {
+            if ((DeploymentRequestStatus as Record<string, DeploymentRequestStatus>)[key] === value) {
                 return true;
             }
         }

samples/client/petstore/typescript-fetch/builds/sagas-and-records/src/models/ErrorCode.ts
@@ -43,7 +43,7 @@ export type ErrorCode = typeof ErrorCode[keyof typeof ErrorCode];
 export function instanceOfErrorCode(value: any): boolean {
     for (const key in ErrorCode) {
         if (Object.prototype.hasOwnProperty.call(ErrorCode, key)) {
-            if (ErrorCode[key] === value) {
+            if ((ErrorCode as Record<string, ErrorCode>)[key] === value) {
                 return true;
             }
         }

samples/client/petstore/typescript-fetch/builds/sagas-and-records/src/models/PetPartType.ts
@@ -28,7 +28,7 @@ export type PetPartType = typeof PetPartType[keyof typeof PetPartType];
 export function instanceOfPetPartType(value: any): boolean {
     for (const key in PetPartType) {
         if (Object.prototype.hasOwnProperty.call(PetPartType, key)) {
-            if (PetPartType[key] === value) {
+            if ((PetPartType as Record<string, PetPartType>)[key] === value) {
                 return true;
             }
         }

samples/client/petstore/typescript-fetch/builds/sagas-and-records/src/models/WarningCode.ts
@@ -28,7 +28,7 @@ export type WarningCode = typeof WarningCode[keyof typeof WarningCode];
 export function instanceOfWarningCode(value: any): boolean {
     for (const key in WarningCode) {
         if (Object.prototype.hasOwnProperty.call(WarningCode, key)) {
-            if (WarningCode[key] === value) {
+            if ((WarningCode as Record<string, WarningCode>)[key] === value) {
                 return true;
             }
         }

samples/client/petstore/typescript-fetch/builds/snakecase-discriminator/models/EnumClass.ts
@@ -28,7 +28,7 @@ export type EnumClass = typeof EnumClass[keyof typeof EnumClass];
 export function instanceOfEnumClass(value: any): boolean {
     for (const key in EnumClass) {
         if (Object.prototype.hasOwnProperty.call(EnumClass, key)) {
-            if (EnumClass[key] === value) {
+            if ((EnumClass as Record<string, EnumClass>)[key] === value) {
                 return true;
             }
         }

samples/client/petstore/typescript-fetch/builds/snakecase-discriminator/models/OuterEnum.ts
@@ -28,7 +28,7 @@ export type OuterEnum = typeof OuterEnum[keyof typeof OuterEnum];
 export function instanceOfOuterEnum(value: any): boolean {
     for (const key in OuterEnum) {
         if (Object.prototype.hasOwnProperty.call(OuterEnum, key)) {
-            if (OuterEnum[key] === value) {
+            if ((OuterEnum as Record<string, OuterEnum>)[key] === value) {
                 return true;
             }
         }

samples/client/petstore/typescript-fetch/builds/snakecase-discriminator/models/OuterEnumDefaultValue.ts
@@ -28,7 +28,7 @@ export type OuterEnumDefaultValue = typeof OuterEnumDefaultValue[keyof typeof Ou
 export function instanceOfOuterEnumDefaultValue(value: any): boolean {
     for (const key in OuterEnumDefaultValue) {
         if (Object.prototype.hasOwnProperty.call(OuterEnumDefaultValue, key)) {
-            if (OuterEnumDefaultValue[key] === value) {
+            if ((OuterEnumDefaultValue as Record<string, OuterEnumDefaultValue>)[key] === value) {
                 return true;
             }
         }

samples/client/petstore/typescript-fetch/builds/snakecase-discriminator/models/OuterEnumInteger.ts
@@ -28,7 +28,7 @@ export type OuterEnumInteger = typeof OuterEnumInteger[keyof typeof OuterEnumInt
 export function instanceOfOuterEnumInteger(value: any): boolean {
     for (const key in OuterEnumInteger) {
         if (Object.prototype.hasOwnProperty.call(OuterEnumInteger, key)) {
-            if (OuterEnumInteger[key] === value) {
+            if ((OuterEnumInteger as Record<string, OuterEnumInteger>)[key] === value) {
                 return true;
             }
         }

samples/client/petstore/typescript-fetch/builds/snakecase-discriminator/models/OuterEnumIntegerDefaultValue.ts
@@ -28,7 +28,7 @@ export type OuterEnumIntegerDefaultValue = typeof OuterEnumIntegerDefaultValue[k
 export function instanceOfOuterEnumIntegerDefaultValue(value: any): boolean {
     for (const key in OuterEnumIntegerDefaultValue) {
         if (Object.prototype.hasOwnProperty.call(OuterEnumIntegerDefaultValue, key)) {
-            if (OuterEnumIntegerDefaultValue[key] === value) {
+            if ((OuterEnumIntegerDefaultValue as Record<string, OuterEnumIntegerDefaultValue>)[key] === value) {
                 return true;
             }
         }

samples/client/petstore/typescript-fetch/builds/snakecase-discriminator/models/SingleRefType.ts
@@ -27,7 +27,7 @@ export type SingleRefType = typeof SingleRefType[keyof typeof SingleRefType];
 export function instanceOfSingleRefType(value: any): boolean {
     for (const key in SingleRefType) {
         if (Object.prototype.hasOwnProperty.call(SingleRefType, key)) {
-            if (SingleRefType[key] === value) {
+            if ((SingleRefType as Record<string, SingleRefType>)[key] === value) {
                 return true;
             }
         }

samples/client/petstore/typescript-fetch/builds/with-string-enums/models/NumberEnum.ts
@@ -27,7 +27,7 @@ export enum NumberEnum {
 export function instanceOfNumberEnum(value: any): boolean {
     for (const key in NumberEnum) {
         if (Object.prototype.hasOwnProperty.call(NumberEnum, key)) {
-            if (NumberEnum[key] === value) {
+            if ((NumberEnum as Record<string, NumberEnum>)[key] === value) {
                 return true;
             }
         }

samples/client/petstore/typescript-fetch/builds/with-string-enums/models/StringEnum.ts
@@ -27,7 +27,7 @@ export enum StringEnum {
 export function instanceOfStringEnum(value: any): boolean {
     for (const key in StringEnum) {
         if (Object.prototype.hasOwnProperty.call(StringEnum, key)) {
-            if (StringEnum[key] === value) {
+            if ((StringEnum as Record<string, StringEnum>)[key] === value) {
                 return true;
             }
         }