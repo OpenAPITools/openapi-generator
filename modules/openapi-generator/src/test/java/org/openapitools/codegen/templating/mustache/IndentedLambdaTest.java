2471ba2d2e96261941d21eb10648f3d70e969423
William Cheng <wing328hk@gmail.com>
2024-05-29 18:58:44 +0800

    Revert back to junit4 (#18786)
    * revert junit5 upgrade
    
    * revert upgrade to junit5
    
    * fix kotlin-wiremock template folder
    
    * fix/comment tests
    
    * update

--- modules/openapi-generator/src/test/java/org/openapitools/codegen/templating/mustache/IndentedLambdaTest.java
+++ modules/openapi-generator/src/test/java/org/openapitools/codegen/templating/mustache/IndentedLambdaTest.java
@@ -1,6 +1,6 @@
 package org.openapitools.codegen.templating.mustache;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.Assert.assertEquals;
 
 import java.util.Map;
 
