0ce2ef7a1ae9c18fb2de4294d59cbaa4ad8a461c
Stephen (Alex) Wallen <wallenstephen@outlook.com>
2024-05-31 14:37:09 -1000

    chore: add triggers

--- samples/client/petstore/zapier/utils/utils.js
+++ samples/client/petstore/zapier/utils/utils.js
@@ -1,26 +1,26 @@
 const _ = require('lodash')
 
 const replacePathParameters = (url) => url.replace(/{([^{}]+)}/g, (keyExpr, key) => `{{bundle.inputData.${key}}}`)
-const childMapping = (objectsArray, prefix, model) => objectsArray ? objectsArray.map(object => model.mapping({inputData: object}, prefix)) : undefined
+const childMapping = (objectsArray, prefix, model) => objectsArray ? objectsArray.map(object => model.mapping({ inputData: object }, prefix)) : undefined
 const removeIfEmpty = (obj) => _.isEmpty(JSON.parse(JSON.stringify(obj))) ? undefined : obj
 const buildKeyAndLabel = (prefix, isInput = true, isArrayChild = false) => {
-    const keyPrefix = !_.isEmpty(prefix) && (!isArrayChild || isInput) ? `${prefix}${isInput ? '.' : '__'}` : prefix
-    const labelPrefix = !_.isEmpty(keyPrefix) ? keyPrefix.replaceAll('__', '.') : ''
-    return {
-        keyPrefix: keyPrefix,
-        labelPrefix: labelPrefix,
-    }
+  const keyPrefix = !_.isEmpty(prefix) && (!isArrayChild || isInput) ? `${prefix}${isInput ? '.' : '__'}` : prefix
+  const labelPrefix = !_.isEmpty(keyPrefix) ? keyPrefix.replaceAll('__', '.') : ''
+  return {
+    keyPrefix: keyPrefix,
+    labelPrefix: labelPrefix,
+  }
 }
 const isSearchAction = (key) => {
-    // TODO: custom logic
-    return false
+  // TODO: custom logic
+  return false
 }
 const hasASearchField = action => action.operation.inputFields.length > 0
 const returnsObjectsArray = action => !!action.operation.outputFields.find(field => 'children' in field)
 const hasSearchRequisites = action => hasASearchField(action) && returnsObjectsArray(action)
 const searchMiddleware = (action) => {
-    // TODO: custom logic
-    return action
+  // TODO: custom logic
+  return action
 }
 
 const requestOptionsMiddleware = (z, bundle, requestOptions) => {
@@ -31,23 +31,24 @@ const requestOptionsMiddleware = (z, bundle, requestOptions) => {
 }
 
 const isTrigger = (key) => {
-    // TODO: custom logic
-    return false
+  // TODO: custom logic
+  return false
 }
 
 const triggerMiddleware = (action) => {
-    return action
+  return action
 }
 
 module.exports = {
-    replacePathParameters: replacePathParameters,
-    childMapping: childMapping,
-    removeIfEmpty: removeIfEmpty,
-    buildKeyAndLabel: buildKeyAndLabel,
-    hasSearchRequisites: hasSearchRequisites,
-    isSearchAction: isSearchAction,
-    searchMiddleware: searchMiddleware,
-    requestOptionsMiddleware: requestOptionsMiddleware,
-    isTrigger: isTrigger,
-    triggerMiddleware: triggerMiddleware,
+  replacePathParameters: replacePathParameters,
+  childMapping: childMapping,
+  removeIfEmpty: removeIfEmpty,
+  buildKeyAndLabel: buildKeyAndLabel,
+  hasSearchRequisites: hasSearchRequisites,
+  isSearchAction: isSearchAction,
+  searchMiddleware: searchMiddleware,
+  requestOptionsMiddleware: requestOptionsMiddleware,
+  isTrigger: isTrigger,
+  triggerMiddleware: triggerMiddleware,
+  responseOptionsMiddleware: responseOptionsMiddleware,
 }
