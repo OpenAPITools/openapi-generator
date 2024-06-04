35a882076f441d861340a5472788dda3c79c6807
Philzen <Philzen@users.noreply.github.com>
2024-06-04 06:40:29 +0200

    [python-fastapi] Fix CLI crash when calling `config-help` & update docs (#18816)
    * Implement test for uniqueness of CliOptions across all generators
    
    * Refactor existing test to use @DataProvider and fluent assertion
    
    * Remove extraneous cliOption definition for disallowAdditionalPropertiesIfNotPresent
    
    This is already defined (matching exactly in all aspects) in DefaultCodegen.
    
    Resolves #18810
    
    * Avoid variable declaration & assignment when only used once
    
    * Add additional uniqueness tests for all DefaultCodegen List<> members
    
    Test uniqueness for lists of:
    - supportingFiles
    - supportedLibraries
    - supportedVendorExtensions
    
    * Disable AllGeneratorsTest.noDuplicateSupportingFiles for the time being
    
    Re-enable when #18831 is fixed
    
    * Generate updated python-fastapi docs

--- modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/PythonFastAPIServerCodegen.java
+++ modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/PythonFastAPIServerCodegen.java
@@ -90,18 +90,17 @@ public class PythonFastAPIServerCodegen extends AbstractPythonCodegen {
         super();
 
         modifyFeatureSet(features -> features.includeSecurityFeatures(
-                SecurityFeature.OAuth2_AuthorizationCode,
-                SecurityFeature.OAuth2_Password
+            SecurityFeature.OAuth2_AuthorizationCode, 
+            SecurityFeature.OAuth2_Password
         ));
 
-        generatorMetadata = GeneratorMetadata.newBuilder(generatorMetadata)
-                .stability(Stability.BETA)
-                .build();
+        generatorMetadata = GeneratorMetadata.newBuilder(generatorMetadata).stability(Stability.BETA).build();
 
-        SimpleModule simpleModule = new SimpleModule();
-        simpleModule.addKeySerializer(String.class, new SnakeCaseKeySerializer());
-        simpleModule.addSerializer(Boolean.class, new PythonBooleanSerializer());
-        MAPPER.registerModule(simpleModule);
+        MAPPER.registerModule(
+            new SimpleModule()
+                .addKeySerializer(String.class, new SnakeCaseKeySerializer())
+                .addSerializer(Boolean.class, new PythonBooleanSerializer())
+        );
 
         /*
          * Additional Properties.  These values can be passed to the templates and
@@ -139,19 +138,6 @@ public class PythonFastAPIServerCodegen extends AbstractPythonCodegen {
                 .defaultValue(DEFAULT_SOURCE_FOLDER));
         cliOptions.add(new CliOption(CodegenConstants.FASTAPI_IMPLEMENTATION_PACKAGE, "python package name for the implementation code (convention: snake_case).")
                 .defaultValue(implPackage));
-
-        // option to change how we process + set the data in the 'additionalProperties' keyword.
-        CliOption disallowAdditionalPropertiesIfNotPresentOpt = CliOption.newBoolean(
-                CodegenConstants.DISALLOW_ADDITIONAL_PROPERTIES_IF_NOT_PRESENT,
-                CodegenConstants.DISALLOW_ADDITIONAL_PROPERTIES_IF_NOT_PRESENT_DESC).defaultValue(Boolean.TRUE.toString());
-        Map<String, String> disallowAdditionalPropertiesIfNotPresentOpts = new HashMap<>();
-        disallowAdditionalPropertiesIfNotPresentOpts.put("false",
-                "The 'additionalProperties' implementation is compliant with the OAS and JSON schema specifications.");
-        disallowAdditionalPropertiesIfNotPresentOpts.put("true",
-                "Keep the old (incorrect) behaviour that 'additionalProperties' is set to false by default.");
-        disallowAdditionalPropertiesIfNotPresentOpt.setEnum(disallowAdditionalPropertiesIfNotPresentOpts);
-        cliOptions.add(disallowAdditionalPropertiesIfNotPresentOpt);
-        this.setDisallowAdditionalPropertiesIfNotPresent(true);
     }
 
     @Override
