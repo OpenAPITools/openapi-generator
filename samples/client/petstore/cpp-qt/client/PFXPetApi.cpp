9361be6a3c1c511d42e593f30d1843a0549d2dc8
Jazzco <30497975+Jazzco@users.noreply.github.com>
2024-06-27 10:50:01 +0200

    Update samples/client/petstore/cpp-qt/client/PFXPetApi.cpp
    Co-authored-by: Martin Delille <martin@delille.org>

--- samples/client/petstore/cpp-qt/client/PFXPetApi.cpp
+++ samples/client/petstore/cpp-qt/client/PFXPetApi.cpp
@@ -905,7 +905,7 @@ void PFXPetApi::findPetsByTags(const QList<QString> &tags) {
 
     connect(_latestWorker, &PFXHttpRequestWorker::on_execution_finished, this, &PFXPetApi::findPetsByTagsCallback);
     connect(this, &PFXPetApi::abortRequestsSignal, _latestWorker, &QObject::deleteLater);
-    connect(_latestWorker, &QObject::destroyed, this, [this](){
+    connect(_latestWorker, &QObject::destroyed, this, [this] {
         if(findChildren<PFXHttpRequestWorker*>().count() == 0){
             Q_EMIT allPendingRequestsCompleted();
         }
