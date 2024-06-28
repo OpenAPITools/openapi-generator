53f1094765408a1b6f12fb3f8570f5e61e876600
Cameron Koegel <53310569+ckoegel@users.noreply.github.com>
2024-06-28 07:27:10 -0400

    [Python] Allow Overriding Accept Header (#19025)
    * [Python] Allow Overriding Accept Header
    
    * generate samples
    
    * add tests

--- samples/client/echo_api/python-disallowAdditionalPropertiesIfNotPresent/openapi_client/api/header_api.py
+++ samples/client/echo_api/python-disallowAdditionalPropertiesIfNotPresent/openapi_client/api/header_api.py
@@ -327,11 +327,12 @@ class HeaderApi:
 
 
         # set the HTTP header `Accept`
-        _header_params['Accept'] = self.api_client.select_header_accept(
-            [
-                'text/plain'
-            ]
-        )
+        if 'Accept' not in _header_params:
+            _header_params['Accept'] = self.api_client.select_header_accept(
+                [
+                    'text/plain'
+                ]
+            )
 
 
         # authentication setting
