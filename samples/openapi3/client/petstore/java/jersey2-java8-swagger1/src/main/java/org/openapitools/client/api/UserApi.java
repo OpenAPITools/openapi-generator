3f15f16bfecb9eae07fa0ac54fc98d7cf58a8363
Giuseppe Tribulato <giuseppe.tribulato@here.com>
2024-07-15 10:54:01 +0200

    Close #19129: java jersey2 and jersey3: Always cast path parameters to string similarly to okhttp-gson

--- samples/openapi3/client/petstore/java/jersey2-java8-swagger1/src/main/java/org/openapitools/client/api/UserApi.java
+++ samples/openapi3/client/petstore/java/jersey2-java8-swagger1/src/main/java/org/openapitools/client/api/UserApi.java
@@ -204,7 +204,7 @@ public class UserApi {
 
     // Path parameters
     String localVarPath = "/user/{username}"
-            .replaceAll("\\{username}", apiClient.escapeString(username));
+            .replaceAll("\\{username}", apiClient.escapeString(username.toString()));
 
     String localVarAccept = apiClient.selectHeaderAccept();
     String localVarContentType = apiClient.selectHeaderContentType();
@@ -253,7 +253,7 @@ public class UserApi {
 
     // Path parameters
     String localVarPath = "/user/{username}"
-            .replaceAll("\\{username}", apiClient.escapeString(username));
+            .replaceAll("\\{username}", apiClient.escapeString(username.toString()));
 
     String localVarAccept = apiClient.selectHeaderAccept("application/xml", "application/json");
     String localVarContentType = apiClient.selectHeaderContentType();
@@ -391,7 +391,7 @@ public class UserApi {
 
     // Path parameters
     String localVarPath = "/user/{username}"
-            .replaceAll("\\{username}", apiClient.escapeString(username));
+            .replaceAll("\\{username}", apiClient.escapeString(username.toString()));
 
     String localVarAccept = apiClient.selectHeaderAccept();
     String localVarContentType = apiClient.selectHeaderContentType("application/json");
