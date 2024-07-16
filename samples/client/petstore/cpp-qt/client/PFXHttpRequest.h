49e33188cf09cbbae86409318dc6f82bc4959a7b
Martin Delille <martin@delille.org>
2024-07-16 11:51:14 +0200

    [qt-cpp] Remove Qt support before Qt 5.15.2 (#19164)

--- samples/client/petstore/cpp-qt/client/PFXHttpRequest.h
+++ samples/client/petstore/cpp-qt/client/PFXHttpRequest.h
@@ -24,9 +24,7 @@
 #include <QObject>
 #include <QString>
 #include <QTimer>
-#if QT_VERSION >= QT_VERSION_CHECK(5, 15, 0)
-    #include <QRandomGenerator>
-#endif
+#include <QRandomGenerator>
 
 #include "PFXHttpFileElement.h"
 
@@ -97,9 +95,7 @@ private:
     bool isResponseCompressionEnabled;
     bool isRequestCompressionEnabled;
     int  httpResponseCode;
-#if QT_VERSION >= QT_VERSION_CHECK(5, 15, 0)
     QRandomGenerator randomGenerator;
-#endif
 
     void on_reply_timeout(QNetworkReply *reply);
     void on_reply_finished(QNetworkReply *reply);
