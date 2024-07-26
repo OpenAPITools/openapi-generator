f0b7c56c1c4c5d639646fe9ed211448b9b124370
ksvirkou-hubspot <ksvirkou@hubspot.com>
2024-07-26 18:06:57 +0300

    Typescript: Update samples

--- samples/openapi3/client/petstore/typescript/builds/composed-schemas/models/PetByAge.ts
+++ samples/openapi3/client/petstore/typescript/builds/composed-schemas/models/PetByAge.ts
@@ -18,6 +18,8 @@ export class PetByAge {
 
     static readonly discriminator: string | undefined = undefined;
 
+    static readonly mapping: {[index: string]: string} | undefined = undefined;
+
     static readonly attributeTypeMap: Array<{name: string, baseName: string, type: string, format: string}> = [
         {
             "name": "age",
