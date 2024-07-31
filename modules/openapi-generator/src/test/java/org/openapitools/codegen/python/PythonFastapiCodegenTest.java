6d629d245da9e41f7e9c5044cad6c3e96cf293e0
Max Goltzsche <max.goltzsche@gmail.com>
2024-07-31 02:01:12 +0200

    [python-fastapi] dont inherit additionalProperties
    Don't let models inherit the value type of additionalProperties and arrays.
    
    This is to fix a bug where the `python-fastapi` server generator generated invalid models that inherited the value type specified within additionalProperties.

--- modules/openapi-generator/src/test/java/org/openapitools/codegen/python/PythonFastapiCodegenTest.java
+++ modules/openapi-generator/src/test/java/org/openapitools/codegen/python/PythonFastapiCodegenTest.java
@@ -1,15 +1,20 @@
 package org.openapitools.codegen.python;
 
-import org.openapitools.codegen.DefaultGenerator;
-import org.openapitools.codegen.TestUtils;
+import com.google.common.collect.Sets;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.media.ArraySchema;
+import io.swagger.v3.oas.models.media.Schema;
+import org.openapitools.codegen.*;
 import org.openapitools.codegen.config.CodegenConfigurator;
+import org.openapitools.codegen.languages.PythonClientCodegen;
+import org.testng.Assert;
 import org.testng.annotations.Test;
-import org.openapitools.codegen.CodegenConstants;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Collections;
 import java.util.List;
 
 public class PythonFastapiCodegenTest {
@@ -52,5 +57,25 @@ public class PythonFastapiCodegenTest {
                 "return await BaseNodescApi.subclasses[0]().nodesc()\n");
         TestUtils.assertFileContains(Paths.get(output + "/src/nodesc/apis/desc_api.py"),
                 "return await BaseDescApi.subclasses[0]().desc()\n");
+    }
+
+    @Test(description = "additionalProperties should not let container type inherit their type")
+    public void additionalPropertiesModelTest() {
+        final Schema model = new ArraySchema()
+                //.description()
+                .items(new Schema().type("object").additionalProperties(new Schema().type("string")))
+                .description("model with additionalProperties");
+        final DefaultCodegen codegen = new PythonClientCodegen();
+        OpenAPI openAPI = TestUtils.createOpenAPIWithOneSchema("sample", model);
+        codegen.setOpenAPI(openAPI);
+        final CodegenModel cm = codegen.fromModel("sample", model);
+
+        Assert.assertEquals(cm.name, "sample");
+        Assert.assertEquals(cm.classname, "Sample");
+        Assert.assertEquals(cm.description, "model with additionalProperties");
+        Assert.assertEquals(cm.vars.size(), 0);
+        Assert.assertEquals(cm.parent, "null<Dict>");
+        Assert.assertEquals(cm.imports.size(), 0);
+        Assert.assertEquals(Sets.intersection(cm.imports, Sets.newHashSet()).size(), 0);
     }
 }
