da2dcec60e8ef77cc9f08444a49a45cbea805532
Tim Quinn <tim.quinn@oracle.com>
2024-08-03 13:51:51 -0500

    Updates samples

--- samples/client/petstore/java-helidon-client/v3/mp/src/main/java/org/openapitools/client/api/PetApi.java
+++ samples/client/petstore/java-helidon-client/v3/mp/src/main/java/org/openapitools/client/api/PetApi.java
@@ -14,8 +14,6 @@ package org.openapitools.client.api;
 
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
 import jakarta.ws.rs.*;
