c6f73b55a7e7203731ce764dd229dacaab322c7f
condorcorde <alessandro.miotto@kyos.ch>
2024-08-12 13:02:30 +0200

    Update PSApiClient.ps1

--- samples/client/petstore/powershell/src/PSPetstore/Private/PSApiClient.ps1
+++ samples/client/petstore/powershell/src/PSPetstore/Private/PSApiClient.ps1
@@ -239,6 +239,7 @@ function DeserializeResponse {
         [Parameter(Mandatory)]
         [Object]$Response,
         [Parameter(Mandatory)]
+        [AllowNull()]
         [AllowEmptyCollection()]
         [string[]]$ContentTypes
     )
