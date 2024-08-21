cc98333d870c090e658a5ebd8143df2fc80b34ee
Rory Schadler <48921090+roryschadler@users.noreply.github.com>
2024-08-21 03:09:10 -0400

    feat(python): handle multiple file parameters (#19329)
    * test: fix broken python test
    
    * fix: handle multiple file parameters
    
    Previously an array of files was not handled correctly, despite the type
    annotation implying it was.
    
    * feat: handle filename,filedata tuples in file param
    
    This allows for users to pass filenames with their data in file params,
    which is useful for multipart formdata requests. Without this, the list
    of files added in the previous commit would have the same filename for
    all files (the parameter name).

--- samples/client/echo_api/python-disallowAdditionalPropertiesIfNotPresent/openapi_client/api/auth_api.py
+++ samples/client/echo_api/python-disallowAdditionalPropertiesIfNotPresent/openapi_client/api/auth_api.py
@@ -239,7 +239,9 @@ class AuthApi:
         _query_params: List[Tuple[str, str]] = []
         _header_params: Dict[str, Optional[str]] = _headers or {}
         _form_params: List[Tuple[str, str]] = []
-        _files: Dict[str, Union[str, bytes]] = {}
+        _files: Dict[
+            str, Union[str, bytes, List[str], List[bytes], List[Tuple[str, bytes]]]
+        ] = {}
         _body_params: Optional[bytes] = None
 
         # process the path parameters
@@ -483,7 +485,9 @@ class AuthApi:
         _query_params: List[Tuple[str, str]] = []
         _header_params: Dict[str, Optional[str]] = _headers or {}
         _form_params: List[Tuple[str, str]] = []
-        _files: Dict[str, Union[str, bytes]] = {}
+        _files: Dict[
+            str, Union[str, bytes, List[str], List[bytes], List[Tuple[str, bytes]]]
+        ] = {}
         _body_params: Optional[bytes] = None
 
         # process the path parameters
