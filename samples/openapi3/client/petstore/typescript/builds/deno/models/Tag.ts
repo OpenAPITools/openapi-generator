7510e6bbf8fddf40d663f86300c870383fea69f4
Joscha Feth <joscha@feth.com>
2024-08-30 15:20:18 +0100

    [Typescript] Generate oneOf schemas as type unions (#19494)
    * Add oneOf model for Typescript generator
    
    * Update import procces: For oneOfs only import $refs within the oneOf
    
    * Remove new line after description
    
    * Update samples
    
    * Typescript: Update model.mustache
    
    * Typescript: Remove emun from oneOf models
    
    * Update samples
    
    * Typescript oneOf: add discriminator and update deserialize procces
    
    * Typescript: update tests
    
    * Typescript oneOf: move type mapping to models
    
    * Typescript: Update samples
    
    * Typescript: Update type of mappig
    
    * Typescript: Update type of mappig
    
    * Typescript oneOf: update deserializing logic
    
    * Typescript: Update samples
    
    * Revert "[typescript] fix: `enum` can not receive stringified class name (#19481)"
    
    This reverts commit 4238f17322bdb0e968f94b621669b737395a3dc9.
    
    * [typescript] chore: update fixtures
    
    ---------
    
    Co-authored-by: ksvirkou-hubspot <ksvirkou@hubspot.com>

--- samples/openapi3/client/petstore/typescript/builds/deno/models/Tag.ts
+++ samples/openapi3/client/petstore/typescript/builds/deno/models/Tag.ts
@@ -21,6 +21,8 @@ export class Tag {
 
     static readonly discriminator: string | undefined = undefined;
 
+    static readonly mapping: {[index: string]: string} | undefined = undefined;
+
     static readonly attributeTypeMap: Array<{name: string, baseName: string, type: string, format: string}> = [
         {
             "name": "id",
@@ -42,4 +44,3 @@ export class Tag {
     public constructor() {
     }
 }
-
