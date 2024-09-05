1f517030220b3ad0bb72b0fc0fd4beb70a0d18ef
Eric Rolli <149762070+eric-rolli@users.noreply.github.com>
2024-09-05 08:25:11 +0200

    Update ApiClient.java
    Jersey doesn't allow entities in method DELETE

--- samples/client/petstore/java/jersey3/src/main/java/org/openapitools/client/ApiClient.java
+++ samples/client/petstore/java/jersey3/src/main/java/org/openapitools/client/ApiClient.java
@@ -1351,7 +1351,7 @@ public class ApiClient extends JavaTimeFormatter {
     } else if ("PUT".equals(method)) {
       response = invocationBuilder.put(entity);
     } else if ("DELETE".equals(method)) {
-      response = invocationBuilder.method("DELETE", entity);
+      response = invocationBuilder.method("DELETE");
     } else if ("PATCH".equals(method)) {
       response = invocationBuilder.method("PATCH", entity);
     } else {
