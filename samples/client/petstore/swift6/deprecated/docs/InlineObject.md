7f6b1306ed203e36b7468807279a4fb78d73baa7
Bruno Coelho <4brunu@users.noreply.github.com>
2024-09-25 09:57:25 +0100

    Add swift6 generator (#19621)
    * [Swift6] create Swift6 generator
    
    * [Swift6] create Swift6 generator
    
    * Update vapor integration
    
    * Update bitrise stack to Xcode 16
    
    * [Swift6] tryped throws
    
    * [Swift6] tryped throws
    
    * [Swift6] combine deferred and api static method
    
    * [Swift6] update readme
    
    * [Swift6] fix some errors
    
    * [Swift6] fix some errors
    
    * [Swift6] fix some errors
    
    * [Swift6] update docs
    
    * [Swift6] update docs
    
    * Use multiline comments for examples in csharp generator (#19079)
    
    * multi
    
    * gen
    
    * Uncomment File::deleteOnExit (#19624)
    
    * [Core/Rust Server] Check references in additionalProperties correctly when checking freeForm status (#19605)
    
    * Check references in additionalProperties correctly
    
    Handle references in additionalProperties correctly when determining free-form status
    
    * Update samples
    
    * [Rust Server] Handle arrays in forms (#19625)
    
    * [Rust Server] Handle arrays in forms correctly
    
    * [Rust Server] Add tests
    
    * Update samples
    
    * [Swift6] fix CI
    
    * [Swift6] fix CI
    
    * [Swift6] fix CI
    
    * [Swift6] fix CI
    
    ---------
    
    Co-authored-by: Liri S <reallyliri@gmail.com>
    Co-authored-by: Beppe Catanese <1771700+gcatanese@users.noreply.github.com>
    Co-authored-by: Richard Whitehouse <git@richardwhiuk.com>
    Co-authored-by: William Cheng <wing328hk@gmail.com>

---
+++ samples/client/petstore/swift6/deprecated/docs/InlineObject.md
@@ -0,0 +1,11 @@
+# InlineObject
+
+## Properties
+Name | Type | Description | Notes
+------------ | ------------- | ------------- | -------------
+**name** | **String** | Updated name of the pet | [optional] 
+**status** | **String** | Updated status of the pet | [optional] 
+
+[[Back to Model list]](../README.md#documentation-for-models) [[Back to API list]](../README.md#documentation-for-api-endpoints) [[Back to README]](../README.md)
+
+
---
+++ samples/client/petstore/swift6/promisekitLibrary/Package.swift
@@ -0,0 +1,35 @@
+// swift-tools-version:6.0
+
+import PackageDescription
+
+let package = Package(
+    name: "PetstoreClient",
+    platforms: [
+        .iOS(.v12),
+        .macOS(.v10_13),
+        .tvOS(.v12),
+        .watchOS(.v4),
+    ],
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "PetstoreClient",
+            targets: ["PetstoreClient"]
+        ),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        .package(url: "https://github.com/Flight-School/AnyCodable", .upToNextMajor(from: "0.6.7")),
+        .package(url: "https://github.com/mxcl/PromiseKit", .upToNextMajor(from: "8.1.2")),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "PetstoreClient",
+            dependencies: ["AnyCodable", "PromiseKit", ],
+            path: "PetstoreClient/Classes"
+        ),
+    ],
+    swiftLanguageModes: [.v6]
+)
