bd4cd66cd4e21f6ea31d1a3ce31548d9ddc1d5d9
Szohr Félix <szohrfelix@outlook.hu>
2024-10-03 18:21:56 +0200

    Revert "Regenerate samples"
    This reverts commit 36abb341a478fab6621b3ecb3867715b274b4b5a.

--- samples/server/petstore/java-camel/src/main/java/org/openapitools/model/ModelApiResponse.java
+++ samples/server/petstore/java-camel/src/main/java/org/openapitools/model/ModelApiResponse.java
@@ -47,6 +47,7 @@ public class ModelApiResponse {
    * Get code
    * @return code
    */
+  
   @Schema(name = "code", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
   @JsonProperty("code")
   @JacksonXmlProperty(localName = "code")
@@ -67,6 +68,7 @@ public class ModelApiResponse {
    * Get type
    * @return type
    */
+  
   @Schema(name = "type", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
   @JsonProperty("type")
   @JacksonXmlProperty(localName = "type")
@@ -87,6 +89,7 @@ public class ModelApiResponse {
    * Get message
    * @return message
    */
+  
   @Schema(name = "message", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
   @JsonProperty("message")
   @JacksonXmlProperty(localName = "message")
