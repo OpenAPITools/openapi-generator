15c27e40d5dc02341f6d2c6e8e6193aa72aa0bb0
William Cheng <wing328hk@gmail.com>
2024-10-31 13:48:05 +0800

    update js samples

--- samples/client/petstore/javascript-es6/src/ApiClient.js
+++ samples/client/petstore/javascript-es6/src/ApiClient.js
@@ -443,7 +443,8 @@ class ApiClient {
         }
 
         if (contentType === 'application/x-www-form-urlencoded') {
-            request.send(querystring.stringify(this.normalizeParams(formParams)));
+            let queryString = querystring.stringify(this.normalizeParams(formParams));
+            request.send(queryString);
         } else if (contentType == 'multipart/form-data') {
             var _formParams = this.normalizeParams(formParams);
             for (var key in _formParams) {
