d3c82616e427d31efdc3201f1115da6c46cb72d2
Sylwester Zielinski <sylwester.zielinski@airthings.com>
2024-11-07 10:26:13 +0100

    Bump libraries for Kotlin-client.

--- samples/client/petstore/kotlin-array-simple-string-multiplatform/src/jsTest/kotlin/util/Coroutine.kt
+++
@@ -1,7 +0,0 @@
-package util
-
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.promise
-
-actual fun <T> runTest(block: suspend (scope : CoroutineScope) -> T): dynamic = GlobalScope.promise { block(this) }
