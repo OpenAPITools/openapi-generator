7deecdc569ddb1c41289829b26d4c2afaefb3b0b
Julien Tschäppät <11425952+elendil-software@users.noreply.github.com>
2024-11-07 13:28:29 +0100

    [csharp] Add a server generator for FastEndpoints  (#19690)
    * Add of a first working generator for FastEndpoints framework
    
    Generator that support the basics of a FastEndpoints project
    https://fast-endpoints.com/
    
    * Add respose code documentation support
    
    * Add an option to enable use of problem details
    
    * Clean enum and model
    
    * Add an option to generate record for requests/models
    
    * Update sample
    
    * Add support of nullable types
    
    * Add option to enable authentication
    
    * Add option to generate validators
    
    * Clean readme.md file
    
    * Add option to enable response caching
    
    * update readme template
    
    * Add missing files references
    
    * Update generated sample
    
    * Add link to documentation
    
    * Add generator documentation
    
    * correctly support packageName
    
    supportingFiles additions moved to processOpts()
    
    * improve useAuthentication option handling
    
    Avoid to copy an empty file
    
    * processOpts, move call to parent at the end
    
    * Update generated sample
    
    * Refactor and fix super.processOpts(); call order
    
    * Handle the case of multi-line description
    
    * Rename field to match naming conventions
    
    * Add useApiVersioning option
    
    * Update generated sample
    
    * Add generator documentation
    
    * Fix record when model contains optional properties
    
    * Add configuration files for each option
    
    * Add sample project for each configuration
    
    * Add GitHub Action workflow for sample projects
    
    * Add FastEndpoints BindFrom attribute on path, query and form params
    
    * Update sample generated projects
    
    * Fix validator template
    
    * fix alphabetical order
    
    * Use fully qualified name for FastEndpoints
    
    * Add options to set GUID to be used in sln file
    
    * update sample projects
    
    * Update generators.md
    
    * Fix path in github workflow
    
    * Put readme, gitignore and solution file at the root of generated project
    
    * update sample projects
    
    * Remove the projectGuid option
    
    This Guid need to be constant, it is related to project type
    
    * update sample projects

---
+++ samples/server/petstore/aspnet/fastendpoints-useAuthentication/src/Org.OpenAPITools/Features/StoreApiEndpoint.cs
@@ -0,0 +1,157 @@
+using Org.OpenAPITools.Models;
+
+namespace Org.OpenAPITools.Features;
+
+
+
+/// <summary>
+/// Delete purchase order by ID
+/// </summary>
+
+public class DeleteOrderEndpoint : FastEndpoints.Endpoint<DeleteOrderRequest>
+{
+    public override void Configure()
+    {
+        Delete("/v2/store/order/{orderId}");
+        
+        
+        //TODO : authentication is enabled, you need to configure the authorizations : https://fast-endpoints.com/docs/security#endpoint-authorization
+        
+        Description(x =>
+        {
+            x.WithTags("store");
+            FastEndpoints.RouteHandlerBuilderExtensions.ProducesProblemFE(x, 400);
+            FastEndpoints.RouteHandlerBuilderExtensions.ProducesProblemFE(x, 404);
+        });
+
+        Summary(s => {
+            s.Summary = "Delete purchase order by ID";
+            s.RequestParam(r => r.OrderId, "ID of the order that needs to be deleted");
+            s.Responses[400] = "Invalid ID supplied";
+            s.Responses[404] = "Order not found";
+        });
+    }
+
+    public override async Task HandleAsync(DeleteOrderRequest req, CancellationToken ct)
+    {
+        //Response = new()
+        //{
+            //...
+        //};
+        //return Task.CompletedTask;
+    }
+}
+
+
+/// <summary>
+/// Returns pet inventories by status
+/// </summary>
+
+public class GetInventoryEndpoint : FastEndpoints.EndpointWithoutRequest<Dictionary<string, int>>
+{
+    public override void Configure()
+    {
+        Get("/v2/store/inventory");
+        
+        
+        //TODO : authentication is enabled, you need to configure the authorizations : https://fast-endpoints.com/docs/security#endpoint-authorization
+        
+        Description(x =>
+        {
+            x.WithTags("store");
+        });
+
+        Summary(s => {
+            s.Summary = "Returns pet inventories by status";
+            s.Responses[200] = "successful operation";
+        });
+    }
+
+    public override async Task HandleAsync(CancellationToken ct)
+    {
+        //Response = new()
+        //{
+            //...
+        //};
+        //return Task.CompletedTask;
+    }
+}
+
+
+/// <summary>
+/// Find purchase order by ID
+/// </summary>
+
+public class GetOrderByIdEndpoint : FastEndpoints.Endpoint<GetOrderByIdRequest, Order>
+{
+    public override void Configure()
+    {
+        Get("/v2/store/order/{orderId}");
+        
+        
+        //TODO : authentication is enabled, you need to configure the authorizations : https://fast-endpoints.com/docs/security#endpoint-authorization
+        
+        Description(x =>
+        {
+            x.WithTags("store");
+            FastEndpoints.RouteHandlerBuilderExtensions.ProducesProblemFE(x, 400);
+            FastEndpoints.RouteHandlerBuilderExtensions.ProducesProblemFE(x, 404);
+        });
+
+        Summary(s => {
+            s.Summary = "Find purchase order by ID";
+            s.RequestParam(r => r.OrderId, "ID of pet that needs to be fetched");
+            s.Responses[200] = "successful operation";
+            s.Responses[400] = "Invalid ID supplied";
+            s.Responses[404] = "Order not found";
+        });
+    }
+
+    public override async Task HandleAsync(GetOrderByIdRequest req, CancellationToken ct)
+    {
+        //Response = new()
+        //{
+            //...
+        //};
+        //return Task.CompletedTask;
+    }
+}
+
+
+/// <summary>
+/// Place an order for a pet
+/// </summary>
+
+public class PlaceOrderEndpoint : FastEndpoints.Endpoint<PlaceOrderRequest, Order>
+{
+    public override void Configure()
+    {
+        Post("/v2/store/order");
+        
+        
+        //TODO : authentication is enabled, you need to configure the authorizations : https://fast-endpoints.com/docs/security#endpoint-authorization
+        
+        Description(x =>
+        {
+            x.WithTags("store");
+            FastEndpoints.RouteHandlerBuilderExtensions.ProducesProblemFE(x, 400);
+        });
+
+        Summary(s => {
+            s.Summary = "Place an order for a pet";
+            s.RequestParam(r => r.order, "order placed for purchasing the pet");
+            s.Responses[200] = "successful operation";
+            s.Responses[400] = "Invalid Order";
+        });
+    }
+
+    public override async Task HandleAsync(PlaceOrderRequest req, CancellationToken ct)
+    {
+        //Response = new()
+        //{
+            //...
+        //};
+        //return Task.CompletedTask;
+    }
+}
+
