711e53a90cd500a6068867e8294e91c394887143
William Cheng <wing328hk@gmail.com>
2024-11-29 17:12:12 +0800

    [Java] Add option to fallback to legacy enum naming (#20172)
    * add option to fallback legacy enum naming (java)
    
    * fix tests

--- samples/client/others/java/okhttp-gson-streaming/src/main/java/org/openapitools/client/model/SomeObj.java
+++ samples/client/others/java/okhttp-gson-streaming/src/main/java/org/openapitools/client/model/SomeObj.java
@@ -58,7 +58,7 @@ public class SomeObj implements Serializable {
    */
   @JsonAdapter(TypeEnum.Adapter.class)
   public enum TypeEnum {
-    SOME_OBJ_IDENTIFIER("SomeObjIdentifier");
+    SOMEOBJIDENTIFIER("SomeObjIdentifier");
 
     private String value;
 
@@ -106,7 +106,7 @@ public class SomeObj implements Serializable {
   public static final String SERIALIZED_NAME_$_TYPE = "$_type";
   @SerializedName(SERIALIZED_NAME_$_TYPE)
   @javax.annotation.Nullable
-  private TypeEnum $type = TypeEnum.SOME_OBJ_IDENTIFIER;
+  private TypeEnum $type = TypeEnum.SOMEOBJIDENTIFIER;
 
   public static final String SERIALIZED_NAME_ID = "id";
   @SerializedName(SERIALIZED_NAME_ID)
