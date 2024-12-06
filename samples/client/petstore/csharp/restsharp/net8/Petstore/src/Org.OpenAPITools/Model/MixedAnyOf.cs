475d8e94508f1ded3b3d10699b846c030c46f82d
Bryan Aldrich <bryan.a.aldrich@gmail.com>
2024-12-05 22:54:26 -0500

    adjusted csharp codegen to allow for nullable properties (on value types only unless nullable reference types is enabled). Edited tests to compensate for the actual data types. They were optional/not-required in the yaml, but came back with non-nullable types.

--- samples/client/petstore/csharp/restsharp/net8/Petstore/src/Org.OpenAPITools/Model/MixedAnyOf.cs
+++ samples/client/petstore/csharp/restsharp/net8/Petstore/src/Org.OpenAPITools/Model/MixedAnyOf.cs
@@ -45,7 +45,7 @@ namespace Org.OpenAPITools.Model
         /// Gets or Sets Content
         /// </summary>
         [DataMember(Name = "content", EmitDefaultValue = false)]
-        public MixedAnyOfContent Content { get; set; }
+        public MixedAnyOfContent? Content { get; set; }
 
         /// <summary>
         /// Returns the string presentation of the object
