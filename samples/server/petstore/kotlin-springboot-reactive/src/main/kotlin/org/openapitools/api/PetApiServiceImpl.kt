67ee7233ec2dfffb2b60cb07c1f4151b57f955ff
William Cheng <wing328hk@gmail.com>
2025-01-11 16:40:32 +0800

    update samples

--- samples/server/petstore/kotlin-springboot-reactive/src/main/kotlin/org/openapitools/api/PetApiServiceImpl.kt
+++ samples/server/petstore/kotlin-springboot-reactive/src/main/kotlin/org/openapitools/api/PetApiServiceImpl.kt
@@ -15,11 +15,11 @@ class PetApiServiceImpl : PetApiService {
         TODO("Implement me")
     }
 
-    override fun findPetsByStatus(status: kotlin.collections.List<kotlin.String>): List<Pet> {
+    override fun findPetsByStatus(status: kotlin.collections.List<kotlin.String>): Flow<Pet> {
         TODO("Implement me")
     }
 
-    override fun findPetsByTags(tags: kotlin.collections.List<kotlin.String>): List<Pet> {
+    override fun findPetsByTags(tags: kotlin.collections.List<kotlin.String>): Flow<Pet> {
         TODO("Implement me")
     }
 
