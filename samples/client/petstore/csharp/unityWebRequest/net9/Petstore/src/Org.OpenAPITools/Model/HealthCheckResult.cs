7f659adcab38b95ef3c98f9c7833dfaf22456181
bllassit <bllassit@microsoft.com>
2025-02-03 14:00:11 -0800

    fix default value in ctor

--- samples/client/petstore/csharp/unityWebRequest/net9/Petstore/src/Org.OpenAPITools/Model/HealthCheckResult.cs
+++ samples/client/petstore/csharp/unityWebRequest/net9/Petstore/src/Org.OpenAPITools/Model/HealthCheckResult.cs
@@ -34,7 +34,7 @@ namespace Org.OpenAPITools.Model
         /// Initializes a new instance of the <see cref="HealthCheckResult" /> class.
         /// </summary>
         /// <param name="nullableMessage">nullableMessage.</param>
-        public HealthCheckResult(string? nullableMessage = default(string?))
+        public HealthCheckResult(string? nullableMessage = default(string))
         {
             this.NullableMessage = nullableMessage;
         }
