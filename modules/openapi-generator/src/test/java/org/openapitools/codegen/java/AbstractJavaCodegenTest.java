c143f10eedd2aacd137203397e7b09db60301b53
Zemke <flzemke@gmail.com>
2025-02-09 16:43:49 +0100

    format date in codegen

--- modules/openapi-generator/src/test/java/org/openapitools/codegen/java/AbstractJavaCodegenTest.java
+++ modules/openapi-generator/src/test/java/org/openapitools/codegen/java/AbstractJavaCodegenTest.java
@@ -965,4 +965,11 @@ public class AbstractJavaCodegenTest {
         Assert.assertEquals(codegen.removeAnnotations("List<@Valid Pet>"), "List<Pet>");
     }
 
+    @Test(description = "test generated example values for string properties")
+    public void testGeneratedExampleValues() {
+        final OpenAPI openAPI = FLATTENED_SPEC.get("3_0/spring/date-time-parameter-types-for-testing");
+        codegen.setOpenAPI(openAPI);
+        DateSchema dateSchema = (DateSchema) openAPI.getPaths().get("/thingy/{date}").getPost().getParameters().get(0).getSchema();
+        Assert.assertTrue(codegen.escapeQuotationMark(codegen.toExampleValue(dateSchema)).matches("2021-01-01"));
+    }
 }
