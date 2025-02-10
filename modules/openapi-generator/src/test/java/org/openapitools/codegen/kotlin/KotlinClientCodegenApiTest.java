d8bdbf9e8d7dd2bca84ddb059f187b8fe7742877
lukasz.suski <lukasz.suski@finanteq.com>
2025-02-10 10:54:57 +0100

    Add useResponseAsReturnType option to kotlin, jvm-retrofit2 and coroutines api template (#15491)

--- modules/openapi-generator/src/test/java/org/openapitools/codegen/kotlin/KotlinClientCodegenApiTest.java
+++ modules/openapi-generator/src/test/java/org/openapitools/codegen/kotlin/KotlinClientCodegenApiTest.java
@@ -3,8 +3,7 @@ package org.openapitools.codegen.kotlin;
 import io.swagger.parser.OpenAPIParser;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.parser.core.models.ParseOptions;
-import lombok.Getter;
-import org.jetbrains.kotlin.com.intellij.openapi.util.text.Strings;
+import org.jetbrains.annotations.NotNull;
 import org.openapitools.codegen.ClientOptInput;
 import org.openapitools.codegen.CodegenConstants;
 import org.openapitools.codegen.DefaultGenerator;
@@ -17,6 +16,7 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.List;
 
 import static org.openapitools.codegen.TestUtils.assertFileContains;
 
@@ -39,29 +39,71 @@ public class KotlinClientCodegenApiTest {
     @Test(dataProvider = "clientLibraries")
     void testPathVariableIsNotEscaped_19930(ClientLibrary library) throws IOException {
 
-        OpenAPI openAPI = new OpenAPIParser()
-                .readLocation("src/test/resources/3_0/kotlin/issue19930-path-escaping.json", null, new ParseOptions()).getOpenAPI();
+        OpenAPI openAPI = readOpenAPI("src/test/resources/3_0/kotlin/issue19930-path-escaping.json");
 
         KotlinClientCodegen codegen = createCodegen(library);
 
         String outputPath = codegen.getOutputDir().replace('\\', '/');
-        ClientOptInput input = new ClientOptInput();
-        input.openAPI(openAPI);
-        input.config(codegen);
+        ClientOptInput input = createClientOptInput(openAPI, codegen);
 
         DefaultGenerator generator = new DefaultGenerator();
 
-        generator.setGeneratorPropertyDefault(CodegenConstants.MODELS, "false");
-        generator.setGeneratorPropertyDefault(CodegenConstants.MODEL_TESTS, "false");
-        generator.setGeneratorPropertyDefault(CodegenConstants.MODEL_DOCS, "false");
-        generator.setGeneratorPropertyDefault(CodegenConstants.APIS, "true");
-        generator.setGeneratorPropertyDefault(CodegenConstants.SUPPORTING_FILES, "false");
+        enableOnlyApiGeneration(generator);
 
         generator.opts(input).generate();
 
         System.out.println(outputPath);
 
         assertFileContains(Paths.get(outputPath + "/src/" + library.getSourceRoot() + "/org/openapitools/client/apis/ArticleApi.kt"), "article('{Id}')");
+    }
+
+    @DataProvider(name = "useResponseAsReturnType")
+    public static Object[][] useResponseAsReturnTypeTestData() {
+        return new Object[][]{{null, "Response<Pet>"}, {true, "Response<Pet>"}, {false, "Pet"}, {"false", "Pet"}}; // null is default
+    }
+
+    @Test(dataProvider = "useResponseAsReturnType")
+    public void testUseResponseAsReturnType(Object useResponseAsReturnType, String expectedResponse) throws IOException {
+        OpenAPI openAPI = readOpenAPI("3_0/kotlin/petstore.yaml");
+
+        KotlinClientCodegen codegen = createCodegen(ClientLibrary.JVM_RETROFIT2);
+        codegen.additionalProperties().put(KotlinClientCodegen.USE_COROUTINES, "true");
+        if (useResponseAsReturnType != null) {
+            codegen.additionalProperties().put(KotlinClientCodegen.USE_RESPONSE_AS_RETURN_TYPE, useResponseAsReturnType);
+        }
+
+        ClientOptInput input = createClientOptInput(openAPI, codegen);
+
+        DefaultGenerator generator = new DefaultGenerator();
+
+        enableOnlyApiGeneration(generator);
+
+        List<File> files = generator.opts(input).generate();
+        File petApi = files.stream().filter(file -> file.getName().equals("PetApi.kt")).findAny().orElseThrow();
+        assertFileContains(petApi.toPath(), "suspend fun addPet(@Body pet: Pet): " + expectedResponse);
+    }
+
+    private static void enableOnlyApiGeneration(DefaultGenerator generator) {
+        generator.setGeneratorPropertyDefault(CodegenConstants.MODELS, "false");
+        generator.setGeneratorPropertyDefault(CodegenConstants.MODEL_TESTS, "false");
+        generator.setGeneratorPropertyDefault(CodegenConstants.MODEL_DOCS, "false");
+        generator.setGeneratorPropertyDefault(CodegenConstants.APIS, "true");
+        generator.setGeneratorPropertyDefault(CodegenConstants.SUPPORTING_FILES, "false");
+        generator.setGeneratorPropertyDefault(CodegenConstants.API_TESTS, "false");
+        generator.setGeneratorPropertyDefault(CodegenConstants.API_DOCS, "false");
+    }
+
+    @NotNull
+    private static ClientOptInput createClientOptInput(OpenAPI openAPI, KotlinClientCodegen codegen) {
+        ClientOptInput input = new ClientOptInput();
+        input.openAPI(openAPI);
+        input.config(codegen);
+        return input;
+    }
+
+    private static OpenAPI readOpenAPI(String url) {
+        return new OpenAPIParser()
+                .readLocation(url, null, new ParseOptions()).getOpenAPI();
     }
 
     private KotlinClientCodegen createCodegen(ClientLibrary library) throws IOException {
