3a8e9301d2fb9920b0ae0ac9392ed1d31e601bea
Ron Reynolds <ron@ronreynolds.com>
2025-02-18 07:19:45 -0800

    added missing import of Configuration to api.mustache (and, thus, all native samples)

--- samples/client/petstore/java/native-jakarta/src/main/java/org/openapitools/client/api/PetApi.java
+++ samples/client/petstore/java/native-jakarta/src/main/java/org/openapitools/client/api/PetApi.java
@@ -15,6 +15,7 @@ package org.openapitools.client.api;
 import org.openapitools.client.ApiClient;
 import org.openapitools.client.ApiException;
 import org.openapitools.client.ApiResponse;
+import org.openapitools.client.Configuration;
 import org.openapitools.client.Pair;
 
 import java.io.File;
