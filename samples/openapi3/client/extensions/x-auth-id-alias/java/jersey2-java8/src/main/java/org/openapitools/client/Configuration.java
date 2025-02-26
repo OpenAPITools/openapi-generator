718523d9aaa568543dd2306294ea3a1705057887
Ron Reynolds <ron@ronreynolds.com>
2025-02-26 04:41:48 -0800

    fresh samples

--- samples/openapi3/client/extensions/x-auth-id-alias/java/jersey2-java8/src/main/java/org/openapitools/client/Configuration.java
+++ samples/openapi3/client/extensions/x-auth-id-alias/java/jersey2-java8/src/main/java/org/openapitools/client/Configuration.java
@@ -17,15 +17,6 @@ import java.util.Objects;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.Supplier;
 
-/**
- * modified version of the java-native version with planned enhancements:
- * <ul>
- *   <li>private ctor (Effective Java item 4)</li>
- *   <li>thread-safe storage of ApIClient ref</li>
- *   <li>lazy eval of ApiClient</li>
- *   <li>configurable callback to create ApiClient on demand</li>
- * </ul>
- */
 @javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", comments = "Generator version: 7.12.0-SNAPSHOT")
 public class Configuration {
   public static final String VERSION = "1.0.0";
