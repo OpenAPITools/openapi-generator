24b48a044b5f7000188028e4279a93c7e7937323
William Cheng <wing328hk@gmail.com>
2025-02-28 12:30:27 +0800

    update samples

--- samples/client/petstore/java/native-async/src/main/java/org/openapitools/client/api/StoreApi.java
+++ samples/client/petstore/java/native-async/src/main/java/org/openapitools/client/api/StoreApi.java
@@ -53,7 +53,7 @@ import java.util.function.Consumer;
 
 import java.util.concurrent.CompletableFuture;
 
-@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", comments = "Generator version: 7.12.0-SNAPSHOT")
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", comments = "Generator version: 7.13.0-SNAPSHOT")
 public class StoreApi {
   private final HttpClient memberVarHttpClient;
   private final ObjectMapper memberVarObjectMapper;
