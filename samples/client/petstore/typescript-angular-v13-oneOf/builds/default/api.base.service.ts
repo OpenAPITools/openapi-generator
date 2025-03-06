ce0c0dcab0e3a9fc04ef12f10652ee93ceefb045
Daniel Schreiber <daniel-schreiber@gmx.de>
2025-03-06 17:05:20 +0100

    [typescript-angular] fix: update basePath initialization to use template variable (#20816)
    fixes #20760

--- samples/client/petstore/typescript-angular-v13-oneOf/builds/default/api.base.service.ts
+++ samples/client/petstore/typescript-angular-v13-oneOf/builds/default/api.base.service.ts
@@ -3,7 +3,7 @@ import { CustomHttpParameterCodec } from './encoder';
 import { Configuration } from './configuration';
 
 export class BaseService {
-    protected basePath = '';
+    protected basePath = 'http://localhost';
     public defaultHeaders = new HttpHeaders();
     public configuration: Configuration;
     public encoder: HttpParameterCodec;
