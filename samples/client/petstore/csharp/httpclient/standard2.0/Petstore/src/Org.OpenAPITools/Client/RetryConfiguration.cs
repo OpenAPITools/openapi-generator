e5c8584f83bc305832d060dcb900a21f725b4326
Jacob Ilsø <jacobilsoe@gmail.com>
2025-03-07 09:13:10 +0100

    Use interfaces for Polly retry policies
    This enables use of ResiliencePipelineBuilder which returns the
    interface when calling AsSyncPolicy / AsAsyncPolicy

--- samples/client/petstore/csharp/httpclient/standard2.0/Petstore/src/Org.OpenAPITools/Client/RetryConfiguration.cs
+++ samples/client/petstore/csharp/httpclient/standard2.0/Petstore/src/Org.OpenAPITools/Client/RetryConfiguration.cs
@@ -21,11 +21,11 @@ namespace Org.OpenAPITools.Client
         /// <summary>
         /// Retry policy
         /// </summary>
-        public static Policy<HttpResponseMessage> RetryPolicy { get; set; }
+        public static ISyncPolicy<HttpResponseMessage> RetryPolicy { get; set; }
 
         /// <summary>
         /// Async retry policy
         /// </summary>
-        public static AsyncPolicy<HttpResponseMessage> AsyncRetryPolicy { get; set; }
+        public static IAsyncPolicy<HttpResponseMessage> AsyncRetryPolicy { get; set; }
     }
 }
