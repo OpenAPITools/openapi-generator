2eaa93731c354c9cb64a41187ba25a1b271cfdea
Juan Treminio <jtreminio@gmail.com>
2025-03-14 17:01:49 -0500

    Output of CLI commands per PR comments

--- samples/client/others/typescript/builds/enum-single-value/types/ObservableAPI.ts
+++ samples/client/others/typescript/builds/enum-single-value/types/ObservableAPI.ts
@@ -3,5 +3,5 @@ import { Configuration, ConfigurationOptions } from '../configuration'
 import type { Middleware } from '../middleware';
 import { Observable, of, from } from '../rxjsStub';
 import {mergeMap, map} from  '../rxjsStub';
-import { SingleValueEnum30 } from '../models/SingleValueEnum30';
-import { SingleValueEnum31 } from '../models/SingleValueEnum31';
+import { SomeObject } from '../models/SomeObject';
+import { WithNullableType } from '../models/WithNullableType';
