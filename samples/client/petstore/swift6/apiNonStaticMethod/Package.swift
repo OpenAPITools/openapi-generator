937924e6192a255e851f85709d7be7add64c8175
Bruno Coelho <4brunu@gmail.com>
2025-03-17 20:53:34 +0000

    [Swift][client] make alamofire version more flexible

--- samples/client/petstore/swift6/apiNonStaticMethod/Package.swift
+++ samples/client/petstore/swift6/apiNonStaticMethod/Package.swift
@@ -19,7 +19,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/Alamofire/Alamofire", exact: "5.10.2"),
+        .package(url: "https://github.com/Alamofire/Alamofire", .upToNextMajor(from: "5.10.2")),
         .package(url: "https://github.com/mxcl/PromiseKit", .upToNextMajor(from: "8.1.2")),
         .package(url: "https://github.com/ReactiveX/RxSwift", .upToNextMajor(from: "6.8.0")),
     ],
