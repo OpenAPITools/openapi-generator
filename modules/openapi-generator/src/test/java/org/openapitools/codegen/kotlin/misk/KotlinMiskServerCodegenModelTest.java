c815f17252a5f5463788e4b8bda0c40e6e7c8b80
Guido Arnau <guido@squareup.com>
2025-03-19 13:35:20 +0100

    Adds unit tests

--- modules/openapi-generator/src/test/java/org/openapitools/codegen/kotlin/misk/KotlinMiskServerCodegenModelTest.java
+++ modules/openapi-generator/src/test/java/org/openapitools/codegen/kotlin/misk/KotlinMiskServerCodegenModelTest.java
@@ -1,31 +1,44 @@
 package org.openapitools.codegen.kotlin.misk;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.List;
 import org.openapitools.codegen.*;
 import org.openapitools.codegen.languages.KotlinMiskServerCodegen;
-import io.swagger.models.*;
-import io.swagger.models.properties.*;
 
 import org.testng.Assert;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 
 @SuppressWarnings("static-method")
 public class KotlinMiskServerCodegenModelTest {
 
     @Test(description = "convert a simple java model")
-    @Ignore
-    public void simpleModelTest() {
-        final Model model = new ModelImpl()
-                .description("a sample model")
-                .property("id", new LongProperty())
-                .property("name", new StringProperty())
-                .required("id")
-                .required("name");
-        final DefaultCodegen codegen = new KotlinMiskServerCodegen();
+    public void simpleModelTest() throws IOException {
+        File output = Files.createTempDirectory("test").toFile().getCanonicalFile();
+        output.deleteOnExit();
 
-        // TODO: Complete this test.
-        Assert.fail("Not implemented.");
+        OpenAPI openAPI = TestUtils.parseFlattenSpec("src/test/resources/3_0/petstore.yaml");
+        KotlinMiskServerCodegen codegen = new KotlinMiskServerCodegen();
+        codegen.setOutputDir(output.getAbsolutePath());
+
+        ClientOptInput input = new ClientOptInput();
+        input.openAPI(openAPI);
+        input.config(codegen);
+
+        DefaultGenerator generator = new DefaultGenerator();
+        generator.setGeneratorPropertyDefault(CodegenConstants.MODELS, "true");
+        generator.setGeneratorPropertyDefault(CodegenConstants.MODEL_TESTS, "false");
+        generator.setGeneratorPropertyDefault(CodegenConstants.MODEL_DOCS, "false");
+        generator.setGeneratorPropertyDefault(CodegenConstants.APIS, "false");
+        generator.setGeneratorPropertyDefault(CodegenConstants.SUPPORTING_FILES, "false");
+
+        List<File> files = generator.opts(input).generate();
+
+        Assert.assertTrue(files.stream()
+            .filter(file -> file.getPath().contains("model"))
+            .anyMatch(file -> file.getName().endsWith(".kt")));
     }
-
 }
 
