16552755bffa4f1a4d7e1afc67cf7e97dc1cd1f4
Daniel Schreiber <daniel-schreiber@gmx.de>
2025-03-24 17:46:04 +0100

    feat(typescript-angular): update enum definitions to use 'as const' for improved type safety (#20958)
    fixes #20809

--- modules/openapi-generator/src/test/java/org/openapitools/codegen/typescript/typescriptangular/TypeScriptAngularClientCodegenTest.java
+++ modules/openapi-generator/src/test/java/org/openapitools/codegen/typescript/typescriptangular/TypeScriptAngularClientCodegenTest.java
@@ -439,4 +439,29 @@ public class TypeScriptAngularClientCodegenTest {
         final String fileContents = Files.readString(Paths.get(output + "/api.base.service.ts"));
         assertThat(fileContents).containsOnlyOnce("basePath = '/relative/url'");
     }
+
+    @Test
+    public void testEnumAsConst() throws IOException {
+        // GIVEN
+        final String specPath = "src/test/resources/3_0/enum.yaml";
+
+        File output = Files.createTempDirectory("test").toFile();
+        output.deleteOnExit();
+
+        // WHEN
+        final CodegenConfigurator configurator = new CodegenConfigurator()
+            .setGeneratorName("typescript-angular")
+            .setInputSpec(specPath)
+            .setOutputDir(output.getAbsolutePath().replace("\\", "/"));
+
+        final ClientOptInput clientOptInput = configurator.toClientOptInput();
+
+        Generator generator = new DefaultGenerator();
+        generator.opts(clientOptInput).generate();
+
+        // THEN
+        final String fileContents = Files.readString(Paths.get(output + "/model/type.ts"));
+        assertThat(fileContents).containsOnlyOnce("} as const;");
+        assertThat(fileContents).doesNotContain(" as Type");
+    }
 }
