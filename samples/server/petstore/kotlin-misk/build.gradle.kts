413db651936317236b7c02ad2fe989b8e35b7100
andrewwilsonnew <andrewwilson@squareup.com>
2025-03-24 10:14:44 +0000

    fix pom version

--- samples/server/petstore/kotlin-misk/build.gradle.kts
+++ samples/server/petstore/kotlin-misk/build.gradle.kts
@@ -9,7 +9,7 @@ version = "1.0.0-SNAPSHOT"
 
 dependencies {
     implementation("jakarta.validation:jakarta.validation-api:3.1.1")
-    implementation("com.squareup.misk:misk:2025.02.11.123913-8a41324")
+    implementation("com.squareup.misk:misk:2025.03.17.160337-2c6953c")
     //implementation("com.squareup.wire:wire-runtime:5.2.1")
 
     testImplementation("com.squareup.misk:misk-testing:2025.02.11.123913-8a41324")
