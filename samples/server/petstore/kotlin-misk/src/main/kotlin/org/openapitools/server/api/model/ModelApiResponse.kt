42aaaef2520301afd8bedb4517de6c13aef0af65
andrewwilsonnew <andrewwilson@squareup.com>
2025-04-03 12:17:08 +0100

    removing old files

--- samples/server/petstore/kotlin-misk/src/main/kotlin/org/openapitools/server/api/model/ModelApiResponse.kt
+++
@@ -1,8 +0,0 @@
-package org.openapitools.server.api.model
-
-
-data class ModelApiResponse(
-    val code: kotlin.Int? = null,
-    val type: kotlin.String? = null,
-    val message: kotlin.String? = null
-)
--- samples/server/petstore/kotlin-misk/src/main/kotlin/org/openapitools/server/api/model/Tag.kt
+++
@@ -1,7 +0,0 @@
-package org.openapitools.server.api.model
-
-
-data class Tag(
-    val id: kotlin.Long? = null,
-    val name: kotlin.String? = null
-)
