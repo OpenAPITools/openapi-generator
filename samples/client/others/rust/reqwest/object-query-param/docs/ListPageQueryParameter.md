7b9c9b46eab8d29e81454790b42c4e13569210a0
Elric Milon <git@whirm.eu>
2025-04-09 15:32:14 +0200

    [Rust] Update samples

---
+++ samples/client/others/rust/reqwest/object-query-param/docs/ListPageQueryParameter.md
@@ -0,0 +1,12 @@
+# ListPageQueryParameter
+
+## Properties
+
+Name | Type | Description | Notes
+------------ | ------------- | ------------- | -------------
+**offset** | Option<**i32**> |  | [optional]
+**limit** | Option<**i32**> |  | [optional]
+
+[[Back to Model list]](../README.md#documentation-for-models) [[Back to API list]](../README.md#documentation-for-api-endpoints) [[Back to README]](../README.md)
+
+
--- samples/client/petstore/rust/reqwest/petstore-awsv4signature/src/apis/pet_api.rs
+++ samples/client/petstore/rust/reqwest/petstore-awsv4signature/src/apis/pet_api.rs
@@ -426,7 +426,8 @@ pub fn pets_post(configuration: &configuration::Configuration, page: Option<mode
     let mut req_builder = configuration.client.request(reqwest::Method::POST, &uri_str);
 
     if let Some(ref param_value) = p_query_page {
-        req_builder = req_builder.query(&[("page", &serde_json::to_string(param_value)?)]);
+        let params = crate::apis::parse_flat_object(&serde_json::to_value(param_value)?);
+        req_builder = req_builder.query(&params);
     }
     if let Some(ref user_agent) = configuration.user_agent {
         req_builder = req_builder.header(reqwest::header::USER_AGENT, user_agent.clone());
