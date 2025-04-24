jasonxu08/openapi-generator-obsolete

[Swift6] Remove QueryStringEncodable for models

samples/client/petstore/swift6/alamofireLibrary/Sources/PetstoreClient/Models/EnumTest.swift
@@ -7,23 +7,23 @@
 
 import Foundation
 
-public struct EnumTest: Sendable, Codable, QueryStringEncodable, Hashable {
+public struct EnumTest: Sendable, Codable, Hashable {
 
-    public enum EnumString: String, Sendable, Codable, CaseIterable {
+    public enum EnumString: String, Sendable, QueryStringEncodable, Codable, CaseIterable {
         case upper = "UPPER"
         case lower = "lower"
         case empty = ""
     }
-    public enum EnumStringRequired: String, Sendable, Codable, CaseIterable {
+    public enum EnumStringRequired: String, Sendable, QueryStringEncodable, Codable, CaseIterable {
         case upper = "UPPER"
         case lower = "lower"
         case empty = ""
     }
-    public enum EnumInteger: Int, Sendable, Codable, CaseIterable {
+    public enum EnumInteger: Int, Sendable, QueryStringEncodable, Codable, CaseIterable {
         case _1 = 1
         case number1 = -1
     }
-    public enum EnumNumber: Double, Sendable, Codable, CaseIterable {
+    public enum EnumNumber: Double, Sendable, QueryStringEncodable, Codable, CaseIterable {
         case _11 = 1.1
         case number12 = -1.2
     }

samples/client/petstore/swift6/objcCompatible/Sources/PetstoreClient/Models/Order.swift
@@ -7,9 +7,9 @@
 
 import Foundation
 
-@objcMembers public class Order: NSObject, Codable, QueryStringEncodable {
+@objcMembers public class Order: NSObject, Codable {
 
-    public enum Status: String, Sendable, Codable, CaseIterable {
+    public enum Status: String, Sendable, QueryStringEncodable, Codable, CaseIterable {
         case placed = "placed"
         case approved = "approved"
         case delivered = "delivered"

samples/client/petstore/swift6/resultLibrary/PetstoreClient/Classes/OpenAPIs/Models/Order.swift
@@ -7,9 +7,9 @@
 
 import Foundation
 
-internal struct Order: Sendable, Codable, QueryStringEncodable {
+internal struct Order: Sendable, Codable {
 
-    internal enum Status: String, Sendable, Codable, CaseIterable, CaseIterableDefaultsLast {
+    internal enum Status: String, Sendable, QueryStringEncodable, Codable, CaseIterable, CaseIterableDefaultsLast {
         case placed = "placed"
         case approved = "approved"
         case delivered = "delivered"

samples/client/petstore/swift6/resultLibrary/PetstoreClient/Classes/OpenAPIs/Models/OuterEnum.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-internal enum OuterEnum: String, Sendable, Codable, CaseIterable, CaseIterableDefaultsLast {
+internal enum OuterEnum: String, Sendable, QueryStringEncodable, Codable, CaseIterable, CaseIterableDefaultsLast {
     case placed = "placed"
     case approved = "approved"
     case delivered = "delivered"

samples/client/petstore/swift6/resultLibrary/PetstoreClient/Classes/OpenAPIs/Models/Pet.swift
@@ -7,9 +7,9 @@
 
 import Foundation
 
-internal struct Pet: Sendable, Codable, QueryStringEncodable, Hashable {
+internal struct Pet: Sendable, Codable, Hashable {
 
-    internal enum Status: String, Sendable, Codable, CaseIterable, CaseIterableDefaultsLast {
+    internal enum Status: String, Sendable, QueryStringEncodable, Codable, CaseIterable, CaseIterableDefaultsLast {
         case available = "available"
         case pending = "pending"
         case sold = "sold"

samples/client/petstore/swift6/urlsessionLibrary/Sources/PetstoreClient/Models/EnumTest.swift
@@ -12,23 +12,23 @@ public typealias EnumTest = PetstoreClientAPI.EnumTest
 
 extension PetstoreClientAPI {
 
-public final class EnumTest: @unchecked Sendable, Codable, QueryStringEncodable, Hashable {
+public final class EnumTest: @unchecked Sendable, Codable, Hashable {
 
-    public enum EnumString: String, Sendable, Codable, CaseIterable {
+    public enum EnumString: String, Sendable, QueryStringEncodable, Codable, CaseIterable {
         case upper = "UPPER"
         case lower = "lower"
         case empty = ""
     }
-    public enum EnumStringRequired: String, Sendable, Codable, CaseIterable {
+    public enum EnumStringRequired: String, Sendable, QueryStringEncodable, Codable, CaseIterable {
         case upper = "UPPER"
         case lower = "lower"
         case empty = ""
     }
-    public enum EnumInteger: Int, Sendable, Codable, CaseIterable {
+    public enum EnumInteger: Int, Sendable, QueryStringEncodable, Codable, CaseIterable {
         case _1 = 1
         case number1 = -1
     }
-    public enum EnumNumber: Double, Sendable, Codable, CaseIterable {
+    public enum EnumNumber: Double, Sendable, QueryStringEncodable, Codable, CaseIterable {
         case _11 = 1.1
         case number12 = -1.2
     }

samples/client/petstore/swift6/vaporLibrary/Sources/PetstoreClient/Models/OuterEnum.swift
@@ -8,7 +8,7 @@
 import Foundation
 import Vapor
 
-public enum OuterEnum: String, Sendable, Content, Hashable, CaseIterable {
+public enum OuterEnum: String, Sendable, QueryStringEncodable, Content, Hashable, CaseIterable {
     case placed = "placed"
     case approved = "approved"
     case delivered = "delivered"

OpenAPITools/openapi-generator

samples/client/petstore/swift6/alamofireLibrary/Sources/PetstoreClient/Models/EnumTest.swift
@@ -7,23 +7,23 @@
 
 import Foundation
 
-public struct EnumTest: Sendable, Codable, QueryStringEncodable, Hashable {
+public struct EnumTest: Sendable, Codable, Hashable {
 
-    public enum EnumString: String, Sendable, Codable, CaseIterable {
+    public enum EnumString: String, Sendable, CustomQueryStringEncodable, Codable, CaseIterable {
         case upper = "UPPER"
         case lower = "lower"
         case empty = ""
     }
-    public enum EnumStringRequired: String, Sendable, Codable, CaseIterable {
+    public enum EnumStringRequired: String, Sendable, CustomQueryStringEncodable, Codable, CaseIterable {
         case upper = "UPPER"
         case lower = "lower"
         case empty = ""
     }
-    public enum EnumInteger: Int, Sendable, Codable, CaseIterable {
+    public enum EnumInteger: Int, Sendable, CustomQueryStringEncodable, Codable, CaseIterable {
         case _1 = 1
         case number1 = -1
     }
-    public enum EnumNumber: Double, Sendable, Codable, CaseIterable {
+    public enum EnumNumber: Double, Sendable, CustomQueryStringEncodable, Codable, CaseIterable {
         case _11 = 1.1
         case number12 = -1.2
     }

samples/client/petstore/swift6/objcCompatible/Sources/PetstoreClient/Models/Order.swift
@@ -7,9 +7,9 @@
 
 import Foundation
 
-@objcMembers public class Order: NSObject, Codable, QueryStringEncodable {
+@objcMembers public class Order: NSObject, Codable {
 
-    public enum Status: String, Sendable, Codable, CaseIterable {
+    public enum Status: String, Sendable, CustomQueryStringEncodable, Codable, CaseIterable {
         case placed = "placed"
         case approved = "approved"
         case delivered = "delivered"

samples/client/petstore/swift6/resultLibrary/PetstoreClient/Classes/OpenAPIs/Models/Order.swift
@@ -7,9 +7,9 @@
 
 import Foundation
 
-internal struct Order: Sendable, Codable, QueryStringEncodable {
+internal struct Order: Sendable, Codable {
 
-    internal enum Status: String, Sendable, Codable, CaseIterable, CaseIterableDefaultsLast {
+    internal enum Status: String, Sendable, CustomQueryStringEncodable, Codable, CaseIterable, CaseIterableDefaultsLast {
         case placed = "placed"
         case approved = "approved"
         case delivered = "delivered"

samples/client/petstore/swift6/resultLibrary/PetstoreClient/Classes/OpenAPIs/Models/OuterEnum.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-internal enum OuterEnum: String, Sendable, Codable, CaseIterable, CaseIterableDefaultsLast {
+internal enum OuterEnum: String, Sendable, CustomQueryStringEncodable, Codable, CaseIterable, CaseIterableDefaultsLast {
     case placed = "placed"
     case approved = "approved"
     case delivered = "delivered"

samples/client/petstore/swift6/resultLibrary/PetstoreClient/Classes/OpenAPIs/Models/Pet.swift
@@ -7,9 +7,9 @@
 
 import Foundation
 
-internal struct Pet: Sendable, Codable, QueryStringEncodable, Hashable {
+internal struct Pet: Sendable, Codable, Hashable {
 
-    internal enum Status: String, Sendable, Codable, CaseIterable, CaseIterableDefaultsLast {
+    internal enum Status: String, Sendable, CustomQueryStringEncodable, Codable, CaseIterable, CaseIterableDefaultsLast {
         case available = "available"
         case pending = "pending"
         case sold = "sold"

samples/client/petstore/swift6/urlsessionLibrary/Sources/PetstoreClient/Models/EnumTest.swift
@@ -12,23 +12,23 @@ public typealias EnumTest = PetstoreClientAPI.EnumTest
 
 extension PetstoreClientAPI {
 
-public final class EnumTest: @unchecked Sendable, Codable, QueryStringEncodable, Hashable {
+public final class EnumTest: @unchecked Sendable, Codable, Hashable {
 
-    public enum EnumString: String, Sendable, Codable, CaseIterable {
+    public enum EnumString: String, Sendable, CustomQueryStringEncodable, Codable, CaseIterable {
         case upper = "UPPER"
         case lower = "lower"
         case empty = ""
     }
-    public enum EnumStringRequired: String, Sendable, Codable, CaseIterable {
+    public enum EnumStringRequired: String, Sendable, CustomQueryStringEncodable, Codable, CaseIterable {
         case upper = "UPPER"
         case lower = "lower"
         case empty = ""
     }
-    public enum EnumInteger: Int, Sendable, Codable, CaseIterable {
+    public enum EnumInteger: Int, Sendable, CustomQueryStringEncodable, Codable, CaseIterable {
         case _1 = 1
         case number1 = -1
     }
-    public enum EnumNumber: Double, Sendable, Codable, CaseIterable {
+    public enum EnumNumber: Double, Sendable, CustomQueryStringEncodable, Codable, CaseIterable {
         case _11 = 1.1
         case number12 = -1.2
     }

samples/client/petstore/swift6/vaporLibrary/Sources/PetstoreClient/Models/OuterEnum.swift
@@ -8,7 +8,7 @@
 import Foundation
 import Vapor
 
-public enum OuterEnum: String, Sendable, Content, Hashable, CaseIterable {
+public enum OuterEnum: String, Sendable, CustomQueryStringEncodable, Content, Hashable, CaseIterable {
     case placed = "placed"
     case approved = "approved"
     case delivered = "delivered"