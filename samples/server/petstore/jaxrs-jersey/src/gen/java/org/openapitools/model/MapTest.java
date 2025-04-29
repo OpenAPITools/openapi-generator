134d5642bd09937ce18a89d383efce0460ff26fe
Eric Rolli <149762070+eric-rolli@users.noreply.github.com>
2025-04-29 16:46:06 +0200

    Update MapTest.java added getValue() to enum

--- samples/server/petstore/jaxrs-jersey/src/gen/java/org/openapitools/model/MapTest.java
+++ samples/server/petstore/jaxrs-jersey/src/gen/java/org/openapitools/model/MapTest.java
@@ -54,8 +54,12 @@ public class MapTest   {
       this.value = value;
     }
 
-    @Override
     @JsonValue
+    public String getValue() {
+      return value;
+    }
+
+    @Override
     public String toString() {
       return String.valueOf(value);
     }
