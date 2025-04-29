3319b922577afbf30faa11e7b260ffc783f19fae
Eric Rolli <149762070+eric-rolli@users.noreply.github.com>
2025-04-29 16:38:24 +0200

    Update Pet.java added getValue() to enum

--- samples/server/petstore/jaxrs-datelib-j8/src/gen/java/org/openapitools/model/Pet.java
+++ samples/server/petstore/jaxrs-datelib-j8/src/gen/java/org/openapitools/model/Pet.java
@@ -81,8 +81,12 @@ public class Pet  implements Serializable {
       this.value = value;
     }
 
-    @Override
     @JsonValue
+    public String getValue() {
+      return value;
+    }
+
+    @Override
     public String toString() {
       return String.valueOf(value);
     }
