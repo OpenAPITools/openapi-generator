f2c3f66db683c93881cee983d50f110f488998eb
Eric Rolli <149762070+eric-rolli@users.noreply.github.com>
2025-04-29 16:29:38 +0200

    Update EnumArrays.java added getValue() to enum

--- samples/server/petstore/jaxrs-datelib-j8/src/gen/java/org/openapitools/model/EnumArrays.java
+++ samples/server/petstore/jaxrs-datelib-j8/src/gen/java/org/openapitools/model/EnumArrays.java
@@ -89,8 +89,12 @@ public class EnumArrays  implements Serializable {
       this.value = value;
     }
 
-    @Override
     @JsonValue
+    public String getValue() {
+      return value;
+    }
+
+    @Override
     public String toString() {
       return String.valueOf(value);
     }
