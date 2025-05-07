b61e6039a4a3a97e7976854d8527a30992c71a42
DavidGrath <davidadeyyinka@gmail.com>
2025-05-07 08:29:23 +0100

    Changed Apple and Banana to let petstore-openapi3-jersey2-java8 tests pass

--- samples/client/petstore/java/jersey3/src/main/java/org/openapitools/client/model/Apple.java
+++ samples/client/petstore/java/jersey3/src/main/java/org/openapitools/client/model/Apple.java
@@ -15,10 +15,6 @@ package org.openapitools.client.model;
 
 import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
-import java.util.Map;
-import java.util.HashMap;
-import com.fasterxml.jackson.annotation.JsonAnyGetter;
-import com.fasterxml.jackson.annotation.JsonAnySetter;
 import java.util.Objects;
 import java.util.Map;
 import java.util.HashMap;
@@ -106,43 +102,6 @@ public class Apple {
     this.origin = origin;
   }
 
-  /**
-   * A container for additional, undeclared properties.
-   * This is a holder for any undeclared properties as specified with
-   * the 'additionalProperties' keyword in the OAS document.
-   */
-  private Map<String, Object> additionalProperties;
-
-  /**
-   * Set the additional (undeclared) property with the specified name and value.
-   * If the property does not already exist, create it otherwise replace it.
-   */
-  @JsonAnySetter
-  public Apple putAdditionalProperty(String key, Object value) {
-    if (this.additionalProperties == null) {
-        this.additionalProperties = new HashMap<>();
-    }
-    this.additionalProperties.put(key, value);
-    return this;
-  }
-
-  /**
-   * Return the additional (undeclared) property.
-   */
-  @JsonAnyGetter
-  public Map<String, Object> getAdditionalProperties() {
-    return additionalProperties;
-  }
-
-  /**
-   * Return the additional (undeclared) property with the specified name.
-   */
-  public Object getAdditionalProperty(String key) {
-    if (this.additionalProperties == null) {
-        return null;
-    }
-    return this.additionalProperties.get(key);
-  }
 
   /**
    * Return true if this apple object is equal to o.
@@ -163,7 +122,6 @@ public class Apple {
     sb.append("class Apple {\n");
     sb.append("    cultivar: ").append(toIndentedString(cultivar)).append("\n");
     sb.append("    origin: ").append(toIndentedString(origin)).append("\n");
-    sb.append("    additionalProperties: ").append(toIndentedString(additionalProperties)).append("\n");
     sb.append("}");
     return sb.toString();
   }
