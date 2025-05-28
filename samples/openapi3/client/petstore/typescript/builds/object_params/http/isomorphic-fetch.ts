ca24d2ae495d3ddb40e7903f709c4c47eec94a2c
David Gamero <david340804@gmail.com>
2025-05-28 12:36:40 -0400

    refresh samples

--- samples/openapi3/client/petstore/typescript/builds/object_params/http/isomorphic-fetch.ts
+++ samples/openapi3/client/petstore/typescript/builds/object_params/http/isomorphic-fetch.ts
@@ -1,7 +1,6 @@
 import {HttpLibrary, RequestContext, ResponseContext} from './http';
 import { from, Observable } from '../rxjsStub';
 import fetch from "node-fetch";
-import { AbortSignal } from "node-fetch/externals";
 
 export class IsomorphicFetchHttpLibrary implements HttpLibrary {
 
