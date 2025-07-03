839c00b5a23c8ee54aacc326b29ceb6fec5cfcad
Mattias-Sehlstedt <60173714+Mattias-Sehlstedt@users.noreply.github.com>
2025-07-03 14:32:56 +0200

    Add samples for when using the useSealed setting for Spring

---
+++ samples/openapi3/server/petstore/spring-boot-oneof-sealed/src/main/java/org/openapitools/model/FruitType.java
@@ -0,0 +1,56 @@
+package org.openapitools.model;
+
+import java.net.URI;
+import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonValue;
+import org.openapitools.jackson.nullable.JsonNullable;
+import java.time.OffsetDateTime;
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+
+import java.util.*;
+import javax.annotation.Generated;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+/**
+ * Gets or Sets FruitType
+ */
+
+@Generated(value = "org.openapitools.codegen.languages.SpringCodegen", comments = "Generator version: 7.15.0-SNAPSHOT")
+public enum FruitType {
+  
+  APPLE("APPLE"),
+  
+  BANANA("BANANA");
+
+  private final String value;
+
+  FruitType(String value) {
+    this.value = value;
+  }
+
+  @JsonValue
+  public String getValue() {
+    return value;
+  }
+
+  @Override
+  public String toString() {
+    return String.valueOf(value);
+  }
+
+  @JsonCreator
+  public static FruitType fromValue(String value) {
+    for (FruitType b : FruitType.values()) {
+      if (b.value.equals(value)) {
+        return b;
+      }
+    }
+    throw new IllegalArgumentException("Unexpected value '" + value + "'");
+  }
+}
+
