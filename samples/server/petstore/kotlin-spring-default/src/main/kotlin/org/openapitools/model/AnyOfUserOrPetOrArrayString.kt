0e97e19bbcfe8e5e2dbe03a4fd753c7f14860863
Stefan Wurzinger <swurzinger@gmx.at>
2025-07-28 10:01:03 +0200

    kotlin-spring: fix exception thrown in enum.forValue (#21622)
    * kotlin-spring: fix exception thrown in enum.forValue
    
    * update samples

--- samples/server/petstore/kotlin-spring-default/src/main/kotlin/org/openapitools/model/AnyOfUserOrPetOrArrayString.kt
+++ samples/server/petstore/kotlin-spring-default/src/main/kotlin/org/openapitools/model/AnyOfUserOrPetOrArrayString.kt
@@ -94,7 +94,8 @@ data class AnyOfUserOrPetOrArrayString(
             @JvmStatic
             @JsonCreator
             fun forValue(value: kotlin.String): Status {
-                return values().first{it -> it.value == value}
+                return values().firstOrNull{it -> it.value == value}
+                    ?: throw IllegalArgumentException("Unexpected value '$value' for enum 'AnyOfUserOrPetOrArrayString'")
             }
         }
     }
