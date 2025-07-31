dba7aee41e59eaf06ec64aec6d7fa70d93833652
some00 <some00@users.noreply.github.com>
2025-07-31 10:29:39 +0200

    Kotlinx polymorphism with custom discriminator support (#21531)
    * kotlinx serialization fixes
    
    - added new config with kotlinx, discriminator (/w custom name) and
      kotlinx_serialization
    - remove discriminator properties from the generator in both base and
      derived classes
    - set discriminatorValue in additionalProperties of derived classes
    - add JsonClassDiscriminator the derived classes in the template
    - set SerialName to discriminatorValue in the template
    - change base classes to sealed class instead of interface
    - make variables in base classes abstract
    
    * Generated kotlin-allOff-discriminator-kotlinx-serialization sample
    
    * Added test for kotlinx_serialization with discriminator
    
    * renamed yaml
    
    * Added new sample to github workflow
    
    * Added comments to KotlinClientCodegen::postProcessAllModels

---
+++ samples/client/petstore/kotlin-allOff-discriminator-kotlinx-serialization/docs/Animal.md
@@ -0,0 +1,11 @@
+
+# Animal
+
+## Properties
+| Name | Type | Description | Notes |
+| ------------ | ------------- | ------------- | ------------- |
+| **id** | [**java.util.UUID**](java.util.UUID.md) |  |  |
+| **optionalProperty** | [**java.math.BigDecimal**](java.math.BigDecimal.md) |  |  [optional] |
+
+
+
---
+++ samples/client/petstore/kotlin-allOff-discriminator-kotlinx-serialization/src/main/kotlin/org/openapitools/client/infrastructure/BigDecimalAdapter.kt
@@ -0,0 +1,15 @@
+package org.openapitools.client.infrastructure
+
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
+import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
+import kotlinx.serialization.descriptors.PrimitiveKind
+import kotlinx.serialization.descriptors.SerialDescriptor
+import java.math.BigDecimal
+
+object BigDecimalAdapter : KSerializer<BigDecimal> {
+    override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor("BigDecimal", PrimitiveKind.STRING)
+    override fun deserialize(decoder: Decoder): BigDecimal = BigDecimal(decoder.decodeString())
+    override fun serialize(encoder: Encoder, value: BigDecimal) = encoder.encodeString(value.toPlainString())
+}
