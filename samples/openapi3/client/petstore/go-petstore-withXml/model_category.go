bdfbb32cddc1fcbaefdb16da31fd58b0729e77f2
William Cheng <wing328hk@gmail.com>
2025-09-08 16:45:46 +0800

    Improve Go client tests (#21919)
    * test withXml option
    
    * fix path
    
    * update go code (withXml: true)
    
    * test one more go client

--- samples/openapi3/client/petstore/go-petstore-withXml/model_category.go
+++ samples/openapi3/client/petstore/go-petstore-withXml/model_category.go
@@ -19,8 +19,8 @@ var _ MappedNullable = &Category{}
 
 // Category A category for a pet
 type Category struct {
-	Id *int64 `json:"id,omitempty"`
-	Name *string `json:"name,omitempty" validate:"regexp=^[a-zA-Z0-9]+[a-zA-Z0-9\\\\.\\\\-_]*[a-zA-Z0-9]+$"`
+	Id *int64 `json:"id,omitempty" xml:"id"`
+	Name *string `json:"name,omitempty" xml:"name" validate:"regexp=^[a-zA-Z0-9]+[a-zA-Z0-9\\\\.\\\\-_]*[a-zA-Z0-9]+$"`
 	AdditionalProperties map[string]interface{}
 }
 
