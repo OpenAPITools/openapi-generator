a5e9b43507483f9c0b81866bf57146125ddf9c66
William Cheng <wing328hk@gmail.com>
2025-09-24 17:01:37 +0800

    add test for jaxrs cxf swagger2 client

---
+++ samples/client/petstore/jaxrs-cxf-client-swagger2/src/gen/java/org/openapitools/model/ModelApiResponse.java
@@ -0,0 +1,122 @@
+package org.openapitools.model;
+
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/**
+ * Describes the result of uploading an image resource
+ */
+@Schema(description="Describes the result of uploading an image resource")
+
+public class ModelApiResponse  {
+  
+  @Schema(description = "")
+
+  private Integer code;
+
+  @Schema(description = "")
+
+  private String type;
+
+  @Schema(description = "")
+
+  private String message;
+ /**
+   * Get code
+   * @return code
+  **/
+  @JsonProperty("code")
+  public Integer getCode() {
+    return code;
+  }
+
+  public void setCode(Integer code) {
+    this.code = code;
+  }
+
+  public ModelApiResponse code(Integer code) {
+    this.code = code;
+    return this;
+  }
+
+ /**
+   * Get type
+   * @return type
+  **/
+  @JsonProperty("type")
+  public String getType() {
+    return type;
+  }
+
+  public void setType(String type) {
+    this.type = type;
+  }
+
+  public ModelApiResponse type(String type) {
+    this.type = type;
+    return this;
+  }
+
+ /**
+   * Get message
+   * @return message
+  **/
+  @JsonProperty("message")
+  public String getMessage() {
+    return message;
+  }
+
+  public void setMessage(String message) {
+    this.message = message;
+  }
+
+  public ModelApiResponse message(String message) {
+    this.message = message;
+    return this;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    ModelApiResponse _apiResponse = (ModelApiResponse) o;
+    return Objects.equals(this.code, _apiResponse.code) &&
+        Objects.equals(this.type, _apiResponse.type) &&
+        Objects.equals(this.message, _apiResponse.message);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(code, type, message);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class ModelApiResponse {\n");
+    
+    sb.append("    code: ").append(toIndentedString(code)).append("\n");
+    sb.append("    type: ").append(toIndentedString(type)).append("\n");
+    sb.append("    message: ").append(toIndentedString(message)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private static String toIndentedString(Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
+
