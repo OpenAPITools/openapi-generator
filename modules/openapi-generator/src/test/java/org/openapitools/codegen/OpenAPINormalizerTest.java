4352a2fcd06404f6d340c8c8628fa295b1c89cbe
Mattias Sehlstedt <60173714+Mattias-Sehlstedt@users.noreply.github.com>
2025-10-06 07:07:50 +0200

    [Test] [Java] Add test example of how to handle x-parent without REF_AS_PARENT_IN_ALLOF normalization (#22058)
    * Add test to illustrate example of how to handle x-parent without REF_AS_PARENT_IN_ALLOF normalization
    
    * Minor change to retrigger build

--- modules/openapi-generator/src/test/java/org/openapitools/codegen/OpenAPINormalizerTest.java
+++ modules/openapi-generator/src/test/java/org/openapitools/codegen/OpenAPINormalizerTest.java
@@ -66,7 +66,7 @@ public class OpenAPINormalizerTest {
         assertEquals(schema5.getExtensions().get(X_PARENT), "abstract");
 
         // Verify that all allOf refs gets marked as parents
-        Schema<?>schemaWithTwoParents = openAPI.getComponents().getSchemas().get("SchemaWithTwoParents");
+        Schema<?>schemaWithTwoParents = openAPI.getComponents().getSchemas().get("SchemaWithTwoAllOfRefs");
         assertNull(schemaWithTwoParents.getExtensions());
         Schema<?>personA = openAPI.getComponents().getSchemas().get("PersonA");
         assertEquals(personA.getExtensions().get(X_PARENT), true);
@@ -79,10 +79,10 @@ public class OpenAPINormalizerTest {
         // to test the both REF_AS_PARENT_IN_ALLOF and REFACTOR_ALLOF_WITH_PROPERTIES_ONLY
         OpenAPI openAPI = TestUtils.parseSpec("src/test/resources/3_0/allOf_extension_parent.yaml");
 
-        Schema schema = openAPI.getComponents().getSchemas().get("Child");
+        Schema<?> schema = openAPI.getComponents().getSchemas().get("Child");
         assertNull(schema.getExtensions());
 
-        Schema schema2 = openAPI.getComponents().getSchemas().get("Ancestor");
+        Schema<?> schema2 = openAPI.getComponents().getSchemas().get("Ancestor");
         assertNull(schema2.getExtensions());
 
         Map<String, String> options = new HashMap<>();
@@ -91,10 +91,10 @@ public class OpenAPINormalizerTest {
         OpenAPINormalizer openAPINormalizer = new OpenAPINormalizer(openAPI, options);
         openAPINormalizer.normalize();
 
-        Schema schema3 = openAPI.getComponents().getSchemas().get("Ancestor");
+        Schema<?> schema3 = openAPI.getComponents().getSchemas().get("Ancestor");
         assertEquals(schema3.getExtensions().get(X_PARENT), true);
 
-        Schema schema4 = openAPI.getComponents().getSchemas().get("Child");
+        Schema<?> schema4 = openAPI.getComponents().getSchemas().get("Child");
         assertNull(schema4.getExtensions());
     }
 
