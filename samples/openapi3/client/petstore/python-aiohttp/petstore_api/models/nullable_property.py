2a102b75536793da330e7484f95e76b3c02f66ee
Espen Haugsdal <espen.haugsdal@gmail.com>
2025-10-12 11:09:06 +0200

    [Python] Use pydantic_core.to_jsonable_python to convert non-standard data types

--- samples/openapi3/client/petstore/python-aiohttp/petstore_api/models/nullable_property.py
+++ samples/openapi3/client/petstore/python-aiohttp/petstore_api/models/nullable_property.py
@@ -22,6 +22,7 @@ from typing import Any, ClassVar, Dict, List, Optional
 from typing_extensions import Annotated
 from typing import Optional, Set
 from typing_extensions import Self
+from pydantic_core import to_jsonable_python
 
 class NullableProperty(BaseModel):
     """
@@ -54,8 +55,7 @@ class NullableProperty(BaseModel):
 
     def to_json(self) -> str:
         """Returns the JSON representation of the model using alias"""
-        # TODO: pydantic v2: use .model_dump_json(by_alias=True, exclude_unset=True) instead
-        return json.dumps(self.to_dict())
+        return json.dumps(to_jsonable_python(self.to_dict()))
 
     @classmethod
     def from_json(cls, json_str: str) -> Optional[Self]:
--- samples/openapi3/client/petstore/python/petstore_api/models/with_nested_one_of.py
+++ samples/openapi3/client/petstore/python/petstore_api/models/with_nested_one_of.py
@@ -23,6 +23,7 @@ from petstore_api.models.one_of_enum_string import OneOfEnumString
 from petstore_api.models.pig import Pig
 from typing import Optional, Set
 from typing_extensions import Self
+from pydantic_core import to_jsonable_python
 
 class WithNestedOneOf(BaseModel):
     """
@@ -47,8 +48,7 @@ class WithNestedOneOf(BaseModel):
 
     def to_json(self) -> str:
         """Returns the JSON representation of the model using alias"""
-        # TODO: pydantic v2: use .model_dump_json(by_alias=True, exclude_unset=True) instead
-        return json.dumps(self.to_dict())
+        return json.dumps(to_jsonable_python(self.to_dict()))
 
     @classmethod
     def from_json(cls, json_str: str) -> Optional[Self]:
