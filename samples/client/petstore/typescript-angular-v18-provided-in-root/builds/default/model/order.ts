784869c0be4fa9bd3590f4f908dd3ca4e6ff4102
William Cheng <wing328hk@gmail.com>
2025-10-15 17:28:44 +0800

    update samples

--- samples/client/petstore/typescript-angular-v18-provided-in-root/builds/default/model/order.ts
+++ samples/client/petstore/typescript-angular-v18-provided-in-root/builds/default/model/order.ts
@@ -27,7 +27,7 @@ export namespace Order {
     export const StatusEnum = {
         Placed: 'placed',
         Approved: 'approved',
-        SHIPPED: 'delivered'
+        Delivered: 'delivered'
     } as const;
     export type StatusEnum = typeof StatusEnum[keyof typeof StatusEnum];
 }
