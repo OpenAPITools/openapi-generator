31c4108810664ebb63275a20a99cf4898c4f746c
William Cheng <wing328hk@gmail.com>
2025-10-15 17:08:01 +0800

    add tests for angular v17, v18, v20

--- samples/client/petstore/typescript-angular-v18-provided-in-root/builds/default/model/order.ts
+++ samples/client/petstore/typescript-angular-v18-provided-in-root/builds/default/model/order.ts
@@ -27,7 +27,7 @@ export namespace Order {
     export const StatusEnum = {
         Placed: 'placed',
         Approved: 'approved',
-        Delivered: 'delivered'
+        SHIPPED: 'delivered'
     } as const;
     export type StatusEnum = typeof StatusEnum[keyof typeof StatusEnum];
 }
