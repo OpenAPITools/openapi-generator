117042aa44c12381ce3c8675c491225a0a30d951
Linh Tran Tuan <linhduonggnu@gmail.com>
2025-10-16 15:25:01 +0900

    [Rust-Axum] Fix wrong validation for non-model Body (#22155)
    * Fix Issue 21143
    
    * Update

--- samples/server/petstore/rust-axum/output/rust-axum-array-params-test/src/models.rs
+++ samples/server/petstore/rust-axum/output/rust-axum-array-params-test/src/models.rs
@@ -8,6 +8,13 @@ use crate::header;
 use crate::{models, types::*};
 
 #[allow(dead_code)]
+fn from_validation_error(e: validator::ValidationError) -> validator::ValidationErrors {
+    let mut errs = validator::ValidationErrors::new();
+    errs.add("na", e);
+    errs
+}
+
+#[allow(dead_code)]
 pub fn check_xss_string(v: &str) -> std::result::Result<(), validator::ValidationError> {
     if ammonia::is_html(v) {
         std::result::Result::Err(validator::ValidationError::new("xss detected"))
