e11d33ded264389fea51a0684321d6bfde31a98e
Aman Rao <amanrao032@gmail.com>
2025-10-21 10:30:35 +0530

    Added handling for when super.getSchemaType method returns null

--- modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/AbstractPythonCodegen.java
+++ modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/AbstractPythonCodegen.java
@@ -672,6 +672,12 @@ public abstract class AbstractPythonCodegen extends DefaultCodegen implements Co
         }
 
         String openAPIType = super.getSchemaType(p);
+        
+        if (openAPIType == null) {
+            LOGGER.error("OpenAPI Type for {} is null. Default to UNKNOWN_OPENAPI_TYPE instead.", p.getName());
+            openAPIType = "UNKNOWN_OPENAPI_TYPE";
+        }
+
         if (typeMapping.containsKey(openAPIType)) {
             return typeMapping.get(openAPIType);
         }
