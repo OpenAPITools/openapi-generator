9655c22ff6fc88784f30e2ec2bf73c340a7623f3
Xi Lu <fridalu66@gmail.com>
2025-11-19 01:46:43 -0800

    [Protobuf] Add isEnumSchema check in generateNestedSchema (#22384)
    Co-authored-by: xil <xil@uber.com>

--- modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/ProtobufSchemaCodegen.java
+++ modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/ProtobufSchemaCodegen.java
@@ -385,7 +385,7 @@ public class ProtobufSchemaCodegen extends DefaultCodegen implements CodegenConf
         if(ModelUtils.isArraySchema(schema)) {
             Schema itemsSchema = ModelUtils.getSchemaItems(schema);
             itemsSchema = ModelUtils.getReferencedSchema(openAPI, itemsSchema);
-            if(ModelUtils.isModel(itemsSchema)) {
+            if(ModelUtils.isModel(itemsSchema) || (itemsSchema != null && ModelUtils.isEnumSchema(itemsSchema))) {
                 String newSchemaName = ModelUtils.getSimpleRef(ModelUtils.getSchemaItems(schema).get$ref()) + ARRAY_SUFFIX;
                 return addSchemas(schema, newSchemaName, visitedSchemas);
             }else if (ModelUtils.isPrimitiveType(itemsSchema)){
@@ -400,7 +400,7 @@ public class ProtobufSchemaCodegen extends DefaultCodegen implements CodegenConf
         } else if(ModelUtils.isMapSchema(schema)) {
             Schema mapValueSchema = ModelUtils.getAdditionalProperties(schema);
             mapValueSchema = ModelUtils.getReferencedSchema(openAPI, mapValueSchema);
-            if(ModelUtils.isModel(mapValueSchema) ) {
+            if(ModelUtils.isModel(mapValueSchema) || (mapValueSchema != null && ModelUtils.isEnumSchema(mapValueSchema))) {
                 String newSchemaName = ModelUtils.getSimpleRef(ModelUtils.getAdditionalProperties(schema).get$ref()) + MAP_SUFFIX;
                 return addSchemas(schema, newSchemaName, visitedSchemas);
             }else if (ModelUtils.isPrimitiveType(mapValueSchema)){
@@ -1116,7 +1116,7 @@ public class ProtobufSchemaCodegen extends DefaultCodegen implements CodegenConf
             } else {
                 Schema additionalProperties = ModelUtils.getAdditionalProperties(schema);
                 if(additionalProperties == null) {
-                   return; 
+                   return;
                 } else if (additionalProperties.getTitle() != null) {
                     addtionalPropertiesName = additionalProperties.getTitle();
                 } else if (additionalProperties.get$ref() != null) {
@@ -1124,8 +1124,8 @@ public class ProtobufSchemaCodegen extends DefaultCodegen implements CodegenConf
                     addtionalPropertiesName = toVarName(toModelName(ref));
                 }
             }
-            
-            properties.put(addtionalPropertiesName, schema);        
+
+            properties.put(addtionalPropertiesName, schema);
         }
     }
 }
