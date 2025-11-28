7a6509447b3ecac8c494fbf980c1a31aae0180e6
Jáchym Metlička <metlicka@2n.com>
2025-11-28 15:34:00 +0100

    fix javadoc

--- samples/server/petstore/kotlin-springboot-3-dollar-issue-swagger2/src/main/kotlin/org/openapitools/api/ItemsApiService.kt
+++ samples/server/petstore/kotlin-springboot-3-dollar-issue-swagger2/src/main/kotlin/org/openapitools/api/ItemsApiService.kt
@@ -6,30 +6,30 @@ import org.openapitools.model.ItemsItemIdSomethingItemSubIdGet200Response
 interface ItemsApiService {
 
     /**
-     * GET /items/{item$Id}/something/{item$SubId} : SQ &#x3D; \&quot;; SBS &#x3D; \\; DBS &#x3D; \\\\; SD &#x3D; $some
-     * SQ &#x3D; \&quot;; SBS &#x3D; \\; DBS &#x3D; \\\\; SD &#x3D; $some
+     * GET /items/{item$Id}/something/{item$SubId} : SQ = "; SBS = \; DBS = \\; SD = $some
+     * SQ = "; SBS = \; DBS = \\; SD = $some
      *
-     * @param itemDollarId SQ &#x3D; \&quot;; SBS &#x3D; \\; DBS &#x3D; \\\\; SD &#x3D; $some (required)
-     * @param itemDollarSubId SQ &#x3D; \&quot;; SBS &#x3D; \\; DBS &#x3D; \\\\; SD &#x3D; $some (required)
-     * @param filterDollarType SQ &#x3D; \&quot;; SBS &#x3D; \\; DBS &#x3D; \\\\; SD &#x3D; $some (optional, default to &quot;SQ &#x3D; \&quot;; SBS &#x3D; \\; DBS &#x3D; \\\\; SD &#x3D; $some&quot;)
-     * @param filterDollarSubType SQ &#x3D; \&quot;; SBS &#x3D; \\; DBS &#x3D; \\\\; SD &#x3D; $some (optional, default to &quot;SQ &#x3D; \&quot;; SBS &#x3D; \\; DBS &#x3D; \\\\; SD &#x3D; $some&quot;)
-     * @param xCustomHeader SQ &#x3D; \&quot;; SBS &#x3D; \\; DBS &#x3D; \\\\; SD &#x3D; $some (optional)
-     * @param xCustomHeaderTwo SQ &#x3D; \&quot;; SBS &#x3D; \\; DBS &#x3D; \\\\; SD &#x3D; $some (optional)
-     * @param sessionDollarToken SQ &#x3D; \&quot;; SBS &#x3D; \\; DBS &#x3D; \\\\; SD &#x3D; $some (optional)
-     * @param sessionDollarTokenTwo SQ &#x3D; \&quot;; SBS &#x3D; \\; DBS &#x3D; \\\\; SD &#x3D; $some (optional)
-     * @return SQ &#x3D; \&quot;; SBS &#x3D; \\; DBS &#x3D; \\\\; SD &#x3D; $some (status code 200)
+     * @param itemDollarId SQ = "; SBS = \; DBS = \\; SD = $some (required)
+     * @param itemDollarSubId SQ = "; SBS = \; DBS = \\; SD = $some (required)
+     * @param filterDollarType SQ = "; SBS = \; DBS = \\; SD = $some (optional, default to "SQ = \"; SBS = \\; DBS = \\\\; SD = \$some")
+     * @param filterDollarSubType SQ = "; SBS = \; DBS = \\; SD = $some (optional, default to "SQ = \"; SBS = \\; DBS = \\\\; SD = \$some")
+     * @param xCustomHeader SQ = "; SBS = \; DBS = \\; SD = $some (optional)
+     * @param xCustomHeaderTwo SQ = "; SBS = \; DBS = \\; SD = $some (optional)
+     * @param sessionDollarToken SQ = "; SBS = \; DBS = \\; SD = $some (optional)
+     * @param sessionDollarTokenTwo SQ = "; SBS = \; DBS = \\; SD = $some (optional)
+     * @return SQ = "; SBS = \; DBS = \\; SD = $some (status code 200)
      * @see ItemsApi#itemsItemIdSomethingItemSubIdGet
      */
     fun itemsItemIdSomethingItemSubIdGet(itemDollarId: kotlin.String, itemDollarSubId: kotlin.String, filterDollarType: kotlin.String, filterDollarSubType: kotlin.String, xCustomHeader: kotlin.String?, xCustomHeaderTwo: kotlin.String?, sessionDollarToken: kotlin.String?, sessionDollarTokenTwo: kotlin.String?): ItemsItemIdSomethingItemSubIdGet200Response
 
     /**
-     * POST /items : SQ &#x3D; \&quot;; SBS &#x3D; \\; DBS &#x3D; \\\\; SD &#x3D; $some
-     * SQ &#x3D; \&quot;; SBS &#x3D; \\; DBS &#x3D; \\\\; SD &#x3D; $some
+     * POST /items : SQ = "; SBS = \; DBS = \\; SD = $some
+     * SQ = "; SBS = \; DBS = \\; SD = $some
      *
-     * @param xPostHeader SQ &#x3D; \&quot;; SBS &#x3D; \\; DBS &#x3D; \\\\; SD &#x3D; $some (optional)
-     * @param formDollarName SQ &#x3D; \\\&quot;; SBS &#x3D; \\\\; DBS &#x3D; \\\\\\\\; SD &#x3D; $some (optional)
-     * @param formDollarValue SQ &#x3D; \\\&quot;; SBS &#x3D; \\\\; DBS &#x3D; \\\\\\\\; SD &#x3D; $some (optional, default to &quot;SQ &#x3D; \&quot;; SBS &#x3D; \\; DBS &#x3D; \\\\; SD &#x3D; $some&quot;)
-     * @return SQ &#x3D; \&quot;; SBS &#x3D; \\; DBS &#x3D; \\\\; SD &#x3D; $some (status code 201)
+     * @param xPostHeader SQ = "; SBS = \; DBS = \\; SD = $some (optional)
+     * @param formDollarName SQ = \"; SBS = \\; DBS = \\\\; SD = $some (optional)
+     * @param formDollarValue SQ = \"; SBS = \\; DBS = \\\\; SD = $some (optional, default to "SQ = \"; SBS = \\; DBS = \\\\; SD = \$some")
+     * @return SQ = "; SBS = \; DBS = \\; SD = $some (status code 201)
      * @see ItemsApi#itemsPost
      */
     fun itemsPost(xPostHeader: kotlin.String?, formDollarName: kotlin.String?, formDollarValue: kotlin.String): ItemWithDollarAttributesAndExamples
