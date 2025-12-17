e4a26ce307040f89b5c5cca38c99dcf594ca6871
Jurrie Overgoor <1213142+Jurrie@users.noreply.github.com>
2025-12-17 08:04:37 +0100

    [Java] Support JsonNullable in JaxRS-spec (issue #9018) (#22412)
    * Support JsonNullable in JaxRS-spec
    
    * Add sample files that are now modified (new import)
    
    * Fix build issue with jaxrs-spec-microprofile-openapi-annotations sample

--- samples/server/petstore/jaxrs-spec-jakarta/src/gen/java/org/openapitools/model/Pet.java
+++ samples/server/petstore/jaxrs-spec-jakarta/src/gen/java/org/openapitools/model/Pet.java
@@ -20,6 +20,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonValue;
 import com.fasterxml.jackson.annotation.JsonTypeName;
+import org.openapitools.jackson.nullable.JsonNullable;
 
 
 
