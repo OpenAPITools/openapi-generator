833d1d3d381c52bb5cb444ba846c6f2a1473313c
thibautrety <thibaut.rety@gmail.com>
2025-12-17 09:42:55 +0100

    [REQ-22001] Add MCP server support to apiService.mustache (#22197)
    * Add MCP server support to apiService.mustache
    
    Refactor apiService.mustache to include MCP server support and enhance operation methods.
    
    * Update samples
    
    * update samples in wsl
    
    * remove useless import when mcp is disabled
    
    ---------
    
    Co-authored-by: RL6172 <thibaut.rety@external.engie.com>

--- samples/server/petstore/jaxrs-resteasy/java8/src/gen/java/org/openapitools/api/PetApiService.java
+++ samples/server/petstore/jaxrs-resteasy/java8/src/gen/java/org/openapitools/api/PetApiService.java
@@ -2,8 +2,8 @@ package org.openapitools.api;
 
 import org.openapitools.api.*;
 import org.openapitools.model.*;
-import org.jboss.resteasy.plugins.providers.multipart.MultipartFormDataInput;
 
+import org.jboss.resteasy.plugins.providers.multipart.MultipartFormDataInput;
 
 import java.io.File;
 import org.openapitools.model.ModelApiResponse;
@@ -11,30 +11,160 @@ import org.openapitools.model.Pet;
 
 import java.util.List;
 import org.openapitools.api.NotFoundException;
-
 import java.io.InputStream;
 
 import javax.validation.constraints.*;
 import javax.validation.Valid;
+
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.SecurityContext;
 
 @javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaResteasyServerCodegen", comments = "Generator version: 7.18.0-SNAPSHOT")
 public interface PetApiService {
-      Response addPet(Pet body,SecurityContext securityContext)
-      throws NotFoundException;
-      Response deletePet(Long petId,String apiKey,SecurityContext securityContext)
-      throws NotFoundException;
-      Response findPetsByStatus(List<String> status,SecurityContext securityContext)
-      throws NotFoundException;
-      Response findPetsByTags(List<String> tags,SecurityContext securityContext)
-      throws NotFoundException;
-      Response getPetById(Long petId,SecurityContext securityContext)
-      throws NotFoundException;
-      Response updatePet(Pet body,SecurityContext securityContext)
-      throws NotFoundException;
-      Response updatePetWithForm(Long petId,String name,String status,SecurityContext securityContext)
-      throws NotFoundException;
-      Response uploadFile(MultipartFormDataInput input,Long petId,SecurityContext securityContext)
-      throws NotFoundException;
+    Response addPet(
+        
+        Pet body,
+        SecurityContext securityContext
+    ) throws NotFoundException;
+
+    default Response addPet(
+        
+        Pet body, 
+        String context
+    ) throws NotFoundException {
+        return addPet(
+            
+            body,
+            (SecurityContext)null
+        );
+    }
+    Response deletePet(
+        
+        Long petId,
+        String apiKey,
+        SecurityContext securityContext
+    ) throws NotFoundException;
+
+    default Response deletePet(
+        
+        Long petId, 
+        String apiKey, 
+        String context
+    ) throws NotFoundException {
+        return deletePet(
+            
+            petId,apiKey,
+            (SecurityContext)null
+        );
+    }
+    Response findPetsByStatus(
+        
+        List<String> status,
+        SecurityContext securityContext
+    ) throws NotFoundException;
+
+    default Response findPetsByStatus(
+        
+        List<String> status, 
+        String context
+    ) throws NotFoundException {
+        return findPetsByStatus(
+            
+            status,
+            (SecurityContext)null
+        );
+    }
+    Response findPetsByTags(
+        
+        List<String> tags,
+        SecurityContext securityContext
+    ) throws NotFoundException;
+
+    default Response findPetsByTags(
+        
+        List<String> tags, 
+        String context
+    ) throws NotFoundException {
+        return findPetsByTags(
+            
+            tags,
+            (SecurityContext)null
+        );
+    }
+    Response getPetById(
+        
+        Long petId,
+        SecurityContext securityContext
+    ) throws NotFoundException;
+
+    default Response getPetById(
+        
+        Long petId, 
+        String context
+    ) throws NotFoundException {
+        return getPetById(
+            
+            petId,
+            (SecurityContext)null
+        );
+    }
+    Response updatePet(
+        
+        Pet body,
+        SecurityContext securityContext
+    ) throws NotFoundException;
+
+    default Response updatePet(
+        
+        Pet body, 
+        String context
+    ) throws NotFoundException {
+        return updatePet(
+            
+            body,
+            (SecurityContext)null
+        );
+    }
+    Response updatePetWithForm(
+        
+        Long petId,
+        String name,
+        String status,
+        SecurityContext securityContext
+    ) throws NotFoundException;
+
+    default Response updatePetWithForm(
+        
+        Long petId, 
+        String name, 
+        String status, 
+        String context
+    ) throws NotFoundException {
+        return updatePetWithForm(
+            
+            petId,name,status,
+            (SecurityContext)null
+        );
+    }
+    Response uploadFile(
+        MultipartFormDataInput input,
+        Long petId,
+        
+        
+        SecurityContext securityContext
+    ) throws NotFoundException;
+
+    default Response uploadFile(
+        MultipartFormDataInput input,
+        Long petId, 
+        
+        
+        String context
+    ) throws NotFoundException {
+        return uploadFile(
+            input,
+            petId,additionalMetadata,_file,
+            (SecurityContext)null
+        );
+    }
 }
