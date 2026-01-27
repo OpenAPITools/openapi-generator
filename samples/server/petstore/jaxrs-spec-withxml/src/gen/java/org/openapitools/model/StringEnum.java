109897610aeb490faf688c96bd30d2678806b5de
Andreas Maechler <amaechler@gmail.com>
2026-01-27 12:07:38 -0700

    Update all samples

--- samples/server/petstore/jaxrs-spec-withxml/src/gen/java/org/openapitools/model/StringEnum.java
+++ samples/server/petstore/jaxrs-spec-withxml/src/gen/java/org/openapitools/model/StringEnum.java
@@ -10,6 +10,7 @@ import com.fasterxml.jackson.annotation.JsonValue;
 /**
  * Gets or Sets StringEnum
  */
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaJAXRSSpecServerCodegen", comments = "Generator version: 7.20.0-SNAPSHOT")
 public enum StringEnum {
   
   FOO("foo"),
