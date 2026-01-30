9d6f0822f869083b878b51068acf8085e2541789
Jáchym Metlička <metlicka@2n.com>
2026-01-30 15:59:43 +0100

    fix interfaces

--- samples/server/petstore/springboot-x-implements-skip/src/main/java/com/custompackage/WithBar.java
+++ samples/server/petstore/springboot-x-implements-skip/src/main/java/com/custompackage/WithBar.java
@@ -1,31 +1,6 @@
-package org.openapitools.model;
+package com.custompackage;
 
-import java.net.URI;
-import java.util.Objects;
-
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonSubTypes;
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
-import com.fasterxml.jackson.annotation.JsonTypeName;
-import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
-
-import org.springframework.lang.Nullable;
-
-import org.openapitools.jackson.nullable.JsonNullable;
-
-import java.time.OffsetDateTime;
-
-import javax.validation.Valid;
-import javax.validation.constraints.*;
-
-import java.util.*;
-
-import javax.annotation.Generated;
-
-interface WithBar {
+public interface WithBar {
 
     public String getBar();
 
