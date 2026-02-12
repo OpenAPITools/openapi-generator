d47ab0f545760b3e359ebe86d558bc25419aa517
Aman Rao <69011576+MercMayhem@users.noreply.github.com>
2026-02-12 10:21:52 +0530

    [rust] Modified api.mustache to ensure correct handling of `Vec<PathBuf>` while adding files to multipart form (#22942)
    * Fixes #22900
    
    * fixed non async block generation when Vec<PathBuf> (files) are present
    
    * updated samples with reqwest multipart form change

--- samples/client/petstore/rust/reqwest/petstore-avoid-box/src/apis/pet_api.rs
+++ samples/client/petstore/rust/reqwest/petstore-avoid-box/src/apis/pet_api.rs
@@ -570,11 +570,11 @@ pub async fn upload_file(configuration: &configuration::Configuration, params: U
         multipart_form = multipart_form.text("additionalMetadata", param_value.to_string());
     }
     if let Some(ref param_value) = params.file {
-        let file = TokioFile::open(param_value).await?;
-        let stream = FramedRead::new(file, BytesCodec::new());
-        let file_name = param_value.file_name().map(|n| n.to_string_lossy().to_string()).unwrap_or_default();
-        let file_part = reqwest::multipart::Part::stream(reqwest::Body::wrap_stream(stream)).file_name(file_name);
-        multipart_form = multipart_form.part("file", file_part);
+                let file = TokioFile::open(param_value).await?;
+                let stream = FramedRead::new(file, BytesCodec::new());
+                let file_name = param_value.file_name().map(|n| n.to_string_lossy().to_string()).unwrap_or_default();
+                let file_part = reqwest::multipart::Part::stream(reqwest::Body::wrap_stream(stream)).file_name(file_name);
+                multipart_form = multipart_form.part("file", file_part);
     }
     req_builder = req_builder.multipart(multipart_form);
 
