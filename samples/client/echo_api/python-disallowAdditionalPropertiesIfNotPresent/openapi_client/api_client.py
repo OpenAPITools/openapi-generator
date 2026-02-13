73dcdd697678e6a6c1f5a887e0fad0acd917aa1d
William Cheng <wing328hk@gmail.com>
2026-02-14 00:57:57 +0800

    Update python sdk to strip any directory traversal in filename (#22965)
    * update python sdk
    
    Strip any directory traversal
    
    * rebased
    
    * update samples, docs
    
    * fallback case
    
    ---------
    
    Co-authored-by: Pavel Slabko <slabkopg@gmail.com>

--- samples/client/echo_api/python-disallowAdditionalPropertiesIfNotPresent/openapi_client/api_client.py
+++ samples/client/echo_api/python-disallowAdditionalPropertiesIfNotPresent/openapi_client/api_client.py
@@ -708,7 +708,9 @@ class ApiClient:
                 content_disposition
             )
             assert m is not None, "Unexpected 'content-disposition' header value"
-            filename = m.group(1)
+            filename = os.path.basename(m.group(1))  # Strip any directory traversal
+            if filename in ("", ".", ".."):  # fall back to tmp filename
+                filename = os.path.basename(path)
             path = os.path.join(os.path.dirname(path), filename)
 
         with open(path, "wb") as f:
