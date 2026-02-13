3c092b4c30d757ba5f0b0e118230ff1a102df081
Jachym Metlicka <metlicka@2n.com>
2026-02-13 17:17:00 +0100

    [KOTLIN;SPRING] - add support for 'x-spring-paginated' to get closer to feature parity with java-spring codegen; add 'autoXSpringPaginated' option; support x-operation-extra-annotation (#22958)
    * add x-kotlin-implements
    
    * implement tests
    
    * update samples
    
    * fix tests - forbidden api issue
    
    * add samples
    
    * add samples. use Pageable only for server-side
    
    * add support for auto-detecting x-spring-paginated in Spring Boot operations
    
    * fix maven dependencies import
    
    * add unit tests
    
    * add support for vendor extension
    
    * remove files
    
    * fix samples
    
    * fix docs
    
    * implement suggestions from CR. Fix declarative interface naming.
    
    * move import around
    
    * add x-operation-extra-annotation
    
    * make sure the PageableAsQueryParam does not remove already present x-operation-extra-annotation content
    
    * support also list format
    
    * regenerate samples and docs
    
    * regenerate samples and docs
    
    * force tests rerun
    
    * remove files
    
    * add files
    
    * trigger test rerun

--- samples/server/petstore/kotlin-springboot-include-http-request-context-delegate/src/main/kotlin/org/openapitools/api/PetApiDelegate.kt
+++ samples/server/petstore/kotlin-springboot-include-http-request-context-delegate/src/main/kotlin/org/openapitools/api/PetApiDelegate.kt
@@ -1,6 +1,7 @@
 package org.openapitools.api
 
 import org.openapitools.model.ModelApiResponse
+import org.springframework.data.domain.Pageable
 import org.openapitools.model.Pet
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
@@ -53,6 +54,88 @@ interface PetApiDelegate {
      */
     suspend fun getPetById(petId: kotlin.Long,
         exchange: org.springframework.web.server.ServerWebExchange): ResponseEntity<Pet>
+
+
+    /**
+     * @see PetApi#listAllPetsPaginated
+     */
+    fun listAllPetsPaginated(exchange: org.springframework.web.server.ServerWebExchange,
+        pageable: Pageable): ResponseEntity<Flow<Pet>>
+
+
+    /**
+     * @see PetApi#listPetsByIdPaginated
+     */
+    fun listPetsByIdPaginated(xRequestID: kotlin.String?,
+        exchange: org.springframework.web.server.ServerWebExchange,
+        pageable: Pageable): ResponseEntity<Flow<Pet>>
+
+
+    /**
+     * @see PetApi#listPetsByOwnerPaginated
+     */
+    fun listPetsByOwnerPaginated(ownerId: kotlin.Long,
+        includeAdopted: kotlin.Boolean,
+        exchange: org.springframework.web.server.ServerWebExchange,
+        pageable: Pageable): ResponseEntity<Flow<Pet>>
+
+
+    /**
+     * @see PetApi#listPetsMixedParams
+     */
+    fun listPetsMixedParams(authorization: kotlin.String?,
+        xTenantID: kotlin.String?,
+        status: kotlin.String?,
+        includeInactive: kotlin.Boolean?,
+        exchange: org.springframework.web.server.ServerWebExchange,
+        pageable: Pageable): ResponseEntity<Flow<Pet>>
+
+
+    /**
+     * @see PetApi#listPetsMultipleParams
+     */
+    fun listPetsMultipleParams(name: kotlin.String?,
+        minAge: kotlin.Int?,
+        maxAge: kotlin.Int?,
+        tags: kotlin.collections.List<kotlin.String>?,
+        exchange: org.springframework.web.server.ServerWebExchange,
+        pageable: Pageable): ResponseEntity<Flow<Pet>>
+
+
+    /**
+     * @see PetApi#listPetsNoPagination
+     */
+    fun listPetsNoPagination(status: kotlin.String?,
+        page: kotlin.Int?,
+        size: kotlin.Int?,
+        sort: kotlin.String?,
+        exchange: org.springframework.web.server.ServerWebExchange): ResponseEntity<Flow<Pet>>
+
+
+    /**
+     * @see PetApi#listPetsPartialPagination
+     */
+    fun listPetsPartialPagination(status: kotlin.String?,
+        exchange: org.springframework.web.server.ServerWebExchange,
+        pageable: Pageable): ResponseEntity<Flow<Pet>>
+
+
+    /**
+     * @see PetApi#listPetsWithFilterPaginated
+     */
+    fun listPetsWithFilterPaginated(status: kotlin.String?,
+        name: kotlin.String?,
+        exchange: org.springframework.web.server.ServerWebExchange,
+        pageable: Pageable): ResponseEntity<Flow<Pet>>
+
+
+    /**
+     * @see PetApi#listPetsWithHeaderSize
+     */
+    fun listPetsWithHeaderSize(size: kotlin.String?,
+        category: kotlin.String?,
+        exchange: org.springframework.web.server.ServerWebExchange,
+        pageable: Pageable): ResponseEntity<Flow<Pet>>
 
 
     /**
