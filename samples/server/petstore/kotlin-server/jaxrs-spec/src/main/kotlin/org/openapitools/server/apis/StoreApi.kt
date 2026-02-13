331fa371cd2bdef93c7fce04a8bb0f7bfe97d09b
feczkob <feczkob@gmail.com>
2026-02-14 00:08:05 +0100

    Add tests and update samples for useTags in kotlin-server JAXRS codegen

--- samples/server/petstore/kotlin-server/jaxrs-spec/src/main/kotlin/org/openapitools/server/apis/StoreApi.kt
+++ samples/server/petstore/kotlin-server/jaxrs-spec/src/main/kotlin/org/openapitools/server/apis/StoreApi.kt
@@ -10,28 +10,32 @@ import java.io.InputStream
 
 
 
-@Path("/")
+@Path("/store")
 @javax.annotation.Generated(value = arrayOf("org.openapitools.codegen.languages.KotlinServerCodegen"), comments = "Generator version: 7.20.0-SNAPSHOT")
 class StoreApi {
 
     @DELETE
+    @Path("/order/{orderId}")
     suspend fun deleteOrder(@PathParam("orderId") orderId: kotlin.String): Response {
         return Response.ok().entity("magic!").build();
     }
 
     @GET
+    @Path("/inventory")
     @Produces("application/json")
     suspend fun getInventory(): Response {
         return Response.ok().entity("magic!").build();
     }
 
     @GET
+    @Path("/order/{orderId}")
     @Produces("application/xml", "application/json")
     suspend fun getOrderById(@PathParam("orderId") orderId: kotlin.Long): Response {
         return Response.ok().entity("magic!").build();
     }
 
     @POST
+    @Path("/order")
     @Produces("application/xml", "application/json")
     suspend fun placeOrder( body: Order): Response {
         return Response.ok().entity("magic!").build();
--- samples/server/petstore/kotlin-server/jaxrs-spec/src/main/kotlin/org/openapitools/server/apis/UserApi.kt
+++ samples/server/petstore/kotlin-server/jaxrs-spec/src/main/kotlin/org/openapitools/server/apis/UserApi.kt
@@ -10,7 +10,7 @@ import java.io.InputStream
 
 
 
-@Path("/")
+@Path("/user")
 @javax.annotation.Generated(value = arrayOf("org.openapitools.codegen.languages.KotlinServerCodegen"), comments = "Generator version: 7.20.0-SNAPSHOT")
 class UserApi {
 
@@ -20,38 +20,45 @@ class UserApi {
     }
 
     @POST
+    @Path("/createWithArray")
     suspend fun createUsersWithArrayInput( body: kotlin.collections.List<User>): Response {
         return Response.ok().entity("magic!").build();
     }
 
     @POST
+    @Path("/createWithList")
     suspend fun createUsersWithListInput( body: kotlin.collections.List<User>): Response {
         return Response.ok().entity("magic!").build();
     }
 
     @DELETE
+    @Path("/{username}")
     suspend fun deleteUser(@PathParam("username") username: kotlin.String): Response {
         return Response.ok().entity("magic!").build();
     }
 
     @GET
+    @Path("/{username}")
     @Produces("application/xml", "application/json")
     suspend fun getUserByName(@PathParam("username") username: kotlin.String): Response {
         return Response.ok().entity("magic!").build();
     }
 
     @GET
+    @Path("/login")
     @Produces("application/xml", "application/json")
     suspend fun loginUser(@QueryParam("username")   username: kotlin.String,@QueryParam("password")   password: kotlin.String): Response {
         return Response.ok().entity("magic!").build();
     }
 
     @GET
+    @Path("/logout")
     suspend fun logoutUser(): Response {
         return Response.ok().entity("magic!").build();
     }
 
     @PUT
+    @Path("/{username}")
     suspend fun updateUser(@PathParam("username") username: kotlin.String, body: User): Response {
         return Response.ok().entity("magic!").build();
     }
