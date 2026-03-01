427455ec4d33b1e75cd8189cfa4b74eb840a53e2
Miklos Marton <martonmiklosqdev@gmail.com>
2026-03-02 00:23:18 +0100

    [cpp-qt-client]Optimize generated code - Use const references at function parameters - Do not perform useless initialization of QStrings - Use qDeleteAll

--- samples/client/petstore/cpp-qt-addDownloadProgress/client/PFXTestAnyType.h
+++ samples/client/petstore/cpp-qt-addDownloadProgress/client/PFXTestAnyType.h
@@ -31,13 +31,13 @@ namespace test_namespace {
 class PFXTestAnyType : public PFXObject {
 public:
     PFXTestAnyType();
-    PFXTestAnyType(QString json);
+    PFXTestAnyType(const QString &json);
     ~PFXTestAnyType() override;
 
     QString asJson() const override;
     QJsonObject asJsonObject() const override;
     void fromJsonObject(QJsonObject json) override;
-    void fromJson(QString jsonString) override;
+    void fromJson(const QString &jsonString) override;
 
     QJsonValue getCode() const;
     void setCode(const QJsonValue &code);
