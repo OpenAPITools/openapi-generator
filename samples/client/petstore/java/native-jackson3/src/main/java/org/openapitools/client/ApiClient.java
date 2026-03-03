a1e24d17ac0101de2f8ad489d59edb5328d70b1b
Thorsten Hirsch <t.hirsch@web.de>
2026-03-03 15:00:49 +0100

    use JsonMapper instead of ObjectMapper for Jackson 3

--- samples/client/petstore/java/native-jackson3/src/main/java/org/openapitools/client/ApiClient.java
+++ samples/client/petstore/java/native-jackson3/src/main/java/org/openapitools/client/ApiClient.java
@@ -13,9 +13,11 @@
 package org.openapitools.client;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import tools.jackson.databind.cfg.DateTimeFeature;
+import tools.jackson.databind.cfg.EnumFeature;
 import tools.jackson.databind.DeserializationFeature;
 import tools.jackson.databind.ObjectMapper;
-import tools.jackson.databind.SerializationFeature;
+import tools.jackson.databind.json.JsonMapper;
 
 import java.io.InputStream;
 import java.io.IOException;
@@ -194,17 +196,16 @@ public class ApiClient {
   }
 
   public static ObjectMapper createDefaultObjectMapper() {
-    ObjectMapper mapper = new ObjectMapper();
-    mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
-    mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-    mapper.configure(DeserializationFeature.FAIL_ON_INVALID_SUBTYPE, false);
-    mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-    mapper.enable(SerializationFeature.WRITE_ENUMS_USING_TO_STRING);
-    mapper.enable(DeserializationFeature.READ_ENUMS_USING_TO_STRING);
-    mapper.disable(DeserializationFeature.ADJUST_DATES_TO_CONTEXT_TIME_ZONE);
-    // FIXME: JsonNullableModule is not yet available for Jackson 3
-    mapper.registerModule(new RFC3339JavaTimeModule());
-    return mapper;
+    return JsonMapper.builder()
+        .changeDefaultPropertyInclusion(v -> v.withValueInclusion(JsonInclude.Include.NON_NULL))
+        .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
+        .disable(DeserializationFeature.FAIL_ON_INVALID_SUBTYPE)
+        .disable(DateTimeFeature.WRITE_DATES_AS_TIMESTAMPS)
+        .enable(EnumFeature.WRITE_ENUMS_USING_TO_STRING)
+        .enable(EnumFeature.READ_ENUMS_USING_TO_STRING)
+        .disable(DateTimeFeature.ADJUST_DATES_TO_CONTEXT_TIME_ZONE)
+        .addModule(new RFC3339JavaTimeModule())
+        .build();
   }
 
   protected final String getDefaultBaseUri() {
