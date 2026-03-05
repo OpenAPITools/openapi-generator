db82accceef5257e4894657bc40a44bdc51c4523
Jorge <jorgerma@ext.inditex.com>
2026-03-05 17:50:41 +0100

    chore: initialize project structure and add initial files

--- samples/client/petstore/java/resttemplate-swagger1/src/main/java/org/openapitools/client/model/Category.java
+++ samples/client/petstore/java/resttemplate-swagger1/src/main/java/org/openapitools/client/model/Category.java
@@ -24,6 +24,7 @@ import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import com.fasterxml.jackson.annotation.JsonTypeName;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 /**
  * A category for a pet
