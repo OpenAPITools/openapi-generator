2d82875882263758ec3714c31c96829f146ee15c
Mateusz "Serafin" Gajewski <66972+wendigo@users.noreply.github.com>
2026-03-06 15:18:45 +0100

    Jackson deserializers fail when FAIL_ON_TRAILING_TOKENS is enabled due to parser.readValueAsTree()  (#23147)
    * Fix incorrect tree parsing with Jackson
    
    * Regenerate Java samples

--- samples/client/petstore/java/jersey3/src/main/java/org/openapitools/client/model/NullableShape.java
+++ samples/client/petstore/java/jersey3/src/main/java/org/openapitools/client/model/NullableShape.java
@@ -97,7 +97,7 @@ public class NullableShape extends AbstractOpenApiSchema {
 
         @Override
         public NullableShape deserialize(JsonParser jp, DeserializationContext ctxt) throws IOException, JsonProcessingException {
-            JsonNode tree = jp.readValueAsTree();
+            JsonNode tree = ctxt.readTree(jp);
             Object deserialized = null;
             NullableShape newNullableShape = new NullableShape();
             Map<String, Object> result2 = tree.traverse(jp.getCodec()).readValueAs(new TypeReference<Map<String, Object>>() {});
--- samples/client/petstore/java/native-async/src/main/java/org/openapitools/client/model/FruitReq.java
+++ samples/client/petstore/java/native-async/src/main/java/org/openapitools/client/model/FruitReq.java
@@ -87,7 +87,7 @@ public class FruitReq extends AbstractOpenApiSchema {
 
         @Override
         public FruitReq deserialize(JsonParser jp, DeserializationContext ctxt) throws IOException, JsonProcessingException {
-            JsonNode tree = jp.readValueAsTree();
+            JsonNode tree = ctxt.readTree(jp);
             Object deserialized = null;
             boolean typeCoercion = ctxt.isEnabled(MapperFeature.ALLOW_COERCION_OF_SCALARS);
             int match = 0;
