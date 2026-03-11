8da211d5deb1e3d95b4bc9bea327262518d6defd
Chrimle <28791817+Chrimle@users.noreply.github.com>
2026-03-12 00:08:22 +0100

    Update Generated Samples
    Signed-off-by: Chrimle <28791817+Chrimle@users.noreply.github.com>

--- samples/server/petstore/java-msf4j/src/gen/java/org/openapitools/model/AdditionalPropertiesAnyType.java
+++ samples/server/petstore/java-msf4j/src/gen/java/org/openapitools/model/AdditionalPropertiesAnyType.java
@@ -68,10 +68,7 @@ public class AdditionalPropertiesAnyType extends HashMap<String, Object>  {
    * (except the first line).
    */
   private String toIndentedString(Object o) {
-    if (o == null) {
-      return "null";
-    }
-    return o.toString().replace("\n", "\n    ");
+    return String.valueOf(o).replace("\n", "\n    ");
   }
 }
 
