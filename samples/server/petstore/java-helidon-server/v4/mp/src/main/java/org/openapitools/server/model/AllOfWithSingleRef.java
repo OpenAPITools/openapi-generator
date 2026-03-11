0381b6a3bb561534444f246743db3b6ac9d88f02
Chrimle <28791817+Chrimle@users.noreply.github.com>
2026-03-12 00:13:41 +0100

    Update Generated Samples
    Signed-off-by: Chrimle <28791817+Chrimle@users.noreply.github.com>

--- samples/server/petstore/java-helidon-server/v4/mp/src/main/java/org/openapitools/server/model/AllOfWithSingleRef.java
+++ samples/server/petstore/java-helidon-server/v4/mp/src/main/java/org/openapitools/server/model/AllOfWithSingleRef.java
@@ -90,10 +90,7 @@ public class AllOfWithSingleRef  {
    * (except the first line).
    */
   private static String toIndentedString(Object o) {
-    if (o == null) {
-      return "null";
-    }
-    return o.toString().replace("\n", "\n    ");
+    return String.valueOf(o).replace("\n", "\n    ");
   }
 }
 
