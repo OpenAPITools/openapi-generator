1437b1d4dd0c45db638347062daa57dbb5221e4a
Chrimle <28791817+Chrimle@users.noreply.github.com>
2026-03-12 00:10:38 +0100

    Update Generated Samples
    Signed-off-by: Chrimle <28791817+Chrimle@users.noreply.github.com>

--- samples/client/petstore/java/okhttp-gson-parcelableModel/src/main/java/org/openapitools/client/model/AdditionalPropertiesClass.java
+++ samples/client/petstore/java/okhttp-gson-parcelableModel/src/main/java/org/openapitools/client/model/AdditionalPropertiesClass.java
@@ -439,10 +439,7 @@ public class AdditionalPropertiesClass implements Parcelable {
    * (except the first line).
    */
   private String toIndentedString(Object o) {
-    if (o == null) {
-      return "null";
-    }
-    return o.toString().replace("\n", "\n    ");
+    return String.valueOf(o).replace("\n", "\n    ");
   }
 
 
