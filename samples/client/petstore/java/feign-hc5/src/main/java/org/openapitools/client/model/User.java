2909915696bbff49873af5b002fd09d3f9d04699
Chrimle <28791817+Chrimle@users.noreply.github.com>
2026-03-14 00:47:35 +0100

    Generate `java-*` Samples
    Signed-off-by: Chrimle <28791817+Chrimle@users.noreply.github.com>

--- samples/client/petstore/java/feign-hc5/src/main/java/org/openapitools/client/model/User.java
+++ samples/client/petstore/java/feign-hc5/src/main/java/org/openapitools/client/model/User.java
@@ -326,10 +326,7 @@ public class User {
    * (except the first line).
    */
   private String toIndentedString(Object o) {
-    if (o == null) {
-      return "null";
-    }
-    return o.toString().replace("\n", "\n    ");
+    return o == null ? "null" : o.toString().replace("\n", "\n    ");
   }
 
 }
--- samples/client/petstore/java/native-jackson3/src/main/java/org/openapitools/client/model/Whale.java
+++ samples/client/petstore/java/native-jackson3/src/main/java/org/openapitools/client/model/Whale.java
@@ -157,10 +157,7 @@ public class Whale {
    * (except the first line).
    */
   private String toIndentedString(Object o) {
-    if (o == null) {
-      return "null";
-    }
-    return o.toString().replace("\n", "\n    ");
+    return o == null ? "null" : o.toString().replace("\n", "\n    ");
   }
 
   /**
