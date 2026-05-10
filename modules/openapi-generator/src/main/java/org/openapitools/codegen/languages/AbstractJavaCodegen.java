00388d68fee161cd38c282b83b62530a7956b4ab
Johny Herangi <johnyherangi@gmail.com>
2026-05-10 15:28:07 +1200

    follow existing pattern for dealing with extensions that support single and multiple values

--- modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/AbstractJavaCodegen.java
+++ modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/AbstractJavaCodegen.java
@@ -2088,18 +2088,15 @@ public abstract class AbstractJavaCodegen extends DefaultCodegen implements Code
             }
         }
 
-        // normalize x-class-extra-annotation to always be a List<String> so that
-        // mustache templates can iterate over it for both single and multiple annotations.
-        // This also ensures allOf schemas with x-class-extra-annotation are handled correctly.
+        // make sure the x-class-extra-annotation is always a List and always at least empty
         for (ModelMap mo : objs.getModels()) {
             CodegenModel cm = mo.getModel();
-            Object annotation = cm.getVendorExtensions().get(VendorExtension.X_CLASS_EXTRA_ANNOTATION.getName());
-            if (annotation instanceof String) {
-                cm.getVendorExtensions().put(
-                        VendorExtension.X_CLASS_EXTRA_ANNOTATION.getName(),
-                        Collections.singletonList(annotation));
+            if (cm.getVendorExtensions().containsKey(VendorExtension.X_CLASS_EXTRA_ANNOTATION.getName())) {
+                List<String> xClassExtraAnnotation = getObjectAsStringList(cm.getVendorExtensions().get(VendorExtension.X_CLASS_EXTRA_ANNOTATION.getName()));
+                cm.getVendorExtensions().replace(VendorExtension.X_CLASS_EXTRA_ANNOTATION.getName(), xClassExtraAnnotation);
+            } else {
+                cm.getVendorExtensions().put(VendorExtension.X_CLASS_EXTRA_ANNOTATION.getName(), new ArrayList<String>());
             }
-            // if annotation is already a List (e.g. from a YAML sequence), leave it as-is
         }
 
         // skip interfaces predefined in open api spec in x-implements via additional property xImplementsSkip
