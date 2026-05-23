f5d9dced505ab0507caae168b80f510986bd77d9
Chrimle <28791817+Chrimle@users.noreply.github.com>
2026-05-24 01:50:04 +0200

    Update `DataChannel` Examples
    Signed-off-by: Chrimle <28791817+Chrimle@users.noreply.github.com>

--- samples/client/others/java/restclient-enum-in-multipart/src/main/java/org/openapitools/client/model/DataChannel.java
+++ samples/client/others/java/restclient-enum-in-multipart/src/main/java/org/openapitools/client/model/DataChannel.java
@@ -25,6 +25,7 @@ import com.fasterxml.jackson.annotation.JsonValue;
 /**
  * The transport-channel
  */
+@jakarta.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", comments = "Generator version: 7.23.0-SNAPSHOT")
 public enum DataChannel {
   
   BIKE("BIKE"),
