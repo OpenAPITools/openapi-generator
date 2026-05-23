a681b7957c52e83fe5392c515115ef0bb21c3938
Chrimle <28791817+Chrimle@users.noreply.github.com>
2026-05-24 01:42:27 +0200

    Update `SingleRefType` Examples
    Signed-off-by: Chrimle <28791817+Chrimle@users.noreply.github.com>

--- samples/client/petstore/java/apache-httpclient/src/main/java/org/openapitools/client/model/SingleRefType.java
+++ samples/client/petstore/java/apache-httpclient/src/main/java/org/openapitools/client/model/SingleRefType.java
@@ -27,6 +27,7 @@ import com.fasterxml.jackson.annotation.JsonValue;
 /**
  * Gets or Sets SingleRefType
  */
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", comments = "Generator version: 7.23.0-SNAPSHOT")
 public enum SingleRefType {
   
   ADMIN("admin"),
