6a4bd103ecd3b98834816e9e94ad0f07bcd62762
Jachym Metlicka <krisna.visnu@gmail.com>
2026-06-27 14:55:07 +0200

    add whitespace to retrigger tests

--- modules/openapi-generator/src/test/java/org/openapitools/codegen/kotlin/spring/KotlinSpringServerCodegenTest.java
+++ modules/openapi-generator/src/test/java/org/openapitools/codegen/kotlin/spring/KotlinSpringServerCodegenTest.java
@@ -6814,6 +6814,7 @@ public class KotlinSpringServerCodegenTest {
         );
     }
 
+
     /**
      * Regression test for https://github.com/OpenAPITools/openapi-generator/issues/24139
      * A property that $ref's an OAS 3.1 schema with type:[object,"null"] is nullable and must
