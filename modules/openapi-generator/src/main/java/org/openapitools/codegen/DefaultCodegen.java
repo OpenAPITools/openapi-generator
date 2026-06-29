9e6fcf25d869de15fa7f41858ecb2998723a28ac
Vaclav Haisman <vhaisman@gmail.com>
2026-06-29 21:51:19 +0200

    Escape streamed examples in annotations

--- modules/openapi-generator/src/main/java/org/openapitools/codegen/DefaultCodegen.java
+++ modules/openapi-generator/src/main/java/org/openapitools/codegen/DefaultCodegen.java
@@ -476,6 +476,7 @@ public class DefaultCodegen implements CodegenConfig {
                 .put("forwardslash", new ForwardSlashLambda())
                 .put("backslash", new BackSlashLambda())
                 .put("doublequote", new DoubleQuoteLambda())
+                .put("escapeJavaString", new EscapeJavaStringLambda())
                 .put("indented", new IndentedLambda())
                 .put("indented_8", new IndentedLambda(8, " ", false, false))
                 .put("indented_12", new IndentedLambda(12, " ", false, false))
