aeaa7d24ee1e21ec3c0eb38ff4be9d554d009681
Vaclav Haisman <vhaisman@gmail.com>
2026-06-29 22:19:52 +0200

    Stream Kotlin Spring line break removal lambda

--- modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/KotlinSpringServerCodegen.java
+++ modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/KotlinSpringServerCodegen.java
@@ -33,6 +33,7 @@ import org.openapitools.codegen.model.ModelMap;
 import org.openapitools.codegen.model.ModelsMap;
 import org.openapitools.codegen.model.OperationMap;
 import org.openapitools.codegen.model.OperationsMap;
+import org.openapitools.codegen.templating.mustache.RemoveLineBreakLambda;
 import org.openapitools.codegen.templating.mustache.SpringHttpStatusLambda;
 import org.openapitools.codegen.utils.ModelUtils;
 import org.openapitools.codegen.utils.URLPathUtils;
@@ -1001,8 +1002,7 @@ public class KotlinSpringServerCodegen extends AbstractKotlinCodegen
         // add lambda for mustache templates
         additionalProperties.put("lambdaEscapeInNormalString",
                 (Mustache.Lambda) (fragment, writer) -> writer.write(fragment.execute().replaceAll("([$\"\\\\])", "\\\\$1")));
-        additionalProperties.put("lambdaRemoveLineBreak",
-                (Mustache.Lambda) (fragment, writer) -> writer.write(fragment.execute().replaceAll("[\\r\\n]", "")));
+        additionalProperties.put("lambdaRemoveLineBreak", new RemoveLineBreakLambda());
     }
 
     @Override
