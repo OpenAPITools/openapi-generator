5bf1037ba794b1a8d842d54aacb9fddda2920965
Jachym Metlicka <krisna.visnu@gmail.com>
2026-07-07 08:25:50 +0200

    [KOTLIN-SPRING;JAVA-SPRING] bugfix: update JsonInclude annotations for optional+nullable fields to handle serialization correctly when openApiNullable = true (#24185)
    * fix: update JsonInclude annotations for optional+nullable fields to handle serialization correctly
    
    * fix: also for java-spring
    
    * update samples
    
    * update samples

--- samples/server/petstore/springboot/src/main/java/org/openapitools/model/ContainerDefaultValueDto.java
+++ samples/server/petstore/springboot/src/main/java/org/openapitools/model/ContainerDefaultValueDto.java
@@ -2,6 +2,7 @@ package org.openapitools.model;
 
 import java.net.URI;
 import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonTypeName;
@@ -29,12 +30,14 @@ import javax.annotation.Generated;
 @Generated(value = "org.openapitools.codegen.languages.SpringCodegen", comments = "Generator version: 7.24.0-SNAPSHOT")
 public class ContainerDefaultValueDto {
 
+  @JsonInclude(JsonInclude.Include.NON_ABSENT)
   private JsonNullable<List<String>> nullableArray = JsonNullable.<List<String>>undefined();
 
   private JsonNullable<List<String>> nullableRequiredArray = JsonNullable.<List<String>>undefined();
 
   private List<String> requiredArray = new ArrayList<>();
 
+  @JsonInclude(JsonInclude.Include.NON_ABSENT)
   private JsonNullable<List<String>> nullableArrayWithDefault = JsonNullable.<List<String>>undefined();
 
   public ContainerDefaultValueDto() {
