a450ff98ba6b6908e97f4c105c3686d08f163f80
Erwin de Haan <erwin.de.haan@calcasa.nl>
2026-07-16 15:51:44 +0200

    Update samples

--- samples/client/echo_api/python/openapi_client/api_client.py
+++ samples/client/echo_api/python/openapi_client/api_client.py
@@ -303,6 +303,7 @@ class ApiClient:
             # if not found, look for '1XX', '2XX', etc.
             response_type = response_types_map.get(str(response_data.status)[0] + "XX", None)
 
+        # If the response_type has not matched (eg. did not match the previous if statements) and the default response is available, use it.
         if response_type is None and str(response_data.status) not in response_types_map \
             and (not isinstance(response_data.status, int) or not 100 <= response_data.status <= 599 or str(response_data.status)[0] + "XX" not in response_types_map) \
             and 'default' in response_types_map:
