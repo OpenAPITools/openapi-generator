fd9d7b177cfb021f319395cf7133757819854978
Jean-François Côté <jfcote@gridsync.ca>
2026-07-21 13:18:36 -0400

    Tentative fix

--- samples/server/others/springboot-pre-authorize/src/main/java/org/openapitools/OpenApiGeneratorApplication.java
+++ samples/server/others/springboot-pre-authorize/src/main/java/org/openapitools/OpenApiGeneratorApplication.java
@@ -4,7 +4,11 @@ import com.fasterxml.jackson.databind.Module;
 import org.openapitools.jackson.nullable.JsonNullableModule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
@@ -19,6 +23,15 @@ import org.springframework.context.annotation.FullyQualifiedAnnotationBeanNameGe
 )
 @EnableMethodSecurity
 public class OpenApiGeneratorApplication {
+
+    @Bean
+    @ConditionalOnMissingBean(SecurityFilterChain.class)
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+            .csrf(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests(authorize -> authorize.anyRequest().permitAll())
+            .build();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(OpenApiGeneratorApplication.class, args);
