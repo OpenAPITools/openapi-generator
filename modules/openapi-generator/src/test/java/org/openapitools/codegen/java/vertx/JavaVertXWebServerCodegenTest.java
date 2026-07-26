32630990bec3865aabb50bdb4211120225e8030c
corbs9 <o.corbs9@gmail.com>
2026-07-27 00:53:30 +0100

    create additional tests for codegen

--- modules/openapi-generator/src/test/java/org/openapitools/codegen/java/vertx/JavaVertXWebServerCodegenTest.java
+++ modules/openapi-generator/src/test/java/org/openapitools/codegen/java/vertx/JavaVertXWebServerCodegenTest.java
@@ -1,9 +1,20 @@
 package org.openapitools.codegen.java.vertx;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import org.openapitools.codegen.ClientOptInput;
+import org.openapitools.codegen.DefaultGenerator;
+import org.openapitools.codegen.TestUtils;
 import org.openapitools.codegen.languages.JavaVertXWebServerCodegen;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class JavaVertXWebServerCodegenTest {
 
@@ -29,5 +40,51 @@ public class JavaVertXWebServerCodegenTest {
         underTest.processOpts();
 
         Assert.assertFalse(underTest.apiTemplateFiles().containsKey("apiImpl.mustache"));
+    }
+
+    @Test
+    public void itShouldRedactCredentialsInBodyParams() throws IOException {
+        Map<String, File> files = generatePetstoreServer();
+        String apiHandlerPath = files.keySet().stream()
+                .filter(path -> path.endsWith("UserApiHandler.java"))
+                .findFirst()
+                .orElseThrow(() -> new AssertionError("UserApiHandler.java not found"));
+
+        File userApiHandler = files.get(apiHandlerPath);
+        
+        TestUtils.assertFileContains(userApiHandler.toPath(), "logger.debug(\"Parameter user is (body omitted)\");");
+    }
+
+    @Test
+    public void itShouldCheckFileUploadEmptiness() throws IOException {
+        Map<String, File> files = generatePetstoreServer();
+        String apiHandlerPath = files.keySet().stream()
+                .filter(path -> path.endsWith("PetApiHandler.java"))
+                .findFirst()
+                .orElseThrow(() -> new AssertionError("PetApiHandler.java not found"));
+
+        File petApiHandler = files.get(apiHandlerPath);
+
+        TestUtils.assertFileContains(petApiHandler.toPath(), "if (routingContext.fileUploads().isEmpty()) {");
+        TestUtils.assertFileContains(petApiHandler.toPath(), "routingContext.fail(400);");
+    }
+
+    private Map<String, File> generatePetstoreServer() throws IOException {
+        File output = Files.createTempDirectory("test").toFile().getCanonicalFile();
+        output.deleteOnExit();
+
+        OpenAPI openAPI = TestUtils.parseFlattenSpec("src/test/resources/3_0/petstore.yaml");
+        DefaultGenerator defaultGenerator = new DefaultGenerator();
+        ClientOptInput clientOptInput = new ClientOptInput();
+        clientOptInput.openAPI(openAPI);
+
+        JavaVertXWebServerCodegen codegen = new JavaVertXWebServerCodegen();
+        codegen.setOutputDir(output.getAbsolutePath());
+
+        clientOptInput.config(codegen);
+        defaultGenerator.opts(clientOptInput);
+
+        return defaultGenerator.generate().stream()
+                .collect(Collectors.toMap(File::getPath, Function.identity()));
     }
 }
