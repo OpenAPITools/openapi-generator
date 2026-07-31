df2d81bb672fd5de03731ce25d6f34c7a53fc571
Kaan Kara <kaank431@gmail.com>
2026-07-31 20:24:39 +0300

    Fix nullable OpenAPI 3.1 container types for typescript-axios (#23778)
    Co-authored-by: Kaan <kaannkara@icloud.com>

--- modules/openapi-generator/src/main/java/org/openapitools/codegen/DefaultCodegen.java
+++ modules/openapi-generator/src/main/java/org/openapitools/codegen/DefaultCodegen.java
@@ -3924,7 +3924,9 @@ public class DefaultCodegen implements CodegenConfig {
         if (p.getWriteOnly() != null) {
             property.isWriteOnly = p.getWriteOnly();
         }
-        if (p.getNullable() != null) {
+        if (ModelUtils.isNullable(p)) {
+            property.isNullable = true;
+        } else if (p.getNullable() != null) {
             property.isNullable = p.getNullable();
         }
 
@@ -3976,7 +3978,9 @@ public class DefaultCodegen implements CodegenConfig {
         }
 
         // set isNullable using nullable or x-nullable in the schema
-        if (referencedSchema.getNullable() != null) {
+        if (ModelUtils.isNullable(referencedSchema)) {
+            property.isNullable = true;
+        } else if (referencedSchema.getNullable() != null) {
             property.isNullable = referencedSchema.getNullable();
         } else if (referencedSchema.getExtensions() != null &&
                 referencedSchema.getExtensions().containsKey(X_NULLABLE)) {
@@ -4079,7 +4083,9 @@ public class DefaultCodegen implements CodegenConfig {
         if (original != null) {
             p = original;
             // evaluate common attributes if defined in the top level
-            if (p.getNullable() != null) {
+            if (ModelUtils.isNullable(p)) {
+                property.isNullable = true;
+            } else if (p.getNullable() != null) {
                 property.isNullable = p.getNullable();
             } else if (p.getExtensions() != null && p.getExtensions().containsKey(X_NULLABLE)) {
                 property.isNullable = (Boolean) p.getExtensions().get(X_NULLABLE);
