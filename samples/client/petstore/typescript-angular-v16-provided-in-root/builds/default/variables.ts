25725d4f30102457681eca74d61b70c51a173643
William Cheng <wing328hk@gmail.com>
2026-08-02 12:15:37 +0800

    remove anuglar v16, v17 samples and tests in CI (#24541)

--- samples/client/petstore/typescript-angular-v16-provided-in-root/builds/default/variables.ts
+++
@@ -1,9 +0,0 @@
-import { InjectionToken } from '@angular/core';
-
-export const BASE_PATH = new InjectionToken<string>('basePath');
-export const COLLECTION_FORMATS = {
-    'csv': ',',
-    'tsv': '   ',
-    'ssv': ' ',
-    'pipes': '|'
-}
