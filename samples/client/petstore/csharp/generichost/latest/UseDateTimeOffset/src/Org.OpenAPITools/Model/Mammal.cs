71a84ff1e8bc6a8c086a6974449be435cf906c79
William Cheng <wing328hk@gmail.com>
2026-08-14 11:41:35 +0800

    fix(csharp): limit public oneOf constructors to operation inputs (#24700)
    Co-authored-by: Kanna Kim <kimkanna18@gmail.com>

--- samples/client/petstore/csharp/generichost/latest/UseDateTimeOffset/src/Org.OpenAPITools/Model/Mammal.cs
+++ samples/client/petstore/csharp/generichost/latest/UseDateTimeOffset/src/Org.OpenAPITools/Model/Mammal.cs
@@ -34,7 +34,7 @@ namespace Org.OpenAPITools.Model
         /// Initializes a new instance of the <see cref="Mammal" /> class.
         /// </summary>
         /// <param name="whale"></param>
-        public Mammal(Whale whale)
+        internal Mammal(Whale whale)
         {
             Whale = whale;
             OnCreated();
@@ -44,7 +44,7 @@ namespace Org.OpenAPITools.Model
         /// Initializes a new instance of the <see cref="Mammal" /> class.
         /// </summary>
         /// <param name="zebra"></param>
-        public Mammal(Zebra zebra)
+        internal Mammal(Zebra zebra)
         {
             Zebra = zebra;
             OnCreated();
@@ -54,7 +54,7 @@ namespace Org.OpenAPITools.Model
         /// Initializes a new instance of the <see cref="Mammal" /> class.
         /// </summary>
         /// <param name="pig"></param>
-        public Mammal(Pig pig)
+        internal Mammal(Pig pig)
         {
             Pig = pig;
             OnCreated();
