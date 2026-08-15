04315dc8eb6aee5c162f512c088d45789292dfce
jpfinne <jeanpaul@finne.be>
2026-08-15 16:24:35 +0200

    Fix failing removeAnnotationsWithExclusion

--- modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/AbstractJavaCodegen.java
+++ modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/AbstractJavaCodegen.java
@@ -2111,6 +2111,9 @@ public abstract class AbstractJavaCodegen extends DefaultCodegen implements Code
      */
     public String removeAnnotationsWithExclusion(String dataType, String annotationToKeep) {
         if (dataType != null && dataType.contains("@")) {
+            if (annotationToKeep == null) {
+                return dataType.replaceAll("(?:(?i)@[a-z0-9\\.]*+([(].*[)]|\\s*))*+", "");
+            }
             annotationToKeep += " ";
             boolean annotationPresent = annotationToKeep != null && dataType.indexOf(annotationToKeep) >=0;
             if (annotationPresent) {
