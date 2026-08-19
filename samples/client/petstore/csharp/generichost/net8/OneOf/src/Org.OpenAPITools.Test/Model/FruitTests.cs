32deebc6b6b10e9f08186846fbe972759a382537
William Cheng <wing328hk@gmail.com>
2026-08-19 20:19:20 +0800

    update samples

--- samples/client/petstore/csharp/generichost/net8/OneOf/src/Org.OpenAPITools.Test/Model/FruitTests.cs
+++ samples/client/petstore/csharp/generichost/net8/OneOf/src/Org.OpenAPITools.Test/Model/FruitTests.cs
@@ -17,7 +17,6 @@ using System.Collections.Generic;
 using Org.OpenAPITools.Model;
 using Org.OpenAPITools.Client;
 using System.Reflection;
-using System.Text.Json;
 
 namespace Org.OpenAPITools.Test.Model
 {
@@ -61,41 +60,6 @@ namespace Org.OpenAPITools.Test.Model
         public void ColorTest()
         {
             // TODO unit test for the property 'Color'
-        }
-
-        [Fact]
-        public void SerializeOneOfReferencedObject()
-        {
-            var options = new JsonSerializerOptions();
-            options.Converters.Add(new AppleJsonConverter());
-            options.Converters.Add(new BananaJsonConverter());
-            options.Converters.Add(new FruitJsonConverter());
-            options.Converters.Add(new OrangeJsonConverter());
-
-            Fruit fruit = new Fruit(new Apple("red"));
-
-            string json = JsonSerializer.Serialize(fruit, options);
-
-            Assert.Equal("{\"kind\":\"red\"}", json);
-        }
-
-        [Fact]
-        public void SerializeOneOfReferencedObjectWithSiblingProperty()
-        {
-            var options = new JsonSerializerOptions();
-            options.Converters.Add(new AppleJsonConverter());
-            options.Converters.Add(new BananaJsonConverter());
-            options.Converters.Add(new FruitJsonConverter());
-            options.Converters.Add(new OrangeJsonConverter());
-
-            Fruit fruit = new Fruit(
-                new Apple("red"),
-                new Option<string?>("green")
-            );
-
-            string json = JsonSerializer.Serialize(fruit, options);
-
-            Assert.Equal("{\"kind\":\"red\",\"color\":\"green\"}", json);
         }
     }
 }
