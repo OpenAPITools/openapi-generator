08933e05e65afc33fdbb894bb8060f510e2c32c9
SimonIT <simonit.orig@gmail.com>
2026-09-10 14:40:10 +0200

    feat(rust-client): Add support for unsigned integer and more signed integer formats

--- modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/RustClientCodegen.java
+++ modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/RustClientCodegen.java
@@ -696,10 +696,22 @@ public class RustClientCodegen extends AbstractRustCodegen implements CodegenCon
                 }
             } else {
                 switch (p.getFormat()) {
+                    case "int8":
+                        return unsigned ? "u8" : "i8";
+                    case "int16":
+                        return unsigned ? "u16" : "i16";
                     case "int32":
                         return unsigned ? "u32" : "i32";
                     case "int64":
                         return unsigned ? "u64" : "i64";
+                    case "uint8":
+                        return "u8";
+                    case "uint16":
+                        return "u16";
+                    case "uint32":
+                        return "u32";
+                    case "uint64":
+                        return "u64";
                 }
             }
         }
