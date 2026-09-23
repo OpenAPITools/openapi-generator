e8164d826a06dfd85d0cc1e56120ab72e330017d
William Cheng <wing328hk@gmail.com>
2026-09-23 18:02:10 +0800

    set min. required ruby version to 2.7

--- modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/RubyClientCodegen.java
+++ modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/RubyClientCodegen.java
@@ -69,7 +69,7 @@ public class RubyClientCodegen extends AbstractRubyCodegen {
     protected String specFolder = "spec";
     protected String libFolder = "lib";
     @Setter protected String gemLicense = "unlicense";
-    @Setter protected String gemRequiredRubyVersion = ">= 1.9";
+    @Setter protected String gemRequiredRubyVersion = ">= 2.7";
     @Setter protected String gemHomepage = "https://openapitools.org";
     @Setter protected String gemSummary = "A Ruby SDK for the REST API";
     @Setter protected String gemDescription = "This gem maps to a REST API";
